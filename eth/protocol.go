d86c5643f1946320bdb8b0adfd4a839715983441
andrew <andrey.ashikhmin@gmail.com>
2019-07-10 15:53:17 +0300

    restore eth/63 since geth 1.9 dropped eth/62

--- eth/protocol.go
+++ eth/protocol.go
@@ -38,10 +38,10 @@ const (
 var ProtocolName = "eth"
 
 // ProtocolVersions are the supported versions of the eth protocol (first is primary).
-var ProtocolVersions = []uint{eth62}
+var ProtocolVersions = []uint{eth63, eth62}
 
 // ProtocolLengths are the number of implemented message corresponding to different protocol versions.
-var ProtocolLengths = []uint64{8}
+var ProtocolLengths = []uint64{17, 8}
 
 // ProtocolMaxMsgSize is the maximum cap on the size of a protocol message.
 const ProtocolMaxMsgSize = 10 * 1024 * 1024
