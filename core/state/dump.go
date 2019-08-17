26f963a7a97e495e4e369a073cb1d54d74aeab3d
b00ris <b00ris@mail.ru>
2019-08-17 10:47:05 +0300

    change addressLength to hashLength

--- core/state/dump.go
+++ core/state/dump.go
@@ -81,8 +81,8 @@ func (self *TrieDbState) RawDump() Dump {
 			return false, err
 		}
 
-		err = self.db.Walk(StorageBucket, GenerateStoragePrefix(addrHash, acc.GetIncarnation()), uint(common.AddressLength*8+8), func(ks, vs []byte) (bool, error) {
-			key := self.GetKey(ks[common.AddressLength+8:]) //remove account address and version from composite key
+		err = self.db.Walk(StorageBucket, GenerateStoragePrefix(addrHash, acc.GetIncarnation()), uint(common.HashLength*8+8), func(ks, vs []byte) (bool, error) {
+			key := self.GetKey(ks[common.HashLength+8:]) //remove account address and version from composite key
 			account.Storage[common.BytesToHash(key).String()] = common.Bytes2Hex(vs)
 			return true, nil
 		})
