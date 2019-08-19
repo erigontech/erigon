a50b579a4f8063f80c6cd504147cb1dba84e4054
b00ris <b00ris@mail.ru>
2019-08-19 09:23:03 +0300

    extract add extra rlp level function

--- core/state/repair.go
+++ core/state/repair.go
@@ -400,17 +400,7 @@ func (rds *RepairDbState) WriteAccountStorage(address common.Address, incarnatio
 		rds.storageUpdates[address] = m
 	}
 	if len(v) > 0 {
-		// Write into 1 extra RLP level
-		var vv []byte
-		if len(v) > 1 || v[0] >= 128 {
-			vv = make([]byte, len(v)+1)
-			vv[0] = byte(128 + len(v))
-			copy(vv[1:], v)
-		} else {
-			vv = make([]byte, 1)
-			vv[0] = v[0]
-		}
-		m[seckey] = vv
+		m[seckey] = AddExtraRLPLevel(v)
 	} else {
 		m[seckey] = nil
 	}
--- core/state/stateless.go
+++ core/state/stateless.go
@@ -561,17 +561,7 @@ func (s *Stateless) WriteAccountStorage(address common.Address, incarnation uint
 	h.sha.Read(secKey[:])
 	v := bytes.TrimLeft(value[:], "\x00")
 	if len(v) > 0 {
-		// Write into 1 extra RLP level
-		var vv []byte
-		if len(v) > 1 || v[0] >= 128 {
-			vv = make([]byte, len(v)+1)
-			vv[0] = byte(128 + len(v))
-			copy(vv[1:], v)
-		} else {
-			vv = make([]byte, 1)
-			vv[0] = v[0]
-		}
-		m[secKey] = vv
+		m[secKey] = AddExtraRLPLevel(v)
 	} else {
 		m[secKey] = nil
 	}
