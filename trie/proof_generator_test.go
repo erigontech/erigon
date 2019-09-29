d5d43146e400a21b130516f8736cc3e0e66cd2e1
Alexey Akhunov <akhounov@gmail.com>
2019-09-29 22:46:25 +0100

    Fix lint

--- trie/proof_generator_test.go
+++ trie/proof_generator_test.go
@@ -7,7 +7,9 @@ import (
 
 func TestKeyValue(t *testing.T) {
 	bwb := NewBlockWitnessBuilder()
-	bwb.keyValue([]byte("key"), []byte("value"))
+	if err := bwb.keyValue([]byte("key"), []byte("value")); err != nil {
+		t.Errorf("Could not set key-value: %v", err)
+	}
 	fmt.Printf("key buffer: %x\n", bwb.Keys.buffer.Bytes())
 	fmt.Printf("value buffer: %x\n", bwb.Values.buffer.Bytes())
 }
