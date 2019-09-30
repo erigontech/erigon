85c6f7d9964998ab7eb4d8b1a2bdd71193b5dd85
Alexey Akhunov <akhounov@gmail.com>
2019-09-30 20:58:41 +0100

    Fix lint

--- trie/proof_generator_test.go
+++ trie/proof_generator_test.go
@@ -19,3 +19,13 @@ func TestKeyValue(t *testing.T) {
 		t.Errorf("Expected 0x4576616c7565 in values tape, got: %x", bwb.Values.buffer.Bytes())
 	}
 }
+
+func TestLeaf(t *testing.T) {
+	bwb := NewBlockWitnessBuilder()
+	if err := bwb.leaf(56); err != nil {
+		t.Errorf("Could not call leaf: %v", err)
+	}
+	if !bytes.Equal(common.FromHex("0x001838"), bwb.Structure.buffer.Bytes()) {
+		t.Errorf("Expected 0x001838 in structure tape, got: %x", bwb.Structure.buffer.Bytes())
+	}
+}
