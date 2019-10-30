ffc1a54c8968116eddf9485c88743046293801a7
ledgerwatch <akhounov@gmail.com>
2019-10-30 17:45:43 +0000

    Merge pull request #134 from ledgerwatch/tables-visualization
    added visualization for tables

--- cmd/pics/state.go
+++ cmd/pics/state.go
@@ -5,14 +5,7 @@ import (
 	"context"
 	"encoding/binary"
 	"fmt"
-	"math/big"
-	"os"
-	"os/exec"
-	"sort"
-
-	"github.com/ledgerwatch/turbo-geth/trie"
-	"github.com/ledgerwatch/turbo-geth/visual"
-
+	"github.com/ledgerwatch/bolt"
 	"github.com/ledgerwatch/turbo-geth/accounts/abi/bind"
 	"github.com/ledgerwatch/turbo-geth/accounts/abi/bind/backends"
 	"github.com/ledgerwatch/turbo-geth/cmd/pics/contracts"
@@ -26,6 +19,12 @@ import (
 	"github.com/ledgerwatch/turbo-geth/crypto"
 	"github.com/ledgerwatch/turbo-geth/ethdb"
 	"github.com/ledgerwatch/turbo-geth/params"
+	"github.com/ledgerwatch/turbo-geth/trie"
+	"github.com/ledgerwatch/turbo-geth/visual"
+	"math/big"
+	"os"
+	"os/exec"
+	"sort"
 )
 
 func constructCodeMap(tds *state.TrieDbState) (map[common.Hash][]byte, error) {
@@ -55,7 +54,7 @@ func statePicture(t *trie.Trie, codeMap map[common.Hash][]byte, number int, keyC
 		indexColors = visual.QuadIndexColors
 		fontColors = visual.QuadFontColors
 	}
-	visual.StartGraph(f)
+	visual.StartGraph(f, false)
 	trie.Visual(t, f, &trie.VisualOpts{
 		Highlights:     highlights,
 		IndexColors:    indexColors,
@@ -84,7 +83,7 @@ func keyTape(t *trie.Trie, number int) error {
 	if err != nil {
 		return err
 	}
-	visual.StartGraph(f)
+	visual.StartGraph(f, false)
 	fk := trie.FullKeys(t)
 	sort.Strings(fk)
 	for i, key := range fk {
@@ -109,19 +108,80 @@ func keyTape(t *trie.Trie, number int) error {
 	return nil
 }
 
+func stateDatabaseMap(db *bolt.DB, number int) error {
+	filename := fmt.Sprintf("state_%d.dot", number)
+	f, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	i := 0
+	visual.StartGraph(f, true)
+
+	if err := db.View(func(readTx *bolt.Tx) error {
+		return readTx.ForEach(func(name []byte, b *bolt.Bucket) error {
+
+			visual.StartCluster(f, i, string(name))
+
+			if err := b.ForEach(func(k, v []byte) error {
+				fmt.Fprintf(f, `k_%d -> v_%d`, i, i)
+				keyKeyBytes := &trie.Keybytes{
+					Data:        k,
+					Odd:         false,
+					Terminating: false,
+				}
+				valKeyBytes := &trie.Keybytes{
+					Data:        v,
+					Odd:         false,
+					Terminating: false,
+				}
+				key := keyKeyBytes.ToHex()
+				val := valKeyBytes.ToHex()
+				visual.Horizontal(f, key, 0, fmt.Sprintf("k_%d", i), visual.HexIndexColors, visual.HexFontColors, 110)
+				if len(val) > 0 {
+					if len(val) > 32 {
+						shortenedVal := val[:32]
+						visual.Horizontal(f, shortenedVal, 0, fmt.Sprintf("v_%d", i), visual.HexIndexColors, visual.HexFontColors, 100)
+					} else {
+						visual.Horizontal(f, val, 0, fmt.Sprintf("v_%d", i), visual.HexIndexColors, visual.HexFontColors, 110)
+					}
+				} else {
+					visual.Circle(f, fmt.Sprintf("v_%d", i), "...", false)
+				}
+				i++
+				return nil
+			}); err != nil {
+				return err
+			}
+			visual.EndCluster(f)
+			return nil
+		})
+	}); err != nil {
+		panic(err)
+	}
+	visual.EndGraph(f)
+	if err := f.Close(); err != nil {
+		return err
+	}
+	cmd := exec.Command("dot", "-Tpng:gd", "-O", filename)
+	if output, err := cmd.CombinedOutput(); err != nil {
+		fmt.Printf("error: %v, output: %s\n", err, output)
+	}
+	return nil
+}
+
 func initialState1() error {
 	fmt.Printf("Initial state 1\n")
 	// Configure and generate a sample block chain
 	var (
-		db       = ethdb.NewMemDatabase()
-		key, _   = crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291")
-		key1, _  = crypto.HexToECDSA("49a7b37aa6f6645917e7b807e9d1c00d4fa71f18343b0d4122a4d2df64dd6fee")
-		key2, _  = crypto.HexToECDSA("8a1f9a8f95be41cd7ccb6168179afb4504aefe388d1e14474d32c45c72ce7b7a")
-		address  = crypto.PubkeyToAddress(key.PublicKey)
-		address1 = crypto.PubkeyToAddress(key1.PublicKey)
-		address2 = crypto.PubkeyToAddress(key2.PublicKey)
-		theAddr  = common.Address{1}
-		gspec    = &core.Genesis{
+		db, dbBolt = ethdb.NewMemDatabase2()
+		key, _     = crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291")
+		key1, _    = crypto.HexToECDSA("49a7b37aa6f6645917e7b807e9d1c00d4fa71f18343b0d4122a4d2df64dd6fee")
+		key2, _    = crypto.HexToECDSA("8a1f9a8f95be41cd7ccb6168179afb4504aefe388d1e14474d32c45c72ce7b7a")
+		address    = crypto.PubkeyToAddress(key.PublicKey)
+		address1   = crypto.PubkeyToAddress(key1.PublicKey)
+		address2   = crypto.PubkeyToAddress(key2.PublicKey)
+		theAddr    = common.Address{1}
+		gspec      = &core.Genesis{
 			Config: &params.ChainConfig{
 				ChainID:             big.NewInt(1),
 				HomesteadBlock:      new(big.Int),
@@ -415,5 +475,8 @@ func initialState1() error {
 		return err
 	}
 
+	if err = stateDatabaseMap(dbBolt, 16); err != nil {
+		return err
+	}
 	return nil
 }
