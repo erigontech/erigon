3494e4e62e265dc91332406957c8fcd1caa2e1b4
b00ris <b00ris@mail.ru>
2019-11-07 18:19:35 +0300

    add comment

--- core/pruner.go
+++ core/pruner.go
@@ -90,6 +90,11 @@ func (p *BasicPruner) pruningLoop(db ethdb.Database) {
 }
 
 func calculateNumOfPrunedBlocks(currentBlock, lastPrunedBlock uint64, blocksBeforePruning uint64, blocksBatch uint64) (uint64, uint64, bool) {
+	//underflow see https://github.com/ledgerwatch/turbo-geth/issues/115
+	if currentBlock <= lastPrunedBlock {
+		return lastPrunedBlock, lastPrunedBlock, false
+	}
+
 	diff := currentBlock - lastPrunedBlock
 	if diff <= blocksBeforePruning {
 		return lastPrunedBlock, lastPrunedBlock, false
