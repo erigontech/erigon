2d5d17cffca8ebf24030eb748d2ccac7e1399bd2
Igor Mandrigin <i@mandrigin.ru>
2019-11-25 16:50:49 +0100

    Write proper stats to the CSV file; restore charting capabilities.

--- cmd/state/stateless/stateless_chart.go
+++ cmd/state/stateless/stateless_chart.go
@@ -5,118 +5,145 @@ import (
 	"bytes"
 	"encoding/csv"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
+	"strconv"
 
 	chart "github.com/wcharczuk/go-chart"
 )
 
-func DoKVChart(filename string, right []int, chartFileName string, start int, startColor int) {
+func MakeChart(filename string, right []int, chartFileName string, start uint64, startColor int) error {
 	file, err := os.Open(filename)
-	check(err)
+	if err != nil {
+		return err
+	}
 	defer file.Close()
-	reader := csv.NewReader(bufio.NewReader(file))
-	var blocks []float64
-	var vals [22][]float64
-	count := 0
-	for records, _ := reader.Read(); len(records) == 16; records, _ = reader.Read() {
-		count++
-		if count < start {
+
+	columnNames, vals, err := readCsv(file, start)
+	if err != nil {
+		return err
+	}
+
+	window := 1024
+	blockNr := 0
+
+	movingAvgs := dataToMovingAvgs(vals[blockNr+1:], window)
+	movingBlock := vals[blockNr][window-1:]
+
+	graph1 := makeChart(movingBlock, movingAvgs, columnNames[blockNr+1:], startColor, right)
+
+	buffer := bytes.NewBuffer([]byte{})
+	if err = graph1.Render(chart.PNG, buffer); err != nil {
+		return err
+	}
+	if err := ioutil.WriteFile(chartFileName, buffer.Bytes(), 0644); err != nil {
+		return err
+	}
+	fmt.Printf("%s\n", chartFileName)
+	return nil
+}
+
+func seriesToMovingAvgs(data []float64, window int) []float64 {
+	if window < 1 {
+		panic("window should be >= 1")
+	}
+
+	if len(data) < window {
+		fmt.Fprintf(os.Stderr, "len of data is too short for sliding avgs: %d, will use raw data\n", len(data))
+		window = 1
+	}
+
+	result := make([]float64, len(data)-(window-1))
+
+	sum := float64(0)
+
+	for i := 0; i < len(result); i++ {
+		if i == 0 {
+			for j := 0; j < window; j++ {
+				sum += data[j]
+			}
+		} else {
+			sum += data[window-1+i]
+			sum -= data[i]
+		}
+		result[i] = sum / float64(window)
+	}
+
+	return result
+}
+
+func dataToMovingAvgs(data [][]float64, window int) [][]float64 {
+	result := make([][]float64, len(data))
+
+	for i := 0; i < len(data); i++ {
+		result[i] = seriesToMovingAvgs(data[i], window)
+	}
+
+	return result
+}
+
+func readCsv(r io.Reader, from uint64) (header []string, data [][]float64, err error) {
+	reader := csv.NewReader(bufio.NewReader(r))
+
+	header, err = reader.Read()
+	if err != nil {
+		return
+	}
+
+	current := uint64(0)
+
+	data = make([][]float64, len(header))
+
+	for records, _ := reader.Read(); len(records) == len(header); records, _ = reader.Read() {
+		current++
+		if current < from {
 			continue
 		}
-		blocks = append(blocks, parseFloat64(records[0])/1000000.0)
-		for i := 0; i < 22; i++ {
-			cProofs := 4.0*parseFloat64(records[2]) + 32.0*parseFloat64(records[3]) + parseFloat64(records[11]) + parseFloat64(records[12])
-			proofs := 4.0*parseFloat64(records[7]) + 32.0*parseFloat64(records[8]) + parseFloat64(records[14]) + parseFloat64(records[15])
-			switch i {
-			case 1, 6:
-				vals[i] = append(vals[i], 4.0*parseFloat64(records[i+1]))
-			case 2, 7:
-				vals[i] = append(vals[i], 32.0*parseFloat64(records[i+1]))
-			case 15:
-				vals[i] = append(vals[i], cProofs)
-			case 16:
-				vals[i] = append(vals[i], proofs)
-			case 17:
-				vals[i] = append(vals[i], cProofs+proofs+parseFloat64(records[13]))
-			case 18:
-				vals[i] = append(vals[i], 4.0*parseFloat64(records[2])+4.0*parseFloat64(records[7]))
-			case 19:
-				vals[i] = append(vals[i], 4.0*parseFloat64(records[2])+4.0*parseFloat64(records[7])+
-					parseFloat64(records[3])+parseFloat64(records[4])+parseFloat64(records[10])+parseFloat64(records[11]))
-			case 20:
-				vals[i] = append(vals[i], 4.0*parseFloat64(records[2])+4.0*parseFloat64(records[7])+
-					parseFloat64(records[4])+parseFloat64(records[5])+parseFloat64(records[10])+parseFloat64(records[11])+
-					32.0*parseFloat64(records[3])+32.0*parseFloat64(records[8]))
-			case 21:
-				vals[i] = append(vals[i], 4.0*parseFloat64(records[2])+4.0*parseFloat64(records[7])+
-					parseFloat64(records[4])+parseFloat64(records[5])+parseFloat64(records[10])+parseFloat64(records[11])+
-					32.0*parseFloat64(records[3])+32.0*parseFloat64(records[8])+parseFloat64(records[13]))
-			default:
-				vals[i] = append(vals[i], parseFloat64(records[i+1]))
+		for i := 0; i < len(records); i++ {
+			var r float64
+			if r, err = strconv.ParseFloat(records[i], 64); err != nil {
+				return
 			}
-		}
-	}
-	var windowSums [22]float64
-	window := 1024
-	var movingAvgs [22][]float64
-	for i := 0; i < 22; i++ {
-		movingAvgs[i] = make([]float64, len(blocks)-(window-1))
-	}
-	for j := 0; j < len(blocks); j++ {
-		for i := 0; i < 22; i++ {
-			windowSums[i] += vals[i][j]
-		}
-		if j >= window {
-			for i := 0; i < 22; i++ {
-				windowSums[i] -= vals[i][j-window]
+
+			if data[i] == nil {
+				data[i] = make([]float64, 0)
 			}
-		}
-		if j >= window-1 {
-			for i := 0; i < 22; i++ {
-				movingAvgs[i][j-window+1] = windowSums[i] / float64(window)
-			}
+
+			data[i] = append(data[i], r)
 		}
 	}
-	movingBlock := blocks[window-1:]
-	seriesNames := [22]string{
-		"Number of contracts",
-		"Contract masks",
-		"Contract hashes",
-		"Number of contract leaf keys",
-		"Number of contract leaf vals",
-		"Number of contract codes",
-		"Masks",
-		"Hashes",
-		"Number of leaf keys",
-		"Number of leaf values",
-		"Total size of contract leaf keys",
-		"Total size of contract leaf vals",
-		"Total size of codes",
-		"Total size of leaf keys",
-		"Total size of leaf vals",
-		"Block proofs (contracts only)",
-		"Block proofs (without contracts)",
-		"Block proofs (total)",
-		"Structure (total)",
-		"Leaves (total)",
-		"Hashes (total)",
-		"Code (total)",
-	}
+	return
+}
+
+func makeChart(xValues []float64, yValues [][]float64, columnNames []string, startColor int, filter []int) chart.Chart {
 	currentColor := startColor
-	series := make([]chart.Series, len(right))
-	for i, r := range right {
+
+	if len(filter) == 0 {
+		// include everything
+		filter = make([]int, len(yValues))
+		for i := 0; i < len(filter); i++ {
+			filter[i] = i
+		}
+	}
+
+	series := make([]chart.Series, len(filter))
+
+	for i, r := range filter {
+		if r < 0 || r >= len(yValues) {
+			fmt.Printf("invalid filter value found: %d, ignoring\n", r)
+			continue
+		}
+
 		s := &chart.ContinuousSeries{
-			Name: seriesNames[r],
+			Name: columnNames[r],
 			Style: chart.Style{
 				Show:        true,
 				StrokeWidth: 0.0,
 				StrokeColor: chartColors[currentColor],
-				FillColor:   chartColors[currentColor],
-				//FillColor:   chartColors[currentColor].WithAlpha(100),
 			},
-			XValues: movingBlock,
-			YValues: movingAvgs[r],
+			XValues: xValues,
+			YValues: yValues[r],
 		}
 		currentColor++
 		series[i] = s
@@ -145,43 +172,25 @@ func DoKVChart(filename string, right []int, chartFileName string, start int, st
 				StrokeColor: chart.ColorBlack,
 				StrokeWidth: 1.0,
 			},
-			//GridLines: days(),
 		},
-		/*
-			YAxisSecondary: chart.YAxis{
-				NameStyle: chart.StyleShow(),
-				Style: chart.StyleShow(),
-				TickStyle: chart.Style{
-					TextRotationDegrees: 45.0,
-				},
-				ValueFormatter: func(v interface{}) string {
-					return fmt.Sprintf("%d", int(v.(float64)))
-				},
-			},
-		*/
 		XAxis: chart.XAxis{
 			Name: "Blocks, million",
 			Style: chart.Style{
 				Show: true,
 			},
 			ValueFormatter: func(v interface{}) string {
-				return fmt.Sprintf("%.3fm", v.(float64))
+				return fmt.Sprintf("%.3fm", v.(float64)/float64(1*1000*1000))
 			},
 			GridMajorStyle: chart.Style{
 				Show:        true,
 				StrokeColor: chart.ColorAlternateGray,
 				StrokeWidth: 1.0,
 			},
-			//GridLines: blockMillions(),
 		},
 		Series: series,
 	}
 
 	graph1.Elements = []chart.Renderable{chart.LegendThin(&graph1)}
 
-	buffer := bytes.NewBuffer([]byte{})
-	err = graph1.Render(chart.PNG, buffer)
-	check(err)
-	err = ioutil.WriteFile(chartFileName, buffer.Bytes(), 0644)
-	check(err)
+	return graph1
 }
