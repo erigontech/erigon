7dc94482529f9f46f96210a8675db5880b74d61d
alex.sharov <alex.sharov@lazada.com>
2019-12-16 20:36:08 +0600

    make linter happy

--- ethdb/remote/remote_bolt_db.go
+++ ethdb/remote/remote_bolt_db.go
@@ -326,7 +326,6 @@ func (db *BoltDatabase) WalkAsOf(bucket, hBucket, startkey []byte, fixedbits uin
 			var cmp int
 			if k == nil {
 				if hK == nil {
-					goOn = false
 					break
 				} else {
 					cmp = 1
@@ -477,7 +476,6 @@ func (db *BoltDatabase) MultiWalkAsOf(bucket, hBucket []byte, startkeys [][]byte
 			var cmp int
 			if !fit {
 				if !hKFit {
-					goOn = false
 					break
 				} else {
 					cmp = 1
