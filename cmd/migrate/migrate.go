0e4b370880ab47efd025d4aaafc4f5d79b2aefb6
Giulio <giulio.rebuffo@gmail.com>
2019-12-30 21:30:26 +0100

    fixed

--- cmd/migrate/migrate.go
+++ cmd/migrate/migrate.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/ledgerwatch/bolt"
+	"github.com/ledgerwatch/turbo-geth/common"
 	"github.com/ledgerwatch/turbo-geth/common/dbutils"
 )
 
@@ -30,6 +31,13 @@ func convertDatabaseToCBOR(db *bolt.DB, maxTxOperations uint) error {
 			var accountBucket *bolt.Bucket = tx.Bucket(dbutils.AccountsBucket)
 			c := accountBucket.Cursor()
 
+			if k == nil {
+				k, v = c.First()
+			} else {
+				k, v = c.Seek(k)
+				k, v = c.Next()
+			}
+
 			for k, v = c.Seek(k); k != nil; k, v = c.Next() {
 
 				enc, err := encodeToCBOR(v)
@@ -49,6 +57,7 @@ func convertDatabaseToCBOR(db *bolt.DB, maxTxOperations uint) error {
 			if k == nil {
 				done = true
 			}
+			k = common.CopyBytes(k)
 			return nil
 		})
 
