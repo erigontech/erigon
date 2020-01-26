9598c5b5983f1cf48c5feb59770eda154b3ed72e
alex.sharov <alex.sharov@lazada.com>
2020-01-26 11:08:56 +0700

    commit to add more tests

--- trie/resolver_stateful_cached.go
+++ trie/resolver_stateful_cached.go
@@ -8,9 +8,11 @@ import (
 	"strings"
 
 	"github.com/ledgerwatch/bolt"
+	"github.com/ledgerwatch/turbo-geth/common"
 	"github.com/ledgerwatch/turbo-geth/common/dbutils"
 	"github.com/ledgerwatch/turbo-geth/common/hexutil"
 	"github.com/ledgerwatch/turbo-geth/ethdb"
+	"github.com/ledgerwatch/turbo-geth/trie/rlphacks"
 )
 
 type ResolverStatefulCached struct {
@@ -75,6 +77,16 @@ func (tr *ResolverStatefulCached) RebuildTrie(
 	}
 	boltDb := typed.GetDb()
 
+	if err := boltDb.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists(dbutils.IntermediateTrieHashesBucket, false)
+		if err != nil {
+			return err
+		}
+		return nil
+	}); err != nil {
+		return err
+	}
+
 	var err error
 	if accounts {
 		if historical {
@@ -99,17 +111,97 @@ func (tr *ResolverStatefulCached) RebuildTrie(
 	return tr.finaliseRoot()
 }
 
-//func (tr *ResolverStatefulCached) Walker2(isAccount bool, keyIdx int, k []byte, v []byte, useCache bool) error {
-//	// Algo here. Make hashgen
-//	return tr.Walker(isAccount bool, keyIdx, k, v)
-//}
-
 func (tr *ResolverStatefulCached) WalkerAccounts(keyIdx int, k []byte, v []byte, useCache bool) error {
-	return tr.Walker(true, keyIdx, k, v)
+	return tr.Walker(true, useCache, keyIdx, k, v)
 }
 
 func (tr *ResolverStatefulCached) WalkerStorage(keyIdx int, k []byte, v []byte, useCache bool) error {
-	return tr.Walker(false, keyIdx, k, v)
+	return tr.Walker(false, useCache, keyIdx, k, v)
+}
+
+// Walker - k, v - shouldn't be reused in the caller's code
+func (tr *ResolverStatefulCached) Walker(isAccount bool, useCache bool, keyIdx int, k []byte, v []byte) error {
+	//fmt.Printf("keyIdx: %d key:%x  value:%x, accounts: %t\n", keyIdx, k, v, tr.accounts)
+	if keyIdx != tr.keyIdx {
+		if err := tr.finaliseRoot(); err != nil {
+			return err
+		}
+		tr.hb.Reset()
+		tr.groups = nil
+		tr.keyIdx = keyIdx
+		tr.currentReq = tr.requests[tr.reqIndices[keyIdx]]
+		tr.currentRs = tr.rss[keyIdx]
+		tr.curr.Reset()
+	}
+	if len(v) > 0 {
+		tr.curr.Reset()
+		tr.curr.Write(tr.succ.Bytes())
+		tr.succ.Reset()
+		skip := tr.currentReq.extResolvePos // how many first nibbles to skip
+		i := 0
+		for _, b := range k {
+			if i >= skip {
+				tr.succ.WriteByte(b / 16)
+			}
+			i++
+			if i >= skip {
+				tr.succ.WriteByte(b % 16)
+			}
+			i++
+		}
+		tr.succ.WriteByte(16)
+		if tr.curr.Len() > 0 {
+			var err error
+			var data GenStructStepData
+			if useCache {
+				data = GenStructStepHashData{Hash: common.BytesToHash(tr.value.Bytes())}
+			} else {
+				if tr.fieldSet == 0 {
+					data = GenStructStepLeafData{Value: rlphacks.RlpSerializableBytes(tr.value.Bytes())}
+				} else {
+					data = GenStructStepAccountData{
+						FieldSet:    tr.fieldSet,
+						StorageSize: tr.a.StorageSize,
+						Balance:     &tr.a.Balance,
+						Nonce:       tr.a.Nonce,
+						Incarnation: tr.a.Incarnation,
+					}
+				}
+			}
+
+			tr.groups, err = GenStructStep(tr.currentRs.HashOnly, tr.curr.Bytes(), tr.succ.Bytes(), tr.hb, data, tr.groups)
+			if err != nil {
+				return err
+			}
+		}
+		// Remember the current key and value
+		if isAccount {
+			if err := tr.a.DecodeForStorage(v); err != nil {
+				return err
+			}
+			if tr.a.IsEmptyCodeHash() && tr.a.IsEmptyRoot() {
+				tr.fieldSet = AccountFieldSetNotContract
+			} else {
+				if tr.a.HasStorageSize {
+					tr.fieldSet = AccountFieldSetContractWithSize
+				} else {
+					tr.fieldSet = AccountFieldSetContract
+				}
+				// the first item ends up deepest on the stack, the seccond item - on the top
+				if err := tr.hb.hash(tr.a.CodeHash); err != nil {
+					return err
+				}
+				if err := tr.hb.hash(tr.a.Root); err != nil {
+					return err
+				}
+			}
+		} else {
+			tr.value.Reset()
+			tr.value.Write(v)
+			tr.fieldSet = AccountFieldSetNotAccount
+		}
+	}
+	return nil
 }
 
 func (tr *ResolverStatefulCached) MultiWalk2(db *bolt.DB, bucket []byte, startkeys [][]byte, fixedbits []uint, walker func(keyIdx int, k []byte, v []byte, useCache bool) error) error {
@@ -137,11 +229,10 @@ func (tr *ResolverStatefulCached) MultiWalk2(db *bolt.DB, bucket []byte, startke
 		_ = cacheK
 		_ = cacheV
 
-		fmt.Printf("Walk Start: \n\t%x, %x, \n\t%x, %x\n", k, v, cacheK, cacheV)
-
 		for k != nil || cacheK != nil {
 			useCache, minKey := keyIsBefore(cacheK, k)
 			_ = minKey
+
 			// Adjust rangeIdx if needed
 			cmp := int(-1)
 			for fixedbytes > 0 && cmp != 0 {
@@ -160,9 +251,7 @@ func (tr *ResolverStatefulCached) MultiWalk2(db *bolt.DB, bucket []byte, startke
 				case -1:
 					k, v = c.SeekTo(startkey)
 					cacheK, cacheV = cache.SeekTo(startkey)
-					if k == nil {
-						return nil
-					}
+					useCache, minKey = keyIsBefore(cacheK, k)
 				default:
 					rangeIdx++
 					if rangeIdx == len(startkeys) {
@@ -179,25 +268,21 @@ func (tr *ResolverStatefulCached) MultiWalk2(db *bolt.DB, bucket []byte, startke
 						return err
 					}
 				}
-
 				next, err := hexIncrement(cacheK)
 				if err != nil {
 					return err
 				}
 				k, v = c.SeekTo(next)
 				cacheK, cacheV = cache.SeekTo(next)
-				if k == nil {
-					return nil
+			} else {
+				if len(v) > 0 {
+					if err := walker(rangeIdx, k, v, useCache); err != nil {
+						return err
+					}
 				}
-				continue
+				k, v = c.Next()
 			}
 
-			if len(v) > 0 {
-				if err := walker(rangeIdx, k, v, useCache); err != nil {
-					return err
-				}
-			}
-			k, v = c.Next()
 		}
 		return nil
 	})
