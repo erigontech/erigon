b6269c69eba2a75f2c7ff5557a33bac8787ac490
andrew <andrey.ashikhmin@gmail.com>
2020-01-27 15:12:05 +0100

    Ref to db_walkthrough

--- ethdb/interface.go
+++ ethdb/interface.go
@@ -20,8 +20,7 @@ import (
 	"errors"
 )
 
-// TODO [Andrew] Add some comments about historical buckets & ChangeSet.
-// https://github.com/AlexeyAkhunov/papers/blob/master/TurboGeth-Devcon4.pdf
+// DESCRIBED: For info on database buckets see docs/programmers_guide/db_walkthrough.MD
 
 // ErrKeyNotFound is returned when key isn't found in the database.
 var ErrKeyNotFound = errors.New("db: key not found")
