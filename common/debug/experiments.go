82c6217d269c3a613f53675d7c5260d7c123d008
andrew <andrey.ashikhmin@gmail.com>
2020-02-13 13:47:22 +0000

    Enable GetNodeData by default

--- common/debug/experiments.go
+++ common/debug/experiments.go
@@ -12,7 +12,13 @@ var ThinHistory bool
 var itcEnv sync.Once
 var intermediateTrieHash bool
 
-var getNodeData uint32 // atomic: bit 0 is the value, bit 1 is the initialized flag
+// atomic: bit 0 is the value, bit 1 is the initialized flag
+var getNodeData uint32
+
+const (
+	gndValueFlag = 1 << iota
+	gndInitializedFlag
+)
 
 func IsThinHistory() bool {
 	gerEnv.Do(func() {
@@ -32,11 +38,11 @@ func IsIntermediateTrieHash() bool {
 }
 
 // IsGetNodeData indicates whether the GetNodeData functionality should be enabled.
-// By default that's driven by the presence or absence of GET_NODE_DATA environment variable.
+// By default that's driven by the presence or absence of DISABLE_GET_NODE_DATA environment variable.
 func IsGetNodeData() bool {
 	x := atomic.LoadUint32(&getNodeData)
-	if x >= 2 { // already initialized
-		return x%2 != 0
+	if x&gndInitializedFlag != 0 { // already initialized
+		return x&gndValueFlag != 0
 	}
 
 	RestoreGetNodeData()
@@ -44,17 +50,17 @@ func IsGetNodeData() bool {
 }
 
 // RestoreGetNodeData enables or disables the GetNodeData functionality
-// according to the presence or absence of GET_NODE_DATA environment variable.
+// according to the presence or absence of DISABLE_GET_NODE_DATA environment variable.
 func RestoreGetNodeData() {
-	_, envVarSet := os.LookupEnv("GET_NODE_DATA")
-	OverrideGetNodeData(envVarSet)
+	_, envVarSet := os.LookupEnv("DISABLE_GET_NODE_DATA")
+	OverrideGetNodeData(!envVarSet)
 }
 
 // OverrideGetNodeData allows to explicitly enable or disable the GetNodeData functionality.
 func OverrideGetNodeData(val bool) {
 	if val {
-		atomic.StoreUint32(&getNodeData, 3)
+		atomic.StoreUint32(&getNodeData, gndInitializedFlag|gndValueFlag)
 	} else {
-		atomic.StoreUint32(&getNodeData, 2)
+		atomic.StoreUint32(&getNodeData, gndInitializedFlag)
 	}
 }
