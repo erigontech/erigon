82fca3624650a1abc3a21f3a3841898a7dd28f1d
Alexey Akhunov <akhounov@gmail.com>
2020-02-17 21:01:41 +0000

    Avoid extra copying

--- ethdb/mutation.go
+++ ethdb/mutation.go
@@ -67,10 +67,7 @@ func (pb putsBucket) Get(key []byte) ([]byte, bool) {
 		return nil, true
 	}
 
-	v := make([]byte, len(value))
-	copy(v, value)
-
-	return v, true
+	return value, true
 }
 
 func (pb putsBucket) GetStr(key string) ([]byte, bool) {
@@ -83,10 +80,7 @@ func (pb putsBucket) GetStr(key string) ([]byte, bool) {
 		return nil, true
 	}
 
-	v := make([]byte, len(value))
-	copy(v, value)
-
-	return v, true
+	return value, true
 }
 
 type mutation struct {
