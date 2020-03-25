e689e825a73dc231ddcaee8cb52ad254feab7360
alex.sharov <AskAlexSharov@gmail.com>
2020-03-25 10:36:15 +0700

    auto-format code by prettier (similar to gofmt)

--- debug-web-ui/src/page/Storage.js
+++ debug-web-ui/src/page/Storage.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
+import { Col, Container, Row } from 'react-bootstrap';
 import LookupStorageForm from '../components/LookupStorageForm';
 
-const StoragePage = ({api}) => (
-    <Container fluid className="mt-1">
-        <Row>
-            <Col>
-                <h1>Storage</h1>
-            </Col>
-        </Row>
-        <Row>
-            <Col>
-                <LookupStorageForm api={api}/>
-            </Col>
-        </Row>
-    </Container>
-)
+const StoragePage = ({ api }) => (
+  <Container fluid className="mt-1">
+    <Row>
+      <Col>
+        <h1>Storage</h1>
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        <LookupStorageForm api={api} />
+      </Col>
+    </Row>
+  </Container>
+);
 
 export default StoragePage;
