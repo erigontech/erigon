439c74a7466136d8938569200e1ed81c86dcd3e7
Igor Mandrigin <i@mandrigin.ru>
2020-04-13 12:33:34 +0300

    verify_snapshot stuff

--- cmd/state/commands/verify_snapshot.go
+++ cmd/state/commands/verify_snapshot.go
@@ -5,9 +5,18 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	snapshotPath string
+)
+
 func init() {
-	withBlock(verifySnapshotCmd)
-	withChaindata(verifySnapshotCmd)
+	verifySnapshotCmd.Flags().StringVar(&snapshotPath, "snapshot", "snapshot", "Path to the snapshot to verify")
+	if err := verifySnapshotCmd.MarkFlagFilename("snapshot", ""); err != nil {
+		panic(err)
+	}
+	if err := verifySnapshotCmd.MarkFlagRequired("snapshot"); err != nil {
+		panic(err)
+	}
 	rootCmd.AddCommand(verifySnapshotCmd)
 }
 
@@ -15,7 +24,7 @@ var verifySnapshotCmd = &cobra.Command{
 	Use:   "verifySnapshot",
 	Short: "Verifies snapshots made by the 'stateless' action",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		stateless.VerifySnapshot(block, chaindata)
+		stateless.VerifySnapshot(snapshotPath)
 		return nil
 	},
 }
