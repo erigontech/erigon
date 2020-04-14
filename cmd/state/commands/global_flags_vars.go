b8282a0336abe20c9f0048fb875d45f829745c50
Igor Mandrigin <mandrigin@users.noreply.github.com>
2020-04-14 15:51:20 +0300

    Implement a little tool to validate the export file against chaindata (#452)
    * verifyExportFile command
    
    * linters

--- cmd/state/commands/global_flags_vars.go
+++ cmd/state/commands/global_flags_vars.go
@@ -11,22 +11,24 @@ var (
 	remoteDbAddress string
 )
 
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 func withBlock(cmd *cobra.Command) {
 	cmd.Flags().Uint64Var(&block, "block", 1, "specifies a block number for operation")
 }
 
 func withChaindata(cmd *cobra.Command) {
 	cmd.Flags().StringVar(&chaindata, "chaindata", "chaindata", "path to the chaindata file used as input to analysis")
-	if err := cmd.MarkFlagFilename("chaindata", ""); err != nil {
-		panic(err)
-	}
+	must(cmd.MarkFlagFilename("chaindata", ""))
 }
 
 func withStatsfile(cmd *cobra.Command) {
 	cmd.Flags().StringVar(&statsfile, "statsfile", "stateless.csv", "path where to write the stats file")
-	if err := cmd.MarkFlagFilename("statsfile", "csv"); err != nil {
-		panic(err)
-	}
+	must(cmd.MarkFlagFilename("statsfile", "csv"))
 }
 
 func withRemoteDb(cmd *cobra.Command) {
