33209fc579d7240a4c206427dcbd32fe5bc20271
Igor Mandrigin <i@mandrigin.ru>
2020-04-26 16:15:00 +0300

    better log entries

--- eth/downloader/stagedsync_downloader.go
+++ eth/downloader/stagedsync_downloader.go
@@ -4,8 +4,10 @@ import "github.com/ledgerwatch/turbo-geth/log"
 
 func (d *Downloader) doStagedSyncWithFetchers(p *peerConnection, headersFetchers []func() error) error {
 
-	log.Info("Downloading headers stage")
+	log.Info("Stage 1/3. Downloading headers...")
+
 	var err error
+
 	/*
 	* Stage 1. Download Headers
 	 */
@@ -13,7 +15,8 @@ func (d *Downloader) doStagedSyncWithFetchers(p *peerConnection, headersFetchers
 		return err
 	}
 
-	log.Info("Finished headers downloading, moving onto the the block downloading stage")
+	log.Info("Stage 1/3. Downloading headers... Complete!")
+	log.Info("Stage 2/3. Downloading block bodies...")
 
 	/*
 	* Stage 2. Download Block bodies
@@ -27,6 +30,9 @@ func (d *Downloader) doStagedSyncWithFetchers(p *peerConnection, headersFetchers
 		return err
 	}
 
+	log.Info("Stage 2/3. Downloading block bodies... Complete!")
+	log.Info("Stage 3/3. Executing blocks w/o hash checks...")
+
 	/*
 	* Stage 3. Execute block bodies w/o calculating trie roots
 	 */
@@ -35,6 +41,8 @@ func (d *Downloader) doStagedSyncWithFetchers(p *peerConnection, headersFetchers
 	if err = d.spawnExecuteBlocksStage(); err != nil {
 		return err
 	}
+
+	log.Info("Stage 3/3. Executing blocks w/o hash checks... Complete!")
 
 	return err
 }
