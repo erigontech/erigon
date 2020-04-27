c516110121b20d284ab61f32cb58cd1278885834
Igor Mandrigin <mandrigin@users.noreply.github.com>
2020-04-27 16:27:37 +0300

    Staged Sync: check trie root hash stage (#490)

---
+++ eth/downloader/stagedsync_stage_hashcheck.go
@@ -0,0 +1,43 @@
+package downloader
+
+import (
+	"github.com/ledgerwatch/turbo-geth/log"
+	"github.com/ledgerwatch/turbo-geth/trie"
+	"github.com/pkg/errors"
+)
+
+func (d *Downloader) spawnCheckFinalHashStage(syncHeadNumber uint64) error {
+	hashProgress, err := GetStageProgress(d.stateDB, HashCheck)
+	if err != nil {
+		return err
+	}
+
+	if hashProgress == syncHeadNumber {
+		// we already did hash check for this block
+		// we don't do the obvious `if hashProgress > syncHeadNumber` to support reorgs more naturally
+		return nil
+	}
+
+	syncHeadBlock := d.blockchain.GetBlockByNumber(syncHeadNumber)
+
+	// make sure that we won't write the the real DB
+	// should never be commited
+	euphemeralMutation := d.stateDB.NewBatch()
+
+	blockNr := syncHeadBlock.Header().Number.Uint64()
+
+	tr := trie.New(syncHeadBlock.Root())
+	// making resolve request for the trie root, so we only get a hash
+	rr := tr.NewResolveRequest(nil, []byte{}, 0, tr.Root())
+
+	log.Info("Validating root hash", "block", blockNr, "blockRoot", syncHeadBlock.Root().Hex())
+
+	resolver := trie.NewResolver(0, true, blockNr)
+	resolver.AddRequest(rr)
+	err = resolver.ResolveStateful(euphemeralMutation, blockNr, false)
+	if err != nil {
+		return errors.Wrap(err, "checking root hash failed")
+	}
+
+	return SaveStageProgress(d.stateDB, HashCheck, blockNr)
+}
