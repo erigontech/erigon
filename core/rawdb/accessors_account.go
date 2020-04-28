1821e438f055f1850d0bd467daf04f047556c8dd
alex.sharov <AskAlexSharov@gmail.com>
2020-04-28 13:20:04 +0700

    Fix: use correct storageResolveSet in finaliseStorageRoot

--- core/rawdb/accessors_account.go
+++ core/rawdb/accessors_account.go
@@ -24,20 +24,37 @@ import (
 
 // ReadAccount reading account object from multiple buckets of db
 func ReadAccount(db DatabaseReader, addrHash common.Hash, acc *accounts.Account) (bool, error) {
-	enc, err := db.Get(dbutils.CurrentStateBucket, addrHash[:])
+	addrHashBytes := addrHash[:]
+	enc, err := db.Get(dbutils.CurrentStateBucket, addrHashBytes)
 	if err != nil {
 		return false, err
 	}
 	if err = acc.DecodeForStorage(enc); err != nil {
 		return false, err
 	}
+	root, err := db.Get(dbutils.IntermediateTrieHashBucket, dbutils.GenerateStoragePrefix(addrHashBytes, acc.Incarnation))
+	if err != nil {
+		return false, err
+	}
+	if enc == nil || root == nil {
+		return false, nil
+	}
+	acc.Root = common.BytesToHash(root)
 	return true, nil
 }
 
 func WriteAccount(db DatabaseWriter, addrHash common.Hash, acc accounts.Account) error {
+	//fmt.Printf("WriteAccount: %x %x\n", addrHash, acc.Root)
+	addrHashBytes := addrHash[:]
 	value := make([]byte, acc.EncodingLengthForStorage())
 	acc.EncodeForStorage(value)
-	return db.Put(dbutils.CurrentStateBucket, addrHash[:], value)
+	if err := db.Put(dbutils.CurrentStateBucket, addrHashBytes, value); err != nil {
+		return err
+	}
+	if err := db.Put(dbutils.IntermediateTrieHashBucket, dbutils.GenerateStoragePrefix(addrHashBytes, acc.Incarnation), acc.Root.Bytes()); err != nil {
+		return err
+	}
+	return nil
 }
 
 type DatabaseReaderDeleter interface {
@@ -46,5 +63,22 @@ type DatabaseReaderDeleter interface {
 }
 
 func DeleteAccount(db DatabaseReaderDeleter, addrHash common.Hash) error {
-	return db.Delete(dbutils.CurrentStateBucket, addrHash[:])
+	addrHashBytes := addrHash[:]
+	enc, err := db.Get(dbutils.CurrentStateBucket, addrHashBytes)
+	if err != nil && err.Error() != "db: key not found" {
+		return err
+	}
+	acc := accounts.NewAccount()
+	if err = acc.DecodeForStorage(enc); err != nil {
+		return err
+	}
+
+	if err := db.Delete(dbutils.CurrentStateBucket, addrHashBytes); err != nil {
+		return err
+	}
+
+	if err := db.Delete(dbutils.IntermediateTrieHashBucket, dbutils.GenerateStoragePrefix(addrHashBytes, acc.Incarnation)); err != nil {
+		return err
+	}
+	return nil
 }
