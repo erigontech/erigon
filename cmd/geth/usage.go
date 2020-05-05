9d7fb112b769647142c15953f576d89c251dac60
rene <41963722+renaynay@users.noreply.github.com>
2020-05-05 10:19:17 +0200

    cmd/utils: renames flags related to http-rpc server (#20935)
    * rpc flags related to starting http server renamed to http
    
    * old rpc flags aliased and still functional
    
    * pprof flags fixed
    
    * renames gpo related flags
    
    * linted
    
    * renamed rpc flags for consistency and clarity
    
    * added warn logs
    
    * added more warn logs for all deprecated flags for consistency
    
    * moves legacy flags to separate file, hides older flags under show-deprecated-flags command
    
    * legacy prefix and moved some more legacy flags to legacy file
    
    * fixed circular import
    
    * added docs
    
    * fixed imports lint error
    
    * added notes about when flags were deprecated
    
    * cmd/utils: group flags by deprecation date + reorder by date,
    
    * modified deprecated comments for consistency, added warn log for --rpc
    
    * making sure deprecated flags are still functional
    
    * show-deprecated-flags command cleaned up
    
    * fixed lint errors
    
    * corrected merge conflict
    
    * IsSet --> GlobalIsSet
    
    * uncategorized flags, if not deprecated, displayed under misc
    
    Co-authored-by: Martin Holst Swende <martin@swende.se>
    # Conflicts:
    #	cmd/geth/main.go
    #	cmd/geth/usage.go
    #	internal/debug/flags.go

--- cmd/geth/usage.go
+++ cmd/geth/usage.go
@@ -165,13 +165,12 @@ var AppHelpFlagGroups = []flagGroup{
 		Flags: []cli.Flag{
 			utils.IPCDisabledFlag,
 			utils.IPCPathFlag,
-			utils.RPCEnabledFlag,
-			utils.RPCListenAddrFlag,
-			utils.RPCPortFlag,
-			utils.RPCApiFlag,
-			utils.RPCGlobalGasCap,
-			utils.RPCCORSDomainFlag,
-			utils.RPCVirtualHostsFlag,
+			utils.HTTPEnabledFlag,
+			utils.HTTPListenAddrFlag,
+			utils.HTTPPortFlag,
+			utils.HTTPApiFlag,
+			utils.HTTPCORSDomainFlag,
+			utils.HTTPVirtualHostsFlag,
 			utils.WSEnabledFlag,
 			utils.WSListenAddrFlag,
 			utils.WSPortFlag,
@@ -182,6 +181,7 @@ var AppHelpFlagGroups = []flagGroup{
 			utils.GraphQLPortFlag,
 			utils.GraphQLCORSDomainFlag,
 			utils.GraphQLVirtualHostsFlag,
+			utils.RPCGlobalGasCap,
 			utils.JSpathFlag,
 			utils.ExecFlag,
 			utils.PreloadJSFlag,
@@ -248,20 +248,28 @@ var AppHelpFlagGroups = []flagGroup{
 		Flags: metricsFlags,
 	},
 	{
-		Name: "DEPRECATED",
-		Flags: []cli.Flag{
-			utils.LegacyTestnetFlag,
-			utils.LightLegacyServFlag,
-			utils.LightLegacyPeersFlag,
-			utils.MinerLegacyThreadsFlag,
-			utils.MinerLegacyGasTargetFlag,
-			utils.MinerLegacyGasPriceFlag,
-			utils.MinerLegacyEtherbaseFlag,
-			utils.MinerLegacyExtraDataFlag,
-		},
+	{
+		Name: "ALIASED (deprecated)",
+		Flags: append([]cli.Flag{
+			utils.LegacyRPCEnabledFlag,
+			utils.LegacyRPCListenAddrFlag,
+			utils.LegacyRPCPortFlag,
+			utils.LegacyRPCCORSDomainFlag,
+			utils.LegacyRPCVirtualHostsFlag,
+			utils.LegacyRPCApiFlag,
+			utils.LegacyWSListenAddrFlag,
+			utils.LegacyWSPortFlag,
+			utils.LegacyWSAllowedOriginsFlag,
+			utils.LegacyWSApiFlag,
+			utils.LegacyGpoBlocksFlag,
+			utils.LegacyGpoPercentileFlag,
+		}, debug.DeprecatedFlags...),
 	},
 	{
 		Name: "MISC",
+		Flags: []cli.Flag{
+			cli.HelpFlag,
+		},
 	},
 }
 
@@ -319,10 +327,17 @@ func init() {
 					categorized[flag.String()] = struct{}{}
 				}
 			}
+			deprecated := make(map[string]struct{})
+			for _, flag := range utils.DeprecatedFlags {
+				deprecated[flag.String()] = struct{}{}
+			}
+			// Only add uncategorized flags if they are not deprecated
 			var uncategorized []cli.Flag
 			for _, flag := range data.(*cli.App).Flags {
 				if _, ok := categorized[flag.String()]; !ok {
-					uncategorized = append(uncategorized, flag)
+					if _, ok := deprecated[flag.String()]; !ok {
+						uncategorized = append(uncategorized, flag)
+					}
 				}
 			}
 			if len(uncategorized) > 0 {
