aaa44e336952c9ca86d1faf378679a95ba191445
Igor Mandrigin <i@mandrigin.ru>
2020-05-18 17:01:33 +0300

    accounts/abi: move U256Bytes to common/math (#21020)
    # Conflicts:
    #	accounts/abi/abi_test.go
    #	accounts/abi/numbers.go
    #	signer/core/signed_data.go

--- signer/core/signed_data.go
+++ signer/core/signed_data.go
@@ -587,7 +587,7 @@ func (typedData *TypedData) EncodePrimitiveValue(encType string, encValue interf
 		if err != nil {
 			return nil, err
 		}
-		return abi.U256(b), nil
+		return math.U256Bytes(b), nil
 	}
 	return nil, fmt.Errorf("unrecognized type '%s'", encType)
 
