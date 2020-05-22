54251a525b646d4e6a958dded8c7c2e66996a75f
b00ris <b00ris@mail.ru>
2020-05-22 17:45:51 +0300

    remove logs

--- eth/downloader/stagedsync_stage_indexes_test.go
+++ eth/downloader/stagedsync_stage_indexes_test.go
@@ -1,14 +1,10 @@
 package downloader
 
 import (
-	"github.com/ledgerwatch/turbo-geth/log"
-	"os"
 	"testing"
 )
 
 func TestName(t *testing.T) {
-	log.Root().SetHandler(log.LvlFilterHandler(log.LvlInfo, log.StreamHandler(os.Stderr, log.TerminalFormat(true))))
-
 	tester := newStagedSyncTester(true)
 	if err := tester.newPeer("peer", 65, testChainForkLightA); err != nil {
 		t.Fatal(err)
