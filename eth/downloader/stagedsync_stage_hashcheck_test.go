90bf39f2a481de5292a97ff408c83404f9e6f47f
Igor Mandrigin <i@mandrigin.ru>
2020-05-25 18:06:24 +0300

    add tests for hashcheck

--- eth/downloader/stagedsync_stage_hashcheck_test.go
+++ eth/downloader/stagedsync_stage_hashcheck_test.go
@@ -3,9 +3,7 @@ package downloader
 import (
 	"testing"
 
-	"github.com/ledgerwatch/turbo-geth/common"
 	"github.com/ledgerwatch/turbo-geth/common/dbutils"
-	"github.com/ledgerwatch/turbo-geth/core/state"
 	"github.com/ledgerwatch/turbo-geth/ethdb"
 )
 
@@ -13,62 +11,76 @@ func TestPromoteHashedStateClearState(t *testing.T) {
 	db1 := ethdb.NewMemDatabase()
 	db2 := ethdb.NewMemDatabase()
 
-	executeBlocks(db1, 50, getHashedStateWriter)
+	generateBlocks(t, 1, 50, hashedWriterGen(db1), changeCodeWithIncarnations)
 
-	executeBlocks(db2, 50, getPlainStateWriter)
+	generateBlocks(t, 1, 50, plainWriterGen(db2), changeCodeWithIncarnations)
 
-	promoteHashedState(db2, 0)
+	m2 := db2.NewBatch()
+	err := promoteHashedState(m2, 0)
+	if err != nil {
+		t.Errorf("error while promoting state: %v", err)
+	}
+	_, err = m2.Commit()
+	if err != nil {
+		t.Errorf("error while commiting state: %v", err)
+	}
 
 	compareCurrentState(t, db1, db2, dbutils.CurrentStateBucket, dbutils.ContractCodeBucket)
 }
 
 func TestPromoteHashedStateIncremental(t *testing.T) {
 	t.Skip("not implemented yet")
-
 	db1 := ethdb.NewMemDatabase()
 	db2 := ethdb.NewMemDatabase()
 
-	executeBlocks(db1, 50, getHashedStateWriter)
-	executeBlocks(db2, 50, getPlainStateWriter)
-	promoteHashedState(db2, 0)
+	generateBlocks(t, 1, 50, hashedWriterGen(db1), changeCodeWithIncarnations)
+	generateBlocks(t, 1, 50, plainWriterGen(db2), changeCodeWithIncarnations)
 
-	executeBlocks(db1, 50, getHashedStateWriter)
-	executeBlocks(db2, 50, getPlainStateWriter)
+	m2 := db2.NewBatch()
+	err := promoteHashedState(m2, 0)
+	if err != nil {
+		t.Errorf("error while promoting state: %v", err)
+	}
+	_, err = m2.Commit()
+	if err != nil {
+		t.Errorf("error while commiting state: %v", err)
+	}
 
-	promoteHashedState(db2, 50)
+	generateBlocks(t, 51, 50, hashedWriterGen(db1), changeCodeWithIncarnations)
+	generateBlocks(t, 51, 50, plainWriterGen(db2), changeCodeWithIncarnations)
+
+	m2 = db2.NewBatch()
+	err = promoteHashedState(m2, 50)
+	if err != nil {
+		t.Errorf("error while promoting state: %v", err)
+	}
+	_, err = m2.Commit()
+	if err != nil {
+		t.Errorf("error while commiting state: %v", err)
+	}
 
 	compareCurrentState(t, db1, db2, dbutils.CurrentStateBucket, dbutils.ContractCodeBucket)
 }
 
 func TestPromoteHashedStateIncrementalMixed(t *testing.T) {
 	t.Skip("not implemented yet")
-
 	db1 := ethdb.NewMemDatabase()
 	db2 := ethdb.NewMemDatabase()
 
-	executeBlocks(db1, 100, getHashedStateWriter)
-	executeBlocks(db2, 50, getHashedStateWriter)
-	executeBlocks(db2, 50, getPlainStateWriter)
+	generateBlocks(t, 1, 100, hashedWriterGen(db1), changeCodeWithIncarnations)
+	generateBlocks(t, 1, 50, hashedWriterGen(db1), changeCodeWithIncarnations)
+	generateBlocks(t, 51, 50, plainWriterGen(db2), changeCodeWithIncarnations)
 
-	promoteHashedState(db2, 50)
+	m2 := db2.NewBatch()
+	err := promoteHashedState(m2, 50)
+	if err != nil {
+		t.Errorf("error while promoting state: %v", err)
+	}
+
+	_, err = m2.Commit()
+	if err != nil {
+		t.Errorf("error while commiting state: %v", err)
+	}
 
 	compareCurrentState(t, db1, db2, dbutils.CurrentStateBucket, dbutils.ContractCodeBucket)
-}
-
-func getHashedStateWriter(db ethdb.Database, blockNum uint64) state.WriterWithChangeSets {
-	uncommitedIncarnations := make(map[common.Address]uint64)
-	return state.NewDbStateWriter(db, db, blockNum, uncommitedIncarnations)
-}
-
-func getPlainStateWriter(db ethdb.Database, blockNum uint64) state.WriterWithChangeSets {
-	uncommitedIncarnations := make(map[common.Address]uint64)
-	return state.NewPlainStateWriter(db, db, blockNum, uncommitedIncarnations)
-}
-
-func executeBlocks(
-	db ethdb.Database,
-	numberOfBlocks uint64,
-	getWriterFunc func(ethdb.Database, uint64) state.WriterWithChangeSets,
-) {
-	panic("implement me!")
 }
