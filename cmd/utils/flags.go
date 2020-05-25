204d9dc985379da83057667e67c267e6323e2f9c
Felix Lange <fjl@twurst.com>
2020-05-25 19:50:36 +0200

    cmd/utils: fix default DNS discovery configuration (#21124)

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -1660,7 +1660,7 @@ func setDNSDiscoveryDefaults(cfg *eth.Config, genesis common.Hash) {
 		return // already set through flags/config
 	}
 
-	protocol := "eth"
+	protocol := "all"
 	if cfg.SyncMode == downloader.LightSync {
 		protocol = "les"
 	}
