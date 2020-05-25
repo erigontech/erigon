af5795eb3c9381c897cdc39e8c5a2215304bd162
Igor Mandrigin <i@mandrigin.ru>
2020-05-25 15:09:13 +0300

    state promotion (clean, naive)

--- eth/downloader/stagedsync_stage_hashcheck.go
+++ eth/downloader/stagedsync_stage_hashcheck.go
@@ -3,6 +3,9 @@ package downloader
 import (
 	"fmt"
 
+	"github.com/ledgerwatch/turbo-geth/common"
+	"github.com/ledgerwatch/turbo-geth/common/dbutils"
+	"github.com/ledgerwatch/turbo-geth/core"
 	"github.com/ledgerwatch/turbo-geth/ethdb"
 	"github.com/ledgerwatch/turbo-geth/log"
 	"github.com/ledgerwatch/turbo-geth/trie"
@@ -15,14 +18,17 @@ func spawnCheckFinalHashStage(stateDB ethdb.Database, blockchain BlockChain, syn
 		return err
 	}
 
-	//REMOVE THE FOLLOWING LINE WHEN PLAIN => HASHED TRANSFORMATION IS READY
-	if hashProgress == 0 {
-		return nil
-	}
 	if hashProgress == syncHeadNumber {
 		// we already did hash check for this block
 		// we don't do the obvious `if hashProgress > syncHeadNumber` to support reorgs more naturally
 		return nil
+	}
+
+	if core.UsePlainStateExecution {
+		err = promoteHashedState(stateDB, hashProgress)
+		if err != nil {
+			return err
+		}
 	}
 
 	syncHeadBlock := blockchain.GetBlockByNumber(syncHeadNumber)
@@ -78,4 +84,77 @@ func unwindHashCheckStage(unwindPoint uint64, stateDB ethdb.Database) error {
 		return fmt.Errorf("unwind HashCheck: failed to write db commit: %v", err)
 	}
 	return nil
+}
+
+func promoteHashedState(db ethdb.Database, progress uint64) error {
+	if progress == 0 {
+		return promoteHashedStateCleanly(db)
+	}
+	return errors.New("incremental state promotion not implemented")
+}
+
+func promoteHashedStateCleanly(db ethdb.Database) error {
+	err := copyBucket(db, dbutils.PlainStateBucket, dbutils.CurrentStateBucket, transformPlainStateKey)
+	if err != nil {
+		return err
+	}
+	return copyBucket(db, dbutils.PlainContractCodeBucket, dbutils.ContractCodeBucket, transformContractCodeKey)
+}
+
+func copyBucket(
+	db ethdb.Database,
+	sourceBucket,
+	destBucket []byte,
+	transformKeyFunc func([]byte) ([]byte, error)) error {
+	return db.Walk(sourceBucket, nil, 0, func(k, v []byte) (bool, error) {
+		newK, err := transformKeyFunc(k)
+		if err != nil {
+			return false, err
+		}
+		err = db.Put(destBucket, newK, v)
+		if err != nil {
+			return false, err
+		}
+		return true, nil
+	})
+}
+
+func transformPlainStateKey(key []byte) ([]byte, error) {
+	switch len(key) {
+	case common.AddressLength:
+		// account
+		hash, err := common.HashData(key)
+		return hash[:], err
+	case common.AddressLength + common.IncarnationLength + common.HashLength:
+		// storage
+		address, incarnation, key := dbutils.PlainParseCompositeStorageKey(key)
+		addrHash, err := common.HashData(address[:])
+		if err != nil {
+			return nil, err
+		}
+		secretKey, err := common.HashData(key[:])
+		if err != nil {
+			return nil, err
+		}
+		compositeKey := dbutils.GenerateCompositeStorageKey(addrHash, incarnation, secretKey)
+		return compositeKey, nil
+	default:
+		// no other keys are supported
+		return nil, fmt.Errorf("could not convert key from plain to hashed, unexpected len: %d", len(key))
+	}
+}
+
+func transformContractCodeKey(key []byte) ([]byte, error) {
+	if len(key) != common.AddressLength+common.IncarnationLength {
+		return nil, fmt.Errorf("could not convert code key from plain to hashed, unexpected len: %d", len(key))
+	}
+	address, incarnation := dbutils.ParseStoragePrefix(key)
+
+	addrHash, err := common.HashData(address[:])
+	if err != nil {
+		return nil, err
+	}
+
+	compositeKey := dbutils.GenerateStoragePrefix(addrHash[:], incarnation)
+	return compositeKey, nil
 }
