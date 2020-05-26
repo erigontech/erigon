1ef4499d65bf45cf7160b2ddf989b11f41a168c1
Alexey Akhunov <akhounov@gmail.com>
2020-05-26 06:09:02 +0100

    Not hash, keep the files

--- eth/downloader/stagedsync_downloader.go
+++ eth/downloader/stagedsync_downloader.go
@@ -86,8 +86,7 @@ func (d *Downloader) doStagedSyncWithFetchers(p *peerConnection, headersFetchers
 	* Stage 4. Execute block bodies w/o calculating trie roots
 	 */
 
-	var syncHeadNumber uint64
-	syncHeadNumber, err = spawnExecuteBlocksStage(d.stateDB, d.blockchain)
+	_, err = spawnExecuteBlocksStage(d.stateDB, d.blockchain)
 	if err != nil {
 		return err
 	}
@@ -97,9 +96,9 @@ func (d *Downloader) doStagedSyncWithFetchers(p *peerConnection, headersFetchers
 	// Further stages go there
 	log.Info("Sync stage 5/7. Validating final hash")
 	if !core.UsePlainStateExecution {
-		if err = d.spawnCheckFinalHashStage(syncHeadNumber); err != nil {
-			return err
-		}
+		//if err = d.spawnCheckFinalHashStage(syncHeadNumber); err != nil {
+		//	return err
+		//}
 	}
 
 	log.Info("Sync stage 5/7. Validating final hash... Complete!")
