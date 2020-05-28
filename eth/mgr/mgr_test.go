474a4fe32469df49a0ca6962a8726d789f343345
alex.sharov <AskAlexSharov@gmail.com>
2020-05-28 17:12:53 +0700

    fix test

--- eth/mgr/mgr_test.go
+++ eth/mgr/mgr_test.go
@@ -11,16 +11,14 @@ func TestScheduleProperties(t *testing.T) {
 	require := require.New(t)
 	stateSize := uint64(123456)
 	block := uint64(11)
-
+	toBlock := block + mgr.BlocksPerCycle + 100
 	var prevTick *mgr.Tick
 
 	var sizeFromTicksAccumulator uint64
-
-	for block <= block+mgr.BlocksPerCycle+100 {
+	for block <= toBlock {
 		tick := mgr.NewTick(block, stateSize, prevTick)
 
 		sizeFromTicksAccumulator += tick.ToSize - tick.FromSize
-
 		// props
 		if prevTick != nil && prevTick.Number < tick.Number {
 			require.LessOrEqual(block, tick.FromBlock)
