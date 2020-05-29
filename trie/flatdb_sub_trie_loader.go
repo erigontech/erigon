7f5a5503c40df177ff69c201f551d0b85cad2216
alex.sharov <AskAlexSharov@gmail.com>
2020-05-29 14:09:21 +0700

    better error messages

--- trie/flatdb_sub_trie_loader.go
+++ trie/flatdb_sub_trie_loader.go
@@ -335,9 +335,6 @@ func (fstl *FlatDbSubTrieLoader) iteration(c, ih ethdb.Cursor, first bool) error
 		if fstl.ihK, fstl.ihV, err = ih.Next(); err != nil {
 			return err
 		} // go to children, not to sibling
-		if err != nil {
-			return err
-		}
 		return nil
 	}
 
@@ -350,9 +347,6 @@ func (fstl *FlatDbSubTrieLoader) iteration(c, ih ethdb.Cursor, first bool) error
 		if fstl.ihK, fstl.ihV, err = ih.Next(); err != nil {
 			return err
 		} // go to children, not to sibling
-		if err != nil {
-			return err
-		}
 		return nil
 	}
 
@@ -658,9 +652,6 @@ func (fstl *FlatDbSubTrieLoader) LoadSubTries() (SubTries, error) {
 			}
 			if !bytes.Equal(k, prefix) {
 				panic(fmt.Sprintf("IH and WitnessSize buckets must have same keys set: %x, %x", k, prefix))
-			}
-			if k != nil && len(v) == 0 {
-				panic(fmt.Errorf("k != nil && v == nil: %x %x %x", prefix, k, v))
 			}
 			return binary.BigEndian.Uint64(v)
 		}
