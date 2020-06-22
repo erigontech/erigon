1e01edd3f15774ad9c57ba7d665a22f19910524b
suhabe <suhabe@bugrara.com>
2020-06-22 07:15:41 -0700

    work on cfg0

--- core/vm/df_interpreter.go
+++ core/vm/df_interpreter.go
@@ -4,35 +4,53 @@ import (
 	"github.com/ledgerwatch/turbo-geth/common"
 )
 
-// DfInterpreter is for static analysis
-type DfInterpreter struct {
-	evm *EVM
-	cfg Config
-
-	jt *JumpTable // EVM instruction table
-
-	hasher    keccakState // Keccak256 hasher instance shared across opcodes
-	hasherBuf common.Hash // Keccak256 hasher result array shared across opcodes
-
-	readOnly   bool   // Whether to throw on stateful modifications
-	returnData []byte // Last CALL's return data for subsequent reuse
-}
-
-//////////////////////////////////////////////////
 type Cfg struct {
-	nodes map[uint64]Node
+	nodes map[uint64]*Node
 }
 
 type Node struct {
 	pc uint64
-	succs []Node
-	preds []Node
+	succs []*Node
+	preds []*Node
 }
 
-func toCfg0(contract *Contract) (cfg *Cfg) {
+func ToCfg0(contract *Contract, jt *JumpTable) (cfg *Cfg, err error) {
 	cfg = &Cfg{}
 
-	op = contract.GetOp(pc)
+	pc := uint64(0)
+	jumps := make([]*Node, 0)
+	var lastNode *Node = nil
+	for int(pc) < len(contract.Code) {
+		op := contract.GetOp(uint64(pc))
+		node := Node {pc, nil, nil}
+		cfg.nodes[pc] = &node
+
+		opLength := uint64(1)
+		operation := &jt[op]
+		if !operation.valid {
+			return nil, &ErrInvalidOpCode{opcode: op}
+		}
+
+		if op.IsPush() {
+			opLength += GetPushBytes(op)
+		} else if op == JUMP {
+			jumps = append(jumps, &node)
+		} else if op == JUMPI {
+			jumps = append(jumps, &node)
+		} else if op == STOP || op == REVERT || op == RETURN || op == SELFDESTRUCT {
+
+		}
+
+		pc += opLength
+	}
+
+	for _, node := range cfg.nodes {
+		for _, jump := range jumps {
+			node.preds = append(node.preds, jump)
+		}
+	}
+
+	return cfg, nil
 }
 
 //////////////////////////////////////////////////
@@ -49,7 +67,7 @@ type AbsConst struct {
 	value uint64
 }
 
-func (lhs *AbsConst) lub(rhs AbsConst) AbsConst {
+func (lhs *AbsConst) Lub(rhs AbsConst) AbsConst {
 	if lhs.kind == Value && rhs.kind == Value {
 		if lhs.value != rhs.value {
 			return AbsConst{Top, 0}
@@ -72,7 +90,7 @@ type AbsState struct {
 	stack []AbsConst
 }
 
-func (state0 *AbsState) canonicalize() {
+func (state0 *AbsState) Canonicalize() {
 	st := state0.stack
 	for len(st) > 0 && st[len(st)-1].kind == Bot {
 		st = st[:len(st)-1]
@@ -80,17 +98,17 @@ func (state0 *AbsState) canonicalize() {
 	state0.stack = st
 }
 
-func (state0 *AbsState) lub (rhs *AbsState) *AbsState {
+func (state0 *AbsState) Lub (rhs *AbsState) *AbsState {
 	res := &AbsState{}
 
 	if len(state0.stack) > len(rhs.stack) {
-		lhs0 := state0
+		state0prev := state0
 		state0 = rhs
-		rhs = lhs0
+		rhs = state0prev
 	}
 
 	for i := 0; i < len(state0.stack); i++ {
-		lub := state0.stack[i].lub(rhs.stack[i])
+		lub := state0.stack[i].Lub(rhs.stack[i])
 		res.stack = append(res.stack, lub)
 	}
 
@@ -98,12 +116,12 @@ func (state0 *AbsState) lub (rhs *AbsState) *AbsState {
 		res.stack = append(res.stack, rhs.stack[i])
 	}
 
-	res.canonicalize()
+	res.Canonicalize()
 
 	return res
 }
 
-func (state0 *AbsState) isDiff(state1 *AbsState) bool {
+func (state0 *AbsState) IsDiff(state1 *AbsState) bool {
 	if len(state0.stack) != len(state1.stack) {
 		return false
 	}
@@ -117,14 +135,21 @@ func (state0 *AbsState) isDiff(state1 *AbsState) bool {
 	return true
 }
 
-func transfer(instrNode *Node, state0 *AbsState) *AbsState {
+func Transfer(instrNode *Node, state0 *AbsState) *AbsState {
 	res := &AbsState{}
 
 	return res
 }
 
-func (in *DfInterpreter) Run(contract *Contract, input []byte, readOnly bool) {
-	cfg0 := toCfg0(contract)
+func (in *DfInterpreter) Run(contract *Contract, input []byte, readOnly bool) error {
+
+	jt := newIstanbulInstructionSet()
+
+	cfg0, err := ToCfg0(contract, &jt)
+	if err != nil {
+		return err
+	}
+
 	states := make(map[uint64]*AbsState)
 	workList := []Node{cfg0.nodes[0]}
 
@@ -138,16 +163,105 @@ func (in *DfInterpreter) Run(contract *Contract, input []byte, readOnly bool) {
 			if !exists {
 				predState = &AbsState{}
 			}
-			prevLub = prevLub.lub(predState)
+			prevLub = prevLub.Lub(predState)
 		}
 
 		state0 := states[instrNode.pc]
-		state1 := transfer(&instrNode, prevLub)
+		state1 := Transfer(&instrNode, prevLub)
 		states[instrNode.pc] = state1
-		if state0.isDiff(state1) {
+		if state0.IsDiff(state1) {
 			for _, s := range instrNode.succs {
 				workList = append(workList, s)
 			}
 		}
 	}
+}
+
+
+
+
+
+func GetPushBytes(op OpCode) uint64 {
+	switch(op) {
+	case PUSH1:
+		return 1
+	case PUSH2:
+		return 2
+	case PUSH3:
+		return 3
+	case PUSH4:
+		return 4
+	case PUSH5:
+		return 5
+	case PUSH6:
+		return 6
+	case PUSH7:
+		return 7
+	case PUSH8:
+		return 8
+	case PUSH9:
+		return 9
+	case PUSH10:
+		return 10
+	case PUSH11:
+		return 11
+	case PUSH12:
+		return 12
+	case PUSH13:
+		return 13
+	case PUSH14:
+		return 14
+	case PUSH15:
+		return 15
+	case PUSH16:
+		return 16
+	case PUSH17:
+		return 17
+	case PUSH18:
+		return 18
+	case PUSH19:
+		return 19
+	case PUSH20:
+		return 20
+	case PUSH21:
+		return 21
+	case PUSH22:
+		return 22
+	case PUSH23:
+		return 23
+	case PUSH24:
+		return 24
+	case PUSH25:
+		return 25
+	case PUSH26:
+		return 26
+	case PUSH27:
+		return 27
+	case PUSH28:
+		return 28
+	case PUSH29:
+		return 29
+	case PUSH30:
+		return 30
+	case PUSH31:
+		return 31
+	case PUSH32:
+		return 32
+	default:
+		panic("Cannot handle")
+	}
+}
+
+// DfInterpreter is for static analysis
+type DfInterpreter struct {
+	evm *EVM
+	cfg Config
+
+	jt *JumpTable // EVM instruction table
+
+	hasher    keccakState // Keccak256 hasher instance shared across opcodes
+	hasherBuf common.Hash // Keccak256 hasher result array shared across opcodes
+
+	readOnly   bool   // Whether to throw on stateful modifications
+	returnData []byte // Last CALL's return data for subsequent reuse
 }
