64565f4b2b06af5e5c514398cc965e3172283d0e
Martin Holst Swende <martin@swende.se>
2020-07-14 21:42:32 +0200

    core: transaction pool optimizations (#21328)
    * core: added local tx pool test case
    
    * core, crypto: various allocation savings regarding tx handling
    
    * core/txlist, txpool: save a reheap operation, avoid some bigint allocs
    
    Co-authored-by: Marius van der Wijden <m.vanderwijden@live.de>

--- core/tx_pool.go
+++ core/tx_pool.go
@@ -1076,8 +1076,8 @@ func (pool *TxPool) runReorg(done chan struct{}, dirtyAccounts *accountSet, even
 
 	// Update all accounts to the latest known pending nonce
 	for addr, list := range pool.pending {
-		txs := list.Flatten() // Heavy but will be cached and is needed by the miner anyway
-		pool.pendingNonces.set(addr, txs[len(txs)-1].Nonce()+1)
+		highestPending := list.LastElement()
+		pool.pendingNonces.set(addr, highestPending.Nonce()+1)
 	}
 	pool.mu.Unlock()
 
@@ -1444,6 +1444,10 @@ func newAccountSet(signer types.Signer, addrs ...common.Address) *accountSet {
 func (as *accountSet) contains(addr common.Address) bool {
 	_, exist := as.accounts[addr]
 	return exist
+}
+
+func (as *accountSet) empty() bool {
+	return len(as.accounts) == 0
 }
 
 // containsTx checks if the sender of a given tx is within the set. If the sender
