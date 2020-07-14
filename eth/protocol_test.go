4f813cfd94d1923652892e02f320d830bce603bd
Alexey Akhunov <akhounov@gmail.com>
2020-07-14 14:42:25 +0100

    Fix more tests

--- eth/protocol_test.go
+++ eth/protocol_test.go
@@ -107,7 +107,7 @@ func TestStatusMsgErrors64(t *testing.T) {
 		genesis = pm.blockchain.Genesis()
 		head    = pm.blockchain.CurrentHeader()
 		td      = pm.blockchain.GetTd(head.Hash(), head.Number.Uint64())
-		forkID  = forkid.NewID(pm.blockchain)
+		forkID  = forkid.NewID(pm.blockchain.Config(), genesis.Hash(), head.Number.Uint64())
 	)
 
 	tests := []struct {
