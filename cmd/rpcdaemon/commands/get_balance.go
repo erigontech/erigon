aae8d9ae8e0156e2d580f860fae202e5386bd75b
Evgeny Danienko <6655321@bk.ru>
2020-07-21 16:47:14 +0300

    rpc addBalance

---
+++ cmd/rpcdaemon/commands/get_balance.go
@@ -0,0 +1,40 @@
+package commands
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/ledgerwatch/turbo-geth/common"
+	"github.com/ledgerwatch/turbo-geth/common/hexutil"
+	"github.com/ledgerwatch/turbo-geth/core/rawdb"
+	"github.com/ledgerwatch/turbo-geth/core/types/accounts"
+	"github.com/ledgerwatch/turbo-geth/ethdb"
+	"github.com/ledgerwatch/turbo-geth/rpc"
+)
+
+func (api *APIImpl) GetBalance(_ context.Context, address common.Address, blockNrOrHash rpc.BlockNumberOrHash) (*hexutil.Big, error) {
+	var blockNumber uint64
+
+	hash, ok := blockNrOrHash.Hash()
+	if !ok {
+		blockNumber = uint64(blockNrOrHash.BlockNumber.Int64())
+	} else {
+		block := rawdb.ReadBlockByHash(api.dbReader, hash)
+		if block == nil {
+			return nil, fmt.Errorf("block %x not found", hash)
+		}
+		blockNumber = block.NumberU64()
+	}
+
+	acc, err := GetAccount(api.db, blockNumber, address)
+	if err != nil {
+		return nil, fmt.Errorf("cant get a balance for account %q for block %v", address.String(), blockNumber)
+	}
+
+	return (*hexutil.Big)(acc.Balance.ToBig()), nil
+}
+
+func GetAccount(chainKV ethdb.KV, blockNumber uint64, address common.Address) (*accounts.Account, error) {
+	reader := NewStateReader(chainKV, blockNumber)
+	return reader.ReadAccountData(address)
+}
