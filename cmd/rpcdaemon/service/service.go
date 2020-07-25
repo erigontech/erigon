d05cfa22f6fe8c584e0cf0b452f03c3edd979cef
Igor Mandrigin <mandrigin@users.noreply.github.com>
2020-07-25 19:18:18 +0200

    Make plain state + staged sync the default (#782)
    * make plain state + staged sync the default
    
    * remove sync mode flag completely
    
    * one thing
    
    * fix the console
    
    * simplify code
    
    * fix and skip tests
    
    * fixup for console tests

---
+++ cmd/rpcdaemon/service/service.go
@@ -0,0 +1,38 @@
+package service
+
+import (
+	"github.com/ledgerwatch/turbo-geth/cmd/rpcdaemon/commands"
+	"github.com/ledgerwatch/turbo-geth/ethdb"
+	"github.com/ledgerwatch/turbo-geth/node"
+	"github.com/ledgerwatch/turbo-geth/p2p"
+	"github.com/ledgerwatch/turbo-geth/rpc"
+)
+
+var _ node.Service = &RPCDaemonService{}
+
+// RPCDaemonService is used in js console and console tests
+type RPCDaemonService struct {
+	api []rpc.API
+	db  *ethdb.ObjectDatabase
+}
+
+func (*RPCDaemonService) Protocols() []p2p.Protocol {
+	return []p2p.Protocol{}
+}
+
+func (r *RPCDaemonService) Start(server *p2p.Server) error {
+	r.api = commands.GetAPI(r.db.KV(), []string{"eth", "debug"})
+	return nil
+}
+
+func (r *RPCDaemonService) Stop() error {
+	return nil
+}
+
+func (r *RPCDaemonService) APIs() []rpc.API {
+	return r.api
+}
+
+func New(db *ethdb.ObjectDatabase) *RPCDaemonService {
+	return &RPCDaemonService{[]rpc.API{}, db}
+}
--- eth/stagedsync/stage_execute_test.go
+++ eth/stagedsync/stage_execute_test.go
@@ -9,78 +9,6 @@ import (
 	"github.com/ledgerwatch/turbo-geth/ethdb"
 )
 
-func TestUnwindExecutionStageHashedStatic(t *testing.T) {
-	initialDb := ethdb.NewMemDatabase()
-	defer initialDb.Close()
-	generateBlocks(t, 1, 50, hashedWriterGen(initialDb), staticCodeStaticIncarnations)
-
-	mutation := ethdb.NewMemDatabase()
-	defer mutation.Close()
-	generateBlocks(t, 1, 100, hashedWriterGen(mutation), staticCodeStaticIncarnations)
-
-	err := stages.SaveStageProgress(mutation, stages.Execution, 100, nil)
-	if err != nil {
-		t.Errorf("error while saving progress: %v", err)
-	}
-
-	u := &UnwindState{UnwindPoint: 50}
-	s := &StageState{BlockNumber: 100}
-	err = UnwindExecutionStage(u, s, mutation)
-	if err != nil {
-		t.Errorf("error while unwinding state: %v", err)
-	}
-
-	compareCurrentState(t, initialDb, mutation, dbutils.CurrentStateBucket, dbutils.ContractCodeBucket)
-}
-
-func TestUnwindExecutionStageHashedWithIncarnationChanges(t *testing.T) {
-	initialDb := ethdb.NewMemDatabase()
-	defer initialDb.Close()
-	generateBlocks(t, 1, 50, hashedWriterGen(initialDb), changeCodeWithIncarnations)
-
-	mutation := ethdb.NewMemDatabase()
-	defer mutation.Close()
-	generateBlocks(t, 1, 100, hashedWriterGen(mutation), changeCodeWithIncarnations)
-
-	err := stages.SaveStageProgress(mutation, stages.Execution, 100, nil)
-	if err != nil {
-		t.Errorf("error while saving progress: %v", err)
-	}
-	u := &UnwindState{UnwindPoint: 50}
-	s := &StageState{BlockNumber: 100}
-	err = UnwindExecutionStage(u, s, mutation)
-
-	if err != nil {
-		t.Errorf("error while unwinding state: %v", err)
-	}
-
-	compareCurrentState(t, initialDb, mutation, dbutils.CurrentStateBucket, dbutils.ContractCodeBucket)
-}
-
-func TestUnwindExecutionStageHashedWithCodeChanges(t *testing.T) {
-	t.Skip("not supported yet, to be restored")
-	initialDb := ethdb.NewMemDatabase()
-	defer initialDb.Close()
-	generateBlocks(t, 1, 50, hashedWriterGen(initialDb), changeCodeIndepenentlyOfIncarnations)
-
-	mutation := ethdb.NewMemDatabase()
-	defer mutation.Close()
-	generateBlocks(t, 1, 100, hashedWriterGen(mutation), changeCodeIndepenentlyOfIncarnations)
-
-	err := stages.SaveStageProgress(mutation, stages.Execution, 100, nil)
-	if err != nil {
-		t.Errorf("error while saving progress: %v", err)
-	}
-	u := &UnwindState{UnwindPoint: 50}
-	s := &StageState{BlockNumber: 100}
-	err = UnwindExecutionStage(u, s, mutation)
-	if err != nil {
-		t.Errorf("error while unwinding state: %v", err)
-	}
-
-	compareCurrentState(t, initialDb, mutation, dbutils.CurrentStateBucket, dbutils.ContractCodeBucket)
-}
-
 func TestUnwindExecutionStagePlainStatic(t *testing.T) {
 	initialDb := ethdb.NewMemDatabase()
 	defer initialDb.Close()
@@ -94,7 +22,6 @@ func TestUnwindExecutionStagePlainStatic(t *testing.T) {
 	if err != nil {
 		t.Errorf("error while saving progress: %v", err)
 	}
-	core.UsePlainStateExecution = true
 	u := &UnwindState{UnwindPoint: 50}
 	s := &StageState{BlockNumber: 100}
 	err = UnwindExecutionStage(u, s, mutation)
@@ -143,7 +70,6 @@ func TestUnwindExecutionStagePlainWithCodeChanges(t *testing.T) {
 	if err != nil {
 		t.Errorf("error while saving progress: %v", err)
 	}
-	core.UsePlainStateExecution = true
 	u := &UnwindState{UnwindPoint: 50}
 	s := &StageState{BlockNumber: 100}
 	err = UnwindExecutionStage(u, s, mutation)
