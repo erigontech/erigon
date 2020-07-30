262c34f9544b213ab2d0ae4c8c66b4c6945d8de3
alex.sharov <AskAlexSharov@gmail.com>
2020-07-30 13:58:47 +0700

    rename_rest_api_param

--- cmd/restapi/commands/root.go
+++ cmd/restapi/commands/root.go
@@ -19,7 +19,7 @@ var (
 )
 
 func init() {
-	rootCmd.Flags().StringVar(&rpcAddr, "private.rpc.addr", "127.0.0.1:9090", "binary RPC network address, for example: 127.0.0.1:9090, empty string means not to start the listener. do not expose to public network. serves remote database interface")
+	rootCmd.Flags().StringVar(&rpcAddr, "private.api.addr", "127.0.0.1:9090", "binary RPC network address, for example: 127.0.0.1:9090, empty string means not to start the listener. do not expose to public network. serves remote database interface")
 	rootCmd.Flags().StringVar(&addr, "http.addr", "127.0.0.1:8080", "REST server listening host")
 	rootCmd.Flags().StringVar(&chaindata, "chaindata", "", "path to the database")
 }
