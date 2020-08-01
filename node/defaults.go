f519c8104db9beb4017545316ae4e701ef558654
Igor Mandrigin <i@mandrigin.ru>
2020-08-01 08:39:11 +0200

    Support XDG_DATA_DIR

--- node/defaults.go
+++ node/defaults.go
@@ -78,7 +78,10 @@ func DefaultDataDir() string {
 			}
 			return filepath.Join(appdata, dirname)
 		default:
-			return filepath.Join(home, fmt.Sprintf(".%s", strings.ToLower(dirname)))
+			if xdgDataDir := os.Getenv("XDG_DATA_HOME"); xdgDataDir != "" {
+				return filepath.Join(xdgDataDir, strings.ToLower(dirname))
+			}
+			return filepath.Join(home, ".local/share", fmt.Sprintf("%s", strings.ToLower(dirname)))
 		}
 	}
 	// As we cannot guess a stable location, return empty and handle later
