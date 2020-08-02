cf290649c1b6268572e8889b951223023400c4c5
alex.sharov <AskAlexSharov@gmail.com>
2020-08-02 17:27:01 +0700

    stage3_block_compression_support

--- eth/stagedsync/stage_senders.go
+++ eth/stagedsync/stage_senders.go
@@ -13,8 +13,10 @@ import (
 	"sync"
 	"time"
 
+	"github.com/golang/snappy"
 	"github.com/ledgerwatch/turbo-geth/common"
 	"github.com/ledgerwatch/turbo-geth/common/dbutils"
+	"github.com/ledgerwatch/turbo-geth/common/debug"
 	"github.com/ledgerwatch/turbo-geth/common/etl"
 	"github.com/ledgerwatch/turbo-geth/core/types"
 	"github.com/ledgerwatch/turbo-geth/crypto/secp256k1"
@@ -136,7 +138,18 @@ func SpawnRecoverSendersStage(cfg Stage3Config, s *StageState, db ethdb.Database
 				}
 			}
 
-			jobs <- &senderRecoveryJob{bodyRlp: common.CopyBytes(v), blockNumber: blockNumber, index: int(blockNumber - s.BlockNumber - 1)}
+			var bodyRlp []byte
+			if debug.IsBlockCompressionEnabled() && len(v) > 0 {
+				var err error
+				bodyRlp, err = snappy.Decode(nil, v)
+				if err != nil {
+					return false, fmt.Errorf("err on decode block: %s", err)
+				}
+			} else {
+				bodyRlp = common.CopyBytes(v)
+			}
+
+			jobs <- &senderRecoveryJob{bodyRlp: bodyRlp, blockNumber: blockNumber, index: int(blockNumber - s.BlockNumber - 1)}
 
 			return true, nil
 		}); err != nil {
