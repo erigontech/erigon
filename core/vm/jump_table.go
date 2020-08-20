d588c07e31e282eb78faf92e59ec61978da4f16f
suhabe <suhabe@bugrara.com>
2020-08-20 08:52:07 -0700

    fix up minor changes to jump table

--- core/vm/jump_table.go
+++ core/vm/jump_table.go
@@ -22,16 +22,15 @@ import (
 )
 
 type (
-	executionFunc   func(pc *uint64, interpreter *EVMInterpreter, callContext *callCtx) ([]byte, error)
-	gasFunc         func(*EVM, *Contract, *stack.Stack, *Memory, uint64) (uint64, error) // last parameter is the requested memory size as a uint64
+	executionFunc  func(pc *uint64, interpreter *EVMInterpreter, callContext *callCtx) ([]byte, error)
+	gasFunc        func(*EVM, *Contract, *stack.Stack, *Memory, uint64) (uint64, error) // last parameter is the requested memory size as a uint64
 	// memorySizeFunc returns the required size, and whether the operation overflowed a uint64
-	memorySizeFunc  func(*stack.Stack) (size uint64, overflow bool)
+	memorySizeFunc func(*stack.Stack) (size uint64, overflow bool)
 )
 
 type operation struct {
 	// execute is the operation function
 	execute     executionFunc
-	// saExecute is the operation for static analysis
 	constantGas uint64
 	dynamicGas  gasFunc
 	// minStack tells how many stack items are required
