0010e411d6bc6bf57a160398a14a55e693eca9b6
alex.sharov <AskAlexSharov@gmail.com>
2020-08-20 09:20:46 +0700

    add validation of namespaces

--- cmd/rpcdaemon/commands/daemon.go
+++ cmd/rpcdaemon/commands/daemon.go
@@ -3,6 +3,7 @@ package commands
 import (
 	"context"
 	"fmt"
+	"github.com/ledgerwatch/turbo-geth/log"
 
 	"github.com/ledgerwatch/turbo-geth/cmd/rpcdaemon/cli"
 	"github.com/ledgerwatch/turbo-geth/core/rawdb"
@@ -34,7 +35,7 @@ func (api *APIImpl) GetBlockByNumber(ctx context.Context, number rpc.BlockNumber
 }
 
 func APIList(db ethdb.KV, eth ethdb.Backend, cfg cli.Flags, customApiList []rpc.API) []rpc.API {
-	var rpcAPI []rpc.API
+	var defaultAPIList []rpc.API
 
 	dbReader := ethdb.NewObjectDatabase(db)
 	apiImpl := NewAPI(db, dbReader, eth, cfg.Gascap)
@@ -44,32 +45,45 @@ func APIList(db ethdb.KV, eth ethdb.Backend, cfg cli.Flags, customApiList []rpc.
 	for _, enabledAPI := range cfg.API {
 		switch enabledAPI {
 		case "eth":
-			rpcAPI = append(rpcAPI, rpc.API{
+			defaultAPIList = append(defaultAPIList, rpc.API{
 				Namespace: "eth",
 				Public:    true,
 				Service:   EthAPI(apiImpl),
 				Version:   "1.0",
 			})
 		case "debug":
-			rpcAPI = append(rpcAPI, rpc.API{
+			defaultAPIList = append(defaultAPIList, rpc.API{
 				Namespace: "debug",
 				Public:    true,
 				Service:   PrivateDebugAPI(dbgAPIImpl),
 				Version:   "1.0",
 			})
 		case "net":
-			rpcAPI = append(rpcAPI, rpc.API{
+			defaultAPIList = append(defaultAPIList, rpc.API{
 				Namespace: "net",
 				Public:    true,
 				Service:   NetAPI(netImpl),
 				Version:   "1.0",
 			})
 
-		default:
-			// TODO: enable validation after checking customApiList
-			//log.Error("Unrecognised", "api", enabledAPI)
 		}
 	}
 
-	return append(rpcAPI, customApiList...)
+	// validation
+	availableNamespaces := map[string]bool{}
+	for _, v := range customApiList {
+		availableNamespaces[v.Namespace] = true
+	}
+	for _, v := range defaultAPIList {
+		availableNamespaces[v.Namespace] = true
+	}
+
+	for i := range cfg.API {
+		_, ok := availableNamespaces[cfg.API[i]]
+		if !ok {
+			log.Error("Unrecognised", "api", cfg.API[i])
+		}
+	}
+
+	return append(defaultAPIList, customApiList...)
 }
