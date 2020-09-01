46882c858d98647c47c3d823ba7908bb3c38340b
Alexey Akhunov <akhounov@gmail.com>
2020-09-01 08:43:41 +0100

    Exclude bits only for StagedSync

--- eth/downloader/downloader.go
+++ eth/downloader/downloader.go
@@ -1541,14 +1541,41 @@ func (d *Downloader) fetchParts(deliveryCh chan dataPack, deliver func(dataPack)
 func (d *Downloader) processHeaders(origin uint64, pivot uint64, blockNumber uint64) error {
 	// Keep a count of uncertain headers to roll back
 	var (
-		mode = d.getMode()
+		rollback    uint64 // Zero means no rollback (fine as you can't unroll the genesis)
+		rollbackErr error
+		mode        = d.getMode()
 	)
+	if mode != StagedSync {
+		defer func() {
+			if rollback > 0 {
+				lastHeader, lastFastBlock, lastBlock := d.lightchain.CurrentHeader().Number, common.Big0, common.Big0
+				if mode != LightSync && mode != StagedSync {
+					lastFastBlock = d.blockchain.CurrentFastBlock().Number()
+					lastBlock = d.blockchain.CurrentBlock().Number()
+				}
+				if err := d.lightchain.SetHead(rollback - 1); err != nil { // -1 to target the parent of the first uncertain block
+					// We're already unwinding the stack, only print the error to make it more visible
+					log.Error("Failed to roll back chain segment", "head", rollback-1, "err", err)
+				}
+				curFastBlock, curBlock := common.Big0, common.Big0
+				if mode != LightSync && mode != StagedSync {
+					curFastBlock = d.blockchain.CurrentFastBlock().Number()
+					curBlock = d.blockchain.CurrentBlock().Number()
+				}
+				log.Warn("Rolled back chain segment",
+					"header", fmt.Sprintf("%d->%d", lastHeader, d.lightchain.CurrentHeader().Number),
+					"fast", fmt.Sprintf("%d->%d", lastFastBlock, curFastBlock),
+					"block", fmt.Sprintf("%d->%d", lastBlock, curBlock), "reason", rollbackErr)
+			}
+		}()
+	}
 	// Wait for batches of headers to process
 	gotHeaders := false
 
 	for {
 		select {
 		case <-d.cancelCh:
+			rollbackErr = errCanceled
 			return errCanceled
 
 		case headers := <-d.headerProcCh:
@@ -1580,6 +1607,10 @@ func (d *Downloader) processHeaders(origin uint64, pivot uint64, blockNumber uin
 					if !gotHeaders && blockNumber > head.NumberU64() {
 						return errStallingPeer
 					}
+				}
+				if mode != StagedSync {
+					// Disable any rollback and return
+					rollback = 0
 				}
 				// If fast or light syncing, ensure promised headers are indeed delivered. This is
 				// needed to detect scenarios where an attacker feeds a bad pivot and then bails out
@@ -1617,6 +1648,7 @@ func (d *Downloader) processHeaders(origin uint64, pivot uint64, blockNumber uin
 					if chunk[len(chunk)-1].Number.Uint64()+uint64(fsHeaderForceVerify) > pivot {
 						frequency = 1
 					}
+					var n int
 					var err error
 					if mode == StagedSync {
 						var reorg bool
@@ -1629,12 +1661,48 @@ func (d *Downloader) processHeaders(origin uint64, pivot uint64, blockNumber uin
 							}
 						}
 					} else {
-						_, err = d.lightchain.InsertHeaderChain(chunk, frequency)
+						n, err = d.lightchain.InsertHeaderChain(chunk, frequency)
 					}
 					if err == nil && mode == StagedSync && d.headersState != nil {
 						if err1 := d.headersState.Update(d.stateDB, chunk[len(chunk)-1].Number.Uint64()); err1 != nil {
 							return fmt.Errorf("saving SyncStage Headers progress: %v", err1)
 						}
+					}
+					if mode != StagedSync && err != nil {
+						// If some headers were inserted, add them too to the rollback list
+						if n > 0 && rollback == 0 {
+							rollback = chunk[0].Number.Uint64()
+						}
+						log.Debug("Invalid header encountered", "number", chunk[n].Number, "hash", chunk[n].Hash(), "parent", chunk[n].ParentHash, "err", err)
+						return fmt.Errorf("%w: %v", errInvalidChain, err)
+					}
+
+					// All verifications passed, track all headers within the alloted limits
+					if mode != StagedSync {
+						head := chunk[len(chunk)-1].Number.Uint64()
+						if head-rollback > uint64(fsHeaderSafetyNet) {
+							rollback = head - uint64(fsHeaderSafetyNet)
+						} else {
+							rollback = 1
+						}
+					}
+				}
+				// Unless we're doing light chains, schedule the headers for associated content retrieval
+				if mode == FullSync || mode == FastSync {
+					// If we've reached the allowed number of pending headers, stall a bit
+					for d.queue.PendingBlocks() >= maxQueuedHeaders || d.queue.PendingReceipts() >= maxQueuedHeaders {
+						select {
+						case <-d.cancelCh:
+							rollbackErr = errCanceled
+							return errCanceled
+						case <-time.After(time.Second):
+						}
+					}
+					// Otherwise insert the headers for content retrieval
+					inserts := d.queue.Schedule(chunk, origin)
+					if len(inserts) != len(chunk) {
+						rollbackErr = fmt.Errorf("stale headers: len inserts %v len(chunk) %v", len(inserts), len(chunk))
+						return fmt.Errorf("%w: stale headers", errBadPeer)
 					}
 				}
 				headers = headers[limit:]
