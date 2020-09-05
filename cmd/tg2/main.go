93ed53bc3c426fba2eabdd4198b83e626cc2ca9d
Igor Mandrigin <i@mandrigin.ru>
2020-09-05 14:03:24 +0200

    create a turbocli

--- cmd/tg2/main.go
+++ cmd/tg2/main.go
@@ -4,20 +4,14 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/ledgerwatch/turbo-geth/cmd/utils"
-	"github.com/ledgerwatch/turbo-geth/console/prompt"
-	"github.com/ledgerwatch/turbo-geth/eth"
 	"github.com/ledgerwatch/turbo-geth/eth/stagedsync"
-	"github.com/ledgerwatch/turbo-geth/internal/debug"
-	"github.com/ledgerwatch/turbo-geth/internal/flags"
-	"github.com/ledgerwatch/turbo-geth/node"
-	"github.com/ledgerwatch/turbo-geth/params"
+	turbocli "github.com/ledgerwatch/turbo-geth/turbo/cli"
 
 	"github.com/urfave/cli"
 )
 
 func main() {
-	app := MakeApp(runTurboGeth)
+	app := turbocli.MakeApp(runTurboGeth, turbocli.DefaultFlags)
 	if err := app.Run(os.Args); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
@@ -30,130 +24,6 @@ func runTurboGeth(ctx *cli.Context) {
 		stagedsync.DefaultUnwindOrder(),
 	)
 
-	tg := MakeTurboGethNode(ctx, sync)
+	tg := turbocli.New(ctx, sync)
 	tg.Serve()
-}
-
-func MakeApp(action func(*cli.Context)) *cli.App {
-	app := flags.NewApp("", "", "turbo-geth experimental cli")
-	app.Action = action
-	app.Flags = DefaultFlags
-	app.Before = func(ctx *cli.Context) error {
-		return debug.Setup(ctx)
-	}
-	app.After = func(ctx *cli.Context) error {
-		debug.Exit()
-		prompt.Stdin.Close() // Resets terminal mode.
-		return nil
-	}
-	return app
-}
-
-var DefaultFlags = []cli.Flag{
-	utils.DataDirFlag,
-	utils.KeyStoreDirFlag,
-	utils.EthashDatasetDirFlag,
-	utils.TxPoolLocalsFlag,
-	utils.TxPoolNoLocalsFlag,
-	utils.TxPoolJournalFlag,
-	utils.TxPoolRejournalFlag,
-	utils.TxPoolPriceLimitFlag,
-	utils.TxPoolPriceBumpFlag,
-	utils.TxPoolAccountSlotsFlag,
-	utils.TxPoolGlobalSlotsFlag,
-	utils.TxPoolAccountQueueFlag,
-	utils.TxPoolGlobalQueueFlag,
-	utils.TxPoolLifetimeFlag,
-	utils.TxLookupLimitFlag,
-	utils.StorageModeFlag,
-	utils.HddFlag,
-	utils.DatabaseFlag,
-	utils.LMDBMapSizeFlag,
-	utils.PrivateApiAddr,
-	utils.ListenPortFlag,
-	utils.NATFlag,
-	utils.NoDiscoverFlag,
-	utils.DiscoveryV5Flag,
-	utils.NetrestrictFlag,
-	utils.NodeKeyFileFlag,
-	utils.NodeKeyHexFlag,
-	utils.DNSDiscoveryFlag,
-	utils.RopstenFlag,
-	utils.RinkebyFlag,
-	utils.GoerliFlag,
-	utils.YoloV1Flag,
-	utils.VMEnableDebugFlag,
-	utils.NetworkIdFlag,
-	utils.EthStatsURLFlag,
-	utils.FakePoWFlag,
-	utils.NoCompactionFlag,
-	utils.GpoBlocksFlag,
-	utils.GpoPercentileFlag,
-	utils.EWASMInterpreterFlag,
-	utils.EVMInterpreterFlag,
-	utils.DebugProtocolFlag,
-	utils.IPCDisabledFlag,
-	utils.IPCPathFlag,
-	utils.InsecureUnlockAllowedFlag,
-	utils.MetricsEnabledFlag,
-	utils.MetricsEnabledExpensiveFlag,
-	utils.MetricsHTTPFlag,
-	utils.MetricsPortFlag,
-	configFileFlag,
-}
-
-var configFileFlag = cli.StringFlag{
-	Name:  "config",
-	Usage: "TOML configuration file",
-}
-
-type TurboGeth struct {
-	stack   *node.Node
-	backend *eth.Ethereum
-}
-
-func (tg *TurboGeth) Serve() error {
-	defer tg.stack.Close()
-
-	tg.run()
-
-	tg.stack.Wait()
-
-	return nil
-}
-
-func (tg *TurboGeth) run() {
-	utils.StartNode(tg.stack)
-	// we don't have accounts locally and we don't do mining
-	// so these parts are ignored
-	// see cmd/geth/main.go#startNode for full implementation
-}
-
-func MakeTurboGethNode(ctx *cli.Context, sync *stagedsync.StagedSync) *TurboGeth {
-	node := MakeConfigNode(ctx)
-
-	ethConfig := eth.DefaultConfig
-	utils.SetEthConfig(ctx, node, &ethConfig)
-	ethConfig.StagedSync = sync
-
-	ethereum := utils.RegisterEthService(node, &ethConfig)
-
-	return &TurboGeth{stack: node, backend: ethereum}
-}
-
-func MakeConfigNode(ctx *cli.Context) *node.Node {
-	nodeConfig := node.DefaultConfig
-	// see simiar changes in `cmd/geth/config.go#defaultNodeConfig`
-	nodeConfig.Version = params.Version
-	nodeConfig.IPCPath = "tg.ipc"
-	nodeConfig.Name = "turbo-geth"
-
-	utils.SetNodeConfig(ctx, &nodeConfig)
-
-	stack, err := node.New(&nodeConfig)
-	if err != nil {
-		utils.Fatalf("Failed to create turbo-geth node: %v", err)
-	}
-
-	return stack
 }
---
+++ turbo/cli/turbogeth.go
@@ -0,0 +1,140 @@
+package cli
+
+import (
+	"math"
+	"runtime/debug"
+	"strconv"
+	"time"
+
+	"github.com/ledgerwatch/turbo-geth/cmd/utils"
+	"github.com/ledgerwatch/turbo-geth/eth"
+	"github.com/ledgerwatch/turbo-geth/eth/stagedsync"
+	"github.com/ledgerwatch/turbo-geth/log"
+	"github.com/ledgerwatch/turbo-geth/metrics"
+	"github.com/ledgerwatch/turbo-geth/node"
+	"github.com/ledgerwatch/turbo-geth/params"
+
+	"github.com/urfave/cli"
+
+	gopsutil "github.com/shirou/gopsutil/mem"
+)
+
+type TurboGeth struct {
+	stack   *node.Node
+	backend *eth.Ethereum
+}
+
+func (tg *TurboGeth) Serve() error {
+	defer tg.stack.Close()
+
+	tg.run()
+
+	tg.stack.Wait()
+
+	return nil
+}
+
+func (tg *TurboGeth) run() {
+	utils.StartNode(tg.stack)
+	// we don't have accounts locally and we don't do mining
+	// so these parts are ignored
+	// see cmd/geth/main.go#startNode for full implementation
+}
+
+func New(ctx *cli.Context, sync *stagedsync.StagedSync) *TurboGeth {
+	prepare(ctx)
+	node := makeConfigNode(ctx)
+
+	ethConfig := eth.DefaultConfig
+	utils.SetEthConfig(ctx, node, &ethConfig)
+	ethConfig.StagedSync = sync
+
+	ethereum := utils.RegisterEthService(node, &ethConfig)
+
+	return &TurboGeth{stack: node, backend: ethereum}
+}
+
+func makeConfigNode(ctx *cli.Context) *node.Node {
+	nodeConfig := node.DefaultConfig
+	// see simiar changes in `cmd/geth/config.go#defaultNodeConfig`
+	nodeConfig.Version = params.Version
+	nodeConfig.IPCPath = "tg.ipc"
+	nodeConfig.Name = "turbo-geth"
+
+	utils.SetNodeConfig(ctx, &nodeConfig)
+
+	stack, err := node.New(&nodeConfig)
+	if err != nil {
+		utils.Fatalf("Failed to create turbo-geth node: %v", err)
+	}
+
+	return stack
+}
+
+// prepare manipulates memory cache allowance and setups metric system.
+// This function should be called before launching devp2p stack.
+func prepare(ctx *cli.Context) {
+	// If we're running a known preset, log it for convenience.
+	switch {
+	case ctx.GlobalIsSet(utils.LegacyTestnetFlag.Name):
+		log.Info("Starting Turbo-Geth on Ropsten testnet...")
+		log.Warn("The --testnet flag is ambiguous! Please specify one of --goerli, --rinkeby, or --ropsten.")
+		log.Warn("The generic --testnet flag is deprecated and will be removed in the future!")
+
+	case ctx.GlobalIsSet(utils.RopstenFlag.Name):
+		log.Info("Starting Turbo-Geth on Ropsten testnet...")
+
+	case ctx.GlobalIsSet(utils.RinkebyFlag.Name):
+		log.Info("Starting Turbo-Geth on Rinkeby testnet...")
+
+	case ctx.GlobalIsSet(utils.GoerliFlag.Name):
+		log.Info("Starting Turbo-Geth on Görli testnet...")
+
+	case ctx.GlobalIsSet(utils.DeveloperFlag.Name):
+		log.Info("Starting Turbo-Geth in ephemeral dev mode...")
+
+	case !ctx.GlobalIsSet(utils.NetworkIdFlag.Name):
+		log.Info("Starting Turbo-Geth on Ethereum mainnet...")
+	}
+	// If we're a full node on mainnet without --cache specified, bump default cache allowance
+	if !ctx.GlobalIsSet(utils.CacheFlag.Name) && !ctx.GlobalIsSet(utils.NetworkIdFlag.Name) {
+		// Make sure we're not on any supported preconfigured testnet either
+		if !ctx.GlobalIsSet(utils.LegacyTestnetFlag.Name) && !ctx.GlobalIsSet(utils.RopstenFlag.Name) && !ctx.GlobalIsSet(utils.RinkebyFlag.Name) && !ctx.GlobalIsSet(utils.GoerliFlag.Name) && !ctx.GlobalIsSet(utils.DeveloperFlag.Name) {
+			// Nope, we're really on mainnet. Bump that cache up!
+			log.Info("Bumping default cache on mainnet", "provided", ctx.GlobalInt(utils.CacheFlag.Name), "updated", 4096)
+			ctx.GlobalSet(utils.CacheFlag.Name, strconv.Itoa(4096))
+		}
+	}
+	// If we're running a light client on any network, drop the cache to some meaningfully low amount
+	if !ctx.GlobalIsSet(utils.CacheFlag.Name) {
+		log.Info("Dropping default light client cache", "provided", ctx.GlobalInt(utils.CacheFlag.Name), "updated", 128)
+		ctx.GlobalSet(utils.CacheFlag.Name, strconv.Itoa(128))
+	}
+	// Cap the cache allowance and tune the garbage collector
+	mem, err := gopsutil.VirtualMemory()
+	if err == nil {
+		if 32<<(^uintptr(0)>>63) == 32 && mem.Total > 2*1024*1024*1024 {
+			log.Warn("Lowering memory allowance on 32bit arch", "available", mem.Total/1024/1024, "addressable", 2*1024)
+			mem.Total = 2 * 1024 * 1024 * 1024
+		}
+		allowance := int(mem.Total / 1024 / 1024 / 3)
+		if cache := ctx.GlobalInt(utils.CacheFlag.Name); cache > allowance {
+			log.Warn("Sanitizing cache to Go's GC limits", "provided", cache, "updated", allowance)
+			if err = ctx.GlobalSet(utils.CacheFlag.Name, strconv.Itoa(allowance)); err != nil {
+				log.Error("Error while sanitizing cache to Go's GC limits", "err", err)
+			}
+		}
+	}
+	// Ensure Go's GC ignores the database cache for trigger percentage
+	cache := ctx.GlobalInt(utils.CacheFlag.Name)
+	gogc := math.Max(20, math.Min(100, 100/(float64(cache)/1024)))
+
+	log.Debug("Sanitizing Go's GC trigger", "percent", int(gogc))
+	debug.SetGCPercent(int(gogc))
+
+	// Start metrics export if enabled
+	utils.SetupMetrics(ctx)
+
+	// Start system runtime metrics collection
+	go metrics.CollectProcessMetrics(3 * time.Second)
+}
