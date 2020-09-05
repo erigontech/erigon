7f5088ca75e9e0e14ff28dcdfe3a22f45f10e8f7
Igor Mandrigin <i@mandrigin.ru>
2020-09-05 14:56:48 +0200

    use a custom parameters within a custom stage

--- cmd/tg2/main.go
+++ cmd/tg2/main.go
@@ -13,15 +13,20 @@ import (
 	"github.com/urfave/cli"
 )
 
+var flag = cli.StringFlag{
+	Name:  "custom-stage-greeting",
+	Value: "default-value",
+}
+
 func main() {
-	app := turbocli.MakeApp(runTurboGeth, turbocli.DefaultFlags)
+	app := turbocli.MakeApp(runTurboGeth, append(turbocli.DefaultFlags, flag))
 	if err := app.Run(os.Args); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
 
-func syncStages() stagedsync.StageBuilders {
+func syncStages(ctx *cli.Context) stagedsync.StageBuilders {
 	return stagedsync.StageBuilders(append(
 		stagedsync.DefaultStages(),
 		stagedsync.StageBuilder{
@@ -31,11 +36,11 @@ func syncStages() stagedsync.StageBuilders {
 					ID:          stages.SyncStage(0x0F),
 					Description: "Custom Stage",
 					ExecFunc: func(s *stagedsync.StageState, _ stagedsync.Unwinder) error {
-						fmt.Println("hello from the custom stage")
+						fmt.Println("hello from the custom stage", ctx.String(flag.Name))
 						return nil
 					},
 					UnwindFunc: func(u *stagedsync.UnwindState, s *stagedsync.StageState) error {
-						fmt.Println("hello from the custom stage unwind")
+						fmt.Println("hello from the custom stage unwind", ctx.String(flag.Name))
 						return nil
 					},
 				}
@@ -46,7 +51,7 @@ func syncStages() stagedsync.StageBuilders {
 
 func runTurboGeth(ctx *cli.Context) {
 	sync := stagedsync.New(
-		syncStages(),
+		syncStages(ctx),
 		stagedsync.DefaultUnwindOrder(),
 	)
 
