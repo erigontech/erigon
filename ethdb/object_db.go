c45a710ce69b8f886cb3d22f0219c2f65542ad33
Alex Sharov <AskAlexSharov@gmail.com>
2020-09-08 14:28:37 +0700

    Warmup logs, less overhead, warmup code bucket (#1054)
    * warmup logs and less overhead.
    
    * warmup logs and less overhead.
    
    * move WarmUp to common func

--- ethdb/object_db.go
+++ ethdb/object_db.go
@@ -29,6 +29,7 @@ import (
 	"github.com/ledgerwatch/turbo-geth/log"
 	"github.com/ledgerwatch/turbo-geth/metrics"
 	"strings"
+	"time"
 )
 
 var (
@@ -446,4 +447,29 @@ func InspectDatabase(db Database) error {
 func NewDatabaseWithFreezer(db *ObjectDatabase, dir, suffix string) (*ObjectDatabase, error) {
 	// FIXME: implement freezer in Turbo-Geth
 	return db, nil
+}
+
+func WarmUp(tx Tx, bucket string, logEvery *time.Ticker, quit <-chan struct{}) error {
+	count := 0
+	c := tx.Cursor(bucket)
+	totalKeys, errCount := c.Count()
+	if errCount != nil {
+		return errCount
+	}
+	for k, _, err := c.First(); k != nil; k, _, err = c.Next() {
+		if err != nil {
+			return err
+		}
+		count++
+
+		select {
+		default:
+		case <-quit:
+			return common.ErrStopped
+		case <-logEvery.C:
+			log.Info("Warmed up state", "progress", fmt.Sprintf("%.2fM/%.2fM", float64(count)/1_000_000, float64(totalKeys)/1_000_000))
+		}
+	}
+
+	return nil
 }
