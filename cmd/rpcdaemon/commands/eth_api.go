2c55851be6ea1022d62ca6530d3627f046d1fd0d
Thomas Jay Rush <jrush@quickblocks.io>
2020-09-10 22:22:38 -0400

    Fixing issue #1070 Add eth_getCode

--- cmd/rpcdaemon/commands/eth_api.go
+++ cmd/rpcdaemon/commands/eth_api.go
@@ -39,6 +39,7 @@ type EthAPI interface {
 	GetTransactionByBlockHashAndIndex(ctx context.Context, blockHash common.Hash, txIndex hexutil.Uint64) (*RPCTransaction, error)
 	GetTransactionByBlockNumberAndIndex(ctx context.Context, blockNr rpc.BlockNumber, txIndex hexutil.Uint) (*RPCTransaction, error)
 	GetStorageAt(ctx context.Context, address common.Address, index string, blockNrOrHash rpc.BlockNumberOrHash) (string, error)
+	GetCode(ctx context.Context, address common.Address, blockNrOrHash rpc.BlockNumberOrHash) (hexutil.Bytes, error)
 }
 
 // APIImpl is implementation of the EthAPI interface based on remote Db access
@@ -234,4 +235,29 @@ func (api *APIImpl) GetStorageAt(ctx context.Context, address common.Address, in
 	}
 
 	return hexutil.Encode(res), nil
+}
+
+// GetCode returns the code stored at the given address in the state for the given block number.
+func (api *APIImpl) GetCode(ctx context.Context, address common.Address, blockNrOrHash rpc.BlockNumberOrHash) (hexutil.Bytes, error) {
+	blockNumber, _, err := rpchelper.GetBlockNumber(blockNrOrHash, api.dbReader)
+	if err != nil {
+		return nil, err
+	}
+
+	reader := adapter.NewStateReader(api.db, blockNumber)
+	acc, err := reader.ReadAccountData(address)
+	if err != nil {
+		return nil, err
+	}
+
+	if acc == nil {
+		return nil, fmt.Errorf("account %s not found", address)
+	}
+
+	res, err := reader.ReadAccountCode(address, acc.CodeHash)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
 }
