6008928a40901acb06b55c4c81a18e60809a29c0
alex.sharov <AskAlexSharov@gmail.com>
2020-09-23 11:47:23 +0700

    clean

--- cmd/integration/commands/stages.go
+++ cmd/integration/commands/stages.go
@@ -317,7 +317,7 @@ func stageLogIndex(ctx context.Context) error {
 	max := 0
 	count := 0
 	if err := db.Walk(dbutils.LogIndex, nil, 0, func(k, v []byte) (bool, error) {
-		if len(v) > 512 {
+		if len(v) > 2048 {
 			fmt.Printf("%d %x\n", len(v), k)
 		}
 
