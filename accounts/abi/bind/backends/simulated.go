48efbc29f3c2804ac7e8858767095c33105100cf
Alexey Akhunov <akhounov@gmail.com>
2020-09-26 12:10:19 +0100

    Ignore errors in the simulated backend

--- accounts/abi/bind/backends/simulated.go
+++ accounts/abi/bind/backends/simulated.go
@@ -167,9 +167,7 @@ func (b *SimulatedBackend) Commit() {
 	//fmt.Printf("---- Start committing block %d\n", b.pendingBlock.NumberU64())
 	b.mu.Lock()
 	defer b.mu.Unlock()
-	if err := stagedsync.InsertBlockInStages(b.database, b.config, b.blockchain.Engine(), b.pendingBlock, b.blockchain); err != nil {
-		panic(err)
-	}
+	stagedsync.InsertBlockInStages(b.database, b.config, b.blockchain.Engine(), b.pendingBlock, b.blockchain)
 	//nolint:prealloc
 	var allLogs []*types.Log
 	for _, r := range b.pendingReceipts {
