f4ec3866f187fd4d31889fe3eac199f03ba011c7
alex.sharov <AskAlexSharov@gmail.com>
2020-09-27 20:32:30 +0700

    remove debug prints

--- migrations/dupsort_state.go
+++ migrations/dupsort_state.go
@@ -180,7 +180,7 @@ var zstd = Migration{
 			}
 			total += len(v)
 			blockNum := binary.BigEndian.Uint64(k)
-			if blockNum%320 == 0 {
+			if blockNum%1024 == 0 {
 				samples = append(samples, v)
 			}
 
