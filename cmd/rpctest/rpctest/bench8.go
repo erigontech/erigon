bd58837183ff98ceedc69b4387c2fce1a6d02e90
Alexey Akhunov <akhounov@gmail.com>
2020-09-27 18:40:31 +0100

    tests for eth_getLogs involving topics

--- cmd/rpctest/rpctest/bench8.go
+++ cmd/rpctest/rpctest/bench8.go
@@ -2,8 +2,11 @@ package rpctest
 
 import (
 	"fmt"
+	"math/rand"
 	"net/http"
 	"time"
+
+	"github.com/ledgerwatch/turbo-geth/log"
 )
 
 func Bench8(tgURL, gethURL string, needCompare bool, blockNum uint64) {
@@ -33,6 +36,7 @@ func Bench8(tgURL, gethURL string, needCompare bool, blockNum uint64) {
 
 	firstBn := int(blockNum)
 	prevBn := firstBn
+	rnd := rand.New(rand.NewSource(42))
 	for bn := firstBn; bn <= int(lastBlock); bn++ {
 
 		if prevBn < bn && bn%100 == 0 {
@@ -77,6 +81,75 @@ func Bench8(tgURL, gethURL string, needCompare bool, blockNum uint64) {
 							fmt.Printf("Different logs for account %x and block %d-%d\n", account, prevBn, bn)
 							return
 						}
+					}
+					log.Info("Results", "count", len(logs.Result))
+					topics := getTopics(&logs)
+					// All combination of account and one topic
+					for _, topic := range topics {
+						reqGen.reqID++
+						var logs1 EthLogs
+						res = reqGen.TurboGeth("eth_getLogs", reqGen.getLogs1(prevBn, bn+10000, account, topic), &logs1)
+						if res.Err != nil {
+							fmt.Printf("Could not get logs for account (turbo-geth) %x %x: %v\n", account, topic, res.Err)
+							return
+						}
+						if logs1.Error != nil {
+							fmt.Printf("Error getting logs for account (turbo-geth) %x %x: %d %s\n", account, topic, logs1.Error.Code, logs1.Error.Message)
+							return
+						}
+						if needCompare {
+							var logsg1 EthLogs
+							res = reqGen.Geth("eth_getLogs", reqGen.getLogs1(prevBn, bn+10000, account, topic), &logsg1)
+							if res.Err != nil {
+								fmt.Printf("Could not get logs for account (geth) %x %x: %v\n", account, topic, res.Err)
+								return
+							}
+							if logsg1.Error != nil {
+								fmt.Printf("Error getting logs for account (geth) %x %x: %d %s\n", account, topic, logsg1.Error.Code, logsg1.Error.Message)
+								return
+							}
+							if !compareLogs(&logs1, &logsg1) {
+								fmt.Printf("Different logs for account %x %x and block %d-%d\n", account, topic, prevBn, bn)
+								return
+							}
+						}
+						log.Info("Results", "count", len(logs1.Result))
+					}
+					// Random combinations of two topics
+					if len(topics) >= 2 {
+						idx1 := rnd.Int31n(int32(len(topics)))
+						idx2 := rnd.Int31n(int32(len(topics) - 1))
+						if idx2 >= idx1 {
+							idx2++
+						}
+						reqGen.reqID++
+						var logs2 EthLogs
+						res = reqGen.TurboGeth("eth_getLogs", reqGen.getLogs2(prevBn, bn+100000, account, topics[idx1], topics[idx2]), &logs2)
+						if res.Err != nil {
+							fmt.Printf("Could not get logs for account (turbo-geth) %x %x %x: %v\n", account, topics[idx1], topics[idx2], res.Err)
+							return
+						}
+						if logs2.Error != nil {
+							fmt.Printf("Error getting logs for account (turbo-geth) %x %x %x: %d %s\n", account, topics[idx1], topics[idx2], logs2.Error.Code, logs2.Error.Message)
+							return
+						}
+						if needCompare {
+							var logsg2 EthLogs
+							res = reqGen.Geth("eth_getLogs", reqGen.getLogs2(prevBn, bn+100000, account, topics[idx1], topics[idx2]), &logsg2)
+							if res.Err != nil {
+								fmt.Printf("Could not get logs for account (geth) %x %x %x: %v\n", account, topics[idx1], topics[idx2], res.Err)
+								return
+							}
+							if logsg2.Error != nil {
+								fmt.Printf("Error getting logs for account (geth) %x %x %x: %d %s\n", account, topics[idx1], topics[idx2], logsg2.Error.Code, logsg2.Error.Message)
+								return
+							}
+							if !compareLogs(&logs2, &logsg2) {
+								fmt.Printf("Different logs for account %x %x %x and block %d-%d\n", account, topics[idx1], topics[idx2], prevBn, bn)
+								return
+							}
+						}
+						log.Info("Results", "count", len(logs2.Result))
 					}
 				}
 			}
