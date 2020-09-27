6f51d64366e867e2f3ba8dabdd055e41b976a846
alex.sharov <AskAlexSharov@gmail.com>
2020-09-27 12:23:22 +0700

    clean

--- migrations/dupsort_state.go
+++ migrations/dupsort_state.go
@@ -2,12 +2,17 @@ package migrations
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 
+	"github.com/ledgerwatch/turbo-geth/common"
 	"github.com/ledgerwatch/turbo-geth/common/dbutils"
 	"github.com/ledgerwatch/turbo-geth/common/etl"
 	"github.com/ledgerwatch/turbo-geth/eth/stagedsync/stages"
 	"github.com/ledgerwatch/turbo-geth/ethdb"
+	"github.com/ledgerwatch/turbo-geth/log"
 	"github.com/ledgerwatch/turbo-geth/turbo/trie"
+	"github.com/valyala/gozstd"
 )
 
 var dupSortHashState = Migration{
@@ -176,9 +181,9 @@ var zstd = Migration{
 			}
 			total += len(v)
 			blockNum := binary.BigEndian.Uint64(k)
-			//if blockNum%3 != 0 {
-			//	continue
-			//}
+			if rand.Intn(10) <= 2 {
+				continue
+			}
 			samples = append(samples, v)
 			if blockNum >= 6_000_000 {
 				break
