a877c09e4656ac148758470be1675b92ffeb2156
alex.sharov <AskAlexSharov@gmail.com>
2020-10-02 15:04:58 +0700

    switch receipts to cbor

--- migrations/receipts.go
+++ migrations/receipts.go
@@ -1,21 +1,38 @@
 package migrations
 
 import (
+	"encoding/binary"
 	"fmt"
+	"runtime"
+	"time"
+
 	"github.com/ledgerwatch/turbo-geth/common"
 	"github.com/ledgerwatch/turbo-geth/common/dbutils"
 	"github.com/ledgerwatch/turbo-geth/common/etl"
 	"github.com/ledgerwatch/turbo-geth/core/types"
 	"github.com/ledgerwatch/turbo-geth/ethdb"
 	"github.com/ledgerwatch/turbo-geth/ethdb/cbor"
+	"github.com/ledgerwatch/turbo-geth/log"
 	"github.com/ledgerwatch/turbo-geth/rlp"
 )
 
 var receiptsCborEncode = Migration{
 	Name: "receipts_cbor_encode",
 	Up: func(db ethdb.Database, datadir string, OnLoadCommit etl.LoadCommitHandler) error {
+		logEvery := time.NewTicker(30 * time.Second)
+		defer logEvery.Stop()
+
 		buf := make([]byte, 0, 100_000)
 		if err := db.Walk(dbutils.BlockReceiptsPrefix, nil, 0, func(k, v []byte) (bool, error) {
+			blockNum := binary.BigEndian.Uint64(k[:8])
+			select {
+			default:
+			case <-logEvery.C:
+				var m runtime.MemStats
+				runtime.ReadMemStats(&m)
+				log.Info("Migration progress", "blockNum", blockNum, "alloc", common.StorageSize(m.Alloc), "sys", common.StorageSize(m.Sys))
+			}
+
 			// Convert the receipts from their storage form to their internal representation
 			storageReceipts := []*types.ReceiptForStorage{}
 			if err := rlp.DecodeBytes(v, &storageReceipts); err != nil {
