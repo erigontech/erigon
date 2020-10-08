f674f121831ccdc5870283fca80e92cf1bcdf496
alex.sharov <AskAlexSharov@gmail.com>
2020-10-08 09:07:30 +0700

    clean

--- ethdb/kv_remote.go
+++ ethdb/kv_remote.go
@@ -245,8 +245,6 @@ func (tx *remoteTx) Commit(ctx context.Context) error {
 }
 
 func (tx *remoteTx) Rollback() {
-	// signal server for graceful shutdown
-	// after signaling need wait for .Recv() or cancel context to ensure that resources are free
 	for _, c := range tx.cursors {
 		c.Close()
 	}
