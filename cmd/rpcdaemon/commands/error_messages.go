ec488186c806668dbdc2478d8d28c41854a2ed6a
Thomas Jay Rush <jrush@quickblocks.io>
2020-10-08 23:30:23 -0400

    Added shh endpoints to RPC, all depreciated, extended testing.

--- cmd/rpcdaemon/commands/error_messages.go
+++ cmd/rpcdaemon/commands/error_messages.go
@@ -5,3 +5,6 @@ const NotImplemented = "the function %s is currently not implemented"
 
 // NotAvailableChainData x
 const NotAvailableChainData = "the function %s is not available, please use --private.api.addr option instead of --chaindata option"
+
+// NotAvailableDepreciated x
+const NotAvailableDepreciated = "the function %s has been depreciated"
