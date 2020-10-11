8415ab475e8075718aa2f11eb50c897f809672f6
Alex Sharov <AskAlexSharov@gmail.com>
2020-10-11 10:01:01 +0700

    increase_stereams_default (#1219)

--- ethdb/remote/remotedbserver/server.go
+++ ethdb/remote/remotedbserver/server.go
@@ -62,7 +62,7 @@ func StartGrpc(kv ethdb.KV, eth core.Backend, addr string, creds *credentials.Tr
 		grpc.NumStreamWorkers(cpus), // reduce amount of goroutines
 		grpc.WriteBufferSize(1024),  // reduce buffers to save mem
 		grpc.ReadBufferSize(1024),
-		grpc.MaxConcurrentStreams(cpus * 3), // to force clients reduce concurrency level
+		grpc.MaxConcurrentStreams(100), // to force clients reduce concurrency level
 		grpc.KeepaliveParams(keepalive.ServerParameters{
 			Time: 10 * time.Minute,
 		}),
