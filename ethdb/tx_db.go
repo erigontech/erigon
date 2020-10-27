a9ae477d8f2e3be7024013f226440a673b1394e8
ledgerwatch <akhounov@gmail.com>
2020-10-27 13:37:37 +0000

    [WIP - Discussion - Perf Testing] Use BTree instead of maps for Mutation (batch) (#1294)
    * Try mutation based on B-tree
    
    * Use pointers
    
    * Fix NPE
    
    * Cleanup

--- ethdb/tx_db.go
+++ ethdb/tx_db.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/google/btree"
 	"github.com/ledgerwatch/turbo-geth/common"
 	"github.com/ledgerwatch/turbo-geth/common/dbutils"
 	"github.com/ledgerwatch/turbo-geth/log"
@@ -82,7 +83,7 @@ func (m *TxDb) Delete(bucket string, key []byte) error {
 func (m *TxDb) NewBatch() DbWithPendingMutations {
 	return &mutation{
 		db:   m,
-		puts: newPuts(),
+		puts: btree.New(32),
 	}
 }
 
