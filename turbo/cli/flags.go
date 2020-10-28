d279c054e0a0dc6a06c732e6e1542be78163b9e1
alex.sharov <AskAlexSharov@gmail.com>
2020-10-28 10:12:55 +0700

    check canonical receipt

--- turbo/cli/flags.go
+++ turbo/cli/flags.go
@@ -18,7 +18,7 @@ import (
 var (
 	DatabaseFlag = cli.StringFlag{
 		Name:  "database",
-		Usage: "Which database software to use? Currently supported values: lmdb",
+		Usage: "Which database software to use? Currently supported values: lmdb|mdbx",
 		Value: "lmdb",
 	}
 	BatchSizeFlag = cli.StringFlag{
@@ -131,6 +131,7 @@ func ApplyFlagsForNodeConfig(ctx *cli.Context, cfg *node.Config) {
 	setPrivateApi(ctx, cfg)
 
 	databaseFlag := ctx.GlobalString(DatabaseFlag.Name)
+	cfg.MDBX = strings.EqualFold(databaseFlag, "mdbx") //case insensitive
 	cfg.LMDB = strings.EqualFold(databaseFlag, "lmdb") //case insensitive
 	if cfg.LMDB && ctx.GlobalString(LMDBMapSizeFlag.Name) != "" {
 		err := cfg.LMDBMapSize.UnmarshalText([]byte(ctx.GlobalString(LMDBMapSizeFlag.Name)))
