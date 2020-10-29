37e36fd2aaf3dcac5c0549e17f9c6fbdabf4fa37
alex.sharov <AskAlexSharov@gmail.com>
2020-10-29 13:20:57 +0700

    up devel

--- ethdb/kv_lmdb.go
+++ ethdb/kv_lmdb.go
@@ -1349,13 +1349,13 @@ func (c *LmdbDupSortCursor) initCursor() error {
 		return nil
 	}
 
-	if c.bucketCfg.AutoDupSortKeysConversion {
-		return fmt.Errorf("class LmdbDupSortCursor not compatible with AutoDupSortKeysConversion buckets")
-	}
+	//if c.bucketCfg.AutoDupSortKeysConversion {
+	//	return fmt.Errorf("class LmdbDupSortCursor not compatible with AutoDupSortKeysConversion buckets")
+	//}
 
-	if c.bucketCfg.Flags&lmdb.DupSort == 0 {
-		return fmt.Errorf("class LmdbDupSortCursor can be used only if bucket created with flag lmdb.DupSort")
-	}
+	//if c.bucketCfg.Flags&lmdb.DupSort == 0 {
+	//	return fmt.Errorf("class LmdbDupSortCursor can be used only if bucket created with flag lmdb.DupSort")
+	//}
 
 	return c.LmdbCursor.initCursor()
 }
