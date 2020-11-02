51c84ab33ae218e75d5a9575feb5ca86127a348e
yperbasis <andrey.ashikhmin@gmail.com>
2020-11-02 18:29:57 +0100

    silkworm ExecuteBlocks

--- turbo/silkworm/silkworm.go
+++ turbo/silkworm/silkworm.go
@@ -4,12 +4,27 @@ package silkworm
 #cgo LDFLAGS: -ldl
 #include <dlfcn.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef int (*SilkwormExecuteBlocksFunctionPointer)(void* txn, uint64_t chain_id, uint64_t start_block,
+                                                    size_t batch_size, bool write_receipts,
+                                                    uint64_t* last_executed_block, int* lmdb_error_code);
+
+int call_silkworm_execute_blocks(void* func_ptr, void* txn, uint64_t chain_id, uint64_t start_block, size_t batch_size,
+                                 bool write_receipts, uint64_t* last_executed_block, int* lmdb_error_code) {
+    return ((SilkwormExecuteBlocksFunctionPointer)func_ptr)(txn, chain_id, start_block, batch_size, write_receipts,
+                                                            last_executed_block, lmdb_error_code);
+}
 */
 import "C"
 
 import (
 	"fmt"
+	"math/big"
 	"unsafe"
+
+	"github.com/ledgerwatch/turbo-geth/ethdb"
 )
 
 const funcName = "silkworm_execute_blocks"
@@ -32,4 +47,20 @@ func LoadExecutionFunctionPointer(dllPath string) (unsafe.Pointer, error) {
 	}
 
 	return funcPtr, nil
+}
+
+func ExecuteBlocks(funcPtr unsafe.Pointer, txn ethdb.Tx, chainID *big.Int, startBlock uint64, batchSize int, writeReceipts bool) (executedBlock uint64, err error) {
+	cChainId := C.uint64_t(chainID.Uint64())
+	cStartBlock := C.uint64_t(startBlock)
+	cBatchSize := C.size_t(batchSize)
+	cWriteReceipts := C._Bool(writeReceipts)
+	cLastExecutedBlock := C.uint64_t(startBlock - 1)
+	cLmdbErrorCode := C.int(0)
+	status := C.call_silkworm_execute_blocks(funcPtr, txn.CHandle(), cChainId, cStartBlock, cBatchSize, cWriteReceipts, &cLastExecutedBlock, &cLmdbErrorCode)
+	executedBlock = uint64(cLastExecutedBlock)
+	if status == 0 || status == 1 {
+		return executedBlock, nil
+	} else {
+		return executedBlock, fmt.Errorf("silkworm_execute_blocks error %d, LMDB error %d", status, cLmdbErrorCode)
+	}
 }
