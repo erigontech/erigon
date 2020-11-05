e4596532d5333c8fd004159b0de5645d00c18c49
Igor Mandrigin <i@mandrigin.ru>
2020-11-05 13:28:50 +0100

    begin 2020.11.2 release cycle

--- params/version.go
+++ params/version.go
@@ -24,7 +24,7 @@ import (
 const (
 	VersionMajor    = 2020    // Major version component of the current release
 	VersionMinor    = 11      // Minor version component of the current release
-	VersionMicro    = 1       // Patch version component of the current release
+	VersionMicro    = 2       // Patch version component of the current release
 	VersionModifier = "alpha" // Patch version component of the current release
 )
 
