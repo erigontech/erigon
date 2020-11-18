9936873d091de2b299f1152d88623f31b8762860
lightclient@protonmail.com <lightclient@protonmail.com>
2020-11-18 15:33:29 -0700

    accept discover,netrestrict,staticpeer arguments for sentry

--- cmd/headers/commands/sentry.go
+++ cmd/headers/commands/sentry.go
@@ -6,8 +6,12 @@ import (
 )
 
 var (
-	natSetting string // NAT setting
-	port       int    // Listening port
+	natSetting  string   // NAT setting
+	port        int      // Listening port
+	staticPeers []string // static peers
+	discovery   bool     // enable sentry's discovery mechanism
+	netRestrict string   // CIDR to restrict peering to
+
 )
 
 func init() {
@@ -15,6 +19,9 @@ func init() {
 	sentryCmd.Flags().IntVar(&port, "port", 30303, "p2p port number")
 	sentryCmd.Flags().StringVar(&sentryAddr, "sentryAddr", "localhost:9091", "sentry address <host>:<port>")
 	sentryCmd.Flags().StringVar(&coreAddr, "coreAddr", "localhost:9092", "core address <host>:<port>")
+	sentryCmd.Flags().StringArrayVar(&staticPeers, "staticpeers", []string{}, "static peer list [enode]")
+	sentryCmd.Flags().BoolVar(&discovery, "discovery", true, "discovery mode")
+	sentryCmd.Flags().StringVar(&netRestrict, "netrestrict", "", "CIDR range to accept peers from <CIDR>")
 	rootCmd.AddCommand(sentryCmd)
 }
 
@@ -22,6 +29,6 @@ var sentryCmd = &cobra.Command{
 	Use:   "sentry",
 	Short: "Run p2p sentry for the downloader",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return download.Sentry(natSetting, port, sentryAddr, coreAddr)
+		return download.Sentry(natSetting, port, sentryAddr, coreAddr, staticPeers, discovery, netRestrict)
 	},
 }
