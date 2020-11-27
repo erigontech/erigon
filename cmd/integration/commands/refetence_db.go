d6ca1b7f834a842672405dfcac73cf9997644e70
alex.sharov <AskAlexSharov@gmail.com>
2020-11-27 10:20:20 +0700

    squash

--- cmd/integration/commands/refetence_db.go
+++ cmd/integration/commands/refetence_db.go
@@ -295,7 +295,7 @@ func toMdbx(ctx context.Context, from, to string) error {
 	_ = os.RemoveAll(to)
 
 	src := ethdb.NewLMDB().Path(from).Flags(func(flags uint) uint {
-		return flags | lmdb.Readonly ^ lmdb.NoReadahead
+		return (flags | lmdb.Readonly) ^ lmdb.NoReadahead
 	}).MustOpen()
 	dst := ethdb.NewMDBX().Path(to).Flags(func(flags uint) uint {
 		return flags | mdbx.WriteMap | mdbx.NoMemInit
