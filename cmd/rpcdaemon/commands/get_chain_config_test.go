2ec43ca67121a986301ff6548a5f752abf7cac9d
alex.sharov <AskAlexSharov@gmail.com>
2020-12-28 16:10:21 +0700

    df

--- cmd/rpcdaemon/commands/get_chain_config_test.go
+++ cmd/rpcdaemon/commands/get_chain_config_test.go
@@ -15,11 +15,19 @@ func TestGetChainConfig(t *testing.T) {
 		t.Fatalf("setting up genensis block: %v", err)
 	}
 
-	config1, err1 := (&APIImpl{}).chainConfig(db)
+	api := (&BaseAPI{})
+	config1, err1 := api.chainConfig(db)
 	if err1 != nil {
 		t.Fatalf("reading chain config: %v", err1)
 	}
 	if config.String() != config1.String() {
 		t.Fatalf("read different config: %s, expected %s", config1.String(), config.String())
+	}
+	config2, err2 := api.chainConfig(db)
+	if err2 != nil {
+		t.Fatalf("reading chain config: %v", err2)
+	}
+	if config.String() != config2.String() {
+		t.Fatalf("read different config: %s, expected %s", config2.String(), config.String())
 	}
 }
