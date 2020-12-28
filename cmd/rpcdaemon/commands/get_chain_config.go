979e3a429c2d7a811dd2a188cfdf98a294b3befc
alex.sharov <AskAlexSharov@gmail.com>
2020-12-28 12:03:38 +0700

    rpcdaemon performance improve - less reading blocks, less allocs on server

--- cmd/rpcdaemon/commands/get_chain_config.go
+++ cmd/rpcdaemon/commands/get_chain_config.go
@@ -3,39 +3,41 @@ package commands
 import (
 	"sync"
 
-	"github.com/ledgerwatch/turbo-geth/common"
 	"github.com/ledgerwatch/turbo-geth/core/rawdb"
 	"github.com/ledgerwatch/turbo-geth/core/types"
 	"github.com/ledgerwatch/turbo-geth/ethdb"
 	"github.com/ledgerwatch/turbo-geth/params"
 )
 
-var chainConfig *params.ChainConfig
-var genesis *types.Block
-var chainConfigLock sync.RWMutex
+var (
+	_chainConfig   *params.ChainConfig
+	_genesis       *types.Block
+	genesisSetOnce sync.Once
+)
 
 func getChainConfig(db ethdb.Database) (*params.ChainConfig, error) {
 	cfg, _, err := getChainConfigWithGenesis(db)
 	return cfg, err
 }
 
-func getChainConfigWithGenesis(db ethdb.Database) (*params.ChainConfig, common.Hash, error) {
-	chainConfigLock.RLock()
-	defer chainConfigLock.RUnlock()
-	if chainConfig == nil {
-		chainConfigLock.Lock()
-		defer chainConfigLock.Unlock()
-		genesisBlock, err := rawdb.ReadBlockByNumber(db, 0)
-		if err != nil {
-			return nil, common.Hash{}, err
-		}
-		genesis = genesisBlock
-		cc, err := rawdb.ReadChainConfig(db, genesis.Hash())
-		if err != nil {
-			return nil, common.Hash{}, err
-		}
-		chainConfig = cc
+func getChainConfigWithGenesis(db ethdb.Database) (*params.ChainConfig, *types.Block, error) {
+	if _chainConfig != nil {
+		return _chainConfig, _genesis, nil
 	}
 
-	return chainConfig, genesis.Hash(), nil
+	genesisBlock, err := rawdb.ReadBlockByNumber(db, 0)
+	if err != nil {
+		return nil, nil, err
+	}
+	cc, err := rawdb.ReadChainConfig(db, genesisBlock.Hash())
+	if err != nil {
+		return nil, nil, err
+	}
+	if cc != nil && genesisBlock != nil {
+		genesisSetOnce.Do(func() {
+			_genesis = genesisBlock
+			_chainConfig = cc
+		})
+	}
+	return _chainConfig, _genesis, nil
 }
