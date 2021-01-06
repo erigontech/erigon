a50f261e8738cfddfa96e1cec078e98520c74170
Péter Szilágyi <peterke@gmail.com>
2021-01-06 08:37:45 +0200

    cmd/utils, eth/downloader: minor snap nitpicks

--- eth/downloader/downloader.go
+++ eth/downloader/downloader.go
@@ -96,7 +96,7 @@ var (
 	errCancelContentProcessing = errors.New("content processing canceled (requested)")
 	errCanceled                = errors.New("syncing canceled (requested)")
 	errNoSyncActive            = errors.New("no sync active")
-	errTooOld                  = errors.New("peer doesn't speak recent enough protocol version (need version >= 64)")
+	errTooOld                  = errors.New("peer's protocol version too old")
 )
 
 type Downloader struct {
@@ -502,7 +502,7 @@ func (d *Downloader) syncWithPeer(p *peerConnection, hash common.Hash, blockNumb
 		}
 	}()
 	if p.version < 64 {
-		return fmt.Errorf("%w, peer version: %d", errTooOld, p.version)
+		return fmt.Errorf("%w: advertized %d < required %d", errTooOld, p.version, 64)
 	}
 	mode := d.getMode()
 
