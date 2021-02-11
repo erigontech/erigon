1aa8e020e8c52705f90c5e8e122aebffd247fe9c
alex.sharov <AskAlexSharov@gmail.com>
2021-02-11 12:59:08 +0700

    increase mdbx OptTxnDpLimit

--- ethdb/kv_mdbx.go
+++ ethdb/kv_mdbx.go
@@ -111,6 +111,11 @@ func (opts MdbxOpts) Open() (KV, error) {
 		return nil, err
 	}
 
+	err = env.SetOption(mdbx.OptTxnDpLimit, 128*1024) // default: 64*1024
+	if err != nil {
+		return nil, err
+	}
+
 	//_ = env.SetDebug(mdbx.LogLvlExtra, mdbx.DbgAssert, mdbx.LoggerDoNotChange) // temporary disable error, because it works if call it 1 time, but returns error if call it twice in same process (what often happening in tests)
 
 	err = env.SetMaxDBs(100)
