9e114bb0395b75245ecc4121f5e64f6f77edb680
alex.sharov <AskAlexSharov@gmail.com>
2021-02-14 10:31:15 +0700

    quit exec unwind

--- eth/integrity/trie.go
+++ eth/integrity/trie.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/ledgerwatch/turbo-geth/common"
 	"github.com/ledgerwatch/turbo-geth/common/dbutils"
+	"github.com/ledgerwatch/turbo-geth/common/hexutil"
 	"github.com/ledgerwatch/turbo-geth/ethdb"
 	"github.com/ledgerwatch/turbo-geth/log"
 	"github.com/ledgerwatch/turbo-geth/turbo/trie"
@@ -25,11 +26,14 @@ func Trie(tx ethdb.Tx, quit <-chan struct{}) {
 	logEvery := time.NewTicker(10 * time.Second)
 	defer logEvery.Stop()
 	seek := make([]byte, 256)
+	buf := make([]byte, 256)
+	buf2 := make([]byte, 256)
 
 	{
-		c, c2 := tx.Cursor(dbutils.TrieOfAccountsBucket), tx.Cursor(dbutils.TrieOfAccountsBucket)
+		c, trieAcc2, accC := tx.Cursor(dbutils.TrieOfAccountsBucket), tx.Cursor(dbutils.TrieOfAccountsBucket), tx.Cursor(dbutils.HashedAccountsBucket)
 		defer c.Close()
-		defer c2.Close()
+		defer trieAcc2.Close()
+		defer accC.Close()
 		for k, v, err := c.First(); k != nil; k, v, err = c.Next() {
 			if err != nil {
 				panic(err)
@@ -46,7 +50,7 @@ func Trie(tx ethdb.Tx, quit <-chan struct{}) {
 			// must have parent with right hasBranch bit
 			for i := len(k) - 1; i > 0 && !found; i-- {
 				parentK = k[:i]
-				kParent, vParent, err := c2.SeekExact(parentK)
+				kParent, vParent, err := trieAcc2.SeekExact(parentK)
 				if err != nil {
 					panic(err)
 				}
@@ -72,7 +76,7 @@ func Trie(tx ethdb.Tx, quit <-chan struct{}) {
 					continue
 				}
 				seek[len(seek)-1] = uint8(i)
-				k2, _, err := c2.Seek(seek)
+				k2, _, err := trieAcc2.Seek(seek)
 				if err != nil {
 					panic(err)
 				}
@@ -83,6 +87,34 @@ func Trie(tx ethdb.Tx, quit <-chan struct{}) {
 					panic(fmt.Errorf("key %x has branches %016b, but there is no child %d in db; last seen key: %x->%x", k, hasBranch, i, seek, k2))
 				}
 			}
+
+			// each IH must cover some state
+			buf = buf[:len(k)+1]
+			copy(buf, k)
+			for i := uint16(0); i < 16; i++ {
+				if 1<<i&hasState == 0 {
+					continue
+				}
+
+				found := false
+				buf = buf[:len(k)+1]
+				buf[len(buf)-1] = uint8(i)
+				bitsToMatch := len(buf) * 4
+				if len(buf)%2 == 1 {
+					buf = append(buf, 0)
+				}
+				hexutil.CompressNibbles(buf, &seek)
+				if err = ethdb.Walk(accC, seek, bitsToMatch, func(k, v []byte) (bool, error) {
+					found = true
+					return false, nil
+				}); err != nil {
+					panic(err)
+				}
+				if !found {
+					panic(fmt.Errorf("key %x has state %016b, but there is no child %d,%x in state", k, hasBranch, i, seek))
+				}
+			}
+
 			select {
 			default:
 			case <-quit:
@@ -93,9 +125,10 @@ func Trie(tx ethdb.Tx, quit <-chan struct{}) {
 		}
 	}
 	{
-		c, c2 := tx.Cursor(dbutils.TrieOfStorageBucket), tx.Cursor(dbutils.TrieOfStorageBucket)
+		c, trieStorage, storageC := tx.Cursor(dbutils.TrieOfStorageBucket), tx.Cursor(dbutils.TrieOfStorageBucket), tx.Cursor(dbutils.HashedStorageBucket)
 		defer c.Close()
-		defer c2.Close()
+		defer trieStorage.Close()
+		defer storageC.Close()
 		for k, v, err := c.First(); k != nil; k, v, err = c.Next() {
 			if err != nil {
 				panic(err)
@@ -114,7 +147,7 @@ func Trie(tx ethdb.Tx, quit <-chan struct{}) {
 			// must have parent with right hasBranch bit
 			for i := len(k) - 1; i >= 40 && !found; i-- {
 				parentK = k[:i]
-				kParent, vParent, err := c2.SeekExact(parentK)
+				kParent, vParent, err := trieStorage.SeekExact(parentK)
 				if err != nil {
 					panic(err)
 				}
@@ -141,12 +174,41 @@ func Trie(tx ethdb.Tx, quit <-chan struct{}) {
 					continue
 				}
 				seek[len(seek)-1] = uint8(i)
-				k2, _, err := c2.Seek(seek)
+				k2, _, err := trieStorage.Seek(seek)
 				if err != nil {
 					panic(err)
 				}
 				if !bytes.HasPrefix(k2, seek) {
 					panic(fmt.Errorf("key %x has branches %016b, but there is no child %d in db", k, hasBranch, i))
+				}
+			}
+
+			// each IH must cover some state
+			buf = buf[:len(k)-40+1]
+			copy(buf, k[40:])
+			for i := uint16(0); i < 16; i++ {
+				if 1<<i&hasState == 0 {
+					continue
+				}
+				found := false
+				buf = buf[:len(k)-40+1]
+				buf[len(buf)-1] = uint8(i)
+				bitsToMatch := 40*8 + len(buf)*4
+				if len(buf)%2 == 1 {
+					buf = append(buf, 0)
+				}
+				hexutil.CompressNibbles(buf, &buf2)
+				seek = seek[:40+len(buf2)]
+				copy(seek, k[:40])
+				copy(seek[40:], buf2)
+				if err = ethdb.Walk(storageC, seek, bitsToMatch, func(k, v []byte) (bool, error) {
+					found = true
+					return false, nil
+				}); err != nil {
+					panic(err)
+				}
+				if !found {
+					panic(fmt.Errorf("key %x has state %016b, but there is no child %d,%x in state", k, hasBranch, i, seek))
 				}
 			}
 
