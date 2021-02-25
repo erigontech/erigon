db1ff55a56d8c481d96c596b097b0276c4b372d9
alex.sharov <AskAlexSharov@gmail.com>
2021-02-25 13:59:56 +0700

    clean

--- turbo/trie/trie_root.go
+++ turbo/trie/trie_root.go
@@ -816,14 +816,14 @@ func (c *AccTrieCursor) _seek(seek []byte, withinPrefix []byte) (bool, error) {
 		// - no child found, means: len(k) <= c.lvl
 		// - looking for first child, means: c.childID[c.lvl] <= int16(bits.TrailingZeros16(c.hasTree[c.lvl]))
 		// otherwise do .Seek call
-		//k, v, err = c.c.Next()
-		//if err != nil {
-		//	return false, err
-		//}
-		//if len(k) > c.lvl && c.childID[c.lvl] > int8(bits.TrailingZeros16(c.hasTree[c.lvl])) {
-		c.is++
-		k, v, err = c.c.Seek(seek)
-		//}
+		k, v, err = c.c.Next()
+		if err != nil {
+			return false, err
+		}
+		if len(k) > c.lvl && c.childID[c.lvl] > int8(bits.TrailingZeros16(c.hasTree[c.lvl])) {
+			c.is++
+			k, v, err = c.c.Seek(seek)
+		}
 	}
 	if err != nil {
 		return false, err
