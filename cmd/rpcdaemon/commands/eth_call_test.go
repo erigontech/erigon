c00f85ab6c39096fd1896e6b897045a2382645e4
ledgerwatch <akhounov@gmail.com>
2021-03-02 22:47:44 +0000

    Simplification of eth_estimageGas (#1531)
    * Simplification of eth_estimageGas
    
    * Set GasCap by default
    
    * Add a test
    
    * Fix crash
    
    Co-authored-by: Alexey Sharp <alexeysharp@Alexeys-iMac.local>

---
+++ cmd/rpcdaemon/commands/eth_call_test.go
@@ -0,0 +1,25 @@
+package commands
+
+import (
+	"context"
+	"testing"
+
+	"github.com/ledgerwatch/turbo-geth/common"
+	"github.com/ledgerwatch/turbo-geth/internal/ethapi"
+)
+
+func TestEstimateGas(t *testing.T) {
+	db, err := createTestDb()
+	if err != nil {
+		t.Fatalf("create test db: %v", err)
+	}
+	api := NewEthAPI(db, nil, 5000000, nil)
+	var from = common.HexToAddress("0x71562b71999873db5b286df957af199ec94617f7")
+	var to = common.HexToAddress("0x0d3ab14bbad3d99f4203bd7a11acb94882050e7e")
+	if _, err := api.EstimateGas(context.Background(), ethapi.CallArgs{
+		From: &from,
+		To:   &to,
+	}); err != nil {
+		t.Errorf("calling EstimateGas: %v", err)
+	}
+}
