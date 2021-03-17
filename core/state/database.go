b92995fafafcbc990907ed4205aab91210a0667f
Igor Mandrigin <i@mandrigin.ru>
2021-03-17 23:04:44 +0100

    fix one issue in TestEIP2718Transition

--- core/state/database.go
+++ core/state/database.go
@@ -1012,9 +1012,18 @@ func (tds *TrieDbState) ReadAccountData(address common.Address) (*accounts.Accou
 	}
 	var account *accounts.Account
 	account, err = tds.readAccountDataByHash(addrHash)
-	if err != nil {
+	if err != nil && err != ethdb.ErrKeyNotFound {
 		return nil, err
 	}
+	var a accounts.Account
+	if ok, err := rawdb.PlainReadAccount(tds.db, address, &a); err != nil {
+		return nil, err
+	} else if !ok {
+		return nil, nil
+	}
+
+	account = &a
+
 	if tds.resolveReads {
 		tds.currentBuffer.accountReads[addrHash] = struct{}{}
 		if account != nil {
