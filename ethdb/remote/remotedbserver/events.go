a583f7f6ca8c62aa3720e5b46a7463d94962d4b8
ledgerwatch <akhounov@gmail.com>
2021-03-19 17:36:38 +0000

    Try to fix multiple head subscriptions (#1565)
    Co-authored-by: Alexey Sharp <alexeysharp@Alexeys-iMac.local>

--- ethdb/remote/remotedbserver/events.go
+++ ethdb/remote/remotedbserver/events.go
@@ -30,7 +30,7 @@ func (e *Events) OnNewHeader(newHeader *types.Header) {
 			// remove subscription
 			if i == len(e.headerSubscriptions)-1 {
 				e.headerSubscriptions = e.headerSubscriptions[:i]
-			} else {
+			} else if i < len(e.headerSubscriptions)-1 {
 				e.headerSubscriptions = append(e.headerSubscriptions[:i], e.headerSubscriptions[i+1:]...)
 			}
 		}
