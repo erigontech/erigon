dbd2a666c242a35f33f42e9291fa42ec1a6cbbac
Alexey Sharp <alexeysharp@Alexeys-iMac.local>
2021-03-21 17:49:22 +0000

    Revert "Fix test"
    This reverts commit d154e07b5f97f0b54485b50d572fb27c6c61af4b.

--- p2p/message_test.go
+++ p2p/message_test.go
@@ -86,12 +86,11 @@ func TestMsgPipeConcurrentClose(t *testing.T) {
 }
 
 func TestEOFSignal(t *testing.T) {
-	t.Skip("does not apply anymore")
 	rb := make([]byte, 10)
 
 	// empty reader
 	eof := make(chan struct{}, 1)
-	sig := &eofSignal{new(bytes.Buffer), 0}
+	sig := &eofSignal{new(bytes.Buffer), 0, eof}
 	if n, err := sig.Read(rb); n != 0 || err != io.EOF {
 		t.Errorf("Read returned unexpected values: (%v, %v)", n, err)
 	}
@@ -103,7 +102,7 @@ func TestEOFSignal(t *testing.T) {
 
 	// count before error
 	eof = make(chan struct{}, 1)
-	sig = &eofSignal{bytes.NewBufferString("aaaaaaaa"), 4}
+	sig = &eofSignal{bytes.NewBufferString("aaaaaaaa"), 4, eof}
 	if n, err := sig.Read(rb); n != 4 || err != nil {
 		t.Errorf("Read returned unexpected values: (%v, %v)", n, err)
 	}
@@ -115,7 +114,7 @@ func TestEOFSignal(t *testing.T) {
 
 	// error before count
 	eof = make(chan struct{}, 1)
-	sig = &eofSignal{bytes.NewBufferString("aaaa"), 999}
+	sig = &eofSignal{bytes.NewBufferString("aaaa"), 999, eof}
 	if n, err := sig.Read(rb); n != 4 || err != nil {
 		t.Errorf("Read returned unexpected values: (%v, %v)", n, err)
 	}
@@ -130,7 +129,7 @@ func TestEOFSignal(t *testing.T) {
 
 	// no signal if neither occurs
 	eof = make(chan struct{}, 1)
-	sig = &eofSignal{bytes.NewBufferString("aaaaaaaaaaaaaaaaaaaaa"), 999}
+	sig = &eofSignal{bytes.NewBufferString("aaaaaaaaaaaaaaaaaaaaa"), 999, eof}
 	if n, err := sig.Read(rb); n != 10 || err != nil {
 		t.Errorf("Read returned unexpected values: (%v, %v)", n, err)
 	}
