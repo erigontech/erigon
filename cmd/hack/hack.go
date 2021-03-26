33382d798262ca97daf3e8ff779ad37aa3d442cf
alex.sharov <AskAlexSharov@gmail.com>
2021-03-26 12:43:43 +0700

    tests

--- cmd/hack/hack.go
+++ cmd/hack/hack.go
@@ -140,7 +140,7 @@ func mychart() {
 			Show:        true,
 			StrokeColor: chart.ColorBlack,
 		},
-		YAxis:   chart.YAxisSecondary,core/rlp_test.go:87
+		YAxis:   chart.YAxisSecondary,
 		XValues: blocks,
 		YValues: dbsize,
 	}
