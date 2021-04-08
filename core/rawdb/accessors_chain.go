bf02e8053131f05c6cd12d7e2dcedf4a6793a26c
alex.sharov <AskAlexSharov@gmail.com>
2021-04-08 15:46:36 +0700

    handle case when no senders in db yet

--- core/rawdb/accessors_chain.go
+++ core/rawdb/accessors_chain.go
@@ -365,6 +365,9 @@ func ReadSenders(db ethdb.KVGetter, hash common.Hash, number uint64) ([]common.A
 	if err != nil {
 		return nil, fmt.Errorf("readSenders failed: %w", err)
 	}
+	if data == nil {
+		return nil, nil
+	}
 	senders := make([]common.Address, len(data)/common.AddressLength)
 	for i := 0; i < len(senders); i++ {
 		copy(senders[i][:], data[i*common.AddressLength:])
@@ -525,6 +528,9 @@ func ReadReceipts(db ethdb.Getter, hash common.Hash, number uint64) types.Receip
 	senders, err := ReadSenders(db, hash, number)
 	if err != nil {
 		log.Error("Failed to read Senders", "hash", hash, "number", number, "err", err)
+		return nil
+	}
+	if senders == nil {
 		return nil
 	}
 	if err = receipts.DeriveFields(hash, number, body.Transactions, senders); err != nil {
@@ -691,14 +697,17 @@ func ReadBlockWithoutTransactions(db ethdb.Getter, hash common.Hash, number uint
 	return types.NewBlockWithHeader(header).WithBody(body.Transactions, body.Uncles)
 }
 
-func ReadBlockWithSenders(db ethdb.Getter, hash common.Hash, number uint64) (*types.Block, error) {
+func ReadBlockWithSenders(db ethdb.Getter, hash common.Hash, number uint64) (*types.Block, []common.Address, error) {
 	block := ReadBlock(db, hash, number)
 	senders, err := ReadSenders(db, hash, number)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
+	}
+	if senders == nil {
+		return nil, nil, nil
 	}
 	block.Body().SendersToTxs(senders)
-	return block, nil
+	return block, senders, nil
 }
 
 // WriteBlock serializes a block into the database, header and body separately.
@@ -892,13 +901,13 @@ func ReadBlockByNumber(db ethdb.Getter, number uint64) (*types.Block, error) {
 	return ReadBlock(db, hash, number), nil
 }
 
-func ReadBlockByNumberWithSenders(db ethdb.Getter, number uint64) (*types.Block, error) {
+func ReadBlockByNumberWithSenders(db ethdb.Getter, number uint64) (*types.Block, []common.Address, error) {
 	hash, err := ReadCanonicalHash(db, number)
 	if err != nil {
-		return nil, fmt.Errorf("failed ReadCanonicalHash: %w", err)
+		return nil, nil, fmt.Errorf("failed ReadCanonicalHash: %w", err)
 	}
 	if hash == (common.Hash{}) {
-		return nil, nil
+		return nil, nil, nil
 	}
 
 	return ReadBlockWithSenders(db, hash, number)
