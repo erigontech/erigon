d6566652dbef9adc9c39421dbf4e3a23de5aab3d
Artem Vorotnikov <artem@vorotnikov.me>
2021-04-12 19:35:31 +0300

    trace_filter

--- cmd/rpcdaemon/commands/trace_filtering.go
+++ cmd/rpcdaemon/commands/trace_filtering.go
@@ -1,7 +1,6 @@
 package commands
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
 	"errors"
@@ -89,26 +88,10 @@ func (api *TraceAPIImpl) Block(ctx context.Context, blockNr rpc.BlockNumber) (Pa
 	if hashErr != nil {
 		return nil, hashErr
 	}
-	block, senders, sendersErr := rawdb.ReadBlockWithSenders(ethdb.NewRoTxDb(dbtx), hash, uint64(bn))
-	if sendersErr != nil {
-		return nil, sendersErr
-	}
-	if block == nil {
-		return nil, nil
-	}
 
-	blockTxs := block.Transactions()
-
-	if len(blockTxs) != len(senders) {
-		return nil, errors.New("block txs len != senders len")
-	}
-
-	txs := make([]TransactionWithSender, 0, len(senders))
-	for n, tx := range blockTxs {
-		txs = append(txs, TransactionWithSender{
-			tx:     tx,
-			sender: senders[n],
-		})
+	txs, txsErr := getBlockTransactions(dbtx, hash, blockNum)
+	if txsErr != nil {
+		return nil, txsErr
 	}
 
 	baseBn := bn
@@ -139,31 +122,14 @@ func (api *TraceAPIImpl) Block(ctx context.Context, blockNr rpc.BlockNumber) (Pa
 }
 
 // Filter implements trace_filter
-// TODO(tjayrush): Eventually, we will need to protect ourselves from 'large' queries. Parity crashes when a range query of a very large size
-// is sent. We need to protect ourselves with maxTraces. It may already be done
+// NOTE: We do not store full traces - we just store index for each address
+// Pull blocks which have txs with matching address
 func (api *TraceAPIImpl) Filter(ctx context.Context, req TraceFilterRequest) (ParityTraces, error) {
-	tx, err1 := api.kv.BeginRo(ctx)
+	dbtx, err1 := api.kv.BeginRo(ctx)
 	if err1 != nil {
 		return nil, fmt.Errorf("traceFilter cannot open tx: %v", err1)
 	}
-	defer tx.Rollback()
-
-	var filteredHashes []common.Hash
-	// TODO(tjayrush): Parity intersperses block/uncle reward traces with transaction call traces. We need to be able to tell the
-	// difference. I do that with this boolean array. This will be re-written shortly. For now, we use this simple boolean flag.
-	// Eventually, we will fill the ParityTrace array directly as we go (and we can probably even do better than that)
-	var traceTypes []bool
-	var maxTracesCount uint64
-	var offset uint64
-	var skipped uint64
-
-	sort.Slice(req.FromAddress, func(i int, j int) bool {
-		return bytes.Compare(req.FromAddress[i].Bytes(), req.FromAddress[j].Bytes()) == -1
-	})
-
-	sort.Slice(req.ToAddress, func(i int, j int) bool {
-		return bytes.Compare(req.ToAddress[i].Bytes(), req.ToAddress[j].Bytes()) == -1
-	})
+	defer dbtx.Rollback()
 
 	var fromBlock uint64
 	var toBlock uint64
@@ -174,194 +140,177 @@ func (api *TraceAPIImpl) Filter(ctx context.Context, req TraceFilterRequest) (Pa
 	}
 
 	if req.ToBlock == nil {
-		headNumber := rawdb.ReadHeaderNumber(tx, rawdb.ReadHeadHeaderHash(tx))
+		headNumber := rawdb.ReadHeaderNumber(dbtx, rawdb.ReadHeadHeaderHash(dbtx))
 		toBlock = *headNumber
 	} else {
 		toBlock = uint64(*req.ToBlock)
 	}
 
 	if fromBlock > toBlock {
-		// TODO(tjayrush): Parity reports no error in this case it simply returns an empty response
-		return nil, nil //fmt.Errorf("invalid parameters: toBlock must be greater than fromBlock")
+		return nil, fmt.Errorf("invalid parameters: fromBlock cannot be greater than toBlock")
 	}
 
-	if req.Count == nil {
-		maxTracesCount = api.maxTraces
-	} else {
-		maxTracesCount = *req.Count
-	}
+	fromAddresses := make(map[common.Address]struct{}, len(req.FromAddress))
+	toAddresses := make(map[common.Address]struct{}, len(req.ToAddress))
 
-	if req.After == nil {
-		offset = 0
-	} else {
-		offset = *req.After
-	}
-
-	if req.FromAddress != nil || req.ToAddress != nil { // use address history index to retrieve matching transactions
-		var historyFilter []*common.Address
-		isFromAddress := req.FromAddress != nil
-		if isFromAddress {
-			historyFilter = req.FromAddress
-		} else {
-			historyFilter = req.ToAddress
+	blocksMap := map[uint32]struct{}{}
+	var loadAddresses = func(addr common.Address) error {
+		// Load bitmap for address from trace index
+		b, err := bitmapdb.Get(dbtx, dbutils.CallFromIndex, addr.Bytes(), uint32(fromBlock), uint32(toBlock))
+		if err != nil {
+			return err
 		}
 
-		for _, addr := range historyFilter {
-
-			addrBytes := addr.Bytes()
-			blockNumbers, errHistory := retrieveHistory(tx, addr, fromBlock, toBlock)
-			if errHistory != nil {
-				return nil, errHistory
-			}
-
-			for _, num := range blockNumbers {
-				block, senders, err := rawdb.ReadBlockByNumberWithSenders(ethdb.NewRoTxDb(tx), num)
-				if err != nil {
-					return nil, err
-				}
-				for i, txn := range block.Transactions() {
-					if uint64(len(filteredHashes)) == maxTracesCount {
-						if uint64(len(filteredHashes)) == api.maxTraces {
-							return nil, fmt.Errorf("too many traces found")
-						}
-						return nil, nil
-					}
-
-					var to *common.Address
-					if txn.GetTo() == nil {
-						to = &common.Address{}
-					} else {
-						to = txn.GetTo()
-					}
-
-					if isFromAddress {
-						if !isAddressInFilter(to, req.ToAddress) {
-							continue
-						}
-						if bytes.Equal(senders[i].Bytes(), addrBytes) {
-							filteredHashes = append(filteredHashes, txn.Hash())
-							traceTypes = append(traceTypes, false)
-						}
-					} else if bytes.Equal(to.Bytes(), addrBytes) {
-						if skipped < offset {
-							skipped++
-							continue
-						}
-						filteredHashes = append(filteredHashes, txn.Hash())
-						traceTypes = append(traceTypes, false)
-					}
-				}
-				// TODO(tjayrush): Parity does not (for some unknown reason) include blockReward traces here
+		// Extract block numbers from bitmap
+		for _, block := range b.ToArray() {
+			// Observe the limits
+			if uint64(block) >= fromBlock && uint64(block) <= toBlock {
+				blocksMap[block] = struct{}{}
 			}
 		}
-	} else if req.FromBlock != nil || req.ToBlock != nil { // iterate over blocks
 
-		for blockNum := fromBlock; blockNum < toBlock+1; blockNum++ {
-			block, err := rawdb.ReadBlockByNumber(ethdb.NewRoTxDb(tx), blockNum)
-			if err != nil {
-				return nil, err
-			}
-			for _, txn := range block.Transactions() {
-				if uint64(len(filteredHashes)) == maxTracesCount {
-					if uint64(len(filteredHashes)) == api.maxTraces {
-						return nil, fmt.Errorf("too many traces found")
-					}
-					return nil, nil
-				}
-				if skipped < offset {
-					skipped++
-					continue
-				}
-				filteredHashes = append(filteredHashes, txn.Hash())
-				traceTypes = append(traceTypes, false)
-			}
-			// TODO(tjayrush): Need much, much better testing surrounding offset and count especially when including block rewards
-			if skipped < offset {
-				skipped++
-				continue
-			}
-			// We need to intersperse block reward traces to match Parity
-			filteredHashes = append(filteredHashes, block.Hash())
-			traceTypes = append(traceTypes, true)
-		}
-	} else {
-		return nil, fmt.Errorf("invalid parameters")
+		return nil
 	}
 
-	getter := adapter.NewBlockGetter(tx)
-	engine := ethash.NewFaker()
-	chainConfig, err := api.chainConfig(tx)
-	if err != nil {
-		return nil, err
-	}
-	traceType := "callTracer" // nolint: goconst
-	traces := ParityTraces{}
-	getHeader := func(hash common.Hash, number uint64) *types.Header {
-		return rawdb.ReadHeader(tx, hash, number)
-	}
-	for i, txOrBlockHash := range filteredHashes {
-		if traceTypes[i] {
-			// In this case, we're processing a block (or uncle) reward trace. The hash is a block hash
-			// Because Geth does not return blockReward or uncleReward traces, we must create them here
-			block, err := rawdb.ReadBlockByHash(ethdb.NewRoTxDb(tx), txOrBlockHash)
-			if err != nil {
+	for _, addr := range req.FromAddress {
+		if addr != nil {
+			if err := loadAddresses(*addr); err != nil {
 				return nil, err
 			}
-			minerReward, uncleRewards := ethash.AccumulateRewards(chainConfig, block.Header(), block.Uncles())
-			fmt.Printf("%v\n", minerReward)
-			var tr ParityTrace
-			//tr.Action.Author = strings.ToLower(block.Coinbase().String())
-			//tr.Action.RewardType = "block" // goconst
-			//tr.Action.Value = minerReward.String()
-			tr.BlockHash = &common.Hash{}
-			copy(tr.BlockHash[:], block.Hash().Bytes())
-			tr.BlockNumber = new(uint64)
-			*tr.BlockNumber = block.NumberU64()
-			tr.Type = "reward" // nolint: goconst
-			traces = append(traces, tr)
-			for i, uncle := range block.Uncles() {
-				fmt.Printf("%v\n", uncle)
-				if i < len(uncleRewards) {
-					var tr ParityTrace
-					//tr.Action.Author = strings.ToLower(uncle.Coinbase.String())
-					//tr.Action.RewardType = "uncle" // goconst
-					//tr.Action.Value = uncleRewards[i].String()
-					tr.BlockHash = &common.Hash{}
-					copy(tr.BlockHash[:], block.Hash().Bytes())
-					tr.BlockNumber = new(uint64)
-					*tr.BlockNumber = block.NumberU64()
-					tr.Type = "reward" // nolint: goconst
-					traces = append(traces, tr)
-				}
-			}
-		} else {
-			// In this case, we're processing a transaction hash
-			txn, blockHash, blockNumber, txIndex := rawdb.ReadTransaction(ethdb.NewRoTxDb(tx), txOrBlockHash)
-			msg, blockCtx, txCtx, ibs, _, err := transactions.ComputeTxEnv(ctx, getter, chainConfig, getHeader, engine, tx, blockHash, txIndex)
-			if err != nil {
-				return nil, err
-			}
-			trace, err := transactions.TraceTx(ctx, msg, blockCtx, txCtx, ibs, &tracers.TraceConfig{Tracer: &traceType}, chainConfig)
-			if err != nil {
-				return nil, err
-			}
-			traceJSON, ok := trace.(json.RawMessage)
-			if !ok {
-				return nil, fmt.Errorf("unknown type in trace_filter")
-			}
-			var gethTrace GethTrace
-			jsonStr, _ := traceJSON.MarshalJSON()
-			json.Unmarshal(jsonStr, &gethTrace) // nolint:errcheck
-			converted := api.convertToParityTrace(gethTrace, blockHash, blockNumber, txn, txIndex, []int{})
-			traces = append(traces, converted...)
+
+			fromAddresses[*addr] = struct{}{}
 		}
 	}
+
+	for _, addr := range req.ToAddress {
+		if addr != nil {
+			if err := loadAddresses(*addr); err != nil {
+				return nil, err
+			}
+
+			toAddresses[*addr] = struct{}{}
+		}
+	}
+
+	// Sort blocks
+	blockSet := make([]int, 0, len(blocksMap))
+	for blk := range blocksMap {
+		blockSet = append(blockSet, int(blk))
+	}
+	sort.Ints(blockSet)
+
+	type blk struct {
+		blockNr   int
+		blockHash common.Hash
+		txs       []TransactionWithSender
+	}
+
+	blocks := make([]blk, 0, len(blocksMap))
+	for _, b := range blockSet {
+		// Extract transactions from block
+		hash, hashErr := rawdb.ReadCanonicalHash(dbtx, uint64(b))
+		if hashErr != nil {
+			return nil, hashErr
+		}
+
+		t, tErr := getBlockTransactions(dbtx, hash, uint64(b))
+		if tErr != nil {
+			return nil, tErr
+		}
+
+		blocks = append(blocks, blk{
+			blockNr:   b,
+			blockHash: hash,
+			txs:       t,
+		})
+	}
+
+	traces := []ParityTrace{}
+
+	// Execute all transactions in picked blocks
+	for _, s := range blocks {
+		t, tErr := api.callManyTransactions(ctx, dbtx, s.txs, s.blockHash, rpc.BlockNumber(s.blockNr))
+		if tErr != nil {
+			return nil, tErr
+		}
+
+		for _, trace := range t {
+			// Check if transaction concerns any of the addresses we wanted
+			if filter_trace(trace, fromAddresses, toAddresses) {
+				for _, pt := range trace.Trace {
+					traces = append(traces, *pt)
+				}
+			}
+		}
+	}
+
 	return traces, nil
+}
+
+func filter_trace(trace *TraceCallResult, fromAddresses map[common.Address]struct{}, toAddresses map[common.Address]struct{}) bool {
+	for _, pt := range trace.Trace {
+		switch action := pt.Action.(type) {
+		case CallTraceAction:
+			_, f := fromAddresses[action.From]
+			_, t := toAddresses[action.To]
+			if f || t {
+				return true
+			}
+		case CreateTraceAction:
+			_, f := fromAddresses[action.From]
+			if f {
+				return true
+			}
+
+			if res, ok := pt.Result.(CreateTraceResult); ok {
+				if res.Address != nil {
+					if _, t := fromAddresses[*res.Address]; t {
+						return true
+					}
+				}
+			}
+		case SuicideTraceAction:
+			_, f := fromAddresses[action.RefundAddress]
+			_, t := toAddresses[action.Address]
+			if f || t {
+				return true
+			}
+		}
+
+	}
+
+	return false
 }
 
 type TransactionWithSender struct {
 	tx     types.Transaction
 	sender common.Address
+}
+
+func getBlockTransactions(dbtx ethdb.Tx, blockHash common.Hash, blockNo uint64) ([]TransactionWithSender, error) {
+	block, senders, sendersErr := rawdb.ReadBlockWithSenders(ethdb.NewRoTxDb(dbtx), blockHash, blockNo)
+	if sendersErr != nil {
+		return nil, sendersErr
+	}
+	if block == nil {
+		return nil, nil
+	}
+
+	blockTxs := block.Transactions()
+
+	if len(blockTxs) != len(senders) {
+		return nil, errors.New("block txs len != senders len")
+	}
+
+	txs := make([]TransactionWithSender, 0, len(senders))
+	for n, tx := range blockTxs {
+		txs = append(txs, TransactionWithSender{
+			tx:     tx,
+			sender: senders[n],
+		})
+	}
+
+	return txs, nil
 }
 
 func (api *TraceAPIImpl) callManyTransactions(ctx context.Context, dbtx ethdb.Tx, txs []TransactionWithSender, blockHash common.Hash, blockNo rpc.BlockNumber) ([]*TraceCallResult, error) {
@@ -398,34 +347,6 @@ func (api *TraceAPIImpl) callManyTransactions(ctx context.Context, dbtx ethdb.Tx
 	}
 
 	return traces, nil
-}
-
-func retrieveHistory(tx ethdb.Tx, addr *common.Address, fromBlock uint64, toBlock uint64) ([]uint64, error) {
-	blocks, err := bitmapdb.Get(tx, dbutils.AccountsHistoryBucket, addr.Bytes(), uint32(fromBlock), uint32(toBlock+1))
-	if err != nil {
-		return nil, err
-	}
-	blocks.RemoveRange(fromBlock, toBlock+1)
-	return toU64(blocks.ToArray()), nil
-}
-
-func toU64(in []uint32) []uint64 {
-	out := make([]uint64, len(in))
-	for i := range in {
-		out[i] = uint64(in[i])
-	}
-	return out
-}
-
-func isAddressInFilter(addr *common.Address, filter []*common.Address) bool {
-	if filter == nil {
-		return true
-	}
-	i := sort.Search(len(filter), func(i int) bool {
-		return bytes.Equal(filter[i].Bytes(), addr.Bytes())
-	})
-
-	return i != len(filter)
 }
 
 // getTransactionTraces - returns the traces for a single transaction. Used by trace_get and trace_transaction.
