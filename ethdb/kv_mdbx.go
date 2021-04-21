70e2c389ce0af8b58400cdbab5023ab7a62116d3
alex.sharov <AskAlexSharov@gmail.com>
2021-04-21 15:49:01 +0700

    slow tests

--- ethdb/kv_mdbx.go
+++ ethdb/kv_mdbx.go
@@ -117,12 +117,14 @@ func (opts MdbxOpts) Open() (RwKV, error) {
 
 	var flags = opts.flags
 	if opts.inMem {
+		flags ^= mdbx.Durable
+		flags |= mdbx.NoMetaSync | mdbx.UtterlyNoSync // it's ok for tests
 		opts.dirtyListMaxPages = 8 * 1024
 	}
 
 	if opts.flags&mdbx.Accede == 0 {
 		if opts.inMem {
-			if err = env.SetGeometry(-1, -1, int(64*datasize.MB), int(2*datasize.MB), 0, 4*1024); err != nil {
+			if err = env.SetGeometry(int(1*datasize.MB), int(1*datasize.MB), int(64*datasize.MB), int(1*datasize.MB), 0, 4*1024); err != nil {
 				return nil, err
 			}
 		} else {
