10359b1add0e88934157006015f4fe166dd6bfe4
Alex Sharp <alexsharp@Alexs-MacBook-Pro.local>
2021-04-23 19:11:22 +0100

    Fix comment

--- cmd/rpcdaemon/commands/eth_api_test.go
+++ cmd/rpcdaemon/commands/eth_api_test.go
@@ -27,7 +27,7 @@ func TestGetTransactionReceiptUnproteced(t *testing.T) {
 	}
 	defer db.Close()
 	api := NewEthAPI(db, nil, 5000000, nil, nil)
-	// Call GetTransactionReceipt for transaction which is not in the database
+	// Call GetTransactionReceipt for un-protected transaction
 	if _, err := api.GetTransactionReceipt(context.Background(), common.HexToHash("0x3f3cb8a0e13ed2481f97f53f7095b9cbc78b6ffb779f2d3e565146371a8830ea")); err != nil {
 		t.Errorf("calling GetTransactionReceipt for unprotected tx: %v", err)
 	}
