5b4d577f28e4bf3050b4f52ecf698257a0a79b10
Evgeny Danienko <6655321@bk.ru>
2021-04-23 18:22:01 +0300

    remove debug

--- consensus/clique/verifier.go
+++ consensus/clique/verifier.go
@@ -305,6 +305,7 @@ func (c *Clique) snapshotByHeader(num uint64, hash common.Hash) *Snapshot {
 			return s
 		}
 	}
+	fmt.Printf("snapshotByHeader %d (epoch %d, checkpoint interval %d) %x returns nil\n", num, c.config.Epoch, c.snapshotConfig.CheckpointInterval, hash)
 
 	return nil
 }
