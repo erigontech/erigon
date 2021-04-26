01ae9624599b2a2d3d988f18248e680879b69ee0
Alex Sharov <AskAlexSharov@gmail.com>
2021-04-26 19:51:01 +0700

    Clean tmp on start (#1808)
    * fininish to remove chain context
    
    * clean
    
    * move migrations folder from
    <datadir>/etl-tmp/migrations
    to
    <datadir>/migrations

--- node/node.go
+++ node/node.go
@@ -537,8 +537,8 @@ func (n *Node) WSEndpoint() string {
 // OpenDatabase opens an existing database with the given name (or creates one if no
 // previous can be found) from within the node's instance directory. If the node is
 // ephemeral, a memory database is returned.
-func (n *Node) OpenDatabase(name string, tmpdir string) (*ethdb.ObjectDatabase, error) {
-	return n.OpenDatabaseWithFreezer(name, tmpdir)
+func (n *Node) OpenDatabase(name string, datadir string) (*ethdb.ObjectDatabase, error) {
+	return n.OpenDatabaseWithFreezer(name, datadir)
 }
 
 // OpenDatabaseWithFreezer opens an existing database with the given name (or
@@ -547,7 +547,7 @@ func (n *Node) OpenDatabase(name string, tmpdir string) (*ethdb.ObjectDatabase, 
 // database to immutable append-only files. If the node is an ephemeral one, a
 // memory database is returned.
 // NOTE: kept for compatibility and for easier rebases (turbo-geth)
-func (n *Node) OpenDatabaseWithFreezer(name string, tmpdir string) (*ethdb.ObjectDatabase, error) {
+func (n *Node) OpenDatabaseWithFreezer(name string, datadir string) (*ethdb.ObjectDatabase, error) {
 	n.lock.Lock()
 	defer n.lock.Unlock()
 
@@ -610,7 +610,7 @@ func (n *Node) OpenDatabaseWithFreezer(name string, tmpdir string) (*ethdb.Objec
 			if err != nil {
 				return nil, err
 			}
-			if err = migrator.Apply(db, tmpdir); err != nil {
+			if err = migrator.Apply(db, datadir); err != nil {
 				return nil, err
 			}
 			db.Close()
