160376df3f16ab27ace76490f49c60f80ece9197
alex.sharov <AskAlexSharov@gmail.com>
2021-04-30 13:51:06 +0700

    don't use eth type

--- cmd/headers/download/broadcast.go
+++ cmd/headers/download/broadcast.go
@@ -14,6 +14,7 @@ import (
 )
 
 // Methods of sentry called by Core
+
 func (cs *ControlServerImpl) PropagateNewBlockHashes(ctx context.Context, announces []headerdownload.Announce) {
 	cs.lock.RLock()
 	defer cs.lock.RUnlock()
