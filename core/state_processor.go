b83304c0558f668cce6c3b233170ab74514cce0c
Alexey Sharp <alexeysharp@Alexeys-iMac.local>
2021-04-30 12:51:56 +0100

    Aleut fix for GASPRICE opcode

--- core/state_processor.go
+++ core/state_processor.go
@@ -86,7 +86,7 @@ func FormatLogs(logs []vm.StructLog) []StructLogRes {
 // for the transaction, gas used and an error if the transaction failed,
 // indicating the block was invalid.
 func applyTransaction(msg types.Message, config *params.ChainConfig, getHeader func(hash common.Hash, number uint64) *types.Header, engine consensus.Engine, author *common.Address, gp *GasPool, statedb *state.IntraBlockState, stateWriter state.StateWriter, header *types.Header, tx types.Transaction, usedGas *uint64, evm *vm.EVM, cfg vm.Config) (*types.Receipt, error) {
-	msg, err := tx.AsMessage(*types.MakeSigner(config, header.Number.Uint64()))
+	msg, err := tx.AsMessage(header, *types.MakeSigner(config, header.Number.Uint64()))
 	if err != nil {
 		return nil, err
 	}
@@ -148,7 +148,7 @@ func applyTransaction(msg types.Message, config *params.ChainConfig, getHeader f
 // for the transaction, gas used and an error if the transaction failed,
 // indicating the block was invalid.
 func ApplyTransaction(config *params.ChainConfig, getHeader func(hash common.Hash, number uint64) *types.Header, engine consensus.Engine, author *common.Address, gp *GasPool, ibs *state.IntraBlockState, stateWriter state.StateWriter, header *types.Header, tx types.Transaction, usedGas *uint64, cfg vm.Config) (*types.Receipt, error) {
-	msg, err := tx.AsMessage(*types.MakeSigner(config, header.Number.Uint64()))
+	msg, err := tx.AsMessage(header, *types.MakeSigner(config, header.Number.Uint64()))
 	if err != nil {
 		return nil, err
 	}
