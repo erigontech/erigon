73c0e4bb1246c489f83fe16205fe5c783633b15c
alex.sharov <AskAlexSharov@gmail.com>
2021-05-17 09:24:53 +0700

    a

--- cmd/headers/commands/root.go
+++ cmd/headers/commands/root.go
@@ -8,24 +8,16 @@ import (
 	"path"
 	"syscall"
 
-	"github.com/c2h5oh/datasize"
 	"github.com/ledgerwatch/turbo-geth/cmd/utils"
-	"github.com/ledgerwatch/turbo-geth/common/paths"
-	"github.com/ledgerwatch/turbo-geth/ethdb"
 	"github.com/ledgerwatch/turbo-geth/internal/debug"
 	"github.com/ledgerwatch/turbo-geth/log"
 	"github.com/spf13/cobra"
 )
 
 var (
-	sentryAddr   string   // Address of the sentry <host>:<port>
-	sentryAddrs  []string // Address of the sentry <host>:<port>
-	chaindata    string   // Path to chaindata
-	snapshotDir  string
-	snapshotMode string
-	datadir      string // Path to td working dir
-	database     string // Type of database (lmdb or mdbx)
-	mapSizeStr   string // Map size for LMDB
+	sentryAddr string // Address of the sentry <host>:<port>
+	chaindata  string // Path to chaindata
+	datadir    string // Path to td working dir
 )
 
 func init() {
@@ -74,56 +66,4 @@ func Execute() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-}
-
-func must(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
-func withDatadir(cmd *cobra.Command) {
-	cmd.Flags().StringVar(&datadir, "datadir", paths.DefaultDataDir(), "data directory for temporary ELT files")
-	must(cmd.MarkFlagDirname("datadir"))
-	cmd.Flags().StringVar(&mapSizeStr, "lmdb.mapSize", "", "map size for LMDB")
-
-	cmd.Flags().StringVar(&chaindata, "chaindata", "", "path to the db")
-	must(cmd.MarkFlagDirname("chaindata"))
-
-	cmd.Flags().StringVar(&snapshotMode, "snapshot.mode", "", "set of snapshots to use")
-	cmd.Flags().StringVar(&snapshotDir, "snapshot.dir", "", "snapshot dir")
-	must(cmd.MarkFlagDirname("snapshot.dir"))
-
-	cmd.Flags().StringVar(&database, "database", "", "lmdb|mdbx")
-}
-
-func openDatabase(path string) *ethdb.ObjectDatabase {
-	db := ethdb.NewObjectDatabase(openKV(path, false))
-	return db
-}
-
-func openKV(path string, exclusive bool) ethdb.RwKV {
-	if database == "mdbx" {
-		opts := ethdb.NewMDBX().Path(path)
-		if exclusive {
-			opts = opts.Exclusive()
-		}
-		if mapSizeStr != "" {
-			var mapSize datasize.ByteSize
-			must(mapSize.UnmarshalText([]byte(mapSizeStr)))
-			opts = opts.MapSize(mapSize)
-		}
-		return opts.MustOpen()
-	}
-
-	opts := ethdb.NewLMDB().Path(path)
-	if exclusive {
-		opts = opts.Exclusive()
-	}
-	if mapSizeStr != "" {
-		var mapSize datasize.ByteSize
-		must(mapSize.UnmarshalText([]byte(mapSizeStr)))
-		opts = opts.MapSize(mapSize)
-	}
-	return opts.MustOpen()
 }
