74595b043c51465c91d2153b21716a46abb140ff
b00ris <b00ris@mail.ru>
2021-05-31 12:54:51 +0300

    fix

--- common/dbutils/bucket.go
+++ common/dbutils/bucket.go
@@ -74,8 +74,8 @@ StorageChangeSetBucket:
 	key - blockNum_u64 + address + incarnation_u64
 	value - plain_storage_key + value
 */
-var AccountChangeSetBucket = "PLAIN-ACS"
-var StorageChangeSetBucket = "PLAIN-SCS"
+const AccountChangeSetBucket = "PLAIN-ACS"
+const StorageChangeSetBucket = "PLAIN-SCS"
 
 const (
 
