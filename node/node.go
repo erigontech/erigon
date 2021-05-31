e90240d5e492c97dfc54b3332d1b911165097641
Alex Sharov <AskAlexSharov@gmail.com>
2021-05-31 11:03:40 +0700

    clean logs (#2058)

--- node/node.go
+++ node/node.go
@@ -528,7 +528,7 @@ func (n *Node) OpenDatabaseWithFreezer(name string, datadir string) (*ethdb.Obje
 
 		var openFunc func(exclusive bool) (*ethdb.ObjectDatabase, error)
 		if n.config.MDBX {
-			log.Info("Opening Database (MDBX)", "mapSize", n.config.LMDBMapSize.HR())
+			log.Info("Opening Database (MDBX)")
 			openFunc = func(exclusive bool) (*ethdb.ObjectDatabase, error) {
 				opts := ethdb.NewMDBX().Path(dbPath).MapSize(n.config.LMDBMapSize).DBVerbosity(n.config.DatabaseVerbosity)
 				if exclusive {
