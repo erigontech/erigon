0e4f394b0bbc8c8243450031a71493b23ced9407
Alex Sharp <alexsharp@Alexs-MacBook-Pro.local>
2021-06-03 09:10:16 +0100

    More to the test

--- cmd/rpcdaemon/commands/call_traces_test.go
+++ cmd/rpcdaemon/commands/call_traces_test.go
@@ -12,21 +12,42 @@ import (
 	"github.com/ledgerwatch/erigon/common/hexutil"
 	"github.com/ledgerwatch/erigon/core"
 	"github.com/ledgerwatch/erigon/turbo/stages"
+	"github.com/valyala/fastjson"
 )
+
+func blockNumbersFromTraces(t *testing.T, b []byte) []int {
+	var err error
+	var p fastjson.Parser
+	response := b
+	var v *fastjson.Value
+	if v, err = p.ParseBytes(response); err != nil {
+		t.Fatalf("parsing response: %v", err)
+	}
+	var elems []*fastjson.Value
+	if elems, err = v.Array(); err != nil {
+		t.Fatalf("expected array in the response: %v", err)
+	}
+	var numbers []int
+	for _, elem := range elems {
+		bn := elem.GetInt("blockNumber")
+		numbers = append(numbers, bn)
+	}
+	return numbers
+}
 
 func TestCallTraceUnwind(t *testing.T) {
 	m := stages.Mock(t)
 	defer m.DB.Close()
 	var chainA, chainB *core.ChainPack
 	var err error
-	chainA, err = core.GenerateChain(m.ChainConfig, m.Genesis, m.Engine, m.DB, 10, func(i int, gen *core.BlockGen) {
+	chainA, err = core.GenerateChain(m.ChainConfig, m.Genesis, m.Engine, m.DB, 100, func(i int, gen *core.BlockGen) {
 		gen.SetCoinbase(common.Address{1})
 	}, false /* intemediateHashes */)
 	if err != nil {
 		t.Fatalf("generate chainA: %v", err)
 	}
-	chainB, err = core.GenerateChain(m.ChainConfig, m.Genesis, m.Engine, m.DB, 12, func(i int, gen *core.BlockGen) {
-		if i < 5 || i >= 10 {
+	chainB, err = core.GenerateChain(m.ChainConfig, m.Genesis, m.Engine, m.DB, 200, func(i int, gen *core.BlockGen) {
+		if i < 50 || i >= 100 {
 			gen.SetCoinbase(common.Address{1})
 		} else {
 			gen.SetCoinbase(common.Address{2})
@@ -43,7 +64,7 @@ func TestCallTraceUnwind(t *testing.T) {
 	stream := jsoniter.NewStream(jsoniter.ConfigDefault, &buf, 4096)
 	var fromBlock, toBlock uint64
 	fromBlock = 1
-	toBlock = 10
+	toBlock = 100
 	toAddress1 := common.Address{1}
 	traceReq1 := TraceFilterRequest{
 		FromBlock: (*hexutil.Uint64)(&fromBlock),
@@ -53,12 +74,13 @@ func TestCallTraceUnwind(t *testing.T) {
 	if err = api.Filter(context.Background(), traceReq1, stream); err != nil {
 		t.Fatalf("trace_filter failed: %v", err)
 	}
-	fmt.Printf("Result: %s\n", buf.Bytes())
-	if err = m.InsertChain(chainB); err != nil {
+
+	fmt.Printf("result1: %d\n", blockNumbersFromTraces(t, buf.Bytes()))
+	if err = m.InsertChain(chainB.Slice(0, 120)); err != nil {
 		t.Fatalf("inserting chainB: %v", err)
 	}
 	buf.Reset()
-	toBlock = 12
+	toBlock = 120
 	traceReq2 := TraceFilterRequest{
 		FromBlock: (*hexutil.Uint64)(&fromBlock),
 		ToBlock:   (*hexutil.Uint64)(&toBlock),
@@ -67,5 +89,20 @@ func TestCallTraceUnwind(t *testing.T) {
 	if err = api.Filter(context.Background(), traceReq2, stream); err != nil {
 		t.Fatalf("trace_filter failed: %v", err)
 	}
-	fmt.Printf("Result2: %s\n", buf.Bytes())
+	fmt.Printf("result2: %d\n", blockNumbersFromTraces(t, buf.Bytes()))
+	if err = m.InsertChain(chainB.Slice(120, chainB.Length)); err != nil {
+		t.Fatalf("inserting chainB: %v", err)
+	}
+	buf.Reset()
+	fromBlock = 120
+	toBlock = 200
+	traceReq3 := TraceFilterRequest{
+		FromBlock: (*hexutil.Uint64)(&fromBlock),
+		ToBlock:   (*hexutil.Uint64)(&toBlock),
+		ToAddress: []*common.Address{&toAddress1},
+	}
+	if err = api.Filter(context.Background(), traceReq3, stream); err != nil {
+		t.Fatalf("trace_filter failed: %v", err)
+	}
+	fmt.Printf("result3: %d\n", blockNumbersFromTraces(t, buf.Bytes()))
 }
