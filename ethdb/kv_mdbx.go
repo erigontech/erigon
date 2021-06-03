b84d03d860b5f317e260335dd91c5c85bbf65a2e
alex.sharov <AskAlexSharov@gmail.com>
2021-06-03 11:26:42 +0700

    add db label - later will add txpool db

--- ethdb/kv_mdbx.go
+++ ethdb/kv_mdbx.go
@@ -28,6 +28,7 @@ const expectMdbxVersionMinor = 10
 type MdbxOpts struct {
 	inMem      bool
 	exclusive  bool
+	label      Label // marker to distinct db instances - one process may open many databases. for example to collect metrics of only 1 database
 	flags      uint
 	path       string
 	bucketsCfg BucketConfigsFunc
@@ -40,6 +41,11 @@ func NewMDBX() MdbxOpts {
 		bucketsCfg: DefaultBucketConfigs,
 		flags:      mdbx.NoReadahead | mdbx.Coalesce | mdbx.Durable,
 	}
+}
+
+func (opts MdbxOpts) Label(label Label) MdbxOpts {
+	opts.label = label
+	return opts
 }
 
 func (opts MdbxOpts) Path(path string) MdbxOpts {
@@ -318,6 +324,9 @@ func (db *MdbxKV) CollectMetrics() {
 	if !metrics.Enabled {
 		return
 	}
+	if db.opts.label != Chain {
+		return
+	}
 	info, err := db.env.Info()
 	if err != nil {
 		return // ignore error for metrics collection
@@ -452,6 +461,9 @@ func (tx *MdbxTx) ForPrefix(bucket string, prefix []byte, walker func(k, v []byt
 
 func (tx *MdbxTx) CollectMetrics() {
 	if !metrics.Enabled {
+		return
+	}
+	if tx.db.opts.label != Chain {
 		return
 	}
 	txInfo, err := tx.tx.Info(true)
