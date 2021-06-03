bb4eb40eeeb54c19194ef713c3eb9bf82fca0046
alex.sharov <AskAlexSharov@gmail.com>
2021-06-03 17:09:35 +0700

    close cursor

--- ethdb/kv_mdbx.go
+++ ethdb/kv_mdbx.go
@@ -380,13 +380,15 @@ func (db *MdbxKV) BeginRw(_ context.Context) (txn RwTx, err error) {
 
 type MdbxTx struct {
 	readOnly         bool
+	cursorID         uint16
 	tx               *mdbx.Txn
 	db               *MdbxKV
-	cursors          []*mdbx.Cursor
+	cursors          map[uint16]*mdbx.Cursor
 	statelessCursors map[string]Cursor
 }
 
 type MdbxCursor struct {
+	id         uint16
 	tx         *MdbxTx
 	bucketName string
 	dbi        mdbx.DBI
@@ -810,7 +812,7 @@ func (tx *MdbxTx) closeCursors() {
 			c.Close()
 		}
 	}
-	tx.cursors = []*mdbx.Cursor{}
+	tx.cursors = map[uint16]*mdbx.Cursor{}
 }
 
 func (tx *MdbxTx) statelessCursor(bucket string) (RwCursor, error) {
@@ -949,7 +951,8 @@ func (tx *MdbxTx) Cursor(bucket string) (Cursor, error) {
 
 func (tx *MdbxTx) stdCursor(bucket string) (RwCursor, error) {
 	b := tx.db.buckets[bucket]
-	c := &MdbxCursor{bucketName: bucket, tx: tx, bucketCfg: b, dbi: mdbx.DBI(tx.db.buckets[bucket].DBI)}
+	c := &MdbxCursor{bucketName: bucket, tx: tx, bucketCfg: b, dbi: mdbx.DBI(tx.db.buckets[bucket].DBI), id: tx.cursorID}
+	tx.cursorID++
 
 	var err error
 	c.c, err = tx.tx.OpenCursor(c.dbi)
@@ -959,9 +962,9 @@ func (tx *MdbxTx) stdCursor(bucket string) (RwCursor, error) {
 
 	// add to auto-cleanup on end of transactions
 	if tx.cursors == nil {
-		tx.cursors = make([]*mdbx.Cursor, 0, 1)
+		tx.cursors = map[uint16]*mdbx.Cursor{}
 	}
-	tx.cursors = append(tx.cursors, c.c)
+	tx.cursors[c.id] = c.c
 	return c, nil
 }
 
@@ -1391,21 +1394,7 @@ func (c *MdbxCursor) Append(k []byte, v []byte) error {
 func (c *MdbxCursor) Close() {
 	if c.c != nil {
 		c.c.Close()
-		l := len(c.tx.cursors)
-		if l == 0 {
-			c.c = nil
-			return
-		}
-		//TODO: Find a better solution to avoid the leak?
-		newCursors := make([]*mdbx.Cursor, l-1)
-		i := 0
-		for _, cc := range c.tx.cursors {
-			if cc != c.c {
-				newCursors[i] = cc
-				i++
-			}
-		}
-		c.tx.cursors = newCursors
+		delete(c.tx.cursors, c.id)
 		c.c = nil
 	}
 }
