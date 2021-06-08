657a8b13dfe6223242b515d8a954a86257a88c68
ledgerwatch <akhounov@gmail.com>
2021-06-08 16:16:27 +0100

    Do not remove headers when unwinding (#2122)
    Co-authored-by: Alex Sharp <alexsharp@Alexs-MacBook-Pro.local>

--- turbo/stages/mock_sentry.go
+++ turbo/stages/mock_sentry.go
@@ -381,7 +381,9 @@ func (ms *MockSentry) InsertChain(chain *core.ChainPack) error {
 		}
 	}
 	// Check if the latest header was imported or rolled back
-	if rawdb.ReadHeader(ethdb.NewObjectDatabase(ms.DB), chain.TopBlock.Hash(), chain.TopBlock.NumberU64()) == nil {
+	if td, tdErr := rawdb.ReadTd(ethdb.NewObjectDatabase(ms.DB), chain.TopBlock.Hash(), chain.TopBlock.NumberU64()); tdErr != nil {
+		return tdErr
+	} else if td == nil {
 		return fmt.Errorf("did not import block %d %x", chain.TopBlock.NumberU64(), chain.TopBlock.Hash())
 	}
 	return nil
