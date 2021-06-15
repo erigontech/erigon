b48c489e5f48b677802d7aade132f4909c3792f3
Artem Vorotnikov <artem@vorotnikov.me>
2021-06-15 18:11:42 +0300

    Add ListenAddr65 to defaults

--- node/defaults.go
+++ node/defaults.go
@@ -39,8 +39,9 @@ var DefaultConfig = Config{
 	//WSPort:    DefaultWSPort,
 	//WSModules: []string{"net", "web3"},
 	P2P: p2p.Config{
-		ListenAddr: ":30303",
-		MaxPeers:   100,
-		NAT:        nat.Any(),
+		ListenAddr:   ":30303",
+		ListenAddr65: ":30304",
+		MaxPeers:     100,
+		NAT:          nat.Any(),
 	},
 }
