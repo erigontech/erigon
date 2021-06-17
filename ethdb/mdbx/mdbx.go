cc5e2a8982dbe69856924bb18b2ecf2c4ca7d687
alex.sharov <AskAlexSharov@gmail.com>
2021-06-17 15:57:44 +0700

    save

--- ethdb/mdbx/mdbx.go
+++ ethdb/mdbx/mdbx.go
@@ -131,10 +131,13 @@ details about dealing with such situations.
 package mdbx
 
 /*
-#cgo !windows CFLAGS: -O2 -g -DMDBX_BUILD_FLAGS='' -Wall -Werror -Wextra -Wpedantic -Wno-deprecated-declarations -fPIC -fvisibility=hidden -pthread -Wno-error=attributes -W -Wall -Wno-format -Wno-implicit-fallthrough -Wno-unused-parameter -Wno-format-extra-args -Wno-missing-field-initializers
-#cgo windows CFLAGS: -O2 -g -DMDBX_BUILD_FLAGS='' -Wno-deprecated-declarations -Wno-bad-function-cast -Wno-cast-function-type -pthread -W -Wall -Wno-format -Wno-implicit-fallthrough -Wno-unused-parameter -Wno-format-extra-args -Wno-missing-field-initializers
+#cgo !windows CFLAGS: -O2 -g -DMDBX_BUILD_FLAGS='' -fPIC -fvisibility=hidden -pthread -Wno-error=attributes -W -Wall -Werror -Wextra -Wpedantic -Wno-deprecated-declarations -Wno-format -Wno-implicit-fallthrough -Wno-unused-parameter -Wno-format-extra-args -Wno-missing-field-initializers
+#cgo windows CFLAGS: -O2 -g -DMDBX_BUILD_FLAGS='' -fvisibility=hidden -ffast-math -fdata-sections -fexceptions -fno-common -ggdb -W -Wno-deprecated-declarations -Wno-bad-function-cast -Wno-cast-function-type -Wall -Wno-format -Wno-implicit-fallthrough -Wno-unused-parameter -Wno-format-extra-args -Wno-missing-field-initializers
 
 #cgo windows LDFLAGS: -lntdll
+#cgo solaris LDFLAGS: -lkstat
+#cgo linux !darwin !openbsd LDFLAGS: -lrt
+
 */
 import "C"
 
