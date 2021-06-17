1d28150a64d5a9a76a485c2a0e3d5d0d64f110c6
Jonas Bostoen <jonas.bostoen@student.kdg.be>
2021-06-17 15:25:23 +0200

    Document version change

--- ethdb/remote/remotedbserver/ethbackend.go
+++ ethdb/remote/remotedbserver/ethbackend.go
@@ -17,6 +17,7 @@ import (
 
 // EthBackendAPIVersion
 // 2.0.0 - move all mining-related methods to 'txpool/mining' server
+// 2.1.0 - add NetPeerCount function
 var EthBackendAPIVersion = &types2.VersionReply{Major: 2, Minor: 1, Patch: 0}
 
 type EthBackendServer struct {
