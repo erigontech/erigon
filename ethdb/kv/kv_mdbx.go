b7bc6a8d069388b490b710c3cd9f6a2c567320c0
Artem Vorotnikov <artem@vorotnikov.me>
2021-06-20 12:03:26 +0300

    Revert "Remove unused tables, soft rename"

--- ethdb/kv/kv_mdbx.go
+++ ethdb/kv/kv_mdbx.go
@@ -621,6 +621,26 @@ func (tx *MdbxTx) CreateBucket(name string) error {
 		return nil
 	}
 
+	// if bucket with this name not found - check renamed one
+	rename := dbutils.Rename[name]
+
+	dbi, err = tx.tx.OpenDBI(rename, mdbx.DBAccede, nil, dcmp)
+	if err != nil && !mdbx.IsNotFound(err) {
+		return fmt.Errorf("create bucket: %s, %w", name, err)
+	}
+	if err == nil {
+		cnfCopy.DBI = dbutils.DBI(dbi)
+		var flags uint
+		flags, err = tx.tx.Flags(dbi)
+		if err != nil {
+			return err
+		}
+		cnfCopy.Flags = dbutils.BucketFlags(flags)
+
+		tx.db.buckets[name] = cnfCopy
+		return nil
+	}
+
 	// if bucket doesn't exists - create it
 
 	var flags = tx.db.buckets[name].Flags
@@ -637,8 +657,11 @@ func (tx *MdbxTx) CreateBucket(name string) error {
 		return fmt.Errorf("some not supported flag provided for bucket")
 	}
 
-	dbi, err = tx.tx.OpenDBI(name, nativeFlags, nil, dcmp)
-
+	if rename != "" {
+		dbi, err = tx.tx.OpenDBI(rename, nativeFlags, nil, dcmp)
+	} else {
+		dbi, err = tx.tx.OpenDBI(name, nativeFlags, nil, dcmp)
+	}
 	if err != nil {
 		return fmt.Errorf("create bucket: %s, %w", name, err)
 	}
