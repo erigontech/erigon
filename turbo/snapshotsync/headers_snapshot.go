920a167a9a95816528c3e15f412fc8b773c71967
b00ris <b00ris@mail.ru>
2021-06-29 10:50:58 +0300

    debug

--- turbo/snapshotsync/headers_snapshot.go
+++ turbo/snapshotsync/headers_snapshot.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/c2h5oh/datasize"
 	"github.com/ledgerwatch/erigon/common"
 	"github.com/ledgerwatch/erigon/common/dbutils"
 	"github.com/ledgerwatch/erigon/core/rawdb"
@@ -90,7 +89,7 @@ func OpenHeadersSnapshot(dbPath string) (ethdb.RoKV, error) {
 		return dbutils.BucketsCfg{
 			dbutils.HeadersBucket: dbutils.BucketsConfigs[dbutils.HeadersBucket],
 		}
-	}).Readonly().MapSize(64 * datasize.MB).Path(dbPath).Open()
+	}).Path(dbPath).Open()
 }
 
 func RemoveHeadersData(db ethdb.RoKV, tx ethdb.RwTx, currentSnapshot, newSnapshot uint64) (err error) {
