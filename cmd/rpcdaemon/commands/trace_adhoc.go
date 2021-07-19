1f984e4a9d34cfc9b5fd89d32ee15a649349938e
Alex Sharp <alexsharp@Alexs-MacBook-Pro.local>
2021-07-19 23:26:25 +0100

    Print gas price

--- cmd/rpcdaemon/commands/trace_adhoc.go
+++ cmd/rpcdaemon/commands/trace_adhoc.go
@@ -182,7 +182,7 @@ func (args *TraceCallParam) ToMessage(globalGasCap uint64, baseFee *uint256.Int)
 	if args.AccessList != nil {
 		accessList = *args.AccessList
 	}
-
+	fmt.Printf("GasPrice = %s\n", gasPrice)
 	msg := types.NewMessage(addr, args.To, 0, value, gas, gasPrice, gasFeeCap, gasTipCap, data, accessList, false /* checkNonce */)
 	return msg, nil
 }
