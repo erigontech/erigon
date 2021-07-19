406fbe444da4d7d36cb66c7173cf60a0ca85d246
Alex Sharp <alexsharp@Alexs-MacBook-Pro.local>
2021-07-19 23:23:22 +0100

    Print gasPrice

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -431,6 +431,7 @@ func (args *CallArgs) ToMessage(globalGasCap uint64, baseFee *uint256.Int) (type
 		accessList = *args.AccessList
 	}
 
+	fmt.Printf("GasPrice = %s\n", gasPrice)
 	msg := types.NewMessage(addr, args.To, 0, value, gas, gasPrice, gasFeeCap, gasTipCap, data, accessList, false)
 	return msg, nil
 }
