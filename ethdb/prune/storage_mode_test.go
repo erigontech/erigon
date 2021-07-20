9f8f65951d1c4b4ddb6f804bae89edf01ebfef22
alex.sharov <AskAlexSharov@gmail.com>
2021-07-20 12:18:03 +0700

    fix test

--- ethdb/prune/storage_mode_test.go
+++ ethdb/prune/storage_mode_test.go
@@ -5,43 +5,19 @@ import (
 
 	"github.com/ledgerwatch/erigon/common/math"
 	"github.com/ledgerwatch/erigon/ethdb/kv"
-	"github.com/ledgerwatch/erigon/params"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestSetStorageModeIfNotExist(t *testing.T) {
 	_, tx := kv.NewTestTx(t)
 	prune, err := Get(tx)
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(t, err)
+	assert.Equal(t, Mode{true, math.MaxUint64, math.MaxUint64, math.MaxUint64, math.MaxUint64, Experiments{TEVM: false}}, prune)
 
-	assert.Equal(t, prune, Mode{Initialised: true, TxIndex: params.FullImmutabilityThreshold,
-		CallTraces: params.FullImmutabilityThreshold, History: params.FullImmutabilityThreshold,
-		Receipts: params.FullImmutabilityThreshold, Experiments: Experiments{TEVM: false}})
-
-	err = SetIfNotExist(tx, Mode{
-		true,
-		math.MaxUint64,
-		math.MaxUint64,
-		math.MaxUint64,
-		math.MaxUint64,
-		Experiments{TEVM: false},
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
+	err = SetIfNotExist(tx, Mode{true, 1, 2, 3, 4, Experiments{TEVM: false}})
+	assert.NoError(t, err)
 
 	prune, err = Get(tx)
-	if err != nil {
-		t.Fatal(err)
-	}
-	assert.Equal(t, prune, Mode{
-		true,
-		math.MaxUint64,
-		math.MaxUint64,
-		math.MaxUint64,
-		math.MaxUint64,
-		Experiments{TEVM: false},
-	})
+	assert.NoError(t, err)
+	assert.Equal(t, Mode{true, 1, 2, 3, 4, Experiments{TEVM: false}}, prune)
 }
