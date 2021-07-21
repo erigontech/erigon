96ee6e15378b8367538b84c709d08fbfdafc9448
ledgerwatch <akhounov@gmail.com>
2021-07-21 19:35:08 +0100

    Fix for eip1559 incorrect assert (#2415)
    Co-authored-by: Alex Sharp <alexsharp@Alexs-MacBook-Pro.local>

--- core/state_transition.go
+++ core/state_transition.go
@@ -206,6 +206,7 @@ func (st *StateTransition) buyGas(gasBailout bool) error {
 		balanceCheck = st.sharedBuyGasBalance.SetUint64(st.msg.Gas())
 		balanceCheck = balanceCheck.Mul(balanceCheck, st.gasFeeCap)
 	}
+	balanceCheck.Add(balanceCheck, st.value)
 	if have, want := st.state.GetBalance(st.msg.From()), balanceCheck; have.Cmp(want) < 0 {
 		if !gasBailout {
 			return fmt.Errorf("%w: address %v have %v want %v", ErrInsufficientFunds, st.msg.From().Hex(), have, want)
