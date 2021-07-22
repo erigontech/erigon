0109ed88f2383b06980787e0cc9551e157c9db13
Alexey Sharp <alexeysharp@Alexeys-iMac.local>
2021-07-22 11:09:27 +0100

    Correct fix for eip1559

--- core/state_transition.go
+++ core/state_transition.go
@@ -205,8 +205,8 @@ func (st *StateTransition) buyGas(gasBailout bool) error {
 	if st.gasFeeCap != nil {
 		balanceCheck = st.sharedBuyGasBalance.SetUint64(st.msg.Gas())
 		balanceCheck = balanceCheck.Mul(balanceCheck, st.gasFeeCap)
+		balanceCheck.Add(balanceCheck, st.value)
 	}
-	balanceCheck.Add(balanceCheck, st.value)
 	if have, want := st.state.GetBalance(st.msg.From()), balanceCheck; have.Cmp(want) < 0 {
 		if !gasBailout {
 			return fmt.Errorf("%w: address %v have %v want %v", ErrInsufficientFunds, st.msg.From().Hex(), have, want)
