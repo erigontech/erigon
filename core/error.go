564a8ed04cf786c2bca89b4f973fbfb5369b7f3c
yperbasis <andrey.ashikhmin@gmail.com>
2021-07-22 15:09:09 +0200

    Remove unnecessary value transfer check from TransitionDb

--- core/error.go
+++ core/error.go
@@ -65,10 +65,6 @@ var (
 	// by a transaction is higher than what's left in the block.
 	ErrGasLimitReached = errors.New("gas limit reached")
 
-	// ErrInsufficientFundsForTransfer is returned if the transaction sender doesn't
-	// have enough funds for transfer(topmost call only).
-	ErrInsufficientFundsForTransfer = errors.New("insufficient funds for transfer")
-
 	// ErrInsufficientFunds is returned if the total cost of executing a transaction
 	// is higher than the balance of the user's account.
 	ErrInsufficientFunds = errors.New("insufficient funds for gas * price + value")
