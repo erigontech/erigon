81c1e73785b1d8ad6e02d534dfbc7df6beaa3e96
alex.sharov <AskAlexSharov@gmail.com>
2021-08-07 10:42:45 +0700

    Invariant: if no notification, then no pools growth

--- txpool/pool.go
+++ txpool/pool.go
@@ -840,12 +840,6 @@ func (l *recentlyConnectedPeers) GetAndClean() []PeerID {
 	return peers
 }
 
-func max(a, b uint64) uint64 {
-	if a >= b {
-		return a
-	}
-	return b
-}
 func min(a, b uint64) uint64 {
 	if a <= b {
 		return a
