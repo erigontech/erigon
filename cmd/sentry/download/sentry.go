7fc9dd00cb4b9b3dcaed0cd5ec460969706dfb34
alex.sharov <AskAlexSharov@gmail.com>
2021-08-16 15:59:30 +0700

    run

--- cmd/sentry/download/sentry.go
+++ cmd/sentry/download/sentry.go
@@ -946,18 +946,11 @@ func (ss *SentryServerImpl) addMessagesStream(ids []proto_sentry.MessageId, serv
 }
 
 func (ss *SentryServerImpl) Messages(req *proto_sentry.MessagesRequest, server proto_sentry.Sentry_MessagesServer) error {
-	//if err := common.Stopped(ss.ctx.Done()); err != nil {
-	//	return ss.ctx.Err()
-	//}
 	log.Debug(fmt.Sprintf("[Messages] new subscriber to: %s", req.Ids))
 	clean := ss.addMessagesStream(req.Ids, server)
 	defer clean()
-	select {
-	case <-ss.ctx.Done():
-		return nil
-	case <-server.Context().Done():
-		return nil
-	}
+	<-server.Context().Done()
+	return nil
 }
 
 // Close performs cleanup operations for the sentry
