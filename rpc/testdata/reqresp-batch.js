e7574a6d14a64c5af58574d909b8c6a014bd2daf
Alex Sharov <AskAlexSharov@gmail.com>
2021-08-19 09:26:06 +0700

    RPC: batch - preserve order, streaming to in-mem buf   (#2541)
    * preserve order in batch
    
    * fix batch order
    
    * base fee in header json
    
    * less logs in tests
    
    * less logs in tests
    
    * save
    
    * save

--- rpc/testdata/reqresp-batch.js
+++ rpc/testdata/reqresp-batch.js
@@ -1,7 +1,7 @@
 // There is no response for all-notification batches.
 
 --> [{"jsonrpc":"2.0","method":"test_echo","params":["x",99]}]
-<-- []
+
 // This test checks regular batch calls.
 
 --> [{"jsonrpc":"2.0","id":2,"method":"test_echo","params":[]}, {"jsonrpc":"2.0","id": 3,"method":"test_echo","params":["x",3]}]
