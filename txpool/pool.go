7f7c312f706765f047c7f88c2f3663c235e8b24c
alex.sharov <AskAlexSharov@gmail.com>
2021-08-27 16:35:07 +0700

    add state check assert routine

--- txpool/pool.go
+++ txpool/pool.go
@@ -1127,9 +1127,9 @@ func (p *TxPool) fromDB(ctx context.Context, tx kv.RwTx, coreTx kv.Tx) error {
 				return err
 			}
 			if len(vv) == 0 {
-				tx.ForEach(kv.PooledSenderIDToAdress, nil, func(k, vvvv []byte) error {
-					if bytes.HasPrefix(k, v[:7]) {
-						fmt.Printf("found:%x,%x\n", k, vvvv)
+				tx.ForEach(kv.PooledSenderIDToAdress, nil, func(kkk, vvvv []byte) error {
+					if bytes.HasPrefix(kkk, v[:7]) {
+						fmt.Printf("found:%x,%x\n", kkk, vvvv)
 					}
 					return nil
 				})
