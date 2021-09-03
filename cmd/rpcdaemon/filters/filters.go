4a8fa3b4e702dcf7f41d5ea8e80a3fe8aa868aad
alex.sharov <AskAlexSharov@gmail.com>
2021-09-03 10:00:07 +0700

    save

--- cmd/rpcdaemon/filters/filters.go
+++ cmd/rpcdaemon/filters/filters.go
@@ -110,9 +110,7 @@ func New(ctx context.Context, ethBackend services.ApiBackend, txPool txpool.Txpo
 				}
 			}
 		}()
-		fmt.Printf("aa:%#v,%t\n", mining, mining == nil)
 		if reflect.ValueOf(mining).IsNil() { //https://groups.google.com/g/golang-nuts/c/wnH302gBa4I
-			fmt.Printf("aa1:%#v,%t\n", mining, mining == nil)
 			go func() {
 				for {
 					select {
