8815829b64bcfb0927cc26a9ce943e008b6b62ab
Alex Sharp <alexsharp@Alexs-MacBook-Pro.local>
2021-09-11 18:20:02 +0100

    Clean up

--- ethdb/mdbx/dist/config.h
+++
@@ -1,5 +0,0 @@
-#define MDBX_BUILD_FLAGS "-std=gnu++2a -std=gnu11 -O2 -g -Wall -Werror -Wextra -Wpedantic -ffunction-sections -fPIC -fvisibility=hidden -pthread -Wno-error=attributes     "
-#define MDBX_BUILD_COMPILER "Apple clang version 12.0.0 (clang-1200.0.32.29)"
-#define MDBX_BUILD_TARGET "x86_64-apple-darwin20.3.0"
-#define MDBX_DEBUG 0
-#define MDBX_FORCE_ASSERTIONS 0
