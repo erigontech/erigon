6e7eb8dea3c3cc253fdb743ece16fd8888c2b383
alex.sharov <AskAlexSharov@gmail.com>
2021-09-12 17:33:15 +0700

    eno

--- p2p/enode/nodedb.go
+++ p2p/enode/nodedb.go
@@ -766,13 +766,17 @@ func (db *DB) commitCache(logit bool) {
 
 // close flushes and closes the database files.
 func (db *DB) Close() {
-	db.kvCacheLock.Lock()
-	defer db.kvCacheLock.Unlock()
+	select {
+	case <-db.quit:
+		return // means closed already
+	default:
+	}
 	if db.quit == nil {
 		return
 	}
 	close(db.quit)
-	db.quit = nil
+	db.kvCacheLock.Lock()
+	defer db.kvCacheLock.Unlock()
 	db.commitCache(true /* logit */)
 	db.kv.Close()
 }
