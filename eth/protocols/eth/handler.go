81e7c7b94bebe4fd0f59d1709d1a15bbfd5064b1
Zachinquarantine <zachinquarantine@yahoo.com>
2021-09-13 16:22:15 -0400

    Update handler.go

--- eth/protocols/eth/handler.go
+++ eth/protocols/eth/handler.go
@@ -125,7 +125,7 @@ func MakeProtocols(backend Backend, readNodeInfo func() *NodeInfo, dnsdisc enode
 // NodeInfo represents a short summary of the `eth` sub-protocol metadata
 // known about the host peer.
 type NodeInfo struct {
-	Network    uint64              `json:"network"`    // Ethereum network ID (1=Frontier, 2=Morden, Ropsten=3, Rinkeby=4)
+	Network    uint64              `json:"network"`    // Ethereum network ID (1=Mainnet, Ropsten=3, Rinkeby=4)
 	Difficulty *big.Int            `json:"difficulty"` // Total difficulty of the host's blockchain
 	Genesis    common.Hash         `json:"genesis"`    // SHA3 hash of the host's genesis block
 	Config     *params.ChainConfig `json:"config"`     // ChainDB configuration for the fork rules
