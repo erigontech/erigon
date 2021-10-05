3f0fa31086b758c7baddfde81f9c30e181f841c5
alex.sharov <AskAlexSharov@gmail.com>
2021-10-05 10:34:38 +0700

    save

--- cmd/rpcdaemon/commands/trace_adhoc.go
+++ cmd/rpcdaemon/commands/trace_adhoc.go
@@ -30,6 +30,7 @@ import (
 	"github.com/ledgerwatch/erigon/turbo/rpchelper"
 	"github.com/ledgerwatch/erigon/turbo/shards"
 	"github.com/ledgerwatch/erigon/turbo/transactions"
+	"github.com/ledgerwatch/log/v3"
 )
 
 const callTimeout = 5 * time.Minute
