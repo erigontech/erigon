2ea31fb032b336c646b3a5e0db0eee6c8c417bb6
alex.sharov <AskAlexSharov@gmail.com>
2021-10-06 14:24:18 +0700

    remove debug prints

--- turbo/transactions/call.go
+++ turbo/transactions/call.go
@@ -45,6 +45,7 @@ func DoCall(ctx context.Context, args ethapi.CallArgs, tx kv.Tx, blockNrOrHash r
 		if err != nil {
 			return nil, err
 		}
+		fmt.Printf("alex: %T\n", cacheView)
 		stateReader = state.NewCachedReader2(cacheView, tx)
 	} else {
 		stateReader = state.NewPlainState(tx, blockNumber)
