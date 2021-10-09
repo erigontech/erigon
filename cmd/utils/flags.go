4b881569c3fab112a60cba1b09df683ce8c61271
Alexandr Borodulin <sashaborodulin@gmail.com>
2021-10-09 04:39:00 +0300

    implement-reading-the-signature-private-key-from-a-file (#2798)
    Co-authored-by: Aleksandr Borodulin <a.borodulin@axioma.lv>

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -804,9 +804,30 @@ func setEtherbase(ctx *cli.Context, cfg *ethconfig.Config) {
 		}
 	}
 
-	if etherbase == "" && ctx.GlobalString(ChainFlag.Name) == params.DevChainName {
-		cfg.Miner.SigKey = core.DevnetSignPrivateKey
-		cfg.Miner.Etherbase = core.DevnetEtherbase
+	setSigKey := func(ctx *cli.Context, cfg *ethconfig.Config) {
+		if ctx.GlobalIsSet(MinerSigningKeyFileFlag.Name) {
+			signingKeyFileName := ctx.GlobalString(MinerSigningKeyFileFlag.Name)
+			key, err := crypto.LoadECDSA(signingKeyFileName)
+			if err != nil {
+				panic(err)
+			}
+			cfg.Miner.SigKey = key
+		}
+	}
+
+	if ctx.GlobalString(ChainFlag.Name) == params.DevChainName {
+		if etherbase == "" {
+			cfg.Miner.SigKey = core.DevnetSignPrivateKey
+			cfg.Miner.Etherbase = core.DevnetEtherbase
+		}
+		setSigKey(ctx, cfg)
+	}
+
+	if ctx.GlobalString(ChainFlag.Name) == params.FermionChainName {
+		if ctx.GlobalIsSet(MiningEnabledFlag.Name) && !ctx.GlobalIsSet(MinerSigningKeyFileFlag.Name) {
+			panic(fmt.Sprintf("Flag --%s is required in %s chain with --%s flag", MinerSigningKeyFileFlag.Name, params.FermionChainName, MiningEnabledFlag.Name))
+		}
+		setSigKey(ctx, cfg)
 	}
 }
 
