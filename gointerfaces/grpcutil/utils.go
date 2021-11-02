3fb38f2547def6c4d4685eb752b92921885fda5b
Alex Sharov <AskAlexSharov@gmail.com>
2021-11-02 22:01:19 +0700

    increase grpc meassage limit (#143)

--- gointerfaces/grpcutil/utils.go
+++ gointerfaces/grpcutil/utils.go
@@ -92,7 +92,7 @@ func Connect(creds credentials.TransportCredentials, dialAddress string) (*grpc.
 	backoffCfg.MaxDelay = 10 * time.Second
 	dialOpts = []grpc.DialOption{
 		grpc.WithConnectParams(grpc.ConnectParams{Backoff: backoffCfg, MinConnectTimeout: 10 * time.Minute}),
-		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(int(15 * datasize.MB))),
+		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(int(200 * datasize.MB))),
 		grpc.WithKeepaliveParams(keepalive.ClientParameters{}),
 	}
 	if creds == nil {
