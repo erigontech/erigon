84dde1e12ef342064d0e61967c6cbbda9e98931d
alex.sharov <AskAlexSharov@gmail.com>
2021-11-08 15:44:18 +0700

    save

--- cmd/downloader/seeder/main.go
+++ cmd/downloader/seeder/main.go
@@ -1,7 +1,150 @@
 package main
 
-import "github.com/ledgerwatch/erigon/cmd/downloader/seeder/commands"
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	lg "github.com/anacrolix/log"
+	"github.com/anacrolix/torrent"
+	"github.com/anacrolix/torrent/bencode"
+	"github.com/anacrolix/torrent/metainfo"
+	libcommon "github.com/ledgerwatch/erigon-lib/common"
+	"github.com/ledgerwatch/erigon/cmd/utils"
+	"github.com/ledgerwatch/erigon/common"
+	"github.com/ledgerwatch/erigon/internal/debug"
+	trnt "github.com/ledgerwatch/erigon/turbo/snapshotsync"
+	"github.com/ledgerwatch/log/v3"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	utils.CobraFlags(rootCmd, append(debug.Flags, utils.MetricFlags...))
+}
 
 func main() {
-	commands.Execute()
+	ctx, cancel := utils.RootContext()
+	defer cancel()
+
+	if err := rootCmd.ExecuteContext(ctx); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+var rootCmd = &cobra.Command{
+	Use:   "seed",
+	Short: "seed snapshot",
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		if err := debug.SetupCobra(cmd); err != nil {
+			panic(err)
+		}
+	},
+	PersistentPostRun: func(cmd *cobra.Command, args []string) {
+		debug.Exit()
+	},
+	Args:       cobra.ExactArgs(1),
+	ArgAliases: []string{"snapshots dir"},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return Seed(cmd.Context(), args[0])
+	},
+}
+
+func Seed(ctx context.Context, datadir string) error {
+	defer func() {
+		//hack origin lib don't have proper close handling
+		time.Sleep(time.Second * 5)
+	}()
+	datadir = filepath.Dir(datadir)
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	cfg := trnt.DefaultTorrentConfig()
+	cfg.NoDHT = false
+	cfg.DisableTrackers = false
+	cfg.Seed = true
+	cfg.Debug = false
+	cfg.Logger = cfg.Logger.FilterLevel(lg.Info)
+	cfg.DataDir = datadir
+
+	pathes := []string{
+		cfg.DataDir + "/headers",
+		cfg.DataDir + "/bodies",
+		cfg.DataDir + "/state",
+	}
+
+	cl, err := torrent.NewClient(cfg)
+	if err != nil {
+		return err
+	}
+	defer cl.Close()
+
+	torrents := make([]*torrent.Torrent, len(pathes))
+	for i, v := range pathes {
+		i := i
+		mi := &metainfo.MetaInfo{
+			CreationDate: time.Now().Unix(),
+			CreatedBy:    "erigon",
+			AnnounceList: trnt.Trackers,
+		}
+
+		if _, err := os.Stat(v); os.IsNotExist(err) {
+			fmt.Println(err)
+			continue
+		} else if err != nil {
+			return err
+		}
+		tt := time.Now()
+		if common.IsCanceled(ctx) {
+			return libcommon.ErrStopped
+		}
+		info, err := trnt.BuildInfoBytesForSnapshot(v, trnt.MdbxFilename)
+		if err != nil {
+			return err
+		}
+
+		mi.InfoBytes, err = bencode.Marshal(info)
+		if err != nil {
+			return err
+		}
+
+		torrents[i], _, err = cl.AddTorrentSpec(&torrent.TorrentSpec{
+			Trackers:  trnt.Trackers,
+			InfoHash:  mi.HashInfoBytes(),
+			InfoBytes: mi.InfoBytes,
+			ChunkSize: trnt.DefaultChunkSize,
+		})
+		if err != nil {
+			return err
+		}
+
+		log.Info("Torrent added", "name", torrents[i].Info().Name, "path", v, "t", time.Since(tt))
+
+		if !torrents[i].Seeding() {
+			log.Warn(torrents[i].Name() + " not seeding")
+		}
+
+		if common.IsCanceled(ctx) {
+			return libcommon.ErrStopped
+		}
+	}
+
+	go func() {
+		ticker := time.NewTicker(10 * time.Second)
+		for range ticker.C {
+			for _, t := range cl.Torrents() {
+				log.Info("Snapshot stats", "snapshot", t.Name(), "active peers", t.Stats().ActivePeers, "seeding", t.Seeding(), "hash", t.Metainfo().HashInfoBytes().String())
+			}
+
+			if common.IsCanceled(ctx) {
+				ticker.Stop()
+				return
+			}
+		}
+	}()
+
+	<-ctx.Done()
+	return nil
 }
