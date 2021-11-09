f87c87ed45d225f88d3936c6500d2de75c11fad3
alex.sharov <AskAlexSharov@gmail.com>
2021-11-09 16:11:56 +0700

    save

--- cmd/hack/hack.go
+++ cmd/hack/hack.go
@@ -65,7 +65,7 @@ import (
 	"github.com/wcharczuk/go-chart/v2"
 )
 
-const ASSERT = true
+const ASSERT = false
 
 var (
 	verbosity  = flag.Uint("verbosity", 3, "Logging verbosity: 0=silent, 1=error, 2=warn, 3=info, 4=debug, 5=detail (default 3)")
