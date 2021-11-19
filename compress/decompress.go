bb1d712834f01b434a6b84abf5adb554936a7938
Alex Sharov <AskAlexSharov@gmail.com>
2021-11-19 22:00:55 +0700

    Hack: dump bodies and headers (#177)
    * save
    
    * save
    
    * save
    
    * save

--- compress/decompress.go
+++ compress/decompress.go
@@ -185,6 +185,18 @@ func (g *Getter) nextPattern() []byte {
 	return g.pattern()
 }
 
+func (d *Decompressor) Count() int {
+	//TODO: likely it can be optimized by don't decompress values, or even by storing count in file
+	counterGetter := d.MakeGetter()
+	word := make([]byte, 0, 64)
+	count := 0
+	for counterGetter.HasNext() {
+		word, _ = counterGetter.Next(word[:0])
+		count++
+	}
+	return count
+}
+
 // MakeGetter creates an object that can be used to access words in the decompressor's file
 // Getter is not thread-safe, but there can be multiple getters used simultaneously and concrently
 // for the same decompressor
--- recsplit/recsplit.go
+++ recsplit/recsplit.go
@@ -117,6 +117,11 @@ type RecSplitArgs struct {
 func NewRecSplit(args RecSplitArgs) (*RecSplit, error) {
 	bucketCount := (args.KeyCount + args.BucketSize - 1) / args.BucketSize
 	rs := &RecSplit{bucketSize: args.BucketSize, keyExpectedCount: uint64(args.KeyCount), bucketCount: uint64(bucketCount)}
+	if len(args.StartSeed) == 0 {
+		args.StartSeed = []uint64{0x106393c187cae21a, 0x6453cec3f7376937, 0x643e521ddbd2be98, 0x3740c6412f6572cb, 0x717d47562f1ce470, 0x4cd6eb4c63befb7c, 0x9bfd8c5e18c8da73,
+			0x082f20e10092a9a3, 0x2ada2ce68d21defc, 0xe33cb4f3e7c6466b, 0x3980be458c509c59, 0xc466fd9584828e8c, 0x45f0aabe1a61ede6, 0xf6e7b8b33ad9b98d,
+			0x4ef95e25f4b4983d, 0x81175195173b92d3, 0x4e50927d8dd15978, 0x1ea2099d1fafae7f, 0x425c8a06fbaaa815, 0xcd4216006c74052a}
+	}
 	rs.salt = args.Salt
 	rs.hasher = murmur3.New128WithSeed(rs.salt)
 	rs.tmpDir = args.TmpDir
@@ -495,8 +500,10 @@ func (rs *RecSplit) Build() error {
 	if rs.indexF, err = os.Create(rs.indexFile); err != nil {
 		return fmt.Errorf("create index file %s: %w", rs.indexFile, err)
 	}
+	defer rs.indexF.Sync()
 	defer rs.indexF.Close()
 	rs.indexW = bufio.NewWriterSize(rs.indexF, etl.BufIOSize)
+	defer rs.indexW.Flush()
 	// Write number of keys
 	binary.BigEndian.PutUint64(rs.numBuf[:], rs.keysAdded)
 	if _, err = rs.indexW.Write(rs.numBuf[:]); err != nil {
@@ -594,9 +601,6 @@ func (rs *RecSplit) Build() error {
 	// Write out elias fano
 	if err := rs.ef.Write(rs.indexW); err != nil {
 		return fmt.Errorf("writing elias fano: %w", err)
-	}
-	if err := rs.indexW.Flush(); err != nil {
-		return err
 	}
 	return nil
 }
