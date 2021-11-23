ab2874bc7c7f5e5e2aa6b5391c12957457f796eb
leonardchinonso <leokingluthers@gmail.com>
2021-11-23 10:31:32 +0100

    Committing changes for devnettest

---
+++ cmd/devnettest/requests/requests.go
@@ -0,0 +1,31 @@
+package requests
+
+import (
+	"crypto/ecdsa"
+	"fmt"
+	"github.com/ledgerwatch/erigon/cmd/rpctest/rpctest"
+	"github.com/ledgerwatch/erigon/common"
+)
+
+func GetBalance(address common.Address, blockNum string) {
+	reqGen := initialiseRequestGenerator()
+	var b rpctest.EthBalance
+
+	res := reqGen.Erigon("eth_getBalance", reqGen.getBalance(address, blockNum), &b)
+	if res.Err != nil {
+		fmt.Printf("Error getting balance: %v\n", res.Err)
+		return
+	}
+
+	fmt.Printf("Balance is: %v\n", b.Balance.ToInt())
+}
+
+func SendTx(from *ecdsa.PrivateKey, to common.Address, value uint64) {
+	fmt.Printf("from is: %v", from)
+	fmt.Printf("to is: %v", to)
+	fmt.Printf("value is: %v", value)
+}
+
+func TxpoolContent() string {
+	return ""
+}
