0ebb28d45f1444448b8fbd81e2dbde50ef3da83b
Dmitry Savelev <darksawa@gmail.com>
2021-11-29 21:22:18 +0300

    replace magic value with a constant.

--- cmd/rpcdaemon/commands/erigon_nodeInfo.go
+++ cmd/rpcdaemon/commands/erigon_nodeInfo.go
@@ -6,6 +6,11 @@ import (
 	"github.com/ledgerwatch/erigon/p2p"
 )
 
+const (
+	// allNodesInfo used in NodeInfo request to receive meta data from all running sentries.
+	allNodesInfo = 0
+)
+
 func (api *ErigonImpl) NodeInfo(ctx context.Context) ([]p2p.NodeInfo, error) {
-	return api.ethBackend.NodeInfo(ctx, 0)
+	return api.ethBackend.NodeInfo(ctx, allNodesInfo)
 }
