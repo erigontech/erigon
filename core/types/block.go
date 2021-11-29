d34d8d6c401d563b18f13a89aee2c353bc2fbb05
TBC Dev <tbc-dev@protonmail.com>
2021-11-29 18:56:04 +0800

    Fix error messages for Random in DecodeRLP()

--- core/types/block.go
+++ core/types/block.go
@@ -558,10 +558,10 @@ func (h *Header) DecodeRLP(s *rlp.Stream) error {
 				}
 				return nil
 			}
-			return fmt.Errorf("read BaseFee: %w", err)
+			return fmt.Errorf("read Random: %w", err)
 		}
 		if len(b) != common.HashLength {
-			return fmt.Errorf("wrong size for BaseFee: %d", len(b))
+			return fmt.Errorf("wrong size for Random: %d", len(b))
 		}
 		h.Eip3675 = true
 		h.Random = common.BytesToHash(b)
