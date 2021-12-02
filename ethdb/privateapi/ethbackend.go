f6475c8b2df581403ce4dc97c5d2410ce4cdbe9c
ledgerwatch <akhounov@gmail.com>
2021-12-02 12:38:04 +0000

    Bump ethBackend interface version

--- ethdb/privateapi/ethbackend.go
+++ ethdb/privateapi/ethbackend.go
@@ -19,7 +19,7 @@ import (
 // 2.0.0 - move all mining-related methods to 'txpool/mining' server
 // 2.1.0 - add NetPeerCount function
 // 2.2.0 - add NodesInfo function
-var EthBackendAPIVersion = &types2.VersionReply{Major: 2, Minor: 1, Patch: 0}
+var EthBackendAPIVersion = &types2.VersionReply{Major: 2, Minor: 2, Patch: 0}
 
 type EthBackendServer struct {
 	remote.UnimplementedETHBACKENDServer // must be embedded to have forward compatible implementations.
