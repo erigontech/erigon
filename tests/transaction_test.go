ab52609358a69b9c338649a8d91fd12299d26f42
yperbasis <andrey.ashikhmin@gmail.com>
2021-12-03 19:43:06 +0100

    Clean up test runners. Don't run legacy tests

--- tests/transaction_test.go
+++ tests/transaction_test.go
@@ -32,19 +32,11 @@ func TestTransaction(t *testing.T) {
 	// This is a pseudo-consensus vulnerability, but not in practice
 	// because of the gas limit
 	txt.skipLoad("^ttGasLimit/TransactionWithGasLimitxPriceOverflow.json")
-	// We _do_ allow more than uint64 in gas price, as opposed to the tests
-	// This is also not a concern, as long as tx.Cost() uses big.Int for
-	// calculating the final cozt
-	txt.skipLoad(".*TransactionWithGasPriceOverflow.*")
 
 	// The nonce is too large for uint64. Not a concern, it means geth won't
 	// accept transactions at a certain point in the distant future
 	txt.skipLoad("^ttNonce/TransactionWithHighNonce256.json")
 
-	// The value is larger than uint64, which according to the test is invalid.
-	// Geth accepts it, which is not a consensus issue since we use big.Int's
-	// internally to calculate the cost
-	txt.skipLoad("^ttValue/TransactionWithHighValueOverflow.json")
 	txt.walk(t, transactionTestDir, func(t *testing.T, name string, test *TransactionTest) {
 		cfg := params.MainnetChainConfig
 		if err := txt.checkFailure(t, test.Run(cfg)); err != nil {
