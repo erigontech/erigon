15815b2a621448f06e67fd0b2f1d02f487569a8d
alex.sharov <AskAlexSharov@gmail.com>
2021-12-04 09:10:40 +0700

    save

--- cmd/hack/hack.go
+++ cmd/hack/hack.go
@@ -2668,9 +2668,7 @@ func checkBlockSnapshot(chaindata string) error {
 	}
 	defer tx.Rollback()
 
-	//snBlockReader.HeaderByNumber(context.Background(), tx, 499993)
-	//return nil
-	for i := uint64(499990); i < snapshots.BlocksAvailable(); i++ {
+	for i := uint64(0); i < snapshots.BlocksAvailable(); i++ {
 		hash, err := rawdb.ReadCanonicalHash(tx, i)
 		if err != nil {
 			return err
