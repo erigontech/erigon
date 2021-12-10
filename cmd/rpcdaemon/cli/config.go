60d8d6b232ef3d0a5248fde3b3609bc95e03feb8
Alexey Sharp <alexeysharp@Alexeys-iMac.local>
2021-12-10 08:20:36 +0000

    Fixup

--- cmd/rpcdaemon/cli/config.go
+++ cmd/rpcdaemon/cli/config.go
@@ -56,6 +56,7 @@ type Flags struct {
 	RpcAllowListFilePath   string
 	RpcBatchConcurrency    uint
 	TraceCompatibility     bool // Bug for bug compatibility for trace_ routines with OpenEthereum
+	TxPoolV2               bool
 	TxPoolApiAddr          string
 	TevmEnabled            bool
 	StateCache             kvcache.CoherentConfig
