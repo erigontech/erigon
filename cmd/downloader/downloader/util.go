e5902a21e408f22ebc841e80f8618f2226d785e3
alex.sharov <AskAlexSharov@gmail.com>
2021-12-14 10:53:49 +0700

    save

--- cmd/downloader/downloader/util.go
+++ cmd/downloader/downloader/util.go
@@ -24,7 +24,7 @@ const DefaultPieceSize = 2 * 1024 * 1024
 
 // Trackers - break down by priority tier
 var Trackers = [][]string{
-	trackers.Best, trackers.Ws, trackers.Udp, trackers.Https, trackers.Http,
+	trackers.Best, trackers.Ws, // trackers.Udp, trackers.Https, trackers.Http,
 }
 
 func allTorrentFiles(dir string) ([]string, error) {
