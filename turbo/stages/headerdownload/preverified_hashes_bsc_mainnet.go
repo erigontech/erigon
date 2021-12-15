8f1c631e81072686626248c53f2a27a21d3f7ca1
Brendan Kelly <brendanhkelly@gmail.com>
2021-12-15 13:12:38 -0800

    Remove BSC hashes for now

--- turbo/stages/headerdownload/preverified_hashes_bsc_mainnet.go
+++
@@ -1,8 +0,0 @@
-package headerdownload
-
-var bscMainnetPreverifiedHashes = []string{
-	"0d21840abff46b96c84b2ac9e10e4f5cdaeb5693cb665db62a2f3b02d2d57b5b",
-	"04055304e432294a65ff31069c4d3092ff8b58f009cdb50eba5351e0332ad0f6",
-}
-
-const bscMainnetPreverifiedHeight uint64 = 1
