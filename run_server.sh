65b4a99f340677cfb44bc3c448ac9c4857d8ba86
Kairat Abylkasymov <racytech0x0@gmail.com>
2021-12-15 14:55:28 -0500

    allow-control-access-origin

--- run_server.sh
+++ run_server.sh
@@ -10,3 +10,7 @@ ENTRY_FOLDER=$BASE/tx_analysis/server
 go build -o $BIN_DIR/$EXECUTABLE $ENTRY_FOLDER/...
 
 $BIN_DIR/$EXECUTABLE
+
+
+# ./build/bin/erigon --datadir=/mnt/mx500_0/goerli --chain goerli --private.api.addr=localhost:9090
+# ./build/bin/rpcdaemon --private.api.addr=localhost:9090 --http.api=eth,erigon,web3,net,debug,trace,txpool --verbosity=4 --datadir /mnt/mx500_0/goerli --ws
