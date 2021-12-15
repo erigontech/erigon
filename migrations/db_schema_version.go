9f861e1b3fe4bf9c5d9b1b643d51dfc17874bcda
ledgerwatch <akhounov@gmail.com>
2021-12-15 21:03:04 +0000

    Check upgrade/downgrade of database schema version to prevent accidental upgrade, remove migrations (#3133)
    * Check upgrade/downgrade
    
    * Remove old migrations
    
    * Fix compilation
    
    * Fix lint
    
    Co-authored-by: Alexey Sharp <alexeysharp@Alexeys-iMac.local>
    Co-authored-by: Alex Sharp <alexsharp@Alexs-MacBook-Pro.local>

--- migrations/db_schema_version.go
+++ migrations/db_schema_version.go
@@ -6,23 +6,6 @@ import (
 	"github.com/ledgerwatch/erigon-lib/kv"
 )
 
-var dbSchemaVersion = Migration{
-	Name: "db_schema_version",
-	Up: func(db kv.RwDB, tmpdir string, progress []byte, BeforeCommit Callback) (err error) {
-		tx, err := db.BeginRw(context.Background())
-		if err != nil {
-			return err
-		}
-		defer tx.Rollback()
-
-		// This migration is no-op, but it forces the migration mechanism to apply it and thus write the DB schema version info
-		if err := BeforeCommit(tx, nil, true); err != nil {
-			return err
-		}
-		return tx.Commit()
-	},
-}
-
 var dbSchemaVersion5 = Migration{
 	Name: "db_schema_version5",
 	Up: func(db kv.RwDB, tmpdir string, progress []byte, BeforeCommit Callback) (err error) {
