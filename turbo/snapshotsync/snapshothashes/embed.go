2397f5d5422cce7a91f0dc830ff50ff852dbc763
alex.sharov <AskAlexSharov@gmail.com>
2021-12-17 09:36:32 +0700

    Revert "Revert "count in .seg""
    This reverts commit 59c2e88fec72dbb49de4c25c00ae796518ffbc00.

--- turbo/snapshotsync/snapshothashes/embed.go
+++ turbo/snapshotsync/snapshothashes/embed.go
@@ -3,6 +3,9 @@ package snapshothashes
 import (
 	_ "embed"
 	"encoding/json"
+	"path/filepath"
+	"strconv"
+	"strings"
 
 	"github.com/ledgerwatch/erigon/params/networkname"
 )
@@ -25,12 +28,46 @@ func fromJson(in []byte) (out Preverified) {
 }
 
 var (
-	MainnetChainSnapshotConfig = &Config{}
-	GoerliChainSnapshotConfig  = &Config{
-		ExpectBlocks: 5_900_000 - 1,
-		Preverified:  Goerli,
-	}
+	MainnetChainSnapshotConfig = newConfig(Mainnet)
+	GoerliChainSnapshotConfig  = newConfig(Goerli)
 )
+
+func newConfig(preverified Preverified) *Config {
+	return &Config{
+		ExpectBlocks: maxBlockNum(preverified),
+		Preverified:  preverified,
+	}
+}
+
+func maxBlockNum(preverified Preverified) uint64 {
+	max := uint64(0)
+	for name := range preverified {
+		_, fileName := filepath.Split(name)
+		ext := filepath.Ext(fileName)
+		if ext != ".seg" {
+			continue
+		}
+		onlyName := fileName[:len(fileName)-len(ext)]
+		parts := strings.Split(onlyName, "-")
+		if parts[0] != "v1" {
+			panic("not implemented")
+		}
+		if parts[3] != "headers" {
+			continue
+		}
+		to, err := strconv.ParseUint(parts[2], 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		if max < to {
+			max = to
+		}
+	}
+	if max == 0 { // to prevent underflow
+		return 0
+	}
+	return max*1_000 - 1
+}
 
 type Config struct {
 	ExpectBlocks uint64
