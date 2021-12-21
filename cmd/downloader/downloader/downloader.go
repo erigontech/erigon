b143a7db77c8623481e7adff1a222ca90a412e07
alex.sharov <AskAlexSharov@gmail.com>
2021-12-21 12:33:27 +0700

    save

--- cmd/downloader/downloader/downloader.go
+++ cmd/downloader/downloader/downloader.go
@@ -77,7 +77,7 @@ func DefaultTorrentConfig() *torrent.ClientConfig {
 	torrentConfig.TorrentPeersHighWater = 100     // default: 500
 	torrentConfig.TorrentPeersLowWater = 50       // default: 50
 
-	torrentConfig.UploadRateLimiter = rate.NewLimiter(rate.Limit(64*DefaultPieceSize), 0) // default: unlimited
+	torrentConfig.UploadRateLimiter = rate.NewLimiter(rate.Limit(32*1024*1024), 0) // default: unlimited
 	//torrentConfig.DownloadRateLimiter = rate.NewLimiter(rate.Limit(256*datasize.MB), 0) // default: unlimited
 
 	return torrentConfig
