c5fcc83c09810412db8074c120128a80dbee967f
alex.sharov <AskAlexSharov@gmail.com>
2021-12-21 11:47:17 +0700

    save

--- cmd/downloader/downloader/downloader.go
+++ cmd/downloader/downloader/downloader.go
@@ -10,6 +10,7 @@ import (
 	lg "github.com/anacrolix/log"
 	"github.com/anacrolix/torrent"
 	"github.com/anacrolix/torrent/metainfo"
+	"github.com/anacrolix/torrent/peer_protocol"
 	"github.com/anacrolix/torrent/storage"
 	"github.com/dustin/go-humanize"
 	"github.com/ledgerwatch/erigon-lib/kv"
@@ -68,7 +69,7 @@ func DefaultTorrentConfig() *torrent.ClientConfig {
 	torrentConfig.NominalDialTimeout = 20 * time.Second // default: 20sec
 	torrentConfig.HandshakesTimeout = 8 * time.Second   // default: 4sec
 
-	//torrentConfig.MinPeerExtensions.SetBit(peer_protocol.ExtensionBitFast, true)
+	torrentConfig.MinPeerExtensions.SetBit(peer_protocol.ExtensionBitFast, true)
 	return torrentConfig
 }
 
@@ -94,7 +95,8 @@ func MainLoop(ctx context.Context, torrentClient *torrent.Client) {
 	logEvery := time.NewTicker(interval)
 	defer logEvery.Stop()
 	for {
-		var prevBytesReadUsefulData, aggByteRate int64
+		var stats aggStats
+		//var prevBytesReadUsefulData, aggByteRate int64
 		select {
 		case <-ctx.Done():
 			return
@@ -123,55 +125,18 @@ func MainLoop(ctx context.Context, torrentClient *torrent.Client) {
 						peers[peer.PeerID] = struct{}{}
 					}
 				}
+
 				log.Info("[torrent] Seeding", "peers", len(peers), "torrents", len(torrents))
 				continue
 			}
 
-			var aggBytesCompleted, aggLen int64
-			//var aggCompletedPieces, aggNumPieces, aggPartialPieces int
-			peers := map[torrent.PeerID]*torrent.PeerConn{}
-
-			for _, t := range torrents {
-				stats := t.Stats()
-				/*
-					var completedPieces, partialPieces int
-					psrs := t.PieceStateRuns()
-					for _, r := range psrs {
-						if r.Complete {
-							completedPieces += r.Length
-						}
-						if r.Partial {
-							partialPieces += r.Length
-						}
-					}
-					aggCompletedPieces += completedPieces
-					aggPartialPieces += partialPieces
-					aggNumPieces = t.NumPieces()
-				*/
-
-				byteRate := int64(time.Second)
-				bytesReadUsefulData := stats.BytesReadUsefulData.Int64()
-				byteRate *= stats.BytesReadUsefulData.Int64() - prevBytesReadUsefulData
-				byteRate /= int64(interval)
-				aggByteRate += byteRate
-
-				prevBytesReadUsefulData = bytesReadUsefulData
-				aggBytesCompleted += t.BytesCompleted()
-				aggLen += t.Length()
-
-				for _, peer := range t.PeerConns() {
-					peers[peer.PeerID] = peer
-				}
-
-			}
+			stats = calcStats(stats, interval, torrentClient)
 
 			line := fmt.Sprintf(
 				"[torrent] Downloading: %d%%, %v/s, peers: %d",
-				int(100*(float64(aggBytesCompleted)/float64(aggLen))),
-				//humanize.Bytes(uint64(aggBytesCompleted)),
-				//	humanize.Bytes(uint64(aggLen)),
-				humanize.Bytes(uint64(aggByteRate)),
-				len(peers),
+				stats.progress,
+				humanize.Bytes(uint64(stats.readBytesPerSec)),
+				stats.peersCount,
 			)
 			log.Info(line)
 		}
@@ -187,6 +152,58 @@ func (cli *Client) StopSeeding(hash metainfo.Hash) error {
 	t.Drop()
 	<-ch
 	return nil
+}
+
+type aggStats struct {
+	readBytesPerSec  int64
+	writeBytesPerSec int64
+	peersCount       int64
+
+	progress int
+
+	bytesRead    int64
+	bytesWritten int64
+}
+
+func calcStats(prevStats aggStats, interval time.Duration, client *torrent.Client) (result aggStats) {
+	var aggBytesCompleted, aggLen int64
+	//var aggCompletedPieces, aggNumPieces, aggPartialPieces int
+	peers := map[torrent.PeerID]*torrent.PeerConn{}
+	for _, t := range client.Torrents() {
+		stats := t.Stats()
+
+		/*
+			var completedPieces, partialPieces int
+			psrs := t.PieceStateRuns()
+			for _, r := range psrs {
+				if r.Complete {
+					completedPieces += r.Length
+				}
+				if r.Partial {
+					partialPieces += r.Length
+				}
+			}
+			aggCompletedPieces += completedPieces
+			aggPartialPieces += partialPieces
+			aggNumPieces = t.NumPieces()
+		*/
+
+		result.bytesRead = stats.BytesReadUsefulData.Int64()
+		result.readBytesPerSec = (result.bytesRead - prevStats.bytesRead) / int64(interval)
+
+		result.bytesWritten = stats.BytesWritten.Int64()
+		result.writeBytesPerSec = (result.bytesWritten - prevStats.bytesWritten) / int64(interval)
+
+		aggBytesCompleted += t.BytesCompleted()
+		aggLen += t.Length()
+
+		for _, peer := range t.PeerConns() {
+			peers[peer.PeerID] = peer
+		}
+	}
+	result.progress = int(100 * (float64(aggBytesCompleted) / float64(aggLen)))
+	result.peersCount = int64(len(peers))
+	return result
 }
 
 // AddTorrentFiles - adding .torrent files to torrentClient (and checking their hashes), if .torrent file
