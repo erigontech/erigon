f7853e915d40320237ae8db576854911b9c2849f
alex.sharov <AskAlexSharov@gmail.com>
2022-01-06 11:17:21 +0700

    save

--- eth/stagedsync/stage_headers.go
+++ eth/stagedsync/stage_headers.go
@@ -68,6 +68,9 @@ func StageHeadersCfg(
 	blockReader interfaces.FullBlockReader,
 	tmpdir string,
 ) HeadersCfg {
+	if blockReader == nil {
+		panic(1)
+	}
 	return HeadersCfg{
 		db:                 db,
 		hd:                 headerDownload,
