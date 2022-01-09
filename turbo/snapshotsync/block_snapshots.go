ac93d5e6cf2f3e2c91467c09f49661cc2d63c44c
alex.sharov <AskAlexSharov@gmail.com>
2022-01-09 11:58:08 +0700

    idx logs

--- turbo/snapshotsync/block_snapshots.go
+++ turbo/snapshotsync/block_snapshots.go
@@ -309,8 +309,9 @@ func (s *AllSnapshots) BuildIndices(ctx context.Context, chainID uint256.Int, tm
 		if err := HeadersHashIdx(f, sn.From, tmpDir); err != nil {
 			return err
 		}
-
-		f = path.Join(s.dir, SegmentFileName(sn.From, sn.To, Bodies))
+	}
+	for _, sn := range s.blocks {
+		f := path.Join(s.dir, SegmentFileName(sn.From, sn.To, Bodies))
 		if err := BodiesIdx(f, sn.From, tmpDir); err != nil {
 			return err
 		}
@@ -738,7 +739,7 @@ func DumpBodies(db kv.RoDB, tmpdir string, fromBlock uint64, blocksAmount int) e
 func TransactionsHashIdx(chainID uint256.Int, sn *BlocksSnapshot, firstTxID, firstBlockNum uint64, segmentFilePath string, expectedCount uint64, tmpDir string) error {
 	logEvery := time.NewTicker(20 * time.Second)
 	defer logEvery.Stop()
-	dir, fileName := filepath.Split(segmentFilePath)
+	dir, _ := filepath.Split(segmentFilePath)
 
 	parseCtx := txpool.NewTxParseContext(chainID)
 	parseCtx.WithSender(false)
@@ -751,7 +752,6 @@ func TransactionsHashIdx(chainID uint256.Int, sn *BlocksSnapshot, firstTxID, fir
 		return err
 	}
 	defer d.Close()
-	total := uint64(d.Count())
 
 	buf := make([]byte, 1024)
 
@@ -800,7 +800,7 @@ RETRY:
 			return err
 		}
 
-		for firstTxID+i > body.BaseTxId+uint64(body.TxAmount) { // read next
+		for firstTxID+i > body.BaseTxId+uint64(body.TxAmount) { // skip empty blocks
 			buf, _ = bodyGetter.Next(buf[:0])
 			if err := rlp.DecodeBytes(buf, body); err != nil {
 				return err
@@ -815,7 +815,7 @@ RETRY:
 		select {
 		default:
 		case <-logEvery.C:
-			log.Info(fmt.Sprintf("[Snapshots Indexing] TransactionsHashIdx: %s, %s", percent(i, total), fileName))
+			log.Info("[Snapshots Indexing] TransactionsHashIdx", "blockNum", blockNum)
 		}
 		j++
 		return nil
@@ -853,14 +853,12 @@ RETRY:
 func HeadersHashIdx(segmentFilePath string, firstBlockNumInSegment uint64, tmpDir string) error {
 	logEvery := time.NewTicker(5 * time.Second)
 	defer logEvery.Stop()
-	_, fileName := filepath.Split(segmentFilePath)
 
 	d, err := compress.NewDecompressor(segmentFilePath)
 	if err != nil {
 		return err
 	}
 	defer d.Close()
-	total := uint64(d.Count())
 
 	if err := Idx(d, firstBlockNumInSegment, tmpDir, func(idx *recsplit.RecSplit, i, offset uint64, word []byte) error {
 		h := types.Header{}
@@ -875,7 +873,7 @@ func HeadersHashIdx(segmentFilePath string, firstBlockNumInSegment uint64, tmpDi
 		select {
 		default:
 		case <-logEvery.C:
-			log.Info(fmt.Sprintf("[Snapshots Indexing] HeadersHashIdx: %s, %s", percent(i, total), fileName))
+			log.Info("[Snapshots Indexing] HeadersHashIdx", "blockNumber", h.Number.Uint64())
 		}
 		return nil
 	}); err != nil {
@@ -887,7 +885,6 @@ func HeadersHashIdx(segmentFilePath string, firstBlockNumInSegment uint64, tmpDi
 func BodiesIdx(segmentFilePath string, firstBlockNumInSegment uint64, tmpDir string) error {
 	logEvery := time.NewTicker(5 * time.Second)
 	defer logEvery.Stop()
-	_, fileName := filepath.Split(segmentFilePath)
 	num := make([]byte, 8)
 
 	d, err := compress.NewDecompressor(segmentFilePath)
@@ -895,7 +892,6 @@ func BodiesIdx(segmentFilePath string, firstBlockNumInSegment uint64, tmpDir str
 		return err
 	}
 	defer d.Close()
-	total := uint64(d.Count())
 
 	if err := Idx(d, firstBlockNumInSegment, tmpDir, func(idx *recsplit.RecSplit, i, offset uint64, word []byte) error {
 		n := binary.PutUvarint(num, i)
@@ -906,7 +902,7 @@ func BodiesIdx(segmentFilePath string, firstBlockNumInSegment uint64, tmpDir str
 		select {
 		default:
 		case <-logEvery.C:
-			log.Info(fmt.Sprintf("[Snapshots Indexing] BodyNumberIdx: %s, %s", percent(i, total), fileName))
+			log.Info("[Snapshots Indexing] BodyNumberIdx", "blockNumber", firstBlockNumInSegment+i)
 		}
 		return nil
 	}); err != nil {
