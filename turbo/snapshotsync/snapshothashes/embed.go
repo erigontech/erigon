957739f518db6584553429170e451869c94e333b
Alex Sharp <alexsharp@Alexs-MacBook-Pro.local>
2022-01-11 20:59:36 +0000

    Fix lint

--- turbo/snapshotsync/snapshothashes/embed.go
+++ turbo/snapshotsync/snapshothashes/embed.go
@@ -2,73 +2,35 @@ package snapshothashes
 
 import (
 	_ "embed"
-	"path/filepath"
-	"strconv"
-	"strings"
-
-	"github.com/pelletier/go-toml/v2"
+	"encoding/json"
 
 	"github.com/ledgerwatch/erigon/params/networkname"
 )
 
-////go:embed erigon-snapshots/mainnet.toml
+//go:embed erigon-snapshots/mainnet.json
 var mainnet []byte
-var Mainnet Preverified // = fromToml(mainnet)
+var Mainnet = fromJson(mainnet)
 
-////go:embed erigon-snapshots/goerli.toml
+//go:embed erigon-snapshots/goerli.json
 var goerli []byte
-var Goerli Preverified // = fromToml(goerli)
+var Goerli = fromJson(goerli)
 
 type Preverified map[string]string
 
-func fromToml(in []byte) (out Preverified) {
-	if err := toml.Unmarshal(in, &out); err != nil {
+func fromJson(in []byte) (out Preverified) {
+	if err := json.Unmarshal(in, &out); err != nil {
 		panic(err)
 	}
 	return out
 }
 
 var (
-	MainnetChainSnapshotConfig = newConfig(Mainnet)
-	GoerliChainSnapshotConfig  = newConfig(Goerli)
+	MainnetChainSnapshotConfig = &Config{}
+	GoerliChainSnapshotConfig  = &Config{
+		ExpectBlocks: 5_900_000 - 1,
+		Preverified:  Goerli,
+	}
 )
-
-func newConfig(preverified Preverified) *Config {
-	return &Config{
-		ExpectBlocks: maxBlockNum(preverified),
-		Preverified:  preverified,
-	}
-}
-
-func maxBlockNum(preverified Preverified) uint64 {
-	max := uint64(0)
-	for name := range preverified {
-		_, fileName := filepath.Split(name)
-		ext := filepath.Ext(fileName)
-		if ext != ".seg" {
-			continue
-		}
-		onlyName := fileName[:len(fileName)-len(ext)]
-		parts := strings.Split(onlyName, "-")
-		if parts[0] != "v1" {
-			panic("not implemented")
-		}
-		if parts[3] != "headers" {
-			continue
-		}
-		to, err := strconv.ParseUint(parts[2], 10, 64)
-		if err != nil {
-			panic(err)
-		}
-		if max < to {
-			max = to
-		}
-	}
-	if max == 0 { // to prevent underflow
-		return 0
-	}
-	return max*1_000 - 1
-}
 
 type Config struct {
 	ExpectBlocks uint64
