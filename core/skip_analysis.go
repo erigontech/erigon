c0db9295b8a0ed934279f2f823ba78ef3be1a7fc
ledgerwatch <akhounov@gmail.com>
2022-01-12 10:36:12 +0000

    Skip analysis and preverified hashe update (#3244)
    Co-authored-by: Alexey Sharp <alexeysharp@Alexeys-iMac.local>

--- core/skip_analysis.go
+++ core/skip_analysis.go
@@ -21,7 +21,7 @@ import (
 )
 
 // MainnetNotCheckedFrom is the first block number not yet checked for invalid jumps
-const MainnetNotCheckedFrom uint64 = 13804600
+const MainnetNotCheckedFrom uint64 = 13986250
 
 // SkipAnalysis function tells us whether we can skip performing jumpdest analysis
 // for the historical blocks (on mainnet now but perhaps on the testsnets
