d427d60d5650a70cf188aa29daba444108f2dd5f
Alexey Sharp <alexeysharp@Alexeys-iMac.local>
2022-01-14 15:22:55 +0000

    Readonly again

--- cmd/state/commands/erigon2.go
+++ cmd/state/commands/erigon2.go
@@ -60,7 +60,7 @@ func Erigon2(genesis *core.Genesis, logger log.Logger, blockNum uint64, datadir 
 		<-sigs
 		interruptCh <- true
 	}()
-	historyDb, err := kv2.NewMDBX(logger).Path(path.Join(datadir, "chaindata")).Open()
+	historyDb, err := kv2.NewMDBX(logger).Path(path.Join(datadir, "chaindata")).Readonly().Open()
 	if err != nil {
 		return fmt.Errorf("opening chaindata as read only: %v", err)
 	}
