69d3a188fc41e7311e4221806e7acb73631f056a
Igor Mandrigin <i@mandrigin.ru>
2022-01-18 10:48:48 +0100

    fix 2

--- cmd/starknet/services/raw_tx_generator_test.go
+++ cmd/starknet/services/raw_tx_generator_test.go
@@ -23,7 +23,7 @@ func TestCreate(t *testing.T) {
 	}{
 		{name: "invalid private key", privateKey: "abc", fileName: "not_exist.json", error: ErrInvalidPrivateKey},
 		{name: "contract file not found", privateKey: generatePrivateKey(t), fileName: "not_exist.json", error: ErrReadContract},
-		{name: "success", privateKey: privateKey, fileName: "contract_test.json", salt: "contract_address_salt", gas: 1, want: "03f88183127ed802830186a0830186a0018001963762323236313632363932323361323035623564376495636f6e74726163745f616464726573735f73616c74c001a0d76aefa12efeb7602858aba7889b5aa89eaca8e530979a9638e8b15b6c818a9aa07737ddf3e01ca1dfac1562eac7b1117a26ad3322621ddb6c4bee71352821698c"},
+		{name: "success", privateKey: privateKey, fileName: "contract_test.json", salt: "contract_address_salt", gas: 1, want: "03f88283127ed880830186a084342770c0018001963762323236313632363932323361323035623564376495636f6e74726163745f616464726573735f73616c74c001a0f4c886792b3b0c92789b18e80cc8587715b40c51b4d85c8c2106f536506f22aba02855010af44a167d4a0c1b3ab38e7757bc45a4c31693a785f35172e510fd77ad"},
 	}
 
 	fs := fstest.MapFS{
