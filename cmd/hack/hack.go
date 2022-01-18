9188705849ce0366b460a9580b36fde9dc391d7d
Alex Sharov <AskAlexSharov@gmail.com>
2022-01-18 15:01:36 +0700

    Switch to parallel (#3285)
    * save
    
    * save
    
    * save
    
    * save
    
    * save
    
    * save
    
    * save
    
    * save
    
    * save
    
    * save

--- cmd/hack/hack.go
+++ cmd/hack/hack.go
@@ -1415,10 +1415,11 @@ func genstate() error {
 }
 
 func compress1(fileName, segmentFileName string) error {
-	compressor, err := compress.NewCompressor2(context.Background(), "", segmentFileName, "", compress.MinPatternScore, runtime.GOMAXPROCS(-1))
+	compressor, err := compress.NewCompressor(context.Background(), "", segmentFileName, "", compress.MinPatternScore, runtime.GOMAXPROCS(-1))
 	if err != nil {
 		return err
 	}
+	defer compressor.Close()
 	if err := compress.ReadSimpleFile(fileName, func(v []byte) error {
 		return compressor.AddWord(v)
 	}); err != nil {
