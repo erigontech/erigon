302946c5e56cc7e95e34514dea9c3ec475817121
Alex Sharov <AskAlexSharov@gmail.com>
2022-01-19 10:48:58 +0700

    Move cli root context to erigon-lib (#251)
    * save
    
    * save

---
+++ common/cli.go
@@ -0,0 +1,46 @@
+/*
+   Copyright 2021 Erigon contributors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package common
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/ledgerwatch/log/v3"
+)
+
+func RootContext() (context.Context, context.CancelFunc) {
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		defer cancel()
+
+		ch := make(chan os.Signal, 1)
+		defer close(ch)
+
+		signal.Notify(ch, os.Interrupt, syscall.SIGTERM)
+		defer signal.Stop(ch)
+
+		select {
+		case sig := <-ch:
+			log.Info("Got interrupt, shutting down...", "sig", sig)
+		case <-ctx.Done():
+		}
+	}()
+	return ctx, cancel
+}
