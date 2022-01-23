98f980947779d942feee44ce0268f2fa81238cef
alex.sharov <AskAlexSharov@gmail.com>
2022-01-23 21:36:54 +0700

    save

--- turbo/snapshotsync/block_reader.go
+++ turbo/snapshotsync/block_reader.go
@@ -469,6 +469,7 @@ func (back *BlockReaderWithSnapshots) txnByHash(txnHash common.Hash, buf []byte)
 		offset := sn.TxnHashIdx.Lookup2(localID)
 		gg := sn.Transactions.MakeGetter()
 		gg.Reset(offset)
+		fmt.Printf("try: %d, %d, %d, %d\n", i, sn.From, localID, blockNum)
 		buf, _ = gg.Next(buf[:0])
 		if txnHash[0] != buf[0] {
 			continue
@@ -476,7 +477,7 @@ func (back *BlockReaderWithSnapshots) txnByHash(txnHash common.Hash, buf []byte)
 
 		localID = sn.TxnHash2BlockNumIdx.Lookup(txnHash[:])
 		blockNum = sn.TxnHash2BlockNumIdx.Lookup2(localID)
-		fmt.Printf("try: %d, %d, %d, %d\n", i, sn.From, localID, blockNum)
+		fmt.Printf("try_succeed: %d, %d, %d, %d\n", i, sn.From, localID, blockNum)
 		//fmt.Printf("try2: %d,%d,%d,%d,%d,%d\n", sn.TxnHash2BlockNumIdx.Lookup2(0), sn.TxnHash2BlockNumIdx.Lookup2(1), sn.TxnHash2BlockNumIdx.Lookup2(2), sn.TxnHash2BlockNumIdx.Lookup2(3), sn.TxnHash2BlockNumIdx.Lookup2(4), sn.TxnHash2BlockNumIdx.Lookup2(5))
 		//fmt.Printf("try3: %d,%d,%d,%d\n", sn.TxnHash2BlockNumIdx.Lookup(common.FromHex("0xc2c3ba07f05ddd8552508e7facf25dc5bd6d16e95c12cff42cb8b9ea6bbfc225")), sn.TxnHash2BlockNumIdx.Lookup(common.FromHex("0xca8a182f21b98318e94ec7884f572c0a1385dbc10a2bea62a38079eab7d8cfef")), sn.TxnHash2BlockNumIdx.Lookup(common.FromHex("0xf1b3306dd4bfa2a86f7f1b3c22bf0b6b4da50f5d37f2fed89d1221cb3690c700")), sn.TxnHash2BlockNumIdx.Lookup(common.FromHex("0xf59129e464525261217833d4bafae0ed8be5a94044eafacb47a45a4b23802a70")))
 		sender := buf[1 : 1+20]
