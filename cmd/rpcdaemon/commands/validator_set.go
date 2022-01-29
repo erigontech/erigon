2e61c41a81ab4d6145190d77899d85d5c232932a
Enrique Avila Asapche <eavilaasapche@gmail.com>
2022-01-29 23:52:25 +0300

    ops

--- cmd/rpcdaemon/commands/validator_set.go
+++ cmd/rpcdaemon/commands/validator_set.go
@@ -272,6 +272,7 @@ func (vals *ValidatorSet) updateTotalVotingPower() error {
 	return nil
 }
 
+
 // TotalVotingPower returns the sum of the voting powers of all validators.
 // It recomputes the total voting power if required.
 func (vals *ValidatorSet) TotalVotingPower() int64 {
