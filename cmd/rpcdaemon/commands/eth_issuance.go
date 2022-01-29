218e493e5d92e9455cc3532ed67d7c609bc44aa1
Enrique Avila Asapche <eavilaasapche@gmail.com>
2022-01-29 21:54:30 +0300

    added eth_api issuance

--- cmd/rpcdaemon/commands/eth_issuance.go
+++ cmd/rpcdaemon/commands/eth_issuance.go
@@ -6,9 +6,11 @@ import (
 	"math/big"
 
 	"github.com/holiman/uint256"
+	"github.com/ledgerwatch/erigon-lib/kv"
 	"github.com/ledgerwatch/erigon/common"
 	"github.com/ledgerwatch/erigon/consensus/ethash"
 	"github.com/ledgerwatch/erigon/core/rawdb"
+	"github.com/ledgerwatch/erigon/core/types"
 	"github.com/ledgerwatch/erigon/rpc"
 )
 
@@ -106,4 +108,48 @@ func (api *ErigonImpl) WatchTheBurn(ctx context.Context, blockNr rpc.BlockNumber
 		}
 	}
 	return ret, nil
+}
+
+// Issuance implements eth_issuance. Returns the total issuance (block reward plus uncle reward) for the given block.
+func (api *APIImpl) Issuance(ctx context.Context, blockNr rpc.BlockNumber) (Issuance, error) {
+	tx, err := api.db.BeginRo(ctx)
+	if err != nil {
+		return Issuance{}, err
+	}
+	defer tx.Rollback()
+
+	chainConfig, err := api.chainConfig(tx)
+	if err != nil {
+		return Issuance{}, err
+	}
+	if chainConfig.Ethash == nil {
+		// Clique for example has no issuance
+		return Issuance{}, nil
+	}
+
+	block, err := api.getBlockByRPCNumber(tx, blockNr)
+	if err != nil {
+		return Issuance{}, err
+	}
+	minerReward, uncleRewards := ethash.AccumulateRewards(chainConfig, block.Header(), block.Uncles())
+	issuance := minerReward
+	for _, r := range uncleRewards {
+		p := r // avoids warning?
+		issuance.Add(&issuance, &p)
+	}
+
+	var ret Issuance
+	ret.BlockReward = minerReward.ToBig()
+	ret.Issuance = issuance.ToBig()
+	issuance.Sub(&issuance, &minerReward)
+	ret.UncleReward = issuance.ToBig()
+	return ret, nil
+}
+
+func (api *APIImpl) getBlockByRPCNumber(tx kv.Tx, blockNr rpc.BlockNumber) (*types.Block, error) {
+	blockNum, err := getBlockNumber(blockNr, tx)
+	if err != nil {
+		return nil, err
+	}
+	return api.blockByNumberWithSenders(tx, blockNum)
 }
