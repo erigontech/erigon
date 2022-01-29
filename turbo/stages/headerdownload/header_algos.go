c612f5b9e11d0aa0a98f1ec95001f5391bced2e4
Enrique Avila Asapche <eavilaasapche@gmail.com>
2022-01-29 16:59:23 +0300

    took out syscall

--- turbo/stages/headerdownload/header_algos.go
+++ turbo/stages/headerdownload/header_algos.go
@@ -23,9 +23,7 @@ import (
 	"github.com/ledgerwatch/erigon/common"
 	"github.com/ledgerwatch/erigon/common/dbutils"
 	"github.com/ledgerwatch/erigon/consensus"
-	"github.com/ledgerwatch/erigon/core"
 	"github.com/ledgerwatch/erigon/core/rawdb"
-	"github.com/ledgerwatch/erigon/core/state"
 	"github.com/ledgerwatch/erigon/core/types"
 	"github.com/ledgerwatch/erigon/eth/stagedsync/stages"
 	"github.com/ledgerwatch/erigon/p2p/enode"
@@ -658,18 +656,21 @@ func (hd *HeaderDownload) InsertHeaders(hf FeedHeaderFunc, terminalTotalDifficul
 			break
 		}
 
-		stateReader := state.NewPlainStateReader(tx)
-		ibs := state.New(stateReader)
+		/*
+			else if err := hd.engine.VerifyHeader(hd.headerReader, link.header, true,
+				func(contract common.Address, data []byte) ([]byte, error) {
+					return core.SysCallContract(contract, data, *hd.headerReader.Config(), ibs, link.header, hd.engine)
+				}); err != nil {
+				log.Warn("Verification failed for header", "hash", link.header.Hash(), "height", link.blockHeight, "error", err)
+		*/
+
 		hd.insertList = hd.insertList[:len(hd.insertList)-1]
 		skip := false
 		if !link.preverified {
 			if _, bad := hd.badHeaders[link.hash]; bad {
 				skip = true
-			} else if err := hd.engine.VerifyHeader(hd.headerReader, link.header, true, /* seal */
-				func(contract common.Address, data []byte) ([]byte, error) {
-					return core.SysCallContract(contract, data, *hd.headerReader.Config(), ibs, link.header, hd.engine)
-				}); err != nil {
-				log.Warn("Verification failed for header", "hash", link.header.Hash(), "height", link.blockHeight, "error", err)
+			} else if err := hd.VerifyHeader(link.header); err != nil {
+				log.Warn("Verification failed for header", "hash", link.hash, "height", link.blockHeight, "error", err)
 				if errors.Is(err, consensus.ErrFutureBlock) {
 					// This may become valid later
 					linksInFuture = append(linksInFuture, link)
