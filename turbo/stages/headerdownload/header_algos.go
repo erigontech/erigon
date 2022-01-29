8ba5493ff804112d6e100280933bee2f72bdfb84
Enrique Avila Asapche <eavilaasapche@gmail.com>
2022-01-29 14:51:32 +0300

    nil

--- turbo/stages/headerdownload/header_algos.go
+++ turbo/stages/headerdownload/header_algos.go
@@ -633,7 +633,7 @@ func (hd *HeaderDownload) RequestSkeleton() *HeaderRequest {
 }
 
 func (hd *HeaderDownload) VerifyHeader(header *types.Header) error {
-	return hd.engine.VerifyHeader(hd.headerReader, header, true /* seal */)
+	return hd.engine.VerifyHeader(hd.headerReader, header, true, nil)
 }
 
 type FeedHeaderFunc = func(header *types.Header, headerRaw []byte, hash common.Hash, blockHeight uint64) (td *big.Int, err error)
