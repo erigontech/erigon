45b36ce9186aeb00de189b31c1acd26b558f85a6
alex.sharov <AskAlexSharov@gmail.com>
2022-02-04 11:56:34 +0700

    save

--- cmd/downloader/downloader/util.go
+++ cmd/downloader/downloader/util.go
@@ -24,8 +24,8 @@ const DefaultPieceSize = 2 * 1024 * 1024
 
 // Trackers - break down by priority tier
 var Trackers = [][]string{
-	trackers.Udp,
-	//trackers.Http,
+	//trackers.Udp,
+	trackers.Http,
 	//trackers.Best,
 	//trackers.Ws,
 	// trackers.Udp, trackers.Https, trackers.Http,
