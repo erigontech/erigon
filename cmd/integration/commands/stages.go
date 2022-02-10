0xPolygon/cdk-erigon

[WIP] Adding Support for Sepolia (#3462)
* [WIP] Adding Sepolia

* Fix formatting

* Added Timestamp to Sepolia genesis

* update config

* fixed formating

cmd/integration/commands/stages.go
@@ -1012,6 +1012,9 @@ func byChain() (*core.Genesis, *params.ChainConfig) {
 	case "", networkname.MainnetChainName:
 		chainConfig = params.MainnetChainConfig
 		genesis = core.DefaultGenesisBlock()
+	case networkname.SepoliaChainName:
+		chainConfig = params.SepoliaChainConfig
+		genesis = core.DefaultSepoliaGenesisBlock()
 	case networkname.RopstenChainName:
 		chainConfig = params.RopstenChainConfig
 		genesis = core.DefaultRopstenGenesisBlock()

cmd/utils/flags.go
@@ -653,6 +653,8 @@ func setBootstrapNodes(ctx *cli.Context, cfg *p2p.Config) {
 		switch chain {
 		case networkname.MainnetChainName:
 			urls = params.MainnetBootnodes
+		case networkname.SepoliaChainName:
+			urls = params.SepoliaBootnodes
 		case networkname.RopstenChainName:
 			urls = params.RopstenBootnodes
 		case networkname.RinkebyChainName:
@@ -698,6 +700,8 @@ func setBootstrapNodesV5(ctx *cli.Context, cfg *p2p.Config) {
 		switch chain {
 		case networkname.MainnetChainName:
 			urls = params.MainnetBootnodes
+		case networkname.SepoliaChainName:
+			urls = params.SepoliaBootnodes
 		case networkname.RopstenChainName:
 			urls = params.RopstenBootnodes
 		case networkname.RinkebyChainName:
@@ -1016,6 +1020,8 @@ func DataDirForNetwork(datadir string, network string) string {
 		return filepath.Join(datadir, "mumbai")
 	case networkname.BorMainnetChainName:
 		return filepath.Join(datadir, "bor-mainnet")
+	case networkname.SepoliaChainName:
+		return filepath.Join(datadir, "sepolia")
 	default:
 		return datadir
 	}
@@ -1424,6 +1430,12 @@ func SetEthConfig(ctx *cli.Context, nodeConfig *node.Config, cfg *ethconfig.Conf
 		}
 		cfg.Genesis = core.DefaultGenesisBlock()
 		SetDNSDiscoveryDefaults(cfg, params.MainnetGenesisHash)
+	case networkname.SepoliaChainName:
+		if !ctx.GlobalIsSet(NetworkIdFlag.Name) {
+			cfg.NetworkID = 11155111
+		}
+		cfg.Genesis = core.DefaultSepoliaGenesisBlock()
+		SetDNSDiscoveryDefaults(cfg, params.SepoliaGenesisHash)
 	case networkname.RopstenChainName:
 		if !ctx.GlobalIsSet(NetworkIdFlag.Name) {
 			cfg.NetworkID = 3
@@ -1556,6 +1568,8 @@ func MakeGenesis(ctx *cli.Context) *core.Genesis {
 	var genesis *core.Genesis
 	chain := ctx.GlobalString(ChainFlag.Name)
 	switch chain {
+	case networkname.SepoliaChainName:
+		genesis = core.DefaultSepoliaGenesisBlock()
 	case networkname.RopstenChainName:
 		genesis = core.DefaultRopstenGenesisBlock()
 	case networkname.RinkebyChainName:

turbo/node/node.go
@@ -57,6 +57,8 @@ func NewNodConfigUrfave(ctx *cli.Context) *node.Config {
 	// If we're running a known preset, log it for convenience.
 	chain := ctx.GlobalString(utils.ChainFlag.Name)
 	switch chain {
+	case networkname.SepoliaChainName:
+		log.Info("Starting Erigon on Sepolia testnet...")
 	case networkname.RopstenChainName:
 		log.Info("Starting Erigon on Ropsten testnet...")
 	case networkname.RinkebyChainName:

erigontech/erigon

cmd/integration/commands/stages.go
@@ -995,6 +995,9 @@ func byChain() (*core.Genesis, *params.ChainConfig) {
 	case "", params.MainnetChainName:
 		chainConfig = params.MainnetChainConfig
 		genesis = core.DefaultGenesisBlock()
+	case params.SepoliaChainName:
+		chainConfig = params.SepoliaChainConfig
+		genesis = core.DefaultSepoliaGenesisBlock()
 	case params.RopstenChainName:
 		chainConfig = params.RopstenChainConfig
 		genesis = core.DefaultRopstenGenesisBlock()

cmd/utils/flags.go
@@ -600,6 +600,8 @@ func setBootstrapNodes(ctx *cli.Context, cfg *p2p.Config) {
 	} else {
 		chain := ctx.GlobalString(ChainFlag.Name)
 		switch chain {
+		case params.SepoliaChainName:
+			urls = params.SepoliaBootnodes
 		case params.RopstenChainName:
 			urls = params.RopstenBootnodes
 		case params.RinkebyChainName:
@@ -631,9 +633,10 @@ func setBootstrapNodesV5(ctx *cli.Context, cfg *p2p.Config) {
 	if ctx.GlobalIsSet(BootnodesFlag.Name) {
 		urls = SplitAndTrim(ctx.GlobalString(BootnodesFlag.Name))
 	} else {
-
 		chain := ctx.GlobalString(ChainFlag.Name)
 		switch chain {
+		case params.SepoliaChainName:
+			urls = params.SepoliaBootnodes
 		case params.RopstenChainName:
 			urls = params.RopstenBootnodes
 		case params.RinkebyChainName:
@@ -916,6 +919,8 @@ func DataDirForNetwork(datadir string, network string) string {
 		return filepath.Join(datadir, "kovan")
 	case params.FermionChainName:
 		return filepath.Join(datadir, "fermion")
+	case params.SepoliaChainName:
+		return filepath.Join(datadir, "sepolia")
 	default:
 		return datadir
 	}
@@ -1264,6 +1269,12 @@ func SetEthConfig(ctx *cli.Context, nodeConfig *node.Config, cfg *ethconfig.Conf
 		}
 		cfg.Genesis = core.DefaultGenesisBlock()
 		SetDNSDiscoveryDefaults(cfg, params.MainnetGenesisHash)
+	case params.SepoliaChainName:
+		if !ctx.GlobalIsSet(NetworkIdFlag.Name) {
+			cfg.NetworkID = 11155111
+		}
+		cfg.Genesis = core.DefaultSepoliaGenesisBlock()
+		SetDNSDiscoveryDefaults(cfg, params.SepoliaGenesisHash)
 	case params.RopstenChainName:
 		if !ctx.GlobalIsSet(NetworkIdFlag.Name) {
 			cfg.NetworkID = 3
@@ -1366,6 +1377,8 @@ func MakeGenesis(ctx *cli.Context) *core.Genesis {
 	var genesis *core.Genesis
 	chain := ctx.GlobalString(ChainFlag.Name)
 	switch chain {
+	case params.SepoliaChainName:
+		genesis = core.DefaultSepoliaGenesisBlock()
 	case params.RopstenChainName:
 		genesis = core.DefaultRopstenGenesisBlock()
 	case params.RinkebyChainName:

turbo/node/node.go
@@ -52,6 +52,9 @@ func NewNodConfigUrfave(ctx *cli.Context) *node.Config {
 	// If we're running a known preset, log it for convenience.
 	chain := ctx.GlobalString(utils.ChainFlag.Name)
 	switch chain {
+	case params.SepoliaChainName:
+		log.Info("Starting Erigon on Sepolia testnet...")
+
 	case params.RopstenChainName:
 		log.Info("Starting Erigon on Ropsten testnet...")
 