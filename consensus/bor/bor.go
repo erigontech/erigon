0bf0b238cbcc9fcf4576126c04e0e47a661ed9bd
Alexey Sharp <alexeysharp@Alexeys-iMac.local>
2022-02-21 15:22:14 +0000

    Print

--- consensus/bor/bor.go
+++ consensus/bor/bor.go
@@ -1035,6 +1035,7 @@ func (c *Bor) fetchAndCommitSpan(
 ) error {
 	var heimdallSpan HeimdallSpan
 
+	fmt.Printf("fetchAndCommitSpan %d\n", header.Number.Uint64())
 	if c.WithoutHeimdall {
 		s, err := c.getNextHeimdallSpanForTest(newSpanID, state, header, chain, syscall)
 		if err != nil {
