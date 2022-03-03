114b6b5c02851c0737924c82a5516e4e06872fe1
alex.sharov <AskAlexSharov@gmail.com>
2022-03-03 13:26:35 +0700

    fefefe

--- cmd/rpcdaemon/commands/eth_accounts.go
+++ cmd/rpcdaemon/commands/eth_accounts.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"math/big"
+	"runtime"
 
 	"github.com/ledgerwatch/erigon-lib/gointerfaces"
 	"github.com/ledgerwatch/erigon/turbo/rpchelper"
@@ -15,8 +16,12 @@ import (
 	"github.com/ledgerwatch/erigon/rpc"
 )
 
+var ch = make(chan struct{}, runtime.NumCPU())
+
 // GetBalance implements eth_getBalance. Returns the balance of an account for a given address.
 func (api *APIImpl) GetBalance(ctx context.Context, address common.Address, blockNrOrHash rpc.BlockNumberOrHash) (*hexutil.Big, error) {
+	ch <- struct{}{}
+	defer func() { <-ch }()
 	tx, err1 := api.db.BeginRo(ctx)
 	if err1 != nil {
 		return nil, fmt.Errorf("getBalance cannot open tx: %w", err1)
