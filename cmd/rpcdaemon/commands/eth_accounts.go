d97d530b4f61da18b39aaf4936781b4e4b558a21
alex.sharov <AskAlexSharov@gmail.com>
2022-03-03 10:51:52 +0700

    save

--- cmd/rpcdaemon/commands/eth_accounts.go
+++ cmd/rpcdaemon/commands/eth_accounts.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"math/big"
+	"runtime"
 	"time"
 
 	"github.com/VictoriaMetrics/metrics"
@@ -18,7 +19,7 @@ import (
 	"github.com/ledgerwatch/erigon/rpc"
 )
 
-var ch = make(chan struct{}, 1024)
+var ch = make(chan struct{}, runtime.NumCPU())
 var beginMetric = metrics.GetOrCreateSummary(`db_begin_ro`) //nolint
 
 // GetBalance implements eth_getBalance. Returns the balance of an account for a given address.
