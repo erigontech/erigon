864bd87b84364a915fd7f515d747408519c74115
Alex Sharov <AskAlexSharov@gmail.com>
2022-03-05 10:26:08 +0700

    torrent better logging (#3649)

--- cmd/downloader/downloader/torrentcfg/logger.go
+++ cmd/downloader/downloader/torrentcfg/logger.go
@@ -1,22 +1,16 @@
 package torrentcfg
 
 import (
+	"strings"
+
+	utp "github.com/anacrolix/go-libutp"
 	lg "github.com/anacrolix/log"
+	"github.com/ledgerwatch/log/v3"
 )
 
 func init() {
-	lg.Default = NewAdapterLogger()
-	//a := stdlog.New(NullWriter(1), "", stdlog.LstdFlags)
-	//var Logger = log.Default.WithContextText("go-libutp")
-	//utp.Logger = a
-}
-
-func NewAdapterLogger() lg.Logger {
-	return lg.Default
-	//lg.Logger{Handlers:}
-	//return lg.Logger{
-	//	LoggerImpl: lg.LoggerImpl(adapterLogger{}),
-	//}
+	lg.Default.Handlers = []lg.Handler{adapterHandler{}}
+	utp.Logger.Handlers = []lg.Handler{noopHandler{}}
 }
 
 var String2LogLevel = map[string]lg.Level{
@@ -26,59 +20,53 @@ var String2LogLevel = map[string]lg.Level{
 	lg.Error.LogString():   lg.Error,
 }
 
-type adapterLogger struct{}
+type noopHandler struct{}
 
-func (b adapterLogger) Log(msg lg.Msg) {
-	/*
-		lvl, ok := msg.GetLevel()
-		if !ok {
-			lvl = lg.Debug
-		}
-
-		switch lvl {
-		case lg.Debug:
-			log.Debug(msg.String())
-		case lg.Info:
-			str := msg.String()
-			if strings.Contains(str, "EOF") ||
-				strings.Contains(str, "spurious timer") ||
-				strings.Contains(str, "banning ip <nil>") { // suppress useless errors
-				break
-			}
-
-			log.Info(str)
-		case lg.Warning:
-			str := msg.String()
-			if strings.Contains(str, "could not find offer for id") { // suppress useless errors
-				break
-			}
-
-			log.Warn(str)
-		case lg.Error:
-			str := msg.String()
-			if strings.Contains(str, "EOF") { // suppress useless errors
-				break
-			}
-
-			log.Error(str)
-		case lg.Critical:
-			str := msg.String()
-			if strings.Contains(str, "EOF") { // suppress useless errors
-				break
-			}
-			if strings.Contains(str, "don't want conns") { // suppress useless errors
-				break
-			}
-
-			log.Error(str)
-		default:
-			log.Warn("unknown logtype", "msg", msg.String())
-		}
-	*/
+func (b noopHandler) Handle(r lg.Record) {
 }
 
-// NullWriter implements the io.Write interface but doesn't do anything.
-type NullWriter int
+type adapterHandler struct{}
 
-// Write implements the io.Write interface but is a noop.
-func (NullWriter) Write([]byte) (int, error) { return 0, nil }
+func (b adapterHandler) Handle(r lg.Record) {
+	lvl := r.Level
+
+	switch lvl {
+	case lg.Debug:
+		log.Debug(r.String())
+	case lg.Info:
+		str := r.String()
+		if strings.Contains(str, "EOF") ||
+			strings.Contains(str, "spurious timer") ||
+			strings.Contains(str, "banning ip <nil>") { // suppress useless errors
+			break
+		}
+
+		log.Info(str)
+	case lg.Warning:
+		str := r.String()
+		if strings.Contains(str, "could not find offer for id") { // suppress useless errors
+			break
+		}
+
+		log.Warn(str)
+	case lg.Error:
+		str := r.String()
+		if strings.Contains(str, "EOF") { // suppress useless errors
+			break
+		}
+
+		log.Error(str)
+	case lg.Critical:
+		str := r.String()
+		if strings.Contains(str, "EOF") { // suppress useless errors
+			break
+		}
+		if strings.Contains(str, "don't want conns") { // suppress useless errors
+			break
+		}
+
+		log.Error(str)
+	default:
+		log.Warn("unknown logtype", "msg", r.String())
+	}
+}
