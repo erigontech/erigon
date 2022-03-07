812fed985c2ee9315350917ede6ae0c4645e1e00
alex.sharov <AskAlexSharov@gmail.com>
2022-03-07 10:42:22 +0700

    save

--- core/rawdb/accessors_chain.go
+++ core/rawdb/accessors_chain.go
@@ -512,6 +512,9 @@ func ReadBody(db kv.Getter, hash common.Hash, number uint64) (*types.Body, uint6
 	}
 	body := new(types.Body)
 	body.Uncles = bodyForStorage.Uncles
+	if bodyForStorage.TxAmount < 2 {
+		panic(1)
+	}
 	return body, bodyForStorage.BaseTxId + 1, bodyForStorage.TxAmount - 2 // 1 system txn in the begining of block, and 1 at the end
 }
 
