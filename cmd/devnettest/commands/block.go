5738d05a4156e58aef91e98ffd50016770413847
leonardchinonso <leokingluthers@gmail.com>
2022-03-14 14:15:58 +0100

    Fixed lint errors

--- cmd/devnettest/commands/block.go
+++ cmd/devnettest/commands/block.go
@@ -25,7 +25,7 @@ import (
 
 var (
 	devnetSignPrivateKey, _ = crypto.HexToECDSA("26e86e45f6fc45ec6e2ecd128cec80fa1d1505e5507dcd2ae58c3130a7a97b48")
-	signer = types.LatestSigner(params.AllCliqueProtocolChanges)
+	signer                  = types.LatestSigner(params.AllCliqueProtocolChanges)
 )
 
 var (
@@ -178,9 +178,9 @@ func subscribe(client *rpc.Client, method string, hash common.Hash) (uint64, err
 	}
 	defer sub.Unsubscribe()
 
-	var(
+	var (
 		blockCount int
-		blockN uint64
+		blockN     uint64
 	)
 ForLoop:
 	for {
