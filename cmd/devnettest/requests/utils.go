f47f8b69be5c274357b2ec22100962d4b5454f0b
leonardchinonso <leokingluthers@gmail.com>
2022-03-16 22:38:38 +0100

    Fixed logs not being populated in result typee.go

--- cmd/devnettest/requests/utils.go
+++ cmd/devnettest/requests/utils.go
@@ -12,17 +12,17 @@ import (
 )
 
 func post(client *http.Client, url, request string, response interface{}) error {
-	fmt.Printf("Request=%s\n", request)
-	log.Info("Getting", "url", url, "request", request)
 	start := time.Now()
 	r, err := client.Post(url, "application/json", strings.NewReader(request))
 	if err != nil {
 		return err
 	}
 	defer r.Body.Close()
+
 	if r.StatusCode != 200 {
 		return fmt.Errorf("status %s", r.Status)
 	}
+
 	decoder := json.NewDecoder(r.Body)
 	err = decoder.Decode(response)
 	log.Info("Got in", "time", time.Since(start).Seconds())
