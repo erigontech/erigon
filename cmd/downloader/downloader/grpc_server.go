0026f4e6de9feb91cf5b4e429c3a26f6a5d46718
alex.sharov <AskAlexSharov@gmail.com>
2022-03-17 14:28:44 +0700

    save

--- cmd/downloader/downloader/grpc_server.go
+++ cmd/downloader/downloader/grpc_server.go
@@ -3,7 +3,6 @@ package downloader
 import (
 	"context"
 	"errors"
-	"fmt"
 	"path/filepath"
 
 	"github.com/anacrolix/torrent"
@@ -59,10 +58,8 @@ type GrpcServer struct {
 
 func (s *GrpcServer) Download(ctx context.Context, request *proto_downloader.DownloadRequest) (*emptypb.Empty, error) {
 	infoHashes := make([]metainfo.Hash, len(request.Items))
-	fmt.Printf("alex: call download\n")
 	for i, it := range request.Items {
 		if it.TorrentHash == nil {
-			fmt.Printf("alex: add torrent file: %s\n", it.Path)
 			if err := BuildTorrentFileIfNeed(ctx, it.Path, s.snapshotDir); err != nil {
 				return nil, err
 			}
@@ -70,7 +67,6 @@ func (s *GrpcServer) Download(ctx context.Context, request *proto_downloader.Dow
 			if err != nil {
 				return nil, err
 			}
-			fmt.Printf("alex: add torrent file done: %s, %x\n", it.Path, metaInfo.HashInfoBytes().Bytes())
 			infoHashes[i] = metaInfo.HashInfoBytes()
 		} else {
 			infoHashes[i] = gointerfaces.ConvertH160toAddress(it.TorrentHash)
