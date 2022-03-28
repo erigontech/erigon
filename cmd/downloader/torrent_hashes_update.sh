f86101ed279d3759573da80c985fd9d984e39848
alex.sharov <AskAlexSharov@gmail.com>
2022-03-28 15:53:13 +0700

    save

--- cmd/downloader/torrent_hashes_update.sh
+++ cmd/downloader/torrent_hashes_update.sh
@@ -10,7 +10,8 @@ network=$2
 # clean
 cd ./../erigon-snapshot
 git reset --hard
-git pull
+git checkout main
+git pull origin main
 cd ../erigon
 
 # it will return only .seg of 500K (because Erigon send to Downloader only such files)
@@ -18,14 +19,13 @@ go run -trimpath ./cmd/downloader torrent_hashes --datadir="$datadir" --targetfi
 cd ./../erigon-snapshot
 git add "$network".toml
 git commit -m "ci: $network"
-git push
 
-# update Erigon submodule
-cd ./../erigon
-cd turbo/snapshotsync/snapshothashes/erigon-snapshots
-git checkout main
-git pull
-cd ../../../../
-git add turbo/snapshotsync/snapshothashes/erigon-snapshots
-git commit -m "ci: $network snapshots"
-git push
+GH_TOKEN=""
+if ! gcloud -v <the_command> &> /dev/null
+then
+    GH_TOKEN=$(gcloud secrets versions access 1 --secret="githug-snapshots-push")
+    exit
+fi
+
+# /dev/null to avoid logging of insecure git output
+git push https://${GH_TOKEN}@github.com/ledgerwatch/erigon-snapshot.git main > /dev/null 2>&1
