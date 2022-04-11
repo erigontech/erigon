dbb84bd25bd0af1156d5aaef06426bfca595af47
alex.sharov <AskAlexSharov@gmail.com>
2022-04-11 18:28:04 +0700

    save

--- cmd/downloader/downloader/torrentcfg/torrentcfg.go
+++ cmd/downloader/downloader/torrentcfg/torrentcfg.go
@@ -71,7 +71,7 @@ func New(snapshotsDir *dir.Rw, verbosity lg.Level, natif nat.Interface, download
 		}
 	}
 	// rates are divided by 2 - I don't know why it works, maybe bug inside torrent lib accounting
-	torrentConfig.UploadRateLimiter = rate.NewLimiter(rate.Limit(uploadRate.Bytes()/2), 2*DefaultPieceSize) // default: unlimited
+	torrentConfig.UploadRateLimiter = rate.NewLimiter(rate.Limit(uploadRate.Bytes()/2), 2*16384) // default: unlimited
 	//torrentConfig.DownloadRateLimiter = rate.NewLimiter(rate.Limit(downloadRate.Bytes()/2), 2*DefaultPieceSize) // default: unlimited
 
 	// debug
