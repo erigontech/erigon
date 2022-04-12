35674e81c0178d886b44aa1e41733c40c6b98e45
alex.sharov <AskAlexSharov@gmail.com>
2022-04-12 14:51:45 +0700

    recompress

--- cmd/downloader/recompress.sh
+++ cmd/downloader/recompress.sh
@@ -4,9 +4,9 @@ dir=$1
 files=$(ls $dir/snapshots | grep tr | grep -v tor | grep seg |  sort -n -t$'-' -k1)
 for file in $files
 do
-#   mv $dir/snapshots/$file $dir/snapshots/$file.old
-   from=$dir/snapshots/$file
-   to=$dir/snapshots/$file.new
+   mv $dir/snapshots/$file $dir/snapshots/$file.old
+   from=$dir/snapshots/$file.old
+   to=$dir/snapshots/$file
    echo "file: $file"
 
    ./build/bin/erigon_old snapshots uncompress $from | ./build/bin/erigon snapshots compress $to --datadir=$dir
