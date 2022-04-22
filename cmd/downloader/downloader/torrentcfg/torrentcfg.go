1865cbd957555756306af8b1eaeda43b4c1e208b
Alexey Sharp <alexeysharp@Alexeys-iMac.local>
2022-04-22 15:18:13 +0100

    Introduce unlimited download rate

--- cmd/downloader/downloader/torrentcfg/torrentcfg.go
+++ cmd/downloader/downloader/torrentcfg/torrentcfg.go
@@ -78,8 +78,10 @@ func New(snapshotsDir *dir.Rw, verbosity lg.Level, natif nat.Interface, download
 		}
 	}
 	// rates are divided by 2 - I don't know why it works, maybe bug inside torrent lib accounting
-	torrentConfig.UploadRateLimiter = rate.NewLimiter(rate.Limit(uploadRate.Bytes()), 2*DefaultPieceSize)     // default: unlimited
-	torrentConfig.DownloadRateLimiter = rate.NewLimiter(rate.Limit(downloadRate.Bytes()), 2*DefaultPieceSize) // default: unlimited
+	torrentConfig.UploadRateLimiter = rate.NewLimiter(rate.Limit(uploadRate.Bytes()), 2*DefaultPieceSize) // default: unlimited
+	if downloadRate.Bytes() < 500_000_000 {
+		torrentConfig.DownloadRateLimiter = rate.NewLimiter(rate.Limit(downloadRate.Bytes()), 2*DefaultPieceSize) // default: unlimited
+	}
 
 	// debug
 	if lg.Debug == verbosity {
