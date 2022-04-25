e98ee7d0e704fc0a4738e3c54189727d1c9e745e
Alexey Sharp <alexeysharp@Alexeys-iMac.local>
2022-04-25 17:55:56 +0100

    Revert "Fix"
    This reverts commit 038e02b8a4d23cd32ddb111e9f325fc4ce1bbe2b.

--- cmd/downloader/downloader/grpc_server.go
+++ cmd/downloader/downloader/grpc_server.go
@@ -77,18 +77,8 @@ func (s *GrpcServer) Download(ctx context.Context, request *proto_downloader.Dow
 		}
 		if _, ok := s.t.TorrentClient.Torrent(infoHashes[i]); !ok {
 			magnet := mi.Magnet(&infoHashes[i], nil)
-			t, err := s.t.TorrentClient.AddMagnet(magnet.String())
-			if err != nil {
+			if _, err := s.t.TorrentClient.AddMagnet(magnet.String()); err != nil {
 				return nil, err
-			}
-			select {
-			case <-ctx.Done():
-				return nil, ctx.Err()
-			case <-t.GotInfo():
-				metaInfo := t.Metainfo()
-				if err := CreateTorrentFileIfNotExists(s.snapshotDir, t.Info(), &metaInfo); err != nil {
-					return nil, err
-				}
 			}
 		}
 	}
@@ -96,6 +86,15 @@ func (s *GrpcServer) Download(ctx context.Context, request *proto_downloader.Dow
 		t, ok := s.t.TorrentClient.Torrent(infoHashes[0])
 		if !ok {
 			return nil, fmt.Errorf("torrent not found: [%x]", infoHashes[0])
+		}
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		case <-t.GotInfo():
+			metaInfo := t.Metainfo()
+			if err := CreateTorrentFileIfNotExists(s.snapshotDir, t.Info(), &metaInfo); err != nil {
+				return nil, err
+			}
 		}
 		t.AllowDataUpload()
 		t.AllowDataDownload()
