8d87399be123519ae4746e3b67c5991e918642ff
alex.sharov <AskAlexSharov@gmail.com>
2022-04-28 09:09:23 +0700

    save

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -650,7 +650,7 @@ var (
 	}
 	TorrentDownloadSlotsFlag = cli.IntFlag{
 		Name:  "torrent.download.slots",
-		Value: 1,
+		Value: 3,
 		Usage: "amount of files to download in parallel. If network has enough seeders 1-2 slot enough, if network has lack of seeders increase to 5 (too big value will slow down everything).",
 	}
 	TorrentPortFlag = cli.IntFlag{
