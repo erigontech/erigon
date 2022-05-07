f54628b7ba3163ec05430b0f16c8219f7e5fb366
Alex Sharov <AskAlexSharov@gmail.com>
2022-05-07 14:52:04 +0700

    remove metrics package, step 1 (#4094)

--- eth/stagedsync/stage_execute_test.go
+++ eth/stagedsync/stage_execute_test.go
@@ -17,14 +17,14 @@ func TestUnwindExecutionStagePlainStatic(t *testing.T) {
 	_, tx1 := memdb.NewTestTx(t)
 	_, tx2 := memdb.NewTestTx(t)
 
-	generateBlocks(t, 1, 50, plainWriterGen(tx1), staticCodeStaticIncarnations)
-	generateBlocks(t, 1, 100, plainWriterGen(tx2), staticCodeStaticIncarnations)
+	generateBlocks(t, 1, 25, plainWriterGen(tx1), staticCodeStaticIncarnations)
+	generateBlocks(t, 1, 50, plainWriterGen(tx2), staticCodeStaticIncarnations)
 
-	err := stages.SaveStageProgress(tx2, stages.Execution, 100)
+	err := stages.SaveStageProgress(tx2, stages.Execution, 50)
 	assert.NoError(err)
 
-	u := &UnwindState{ID: stages.Execution, UnwindPoint: 50}
-	s := &StageState{ID: stages.Execution, BlockNumber: 100}
+	u := &UnwindState{ID: stages.Execution, UnwindPoint: 25}
+	s := &StageState{ID: stages.Execution, BlockNumber: 50}
 	err = UnwindExecutionStage(u, s, tx2, ctx, ExecuteBlockCfg{}, false)
 	assert.NoError(err)
 
@@ -36,14 +36,14 @@ func TestUnwindExecutionStagePlainWithIncarnationChanges(t *testing.T) {
 	_, tx1 := memdb.NewTestTx(t)
 	_, tx2 := memdb.NewTestTx(t)
 
-	generateBlocks(t, 1, 50, plainWriterGen(tx1), changeCodeWithIncarnations)
-	generateBlocks(t, 1, 100, plainWriterGen(tx2), changeCodeWithIncarnations)
+	generateBlocks(t, 1, 25, plainWriterGen(tx1), changeCodeWithIncarnations)
+	generateBlocks(t, 1, 50, plainWriterGen(tx2), changeCodeWithIncarnations)
 
-	err := stages.SaveStageProgress(tx2, stages.Execution, 100)
+	err := stages.SaveStageProgress(tx2, stages.Execution, 50)
 	assert.NoError(err)
 
-	u := &UnwindState{ID: stages.Execution, UnwindPoint: 50}
-	s := &StageState{ID: stages.Execution, BlockNumber: 100}
+	u := &UnwindState{ID: stages.Execution, UnwindPoint: 25}
+	s := &StageState{ID: stages.Execution, BlockNumber: 50}
 	err = UnwindExecutionStage(u, s, tx2, ctx, ExecuteBlockCfg{}, false)
 	assert.NoError(err)
 
@@ -56,15 +56,15 @@ func TestUnwindExecutionStagePlainWithCodeChanges(t *testing.T) {
 	_, tx1 := memdb.NewTestTx(t)
 	_, tx2 := memdb.NewTestTx(t)
 
-	generateBlocks(t, 1, 50, plainWriterGen(tx1), changeCodeIndepenentlyOfIncarnations)
-	generateBlocks(t, 1, 100, plainWriterGen(tx2), changeCodeIndepenentlyOfIncarnations)
+	generateBlocks(t, 1, 25, plainWriterGen(tx1), changeCodeIndepenentlyOfIncarnations)
+	generateBlocks(t, 1, 50, plainWriterGen(tx2), changeCodeIndepenentlyOfIncarnations)
 
-	err := stages.SaveStageProgress(tx2, stages.Execution, 100)
+	err := stages.SaveStageProgress(tx2, stages.Execution, 50)
 	if err != nil {
 		t.Errorf("error while saving progress: %v", err)
 	}
-	u := &UnwindState{ID: stages.Execution, UnwindPoint: 50}
-	s := &StageState{ID: stages.Execution, BlockNumber: 100}
+	u := &UnwindState{ID: stages.Execution, UnwindPoint: 25}
+	s := &StageState{ID: stages.Execution, BlockNumber: 50}
 	err = UnwindExecutionStage(u, s, tx2, ctx, ExecuteBlockCfg{}, false)
 	if err != nil {
 		t.Errorf("error while unwinding state: %v", err)
@@ -77,15 +77,15 @@ func TestPruneExecution(t *testing.T) {
 	ctx, assert := context.Background(), assert.New(t)
 	_, tx := memdb.NewTestTx(t)
 
-	generateBlocks(t, 1, 50, plainWriterGen(tx), changeCodeIndepenentlyOfIncarnations)
-	err := stages.SaveStageProgress(tx, stages.Execution, 50)
+	generateBlocks(t, 1, 20, plainWriterGen(tx), changeCodeIndepenentlyOfIncarnations)
+	err := stages.SaveStageProgress(tx, stages.Execution, 20)
 	assert.NoError(err)
 
 	available, err := changeset.AvailableFrom(tx)
 	assert.NoError(err)
 	assert.Equal(uint64(1), available)
 
-	s := &PruneState{ID: stages.Execution, ForwardProgress: 50}
+	s := &PruneState{ID: stages.Execution, ForwardProgress: 20}
 	// check pruning distance > than current stage progress
 	err = PruneExecutionStage(s, tx, ExecuteBlockCfg{prune: prune.Mode{History: prune.Distance(100), Receipts: prune.Distance(101), CallTraces: prune.Distance(200)}}, ctx, false)
 	assert.NoError(err)
@@ -99,15 +99,15 @@ func TestPruneExecution(t *testing.T) {
 
 	// pruning distance, first run
 	err = PruneExecutionStage(s, tx, ExecuteBlockCfg{prune: prune.Mode{History: prune.Distance(5),
-		Receipts: prune.Distance(15), CallTraces: prune.Distance(25)}}, ctx, false)
+		Receipts: prune.Distance(10), CallTraces: prune.Distance(15)}}, ctx, false)
 	assert.NoError(err)
 
 	available, err = changeset.AvailableFrom(tx)
 	assert.NoError(err)
-	assert.Equal(uint64(45), available)
+	assert.Equal(uint64(15), available)
 	available, err = changeset.AvailableStorageFrom(tx)
 	assert.NoError(err)
-	assert.Equal(uint64(45), available)
+	assert.Equal(uint64(15), available)
 
 	// pruning distance, second run
 	err = PruneExecutionStage(s, tx, ExecuteBlockCfg{prune: prune.Mode{History: prune.Distance(5),
@@ -116,8 +116,8 @@ func TestPruneExecution(t *testing.T) {
 
 	available, err = changeset.AvailableFrom(tx)
 	assert.NoError(err)
-	assert.Equal(uint64(45), available)
+	assert.Equal(uint64(15), available)
 	available, err = changeset.AvailableStorageFrom(tx)
 	assert.NoError(err)
-	assert.Equal(uint64(45), available)
+	assert.Equal(uint64(15), available)
 }
