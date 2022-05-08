4227a6a7b7c44620cb83de9311aabca4e66e107c
leonardchinonso <leokingluthers@gmail.com>
2022-05-08 05:21:51 +0100

    Committing to save for later edit

--- cmd/devnettest/console/daemon.go
+++ cmd/devnettest/console/daemon.go
@@ -9,9 +9,9 @@ import (
 )
 
 func StartProcess(rpcFlags *utils.RPCFlags) {
-	go rpcdaemon.RunDaemon(rpcFlags)
-	time.Sleep(2 * time.Second)
 	go erigon.RunNode()
+	time.Sleep(2 * time.Second)
+	go rpcdaemon.RunDaemon(rpcFlags)
 	var wg sync.WaitGroup
 	wg.Add(1)
 	wg.Wait()
