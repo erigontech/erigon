b9b8e23b91bf71f6eb81b29f32de33231859ed99
alex.sharov <AskAlexSharov@gmail.com>
2022-05-17 12:28:59 +0700

    save

--- eth/backend.go
+++ eth/backend.go
@@ -278,6 +278,7 @@ func New(stack *node.Node, config *ethconfig.Config, txpoolCfg txpool2.Config, l
 		allSnapshots = snapshotsync.NewRoSnapshots(config.Snapshot, config.SnapDir)
 		allSnapshots.Reopen()
 		fmt.Printf("reopen done: %d\n", allSnapshots.BlocksAvailable())
+		time.Sleep(time.Minute)
 		blockReader = snapshotsync.NewBlockReaderWithSnapshots(allSnapshots)
 
 		if len(stack.Config().DownloaderAddr) > 0 {
