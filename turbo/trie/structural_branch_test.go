2da927b74aafabcc28da18e7382b2866e69f90be
battlmonstr <battlmonstr@users.noreply.github.com>
2022-05-23 13:01:04 +0200

    trie (#4236): skip TestIHCursor until it is fixed (#4237)

--- turbo/trie/structural_branch_test.go
+++ turbo/trie/structural_branch_test.go
@@ -31,6 +31,7 @@ import (
 )
 
 func TestIHCursor(t *testing.T) {
+	t.Skip("FIXME #4236")
 	db, tx := memdb.NewTestTx(t)
 	require := require.New(t)
 	hash := common.HexToHash(fmt.Sprintf("%064d", 0))
