a902cd05de66b2892a567b9072fb2d0f6ed39779
alex.sharov <AskAlexSharov@gmail.com>
2022-05-26 11:29:13 +0700

    merge devel

--- core/rawdb/accessors_chain.go
+++ core/rawdb/accessors_chain.go
@@ -1087,8 +1087,8 @@ func WriteBlock(db kv.RwTx, block *types.Block) error {
 // keeps genesis in db: [1, to)
 // doesn't change sequnces of kv.EthTx and kv.NonCanonicalTxs
 // doesn't delete Reciepts, Senders, Canonical markers, TotalDifficulty
-func DeleteAncientBlocks(db kv.RwTx, blockTo uint64, blocksDeleteLimit int) error {
-	c, err := db.Cursor(kv.Headers)
+func DeleteAncientBlocks(tx kv.RwTx, blockTo uint64, blocksDeleteLimit int) error {
+	c, err := tx.Cursor(kv.Headers)
 	if err != nil {
 		return err
 	}
@@ -1115,13 +1115,13 @@ func DeleteAncientBlocks(db kv.RwTx, blockTo uint64, blocksDeleteLimit int) erro
 			break
 		}
 
-		canonicalHash, err := ReadCanonicalHash(db, n)
+		canonicalHash, err := ReadCanonicalHash(tx, n)
 		if err != nil {
 			return err
 		}
 		isCanonical := bytes.Equal(k[8:], canonicalHash[:])
 
-		b, err := ReadBodyForStorageByKey(db, k)
+		b, err := ReadBodyForStorageByKey(tx, k)
 		if err != nil {
 			return err
 		}
@@ -1135,15 +1135,15 @@ func DeleteAncientBlocks(db kv.RwTx, blockTo uint64, blocksDeleteLimit int) erro
 				if !isCanonical {
 					bucket = kv.NonCanonicalTxs
 				}
-				if err := db.Delete(bucket, txIDBytes, nil); err != nil {
+				if err := tx.Delete(bucket, txIDBytes, nil); err != nil {
 					return err
 				}
 			}
 		}
-		if err := db.Delete(kv.Headers, k, nil); err != nil {
+		if err := tx.Delete(kv.Headers, k, nil); err != nil {
 			return err
 		}
-		if err := db.Delete(kv.BlockBody, k, nil); err != nil {
+		if err := tx.Delete(kv.BlockBody, k, nil); err != nil {
 			return err
 		}
 	}
