9e3570088c5b5d697ef6e4bb0c64b0fb99f204fc
Alex Sharp <alexsharp@Alexs-MacBook-Pro.local>
2022-06-09 20:52:35 +0100

    Fix calltracer

--- cmd/state/commands/erigon22.go
+++ cmd/state/commands/erigon22.go
@@ -323,6 +323,7 @@ func processBlock22(trace bool, txNumStart uint64, rw *ReaderWrapper22, ww *Writ
 		}
 		ibs.Prepare(tx.Hash(), block.Hash(), i)
 		ct := NewCallTracer()
+		vmConfig.Tracer = ct
 		receipt, _, err := core.ApplyTransaction(chainConfig, getHeader, engine, nil, gp, ibs, ww, header, tx, usedGas, vmConfig, nil)
 		if err != nil {
 			return 0, nil, fmt.Errorf("could not apply tx %d [%x] failed: %w", i, tx.Hash(), err)
