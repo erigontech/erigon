2fe79c7e7fb587411cdac054e2a3e4b0e48cd879
Alexey Sharp <alexeysharp@Alexeys-iMac.local>
2022-06-11 16:11:04 +0100

    Print txNums

--- cmd/rpcdaemon22/commands/trace_filtering.go
+++ cmd/rpcdaemon22/commands/trace_filtering.go
@@ -3,6 +3,7 @@ package commands
 import (
 	"context"
 	"fmt"
+	"os"
 	"sort"
 
 	"github.com/RoaringBitmap/roaring/roaring64"
@@ -310,6 +311,12 @@ func (api *TraceAPIImpl) Filter(ctx context.Context, req TraceFilterRequest, str
 	}
 	nSeen := uint64(0)
 	nExported := uint64(0)
+
+	f, _ := os.Create("txnums.txt")
+	for blockNum, txNum := range api._txNums {
+		fmt.Fprintf(f, "%d = %d\n", blockNum, txNum)
+	}
+	f.Close()
 
 	includeAll := len(fromAddresses) == 0 && len(toAddresses) == 0
 	fmt.Printf("allTxs = %d\n", allTxs.ToArray())
