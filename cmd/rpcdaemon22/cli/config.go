5efe2281f3daab61a8ccc11871d340abad327d58
Alexey Sharp <alexeysharp@Alexeys-iMac.local>
2022-06-11 16:30:34 +0100

    Fix

--- cmd/rpcdaemon22/cli/config.go
+++ cmd/rpcdaemon22/cli/config.go
@@ -351,7 +351,7 @@ func RemoteServices(ctx context.Context, cfg httpcfg.HttpCfg, logger log.Logger,
 			if err = allSnapshots.Bodies.View(func(bs []*snapshotsync.BodySegment) error {
 				for _, b := range bs {
 					if err = b.Iterate(func(blockNum, baseTxNum, txAmount uint64) {
-						txNums[blockNum] = baseTxNum + txAmount + 2
+						txNums[blockNum] = baseTxNum + txAmount
 					}); err != nil {
 						return err
 					}
