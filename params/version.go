0d940d45f3257362bdffb07f5ecc2b9455ba778e
Alexey Sharp <alexeysharp@Alexeys-iMac.local>
2022-06-14 15:25:58 +0100

    Fix lint

--- params/version.go
+++ params/version.go
@@ -31,9 +31,9 @@ var (
 
 // see https://calver.org
 const (
-	VersionMajor       = 2022   // Major version component of the current release
-	VersionMinor       = 4      // Minor version component of the current release
-	VersionMicro       = 3      // Patch version component of the current release
+	VersionMajor       = 2022         // Major version component of the current release
+	VersionMinor       = 4            // Minor version component of the current release
+	VersionMicro       = 3            // Patch version component of the current release
 	VersionModifier    = "deprecated" // Patch version component of the current release
 	VersionKeyCreated  = "ErigonVersionCreated"
 	VersionKeyFinished = "ErigonVersionFinished"
