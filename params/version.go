9b45d11841561f80f6f21eb1e0ce7783a3543445
ledgerwatch <akhounov@gmail.com>
2022-06-14 15:05:17 +0100

    Update version.go

--- params/version.go
+++ params/version.go
@@ -33,8 +33,8 @@ var (
 const (
 	VersionMajor       = 2022   // Major version component of the current release
 	VersionMinor       = 4      // Minor version component of the current release
-	VersionMicro       = 2      // Patch version component of the current release
-	VersionModifier    = "beta" // Patch version component of the current release
+	VersionMicro       = 3      // Patch version component of the current release
+	VersionModifier    = "deprecated" // Patch version component of the current release
 	VersionKeyCreated  = "ErigonVersionCreated"
 	VersionKeyFinished = "ErigonVersionFinished"
 )
