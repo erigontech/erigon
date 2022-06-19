307cc40f2c3c8eb36b67c7096e13a17422817c93
ledgerwatch <akhounov@gmail.com>
2022-06-19 06:34:45 +0100

    Update skip_analysis.go

--- core/skip_analysis.go
+++ core/skip_analysis.go
@@ -29,9 +29,9 @@ import (
 // where codeBitmap was useful. Invalid jumps either did not occur, or were
 // prevented simply by checking whether the jump destination has JUMPDEST opcode
 // Mainnet transactions that use jumpdest analysis are:
+// 0x3666640316df11865abd1352f4c0b4c5126f8ac1d858ef2a0c6e744a4865bca2 (block 5800596)
 // 0x88a1f2a9f048a21fd944b28ad9962f533ab5d3c40e17b1bc3f99ae999a4021b2 (block 6426432)
 // 0x86e55d1818b5355424975de9633a57c40789ca08552297b726333a9433949c92 (block 6426298)
-// 0x3666640316df11865abd1352f4c0b4c5126f8ac1d858ef2a0c6e744a4865bca2 (block 5800596)
 // 0xcdb5bf0b4b51093e1c994f471921f88623c9d3e1b6aa2782049f53a0048f2b32 (block 11079912)
 // 0x21ab7bf7245a87eae265124aaf180d91133377e47db2b1a4866493ec4b371150 (block 13119520)
 
