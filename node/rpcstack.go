8a42ccb5e7535cf579be408ae9b3e18acc76c022
Alexey Sharp <alexeysharp@Alexeys-iMac.local>
2022-06-19 10:58:00 +0100

    Fix lint

--- node/rpcstack.go
+++ node/rpcstack.go
@@ -223,23 +223,6 @@ func checkPath(r *http.Request, path string) bool {
 	return len(r.URL.Path) >= len(path) && r.URL.Path[:len(path)] == path
 }
 
-// validatePrefix checks if 'path' is a valid configuration value for the RPC prefix option.
-func validatePrefix(what, path string) error {
-	if path == "" {
-		return nil
-	}
-	if path[0] != '/' {
-		return fmt.Errorf(`%s RPC path prefix %q does not contain leading "/"`, what, path)
-	}
-	if strings.ContainsAny(path, "?#") {
-		// This is just to avoid confusion. While these would match correctly (i.e. they'd
-		// match if URL-escaped into path), it's not easy to understand for users when
-		// setting that on the command line.
-		return fmt.Errorf("%s RPC path prefix %q contains URL meta-characters", what, path)
-	}
-	return nil
-}
-
 // stop shuts down the HTTP server.
 func (h *httpServer) stop() {
 	h.mu.Lock()
