88b1a6ef382572493d8c6ab7482fbfa63b3b5956
Giulio Rebuffo <giulio.rebuffo@gmail.com>
2022-06-30 02:26:57 +0200

    done

--- ethdb/privateapi/ethbackend.go
+++ ethdb/privateapi/ethbackend.go
@@ -278,20 +278,6 @@ func (s *EthBackendServer) EngineNewPayloadV1(ctx context.Context, req *types2.E
 		log.Error("[NewPayload] not a proof-of-stake chain")
 		return nil, fmt.Errorf("not a proof-of-stake chain")
 	}
-	// If another payload is already commissioned then we just reply with syncing
-	if s.stageLoopIsBusy() {
-		// We are still syncing a commissioned payload
-		// TODO(yperbasis): not entirely correct since per the spec:
-		// The process of validating a payload on the canonical chain MUST NOT be affected by an active sync process on a side branch of the block tree.
-		// For example, if side branch B is SYNCING but the requisite data for validating a payload from canonical branch A is available, client software MUST initiate the validation process.
-		// https://github.com/ethereum/execution-apis/blob/v1.0.0-alpha.6/src/engine/specification.md#payload-validation
-		log.Debug("[NewPayload] stage loop is busy")
-		return &remote.EnginePayloadStatus{Status: remote.EngineStatus_SYNCING}, nil
-	}
-	log.Debug("[NewPayload] acquiring lock")
-	s.lock.Lock()
-	defer s.lock.Unlock()
-	log.Debug("[NewPayload] lock acquired")
 
 	var baseFee *big.Int
 	eip1559 := false
@@ -300,7 +286,6 @@ func (s *EthBackendServer) EngineNewPayloadV1(ctx context.Context, req *types2.E
 		baseFee = gointerfaces.ConvertH256ToUint256Int(req.BaseFeePerGas).ToBig()
 		eip1559 = true
 	}
-
 	header := types.Header{
 		ParentHash:  gointerfaces.ConvertH256ToHash(req.ParentHash),
 		Coinbase:    gointerfaces.ConvertH160toAddress(req.Coinbase),
@@ -322,16 +307,13 @@ func (s *EthBackendServer) EngineNewPayloadV1(ctx context.Context, req *types2.E
 	}
 
 	blockHash := gointerfaces.ConvertH256ToHash(req.BlockHash)
-	if header.Hash() != blockHash {
-		log.Error("[NewPayload] invalid block hash", "stated", common.Hash(blockHash), "actual", header.Hash())
-		return &remote.EnginePayloadStatus{Status: remote.EngineStatus_INVALID_BLOCK_HASH}, nil
-	}
 
 	tx, err := s.db.BeginRo(ctx)
 	if err != nil {
 		return nil, err
 	}
 	defer tx.Rollback()
+
 	parentTd, err := rawdb.ReadTd(tx, header.ParentHash, req.BlockNumber-1)
 	if err != nil {
 		return nil, err
@@ -341,6 +323,27 @@ func (s *EthBackendServer) EngineNewPayloadV1(ctx context.Context, req *types2.E
 		return &remote.EnginePayloadStatus{Status: remote.EngineStatus_INVALID, LatestValidHash: gointerfaces.ConvertHashToH256(common.Hash{})}, nil
 	}
 	tx.Rollback()
+	// If another payload is already commissioned then we just reply with syncing
+	if s.stageLoopIsBusy() {
+		// We are still syncing a commissioned payload
+		// TODO(yperbasis): not entirely correct since per the spec:
+		// The process of validating a payload on the canonical chain MUST NOT be affected by an active sync process on a side branch of the block tree.
+		// For example, if side branch B is SYNCING but the requisite data for validating a payload from canonical branch A is available, client software MUST initiate the validation process.
+		// https://github.com/ethereum/execution-apis/blob/v1.0.0-alpha.6/src/engine/specification.md#payload-validation
+		log.Debug("[NewPayload] stage loop is busy")
+		return &remote.EnginePayloadStatus{Status: remote.EngineStatus_SYNCING}, nil
+	}
+
+	if header.Hash() != blockHash {
+		log.Error("[NewPayload] invalid block hash", "stated", common.Hash(blockHash), "actual", header.Hash())
+		return &remote.EnginePayloadStatus{Status: remote.EngineStatus_INVALID_BLOCK_HASH}, nil
+	}
+
+	// Lock the thread (We modify shared resources).
+	log.Debug("[NewPayload] acquiring lock")
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	log.Debug("[NewPayload] lock acquired")
 
 	log.Debug("[NewPayload] sending block", "height", header.Number, "hash", common.Hash(blockHash))
 	s.requestList.AddPayloadRequest(&engineapi.PayloadMessage{
@@ -421,17 +424,6 @@ func (s *EthBackendServer) EngineForkChoiceUpdatedV1(ctx context.Context, req *r
 		return nil, fmt.Errorf("not a proof-of-stake chain")
 	}
 
-	if s.stageLoopIsBusy() {
-		log.Debug("[ForkChoiceUpdated] stage loop is busy")
-		return &remote.EngineForkChoiceUpdatedReply{
-			PayloadStatus: &remote.EnginePayloadStatus{Status: remote.EngineStatus_SYNCING},
-		}, nil
-	}
-	log.Debug("[ForkChoiceUpdated] acquiring lock")
-	s.lock.Lock()
-	defer s.lock.Unlock()
-	log.Debug("[ForkChoiceUpdated] lock acquired")
-
 	forkChoice := engineapi.ForkChoiceMessage{
 		HeadBlockHash:      gointerfaces.ConvertH256ToHash(req.ForkchoiceState.HeadBlockHash),
 		SafeBlockHash:      gointerfaces.ConvertH256ToHash(req.ForkchoiceState.SafeBlockHash),
@@ -454,6 +446,18 @@ func (s *EthBackendServer) EngineForkChoiceUpdatedV1(ctx context.Context, req *r
 			PayloadStatus: &remote.EnginePayloadStatus{Status: remote.EngineStatus_INVALID, LatestValidHash: gointerfaces.ConvertHashToH256(common.Hash{})},
 		}, nil
 	}
+
+	if s.stageLoopIsBusy() {
+		log.Debug("[ForkChoiceUpdated] stage loop is busy")
+		return &remote.EngineForkChoiceUpdatedReply{
+			PayloadStatus: &remote.EnginePayloadStatus{Status: remote.EngineStatus_SYNCING},
+		}, nil
+	}
+
+	log.Debug("[ForkChoiceUpdated] acquiring lock")
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	log.Debug("[ForkChoiceUpdated] lock acquired")
 
 	log.Debug("[ForkChoiceUpdated] sending forkChoiceMessage", "head", forkChoice.HeadBlockHash)
 	s.requestList.AddForkChoiceRequest(&forkChoice)
