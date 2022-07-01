ed452dbc4b210fe261677bffc131d3ff3adae51e
Alex Sharov <AskAlexSharov@gmail.com>
2022-07-01 10:20:32 +0600

    save (#509)

--- direct/downloader_client.go
+++ direct/downloader_client.go
@@ -19,6 +19,9 @@ func NewDownloaderClient(server proto_downloader.DownloaderServer) *DownloaderCl
 func (c *DownloaderClient) Download(ctx context.Context, in *proto_downloader.DownloadRequest, opts ...grpc.CallOption) (*emptypb.Empty, error) {
 	return c.server.Download(ctx, in)
 }
+func (c *DownloaderClient) Verify(ctx context.Context, in *proto_downloader.VerifyRequest, opts ...grpc.CallOption) (*emptypb.Empty, error) {
+	return c.server.Verify(ctx, in)
+}
 func (c *DownloaderClient) Stats(ctx context.Context, in *proto_downloader.StatsRequest, opts ...grpc.CallOption) (*proto_downloader.StatsReply, error) {
 	return c.server.Stats(ctx, in)
 }
