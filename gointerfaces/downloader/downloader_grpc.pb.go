2a6be9cb923e47a70ce1db02d06295540ded157c
Alex Sharov <AskAlexSharov@gmail.com>
2022-07-01 10:06:58 +0600

    Downloader verify (#507)
    * Remove interfaces for replacement
    
    * Remove interfaces for replacement
    
    * Remove interfaces for replacement
    
    * Squashed 'interfaces/' content from commit f047f61c
    
    git-subtree-dir: interfaces
    git-subtree-split: f047f61c2a0388aeeef2750cab1045f7212acfcc
    
    * save

--- gointerfaces/downloader/downloader_grpc.pb.go
+++ gointerfaces/downloader/downloader_grpc.pb.go
@@ -24,6 +24,7 @@ const _ = grpc.SupportPackageIsVersion7
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type DownloaderClient interface {
 	Download(ctx context.Context, in *DownloadRequest, opts ...grpc.CallOption) (*emptypb.Empty, error)
+	Verify(ctx context.Context, in *VerifyRequest, opts ...grpc.CallOption) (*emptypb.Empty, error)
 	Stats(ctx context.Context, in *StatsRequest, opts ...grpc.CallOption) (*StatsReply, error)
 }
 
@@ -44,6 +45,15 @@ func (c *downloaderClient) Download(ctx context.Context, in *DownloadRequest, op
 	return out, nil
 }
 
+func (c *downloaderClient) Verify(ctx context.Context, in *VerifyRequest, opts ...grpc.CallOption) (*emptypb.Empty, error) {
+	out := new(emptypb.Empty)
+	err := c.cc.Invoke(ctx, "/downloader.Downloader/Verify", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 func (c *downloaderClient) Stats(ctx context.Context, in *StatsRequest, opts ...grpc.CallOption) (*StatsReply, error) {
 	out := new(StatsReply)
 	err := c.cc.Invoke(ctx, "/downloader.Downloader/Stats", in, out, opts...)
@@ -58,6 +68,7 @@ func (c *downloaderClient) Stats(ctx context.Context, in *StatsRequest, opts ...
 // for forward compatibility
 type DownloaderServer interface {
 	Download(context.Context, *DownloadRequest) (*emptypb.Empty, error)
+	Verify(context.Context, *VerifyRequest) (*emptypb.Empty, error)
 	Stats(context.Context, *StatsRequest) (*StatsReply, error)
 	mustEmbedUnimplementedDownloaderServer()
 }
@@ -68,6 +79,9 @@ type UnimplementedDownloaderServer struct {
 
 func (UnimplementedDownloaderServer) Download(context.Context, *DownloadRequest) (*emptypb.Empty, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Download not implemented")
+}
+func (UnimplementedDownloaderServer) Verify(context.Context, *VerifyRequest) (*emptypb.Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Verify not implemented")
 }
 func (UnimplementedDownloaderServer) Stats(context.Context, *StatsRequest) (*StatsReply, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Stats not implemented")
@@ -103,6 +117,24 @@ func _Downloader_Download_Handler(srv interface{}, ctx context.Context, dec func
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Downloader_Verify_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(VerifyRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(DownloaderServer).Verify(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/downloader.Downloader/Verify",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(DownloaderServer).Verify(ctx, req.(*VerifyRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 func _Downloader_Stats_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(StatsRequest)
 	if err := dec(in); err != nil {
@@ -131,6 +163,10 @@ var Downloader_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "Download",
 			Handler:    _Downloader_Download_Handler,
+		},
+		{
+			MethodName: "Verify",
+			Handler:    _Downloader_Verify_Handler,
 		},
 		{
 			MethodName: "Stats",
