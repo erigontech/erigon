f745313c654f5729ce48eb018ecb9f417a6fbf4a
Alex Sharp <alexsharp@Alexs-MacBook-Pro.local>
2022-07-02 23:05:18 +0100

    Init

---
+++ cmd/state/commands/state_recon_1.go
@@ -0,0 +1,87 @@
+package commands
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"os/signal"
+	"path"
+	"runtime"
+	"syscall"
+
+	kv2 "github.com/ledgerwatch/erigon-lib/kv/mdbx"
+	"github.com/ledgerwatch/erigon/core"
+	"github.com/ledgerwatch/erigon/eth/ethconfig"
+	"github.com/ledgerwatch/erigon/turbo/services"
+	"github.com/ledgerwatch/erigon/turbo/snapshotsync"
+	"github.com/ledgerwatch/log/v3"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	withBlock(recon1Cmd)
+	withDataDir(recon1Cmd)
+	rootCmd.AddCommand(recon1Cmd)
+}
+
+var recon1Cmd = &cobra.Command{
+	Use:   "recon1",
+	Short: "Exerimental command to reconstitute the state at given block",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		logger := log.New()
+		return Recon1(genesis, logger)
+	},
+}
+
+func Recon1(genesis *core.Genesis, logger log.Logger) error {
+	sigs := make(chan os.Signal, 1)
+	interruptCh := make(chan bool, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-sigs
+		interruptCh <- true
+	}()
+	ctx := context.Background()
+	reconDbPath := path.Join(datadir, "recon1db")
+	var err error
+	if _, err = os.Stat(reconDbPath); err != nil {
+		if !errors.Is(err, os.ErrNotExist) {
+			return err
+		}
+	} else if err = os.RemoveAll(reconDbPath); err != nil {
+		return err
+	}
+	db, err := kv2.NewMDBX(logger).Path(reconDbPath).WriteMap().Open()
+	if err != nil {
+		return err
+	}
+	var blockReader services.FullBlockReader
+	var allSnapshots *snapshotsync.RoSnapshots
+	allSnapshots = snapshotsync.NewRoSnapshots(ethconfig.NewSnapCfg(true, false, true), path.Join(datadir, "snapshots"))
+	defer allSnapshots.Close()
+	if err := allSnapshots.Reopen(); err != nil {
+		return fmt.Errorf("reopen snapshot segments: %w", err)
+	}
+	blockReader = snapshotsync.NewBlockReaderWithSnapshots(allSnapshots)
+	// Compute mapping blockNum -> last TxNum in that block
+	txNums := make([]uint64, allSnapshots.BlocksAvailable()+1)
+	if err = allSnapshots.Bodies.View(func(bs []*snapshotsync.BodySegment) error {
+		for _, b := range bs {
+			if err = b.Iterate(func(blockNum, baseTxNum, txAmount uint64) {
+				txNums[blockNum] = baseTxNum + txAmount
+			}); err != nil {
+				return err
+			}
+		}
+		return nil
+	}); err != nil {
+		return fmt.Errorf("build txNum => blockNum mapping: %w", err)
+	}
+	blockNum := block + 1
+	txNum := txNums[blockNum-1]
+	fmt.Printf("Corresponding block num = %d, txNum = %d\n", blockNum, txNum)
+	workerCount := runtime.NumCPU()
+	return nil
+}
---
+++ core/state/recon_state_1.go
@@ -0,0 +1,73 @@
+package state
+
+import "sync"
+
+type ReconState1 struct {
+	lock sync.RWMutex
+	queue         theap[uint64]
+	changes       map[string]map[string][]byte
+	sizeEstimate  uint64
+	rollbackCount uint64
+}
+
+func NewReconState1() *ReconState1 {
+	rs := &ReconState1{
+		changes:  map[string]map[string][]byte{},
+	}
+	return rs
+}
+
+func (rs *ReconState1) Put(table string, key, val []byte) {
+	rs.lock.Lock()
+	defer rs.lock.Unlock()
+	t, ok := rs.changes[table]
+	if !ok {
+		t = map[string][]byte{}
+		rs.changes[table] = t
+	}
+	t[string(key)] = val
+	rs.sizeEstimate += uint64(len(key)) + uint64(len(val))
+}
+
+func (rs *ReconState1) Delete(table string, key []byte) {
+	rs.lock.Lock()
+	defer rs.lock.Unlock()
+	t, ok := rs.changes[table]
+	if !ok {
+		t = map[string][]byte{}
+		rs.changes[table] = t
+	}
+	t[string(key)] = nil
+	rs.sizeEstimate += uint64(len(key))
+}
+
+func (rs *ReconState1) Get(table string, key []byte) []byte {
+	rs.lock.RLock()
+	defer rs.lock.RUnlock()
+	t, ok := rs.changes[table]
+	if !ok {
+		return nil
+	}
+	return t[string(key)]
+}
+
+func (rs *ReconState1) Flush(rwTx kv.RwTx) error {
+	rs.lock.Lock()
+	defer rs.lock.Unlock()
+	for table, t := range rs.changes {
+		for ks, val := range t {
+			if len(val) == 0 {
+				if err := rwTx.Delete(table, []byte(ks), nil); err != nil {
+					return err
+				}
+			} else {
+				if err := rwTx.Put(table, []byte(ks), val); err != nil {
+					return err
+				}
+			}
+		}
+	}
+	rs.changes = map[string]map[string][]byte{}
+	rs.sizeEstimate = 0
+	return nil
+}
