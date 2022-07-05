4636ef29ef8892f051181517d7540367b4fd15b1
Igor Mandrigin <i@mandrigin.ru>
2022-07-05 16:34:03 +0200

    rename logs

--- node/endpoints.go
+++ node/endpoints.go
@@ -48,7 +48,7 @@ func StartHTTPEndpoint(endpoint string, timeouts rpccfg.HTTPTimeouts, handler ht
 	}
 	go func() {
 		errServe := httpSrv.Serve(listener)
-		fmt.Println("IGORM: HTTP server stopped", errServe)
+		fmt.Println("ISSUE_4543: HTTP server stopped", "endpoint", endpoint, "err", errServe)
 	}()
 	return httpSrv, listener.Addr(), err
 }
