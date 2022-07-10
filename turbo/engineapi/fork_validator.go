7f29fa955f87bb0e330b93532ab52c49649720f1
Giulio Rebuffo <giulio.rebuffo@gmail.com>
2022-07-11 01:38:25 +0200

    32 blocks maxForkDepth

--- turbo/engineapi/fork_validator.go
+++ turbo/engineapi/fork_validator.go
@@ -1,3 +1,16 @@
+/*
+   Copyright 2022 Erigon contributors
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 package engineapi
 
 import (
@@ -13,7 +26,7 @@ import (
 )
 
 // the maximum point from the current head, past which side forks are not validated anymore.
-const maxForkDepth = 16
+const maxForkDepth = 32 // 32 slots is the duration of an epoch thus there cannot be side forks in PoS deeper than 32 blocks from head.
 
 type validatePayloadFunc func(kv.RwTx, *types.Header, *types.RawBody, uint64, []*types.Header, []*types.RawBody) error
 
