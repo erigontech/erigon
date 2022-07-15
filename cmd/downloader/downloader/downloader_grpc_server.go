5b40cf390f428e0259b647e846a7be04b429d332
Enrique Avila <eavilaasapche@gmail.com>
2022-07-15 16:26:31 +0300

    refactored Download

--- cmd/downloader/downloader/downloader_grpc_server.go
+++ cmd/downloader/downloader/downloader_grpc_server.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/anacrolix/torrent"
 	"github.com/anacrolix/torrent/metainfo"
 	"github.com/ledgerwatch/erigon-lib/gointerfaces"
 	proto_downloader "github.com/ledgerwatch/erigon-lib/gointerfaces/downloader"
@@ -32,50 +33,35 @@ func (s *GrpcServer) Download(ctx context.Context, request *proto_downloader.Dow
 	defer logEvery.Stop()
 
 	torrentClient := s.d.Torrent()
-	mi := &metainfo.MetaInfo{AnnounceList: Trackers}
+	snapDir := s.d.SnapDir()
 	for i, it := range request.Items {
-		var hash *metainfo.Hash
-		if it.TorrentHash == nil { // seed new snapshot
-			if err := BuildTorrentFileIfNeed(it.Path, s.d.SnapDir()); err != nil {
+		var ok bool
+		var err error
+		if it.TorrentHash == nil {
+			// if we dont have the torrent hash then we seed a new snapshot
+			ok, err = seedNewSnapshot(it, torrentClient, snapDir)
+			if err != nil {
 				return nil, err
-			}
-		} else {
-			*hash = Proto2InfoHash(it.TorrentHash)
-			if _, ok := torrentClient.Torrent(*hash); ok {
-				continue
 			}
 		}
 
-		ok, err := AddSegment(it.Path, s.d.SnapDir(), torrentClient)
-		if err != nil {
-			return nil, fmt.Errorf("AddSegment: %w", err)
-		}
 		select {
 		case <-logEvery.C:
-			log.Info("[snpshots] initializing", "files", fmt.Sprintf("%d/%d", i, len(request.Items)))
+			log.Info("[snapshots] initializing", "files", fmt.Sprintf("%d/%d", i, len(request.Items)))
 		default:
 		}
 		if ok {
 			continue
 		}
 
-		magnet := mi.Magnet(hash, nil)
-		go func(magnetUrl string) {
-			t, err := torrentClient.AddMagnet(magnetUrl)
-			if err != nil {
-				log.Warn("[downloader] add magnet link", "err", err)
-				return
-			}
-			t.DisallowDataDownload()
-			t.AllowDataUpload()
-			<-t.GotInfo()
+		ok, err = createMagnetLinkWithInfoHash(it.TorrentHash, torrentClient, snapDir)
+		if err != nil {
+			return nil, err
+		}
 
-			mi := t.Metainfo()
-			if err := CreateTorrentFileIfNotExists(s.d.SnapDir(), t.Info(), &mi); err != nil {
-				log.Warn("[downloader] create torrent file", "err", err)
-				return
-			}
-		}(magnet.String())
+		if ok {
+			continue
+		}
 	}
 	s.d.ReCalcStats(10 * time.Second) // immediately call ReCalc to set stat.Complete flag
 	return &emptypb.Empty{}, nil
@@ -110,4 +96,53 @@ func (s *GrpcServer) Stats(ctx context.Context, request *proto_downloader.StatsR
 
 func Proto2InfoHash(in *prototypes.H160) metainfo.Hash {
 	return gointerfaces.ConvertH160toAddress(in)
+}
+
+func seedNewSnapshot(it *proto_downloader.DownloadItem, torrentClient *torrent.Client, snapDir string) (bool, error) {
+	// if we dont have the torrent file we build it
+	if err := BuildTorrentFileIfNeed(it.Path, snapDir); err != nil {
+		return false, err
+	}
+
+	ok, err := AddSegment(it.Path, snapDir, torrentClient)
+	if err != nil {
+		return false, fmt.Errorf("AddSegment: %w", err)
+	}
+
+	if !ok {
+		return createMagnetLinkWithInfoHash(nil, torrentClient, snapDir)
+	}
+
+	return true, nil
+}
+
+// we dont have .seg or .torrent so we get them through the torrent hash
+func createMagnetLinkWithInfoHash(hash *prototypes.H160, torrentClient *torrent.Client, snapDir string) (bool, error) {
+	mi := &metainfo.MetaInfo{AnnounceList: Trackers}
+	var infoHash *metainfo.Hash
+	if hash != nil {
+		*infoHash = Proto2InfoHash(hash)
+		if _, ok := torrentClient.Torrent(*infoHash); ok {
+			return true, nil
+		}
+	}
+	magnet := mi.Magnet(infoHash, nil)
+	go func(magnetUrl string) {
+		t, err := torrentClient.AddMagnet(magnetUrl)
+		if err != nil {
+			log.Warn("[downloader] add magnet link", "err", err)
+			return
+		}
+		t.DisallowDataDownload()
+		t.AllowDataUpload()
+		<-t.GotInfo()
+
+		mi := t.Metainfo()
+		if err := CreateTorrentFileIfNotExists(snapDir, t.Info(), &mi); err != nil {
+			log.Warn("[downloader] create torrent file", "err", err)
+			return
+		}
+	}(magnet.String())
+
+	return false, nil
 }
