5821ae7bbc8815c4a9f503d504a42fba2c5a64de
TKartist <95535167+TKartist@users.noreply.github.com>
2022-07-18 09:27:03 +0200

    kv_mdbx_test updated (#522)
    * tests for GetOne and Put added
    
    * test configured
    
    * multiple tests added
    
    * conflict resolve

--- kv/mdbx/kv_mdbx_test.go
+++ kv/mdbx/kv_mdbx_test.go
@@ -142,7 +142,7 @@ func TestPutGet(t *testing.T) {
 	require.Nil(t, v)
 }
 
-func TestIncrementSequence(t *testing.T) {
+func TestIncrementRead(t *testing.T) {
 	path := t.TempDir()
 	logger := log.New()
 	table := "Table"
@@ -169,8 +169,275 @@ func TestIncrementSequence(t *testing.T) {
 	require.NoError(t, tx.Put(table, []byte("key4"), []byte("value4.1")))
 	require.NoError(t, tx.Put(table, []byte("key5"), []byte("value5.1")))
 
-	tx.IncrementSequence(table, uint64(12))
+	_, err = tx.IncrementSequence(table, uint64(12))
+	require.Nil(t, err)
 	chaV, err := tx.ReadSequence(table)
 	require.Nil(t, err)
 	require.Equal(t, chaV, uint64(0xc))
+	_, err = tx.IncrementSequence(table, uint64(240))
+	require.Nil(t, err)
+	chaV, err = tx.ReadSequence(table)
+	require.Nil(t, err)
+	require.Equal(t, chaV, uint64(0xfc))
+}
+
+func TestHasDelete(t *testing.T) {
+	path := t.TempDir()
+	logger := log.New()
+	table := "Table"
+	db := NewMDBX(logger).Path(path).WithTablessCfg(func(defaultBuckets kv.TableCfg) kv.TableCfg {
+		return kv.TableCfg{
+			table: kv.TableCfgItem{Flags: kv.DupSort},
+		}
+	}).MustOpen()
+	defer db.Close()
+
+	tx, err := db.BeginRw(context.Background())
+	require.NoError(t, err)
+	defer tx.Rollback()
+
+	c, err := tx.RwCursorDupSort(table)
+	require.NoError(t, err)
+	defer c.Close()
+
+	require.NoError(t, tx.Put(table, []byte("key1"), []byte("value1.1")))
+	require.NoError(t, tx.Put(table, []byte("key2"), []byte("value2.1")))
+	require.NoError(t, tx.Put(table, []byte("key3"), []byte("value3.1")))
+	require.NoError(t, tx.Put(table, []byte("key4"), []byte("value4.1")))
+	require.NoError(t, tx.Put(table, []byte("key5"), []byte("value5.1")))
+
+	require.NoError(t, tx.Delete(table, []byte("key1"), []byte("value1.1")))
+	require.NoError(t, tx.Delete(table, []byte("key1"), []byte("value1.1"))) //valid but already deleted
+	require.NoError(t, tx.Delete(table, []byte("key2"), []byte("value1.1"))) //valid key but wrong value
+
+	res, err := tx.Has(table, []byte("key1"))
+	require.Nil(t, err)
+	require.False(t, res)
+
+	res, err = tx.Has(table, []byte("key2"))
+	require.Nil(t, err)
+	require.True(t, res)
+
+	res, err = tx.Has(table, []byte("key3"))
+	require.Nil(t, err)
+	require.True(t, res)
+
+	res, err = tx.Has(table, []byte("k"))
+	require.Nil(t, err)
+	require.False(t, res)
+}
+
+func TestForAmount(t *testing.T) {
+	path := t.TempDir()
+	logger := log.New()
+	table := "Table"
+	db := NewMDBX(logger).Path(path).WithTablessCfg(func(defaultBuckets kv.TableCfg) kv.TableCfg {
+		return kv.TableCfg{
+			table:       kv.TableCfgItem{Flags: kv.DupSort},
+			kv.Sequence: kv.TableCfgItem{},
+		}
+	}).MustOpen()
+	defer db.Close()
+
+	tx, err := db.BeginRw(context.Background())
+	require.NoError(t, err)
+	defer tx.Rollback()
+
+	c, err := tx.RwCursorDupSort(table)
+	require.NoError(t, err)
+	defer c.Close()
+
+	require.NoError(t, tx.Put(table, []byte("key1"), []byte("value1.1")))
+	require.NoError(t, tx.Put(table, []byte("key2"), []byte("value2.1")))
+	require.NoError(t, tx.Put(table, []byte("key3"), []byte("value3.1")))
+	require.NoError(t, tx.Put(table, []byte("key4"), []byte("value4.1")))
+	require.NoError(t, tx.Put(table, []byte("key5"), []byte("value5.1")))
+
+	var keys []string
+
+	err = tx.ForAmount(table, []byte("key3"), uint32(2), func(k, v []byte) error {
+		keys = append(keys, string(k))
+		return nil
+	})
+	require.Nil(t, err)
+	require.Equal(t, []string{"key3", "key4"}, keys)
+
+	var keys1 []string
+
+	err1 := tx.ForAmount(table, []byte("key1"), 100, func(k, v []byte) error {
+		keys1 = append(keys1, string(k))
+		return nil
+	})
+	require.Nil(t, err1)
+	require.Equal(t, []string{"key1", "key2", "key3", "key4", "key5"}, keys1)
+
+	var keys2 []string
+
+	err2 := tx.ForAmount(table, []byte("value"), 100, func(k, v []byte) error {
+		keys2 = append(keys2, string(k))
+		return nil
+	})
+	require.Nil(t, err2)
+	require.Nil(t, keys2)
+
+	var keys3 []string
+
+	err3 := tx.ForAmount(table, []byte("key1"), 0, func(k, v []byte) error {
+		keys3 = append(keys3, string(k))
+		return nil
+	})
+	require.Nil(t, err3)
+	require.Nil(t, keys3)
+}
+
+func TestForPrefix(t *testing.T) {
+	path := t.TempDir()
+	logger := log.New()
+	table := "Table"
+	db := NewMDBX(logger).Path(path).WithTablessCfg(func(defaultBuckets kv.TableCfg) kv.TableCfg {
+		return kv.TableCfg{
+			table:       kv.TableCfgItem{Flags: kv.DupSort},
+			kv.Sequence: kv.TableCfgItem{},
+		}
+	}).MustOpen()
+	defer db.Close()
+
+	tx, err := db.BeginRw(context.Background())
+	require.NoError(t, err)
+	defer tx.Rollback()
+
+	c, err := tx.RwCursorDupSort(table)
+	require.NoError(t, err)
+	defer c.Close()
+
+	require.NoError(t, tx.Put(table, []byte("key1"), []byte("value1.1")))
+	require.NoError(t, tx.Put(table, []byte("key2"), []byte("value2.1")))
+	require.NoError(t, tx.Put(table, []byte("key3"), []byte("value3.1")))
+	require.NoError(t, tx.Put(table, []byte("key4"), []byte("value4.1")))
+	require.NoError(t, tx.Put(table, []byte("key5"), []byte("value5.1")))
+
+	var keys []string
+
+	err = tx.ForPrefix(table, []byte("key"), func(k, v []byte) error {
+		keys = append(keys, string(k))
+		return nil
+	})
+	require.Nil(t, err)
+	require.Equal(t, []string{"key1", "key2", "key3", "key4", "key5"}, keys)
+
+	var keys1 []string
+
+	err = tx.ForPrefix(table, []byte("key1"), func(k, v []byte) error {
+		keys1 = append(keys1, string(k))
+		return nil
+	})
+	require.Nil(t, err)
+	require.Equal(t, []string{"key1"}, keys1)
+
+	var keys2 []string
+
+	err = tx.ForPrefix(table, []byte("e"), func(k, v []byte) error {
+		keys2 = append(keys2, string(k))
+		return nil
+	})
+	require.Nil(t, err)
+	require.Nil(t, keys2)
+}
+
+func TestAppendFirstLast(t *testing.T) {
+	path := t.TempDir()
+	logger := log.New()
+	table := "Table"
+	db := NewMDBX(logger).Path(path).WithTablessCfg(func(defaultBuckets kv.TableCfg) kv.TableCfg {
+		return kv.TableCfg{
+			table:       kv.TableCfgItem{Flags: kv.DupSort},
+			kv.Sequence: kv.TableCfgItem{},
+		}
+	}).MustOpen()
+	defer db.Close()
+
+	tx, err := db.BeginRw(context.Background())
+	require.NoError(t, err)
+	defer tx.Rollback()
+
+	c, err := tx.RwCursorDupSort(table)
+	require.NoError(t, err)
+	defer c.Close()
+
+	require.NoError(t, tx.Append(table, []byte("key1"), []byte("value1.1")))
+	require.NoError(t, tx.Append(table, []byte("key3"), []byte("value3.1")))
+	require.Error(t, tx.Append(table, []byte("key2"), []byte("value2.1")))
+	require.NoError(t, tx.Append(table, []byte("key6"), []byte("value6.1")))
+	require.Error(t, tx.Append(table, []byte("key4"), []byte("value4.1")))
+	require.NoError(t, tx.Put(table, []byte("key2"), []byte("value1.11")))
+
+	k, v, err := c.First()
+	require.Nil(t, err)
+	require.Equal(t, k, []byte("key1"))
+	require.Equal(t, v, []byte("value1.1"))
+
+	k, v, err = c.Last()
+	require.Nil(t, err)
+	require.Equal(t, k, []byte("key6"))
+	require.Equal(t, v, []byte("value6.1"))
+}
+
+func TestNextPrevCurrent(t *testing.T) {
+	path := t.TempDir()
+	logger := log.New()
+	table := "Table"
+	db := NewMDBX(logger).Path(path).WithTablessCfg(func(defaultBuckets kv.TableCfg) kv.TableCfg {
+		return kv.TableCfg{
+			table:       kv.TableCfgItem{Flags: kv.DupSort},
+			kv.Sequence: kv.TableCfgItem{},
+		}
+	}).MustOpen()
+	defer db.Close()
+
+	tx, err := db.BeginRw(context.Background())
+	require.NoError(t, err)
+	defer tx.Rollback()
+
+	c, err := tx.RwCursorDupSort(table)
+	require.NoError(t, err)
+	defer c.Close()
+
+	require.NoError(t, tx.Append(table, []byte("key1"), []byte("value1.1")))
+	require.NoError(t, tx.Append(table, []byte("key3"), []byte("value3.1")))
+	require.NoError(t, tx.Put(table, []byte("key2"), []byte("value1.11")))
+
+	k, v, err := c.Next()
+	require.Nil(t, err)
+	require.Equal(t, k, []byte("key1"))
+	require.Equal(t, v, []byte("value1.1"))
+
+	k, v, err = c.Next()
+	require.Nil(t, err)
+	require.Equal(t, k, []byte("key2"))
+	require.Equal(t, v, []byte("value1.11"))
+
+	k, v, err = c.Current()
+	require.Nil(t, err)
+	require.Equal(t, k, []byte("key2"))
+	require.Equal(t, v, []byte("value1.11"))
+
+	k, v, err = c.Next()
+	require.Nil(t, err)
+	require.Equal(t, k, []byte("key3"))
+	require.Equal(t, v, []byte("value3.1"))
+
+	k, v, err = c.Prev()
+	require.Nil(t, err)
+	require.Equal(t, k, []byte("key2"))
+	require.Equal(t, v, []byte("value1.11"))
+
+	k, v, err = c.Current()
+	require.Nil(t, err)
+	require.Equal(t, k, []byte("key2"))
+	require.Equal(t, v, []byte("value1.11"))
+
+	k, v, err = c.Prev()
+	require.Nil(t, err)
+	require.Equal(t, k, []byte("key1"))
+	require.Equal(t, v, []byte("value1.1"))
 }
