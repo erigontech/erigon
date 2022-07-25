0436e97a721bc6be83b5ab0c6c2bb08b31870ffd
TKartist <95535167+TKartist@users.noreply.github.com>
2022-07-25 10:53:58 +0200

    memory_mutation_test added (#546)
    * tests for GetOne and Put added
    
    * test configured
    
    * multiple tests added
    
    * conflict resolve
    
    * optimized and tests updated
    
    * conflict fix
    
    * tests added and tested with iteration
    
    * conflict resolve
    
    * lint error resolve
    
    * resolve unnecessary code
    
    * memory_mutation_test implemented
    
    * test error fixed
    
    * test error fixed

--- kv/memdb/memory_mutation_test.go
+++ kv/memdb/memory_mutation_test.go
@@ -28,6 +28,35 @@ func initializeDB(rwTx kv.RwTx) {
 	rwTx.Put(kv.HashedAccounts, []byte("CCAA"), []byte("value3"))
 }
 
+func TestPutAppendHas(t *testing.T) {
+	rwTx, err := New().BeginRw(context.Background())
+	require.Nil(t, err)
+
+	initializeDB(rwTx)
+
+	batch := NewMemoryBatch(rwTx)
+	require.NoError(t, batch.Append(kv.HashedAccounts, []byte("AAAA"), []byte("value1.5")))
+	require.Error(t, batch.Append(kv.HashedAccounts, []byte("AAAA"), []byte("value1.3")))
+	require.NoError(t, batch.Put(kv.HashedAccounts, []byte("AAAA"), []byte("value1.3")))
+	require.NoError(t, batch.Append(kv.HashedAccounts, []byte("CBAA"), []byte("value3.5")))
+	require.Error(t, batch.Append(kv.HashedAccounts, []byte("CBAA"), []byte("value3.1")))
+	require.NoError(t, batch.AppendDup(kv.HashedAccounts, []byte("CBAA"), []byte("value3.1")))
+
+	require.Nil(t, batch.Flush(rwTx))
+
+	exist, err := batch.Has(kv.HashedAccounts, []byte("AAAA"))
+	require.Nil(t, err)
+	require.Equal(t, exist, true)
+
+	val, err := batch.Get(kv.HashedAccounts, []byte("AAAA"))
+	require.Nil(t, err)
+	require.Equal(t, val, []byte("value1.3"))
+
+	exist, err = batch.Has(kv.HashedAccounts, []byte("KKKK"))
+	require.Nil(t, err)
+	require.Equal(t, exist, false)
+}
+
 func TestLastMiningDB(t *testing.T) {
 	rwTx, err := New().BeginRw(context.Background())
 	require.NoError(t, err)
@@ -124,4 +153,158 @@ func TestFlush(t *testing.T) {
 	value, err = rwTx.GetOne(kv.HashedAccounts, []byte("AAAA"))
 	require.NoError(t, err)
 	require.Equal(t, value, []byte("value5"))
+}
+
+func TestForEach(t *testing.T) {
+	rwTx, err := New().BeginRw(context.Background())
+	require.NoError(t, err)
+
+	initializeDB(rwTx)
+
+	batch := NewMemoryBatch(rwTx)
+	batch.Put(kv.HashedAccounts, []byte("FCAA"), []byte("value5"))
+	require.NoError(t, batch.Flush(rwTx))
+
+	var keys []string
+	var values []string
+	err = batch.ForEach(kv.HashedAccounts, []byte("XYAZ"), func(k, v []byte) error {
+		keys = append(keys, string(k))
+		values = append(values, string(v))
+		return nil
+	})
+	require.Nil(t, err)
+	require.Nil(t, keys)
+	require.Nil(t, values)
+
+	err = batch.ForEach(kv.HashedAccounts, []byte("CC"), func(k, v []byte) error {
+		keys = append(keys, string(k))
+		values = append(values, string(v))
+		return nil
+	})
+	require.Nil(t, err)
+	require.Equal(t, []string{"CCAA", "FCAA"}, keys)
+	require.Equal(t, []string{"value3", "value5"}, values)
+
+	var keys1 []string
+	var values1 []string
+
+	err = batch.ForEach(kv.HashedAccounts, []byte("A"), func(k, v []byte) error {
+		keys1 = append(keys1, string(k))
+		values1 = append(values1, string(v))
+		return nil
+	})
+	require.Nil(t, err)
+	require.Equal(t, []string{"AAAA", "CAAA", "CBAA", "CCAA", "FCAA"}, keys1)
+	require.Equal(t, []string{"value", "value1", "value2", "value3", "value5"}, values1)
+}
+
+func TestForPrefix(t *testing.T) {
+	rwTx, err := New().BeginRw(context.Background())
+	require.NoError(t, err)
+
+	initializeDB(rwTx)
+
+	batch := NewMemoryBatch(rwTx)
+	var keys1 []string
+	var values1 []string
+
+	err = batch.ForPrefix(kv.HashedAccounts, []byte("AB"), func(k, v []byte) error {
+		keys1 = append(keys1, string(k))
+		values1 = append(values1, string(v))
+		return nil
+	})
+	require.Nil(t, err)
+	require.Nil(t, keys1)
+	require.Nil(t, values1)
+
+	err = batch.ForPrefix(kv.HashedAccounts, []byte("AAAA"), func(k, v []byte) error {
+		keys1 = append(keys1, string(k))
+		values1 = append(values1, string(v))
+		return nil
+	})
+	require.Nil(t, err)
+	require.Equal(t, []string{"AAAA"}, keys1)
+	require.Equal(t, []string{"value"}, values1)
+
+	var keys []string
+	var values []string
+	err = batch.ForPrefix(kv.HashedAccounts, []byte("C"), func(k, v []byte) error {
+		keys = append(keys, string(k))
+		values = append(values, string(v))
+		return nil
+	})
+	require.Nil(t, err)
+	require.Equal(t, []string{"CAAA", "CBAA", "CCAA"}, keys)
+	require.Equal(t, []string{"value1", "value2", "value3"}, values)
+}
+
+func TestForAmount(t *testing.T) {
+	rwTx, err := New().BeginRw(context.Background())
+	require.NoError(t, err)
+
+	initializeDB(rwTx)
+
+	batch := NewMemoryBatch(rwTx)
+	defer batch.Close()
+
+	var keys []string
+	var values []string
+	err = batch.ForAmount(kv.HashedAccounts, []byte("C"), uint32(3), func(k, v []byte) error {
+		keys = append(keys, string(k))
+		values = append(values, string(v))
+		return nil
+	})
+
+	require.Nil(t, err)
+	require.Equal(t, []string{"CAAA", "CBAA", "CCAA"}, keys)
+	require.Equal(t, []string{"value1", "value2", "value3"}, values)
+
+	var keys1 []string
+	var values1 []string
+	err = batch.ForAmount(kv.HashedAccounts, []byte("C"), uint32(10), func(k, v []byte) error {
+		keys1 = append(keys1, string(k))
+		values1 = append(values1, string(v))
+		return nil
+	})
+
+	require.Nil(t, err)
+	require.Equal(t, []string{"CAAA", "CBAA", "CCAA"}, keys1)
+	require.Equal(t, []string{"value1", "value2", "value3"}, values1)
+}
+
+func TestGetClearBucket(t *testing.T) {
+	rwTx, err := New().BeginRw(context.Background())
+	require.NoError(t, err)
+
+	initializeDB(rwTx)
+
+	batch := NewMemoryBatch(rwTx)
+	defer batch.Close()
+
+	err = batch.ClearBucket(kv.HashedAccounts)
+	require.Nil(t, err)
+
+	cond := batch.isTableCleared(kv.HashedAccounts)
+	require.Equal(t, cond, true)
+
+	val, err := batch.GetOne(kv.HashedAccounts, []byte("A"))
+	require.Nil(t, err)
+	require.Nil(t, val)
+}
+
+func TestIncReadSequence(t *testing.T) {
+	rwTx, err := New().BeginRw(context.Background())
+	require.NoError(t, err)
+
+	initializeDB(rwTx)
+
+	batch := NewMemoryBatch(rwTx)
+	defer batch.Close()
+
+	_, err = batch.IncrementSequence(kv.HashedAccounts, uint64(12))
+	require.Nil(t, err)
+
+	val, err := batch.ReadSequence(kv.HashedAccounts)
+	require.Nil(t, err)
+	require.Equal(t, val, uint64(12))
 }
