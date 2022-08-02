3ddb15e16ac3c39d8c596d5b8394dfc1b8b8e477
alex.sharov <AskAlexSharov@gmail.com>
2022-08-02 11:49:29 +0700

    save

--- cmd/integration/commands/refetence_db.go
+++ cmd/integration/commands/refetence_db.go
@@ -349,7 +349,7 @@ MainLoop:
 func mdbxToMdbx(ctx context.Context, logger log.Logger, from, to string) error {
 	_ = os.RemoveAll(to)
 	src := mdbx2.NewMDBX(logger).Path(from).Flags(func(flags uint) uint { return mdbx.Readonly | mdbx.Accede }).MustOpen()
-	dst := mdbx2.NewMDBX(logger).Path(to).
+	dst := mdbx2.NewMDBX(logger).Path(to).WriteMap().
 		MustOpen()
 	return kv2kv(ctx, src, dst)
 }
