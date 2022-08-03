0c9ada1ab0e82133237d9f0659805491ef703943
Alex Sharov <AskAlexSharov@gmail.com>
2022-08-03 15:07:36 +0700

    dir.Exists() method (#564)

--- common/dir/rw_dir.go
+++ common/dir/rw_dir.go
@@ -63,3 +63,11 @@ func MustExist(path string) {
 		panic(err)
 	}
 }
+
+func Exist(path string) bool {
+	_, err := os.Stat(path)
+	if err != nil && os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
