ebb8cae9376b7f6886e14e68961c1ecb7d57be04
DenrianWeiss <42314109+DenrianWeiss@users.noreply.github.com>
2022-08-05 16:52:16 +0800

    [fix] eth_gasPrice RPC issue. big.Int's Add method will save the result to self, since oracle.SuggestTipCap might come from gpo.lastPrice, this will change all following rpc result

--- cmd/rpcdaemon/commands/eth_system.go
+++ cmd/rpcdaemon/commands/eth_system.go
@@ -115,13 +115,14 @@ func (api *APIImpl) GasPrice(ctx context.Context) (*hexutil.Big, error) {
 	}
 	oracle := gasprice.NewOracle(NewGasPriceOracleBackend(tx, cc, api.BaseAPI), ethconfig.Defaults.GPO)
 	tipcap, err := oracle.SuggestTipCap(ctx)
+	gasResult := big.NewInt(0)
 	if err != nil {
 		return nil, err
 	}
 	if head := rawdb.ReadCurrentHeader(tx); head != nil && head.BaseFee != nil {
-		tipcap.Add(tipcap, head.BaseFee)
+		gasResult.Add(tipcap, head.BaseFee)
 	}
-	return (*hexutil.Big)(tipcap), err
+	return (*hexutil.Big)(gasResult), err
 }
 
 // MaxPriorityFeePerGas returns a suggestion for a gas tip cap for dynamic fee transactions.
