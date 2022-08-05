7dabdc3269f1e3b098fee5ab4d96ec843823a6fd
Alexey Sharp <alexeysharp@Alexeys-iMac.local>
2022-08-05 17:42:22 +0100

    Alpha modifications

--- params/version.go
+++ params/version.go
@@ -31,10 +31,10 @@ var (
 
 // see https://calver.org
 const (
-	VersionMajor       = 2022  // Major version component of the current release
-	VersionMinor       = 99    // Minor version component of the current release
-	VersionMicro       = 99    // Patch version component of the current release
-	VersionModifier    = "dev" // Modifier component of the current release
+	VersionMajor       = 2022    // Major version component of the current release
+	VersionMinor       = 8       // Minor version component of the current release
+	VersionMicro       = 1       // Patch version component of the current release
+	VersionModifier    = "alpha" // Modifier component of the current release
 	VersionKeyCreated  = "ErigonVersionCreated"
 	VersionKeyFinished = "ErigonVersionFinished"
 )
