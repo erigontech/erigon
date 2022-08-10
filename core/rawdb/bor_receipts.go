71c1b06355b5cae7be723024104e24088b149dc4
Enrique Avila <eavilaasapche@gmail.com>
2022-08-10 17:51:39 +0300

    storing bor logs

--- core/rawdb/bor_receipts.go
+++ core/rawdb/bor_receipts.go
@@ -1,12 +1,14 @@
 package rawdb
 
 import (
+	"bytes"
 	"errors"
 
 	"github.com/ledgerwatch/erigon-lib/kv"
 	"github.com/ledgerwatch/erigon/common"
 	"github.com/ledgerwatch/erigon/common/dbutils"
 	"github.com/ledgerwatch/erigon/core/types"
+	"github.com/ledgerwatch/erigon/ethdb/cbor"
 	"github.com/ledgerwatch/erigon/rlp"
 	"github.com/ledgerwatch/log/v3"
 )
@@ -93,9 +95,10 @@ func ReadBorReceiptLogs(db kv.Tx, blockHash common.Hash, blockNumber uint64, txI
 	return borLogs
 }
 
-// WriteBorReceipt stores all the bor receipt belonging to a block.
-func WriteBorReceipt(tx kv.RwTx, hash common.Hash, number uint64, borReceipt *types.ReceiptForStorage) error {
+// WriteBorReceipt stores all the bor receipt belonging to a block (storing the state sync recipt and log).
+func WriteBorReceipt(tx kv.RwTx, hash common.Hash, number uint64, borReceipt *types.ReceiptForStorage, idx uint32) error {
 	// Convert the bor receipt into their storage form and serialize them
+	buf := bytes.NewBuffer(make([]byte, 0, 1024))
 	bytes, err := rlp.EncodeToBytes(borReceipt)
 	if err != nil {
 		return err
@@ -103,6 +106,11 @@ func WriteBorReceipt(tx kv.RwTx, hash common.Hash, number uint64, borReceipt *ty
 
 	// Store the flattened receipt slice
 	if err := tx.Append(kv.BorReceipts, borReceiptKey(number), bytes); err != nil {
+		return err
+	}
+
+	cbor.Marshal(buf, borReceipt.Logs)
+	if err := tx.Append(kv.Log, dbutils.LogKey(number, idx), buf.Bytes()); err != nil {
 		return err
 	}
 
