b536a5b86976c387900d120e25da98ba727f124e
alex.sharov <AskAlexSharov@gmail.com>
2022-08-12 12:09:44 +0700

    save

--- eth/backend.go
+++ eth/backend.go
@@ -205,10 +205,11 @@ func New(stack *node.Node, config *ethconfig.Config, logger log.Logger) (*Ethere
 		if err != nil {
 			return err
 		}
-		config.HistoryV2, err = rawdb.WriteHisoryV2(tx, config.HistoryV2)
-		if err != nil {
-			return err
-		}
+
+		//config.HistoryV2, err = rawdb.WriteHisoryV2(tx, config.HistoryV2)
+		//if err != nil {
+		//	return err
+		//}
 
 		// if we are in the incorrect syncmode then we change it to the appropriate one
 		if !isCorrectSync {
