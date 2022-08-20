64b2b1f1f018f8a1a5805d416655328612b535ab
giuliorebuffo <giuliorebuffo@system76-pc.localdomain>
2022-08-20 15:57:48 +0200

    fixed tests

--- ethdb/privateapi/engine_test.go
+++ ethdb/privateapi/engine_test.go
@@ -91,7 +91,7 @@ func TestMockDownloadRequest(t *testing.T) {
 
 	makeTestDb(ctx, db)
 	hd := headerdownload.NewHeaderDownload(0, 0, nil, nil)
-
+	hd.SetPOSSync(true)
 	events := NewEvents()
 	backend := NewEthBackendServer(ctx, nil, db, events, nil, &params.ChainConfig{TerminalTotalDifficulty: common.Big1}, nil, hd, false)
 
@@ -149,6 +149,7 @@ func TestMockValidExecution(t *testing.T) {
 	makeTestDb(ctx, db)
 
 	hd := headerdownload.NewHeaderDownload(0, 0, nil, nil)
+	hd.SetPOSSync(true)
 
 	events := NewEvents()
 	backend := NewEthBackendServer(ctx, nil, db, events, nil, &params.ChainConfig{TerminalTotalDifficulty: common.Big1}, nil, hd, false)
@@ -184,6 +185,7 @@ func TestMockInvalidExecution(t *testing.T) {
 	makeTestDb(ctx, db)
 
 	hd := headerdownload.NewHeaderDownload(0, 0, nil, nil)
+	hd.SetPOSSync(true)
 
 	events := NewEvents()
 	backend := NewEthBackendServer(ctx, nil, db, events, nil, &params.ChainConfig{TerminalTotalDifficulty: common.Big1}, nil, hd, false)
