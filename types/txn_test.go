215695f9392da0fa808be014ff8454f323b4dae3
Andrew Ashikhmin <34320705+yperbasis@users.noreply.github.com>
2022-09-02 10:37:02 +0200

    Add a test with the bad txn on Görli (#619)

--- types/txn_test.go
+++ types/txn_test.go
@@ -67,7 +67,7 @@ func TestParseTransactionRLP(t *testing.T) {
 	}
 }
 
-func TestTransactionSignatureValidity(t *testing.T) {
+func TestTransactionSignatureValidity1(t *testing.T) {
 	chainId := new(uint256.Int).SetUint64(1)
 	ctx := NewTxParseContext(*chainId)
 	ctx.WithAllowPreEip2s(true)
@@ -87,6 +87,16 @@ func TestTransactionSignatureValidity(t *testing.T) {
 	assert.NoError(t, err)
 
 	_, err = ctx.ParseTransaction(preEip2Txn, 0, tx, txSender[:], false /* hasEnvelope */, nil)
+	assert.Error(t, err)
+}
+
+// Problematic txn included in a bad block on Görli
+func TestTransactionSignatureValidity2(t *testing.T) {
+	chainId := new(uint256.Int).SetUint64(5)
+	ctx := NewTxParseContext(*chainId)
+	slot, sender := &TxSlot{}, [20]byte{}
+	rlp := common.MustDecodeHex("02f8720513844190ab00848321560082520894cab441d2f45a3fee83d15c6b6b6c36a139f55b6288054607fc96a6000080c001a0dffe4cb5651e663d0eac8c4d002de734dd24db0f1109b062d17da290a133cc02a0913fb9f53f7a792bcd9e4d7cced1b8545d1ab82c77432b0bc2e9384ba6c250c5")
+	_, err := ctx.ParseTransaction(rlp, 0, slot, sender[:], false /* hasEnvelope */, nil)
 	assert.Error(t, err)
 }
 
