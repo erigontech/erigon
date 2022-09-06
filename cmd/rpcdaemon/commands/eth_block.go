b483d0a36a16417ec004a793e072967232ee4d24
Igor Mandrigin <i@mandrigin.ru>
2022-09-06 20:00:01 +0200

    fixup

--- cmd/rpcdaemon/commands/eth_block.go
+++ cmd/rpcdaemon/commands/eth_block.go
@@ -337,6 +337,7 @@ func (api *APIImpl) GetBlockTransactionCountByHash(ctx context.Context, blockHas
 	blockNum, _, _, err := rpchelper.GetBlockNumber(rpc.BlockNumberOrHash{BlockHash: &blockHash}, tx, nil)
 	if err != nil {
 		// (Compatibility) Every other node just return `null` for when the block does not exist.
+		log.Debug("eth_getBlockTransactionCountByHash GetBlockNumber failed", "err", err)
 		return nil, nil
 	}
 	_, txAmount, err := api._blockReader.Body(ctx, tx, blockHash, blockNum)
