18554a2f90f767ee98c59b9009dc38321e0b15b1
Andrew Ashikhmin <34320705+yperbasis@users.noreply.github.com>
2022-09-08 10:19:45 +0200

    Revert "fix: relax deposit contract prune to first deposit (#5295)" (#5317)
    This reverts commit d8c9b1151d574491baf14e208edabe72c1c65d89.

--- ethdb/prune/storage_mode.go
+++ ethdb/prune/storage_mode.go
@@ -24,9 +24,9 @@ var DefaultMode = Mode{
 }
 
 var (
-	mainnetDepositContractBlock uint64 = 11185311
+	mainnetDepositContractBlock uint64 = 11052984
 	sepoliaDepositContractBlock uint64 = 1273020
-	goerliDepositContractBlock  uint64 = 4422009
+	goerliDepositContractBlock  uint64 = 4367322
 )
 
 type Experiments struct {
