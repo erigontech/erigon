575e743dfe7977eb254451ca2a68c2cc62507e85
Enrique Jose Avila Asapche <eavilaasapche@gmail.com>
2022-09-12 15:40:07 +0300

    added a way to get latest executed block post POS

--- cmd/rpcdaemon/commands/erigon_system.go
+++ cmd/rpcdaemon/commands/erigon_system.go
@@ -4,7 +4,9 @@ import (
 	"context"
 
 	"github.com/ledgerwatch/erigon/common"
+	"github.com/ledgerwatch/erigon/common/hexutil"
 	"github.com/ledgerwatch/erigon/core/forkid"
+	"github.com/ledgerwatch/erigon/turbo/rpchelper"
 )
 
 // Forks is a data type to record a list of forks passed by this node
@@ -28,4 +30,21 @@ func (api *ErigonImpl) Forks(ctx context.Context) (Forks, error) {
 	forksBlocks := forkid.GatherForks(chainConfig)
 
 	return Forks{genesis.Hash(), forksBlocks}, nil
+}
+
+// Post the merge eth_blockNumber will return latest forkChoiceHead block number
+// erigon_executedBlockNumber will return latest executed block number
+func (api *ErigonImpl) ExecutedBlockNumber(ctx context.Context) (hexutil.Uint64, error) {
+	tx, err := api.db.BeginRo(ctx)
+	if err != nil {
+		return 0, err
+	}
+	defer tx.Rollback()
+
+	blockNum, err := rpchelper.GetLatestExecutedBlockNumber(tx)
+	if err != nil {
+		return 0, err
+	}
+
+	return hexutil.Uint64(blockNum), nil
 }
