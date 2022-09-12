48d626db4697d8e0e504838ae618b5a0e117567b
Enrique Jose Avila Asapche <eavilaasapche@gmail.com>
2022-09-12 19:00:38 +0300

    using SUICIDE

--- eth/tracers/internal/tracers/evmdis_tracer.js
+++ eth/tracers/internal/tracers/evmdis_tracer.js
@@ -76,7 +76,7 @@
 				var outsize = log.stack.peek(1).valueOf();
 				frame.return = log.memory.slice(out, out + outsize);
 				break;
-			case "STOP": case "SELFDESTRUCT":
+			case "STOP": case "SUICIDE":
 				frame.return = log.memory.slice(0, 0);
 				break;
 			case "JUMPDEST":
