5224583627bc4c448429c6a8ed084403cc452002
alex.sharov <AskAlexSharov@gmail.com>
2022-09-13 15:47:05 +0700

    save

--- cmd/state/exec22/txNums.go
+++ cmd/state/exec22/txNums.go
@@ -6,6 +6,7 @@ import (
 	"sort"
 
 	"github.com/ledgerwatch/erigon-lib/common/cmp"
+	"github.com/ledgerwatch/erigon-lib/common/dbg"
 	"github.com/ledgerwatch/erigon-lib/kv"
 	"github.com/ledgerwatch/erigon/core/rawdb"
 	"github.com/ledgerwatch/erigon/eth/stagedsync/stages"
@@ -50,6 +51,7 @@ func (s *TxNums) Find(endTxNumMinimax uint64) (ok bool, blockNum uint64) {
 }
 
 func (s *TxNums) Restore(sn *snapshotsync.RoSnapshots, tx kv.Tx) error {
+	fmt.Printf("alex: %s\n", dbg.Stack())
 	historyV2, err := rawdb.HistoryV2.Enabled(tx)
 	if err != nil {
 		return err
