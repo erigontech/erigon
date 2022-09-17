7b1bf44ce6ec9b5400c8a5a83343f25e81a79fcf
Enrique Jose  Avila Asapche <eavilaasapche@gmail.com>
2022-09-17 23:04:37 +0200

    dont check for block reward or uncles if we are tracing POS (#5410)
    * dont check for block reward or uncles if we are tracing POS
    
    * comparing chainConfig to header difficulty
    
    * fixed test
    
    * difficulty == 0 then we are POS

--- cmd/rpcdaemon/commands/trace_filtering.go
+++ cmd/rpcdaemon/commands/trace_filtering.go
@@ -325,6 +325,7 @@ func (api *TraceAPIImpl) Filter(ctx context.Context, req TraceFilterRequest, str
 	nExported := uint64(0)
 
 	it := allBlocks.Iterator()
+	isPos := false
 	for it.HasNext() {
 		b := it.Next()
 		// Extract transactions from block
@@ -367,6 +368,10 @@ func (api *TraceAPIImpl) Filter(ctx context.Context, req TraceFilterRequest, str
 
 		blockHash := block.Hash()
 		blockNumber := block.NumberU64()
+		if !isPos && api._chainConfig.TerminalTotalDifficulty != nil {
+			header := block.Header()
+			isPos = header.Difficulty.Cmp(common.Big0) == 0 || header.Difficulty.Cmp(api._chainConfig.TerminalTotalDifficulty) >= 0
+		}
 		txs := block.Transactions()
 		t, tErr := api.callManyTransactions(ctx, dbtx, txs, []string{TraceTypeTrace}, block.ParentHash(), rpc.BlockNumber(block.NumberU64()-1), block.Header(), -1 /* all tx indices */, types.MakeSigner(chainConfig, b), chainConfig.Rules(b))
 		if tErr != nil {
@@ -416,6 +421,13 @@ func (api *TraceAPIImpl) Filter(ctx context.Context, req TraceFilterRequest, str
 				}
 			}
 		}
+
+		// if we are in POS
+		// we dont check for uncles or block rewards
+		if isPos {
+			continue
+		}
+
 		minerReward, uncleRewards := ethash.AccumulateRewards(chainConfig, block.Header(), block.Uncles())
 		if _, ok := toAddresses[block.Coinbase()]; ok || includeAll {
 			nSeen++
