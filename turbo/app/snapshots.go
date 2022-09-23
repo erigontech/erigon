668a87f6e695c76f45115f7dcf378ac54927db4a
alex.sharov <AskAlexSharov@gmail.com>
2022-09-23 09:51:37 +0700

    save

--- turbo/app/snapshots.go
+++ turbo/app/snapshots.go
@@ -234,7 +234,7 @@ func doUncompress(cliCtx *cli.Context) error {
 		return err
 	}
 	defer decompressor.Close()
-	wr := bufio.NewWriterSize(os.Stdout, 512*1024*1024)
+	wr := bufio.NewWriterSize(os.Stdout, 256*1024*1024)
 	defer wr.Flush()
 	var numBuf [binary.MaxVarintLen64]byte
 	if err := decompressor.WithReadAhead(func() error {
@@ -280,7 +280,7 @@ func doCompress(cliCtx *cli.Context) error {
 	}
 	defer c.Close()
 
-	r := bufio.NewReaderSize(os.Stdin, 512*1024*1024)
+	r := bufio.NewReaderSize(os.Stdin, 256*1024*1024)
 	buf := make([]byte, 0, 32*1024*1024)
 	var l uint64
 	for l, err = binary.ReadUvarint(r); err == nil; l, err = binary.ReadUvarint(r) {
