a1b234cced64e76994d90d61536bd98e35140faf
alex.sharov <AskAlexSharov@gmail.com>
2022-09-24 13:56:14 +0700

    save

--- core/rawdb/accessors_chain.go
+++ core/rawdb/accessors_chain.go
@@ -1309,8 +1309,8 @@ func TruncateBlocks(ctx context.Context, tx kv.RwTx, blockFrom uint64) error {
 
 	if blockFrom == 1 || blockFrom == 0 {
 		log.Info("clear transactions table, may take time...")
-		_ = tx.ClearBucket(kv.EthTx)
-		_ = tx.ClearBucket(kv.NonCanonicalTxs)
+		//_ = tx.ClearBucket(kv.EthTx)
+		//_ = tx.ClearBucket(kv.NonCanonicalTxs)
 	}
 
 	c, err := tx.Cursor(kv.Headers)
