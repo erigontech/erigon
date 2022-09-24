43c41df75d68852eaba259e9c131d4d241fb1631
alex.sharov <AskAlexSharov@gmail.com>
2022-09-24 13:55:01 +0700

    save

--- core/rawdb/rawdbreset/reset_stages.go
+++ core/rawdb/rawdbreset/reset_stages.go
@@ -47,10 +47,10 @@ func ResetState(db kv.RwDB, ctx context.Context, chain string) error {
 }
 
 func ResetBlocks(tx kv.RwTx, db kv.RoDB, snapshots *snapshotsync.RoSnapshots, br services.HeaderAndCanonicalReader) error {
-	kv.ReadAhead(context.Background(), db, atomic.NewBool(false), kv.EthTx, nil, math.MaxUint64)
-	kv.ReadAhead(context.Background(), db, atomic.NewBool(false), kv.NonCanonicalTxs, nil, math.MaxUint64)
-	kv.ReadAhead(context.Background(), db, atomic.NewBool(false), kv.Headers, nil, math.MaxUint64)
-	kv.ReadAhead(context.Background(), db, atomic.NewBool(false), kv.BlockBody, nil, math.MaxUint64)
+	kv.ReadAhead(context.Background(), db, atomic.NewBool(false), kv.EthTx, nil, math.MaxUint32)
+	kv.ReadAhead(context.Background(), db, atomic.NewBool(false), kv.NonCanonicalTxs, nil, math.MaxUint32)
+	kv.ReadAhead(context.Background(), db, atomic.NewBool(false), kv.Headers, nil, math.MaxUint32)
+	kv.ReadAhead(context.Background(), db, atomic.NewBool(false), kv.BlockBody, nil, math.MaxUint32)
 
 	// keep Genesis
 	if err := rawdb.TruncateBlocks(context.Background(), tx, 1); err != nil {
