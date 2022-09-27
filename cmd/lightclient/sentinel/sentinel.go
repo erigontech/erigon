6f5496d7703d7d7805e618e92519ff3ecd1f9f10
giuliorebuffo <giuliorebuffo@system76-pc.localdomain>
2022-09-27 23:26:15 +0200

    made handlers a bit better

--- cmd/lightclient/sentinel/sentinel.go
+++ cmd/lightclient/sentinel/sentinel.go
@@ -27,12 +27,9 @@ import (
 	"github.com/libp2p/go-libp2p"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	"github.com/libp2p/go-libp2p/core/host"
-	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/libp2p/go-libp2p/p2p/protocol/identify"
 	"github.com/pkg/errors"
 )
-
-var disconnectPeerCh = make(chan peer.ID, 0)
 
 type Sentinel struct {
 	started  bool
@@ -118,19 +115,6 @@ func (s *Sentinel) createListener() (*discover.UDPv5, error) {
 	if err != nil {
 		return nil, err
 	}
-
-	go func() {
-		for {
-			select {
-			case <-s.ctx.Done():
-				close(disconnectPeerCh)
-				break
-			case pid := <-disconnectPeerCh:
-				s.peers.DisconnectPeer(pid)
-			default:
-			}
-		}
-	}()
 
 	return net, err
 }
