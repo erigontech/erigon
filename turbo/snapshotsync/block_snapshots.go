dbc9f6c7588e94f31ab4a437dcd16645c5fbf9fa
Alex Sharp <alexsharp@Alexs-MacBook-Pro.local>
2022-09-30 20:57:12 +0100

    Remove print

--- turbo/snapshotsync/block_snapshots.go
+++ turbo/snapshotsync/block_snapshots.go
@@ -1772,7 +1772,6 @@ func Idx(ctx context.Context, d *compress.Decompressor, firstDataID uint64, tmpD
 	if err != nil {
 		return err
 	}
-	fmt.Printf("d.Count() = %d\n", d.Count())
 	rs.LogLvl(lvl)
 
 	defer d.EnableReadAhead().DisableReadAhead()
