5446f539d6c08219dca9e0ecc6cb03cf64aa761c
Alex Sharp <alexsharp@Alexs-MacBook-Pro.local>
2022-10-03 07:09:43 +0100

    Prints

--- eth/stagedsync/stage_snapshots.go
+++ eth/stagedsync/stage_snapshots.go
@@ -500,16 +500,20 @@ func SnapshotsPrune(s *PruneState, cfg SnapshotsCfg, ctx context.Context, tx kv.
 func retireBlocksInSingleBackgroundThread(s *PruneState, blockRetire *snapshotsync.BlockRetire, ctx context.Context, tx kv.RwTx) (err error) {
 	// if something already happens in background - noop
 	if blockRetire.Working() {
+		fmt.Printf("already working\n")
 		return nil
 	}
 	ok, err := blockRetire.BackgroundResult.GetAndReset()
 	if !ok {
+		fmt.Printf("not ok\n")
 		return nil
 	}
 	if err != nil {
+		fmt.Printf("error1: %v\n", err)
 		return fmt.Errorf("[%s] %w", s.LogPrefix(), err)
 	}
 	if err := rawdb.WriteSnapshots(tx, blockRetire.Snapshots().Files()); err != nil {
+		fmt.Printf("erro2: %v\n", err)
 		return err
 	}
 
