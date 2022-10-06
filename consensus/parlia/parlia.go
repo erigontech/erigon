59246cf17ceb6050209afb57945f12ee00310e54
alex.sharov <AskAlexSharov@gmail.com>
2022-10-06 09:57:17 +0700

    save

--- consensus/parlia/parlia.go
+++ consensus/parlia/parlia.go
@@ -528,8 +528,10 @@ func (p *Parlia) snapshot(chain consensus.ChainHeaderReader, number uint64, hash
 			}
 			parents = parents[:len(parents)-1]
 		} else {
-			// No explicit parents (or no more left), reach out to the database
-			log.Info("parlia", "get header2", number)
+			if number%1_000 == 0 {
+				// No explicit parents (or no more left), reach out to the database
+				log.Info("parlia", "get header2", number)
+			}
 			header = chain.GetHeader(hash, number)
 			if header == nil {
 				return nil, consensus.ErrUnknownAncestor
