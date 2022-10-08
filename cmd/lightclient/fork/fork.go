2a7a8a360e6c14fdc2b36d8857aa615ec21e4ef4
giuliorebuffo <giuliorebuffo@system76-pc.localdomain>
2022-10-08 15:12:24 +0200

    added licensing

--- cmd/lightclient/fork/fork.go
+++ cmd/lightclient/fork/fork.go
@@ -1,3 +1,16 @@
+/*
+   Copyright 2022 Erigon-Lightclient contributors
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 package fork
 
 import (
@@ -10,6 +23,7 @@ import (
 	"github.com/ledgerwatch/erigon/cmd/lightclient/sentinel/communication/p2p"
 	"github.com/ledgerwatch/erigon/cmd/lightclient/utils"
 	"github.com/ledgerwatch/erigon/common"
+	pubsubpb "github.com/libp2p/go-libp2p-pubsub/pb"
 )
 
 func ComputeForkDigest(
@@ -116,4 +130,10 @@ func getLastForkEpoch(
 		break
 	}
 	return currentForkEpoch
+}
+
+// The one suggested by the spec is too over-engineered.
+func MsgID(pmsg *pubsubpb.Message) string {
+	hash := utils.Keccak256(pmsg.Data)
+	return string(hash[:])
 }
