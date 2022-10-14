e90c517753c6560b687d467320cbc7b3b183a69d
a <a@a.a>
2022-10-13 21:04:56 -0500

    Add headers

--- eth/stagedsync/all_stages.go
+++ eth/stagedsync/all_stages.go
@@ -1,7 +1,10 @@
 package stagedsync
 
 import (
+	"fmt"
+
 	"github.com/VictoriaMetrics/metrics"
+	"github.com/huandu/xstrings"
 	"github.com/ledgerwatch/erigon-lib/kv"
 	"github.com/ledgerwatch/erigon/eth/stagedsync/stages"
 )
@@ -10,6 +13,26 @@ var syncMetrics = map[stages.SyncStage]*metrics.Counter{
 	stages.Headers:   metrics.GetOrCreateCounter(`sync{stage="headers"}`),
 	stages.Execution: metrics.GetOrCreateCounter(`sync{stage="execution"}`),
 	stages.Finish:    metrics.GetOrCreateCounter(`sync{stage="finish"}`),
+	// additional
+	stages.Snapshots:           makeSyncMetric(stages.Snapshots),
+	stages.CumulativeIndex:     makeSyncMetric(stages.CumulativeIndex),
+	stages.BlockHashes:         makeSyncMetric(stages.BlockHashes),
+	stages.Bodies:              makeSyncMetric(stages.Bodies),
+	stages.Senders:             makeSyncMetric(stages.Senders),
+	stages.Translation:         makeSyncMetric(stages.Translation),
+	stages.VerkleTrie:          makeSyncMetric(stages.VerkleTrie),
+	stages.IntermediateHashes:  makeSyncMetric(stages.IntermediateHashes),
+	stages.HashState:           makeSyncMetric(stages.HashState),
+	stages.AccountHistoryIndex: makeSyncMetric(stages.AccountHistoryIndex),
+	stages.StorageHistoryIndex: makeSyncMetric(stages.StorageHistoryIndex),
+	stages.LogIndex:            makeSyncMetric(stages.LogIndex),
+	stages.CallTraces:          makeSyncMetric(stages.CallTraces),
+	stages.TxLookup:            makeSyncMetric(stages.TxLookup),
+	stages.Issuance:            makeSyncMetric(stages.Issuance),
+}
+
+func makeSyncMetric(stage stages.SyncStage) *metrics.Counter {
+	return metrics.GetOrCreateCounter(fmt.Sprintf(`sync{stage="%s"}`, xstrings.ToSnakeCase(string(stage))))
 }
 
 // UpdateMetrics - need update metrics manually because current "metrics" package doesn't support labels
