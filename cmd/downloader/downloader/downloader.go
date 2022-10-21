6a073e2403588313df5d4acef3379672faa06691
alex.sharov <AskAlexSharov@gmail.com>
2022-10-21 11:23:05 +0700

    save

--- cmd/downloader/downloader/downloader.go
+++ cmd/downloader/downloader/downloader.go
@@ -131,6 +131,9 @@ func (d *Downloader) ReCalcStats(interval time.Duration) {
 			}
 			stats.BytesCompleted += uint64(t.BytesCompleted())
 			stats.BytesTotal += uint64(t.Length())
+			if !t.Complete.Bool() {
+				log.Debug("[downloader] file not downloaded yet", "name", t.Name())
+			}
 		default:
 			log.Debug("[downloader] file has no metadata yet", "name", t.Name())
 		}
