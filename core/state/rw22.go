414c3204209300c155b89bfa98b937d0842bfe90
alex.sharov <AskAlexSharov@gmail.com>
2022-10-25 14:13:26 +0700

    save

--- core/state/rw22.go
+++ core/state/rw22.go
@@ -391,30 +391,30 @@ func (rs *State22) Apply(roTx kv.Tx, txTask *TxTask, agg *libstate.Aggregator22)
 		//	return err
 		//}
 	}
-	//if txTask.TraceFroms != nil {
-	//	for addr := range txTask.TraceFroms {
-	//		if err := agg.AddTraceFrom(addr[:]); err != nil {
-	//			return err
-	//		}
-	//	}
-	//}
-	//if txTask.TraceTos != nil {
-	//	for addr := range txTask.TraceTos {
-	//		if err := agg.AddTraceTo(addr[:]); err != nil {
-	//			return err
-	//		}
-	//	}
-	//}
-	//for _, log := range txTask.Logs {
-	//	if err := agg.AddLogAddr(log.Address[:]); err != nil {
-	//		return fmt.Errorf("adding event log for addr %x: %w", log.Address, err)
-	//	}
-	//	for _, topic := range log.Topics {
-	//		if err := agg.AddLogTopic(topic[:]); err != nil {
-	//			return fmt.Errorf("adding event log for topic %x: %w", topic, err)
-	//		}
-	//	}
-	//}
+	if txTask.TraceFroms != nil {
+		for addr := range txTask.TraceFroms {
+			if err := agg.AddTraceFrom(addr[:]); err != nil {
+				return err
+			}
+		}
+	}
+	if txTask.TraceTos != nil {
+		for addr := range txTask.TraceTos {
+			if err := agg.AddTraceTo(addr[:]); err != nil {
+				return err
+			}
+		}
+	}
+	for _, log := range txTask.Logs {
+		if err := agg.AddLogAddr(log.Address[:]); err != nil {
+			return fmt.Errorf("adding event log for addr %x: %w", log.Address, err)
+		}
+		for _, topic := range log.Topics {
+			if err := agg.AddLogTopic(topic[:]); err != nil {
+				return fmt.Errorf("adding event log for topic %x: %w", topic, err)
+			}
+		}
+	}
 	if err := agg.FinishTx(); err != nil {
 		return err
 	}
