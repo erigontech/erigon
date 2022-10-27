76d128eebdd44660e547cd3c1b2d5f4e0b60b98e
alex.sharov <AskAlexSharov@gmail.com>
2022-10-27 09:36:04 +0700

    save

--- eth/stagedsync/exec3.go
+++ eth/stagedsync/exec3.go
@@ -345,14 +345,16 @@ func Exec3(ctx context.Context,
 					}
 					log.Info("Committed", "time", time.Since(commitStart))
 				case <-pruneEvery.C:
-					pruneCtx, cancel := context.WithTimeout(ctx, time.Second)
-					defer cancel()
-					t := time.Now()
-					for time.Since(t) < time.Second {
-						agg.EndTxNumMinimax()
-						if err = agg.Prune(pruneCtx, 1_000); err != nil { // prune part of retired data, before commit
-							panic(err)
+					if agg.CanPrune(tx) {
+						pruneCtx, cancel := context.WithTimeout(ctx, time.Second)
+						t := time.Now()
+						for time.Since(t) < time.Second {
+							agg.EndTxNumMinimax()
+							if err = agg.Prune(pruneCtx, 1_000); err != nil { // prune part of retired data, before commit
+								panic(err)
+							}
 						}
+						cancel()
 					}
 				}
 			}
