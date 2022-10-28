erigontech/erigon

commit d697b6fe7cf0d9d5a7d26f991a271cce0f8ef964

large performance optimization for filterLogs (getLogs) when doing queries with many addresses and logs. (#5805)
this pr changes filterLogs to use a pre computed hashset of addresses,
instead of iterating across the list of addresses once per log.

this greatly increases the speed of filter queries that use many
addresses and also return a large number of logs. In our case, we are
performing a query for all the trades performed in a uniswap v3 pool in
a 250 block range.
 
my benchmarks were performed with the data & code below:
 
address list gist is here
[addrs](https://gist.githubusercontent.com/elee1766/861c6a55838c88522beae651d9d3b584/raw/2c30b0df439b9c8114ee8aea4083a8039ffcb93b/gistfile1.txt)

 ```
 c := NewRpcClient()
 addrs := []common.Address{AddressListGist}
logs, err := c.FilterLogs(context.TODO(), ethereum.FilterQuery{
	FromBlock:big.NewInt(15640000),
	ToBlock:   big.NewInt(15640250),
	Addresses: addrs,
	Topics: [][]common.Hash{
		{

common.HexToHash("c42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67"),
		},
	},
```
the query contains 8442 addresses, while the response contains 1277 logs

On average, current devel averages a 15.57 second response time on my machine after 10 runs, while the new filterLogs averages 1.05 seconds.

for CURRENT DEVEL, the profile is here: https://pprof.aaaaa.news/cd8dkv0tidul37sctmi0/flamegraph 

for the filterLogs branch, the profile is here: https://pprof.aaaaa.news/cd8dlmgtidul37sctmig/flamegraph

while the tests pass with this branch, I am not really sure why filterLogs was originally programmed the way it was. Is there some sort of edge case / compatibility thing that I am missing with this change?

Co-authored-by: a <a@a.a>

cmd/rpcdaemon/commands/eth_receipts.go
@@ -164,7 +164,10 @@ func (api *APIImpl) GetLogs(ctx context.Context, crit filters.FilterCriteria) (t
 	if blockNumbers.GetCardinality() == 0 {
 		return logs, nil
 	}
-
+	addrMap := make(map[common.Address]struct{}, len(crit.Addresses))
+	for _, v := range crit.Addresses {
+		addrMap[v] = struct{}{}
+	}
 	iter := blockNumbers.Iterator()
 	for iter.HasNext() {
 		if err = ctx.Err(); err != nil {
@@ -175,6 +178,7 @@ func (api *APIImpl) GetLogs(ctx context.Context, crit filters.FilterCriteria) (t
 		var logIndex uint
 		var txIndex uint
 		var blockLogs []*types.Log
+
 		err := tx.ForPrefix(kv.Log, dbutils.EncodeBlockNumber(blockNumber), func(k, v []byte) error {
 			var logs types.Logs
 			if err := cbor.Unmarshal(&logs, bytes.NewReader(v)); err != nil {
@@ -184,7 +188,7 @@ func (api *APIImpl) GetLogs(ctx context.Context, crit filters.FilterCriteria) (t
 				log.Index = logIndex
 				logIndex++
 			}
-			filtered := filterLogs(logs, crit.Addresses, crit.Topics)
+			filtered := filterLogs(logs, addrMap, crit.Topics)
 			if len(filtered) == 0 {
 				return nil
 			}
@@ -337,6 +341,10 @@ func (api *APIImpl) getLogsV3(ctx context.Context, tx kv.Tx, begin, end uint64, 
 	if err != nil {
 		return nil, err
 	}
+	addrMap := make(map[common.Address]struct{}, len(crit.Addresses))
+	for _, v := range crit.Addresses {
+		addrMap[v] = struct{}{}
+	}
 	for iter.HasNext() {
 		txNum := iter.Next()
 		// Find block number
@@ -398,7 +406,7 @@ func (api *APIImpl) getLogsV3(ctx context.Context, tx kv.Tx, begin, end uint64, 
 			log.Index = logIndex
 			logIndex++
 		}
-		filtered := filterLogs(rawLogs, crit.Addresses, crit.Topics)
+		filtered := filterLogs(rawLogs, addrMap, crit.Topics)
 		for _, log := range filtered {
 			log.BlockNumber = blockNum
 			log.BlockHash = lastBlockHash
@@ -638,16 +646,14 @@ func includes(addresses []common.Address, a common.Address) bool {
 			return true
 		}
 	}
-
 	return false
 }
 
 // filterLogs creates a slice of logs matching the given criteria.
-func filterLogs(logs []*types.Log, addresses []common.Address, topics [][]common.Hash) []*types.Log {
+func filterLogsOld(logs []*types.Log, addresses []common.Address, topics [][]common.Hash) []*types.Log {
 	result := make(types.Logs, 0, len(logs))
 Logs:
 	for _, log := range logs {
-
 		if len(addresses) > 0 && !includes(addresses, log.Address) {
 			continue
 		}
@@ -666,6 +672,44 @@ Logs:
 			if !match {
 				continue Logs
 			}
+		}
+		result = append(result, log)
+	}
+	return result
+}
+
+func filterLogs(logs []*types.Log, addresses map[common.Address]struct{}, topics [][]common.Hash) []*types.Log {
+	result := make(types.Logs, 0, len(logs))
+	// populate a set of addresses
+	for _, log := range logs {
+		// empty address list means no filter
+		if len(addresses) > 0 {
+			// this is basically the includes function but done with a map
+			if _, ok := addresses[log.Address]; !ok {
+				continue
+			}
+		}
+		// If the to filtered topics is greater than the amount of topics in logs, skip.
+		if len(topics) > len(log.Topics) {
+			continue
+		}
+		var match bool
+		for i, sub := range topics {
+			match = len(sub) == 0 // empty rule set == wildcard
+			// iterate over the subtopics and look for any match.
+			for _, topic := range sub {
+				if log.Topics[i] == topic {
+					match = true
+					break
+				}
+			}
+			// there was no match, so this log is invalid.
+			if !match {
+				break
+			}
+		}
+		if !match {
+			continue
 		}
 		result = append(result, log)
 	}

cmd/rpcdaemon/commands/erigon_receipts.go
@@ -51,7 +51,7 @@ func (api *ErigonImpl) GetLogsByHash(ctx context.Context, hash common.Hash) ([][
 	return logs, nil
 }
 
-// GetLogs implements eth_getLogs. Returns an array of logs matching a given filter object.
+// GetLogs implements erigon_getLogs. Returns an array of logs matching a given filter object.
 func (api *ErigonImpl) GetLogs(ctx context.Context, crit filters.FilterCriteria) (types.ErigonLogs, error) {
 	var begin, end uint64
 	erigonLogs := types.ErigonLogs{}
@@ -129,6 +129,10 @@ func (api *ErigonImpl) GetLogs(ctx context.Context, crit filters.FilterCriteria)
 		return erigonLogs, nil
 	}
 
+	addrMap := make(map[common.Address]struct{}, len(crit.Addresses))
+	for _, v := range crit.Addresses {
+		addrMap[v] = struct{}{}
+	}
 	iter := blockNumbers.Iterator()
 	for iter.HasNext() {
 		if err = ctx.Err(); err != nil {
@@ -148,7 +152,7 @@ func (api *ErigonImpl) GetLogs(ctx context.Context, crit filters.FilterCriteria)
 				log.Index = logIndex
 				logIndex++
 			}
-			filtered := filterLogs(logs, crit.Addresses, crit.Topics)
+			filtered := filterLogs(logs, addrMap, crit.Topics)
 			if len(filtered) == 0 {
 				return nil
 			}

commit 880f673dff92192750bca7ac8d4110ba08c755a9

allow empty topics filter in eth_getLogs - regression after #5805  (#5885)

cmd/rpcdaemon/commands/eth_receipts.go
@@ -683,6 +683,7 @@ Logs:
 func filterLogs(logs []*types.Log, addresses map[common.Address]struct{}, topics [][]common.Hash) []*types.Log {
 	result := make(types.Logs, 0, len(logs))
 	// populate a set of addresses
+Logs:
 	for _, log := range logs {
 		// empty address list means no filter
 		if len(addresses) > 0 {
@@ -695,9 +696,8 @@ func filterLogs(logs []*types.Log, addresses map[common.Address]struct{}, topics
 		if len(topics) > len(log.Topics) {
 			continue
 		}
-		var match bool
 		for i, sub := range topics {
-			match = len(sub) == 0 // empty rule set == wildcard
+			match := len(sub) == 0 // empty rule set == wildcard
 			// iterate over the subtopics and look for any match.
 			for _, topic := range sub {
 				if log.Topics[i] == topic {
@@ -707,11 +707,8 @@ func filterLogs(logs []*types.Log, addresses map[common.Address]struct{}, topics
 			}
 			// there was no match, so this log is invalid.
 			if !match {
-				break
+				continue Logs
 			}
-		}
-		if !match {
-			continue
 		}
 		result = append(result, log)
 	}