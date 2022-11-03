805230ba638bd358f34653e25be377e3709c15c2
Leonard Chinonso <36096513+leonardchinonso@users.noreply.github.com>
2022-11-03 03:45:36 +0100

    Setup web socket endpoints to listen for new transactions after sending (#5940)
    - Added listening methods for WebSocket subscriptions 
    - Listened for new blocks using the newHeads method to determine when to
    look for a transaction
    - Added new util methods and tests for them
    - Simplified communication to the user upon initiating the devnet tool

--- cmd/devnet/commands/all.go
+++ cmd/devnet/commands/all.go
@@ -8,8 +8,10 @@ import (
 // ExecuteAllMethods runs all the simulation tests for erigon devnet
 func ExecuteAllMethods() {
 	// test connection to JSON RPC
-	fmt.Println("Mocking get requests to JSON RPC...")
-	pingErigonRpc()
+	fmt.Printf("\nPINGING JSON RPC...\n")
+	if err := pingErigonRpc(); err != nil {
+		return
+	}
 	fmt.Println()
 
 	// get balance of the receiver's account
@@ -17,14 +19,29 @@ func ExecuteAllMethods() {
 	fmt.Println()
 
 	// confirm that the txpool is empty
-	fmt.Println("Confirming the tx pool is empty...")
-	callTxPoolContent()
-
-	// send a token from the dev address to the recipient address
-	callSendTx(sendValue, recipientAddress, models.DevAddress)
+	fmt.Println("CONFIRMING TXPOOL IS EMPTY BEFORE SENDING TRANSACTION...")
+	checkTxPoolContent(0, 0)
 	fmt.Println()
 
-	// confirm that the txpool has this transaction in the queue
-	fmt.Println("Confirming the tx pool has the latest transaction...")
-	callTxPoolContent()
+	// send a token from the dev address to the recipient address
+	hash, err := callSendTx(sendValue, recipientAddress, models.DevAddress)
+	if err != nil {
+		fmt.Printf("callSendTx error: %v\n", err)
+		return
+	}
+	fmt.Println()
+
+	// confirm that the txpool has this transaction in the pending queue
+	fmt.Println("CONFIRMING TXPOOL HAS THE LATEST TRANSACTION...")
+	checkTxPoolContent(1, 0)
+	fmt.Println()
+
+	// look for the transaction hash in the newly mined block
+	fmt.Println("LOOKING FOR TRANSACTION IN THE LATEST BLOCK...")
+	callSubscribeToNewHeads(*hash)
+	fmt.Println()
+
+	// confirm that the transaction has been moved from the pending queue and the txpool is empty once again
+	fmt.Println("CONFIRMING TXPOOL IS EMPTY ONCE AGAIN...")
+	checkTxPoolContent(0, 0)
 }
