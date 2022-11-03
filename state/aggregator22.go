b2f53fc10f98d194a47c3002ccbbf8cdc598037f
Alex Sharov <AskAlexSharov@gmail.com>
2022-11-03 11:15:14 +0700

    e3: prune debug log (#724)

--- state/aggregator22.go
+++ state/aggregator22.go
@@ -617,11 +617,11 @@ func (a *Aggregator22) CanPruneFrom(tx kv.Tx) uint64 {
 }
 func (a *Aggregator22) Prune(ctx context.Context, limit uint64) error {
 	a.Warmup(0, cmp.Max(a.aggregationStep, limit)) // warmup is asyn and moving faster than data deletion
-	//defer func(t time.Time) {
-	//	if time.Since(t) > 150*time.Millisecond {
-	//		log.Debug(fmt.Sprintf("prune took: %s\n", time.Since(t)))
-	//	}
-	//}(time.Now())
+	defer func(t time.Time) {
+		if time.Since(t) > time.Second {
+			log.Debug(fmt.Sprintf("prune took: %s\n", time.Since(t)))
+		}
+	}(time.Now())
 	return a.prune(ctx, 0, a.maxTxNum.Load(), limit)
 }
 
