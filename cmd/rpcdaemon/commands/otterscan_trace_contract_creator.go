11f4978ed41dd5db19cceebd982ad7376ddf84cd
Willian Mitsuda <wmitsuda@gmail.com>
2022-11-03 01:32:15 -0300

    Upstream all Otterscan patches to devel (#5945)
    Following our previous discussion on erigon's discord, this PR requests
    to upstream all Otterscan modifications to erigon's repo.
    
    That decision comes after getting feedback from lots of users at events
    this year, and although it may introduce some friction for development,
    it will make integrators life easier by having all our modifications
    available out of box, e.g., dappnode users will get our RPCs since their
    official packages are built from erigon repo.
    
    I'm submitting the source-code as-is, please let me know if you think
    there is a better code organization.
    
    The current set of modifications comprises only new RPCs. There are some
    proposals for extra-stages that would add new tables, but they are still
    WIP and will be submitted separately in future after more testing.

---
+++ cmd/rpcdaemon/commands/otterscan_trace_contract_creator.go
@@ -0,0 +1,50 @@
+package commands
+
+import (
+	"context"
+	"math/big"
+
+	"github.com/ledgerwatch/erigon/common"
+	"github.com/ledgerwatch/erigon/core/types"
+	"github.com/ledgerwatch/erigon/core/vm"
+)
+
+type CreateTracer struct {
+	DefaultTracer
+	ctx     context.Context
+	target  common.Address
+	found   bool
+	Creator common.Address
+	Tx      types.Transaction
+}
+
+func NewCreateTracer(ctx context.Context, target common.Address) *CreateTracer {
+	return &CreateTracer{
+		ctx:    ctx,
+		target: target,
+		found:  false,
+	}
+}
+
+func (t *CreateTracer) SetTransaction(tx types.Transaction) {
+	t.Tx = tx
+}
+
+func (t *CreateTracer) Found() bool {
+	return t.found
+}
+
+func (t *CreateTracer) CaptureStart(env *vm.EVM, depth int, from common.Address, to common.Address, precompile bool, create bool, calltype vm.CallType, input []byte, gas uint64, value *big.Int, code []byte) {
+	if t.found {
+		return
+	}
+	if !create {
+		return
+	}
+	if to != t.target {
+		return
+	}
+
+	t.found = true
+	t.Creator = from
+}
