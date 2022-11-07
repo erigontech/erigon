224f3cf8cb001c9e5020d8f6dc716999db58f8cc
alex.sharov <AskAlexSharov@gmail.com>
2022-11-07 14:56:47 +0700

    save

--- cmd/integration/commands/state_stages.go
+++ cmd/integration/commands/state_stages.go
@@ -182,7 +182,7 @@ func syncBySmallSteps(db kv.RwDB, miningConfig params.MiningConfig, ctx context.
 		}
 	}
 
-	stateStages.DisableStages(stages.Headers, stages.BlockHashes, stages.Bodies, stages.Senders)
+	stateStages.DisableStages(stages.Snapshots, stages.Headers, stages.BlockHashes, stages.Bodies, stages.Senders)
 
 	genesis := core.DefaultGenesisBlockByChainName(chain)
 	syncCfg := ethconfig.Defaults.Sync
@@ -368,6 +368,11 @@ func syncBySmallSteps(db kv.RwDB, miningConfig params.MiningConfig, ctx context.
 			return err
 		}
 		defer tx.Rollback()
+
+		// allow backward loop
+		if unwind > 0 && unwindEvery == 0 {
+			stopAt -= unwind
+		}
 	}
 
 	return nil
@@ -423,7 +428,7 @@ func loopIh(db kv.RwDB, ctx context.Context, unwind uint64) error {
 		return err
 	}
 	defer tx.Rollback()
-	sync.DisableStages(stages.Headers, stages.BlockHashes, stages.Bodies, stages.Senders, stages.Execution, stages.Translation, stages.AccountHistoryIndex, stages.StorageHistoryIndex, stages.TxLookup, stages.Finish)
+	sync.DisableStages(stages.Snapshots, stages.Headers, stages.BlockHashes, stages.Bodies, stages.Senders, stages.Execution, stages.Translation, stages.AccountHistoryIndex, stages.StorageHistoryIndex, stages.TxLookup, stages.Finish)
 	if err = sync.Run(db, tx, false /* firstCycle */, false /* quiet */); err != nil {
 		return err
 	}
