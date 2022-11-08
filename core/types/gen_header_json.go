9cc046defc429cfa285eceea089ac2b5d90ca58f
yperbasis <andrey.ashikhmin@gmail.com>
2022-11-08 10:45:18 +0100

    Simplify Header.EncodeRLP

--- core/types/gen_header_json.go
+++ core/types/gen_header_json.go
@@ -31,7 +31,7 @@ func (h Header) MarshalJSON() ([]byte, error) {
 		Extra       hexutil.Bytes  `json:"extraData"        gencodec:"required"`
 		MixDigest   common.Hash    `json:"mixHash"`
 		Nonce       BlockNonce     `json:"nonce"`
-		BaseFee     *hexutil.Big   `json:"baseFeePerGas" rlp:"optional"`
+		BaseFee     *hexutil.Big   `json:"baseFeePerGas"`
 		Hash        common.Hash    `json:"hash"`
 	}
 	var enc Header
@@ -73,7 +73,7 @@ func (h *Header) UnmarshalJSON(input []byte) error {
 		Extra       *hexutil.Bytes  `json:"extraData"        gencodec:"required"`
 		MixDigest   *common.Hash    `json:"mixHash"`
 		Nonce       *BlockNonce     `json:"nonce"`
-		BaseFee     *hexutil.Big    `json:"baseFeePerGas" rlp:"optional"`
+		BaseFee     *hexutil.Big    `json:"baseFeePerGas"`
 	}
 	var dec Header
 	if err := json.Unmarshal(input, &dec); err != nil {
