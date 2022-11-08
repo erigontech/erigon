b7650e2509db842f4ab1bfd3e0878b1856861df8
leonardchinonso <36096513+leonardchinonso@users.noreply.github.com>
2022-11-08 12:07:16 +0100

    Simple test for powerOf2

---
+++ cmd/lightclient/lightclient/utils_test.go
@@ -0,0 +1,32 @@
+package lightclient
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func Test_powerOf2(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			require.EqualValues(t, r, "integer overflow")
+		}
+	}()
+
+	testCases := []struct {
+		input       uint64
+		expected    uint64
+		shouldPanic bool
+	}{
+		{0, 1, false},
+		{2, 4, false},
+		{16, 65536, false},
+		{64, 0, true},
+	}
+
+	for _, testCase := range testCases {
+		got := powerOf2(testCase.input)
+		if !testCase.shouldPanic {
+			require.EqualValues(t, got, testCase.expected)
+		}
+	}
+}
