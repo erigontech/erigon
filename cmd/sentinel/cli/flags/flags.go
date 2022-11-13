d9b94a89e816588e4804febd35e7931d91c52876
Enrique Jose Avila Asapche <eavilaasapche@gmail.com>
2022-11-13 13:14:35 +0000

    changed from lc to cl

--- cmd/sentinel/cli/flags/flags.go
+++ cmd/sentinel/cli/flags/flags.go
@@ -14,37 +14,37 @@ var (
 		Value: "127.0.0.1",
 	}
 	ConsensusLayerTcpPort = cli.UintFlag{
-		Name:  "lc.tcp.port",
+		Name:  "cl.tcp.port",
 		Usage: "sets consensus layer tcp port",
 		Value: 9000,
 	}
 	ConsensusLayerVerbosity = cli.UintFlag{
-		Name:  "lc.verbosity",
+		Name:  "cl.verbosity",
 		Usage: "specify consensus layer verbosity level 0=silent, 1=err, 2=warn, 3=info, 4=debug, 5=details",
 		Value: 3,
 	}
 	ConsensusLayerServerPort = cli.IntFlag{
-		Name:  "lc.server.port",
+		Name:  "cl.server.port",
 		Usage: "sets the consensus layer server port",
 		Value: 7777,
 	}
 	ConsensusLayerServerAddr = cli.StringFlag{
-		Name:  "lc.server.addr",
+		Name:  "cl.server.addr",
 		Usage: "sets the consensus layer server host addr",
 		Value: "localhost",
 	}
 	ConsensusLayerServerProtocol = cli.UintFlag{
-		Name:  "lc.server.protocol",
+		Name:  "cl.server.protocol",
 		Usage: "sets the consensus layer server protocol 1=tcp 2=udp",
 		Value: 1,
 	}
 	ConsensusLayerChain = cli.StringFlag{
-		Name:  "lc.chain",
+		Name:  "cl.chain",
 		Usage: "sets the chain specs for the consensus layer",
 		Value: "mainnet",
 	}
 	ConsensusLayerDiscovery = cli.BoolTFlag{
-		Name:  "lc.discover",
+		Name:  "cl.discover",
 		Usage: "turn off or on the consensus layer finding peers",
 	}
 )
