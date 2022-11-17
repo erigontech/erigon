e817ee83b512648620b5e500d9bbbf84debdc43d
Michael Neuder <mikeneuder@google.com>
2022-11-17 10:53:47 -0700

    Send multiple requests

--- cmd/erigon-cl/cl-core/syncfromcheckpoint.go
+++ cmd/erigon-cl/cl-core/syncfromcheckpoint.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/hex"
 	"fmt"
-	"strings"
 	"time"
 
 	"github.com/ledgerwatch/erigon/cl/cltypes"
@@ -15,31 +14,53 @@ import (
 
 const blocksPerRequest = 1024
 
+// Debug function to recieve test packets on the req/resp domain.
+func sendRequest(ctx context.Context, s consensusrpc.SentinelClient, req *consensusrpc.RequestData) {
+	newReqTicker := time.NewTicker(1000 * time.Millisecond)
+	for {
+		select {
+		case <-ctx.Done():
+		case <-newReqTicker.C:
+			go func() {
+
+			}()
+		}
+	}
+}
+
 func GetCheckpointBlock(ctx context.Context, sc consensusrpc.SentinelClient, cp *cltypes.BeaconState, fd [4]byte) (*cltypes.SignedBeaconBlockBellatrix, error) {
 	// Request for blocks by root given the finalized checkpoint root.
 	finalizedRootSlice := [][32]byte{cp.FinalizedCheckpoint.Root}
 
-	var resp []cltypes.ObjectSSZ
-	var err error
-	for {
-		log.Info("Getting checkpoint block by root", "root", hex.EncodeToString(cp.FinalizedCheckpoint.Root[:]))
-		resp, err = rpc.SendBeaconBlocksByRootReq(ctx, finalizedRootSlice, sc)
-		if err != nil && strings.Contains(err.Error(), "no peers") {
-			log.Info("Retrying request, no peers found", "retrying", err)
-		} else if err != nil {
-			return nil, fmt.Errorf("unable to receive BeaconBlocksByRoot: %v", err)
-		} else {
-			break
-		}
-		time.Sleep(10 * time.Second)
-	}
+	newReqTicker := time.NewTicker(1000 * time.Millisecond)
 
-	if len(resp) != 1 {
-		return nil, fmt.Errorf("unexpected response length, got %d want %d", len(resp), 1)
+	respChan := make(chan *cltypes.SignedBeaconBlockBellatrix)
+	errChan := make(chan error)
+	for {
+		select {
+		case <-newReqTicker.C:
+			go func() {
+				log.Info("Getting checkpoint block by root", "root", hex.EncodeToString(cp.FinalizedCheckpoint.Root[:]))
+				resp, err := rpc.SendBeaconBlocksByRootReq(ctx, finalizedRootSlice, sc)
+				if err != nil {
+					errChan <- err
+					return
+				}
+				if len(resp) != 1 {
+					errChan <- fmt.Errorf("unexpected response length, got %d want %d", len(resp), 1)
+					return
+				}
+				result, ok := resp[0].(*cltypes.SignedBeaconBlockBellatrix)
+				if !ok {
+					errChan <- fmt.Errorf("unable to cast object: %+v into block", resp[0])
+				}
+				respChan <- result
+			}()
+		case err := <-errChan:
+			log.Error("Error received from peer", "error", err)
+		case block := <-respChan:
+			log.Info("Block received by root")
+			return block, nil
+		}
 	}
-	result, ok := resp[0].(*cltypes.SignedBeaconBlockBellatrix)
-	if !ok {
-		return nil, fmt.Errorf("unable to cast object: %+v into block", resp[0])
-	}
-	return result, nil
 }
