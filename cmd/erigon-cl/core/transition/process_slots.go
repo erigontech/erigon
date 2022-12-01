7b1ac7c07698684999e2bb8a5c44b08d885bd678
Michael Neuder <mikeneuder@google.com>
2022-11-30 23:17:34 -0500

    Add state transition function and unit test.

--- cmd/erigon-cl/core/transition/process_slots.go
+++ cmd/erigon-cl/core/transition/process_slots.go
@@ -7,6 +7,31 @@ import (
 	"github.com/ledgerwatch/erigon/cl/cltypes"
 )
 
+func (s *StateTransistor) transitionState(state *cltypes.BeaconState, block *cltypes.SignedBeaconBlockBellatrix, validate bool) error {
+	cur_block := block.Block
+	s.processSlots(state, cur_block.Slot)
+	if validate {
+		valid, err := s.verifyBlockSignature(state, block)
+		if err != nil {
+			return fmt.Errorf("error validating block signature: %v", err)
+		}
+		if !valid {
+			return fmt.Errorf("block not valid")
+		}
+	}
+	// TODO add logic to process block and update state.
+	if validate {
+		expectedStateRoot, err := state.HashTreeRoot()
+		if err != nil {
+			return fmt.Errorf("unable to generate state root: %v", err)
+		}
+		if expectedStateRoot != cur_block.StateRoot {
+			return fmt.Errorf("expected state root differs from received state root")
+		}
+	}
+	return nil
+}
+
 // transitionSlot is called each time there is a new slot to process
 func (s *StateTransistor) transitionSlot(state *cltypes.BeaconState) error {
 	previousStateRoot, err := state.HashTreeRoot()
@@ -29,7 +54,7 @@ func (s *StateTransistor) processSlots(state *cltypes.BeaconState, slot uint64) 
 	if slot <= state.Slot {
 		return fmt.Errorf("new slot: %d not greater than state slot: %d", slot, state.Slot)
 	}
-	// Process each slot
+	// Process each slot.
 	for i := state.Slot; i < slot; i++ {
 		err := s.transitionSlot(state)
 		if err != nil {
