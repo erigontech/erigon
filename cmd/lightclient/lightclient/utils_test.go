4e68dfc3ffe0c98d537b7824e20a429a592cdfbe
leonardchinonso <36096513+leonardchinonso@users.noreply.github.com>
2022-12-01 01:52:49 +0100

    Updating tests

--- cmd/lightclient/lightclient/utils_test.go
+++ cmd/lightclient/lightclient/utils_test.go
@@ -6,18 +6,9 @@ import (
 )
 
 func TestPowerOf2(t *testing.T) {
-	defer func() {
-		if r := recover(); r != nil {
-			require.EqualValues(t, r, "integer overflow")
-		}
-	}()
-
 	// naively cover all test cases from 0 to 63
 	for i := 0; i < 64; i++ {
 		got := powerOf2(uint64(i))
 		require.EqualValues(t, got, 1<<i)
 	}
-
-	// check that 64 panics
-	powerOf2(64)
 }
