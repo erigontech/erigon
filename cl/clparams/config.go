e864fc26ae82ffa344baefaace2a091656d89a69
Chris Ziogas <ziogaschr@gmail.com>
2022-12-02 11:15:26 +0200

    cl/clparams: add beacon state endpoints to Shandong config

--- cl/clparams/config.go
+++ cl/clparams/config.go
@@ -243,7 +243,9 @@ var CheckpointSyncEndpoints = map[NetworkType][]string{
 		"https://sepolia.checkpoint-sync.ethdevops.io/eth/v2/debug/beacon/states/finalized",
 		"https://sepolia.beaconstate.info/eth/v2/debug/beacon/states/finalized",
 	},
-	// ShandongNetwork: {},
+	ShandongNetwork: {
+		"https://raw.githubusercontent.com/ethereumjs/consensus-deployment-ansible/master/shandong-testnet/custom_config_data/genesis.ssz",
+	},
 }
 
 // BeaconChainConfig contains constant configs for node to participate in beacon chain.
