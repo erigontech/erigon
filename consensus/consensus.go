1e2829cdb80b78dbc39bf3c9639c4e1b35354472
alex.sharov <AskAlexSharov@gmail.com>
2022-12-04 19:40:23 +0700

    save

--- consensus/consensus.go
+++ consensus/consensus.go
@@ -109,7 +109,8 @@ type EngineWriter interface {
 	Prepare(chain ChainHeaderReader, header *types.Header, state *state.IntraBlockState) error
 
 	// Initialize runs any pre-transaction state modifications (e.g. epoch start)
-	Initialize(config *params.ChainConfig, chain ChainHeaderReader, e EpochReader, header *types.Header, state *state.IntraBlockState, uncles []*types.Header, syscall SystemCall)
+	Initialize(config *params.ChainConfig, chain ChainHeaderReader, e EpochReader, header *types.Header,
+		state *state.IntraBlockState, uncles []*types.Header, syscall SystemCall)
 
 	// Finalize runs any post-transaction state modifications (e.g. block rewards)
 	// but does not assemble the block.
