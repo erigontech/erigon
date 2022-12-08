aafd188cab72ec25f2d71b5282d1afc869781276
Giulio Rebuffo <giulio.rebuffo@gmail.com>
2022-12-08 23:48:06 +0100

    new pkg

---
+++ cmd/erigon-cl/core/state/getters.go
@@ -0,0 +1,142 @@
+package state
+
+import (
+	"github.com/ledgerwatch/erigon/cl/cltypes"
+	"github.com/ledgerwatch/erigon/common"
+)
+
+// Just a bunch of simple getters.
+
+func (b *BeaconState) GenesisTime() uint64 {
+	return b.genesisTime
+}
+
+func (b *BeaconState) GenesisValidatorsRoot() common.Hash {
+	return b.genesisValidatorsRoot
+}
+
+func (b *BeaconState) Slot() uint64 {
+	return b.slot
+}
+
+func (b *BeaconState) Fork() *cltypes.Fork {
+	return b.fork
+}
+
+func (b *BeaconState) LatestBlockHeader() *cltypes.BeaconBlockHeader {
+	return b.latestBlockHeader
+}
+
+func (b *BeaconState) BlockRoots() [][32]byte {
+	return b.blockRoots
+}
+
+func (b *BeaconState) StateRoots() [][32]byte {
+	return b.stateRoots
+}
+
+func (b *BeaconState) HistoricalRoots() [][32]byte {
+	return b.historicalRoots
+}
+
+func (b *BeaconState) Eth1Data() *cltypes.Eth1Data {
+	return b.eth1Data
+}
+
+func (b *BeaconState) Eth1DataVotes() []*cltypes.Eth1Data {
+	return b.eth1DataVotes
+}
+
+func (b *BeaconState) Eth1DepositIndex() uint64 {
+	return b.eth1DepositIndex
+}
+
+func (b *BeaconState) Validators() []*cltypes.Validator {
+	return b.validators
+}
+
+func (b *BeaconState) Balances() []uint64 {
+	return b.balances
+}
+
+func (b *BeaconState) RandaoMixes() [][32]byte {
+	return b.randaoMixes
+}
+
+func (b *BeaconState) Slashings() []uint64 {
+	return b.slashings
+}
+
+func (b *BeaconState) PreviousEpochParticipation() []byte {
+	return b.previousEpochParticipation
+}
+
+func (b *BeaconState) CurrentEpochParticipation() []byte {
+	return b.currentEpochParticipation
+}
+
+func (b *BeaconState) JustificationBits() []byte {
+	return b.justificationBits
+}
+
+func (b *BeaconState) PreviousJustifiedCheckpoint() *cltypes.Checkpoint {
+	return b.previousJustifiedCheckpoint
+}
+
+func (b *BeaconState) CurrentJustifiedCheckpoint() *cltypes.Checkpoint {
+	return b.currentJustifiedCheckpoint
+}
+
+func (b *BeaconState) FinalizedCheckpoint() *cltypes.Checkpoint {
+	return b.finalizedCheckpoint
+}
+
+func (b *BeaconState) CurrentSyncCommittee() *cltypes.SyncCommittee {
+	return b.currentSyncCommittee
+}
+
+func (b *BeaconState) NextSyncCommittee() *cltypes.SyncCommittee {
+	return b.nextSyncCommittee
+}
+
+func (b *BeaconState) LatestExecutionPayloadHeader() *cltypes.ExecutionHeader {
+	return b.latestExecutionPayloadHeader
+}
+
+// GetStateSSZObject allows us to use ssz methods.
+func (b *BeaconState) GetStateSSZObject() cltypes.ObjectSSZ {
+	switch b.version {
+	case BellatrixVersion:
+		return &cltypes.BeaconStateBellatrix{
+			GenesisTime:                  b.genesisTime,
+			GenesisValidatorsRoot:        b.genesisValidatorsRoot,
+			Slot:                         b.slot,
+			Fork:                         b.fork,
+			LatestBlockHeader:            b.latestBlockHeader,
+			BlockRoots:                   b.blockRoots,
+			StateRoots:                   b.stateRoots,
+			HistoricalRoots:              b.historicalRoots,
+			Eth1Data:                     b.eth1Data,
+			Eth1DataVotes:                b.eth1DataVotes,
+			Eth1DepositIndex:             b.eth1DepositIndex,
+			Validators:                   b.validators,
+			Balances:                     b.balances,
+			RandaoMixes:                  b.randaoMixes,
+			Slashings:                    b.slashings,
+			PreviousEpochParticipation:   b.previousEpochParticipation,
+			CurrentEpochParticipation:    b.currentEpochParticipation,
+			JustificationBits:            b.justificationBits,
+			FinalizedCheckpoint:          b.finalizedCheckpoint,
+			CurrentJustifiedCheckpoint:   b.currentJustifiedCheckpoint,
+			PreviousJustifiedCheckpoint:  b.previousJustifiedCheckpoint,
+			InactivityScores:             b.inactivityScores,
+			CurrentSyncCommittee:         b.currentSyncCommittee,
+			NextSyncCommittee:            b.nextSyncCommittee,
+			LatestExecutionPayloadHeader: b.latestExecutionPayloadHeader,
+		}
+	case CapellaVersion:
+		panic("not implemented")
+	default:
+		panic("not a valid version")
+	}
+}
---
+++ cmd/erigon-cl/core/state/setters.go
@@ -0,0 +1,128 @@
+package state
+
+import (
+	"github.com/ledgerwatch/erigon/cl/cltypes"
+	"github.com/ledgerwatch/erigon/common"
+)
+
+// Below are setters. Note that they also dirty the state.
+
+func (b *BeaconState) SetGenesisTime(genesisTime uint64) {
+	b.touchedLeaves[GenesisTimeLeafIndex] = true
+	b.genesisTime = genesisTime
+}
+
+func (b *BeaconState) SetGenesisValidatorsRoot(genesisValidatorRoot common.Hash) {
+	b.touchedLeaves[GenesisValidatorsRootLeafIndex] = true
+	b.genesisValidatorsRoot = genesisValidatorRoot
+}
+
+func (b *BeaconState) SetSlot(slot uint64) {
+	b.touchedLeaves[SlotLeafIndex] = true
+	b.slot = slot
+}
+
+func (b *BeaconState) SetFork(fork *cltypes.Fork) {
+	b.touchedLeaves[ForkLeafIndex] = true
+	b.fork = fork
+}
+
+func (b *BeaconState) SetLatestBlockHeader(header *cltypes.BeaconBlockHeader) {
+	b.touchedLeaves[LatestBlockHeaderLeafIndex] = true
+	b.latestBlockHeader = header
+}
+
+func (b *BeaconState) SetBlockRoots(blockRoots [][32]byte) {
+	b.touchedLeaves[BlockRootsLeafIndex] = true
+	b.blockRoots = blockRoots
+}
+
+func (b *BeaconState) SetStateRoots(stateRoots [][32]byte) {
+	b.touchedLeaves[StateRootsLeafIndex] = true
+	b.stateRoots = stateRoots
+}
+
+func (b *BeaconState) SetHistoricalRoots(historicalRoots [][32]byte) {
+	b.touchedLeaves[HistoricalRootsLeafIndex] = true
+	b.historicalRoots = historicalRoots
+}
+
+func (b *BeaconState) SetEth1Data(eth1Data *cltypes.Eth1Data) {
+	b.touchedLeaves[Eth1DataLeafIndex] = true
+	b.eth1Data = eth1Data
+}
+
+func (b *BeaconState) SetEth1DataVotes(eth1DataVotes []*cltypes.Eth1Data) {
+	b.touchedLeaves[Eth1DataVotesLeafIndex] = true
+	b.eth1DataVotes = eth1DataVotes
+}
+
+func (b *BeaconState) SetEth1DepositIndex(eth1DepositIndex uint64) {
+	b.touchedLeaves[Eth1DepositIndexLeafIndex] = true
+	b.eth1DepositIndex = eth1DepositIndex
+}
+
+func (b *BeaconState) SetValidators(validators []*cltypes.Validator) {
+	b.touchedLeaves[ValidatorsLeafIndex] = true
+	b.validators = validators
+}
+
+func (b *BeaconState) SetBalances(balances []uint64) {
+	b.touchedLeaves[BalancesLeafIndex] = true
+	b.balances = balances
+}
+
+func (b *BeaconState) SetRandaoMixes(randaoMixes [][32]byte) {
+	b.touchedLeaves[RandaoMixesLeafIndex] = true
+	b.randaoMixes = randaoMixes
+}
+
+func (b *BeaconState) SetSlashings(slashings []uint64) {
+	b.touchedLeaves[SlashingsLeafIndex] = true
+	b.slashings = slashings
+}
+
+func (b *BeaconState) SetPreviousEpochParticipation(previousEpochParticipation []byte) {
+	b.touchedLeaves[PreviousEpochParticipationLeafIndex] = true
+	b.previousEpochParticipation = previousEpochParticipation
+}
+
+func (b *BeaconState) SetCurrentEpochParticipation(currentEpochParticipation []byte) {
+	b.touchedLeaves[CurrentEpochParticipationLeafIndex] = true
+	b.currentEpochParticipation = currentEpochParticipation
+}
+
+func (b *BeaconState) SetJustificationBits(justificationBits []byte) {
+	b.touchedLeaves[JustificationBitsLeafIndex] = true
+	b.justificationBits = justificationBits
+}
+
+func (b *BeaconState) SetPreviousJustifiedCheckpoint(previousJustifiedCheckpoint *cltypes.Checkpoint) {
+	b.touchedLeaves[PreviousJustifiedCheckpointLeafIndex] = true
+	b.previousJustifiedCheckpoint = previousJustifiedCheckpoint
+}
+
+func (b *BeaconState) SetCurrentJustifiedCheckpoint(currentJustifiedCheckpoint *cltypes.Checkpoint) {
+	b.touchedLeaves[CurrentJustifiedCheckpointLeafIndex] = true
+	b.currentJustifiedCheckpoint = currentJustifiedCheckpoint
+}
+
+func (b *BeaconState) SetFinalizedCheckpoint(finalizedCheckpoint *cltypes.Checkpoint) {
+	b.touchedLeaves[FinalizedCheckpointLeafIndex] = true
+	b.finalizedCheckpoint = finalizedCheckpoint
+}
+
+func (b *BeaconState) SetCurrentSyncCommittee(currentSyncCommittee *cltypes.SyncCommittee) {
+	b.touchedLeaves[CurrentSyncCommitteeLeafIndex] = true
+	b.currentSyncCommittee = currentSyncCommittee
+}
+
+func (b *BeaconState) SetNextSyncCommittee(nextSyncCommittee *cltypes.SyncCommittee) {
+	b.touchedLeaves[NextSyncCommitteeLeafIndex] = true
+	b.nextSyncCommittee = nextSyncCommittee
+}
+
+func (b *BeaconState) SetLatestExecutionPayloadHeader(header *cltypes.ExecutionHeader) {
+	b.touchedLeaves[LatestExecutionPayloadHeaderLeafIndex] = true
+	b.latestExecutionPayloadHeader = header
+}
