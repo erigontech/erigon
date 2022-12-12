581f18ffb84918667677e8ae1717aaa874ab1325
Chris Ziogas <ziogaschr@gmail.com>
2022-12-12 13:00:58 +0200

    tests: keep ArrowGlacierToMergeAtDiffC0000 as a fork in tests

--- tests/init.go
+++ tests/init.go
@@ -227,6 +227,21 @@ var Forks = map[string]*params.ChainConfig{
 		MergeNetsplitBlock:      big.NewInt(0),
 		TerminalTotalDifficulty: big.NewInt(0),
 	},
+	"ArrowGlacierToMergeAtDiffC0000": {
+		ChainID:                 big.NewInt(1),
+		HomesteadBlock:          big.NewInt(0),
+		TangerineWhistleBlock:   big.NewInt(0),
+		SpuriousDragonBlock:     big.NewInt(0),
+		ByzantiumBlock:          big.NewInt(0),
+		ConstantinopleBlock:     big.NewInt(0),
+		PetersburgBlock:         big.NewInt(0),
+		IstanbulBlock:           big.NewInt(0),
+		MuirGlacierBlock:        big.NewInt(0),
+		BerlinBlock:             big.NewInt(0),
+		LondonBlock:             big.NewInt(0),
+		ArrowGlacierBlock:       big.NewInt(0),
+		TerminalTotalDifficulty: big.NewInt(0xC0000),
+	},
 	"Shanghai": {
 		ChainID:                 big.NewInt(1),
 		HomesteadBlock:          big.NewInt(0),
@@ -240,8 +255,10 @@ var Forks = map[string]*params.ChainConfig{
 		BerlinBlock:             big.NewInt(0),
 		LondonBlock:             big.NewInt(0),
 		ArrowGlacierBlock:       big.NewInt(0),
+		GrayGlacierBlock:        big.NewInt(0),
+		MergeNetsplitBlock:      big.NewInt(0),
 		ShanghaiTime:            big.NewInt(0),
-		TerminalTotalDifficulty: big.NewInt(0xC0000),
+		TerminalTotalDifficulty: big.NewInt(0),
 	},
 }
 
