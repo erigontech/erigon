8b59c136485d6155117252f6555269dd00a99273
alex.sharov <AskAlexSharov@gmail.com>
2022-12-22 12:58:35 +0700

    save

--- turbo/snapshotsync/block_reader.go
+++ turbo/snapshotsync/block_reader.go
@@ -176,7 +176,7 @@ func (back *RemoteBlockReader) TxnByIdxInBlock(ctx context.Context, tx kv.Getter
 	if b == nil {
 		return nil, nil
 	}
-	if len(b.Transactions) < i {
+	if len(b.Transactions) <= i {
 		return nil, nil
 	}
 	return b.Transactions[i], nil
