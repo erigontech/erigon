1a8f7651389540386de8902fc319824eb6039aa6
a <a@tuxpa.in>
2022-12-23 15:51:24 -0600

    save me god

--- turbo/rpchelper/subscription.go
+++ turbo/rpchelper/subscription.go
@@ -2,8 +2,7 @@ package rpchelper
 
 import (
 	"context"
-
-	"github.com/google/uuid"
+	"sync"
 )
 
 type Sub[T any] interface {
@@ -15,37 +14,75 @@ type Sub[T any] interface {
 
 type chan_sub[T any] struct {
 	ch chan T
+	in chan T
 
 	closed chan struct{}
-	ctx    context.Context
-	cn     context.CancelFunc
-	uuid   uuid.UUID
+
+	ctx context.Context
+	cn  context.CancelFunc
+
+	c sync.Cond
 }
 
 func NewChanSub[T any](ch chan T) *chan_sub[T] {
 	o := &chan_sub[T]{}
-	o.uuid = uuid.New()
 	o.ch = ch
+	o.in = make(chan T)
+	o.closed = make(chan struct{})
 	o.ctx, o.cn = context.WithCancel(context.Background())
-	o.closed = make(chan struct{}, 1)
+	go o.loop()
 	return o
 }
 
+func (s *chan_sub[T]) loop() {
+	for {
+		select {
+		// if canceled, exit loop
+		case <-s.ctx.Done():
+			return
+			// if there are any entries in the input buffer, grab
+		case v := <-s.in:
+			select {
+			// send to output channel
+			case s.ch <- v:
+			case <-s.ctx.Done():
+				return
+			}
+		}
+	}
+}
+
 func (s *chan_sub[T]) Send(x T) {
-	//log.Println(s.uuid, "send")
-	//defer log.Println(s.uuid, "send exit")
 	select {
+	// if the output buffer is empty, send
 	case s.ch <- x:
+		// if sub is canceled, dispose message
 	case <-s.ctx.Done():
+	default:
+		select {
+		// if input buffer is empty, send
+		case s.in <- x:
+			// if sub is canceled, dispose message
+		case <-s.ctx.Done():
+		}
 	}
 }
 func (s *chan_sub[T]) Close() {
-	//log.Println(s.uuid, "close")
-	//defer log.Println(s.uuid, "close exit")
-	s.cn()
 	select {
 	case <-s.ctx.Done():
 		return
+	default:
+	}
+	// its possible for multiple goroutines to get to this point, if Close is called twice at the same time
+	// close the context - allows any sends to exit
+	s.cn()
+	select {
+	case s.closed <- struct{}{}:
+		// but it is not possible for multiple goroutines to get to this point
+		// drain the channel
+		for range s.ch {
+		}
+		close(s.ch)
 	default:
 	}
 }
