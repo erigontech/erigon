086f696bcc69e2abc854b8644278714fbd092b1a
yperbasis <andrey.ashikhmin@gmail.com>
2022-12-23 15:29:04 +0100

    Add TestExecutionSpec

---
+++ tests/exec_spec_test.go
@@ -0,0 +1,25 @@
+package tests
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/ledgerwatch/log/v3"
+)
+
+func TestExecutionSpec(t *testing.T) {
+	defer log.Root().SetHandler(log.Root().GetHandler())
+	log.Root().SetHandler(log.LvlFilterHandler(log.LvlError, log.StderrHandler))
+
+	bt := new(testMatcher)
+
+	// TODO(yperbasis): test everything, not only withdrawals
+	dir := filepath.Join(".", "execution-spec-tests", "withdrawals")
+
+	bt.walk(t, dir, func(t *testing.T, name string, test *BlockTest) {
+		// import pre accounts & construct test genesis block & state root
+		if err := bt.checkFailure(t, test.Run(t, false)); err != nil {
+			t.Error(err)
+		}
+	})
+}
