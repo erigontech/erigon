cf7e0e292cbe3f020b82ef98c3d20cc1e6e2e4f9
Giulio rebuffo <giulio.rebuffo@gmail.com>
2022-12-27 17:22:52 +0100

    Compact Attestations final form (#6446)

--- cl/cltypes/attestation_encoding.go
+++ cl/cltypes/attestation_encoding.go
@@ -3,7 +3,6 @@ package cltypes
 import (
 	"encoding/binary"
 
-	"github.com/ledgerwatch/erigon/cl/utils"
 	"github.com/ledgerwatch/erigon/common"
 )
 
@@ -13,53 +12,81 @@ func EncodeAttestationsForStorage(attestations []*Attestation) []byte {
 	if len(attestations) == 0 {
 		return nil
 	}
+
+	referencedAttestations := []*AttestationData{
+		nil, // Full diff
+	}
 	// Pre-allocate some memory.
 	encoded := make([]byte, 0, maxAttestationSize*len(attestations)+1)
 
-	// Take the first attestation datas and use it as the default value.
-	defaultData := attestations[0].Data
-	// encode default attestation
-	encoded = append(encoded, EncodeAttestationDataForStorage(defaultData, nil)...)
 	for _, attestation := range attestations {
 		// Encode attestation metadata
 		// Also we need to keep track of aggregation bits size manually.
-		encoded = append(encoded, encodeNumber(uint64(len(attestation.AggregationBits)))...)
-		encoded = append(encoded, attestation.AggregationBits...)
+		encoded = append(encoded, encodeAggregationBits(attestation.AggregationBits)...)
+		// Encode signature
 		encoded = append(encoded, attestation.Signature[:]...)
 		// Encode attestation body
-		encoded = append(encoded, EncodeAttestationDataForStorage(attestation.Data, defaultData)...)
+		var bestEncoding []byte
+		bestEncodingIndex := 0
+		// try all non-repeating attestations.
+		for i, att := range referencedAttestations {
+			currentEncoding := EncodeAttestationDataForStorage(attestation.Data, att)
+			// check if we find a better fit.
+			if len(bestEncoding) == 0 || len(bestEncoding) > len(currentEncoding) {
+				bestEncodingIndex = i
+				bestEncoding = currentEncoding
+				// cannot get lower than 1, so accept it as best.
+				if len(bestEncoding) == 1 {
+					break
+				}
+			}
+		}
+		// If it is not repeated then save it.
+		if len(bestEncoding) != 1 {
+			referencedAttestations = append(referencedAttestations, attestation.Data)
+		}
+		encoded = append(encoded, byte(bestEncodingIndex))
+		encoded = append(encoded, bestEncoding...)
+		// Encode attester index
+		encoded = append(encoded, encodeNumber(attestation.Data.Index)...)
 	}
-	return utils.CompressSnappy(encoded)
+	return encoded
 }
 
 func DecodeAttestationsForStorage(buf []byte) ([]*Attestation, error) {
 	if len(buf) == 0 {
 		return nil, nil
 	}
-	buf, err := utils.DecompressSnappy(buf)
-	if err != nil {
-		return nil, err
+
+	referencedAttestations := []*AttestationData{
+		nil, // Full diff
 	}
 	var attestations []*Attestation
-	// Take the first attestation datas and use it as the default value.
-	n, defaultData := DecodeAttestationDataForStorage(buf, nil)
 	// current position is how much we read.
-	pos := n
+	pos := 0
 	for pos != len(buf) {
-		// Figure out how long are aggragation bits
-		bitsLength := decodeNumber(buf[pos:])
-		pos += 4
+		n, aggrBits := rebuildAggregationBits(buf[pos:])
+		pos += n
 		// Decode aggrefation bits
 		attestation := &Attestation{
-			AggregationBits: common.CopyBytes(buf[pos : pos+int(bitsLength)]),
+			AggregationBits: aggrBits,
 		}
-		pos += int(bitsLength)
 		// Decode signature
 		copy(attestation.Signature[:], buf[pos:])
 		pos += 96
-		// Encode attestation body
-		n, attestation.Data = DecodeAttestationDataForStorage(buf[pos:], defaultData)
+		// decode attestation body
+		// 1) read comparison index
+		comparisonIndex := int(buf[pos])
+		pos++
+		n, attestation.Data = DecodeAttestationDataForStorage(buf[pos:], referencedAttestations[comparisonIndex])
+		// field set is not null, so we need to remember it.
+		if n != 1 {
+			referencedAttestations = append(referencedAttestations, attestation.Data)
+		}
 		pos += n
+		// decode attester index
+		attestation.Data.Index = decodeNumber(buf[pos:])
+		pos += 3
 		attestations = append(attestations, attestation)
 	}
 	return attestations, nil
@@ -68,11 +95,13 @@ func DecodeAttestationsForStorage(buf []byte) ([]*Attestation, error) {
 func encodeNumber(x uint64) []byte {
 	b := make([]byte, 4)
 	binary.BigEndian.PutUint32(b, uint32(x))
-	return b
+	return b[1:]
 }
 
 func decodeNumber(b []byte) uint64 {
-	return uint64(binary.BigEndian.Uint32(b[:4]))
+	tmp := make([]byte, 4)
+	copy(tmp[1:], b[:3])
+	return uint64(binary.BigEndian.Uint32(tmp))
 }
 
 // EncodeAttestationsDataForStorage encodes attestation data and compress everything by defaultData.
@@ -86,40 +115,34 @@ func EncodeAttestationDataForStorage(data *AttestationData, defaultData *Attesta
 		fieldSet = 1
 	}
 
-	if defaultData == nil || data.Index != defaultData.Index {
-		ret = append(ret, encodeNumber(data.Index)...)
-	} else {
-		fieldSet |= 2
-	}
-
 	if defaultData == nil || data.BeaconBlockHash != defaultData.BeaconBlockHash {
 		ret = append(ret, data.BeaconBlockHash[:]...)
 	} else {
-		fieldSet |= 4
+		fieldSet |= 2
 	}
 
 	if defaultData == nil || data.Source.Epoch != defaultData.Source.Epoch {
 		ret = append(ret, encodeNumber(data.Source.Epoch)...)
 	} else {
-		fieldSet |= 8
+		fieldSet |= 4
 	}
 
 	if defaultData == nil || data.Source.Root != defaultData.Source.Root {
 		ret = append(ret, data.Source.Root[:]...)
 	} else {
-		fieldSet |= 16
+		fieldSet |= 8
 	}
 
 	if defaultData == nil || data.Target.Epoch != defaultData.Target.Epoch {
 		ret = append(ret, encodeNumber(data.Target.Epoch)...)
 	} else {
-		fieldSet |= 32
+		fieldSet |= 16
 	}
 
 	if defaultData == nil || data.Target.Root != defaultData.Target.Root {
 		ret = append(ret, data.Target.Root[:]...)
 	} else {
-		fieldSet |= 64
+		fieldSet |= 32
 	}
 	return append([]byte{fieldSet}, ret...)
 }
@@ -139,49 +162,66 @@ func DecodeAttestationDataForStorage(buf []byte, defaultData *AttestationData) (
 		data.Slot = defaultData.Slot
 	} else {
 		data.Slot = decodeNumber(buf[n:])
-		n += 4
+		n += 3
 	}
 
 	if fieldSet&2 > 0 {
-		data.Index = defaultData.Index
-	} else {
-		data.Index = decodeNumber(buf[n:])
-		n += 4
-	}
-
-	if fieldSet&4 > 0 {
 		data.BeaconBlockHash = defaultData.BeaconBlockHash
 	} else {
 		data.BeaconBlockHash = common.BytesToHash(buf[n : n+32])
 		n += 32
 	}
 
-	if fieldSet&8 > 0 {
+	if fieldSet&4 > 0 {
 		data.Source.Epoch = defaultData.Source.Epoch
 	} else {
 		data.Source.Epoch = decodeNumber(buf[n:])
-		n += 4
+		n += 3
 	}
 
-	if fieldSet&16 > 0 {
+	if fieldSet&8 > 0 {
 		data.Source.Root = defaultData.Source.Root
 	} else {
 		data.Source.Root = common.BytesToHash(buf[n : n+32])
 		n += 32
 	}
 
-	if fieldSet&32 > 0 {
+	if fieldSet&16 > 0 {
 		data.Target.Epoch = defaultData.Target.Epoch
 	} else {
 		data.Target.Epoch = decodeNumber(buf[n:])
-		n += 4
+		n += 3
 	}
 
-	if fieldSet&64 > 0 {
+	if fieldSet&32 > 0 {
 		data.Target.Root = defaultData.Target.Root
 	} else {
 		data.Target.Root = common.BytesToHash(buf[n : n+32])
 		n += 32
 	}
 	return
+}
+
+func encodeAggregationBits(bits []byte) (encoded []byte) {
+	// Encoding of aggregation bits: save first byte and prefix repetition
+	encoded = append(encoded, byte(len(bits)), bits[0])
+	rep := byte(0)
+	for int(rep) < len(bits) && bits[rep] == bits[0] {
+		rep++
+	}
+	encoded = append(encoded, rep)
+	encoded = append(encoded, bits[rep:]...)
+	return
+}
+
+func rebuildAggregationBits(buf []byte) (int, []byte) {
+	bitsLen := int(buf[0])
+	firstByte := buf[1]
+	rep := int(buf[2])
+	ret := make([]byte, bitsLen)
+	for i := 0; i < rep; i++ {
+		ret[i] = firstByte
+	}
+	copy(ret[rep:], buf[3:])
+	return 3 + (bitsLen - rep), ret
 }
