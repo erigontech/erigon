7853e7c75d35d9924ebe74dc381c30dec259c089
alex.sharov <AskAlexSharov@gmail.com>
2022-12-28 16:35:16 +0700

    save

--- consensus/parlia/parlia.go
+++ consensus/parlia/parlia.go
@@ -600,7 +600,8 @@ func (p *Parlia) snapshot(chain consensus.ChainHeaderReader, number uint64, hash
 
 	// If we've generated a new checkpoint snapshot, save to disk
 	if verify && snap.Number%checkpointInterval == 0 {
-		if len(headers) > 0 {
+		fromSnap := p.snapshots != nil && number <= p.snapshots.BlocksAvailable()
+		if len(headers) > 0 || fromSnap {
 			log.Warn("write", "n", snap.Number, "len(headers)", len(headers))
 			if err = snap.store(p.db); err != nil {
 				return nil, err
