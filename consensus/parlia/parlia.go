b9e859439e659ff939d328aabf88e8732863b966
alex.sharov <AskAlexSharov@gmail.com>
2022-12-28 13:48:06 +0700

    save

--- consensus/parlia/parlia.go
+++ consensus/parlia/parlia.go
@@ -475,10 +475,6 @@ func (p *Parlia) verifySeal(chain consensus.ChainHeaderReader, header *types.Hea
 	}
 
 	if _, ok := snap.Validators[signer]; !ok {
-		log.Warn("chain config: ", "id", p.chainConfig.ChainID.Uint64())
-		for s := range snap.Validators {
-			fmt.Printf("see: %x\n", s.Bytes())
-		}
 		return fmt.Errorf("parlia.verifySeal: headerNum=%d, validator=%x, %w", header.Number.Uint64(), signer.Bytes(), errUnauthorizedValidator)
 	}
 
