1aa0a3e8b7e6ad3f2f661b96635901a4b3f21589
lightclient@protonmail.com <lightclient@protonmail.com>
2022-12-29 10:42:40 -0700

    core/vm: properly validated deployed code if eof

--- core/vm/evm.go
+++ core/vm/evm.go
@@ -398,8 +398,13 @@ func (evm *EVM) create(caller ContractRef, codeAndHash *codeAndHash, gas uint64,
 	maxCodeSizeExceeded := evm.chainRules.IsSpuriousDragon && len(ret) > params.MaxCodeSize && !evm.chainRules.IsAura
 
 	// Reject code starting with 0xEF if EIP-3541 is enabled.
-	if err == nil && !maxCodeSizeExceeded {
-		if evm.chainRules.IsLondon && len(ret) >= 1 && ret[0] == 0xEF {
+	if err == nil && !maxCodeSizeExceeded && len(ret) >= 1 && ret[0] == 0xEF {
+		if evm.chainRules.IsShanghai {
+			var c Container
+			if err = c.UnmarshalBinary(ret); err != nil {
+				err = fmt.Errorf("invalid code: %v", err)
+			}
+		} else if evm.chainRules.IsLondon {
 			err = ErrInvalidCode
 		}
 	}
