8c51bceeb63a7468cf3d0f9f90c2f20dff315021
lightclient@protonmail.com <lightclient@protonmail.com>
2023-01-02 06:40:16 -0700

    core/vm: disallow legacy initcode deploying eof bytecode

--- core/vm/evm.go
+++ core/vm/evm.go
@@ -408,6 +408,9 @@ func (evm *EVM) create(caller ContractRef, codeAndHash *codeAndHash, gas uint64,
 	if err == nil && isInitcodeEOF && !hasEOFByte(ret) {
 		err = ErrLegacyCode
 	}
+	if err == nil && !isInitcodeEOF && hasEOFByte(ret) {
+		err = ErrLegacyDeploysEOF
+	}
 
 	if err == nil && hasEOFByte(ret) {
 		if evm.chainRules.IsShanghai {
