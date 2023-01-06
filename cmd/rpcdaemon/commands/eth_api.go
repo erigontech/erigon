1d475648fc96cc1b45d9abc1fdedb7028f115d50
a <a@tuxpa.in>
2023-01-06 04:26:40 -0600

    fix interface method

--- cmd/rpcdaemon/commands/eth_api.go
+++ cmd/rpcdaemon/commands/eth_api.go
@@ -64,7 +64,7 @@ type EthAPI interface {
 	NewFilter(_ context.Context, crit ethFilters.FilterCriteria) (string, error)
 	UninstallFilter(_ context.Context, index string) (bool, error)
 	GetFilterChanges(_ context.Context, index string) ([]any, error)
-	GetFilterLogs(_ context.Context, index string) ([]any, error)
+	GetFilterLogs(_ context.Context, index string) ([]*types.Log, error)
 
 	// Account related (see ./eth_accounts.go)
 	Accounts(ctx context.Context) ([]common.Address, error)
