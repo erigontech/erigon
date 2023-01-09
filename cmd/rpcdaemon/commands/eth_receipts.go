a5d86e2b8028e4ac7c6b354fa34e7d2870750829
alex.sharov <AskAlexSharov@gmail.com>
2023-01-09 20:28:37 +0700

    save

--- cmd/rpcdaemon/commands/eth_receipts.go
+++ cmd/rpcdaemon/commands/eth_receipts.go
@@ -423,7 +423,7 @@ func (api *APIImpl) getLogsV3(ctx context.Context, tx kv.TemporalTx, begin, end 
 			continue
 		}
 		//stateReader.SetTxNum(txNum + 1)
-		//stateReader.SetTxNum(txNum - 1)
+		stateReader.SetTxNum(txNum - 1)
 		stateReader.SetTxNum(txNum)
 		txHash := txn.Hash()
 		msg, err := txn.AsMessage(*signer, header.BaseFee, rules)
