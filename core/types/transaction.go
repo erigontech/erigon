c900f74e68f970146f05725a100fc1511a1c7ab5
yperbasis <andrey.ashikhmin@gmail.com>
2023-01-10 10:35:05 +0100

    Remove unused Transaction.Size

--- core/types/transaction.go
+++ core/types/transaction.go
@@ -70,7 +70,6 @@ type Transaction interface {
 	FakeSign(address common.Address) (Transaction, error)
 	Hash() common.Hash
 	SigningHash(chainID *big.Int) common.Hash
-	Size() common.StorageSize
 	GetData() []byte
 	GetAccessList() AccessList
 	Protected() bool
