2db82a8e4f5c272fad7b8c9739abac9864e6ef4f
alex.sharov <AskAlexSharov@gmail.com>
2023-01-11 15:33:25 +0700

    Revert "save"
    This reverts commit b080f3fcdf0c90ffcd4c77a88e8e6cc38d162bfa.

--- cmd/rpcdaemon/commands/eth_receipts.go
+++ cmd/rpcdaemon/commands/eth_receipts.go
@@ -471,11 +471,13 @@ func getTopicsBitmapV3(tx kv.TemporalTx, topics [][]common.Hash, from, to uint64
 			if err != nil {
 				return nil, err
 			}
-			bm, err := it.ToBitmap()
-			if err != nil {
-				return nil, err
+			for it.HasNext() {
+				n, err := it.NextBatch()
+				if err != nil {
+					return nil, err
+				}
+				bitmapForORing.AddMany(n)
 			}
-			bitmapForORing.Or(bm)
 		}
 
 		if bitmapForORing.GetCardinality() == 0 {
@@ -499,10 +501,15 @@ func getAddrsBitmapV3(tx kv.TemporalTx, addrs []common.Address, from, to uint64)
 		if err != nil {
 			return nil, err
 		}
-		rx[idx], err = it.ToBitmap()
-		if err != nil {
-			return nil, err
+		m := roaring64.New()
+		for it.HasNext() {
+			n, err := it.NextBatch()
+			if err != nil {
+				return nil, err
+			}
+			m.AddMany(n)
 		}
+		rx[idx] = m
 	}
 	return roaring64.FastOr(rx...), nil
 }
