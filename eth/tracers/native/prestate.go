a9d43280ab11f3eab9f256d1f4c6126a9040bd9d
Devon Bear <itsdevbear@berachain.com>
2023-01-16 15:44:20 -0500

    tracer interface

--- eth/tracers/native/prestate.go
+++ eth/tracers/native/prestate.go
@@ -57,7 +57,7 @@ type accountMarshaling struct {
 
 type prestateTracer struct {
 	noopTracer
-	env       *vm.EVM
+	env       vm.VMInterface
 	pre       state
 	post      state
 	create    bool
@@ -91,7 +91,7 @@ func newPrestateTracer(ctx *tracers.Context, cfg json.RawMessage) (tracers.Trace
 }
 
 // CaptureStart implements the EVMLogger interface to initialize the tracing operation.
-func (t *prestateTracer) CaptureStart(env *vm.EVM, from libcommon.Address, to libcommon.Address, precomplile, create bool, input []byte, gas uint64, value *uint256.Int, code []byte) {
+func (t *prestateTracer) CaptureStart(env vm.VMInterface, from libcommon.Address, to libcommon.Address, precomplile, create bool, input []byte, gas uint64, value *uint256.Int, code []byte) {
 	t.env = env
 	t.create = create
 	t.to = to
