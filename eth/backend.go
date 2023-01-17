5ad4eb936a38580f100c23233149c74bc5a7df84
Alex Sharov <AskAlexSharov@gmail.com>
2023-01-17 13:20:31 +0700

    split backend.go:new to 2 funcs (#6595)
    Split backend.go:New method to 2. 1st will create db and basic
    configuration. 2nd will create instances of stagedsync, txpool, etc...
    based on this configuration.

--- eth/backend.go
+++ eth/backend.go
@@ -129,9 +129,15 @@ type Ethereum struct {
 
 	networkID uint64
 
-	lock              sync.RWMutex // Protects the variadic fields (e.g. gas price and etherbase)
-	chainConfig       *chain.Config
-	genesisHash       libcommon.Hash
+	lock         sync.RWMutex // Protects the variadic fields (e.g. gas price and etherbase)
+	chainConfig  *chain.Config
+	genesisBlock *types.Block
+	genesisHash  libcommon.Hash
+
+	ethBackendRPC      *privateapi.EthBackendServer
+	miningRPC          txpool_proto.MiningServer
+	stateChangesClient txpool2.StateChangesClient
+
 	miningSealingQuit chan struct{}
 	pendingBlocks     chan *types.Block
 	minedBlocks       chan *types.Block
@@ -142,7 +148,10 @@ type Ethereum struct {
 	sentriesClient *sentry.MultiClient
 	sentryServers  []*sentry.GrpcServer
 
-	stagedSync *stagedsync.Sync
+	stagedSync      *stagedsync.Sync
+	syncStages      []*stagedsync.Stage
+	syncUnwindOrder stagedsync.UnwindOrder
+	syncPruneOrder  stagedsync.PruneOrder
 
 	downloaderClient proto_downloader.DownloaderClient
 
@@ -162,7 +171,10 @@ type Ethereum struct {
 	forkValidator           *engineapi.ForkValidator
 	downloader              *downloader3.Downloader
 
-	agg *libstate.AggregatorV3
+	agg            *libstate.AggregatorV3
+	blockSnapshots *snapshotsync.RoSnapshots
+	blockReader    services.FullBlockReader
+	kvRPC          *remotedbserver.KvServer
 }
 
 func splitAddrIntoHostAndPort(addr string) (host string, port int, err error) {
@@ -274,6 +286,7 @@ func New(stack *node.Node, config *ethconfig.Config, logger log.Logger) (*Ethere
 		networkID:            config.NetworkID,
 		etherbase:            config.Miner.Etherbase,
 		chainConfig:          chainConfig,
+		genesisBlock:         genesis,
 		genesisHash:          genesis.Hash(),
 		waitForStageLoopStop: make(chan struct{}),
 		waitForMiningStop:    make(chan struct{}),
@@ -286,7 +299,7 @@ func New(stack *node.Node, config *ethconfig.Config, logger log.Logger) (*Ethere
 	if err != nil {
 		return nil, err
 	}
-	backend.agg = agg
+	backend.agg, backend.blockSnapshots, backend.blockReader = agg, allSnapshots, blockReader
 
 	if config.HistoryV3 {
 		backend.chainDB = temporal.New(backend.chainDB, agg, accounts.ConvertV3toV2, historyv2read.RestoreCodeHash)
@@ -295,6 +308,7 @@ func New(stack *node.Node, config *ethconfig.Config, logger log.Logger) (*Ethere
 
 	kvRPC := remotedbserver.NewKvServer(ctx, chainKv, allSnapshots, agg)
 	backend.notifications.StateChangesConsumer = kvRPC
+	backend.kvRPC = kvRPC
 
 	backend.gasPrice, _ = uint256.FromBig(config.Miner.GasPrice)
 
@@ -644,10 +658,22 @@ func New(stack *node.Node, config *ethconfig.Config, logger log.Logger) (*Ethere
 		return nil, err
 	}
 
-	backend.stagedSync, err = stages2.NewStagedSync(backend.sentryCtx, backend.chainDB, stack.Config().P2P, config, backend.sentriesClient, backend.notifications, backend.downloaderClient, allSnapshots, backend.agg, backend.forkValidator, backend.engine)
-	if err != nil {
-		return nil, err
-	}
+	backend.ethBackendRPC, backend.miningRPC, backend.stateChangesClient = ethBackendRPC, miningRPC, stateDiffClient
+
+	backend.syncStages = stages2.NewDefaultStages(backend.sentryCtx, backend.chainDB, stack.Config().P2P, config, backend.sentriesClient, backend.notifications, backend.downloaderClient, allSnapshots, backend.agg, backend.forkValidator, backend.engine)
+	backend.syncUnwindOrder = stagedsync.DefaultUnwindOrder
+	backend.syncPruneOrder = stagedsync.DefaultPruneOrder
+
+	return backend, nil
+}
+func (backend *Ethereum) Init(stack *node.Node, config *ethconfig.Config) error {
+	ethBackendRPC, miningRPC, stateDiffClient := backend.ethBackendRPC, backend.miningRPC, backend.stateChangesClient
+	blockReader := backend.blockReader
+	ctx := backend.sentryCtx
+	chainKv := backend.chainDB
+	var err error
+
+	backend.stagedSync = stagedsync.New(backend.syncStages, backend.syncUnwindOrder, backend.syncPruneOrder)
 
 	backend.sentriesClient.Hd.StartPoSDownloader(backend.sentryCtx, backend.sentriesClient.SendHeaderRequest, backend.sentriesClient.Penalize)
 
@@ -659,7 +685,7 @@ func New(stack *node.Node, config *ethconfig.Config, logger log.Logger) (*Ethere
 			badBlockHeader = header
 			return hErr
 		}); err != nil {
-			return nil, err
+			return err
 		}
 
 		if badBlockHeader != nil {
@@ -678,14 +704,14 @@ func New(stack *node.Node, config *ethconfig.Config, logger log.Logger) (*Ethere
 		var headCh chan [][]byte
 		headCh, backend.unsubscribeEthstat = backend.notifications.Events.AddHeaderSubscription()
 		if err := ethstats.New(stack, backend.sentryServers, chainKv, backend.engine, config.Ethstats, backend.networkID, ctx.Done(), headCh); err != nil {
-			return nil, err
+			return err
 		}
 	}
 	// start HTTP API
 	httpRpcCfg := stack.Config().Http
 	ethRpcClient, txPoolRpcClient, miningRpcClient, stateCache, ff, err := cli.EmbeddedServices(ctx, chainKv, httpRpcCfg.StateCache, blockReader, ethBackendRPC, backend.txPool2GrpcServer, miningRPC, stateDiffClient)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	var borDb kv.RoDB
@@ -703,7 +729,7 @@ func New(stack *node.Node, config *ethconfig.Config, logger log.Logger) (*Ethere
 
 	// Register the backend on the node
 	stack.RegisterLifecycle(backend)
-	return backend, nil
+	return nil
 }
 
 func (s *Ethereum) APIs() []rpc.API {
