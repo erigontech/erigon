ec1a803e623fba7dc1d5f356dc5785dda2410c7f
Alexey Sharp <alexeysharp@Alexeys-iMac.local>
2023-01-17 12:36:16 +0000

    Fix lint

--- turbo/stages/mock_sentry.go
+++ turbo/stages/mock_sentry.go
@@ -635,18 +635,18 @@ func (ms *MockSentry) InsertChain(chain *core.ChainPack) error {
 		return fmt.Errorf("block %d %x was invalid", chain.TopBlock.NumberU64(), chain.TopBlock.Hash())
 	}
 	//if ms.HistoryV3 {
-		//if err := ms.agg.BuildFiles(ms.Ctx, ms.DB); err != nil {
-		//	return err
-		//}
-		//if err := ms.DB.UpdateAsync(ms.Ctx, func(tx kv.RwTx) error {
-		//	ms.agg.SetTx(tx)
-		//	if err := ms.agg.Prune(ms.Ctx, math.MaxUint64); err != nil {
-		//		return err
-		//	}
-		//	return nil
-		//}); err != nil {
-		//	return err
-		//}
+	//if err := ms.agg.BuildFiles(ms.Ctx, ms.DB); err != nil {
+	//	return err
+	//}
+	//if err := ms.DB.UpdateAsync(ms.Ctx, func(tx kv.RwTx) error {
+	//	ms.agg.SetTx(tx)
+	//	if err := ms.agg.Prune(ms.Ctx, math.MaxUint64); err != nil {
+	//		return err
+	//	}
+	//	return nil
+	//}); err != nil {
+	//	return err
+	//}
 	//}
 	return nil
 }
