b5ab5aef1746bad9217e491b2117a2f7cc983306
alex.sharov <AskAlexSharov@gmail.com>
2023-01-19 10:29:34 +0700

    evm: prealloc zeroes array for resize

--- core/vm/memory.go
+++ core/vm/memory.go
@@ -61,20 +61,17 @@ func (m *Memory) Set32(offset uint64, val *uint256.Int) {
 	val.WriteToSlice(m.store[offset:])
 }
 
+// zeroes - pre-allocated zeroes for Resize()
 var zeroes = make([]byte, 4096)
 
 // Resize resizes the memory to size
 func (m *Memory) Resize(size uint64) {
 	if uint64(m.Len()) < size {
-		m.store = append(m.store, make([]byte, size-uint64(m.Len()))...)
-	}
-}
-func (m *Memory) Resize2(size uint64) {
-	if uint64(m.Len()) < size {
-		if size-uint64(m.Len()) < uint64(len(zeroes)) {
-			m.store = append(m.store, zeroes[:size-uint64(m.Len())]...)
+		l := int(size - uint64(m.Len()))
+		if l < len(zeroes) {
+			m.store = append(m.store, zeroes[:l]...)
 		} else {
-			m.store = append(m.store, make([]byte, size-uint64(m.Len()))...)
+			m.store = append(m.store, make([]byte, l)...)
 		}
 	}
 }
