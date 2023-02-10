37e5302603587c0ec31fd2086ddeb8786fe9b728
Alexey Sharp <alexeysharp@Alexeys-iMac.local>
2023-02-10 10:36:15 +0000

    CL lightclient to create memdb in the tmpdir which gets cleaned up on startup

--- eth/backend.go
+++ eth/backend.go
@@ -49,7 +49,7 @@ import (
 	"github.com/ledgerwatch/erigon-lib/kv"
 	"github.com/ledgerwatch/erigon-lib/kv/kvcache"
 	"github.com/ledgerwatch/erigon-lib/kv/kvcfg"
-	"github.com/ledgerwatch/erigon-lib/kv/memdb"
+	"github.com/ledgerwatch/erigon-lib/kv/mdbx"
 	"github.com/ledgerwatch/erigon-lib/kv/remotedbserver"
 	libstate "github.com/ledgerwatch/erigon-lib/state"
 	txpool2 "github.com/ledgerwatch/erigon-lib/txpool"
@@ -578,7 +578,7 @@ func New(stack *node.Node, config *ethconfig.Config, logger log.Logger) (*Ethere
 			return nil, err
 		}
 
-		lc, err := lightclient.NewLightClient(ctx, memdb.New(), genesisCfg, beaconCfg, ethBackendRPC, nil, client, currentBlockNumber, false)
+		lc, err := lightclient.NewLightClient(ctx, mdbx.NewMDBX(log.New()).InMem(tmpdir).MustOpen(), genesisCfg, beaconCfg, ethBackendRPC, nil, client, currentBlockNumber, false)
 		if err != nil {
 			return nil, err
 		}
