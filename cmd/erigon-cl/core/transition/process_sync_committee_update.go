775318285126dfad0326bb37ccdd397ee9c1e027
Giulio Rebuffo <giulio.rebuffo@gmail.com>
2023-02-10 17:02:47 +0100

    added processing of sync committee

--- cmd/erigon-cl/core/transition/process_sync_committee_update.go
+++ cmd/erigon-cl/core/transition/process_sync_committee_update.go
@@ -4,17 +4,12 @@ import (
 	"encoding/binary"
 	"math"
 
+	"github.com/Giulio2002/bls"
 	"github.com/ledgerwatch/erigon/cl/cltypes"
 	"github.com/ledgerwatch/erigon/cl/utils"
 )
 
-/*
-def process_sync_committee_updates(state: BeaconState) -> None:
-    next_epoch = get_current_epoch(state) + Epoch(1)
-    if next_epoch % EPOCHS_PER_SYNC_COMMITTEE_PERIOD == 0:
-        state.current_sync_committee = state.next_sync_committee
-        state.next_sync_committee = get_next_sync_committee(state)
-*/
+// ProcessSyncCommitteeUpdate implements processing for the sync committee update. unfortunately there is no easy way to test it.
 func (s *StateTransistor) ProcessSyncCommitteeUpdate() error {
 	if (s.state.Epoch()+1)%s.beaconConfig.EpochsPerSyncCommitteePeriod != 0 {
 		return nil
@@ -61,32 +56,20 @@ func (s *StateTransistor) computeNextSyncCommittee() (*cltypes.SyncCommittee, er
 		}
 		i++
 	}
-	// TODO(Giulio2002): Aggregate public keys.
+	// Format public keys.
+	formattedKeys := make([][]byte, cltypes.SyncCommitteeSize)
+	for i := range formattedKeys {
+		formattedKeys[i] = make([]byte, 48)
+		copy(formattedKeys[i], syncCommitteePubKeys[i][:])
+	}
+	aggregatePublicKeyBytes, err := bls.AggregatePublickKeys(formattedKeys)
+	if err != nil {
+		return nil, err
+	}
+	var aggregate [48]byte
+	copy(aggregate[:], aggregatePublicKeyBytes)
 	return &cltypes.SyncCommittee{
-		PubKeys: syncCommitteePubKeys,
+		PubKeys:            syncCommitteePubKeys,
+		AggregatePublicKey: aggregate,
 	}, nil
 }
-
-/*
-def get_next_sync_committee_indices(state: BeaconState) -> Sequence[ValidatorIndex]:
-    """
-    Return the sync committee indices, with possible duplicates, for the next sync committee.
-    """
-    epoch = Epoch(get_current_epoch(state) + 1)
-
-    MAX_RANDOM_BYTE = 2**8 - 1
-    active_validator_indices = get_active_validator_indices(state, epoch)
-    active_validator_count = uint64(len(active_validator_indices))
-    seed = get_seed(state, epoch, DOMAIN_SYNC_COMMITTEE)
-    i = 0
-    sync_committee_indices: List[ValidatorIndex] = []
-    while len(sync_committee_indices) < SYNC_COMMITTEE_SIZE:
-        shuffled_index = compute_shuffled_index(uint64(i % active_validator_count), active_validator_count, seed)
-        candidate_index = active_validator_indices[shuffled_index]
-        random_byte = hash(seed + uint_to_bytes(uint64(i // 32)))[i % 32]
-        effective_balance = state.validators[candidate_index].effective_balance
-        if effective_balance * MAX_RANDOM_BYTE >= MAX_EFFECTIVE_BALANCE * random_byte:
-            sync_committee_indices.append(candidate_index)
-        i += 1
-    return sync_committee_indices
-*/
