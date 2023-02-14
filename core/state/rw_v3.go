dbc3bb178f32074ec7b49c17210eea380675d631
alex.sharov <AskAlexSharov@gmail.com>
2023-02-14 14:51:03 +0700

    fix lint

--- core/state/rw_v3.go
+++ core/state/rw_v3.go
@@ -563,7 +563,9 @@ func (rs *StateV3) ReadsValid(readLists map[string]*exec22.KvList) bool {
 			continue
 		}
 		for i, key := range list.Keys {
-			if val, ok := t.Get(*(*string)(unsafe.Pointer(&key))); ok {
+			key := key
+			keyS := *(*string)(unsafe.Pointer(&key))
+			if val, ok := t.Get(keyS); ok {
 				if table == CodeSizeTable {
 					if binary.BigEndian.Uint64(list.Vals[i]) != uint64(len(val)) {
 						return false
