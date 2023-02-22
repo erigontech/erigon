b56c2cad3bd1e95644a0bf632c2abc975ee3065a
Alexey Sharp <alexeysharp@Alexeys-iMac.local>
2023-02-22 11:02:22 +0000

    More db access

--- diagnostics/db_access.go
+++ diagnostics/db_access.go
@@ -2,6 +2,7 @@ package diagnostics
 
 import (
 	"context"
+	"encoding/hex"
 	"fmt"
 	"io"
 	"net/http"
@@ -76,5 +77,63 @@ func writeDbTables(w io.Writer, r *http.Request, dataDir string) {
 }
 
 func writeDbRead(w io.Writer, r *http.Request, dataDir string) {
-
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ERROR: parsing arguments: %v\n", err)
+		return
+	}
+	path := r.Form.Get("path")
+	if path == "" {
+		fmt.Fprintf(w, "ERROR: path argument is required - specify the relative path to an MDBX database directory")
+		return
+	}
+	m := mdbx.PathDbMap()
+	db, ok := m[path]
+	if !ok {
+		fmt.Fprintf(w, "ERROR: path %s is not in the list of allowed paths", path)
+		return
+	}
+	table := r.Form.Get("table")
+	if table == "" {
+		fmt.Fprintf(w, "ERROR: table argument is required - specify the table to read from")
+		return
+	}
+	var key []byte
+	var err error
+	keyHex := r.Form.Get("key")
+	if keyHex != "" {
+		if key, err = hex.DecodeString(keyHex); err != nil {
+			fmt.Fprintf(w, "ERROR: key [%s] argument may only contain hexadecimal digits: %v\n", keyHex, err)
+			return
+		}
+	}
+	var results []string
+	if err := db.View(context.Background(), func(tx kv.Tx) error {
+		c, e := tx.Cursor(table)
+		if e != nil {
+			return e
+		}
+		defer c.Close()
+		var k, v []byte
+		if key == nil {
+			if k, v, e = c.First(); err != nil {
+				return e
+			}
+		} else if k, v, e = c.Seek(key); e != nil {
+			return e
+		}
+		count := 0
+		for e == nil && k != nil && count < 256 {
+			results = append(results, fmt.Sprintf("%x | %x", k, v))
+			count++
+			k, v, e = c.Next()
+		}
+		return nil
+	}); err != nil {
+		fmt.Fprintf(w, "ERROR: reading table %s in %s: %v\n", table, path, err)
+		return
+	}
+	fmt.Fprintf(w, "SUCCESS\n")
+	for _, result := range results {
+		fmt.Fprintf(w, "%s\n", result)
+	}
 }
