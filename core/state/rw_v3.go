d3ced6f6ef0d461cf2d9946250a01cf0c8b2e043
alex.sharov <AskAlexSharov@gmail.com>
2023-02-27 10:30:57 +0700

    save

--- core/state/rw_v3.go
+++ core/state/rw_v3.go
@@ -343,6 +343,7 @@ func (rs *StateV3) appplyState(roTx kv.Tx, txTask *exec22.TxTask, agg *libstate.
 	defer rs.lock.Unlock()
 
 	for addr, increase := range txTask.BalanceIncreaseSet {
+		increase := increase
 		addrBytes := addr.Bytes()
 		enc0 := rs.get(kv.PlainState, addrBytes)
 		if enc0 == nil {
