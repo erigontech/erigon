538370639b0c47c6ddeea53b62011f47988b252e
alex.sharov <AskAlexSharov@gmail.com>
2023-02-28 13:16:40 +0700

    save

--- cmd/state/exec3/calltracer_v3.go
+++ cmd/state/exec3/calltracer_v3.go
@@ -24,24 +24,30 @@ func (ct *CallTracer) Reset() {
 	ct.tos = nil
 }
 func (ct *CallTracer) Froms() map[libcommon.Address]struct{} {
-	if ct.froms == nil {
-		ct.froms = map[libcommon.Address]struct{}{}
-	}
 	return ct.froms
 }
 func (ct *CallTracer) Tos() map[libcommon.Address]struct{} {
-	if ct.tos == nil {
-		ct.tos = map[libcommon.Address]struct{}{}
-	}
 	return ct.tos
 }
 
 func (ct *CallTracer) CaptureTxStart(gasLimit uint64) {}
 func (ct *CallTracer) CaptureTxEnd(restGas uint64)    {}
 func (ct *CallTracer) CaptureStart(env vm.VMInterface, from libcommon.Address, to libcommon.Address, precompile bool, create bool, input []byte, gas uint64, value *uint256.Int, code []byte) {
+	if ct.froms == nil {
+		ct.froms = map[libcommon.Address]struct{}{}
+	}
+	if ct.tos == nil {
+		ct.tos = map[libcommon.Address]struct{}{}
+	}
 	ct.froms[from], ct.tos[to] = struct{}{}, struct{}{}
 }
 func (ct *CallTracer) CaptureEnter(typ vm.OpCode, from libcommon.Address, to libcommon.Address, precompile bool, create bool, input []byte, gas uint64, value *uint256.Int, code []byte) {
+	if ct.froms == nil {
+		ct.froms = map[libcommon.Address]struct{}{}
+	}
+	if ct.tos == nil {
+		ct.tos = map[libcommon.Address]struct{}{}
+	}
 	ct.froms[from], ct.tos[to] = struct{}{}, struct{}{}
 }
 func (ct *CallTracer) CaptureState(pc uint64, op vm.OpCode, gas, cost uint64, scope *vm.ScopeContext, rData []byte, depth int, err error) {
