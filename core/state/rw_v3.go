d95dddbab50e0762ed1636b1fe90d4d8de96abe2
alex.sharov <AskAlexSharov@gmail.com>
2023-03-03 09:49:51 +0700

    save

--- core/state/rw_v3.go
+++ core/state/rw_v3.go
@@ -81,21 +81,19 @@ func (rs *StateV3) put(table string, key, val []byte) {
 
 func (rs *StateV3) puts(table string, key string, val []byte) {
 	switch table {
-	case kv.PlainState:
-		if len(key) == 20 {
-			if old, ok := rs.chAccs[key]; ok {
-				rs.sizeEstimate += len(val) - len(old)
-			} else {
-				rs.sizeEstimate += len(key) + len(val)
-			}
-			rs.chAccs[key] = val
+	case StorageTable:
+		if old, ok := rs.chStorage.Set(key, val); ok {
+			rs.sizeEstimate += len(val) - len(old)
 		} else {
-			if old, ok := rs.chStorage.Set(key, val); ok {
-				rs.sizeEstimate += len(val) - len(old)
-			} else {
-				rs.sizeEstimate += len(key) + len(val)
-			}
+			rs.sizeEstimate += len(key) + len(val)
 		}
+	case kv.PlainState:
+		if old, ok := rs.chAccs[key]; ok {
+			rs.sizeEstimate += len(val) - len(old)
+		} else {
+			rs.sizeEstimate += len(key) + len(val)
+		}
+		rs.chAccs[key] = val
 	case kv.Code:
 		if old, ok := rs.chCode[key]; ok {
 			rs.sizeEstimate += len(val) - len(old)
@@ -132,12 +130,10 @@ func (rs *StateV3) Get(table string, key []byte) []byte {
 func (rs *StateV3) get(table string, key []byte) (v []byte) {
 	keyS := *(*string)(unsafe.Pointer(&key))
 	switch table {
+	case StorageTable:
+		v, _ = rs.chStorage.Get(keyS)
 	case kv.PlainState:
-		if len(key) == 20 {
-			v = rs.chAccs[keyS]
-		} else {
-			v, _ = rs.chStorage.Get(keyS)
-		}
+		v = rs.chAccs[keyS]
 	case kv.Code:
 		v = rs.chCode[keyS]
 	case kv.IncarnationMap:
@@ -815,8 +811,8 @@ func (w *StateWriterV3) WriteAccountStorage(address common.Address, incarnation 
 	}
 	composite := dbutils.PlainGenerateCompositeStorageKey(address[:], incarnation, key.Bytes())
 	cmpositeS := string(composite)
-	w.writeLists[kv.PlainState].Keys = append(w.writeLists[kv.PlainState].Keys, cmpositeS)
-	w.writeLists[kv.PlainState].Vals = append(w.writeLists[kv.PlainState].Vals, value.Bytes())
+	w.writeLists[StorageTable].Keys = append(w.writeLists[StorageTable].Keys, cmpositeS)
+	w.writeLists[StorageTable].Vals = append(w.writeLists[StorageTable].Vals, value.Bytes())
 	//fmt.Printf("storage [%x] [%x] => [%x], txNum: %d\n", address, *key, v, w.txNum)
 	if w.storagePrevs == nil {
 		w.storagePrevs = map[string][]byte{}
@@ -884,7 +880,7 @@ func (r *StateReaderV3) ReadAccountData(address common.Address) (*accounts.Accou
 
 func (r *StateReaderV3) ReadAccountStorage(address common.Address, incarnation uint64, key *common.Hash) ([]byte, error) {
 	composite := dbutils.PlainGenerateCompositeStorageKey(address.Bytes(), incarnation, key.Bytes())
-	enc := r.rs.Get(kv.PlainState, composite)
+	enc := r.rs.Get(StorageTable, composite)
 	if enc == nil {
 		var err error
 		enc, err = r.tx.GetOne(kv.PlainState, composite)
@@ -976,6 +972,7 @@ var writeListPool = sync.Pool{
 	New: func() any {
 		return map[string]*exec22.KvList{
 			kv.PlainState:        {},
+			StorageTable:         {},
 			kv.Code:              {},
 			kv.PlainContractCode: {},
 			kv.IncarnationMap:    {},
