3abadc48e8cf307863afd37609483a744576d52b
alex.sharov <AskAlexSharov@gmail.com>
2023-03-07 15:24:53 +0700

    save

--- eth/stagedsync/exec3.go
+++ eth/stagedsync/exec3.go
@@ -211,6 +211,8 @@ func ExecV3(ctx context.Context,
 	applyLoopWg := sync.WaitGroup{} // to wait for finishing of applyLoop after applyCtx cancel
 	defer applyLoopWg.Wait()
 
+	stepsInDBAfterCommit := rawdbhelpers.IdxStepsCountV3(applyTx)
+
 	applyLoopInner := func(ctx context.Context) error {
 		tx, err := chainDb.BeginRo(ctx)
 		if err != nil {
@@ -330,8 +332,7 @@ func ExecV3(ctx context.Context,
 						// too much steps in db will slow-down everything: flush and prune
 						// it means better spend time for pruning, before flushing more data to db
 						// also better do it now - instead of before Commit() - because Commit does block execution
-						stepsInDB := rawdbhelpers.IdxStepsCountV3(tx)
-						if stepsInDB > 10 && rs.SizeEstimate() < uint64(float64(commitThreshold)*0.2) {
+						if stepsInDBAfterCommit > 10 && rs.SizeEstimate() < uint64(float64(commitThreshold)*0.2) {
 							if err = agg.Prune(ctx, ethconfig.HistoryV3AggregationStep/2); err != nil { // prune part of retired data, before commit
 								panic(err)
 							}
@@ -447,6 +448,7 @@ func ExecV3(ctx context.Context,
 					}
 					defer tx.Rollback()
 					agg.SetTx(tx)
+					stepsInDBAfterCommit = rawdbhelpers.IdxStepsCountV3(tx)
 
 					applyCtx, cancelApplyCtx = context.WithCancel(ctx)
 					defer cancelApplyCtx()
