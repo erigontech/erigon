279f0e6f7c700c3da4a4cb493d6d0d09cc83c730
TY <ty@sentio.xyz>
2023-03-14 16:22:58 +0800

    chore(ci): use docker buildx

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -xe
 IMAGE_TAG="${IMAGE_TAG:-dev}"
-DOCKER_BUILDKIT=1 docker build -t us-west1-docker.pkg.dev/sentio-352722/sentio/erigon:$IMAGE_TAG . && docker push us-west1-docker.pkg.dev/sentio-352722/sentio/erigon:$IMAGE_TAG
+docker buildx build -t us-west1-docker.pkg.dev/sentio-352722/sentio/erigon:$IMAGE_TAG . && docker push us-west1-docker.pkg.dev/sentio-352722/sentio/erigon:$IMAGE_TAG
