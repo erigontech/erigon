d029d2ef9e1a1db77fa9450779377841aba12927
alex.sharov <AskAlexSharov@gmail.com>
2023-03-22 10:42:32 +0700

    e3: test for DomainGC

--- state/gc_test.go
+++ state/gc_test.go
@@ -94,3 +94,79 @@ func TestGCReadAfterRemoveFile(t *testing.T) {
 		test(t, h, db, txs)
 	})
 }
+
+func TestDomainGCReadAfterRemoveFile(t *testing.T) {
+	logEvery := time.NewTicker(30 * time.Second)
+	defer logEvery.Stop()
+	ctx := context.Background()
+
+	test := func(t *testing.T, h *Domain, db kv.RwDB, txs uint64) {
+		t.Helper()
+		require := require.New(t)
+		collateAndMerge(t, db, nil, h, txs)
+
+		t.Run("read after: remove when have reader", func(t *testing.T) {
+			tx, err := db.BeginRo(ctx)
+			require.NoError(err)
+			defer tx.Rollback()
+
+			// - create immutable view
+			// - del cold file
+			// - read from canDelete file
+			// - close view
+			// - open new view
+			// - make sure there is no canDelete file
+			hc := h.MakeContext()
+			_ = hc
+			lastOnFs, _ := h.files.Max()
+			require.False(lastOnFs.frozen) // prepared dataset must have some non-frozen files. or it's bad dataset.
+			h.integrateMergedFiles([]*filesItem{lastOnFs}, nil, nil, nil, nil, nil)
+			require.NotNil(lastOnFs.decompressor)
+
+			lastInView := hc.files[len(hc.files)-1]
+			g := lastInView.src.decompressor.MakeGetter()
+			require.Equal(lastInView.startTxNum, lastOnFs.startTxNum)
+			require.Equal(lastInView.endTxNum, lastOnFs.endTxNum)
+			if g.HasNext() {
+				k, _ := g.Next(nil)
+				require.Equal(8, len(k))
+				v, _ := g.Next(nil)
+				require.Equal(8, len(v))
+			}
+
+			require.NotNil(lastOnFs.decompressor)
+			hc.Close()
+			require.Nil(lastOnFs.decompressor)
+
+			nonDeletedOnFs, _ := h.files.Max()
+			require.False(nonDeletedOnFs.frozen)
+			require.NotNil(nonDeletedOnFs.decompressor) // non-canDelete files are not closed
+
+			hc = h.MakeContext()
+			newLastInView := hc.files[len(hc.files)-1]
+			require.False(lastOnFs.frozen)
+			require.False(lastInView.startTxNum == newLastInView.startTxNum && lastInView.endTxNum == newLastInView.endTxNum)
+
+			hc.Close()
+		})
+
+		t.Run("read after: remove when no readers", func(t *testing.T) {
+			tx, err := db.BeginRo(ctx)
+			require.NoError(err)
+			defer tx.Rollback()
+
+			// - del cold file
+			// - new reader must not see canDelete file
+			hc := h.MakeContext()
+			lastOnFs, _ := h.files.Max()
+			require.False(lastOnFs.frozen) // prepared dataset must have some non-frozen files. or it's bad dataset.
+			h.integrateMergedFiles(nil, []*filesItem{lastOnFs}, nil, nil, nil, nil)
+
+			require.NotNil(lastOnFs.decompressor)
+			hc.Close()
+			require.Nil(lastOnFs.decompressor)
+		})
+	}
+	_, db, d, txs := filledDomain(t)
+	test(t, d, db, txs)
+}
