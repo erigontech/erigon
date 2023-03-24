f2e0f1ead44437ee2c191845b203c346a2d017f7
alex.sharov <AskAlexSharov@gmail.com>
2023-03-24 10:58:33 +0700

    clean

--- state/domain.go
+++ state/domain.go
@@ -1029,7 +1029,7 @@ func (d *Domain) collate(ctx context.Context, step, txFrom, txTo uint64, roTx kv
 				"progress", fmt.Sprintf("%.2f%%", float64(pos)/float64(totalKeys)*100))
 		case <-ctx.Done():
 			log.Warn("[snapshots] collate domain cancelled", "name", d.filenameBase, "err", ctx.Err())
-			return Collation{}, err
+			return Collation{}, ctx.Err()
 		default:
 		}
 
