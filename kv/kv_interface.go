daa2d67790b10b1c58fd2d9689edda08a3da36fe
alex.sharov <AskAlexSharov@gmail.com>
2023-03-26 12:18:29 +0700

    clean

--- kv/kv_interface.go
+++ kv/kv_interface.go
@@ -26,11 +26,11 @@ import (
 )
 
 //Variables Naming:
-//  ts - TimeStap. Ususally it's Etherum's TransactionNumber (auto-increment ID). Or BlockNumber.
 //  tx - Database Transaction
 //  txn - Ethereum Transaction (and TxNum - is also number of Etherum Transaction)
 //  RoTx - Read-Only Database Transaction. RwTx - read-write
 //  k, v - key, value
+//  ts - TimeStamp. Ususally it's Etherum's TransactionNumber (auto-increment ID). Or BlockNumber.
 //  Cursor - low-level mdbx-tide api to navigate over Table
 //  Iter - high-level iterator-like api over Table/InvertedIndex/History/Domain. Has less features than Cursor. See package `iter`
 
