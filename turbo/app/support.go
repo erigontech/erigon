a98f1b8a8cfa0fb6e3040002ec1ad901957d0324
Alexey Sharp <alexeysharp@Alexeys-iMac.local>
2023-03-29 19:36:27 +0100

    Remove unused code

--- turbo/app/support.go
+++ turbo/app/support.go
@@ -60,35 +60,6 @@ type Conn struct {
 	rLock sync.Mutex
 }
 
-func newConn(ctx context.Context, r io.Reader, wc io.WriteCloser) (*Conn, context.Context) {
-	ctx, cancel := context.WithCancel(ctx)
-	return &Conn{
-		r:      r,
-		wc:     wc,
-		cancel: cancel,
-	}, ctx
-}
-
-// Write writes data to the connection
-func (c *Conn) Write(data []byte) (int, error) {
-	c.wLock.Lock()
-	defer c.wLock.Unlock()
-	return c.wc.Write(data)
-}
-
-// Read reads data from the connection
-func (c *Conn) Read(data []byte) (int, error) {
-	c.rLock.Lock()
-	defer c.rLock.Unlock()
-	return c.r.Read(data)
-}
-
-// Close closes the connection
-func (c *Conn) Close() error {
-	c.cancel()
-	return c.wc.Close()
-}
-
 func connectDiagnostics(cliCtx *cli.Context) error {
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
