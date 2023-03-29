afe0e385d2db940a37d08ea82f557930ad6006ef
alex.sharov <AskAlexSharov@gmail.com>
2023-03-29 11:51:05 +0700

    e4: add flag in tests

--- eth/stagedsync/stage_execute_test.go
+++ eth/stagedsync/stage_execute_test.go
@@ -39,7 +39,7 @@ func TestExec(t *testing.T) {
 		err = UnwindExecutionStage(u, s, tx2, ctx, cfg, false)
 		require.NoError(err)
 
-		compareCurrentState(t, tx1, tx2, kv.PlainState, kv.PlainContractCode, kv.ContractTEVMCode)
+		compareCurrentState(t, newAgg(t), tx1, tx2, kv.PlainState, kv.PlainContractCode, kv.ContractTEVMCode)
 	})
 	t.Run("UnwindExecutionStagePlainWithIncarnationChanges", func(t *testing.T) {
 		require, tx1, tx2 := require.New(t), memdb.BeginRw(t, db1), memdb.BeginRw(t, db2)
@@ -55,7 +55,7 @@ func TestExec(t *testing.T) {
 		err = UnwindExecutionStage(u, s, tx2, ctx, cfg, false)
 		require.NoError(err)
 
-		compareCurrentState(t, tx1, tx2, kv.PlainState, kv.PlainContractCode)
+		compareCurrentState(t, newAgg(t), tx1, tx2, kv.PlainState, kv.PlainContractCode)
 	})
 	t.Run("UnwindExecutionStagePlainWithCodeChanges", func(t *testing.T) {
 		t.Skip("not supported yet, to be restored")
@@ -73,7 +73,7 @@ func TestExec(t *testing.T) {
 		err = UnwindExecutionStage(u, s, tx2, ctx, cfg, false)
 		require.NoError(err)
 
-		compareCurrentState(t, tx1, tx2, kv.PlainState, kv.PlainContractCode)
+		compareCurrentState(t, newAgg(t), tx1, tx2, kv.PlainState, kv.PlainContractCode)
 	})
 
 	t.Run("PruneExecution", func(t *testing.T) {
@@ -198,7 +198,7 @@ func TestExec22(t *testing.T) {
 		err = UnwindExecutionStage(u, s, tx2, ctx, cfg, false)
 		require.NoError(err)
 
-		compareCurrentState(t, tx1, tx2, kv.PlainState, kv.PlainContractCode)
+		compareCurrentState(t, agg, tx1, tx2, kv.PlainState, kv.PlainContractCode)
 	})
 	t.Run("UnwindExecutionStagePlainWithIncarnationChanges", func(t *testing.T) {
 		t.Skip("we don't delete newer incarnations - seems it's a feature?")
@@ -235,6 +235,6 @@ func TestExec22(t *testing.T) {
 			return nil
 		})
 
-		compareCurrentState(t, tx1, tx2, kv.PlainState, kv.PlainContractCode)
+		compareCurrentState(t, newAgg(t), tx1, tx2, kv.PlainState, kv.PlainContractCode)
 	})
 }
