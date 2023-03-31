04b5c0c67b8a0d3a6830ef7c1cb4aabcda172454
Alex Sharov <AskAlexSharov@gmail.com>
2023-03-31 09:19:56 +0700

    e4: small tests preparations (#7220)

--- core/state/temporal/kv_temporal.go
+++ core/state/temporal/kv_temporal.go
@@ -71,7 +71,7 @@ func New(db kv.RwDB, agg *state.AggregatorV3, cb1 tConvertV3toV2, cb2 tRestoreCo
 
 	return &DB{RwDB: db, agg: agg, convertV3toV2: cb1, restoreCodeHash: cb2, parseInc: cb3, systemContractLookup: systemContractLookup}, nil
 }
-func (db *DB) GetAgg() *state.AggregatorV3 { return db.agg }
+func (db *DB) Agg() *state.AggregatorV3 { return db.agg }
 
 func (db *DB) BeginTemporalRo(ctx context.Context) (kv.TemporalTx, error) {
 	kvTx, err := db.RwDB.BeginRo(ctx)
@@ -162,6 +162,8 @@ type Tx struct {
 	resourcesToClose []kv.Closer
 }
 
+func (tx *Tx) AggCtx() *state.AggregatorV3Context { return tx.agg }
+func (tx *Tx) Agg() *state.AggregatorV3           { return tx.db.agg }
 func (tx *Tx) Rollback() {
 	for _, closer := range tx.resourcesToClose {
 		closer.Close()
