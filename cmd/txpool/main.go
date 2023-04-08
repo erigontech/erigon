e56793d5ea1cdbdf98c57b2eed53850880d99a7c
Russel Waters <6511720+argakiig@users.noreply.github.com>
2023-04-07 23:52:04 -0700

    txpool: update cli flags (#7282)
    it would appear that an extra `e` was added somewhere along the way to
    the cli flags for txpool. If this was intentional I was curious as to
    why, otherwise it seems like fixing the flag would remedy some
    confusion. Thank you
    
    Upon further investigation it would appear
    https://github.com/ledgerwatch/erigon/blob/devel/cmd/utils/flags.go#L173
    shows corrected spelling.

--- cmd/txpool/main.go
+++ cmd/txpool/main.go
@@ -70,7 +70,7 @@ func init() {
 	rootCmd.PersistentFlags().StringVar(&TLSCACert, "tls.cacert", "", "CA certificate for client side TLS handshake")
 
 	rootCmd.PersistentFlags().IntVar(&pendingPoolLimit, "txpool.globalslots", txpoolcfg.DefaultConfig.PendingSubPoolLimit, "Maximum number of executable transaction slots for all accounts")
-	rootCmd.PersistentFlags().IntVar(&baseFeePoolLimit, "txpool.globalbasefeeeslots", txpoolcfg.DefaultConfig.BaseFeeSubPoolLimit, "Maximum number of non-executable transactions where only not enough baseFee")
+	rootCmd.PersistentFlags().IntVar(&baseFeePoolLimit, "txpool.globalbasefeeslots", txpoolcfg.DefaultConfig.BaseFeeSubPoolLimit, "Maximum number of non-executable transactions where only not enough baseFee")
 	rootCmd.PersistentFlags().IntVar(&queuedPoolLimit, "txpool.globalqueue", txpoolcfg.DefaultConfig.QueuedSubPoolLimit, "Maximum number of non-executable transaction slots for all accounts")
 	rootCmd.PersistentFlags().Uint64Var(&priceLimit, "txpool.pricelimit", txpoolcfg.DefaultConfig.MinFeeCap, "Minimum gas price (fee cap) limit to enforce for acceptance into the pool")
 	rootCmd.PersistentFlags().Uint64Var(&accountSlots, "txpool.accountslots", txpoolcfg.DefaultConfig.AccountSlots, "Minimum number of executable transaction slots guaranteed per account")
