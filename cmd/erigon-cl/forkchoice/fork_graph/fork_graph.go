8d8d3f125d4d9391401b82e8c124eb1aa7ffa025
Giulio Rebuffo <giulio.rebuffo@gmail.com>
2023-04-09 02:47:58 +0200

    oops

--- cmd/erigon-cl/forkchoice/fork_graph/fork_graph.go
+++ cmd/erigon-cl/forkchoice/fork_graph/fork_graph.go
@@ -23,7 +23,7 @@ const (
 	PreValidated   ChainSegmentInsertionResult = 5
 )
 
-const maxGraphExtension = 48
+const maxGraphExtension = 128
 
 /*
 * The state store process is related to graph theory in the sense that the Ethereum blockchain can be thought of as a directed graph,
