f31d2b097d992e1a9d9305d5f44be63aa3e8d65d
racytech <82003208+racytech@users.noreply.github.com>
2023-04-30 13:03:15 +0600

    eip-4844: Unwrap() additional tx method (#7410)
    Transaction extension to support BlobTxWrapper (or network
    representation of BlobTx). BlobTxWrapper wraps BlobTx with additional
    data: blobs, commitments and proofs. Unwrap() returns unwrapped tx if
    its network tx, otherwise returns itself.

--- core/types/transaction.go
+++ core/types/transaction.go
@@ -93,6 +93,7 @@ type Transaction interface {
 	GetSender() (libcommon.Address, bool)
 	SetSender(libcommon.Address)
 	IsContractDeploy() bool
+	Unwrap() Transaction // If this is a network wrapper, returns the unwrapped tx. Otherwiwes returns itself.
 }
 
 // TransactionMisc is collection of miscelaneous fields for transaction that is supposed to be embedded into concrete
