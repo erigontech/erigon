9c57de5f09d4ef0b427ba9457b8e2b0364a62ce1
alex.sharov <AskAlexSharov@gmail.com>
2023-05-01 15:12:07 +0700

    save

--- state/aggregator_v3.go
+++ state/aggregator_v3.go
@@ -1027,7 +1027,6 @@ type flusher interface {
 }
 
 func (a *AggregatorV3) Flush(ctx context.Context, tx kv.RwTx) error {
-	fmt.Printf("a.Flush: %d\n", a.blockNum.Load())
 	a.walLock.Lock()
 	flushers := []flusher{
 		a.accounts.Rotate(),
