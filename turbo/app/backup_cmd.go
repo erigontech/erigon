6cfd2cec615ce0e7649be472de97e3c8cf2ca2b4
alex.sharov <AskAlexSharov@gmail.com>
2023-05-01 09:27:35 +0700

    erigon backup: docs

--- turbo/app/backup_cmd.go
+++ turbo/app/backup_cmd.go
@@ -21,11 +21,11 @@ import (
 // nolint
 var backupCommand = cli.Command{
 	Name: "alpha_backup",
-	Description: `Alpha verison of command. Backup all databases of Erigon. 
-Can do backup without stopping Erigon.
+	Description: `Alpha verison of command. Backup all databases without stopping of Erigon.
+While this command has Alpha prefix - we recommend to stop Erigon for backup. 
 Limitations: 
+- no support of datadir/snapshots folder. Recommendation: backup snapshots dir manually AFTER databases backup. Possible to implement in future.
 - no support of Consensus DB (copy it manually if you need). Possible to implement in future.
-- no support of datadir/snapshots folder. Possible to implement in future. Can copy it manually or rsync or symlink/mount.
 - way to pipe output to compressor (lz4/zstd). Can compress target floder later or use zfs-with-enabled-compression.
 - jwt tocken: copy it manually - if need. 
 - no support of SentryDB (datadir/nodes folder). Because seems no much reason to backup it.
