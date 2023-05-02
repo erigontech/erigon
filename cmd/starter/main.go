0334ecf1304ae5b7b2fae638b9543b860c102036
Daniil <daniil.melnik@chainstack.com>
2023-05-02 15:10:05 +0200

    exitChan + logging

--- cmd/starter/main.go
+++ cmd/starter/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"os/exec"
 	"os/signal"
+	"sort"
 	"strconv"
 	"strings"
 	"sync"
@@ -97,17 +98,19 @@ func main() {
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 	restartChan := make(chan struct{}, 2)
+	exitChan := make(chan struct{}, 2)
 	var cmd *exec.Cmd
 	var stdoutPipe, stderrPipe io.ReadCloser
 	var wg sync.WaitGroup
+	restartInit := false
 
-	scanAndTrackErrors := func(scanner *bufio.Scanner, trackers []ErrorTracker, lasrwriter io.Writer, restartChan chan struct{}) {
-		restartInit := false
+	scanAndTrackErrors := func(scanner *bufio.Scanner, trackers []ErrorTracker, lastwriter io.Writer, restartChan, exitChan chan struct{}) {
+
 		for scanner.Scan() {
 
 			line := scanner.Text()
 
-			fmt.Fprintln(lasrwriter, line)
+			fmt.Fprintln(lastwriter, line)
 
 			if restartInit {
 				continue
@@ -116,15 +119,32 @@ func main() {
 			for i := range trackers {
 				if strings.Contains(line, trackers[i].Substring) {
 					trackers[i].AddError(time.Now())
-					log("Find error substring %q, %d errors in %s", trackers[i].Substring, len(trackers[i].Errors), trackers[i].Window)
 
-					if trackers[i].ShouldRestartProcess() {
+					if len(trackers[i].Errors) == 1 {
+						log("Find error substring %q", trackers[i].Substring)
+					} else if len(trackers[i].Errors) < trackers[i].Limit {
+						log("Find error substring %q, %d errors in %s", trackers[i].Substring, len(trackers[i].Errors), trackers[i].Errors[len(trackers[i].Errors)-1].Sub(trackers[i].Errors[0]).Round(time.Second))
+					} else {
+						// find the number of errors in the last window (last is the most recent)
+						n := sort.Search(len(trackers[i].Errors), func(j int) bool {
+							return trackers[i].Errors[len(trackers[i].Errors)-1].Sub(trackers[i].Errors[j]) <= trackers[i].Window
+						})
+						log("Find error substring %q, %d errors in %s (%d errors in last %s)", trackers[i].Substring, len(trackers[i].Errors),
+							trackers[i].Errors[len(trackers[i].Errors)-1].Sub(trackers[i].Errors[0]).Round(time.Second),
+							len(trackers[i].Errors)-n, trackers[i].Window.Round(time.Second),
+						)
+					}
+
+					if trackers[i].ShouldRestartProcess() && !restartInit {
 						log("Restarting process due to error limit for substring %q", trackers[i].Substring)
-						restartChan <- struct{}{}
 						restartInit = true
+						restartChan <- struct{}{}
 					}
 				}
 			}
+		}
+		if !restartInit {
+			exitChan <- struct{}{}
 		}
 	}
 
@@ -132,20 +152,24 @@ func main() {
 		cmd = exec.Command(cmdName, cmdArgs...)
 		stdoutPipe, _ = cmd.StdoutPipe()
 		stderrPipe, _ = cmd.StderrPipe()
+		restartInit = false
 
 		wg.Add(2)
 		go func() {
 			defer wg.Done()
 			scanner := bufio.NewScanner(stdoutPipe)
-			scanAndTrackErrors(scanner, trackers, os.Stdout, restartChan)
+			scanAndTrackErrors(scanner, trackers, os.Stdout, restartChan, exitChan)
 		}()
 
 		go func() {
 			defer wg.Done()
 			scanner := bufio.NewScanner(stderrPipe)
-			scanAndTrackErrors(scanner, trackers, os.Stderr, restartChan)
+			scanAndTrackErrors(scanner, trackers, os.Stderr, restartChan, exitChan)
 		}()
+
+		log("Starting process %q", cmdName)
 		cmd.Start()
+
 	}
 
 	runCmd()
@@ -153,24 +177,34 @@ func main() {
 	for {
 		select {
 		case sig := <-sigChan:
+			log("Received signal %v", sig)
 			switch sig {
 			case syscall.SIGINT, syscall.SIGTERM:
+				log("Stopping %q process", cmdName)
 				cmd.Process.Signal(sig)
-				cmd.Process.Wait()
+				cmd.Wait()
 				wg.Wait()
+				time.Sleep(100 * time.Millisecond)
 				os.Exit(0)
 			}
 		case <-restartChan:
+			log("Too many errors, restarting process")
 			if err := cmd.Process.Signal(syscall.SIGINT); err != nil {
 				log("Error sending signal to process: %v", err)
 			}
-			cmd.Process.Wait()
+			cmd.Wait()
 			wg.Wait()
-			log("Restarting process due to error limits")
+			log("%q exited, restarting", cmdName)
 			for i := range trackers {
 				trackers[i].Errors = []time.Time{}
 			}
 			runCmd()
+		case <-exitChan:
+			log("%q exited, exiting", cmdName)
+			cmd.Wait()
+			wg.Wait()
+			time.Sleep(100 * time.Millisecond)
+			os.Exit(0)
 		}
 	}
 }
