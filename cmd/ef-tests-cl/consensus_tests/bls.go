88801860137a3a3b6ce36622f66b22871c18cf2c
a <a@tuxpa.in>
2023-05-02 06:10:20 -0500

    test

--- cmd/ef-tests-cl/consensus_tests/bls.go
+++ cmd/ef-tests-cl/consensus_tests/bls.go
@@ -4,8 +4,6 @@ import (
 	"io/fs"
 	"testing"
 
-	"github.com/ledgerwatch/erigon-lib/common"
-	"github.com/ledgerwatch/erigon-lib/common/hexutility"
 	"github.com/ledgerwatch/erigon/cmd/ef-tests-cl/spectest"
 )
 
@@ -13,16 +11,18 @@ type BlsAggregateVerify struct {
 }
 
 func (b *BlsAggregateVerify) Run(t *testing.T, root fs.FS, c spectest.TestCase) (err error) {
-	var meta struct {
-		Input struct {
-			Pubkeys   []hexutility.Bytes `yaml:"pubkeys"`
-			Messages  []common.Hash      `yaml:"messages"`
-			Signature hexutility.Bytes   `yaml:"signature"`
-		} `yaml:"input"`
-		Output bool `yaml:"output"`
-	}
-	if err := spectest.ReadMeta(root, "data.yaml", &meta); err != nil {
-		return err
-	}
-	return spectest.ErrorHandlerNotImplemented
+	t.Skipf("Skipping BLS because it is done by library")
+	return
+	//var meta struct {
+	//	Input struct {
+	//		Pubkeys   []hexutility.Bytes `yaml:"pubkeys"`
+	//		Messages  []common.Hash      `yaml:"messages"`
+	//		Signature hexutility.Bytes   `yaml:"signature"`
+	//	} `yaml:"input"`
+	//	Output bool `yaml:"output"`
+	//}
+	//if err := spectest.ReadMeta(root, "data.yaml", &meta); err != nil {
+	//	return err
+	//}
+	//return spectest.ErrorHandlerNotImplemented
 }
