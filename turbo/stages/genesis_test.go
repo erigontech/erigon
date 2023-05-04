f5bfe79d5a07d740ad223886747962d8d41e327b
alex.sharov <AskAlexSharov@gmail.com>
2023-05-04 11:22:49 +0700

    e4: tests compat

--- turbo/stages/genesis_test.go
+++ turbo/stages/genesis_test.go
@@ -25,9 +25,11 @@ import (
 	"github.com/davecgh/go-spew/spew"
 	"github.com/ledgerwatch/erigon-lib/chain"
 	libcommon "github.com/ledgerwatch/erigon-lib/common"
+	"github.com/ledgerwatch/erigon-lib/common/datadir"
 	"github.com/ledgerwatch/erigon-lib/kv"
 	"github.com/ledgerwatch/erigon/core"
 	"github.com/ledgerwatch/erigon/core/rawdb"
+	"github.com/ledgerwatch/erigon/core/state/temporal"
 	"github.com/ledgerwatch/erigon/core/types"
 	"github.com/ledgerwatch/erigon/crypto"
 	"github.com/ledgerwatch/erigon/params"
@@ -138,8 +140,7 @@ func TestSetupGenesis(t *testing.T) {
 	for _, test := range tests {
 		test := test
 		t.Run(test.name, func(t *testing.T) {
-			m := stages.Mock(t)
-			db := m.DB
+			_, db, _ := temporal.NewTestDB(t, context.Background(), datadir.New(tmpdir), nil)
 			config, genesis, err := test.fn(db)
 			// Check the return values.
 			if !reflect.DeepEqual(err, test.wantErr) {
