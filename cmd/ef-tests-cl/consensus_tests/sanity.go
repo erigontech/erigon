60ff904077fae64e0ce9708047b780b97a06fb54
a <a@tuxpa.in>
2023-05-07 08:04:25 -0500

    remove

--- cmd/ef-tests-cl/consensus_tests/sanity.go
+++ cmd/ef-tests-cl/consensus_tests/sanity.go
@@ -2,7 +2,6 @@ package consensus_tests
 
 import (
 	"io/fs"
-	"log"
 	"os"
 	"testing"
 
@@ -63,7 +62,6 @@ var SanityBlocks = spectest.HandlerFunc(func(t *testing.T, root fs.FS, c spectes
 
 	var block *cltypes.SignedBeaconBlock
 	for _, block = range blocks {
-		log.Println(block)
 		err = transition.TransitionState(testState, block, true)
 		if err != nil {
 			break
