cbb3c51f722b989fe8094c2599ec55228d115002
a <a@tuxpa.in>
2023-05-09 13:45:12 -0500

    note

--- cl/merkle_tree/merkle_root.go
+++ cl/merkle_tree/merkle_root.go
@@ -29,18 +29,26 @@ func HashByteSlice(out, in []byte) error {
 }
 
 func convertHeader(xs []byte) [][32]byte {
-	header := *(*reflect.SliceHeader)(unsafe.Pointer(&xs))
-	header.Len /= 32
-	header.Cap /= 32
-	chunkedChunks := *(*[][32]byte)(unsafe.Pointer(&header))
-	return chunkedChunks
-}
-func convertHeaderBack(xs [][32]byte) []byte {
-	header := *(*reflect.SliceHeader)(unsafe.Pointer(&xs))
-	header.Len *= 32
-	header.Cap *= 32
-	unchunkedChunks := *(*[]byte)(unsafe.Pointer(&header))
-	return unchunkedChunks
+	// this commented naive method of conversion supposedly leads to corruption https://github.com/golang/go/issues/40701
+	//header := *(*reflect.SliceHeader)(unsafe.Pointer(&xs))
+	//header.Len /= 32
+	//header.Cap /= 32
+	//chunkedChunks := *(*[][32]byte)(unsafe.Pointer(&header))
+	//supposedly, this is because escape analysis does not correctly analyze this, and so you have this ghost header?
+
+	// i wont pretend to understand, but my solution for the problem is as so
+
+	// first i grab the slice header of the input
+	header := (*reflect.SliceHeader)(unsafe.Pointer(&xs))
+	// then i allocate a new result slice of no size - this should make the escape analyzer happy i think?
+	dat := make([][32]byte, 0)
+	// we then get the header of our output  to modify
+	chunkedHeader := (*reflect.SliceHeader)(unsafe.Pointer(&dat))
+	// then we move over the values
+	chunkedHeader.Len = header.Len / 32
+	chunkedHeader.Cap = header.Cap / 32
+	chunkedHeader.Data = header.Data
+	return dat
 }
 
 func MerkleRootFromLeaves(leaves [][32]byte) ([32]byte, error) {
