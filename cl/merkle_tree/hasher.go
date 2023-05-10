1b02f41c4e2239bf4fb17e5ff5ee12d334a8920a
Giulio <giulio.rebuffo@gmail.com>
2023-05-10 22:48:38 +0200

    added extra buffer

--- cl/merkle_tree/hasher.go
+++ cl/merkle_tree/hasher.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"sync"
 
+	libcommon "github.com/ledgerwatch/erigon-lib/common"
 	"github.com/ledgerwatch/erigon/cl/utils"
 	"github.com/prysmaticlabs/gohashtree"
 )
@@ -15,10 +16,12 @@ const initialBufferSize = 0 // it is whatever
 // merkleHasher is used internally to provide shared buffer internally to the merkle_tree package.
 type merkleHasher struct {
 	// internalBuffer is the shared buffer we use for each operation
-	internalBuffer     [][32]byte
-	internalFlatBuffer []byte
+	internalBuffer           [][32]byte
+	internalFlatBuffer       []byte
+	internalBufferForSSZList [][32]byte
 	// mu is the lock to ensure thread safety
-	mu sync.Mutex
+	mu  sync.Mutex
+	mu2 sync.Mutex // lock onto ssz list buffer
 }
 
 func newMerkleHasher() *merkleHasher {
@@ -49,6 +52,7 @@ func (m *merkleHasher) merkleizeTrieLeaves(leaves [][32]byte) ([32]byte, error) 
 func (m *merkleHasher) merkleizeTrieLeavesFlat(leaves []byte, out []byte) (err error) {
 	m.mu.Lock()
 	defer m.mu.Unlock()
+
 	layer := m.getBufferFromFlat(leaves)
 	for len(layer) > 1 {
 		if err := gohashtree.Hash(layer, layer); err != nil {
@@ -83,6 +87,14 @@ func (m *merkleHasher) getBuffer(size int) [][32]byte {
 	return m.internalBuffer[:size]
 }
 
+// getBuffer provides buffer of given size.
+func (m *merkleHasher) getBufferForSSZList(size int) [][32]byte {
+	if size > len(m.internalBufferForSSZList) {
+		m.internalBufferForSSZList = make([][32]byte, size*2)
+	}
+	return m.internalBufferForSSZList[:size]
+}
+
 func (m *merkleHasher) getBufferFromFlat(xs []byte) [][32]byte {
 	buf := m.getBuffer(len(xs) / 32)
 	for i := 0; i < len(xs)/32; i = i + 1 {
@@ -97,4 +109,31 @@ func (m *merkleHasher) getFlatBuffer(size int) []byte {
 		m.internalFlatBuffer = make([]byte, size)
 	}
 	return m.internalFlatBuffer[:size]
+}
+
+func (m *merkleHasher) transactionsListRoot(transactions [][]byte) ([32]byte, error) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	txCount := uint64(len(transactions))
+
+	leaves := m.getBuffer(len(transactions))
+	for i, transaction := range transactions {
+		transactionLength := uint64(len(transaction))
+		packedTransactions := packBits(transaction) // Pack transactions
+		transactionsBaseRoot, err := MerkleizeVector(packedTransactions, 33554432)
+		if err != nil {
+			return [32]byte{}, err
+		}
+
+		lengthRoot := Uint64Root(transactionLength)
+		leaves[i] = utils.Keccak256(transactionsBaseRoot[:], lengthRoot[:])
+	}
+	transactionsBaseRoot, err := MerkleizeVector(leaves, 1048576)
+	if err != nil {
+		return libcommon.Hash{}, err
+	}
+
+	countRoot := Uint64Root(txCount)
+
+	return utils.Keccak256(transactionsBaseRoot[:], countRoot[:]), nil
 }
