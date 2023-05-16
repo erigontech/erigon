06079a1569b01dcd84f1245f404ab984c201dc8a
a <a@tuxpa.in>
2023-05-15 20:07:20 -0500

    ok

--- cl/merkle_tree/merkle_root.go
+++ cl/merkle_tree/merkle_root.go
@@ -9,6 +9,7 @@ import (
 )
 
 func HashByteSlice(out, in []byte) error {
+	return gohashtree.HashByteSlice(out, in)
 }
 
 // HashByteSlice is gohashtree HashBytSlice but using our hopefully safer header converstion
