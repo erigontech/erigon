c076d1c39d111c67a0a6b8beb4bc6e1d2defc227
Giulio <giulio.rebuffo@gmail.com>
2023-05-22 20:55:31 +0200

    added tests

--- cl/cltypes/solid/uint64slice_byte.go
+++ cl/cltypes/solid/uint64slice_byte.go
@@ -10,18 +10,23 @@ import (
 	"github.com/ledgerwatch/erigon/cl/utils"
 )
 
+// byteBasedUint64Slice represents a dynamic Uint64Slice data type that is byte-backed.
+// The underlying storage for the slice is a byte array. This approach allows for efficient
+// memory usage, especially when dealing with large slices.
 type byteBasedUint64Slice struct {
-	// the bytes that back the slice
+	// The bytes that back the slice
 	u []byte
 
-	// length of slice
-	l int // len
-	// cap of slize
-	c int // cap
+	// Length of the slice
+	l int
+
+	// Capacity of the slice
+	c int
 
 	hashBuf
 }
 
+// NewUint64Slice creates a new instance of byteBasedUint64Slice with a specified capacity limit.
 func NewUint64Slice(limit int) *byteBasedUint64Slice {
 	o := &byteBasedUint64Slice{
 		c: limit,
@@ -30,6 +35,7 @@ func NewUint64Slice(limit int) *byteBasedUint64Slice {
 	return o
 }
 
+// Clear clears the slice by setting its length to 0 and zeroing out its backing array.
 func (arr *byteBasedUint64Slice) Clear() {
 	arr.l = 0
 	for i := range arr.u {
@@ -37,6 +43,7 @@ func (arr *byteBasedUint64Slice) Clear() {
 	}
 }
 
+// CopyTo copies the slice to a target slice.
 func (arr *byteBasedUint64Slice) CopyTo(target *byteBasedUint64Slice) {
 	target.Clear()
 
@@ -49,10 +56,12 @@ func (arr *byteBasedUint64Slice) CopyTo(target *byteBasedUint64Slice) {
 	copy(target.u, arr.u)
 }
 
+// depth returns the depth of the Merkle tree representing the slice.
 func (arr *byteBasedUint64Slice) depth() int {
 	return int(getDepth(uint64(arr.c) / 4))
 }
 
+// Range iterates over the slice and applies a provided function to each element.
 func (arr *byteBasedUint64Slice) Range(fn func(index int, value uint64, length int) bool) {
 	for i := 0; i < arr.l; i++ {
 		cont := fn(i, arr.Get(i), arr.l)
@@ -62,6 +71,7 @@ func (arr *byteBasedUint64Slice) Range(fn func(index int, value uint64, length i
 	}
 }
 
+// Pop removes and returns the last element of the slice.
 func (arr *byteBasedUint64Slice) Pop() uint64 {
 	offset := (arr.l - 1) * 8
 	val := binary.LittleEndian.Uint64(arr.u[offset : offset+8])
@@ -70,6 +80,7 @@ func (arr *byteBasedUint64Slice) Pop() uint64 {
 	return val
 }
 
+// Append adds a new element to the end of the slice.
 func (arr *byteBasedUint64Slice) Append(v uint64) {
 	if len(arr.u) <= arr.l*8 {
 		arr.u = append(arr.u, make([]byte, 32)...)
@@ -79,6 +90,7 @@ func (arr *byteBasedUint64Slice) Append(v uint64) {
 	arr.l = arr.l + 1
 }
 
+// Get returns the element at the given index.
 func (arr *byteBasedUint64Slice) Get(index int) uint64 {
 	if index >= arr.l {
 		panic("index out of range")
@@ -87,6 +99,7 @@ func (arr *byteBasedUint64Slice) Get(index int) uint64 {
 	return binary.LittleEndian.Uint64(arr.u[offset : offset+8])
 }
 
+// Set replaces the element at the given index with a new value.
 func (arr *byteBasedUint64Slice) Set(index int, v uint64) {
 	if index >= arr.l {
 		panic("index out of range")
@@ -95,14 +108,17 @@ func (arr *byteBasedUint64Slice) Set(index int, v uint64) {
 	binary.LittleEndian.PutUint64(arr.u[offset:offset+8], v)
 }
 
+// Length returns the current length (number of elements) of the slice.
 func (arr *byteBasedUint64Slice) Length() int {
 	return arr.l
 }
 
+// Cap returns the capacity (maximum number of elements) of the slice.
 func (arr *byteBasedUint64Slice) Cap() int {
 	return arr.c
 }
 
+// HashListSSZ computes the SSZ hash of the slice as a list. It returns the hash and any error encountered.
 func (arr *byteBasedUint64Slice) HashListSSZ() ([32]byte, error) {
 	depth := getDepth((uint64(arr.c)*8 + 31) / 32)
 	baseRoot := [32]byte{}
@@ -119,12 +135,12 @@ func (arr *byteBasedUint64Slice) HashListSSZ() ([32]byte, error) {
 	return utils.Keccak256(baseRoot[:], lengthRoot[:]), nil
 }
 
+// HashVectorSSZ computes the SSZ hash of the slice as a vector. It returns the hash and any error encountered.
 func (arr *byteBasedUint64Slice) HashVectorSSZ() ([32]byte, error) {
 	depth := getDepth((uint64(arr.c)*8 + 31) / 32)
 	offset := 32*((arr.l-1)/4) + 32
 	elements := arr.u[:offset]
 	for i := uint8(0); i < depth; i++ {
-		// Sequential
 		layerLen := len(elements)
 		if layerLen%64 == 32 {
 			elements = append(elements, merkle_tree.ZeroHashes[i][:]...)
@@ -140,11 +156,13 @@ func (arr *byteBasedUint64Slice) HashVectorSSZ() ([32]byte, error) {
 	return common.BytesToHash(elements[:32]), nil
 }
 
+// EncodeSSZ encodes the slice in SSZ format. It appends the encoded data to the provided buffer and returns the result.
 func (arr *byteBasedUint64Slice) EncodeSSZ(buf []byte) (dst []byte, err error) {
 	dst = append(buf, arr.u[:arr.l*8]...)
 	return
 }
 
+// DecodeSSZ decodes the slice from SSZ format. It takes a byte slice as input and updates the current slice.
 func (arr *byteBasedUint64Slice) DecodeSSZ(buf []byte, _ int) error {
 	if len(buf)%8 > 0 {
 		return ssz.ErrBadDynamicLength
@@ -156,6 +174,7 @@ func (arr *byteBasedUint64Slice) DecodeSSZ(buf []byte, _ int) error {
 	return nil
 }
 
+// EncodingSizeSSZ returns the size in bytes that the slice would occupy when encoded in SSZ format.
 func (arr *byteBasedUint64Slice) EncodingSizeSSZ() int {
 	return arr.l * 8
 }
