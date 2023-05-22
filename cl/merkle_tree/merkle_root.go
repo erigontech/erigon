be2691916e6ec3b01b823e7bc2655433355de22a
Giulio <giulio.rebuffo@gmail.com>
2023-05-23 01:07:15 +0200

    more tests

--- cl/merkle_tree/merkle_root.go
+++ cl/merkle_tree/merkle_root.go
@@ -13,19 +13,30 @@ import (
 	"github.com/prysmaticlabs/gohashtree"
 )
 
+// HashTreeRoot returns the hash for a given schema of objects.
+// IMPORTANT: DATA TYPE MUST IMPLEMENT HASHABLE
+// SUPPORTED PRIMITIVES: uint64, *uint64 and []byte
 func HashTreeRoot(schema ...interface{}) ([32]byte, error) {
+	// Calculate the total number of leaves needed based on the schema length
 	leaves := make([]byte, NextPowerOfTwo(uint64(len(schema)*length.Hash)))
 	pos := 0
+
+	// Iterate over each element in the schema
 	for i, element := range schema {
 		switch obj := element.(type) {
 		case uint64:
+			// If the element is a uint64, encode it as little-endian and store it in the leaves
 			binary.LittleEndian.PutUint64(leaves[pos:], obj)
 		case *uint64:
+			// If the element is a pointer to uint64, dereference it, encode it as little-endian, and store it in the leaves
 			binary.LittleEndian.PutUint64(leaves[pos:], *obj)
 		case []byte:
+			// If the element is a byte slice
 			if len(obj) < length.Hash {
+				// If the slice is shorter than the length of a hash, copy the slice into the leaves
 				copy(leaves[pos:], obj)
 			} else {
+				// If the slice is longer or equal to the length of a hash, calculate the hash of the slice and store it in the leaves
 				root, err := BytesRoot(obj)
 				if err != nil {
 					return [32]byte{}, err
@@ -33,19 +44,27 @@ func HashTreeRoot(schema ...interface{}) ([32]byte, error) {
 				copy(leaves[pos:], root[:])
 			}
 		case ssz.HashableSSZ:
+			// If the element implements the HashableSSZ interface, calculate the SSZ hash and store it in the leaves
 			root, err := obj.HashSSZ()
 			if err != nil {
 				return [32]byte{}, err
 			}
 			copy(leaves[pos:], root[:])
 		default:
+			// If the element does not match any supported types, panic with an error message
 			panic(fmt.Sprintf("get it out of my face, u put a bad component in the schema. index %d", i))
 		}
+
+		// Move the position pointer to the next leaf
 		pos += length.Hash
 	}
+
+	// Calculate the Merkle root from the flat leaves
 	if err := MerkleRootFromFlatLeaves(leaves, leaves); err != nil {
 		return [32]byte{}, err
 	}
+
+	// Convert the bytes of the resulting hash into a [32]byte and return it
 	return common.BytesToHash(leaves[:length.Hash]), nil
 }
 
@@ -71,13 +90,6 @@ func HashByteSlice(out, in []byte) error {
 }
 
 func convertHeader(xs []byte) [][32]byte {
-	// this commented naive method of conversion supposedly leads to corruption https://github.com/golang/go/issues/40701
-	//header := *(*reflect.SliceHeader)(unsafe.Pointer(&xs))
-	//header.Len /= 32
-	//header.Cap /= 32
-	//chunkedChunks := *(*[][32]byte)(unsafe.Pointer(&header))
-	//supposedly, this is because escape analysis does not correctly analyze this, and so you have this ghost header?
-
 	// i wont pretend to understand, but my solution for the problem is as so
 
 	// first i grab the slice header of the input
---
+++ cl/merkle_tree/merkle_root_test.go
@@ -0,0 +1,35 @@
+package merkle_tree_test
+
+import (
+	_ "embed"
+	"testing"
+
+	"github.com/ledgerwatch/erigon-lib/common"
+	"github.com/ledgerwatch/erigon/cl/clparams"
+	"github.com/ledgerwatch/erigon/cl/merkle_tree"
+	"github.com/ledgerwatch/erigon/cl/phase1/core/state"
+	"github.com/ledgerwatch/erigon/cl/utils"
+	"github.com/stretchr/testify/require"
+)
+
+//go:embed testdata/serialized.ssz_snappy
+var beaconState []byte
+
+func TestHashTreeRoot(t *testing.T) {
+	bs := state.New(&clparams.MainnetBeaconConfig)
+	require.NoError(t, utils.DecodeSSZSnappy(bs, beaconState, int(clparams.DenebVersion)))
+	root, err := bs.HashSSZ()
+	require.NoError(t, err)
+	require.Equal(t, common.Hash(root), common.HexToHash("0x7d085d9f2cce04eefb4c0aafad744fd2ce4ff962b2c3589fda53aab084171406"))
+}
+
+func TestHashTreeRootTxs(t *testing.T) {
+	txs := [][]byte{
+		{1, 2, 3},
+		{1, 2, 3},
+		{1, 2, 3},
+	}
+	root, err := merkle_tree.TransactionsListRoot(txs)
+	require.NoError(t, err)
+	require.Equal(t, common.Hash(root), common.HexToHash("0x987269bc1075122edff32bfc38479757103cee5c1ed6e990de7ffee85b5dd18a"))
+}
