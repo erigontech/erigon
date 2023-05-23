7d6062cb51d5df5707618d30bf5c2f3a08f52108
alex.sharov <AskAlexSharov@gmail.com>
2023-05-23 12:18:17 +0700

    save

--- turbo/stages/genesis_test.go
+++ turbo/stages/genesis_test.go
@@ -26,10 +26,12 @@ import (
 	"github.com/ledgerwatch/erigon-lib/chain"
 	libcommon "github.com/ledgerwatch/erigon-lib/common"
 	"github.com/ledgerwatch/erigon-lib/kv"
+	"github.com/ledgerwatch/erigon-lib/kv/memdb"
 	"github.com/ledgerwatch/erigon/core"
 	"github.com/ledgerwatch/erigon/core/types"
 	"github.com/ledgerwatch/erigon/crypto"
 	"github.com/ledgerwatch/erigon/params"
+	"github.com/ledgerwatch/erigon/turbo/snapshotsync"
 	"github.com/ledgerwatch/erigon/turbo/stages"
 	"github.com/ledgerwatch/log/v3"
 )
@@ -139,9 +141,8 @@ func TestSetupGenesis(t *testing.T) {
 	for _, test := range tests {
 		test := test
 		t.Run(test.name, func(t *testing.T) {
-			m := stages.Mock(t)
-			db := m.DB
-			blockReader, _ := m.NewBlocksIO()
+			db := memdb.NewTestDB(t)
+			blockReader := snapshotsync.NewBlockReader(EnableTxsV3InTest)
 			config, genesis, err := test.fn(db)
 			// Check the return values.
 			if !reflect.DeepEqual(err, test.wantErr) {
