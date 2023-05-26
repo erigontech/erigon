3e2b1327ea5d3f5dbba2b8b319050e7e80fc3aee
alex.sharov <AskAlexSharov@gmail.com>
2023-05-26 15:11:51 +0700

    save

--- cmd/rpcdaemon/rpcservices/eth_backend.go
+++ cmd/rpcdaemon/rpcservices/eth_backend.go
@@ -62,12 +62,12 @@ func (back *RemoteBackend) BlockByNumber(ctx context.Context, db kv.Tx, number u
 	block, _, err := back.BlockWithSenders(ctx, db, hash, number)
 	return block, err
 }
-func (r *RemoteBackend) BlockByHash(ctx context.Context, db kv.Tx, hash libcommon.Hash) (*types.Block, error) {
+func (back *RemoteBackend) BlockByHash(ctx context.Context, db kv.Tx, hash libcommon.Hash) (*types.Block, error) {
 	number := rawdb.ReadHeaderNumber(db, hash)
 	if number == nil {
 		return nil, nil
 	}
-	block, _, err := r.BlockWithSenders(ctx, db, hash, *number)
+	block, _, err := back.BlockWithSenders(ctx, db, hash, *number)
 	return block, err
 }
 func (back *RemoteBackend) TxsV3Enabled() bool {
