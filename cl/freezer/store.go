2424198a1a1787df69f9c34c3b981a04570875a2
a <a@tuxpa.in>
2023-06-03 19:26:31 -0500

    refac

--- cl/freezer/store.go
+++ cl/freezer/store.go
@@ -5,19 +5,6 @@ import (
 	"io"
 )
 
-type Freezer interface {
-	Getter
-	Putter
-}
-
-type Getter interface {
-	Get(namespace, object, id string, extra ...string) (data io.ReadCloser, sidecar []byte, err error)
-}
-
-type Putter interface {
-	Put(data io.Reader, sidecar []byte, namespace, object, id string, extra ...string) error
-}
-
 type BlobStore struct {
 	f Freezer
 }
