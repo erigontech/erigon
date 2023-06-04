be1db89b92d76cf8e530b34e0dcb952a2d74bfba
a <a@tuxpa.in>
2023-06-03 19:22:46 -0500

    test

---
+++ cl/freezer/freezer_test.go
@@ -0,0 +1,35 @@
+package freezer_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/ledgerwatch/erigon/cl/freezer"
+	"github.com/stretchr/testify/assert"
+)
+
+func runBlobStoreTest(t *testing.T, b *freezer.BlobStore) {
+	// put item into obj
+	orig := []byte{1, 2, 3, 4}
+	err := b.Put(orig, "test", "a", "b")
+	assert.NoError(t, err)
+
+	// get item from obj
+	ans, err := b.Get("test", "a", "b")
+	assert.NoError(t, err)
+	assert.EqualValues(t, orig, ans)
+
+	ans, err = b.Get("test", "b", "a")
+	assert.ErrorIs(t, err, os.ErrNotExist)
+	assert.Nil(t, ans)
+}
+
+func TestMemoryStore(t *testing.T) {
+	runBlobStoreTest(t, freezer.NewBlobStore(&freezer.InMemory{}))
+}
+
+func TestRootPathStore(t *testing.T) {
+	fz := &freezer.RootPathOsFs{"test_output"}
+	defer os.RemoveAll("test_output")
+	runBlobStoreTest(t, freezer.NewBlobStore(fz))
+}
--- cl/freezer/store.go
+++ cl/freezer/store.go
@@ -1,6 +1,9 @@
 package freezer
 
-import "io"
+import (
+	"bytes"
+	"io"
+)
 
 type Freezer interface {
 	Getter
@@ -13,4 +16,24 @@ type Getter interface {
 
 type Putter interface {
 	Put(data io.Reader, sidecar []byte, namespace, object, id string, extra ...string) error
+}
+
+type BlobStore struct {
+	f Freezer
+}
+
+func NewBlobStore(f Freezer) *BlobStore {
+	return &BlobStore{f: f}
+}
+
+func (b *BlobStore) Get(namespace, object, id string) ([]byte, error) {
+	o, _, err := b.f.Get(namespace, object, id)
+	if err != nil {
+		return nil, err
+	}
+	return io.ReadAll(o)
+}
+
+func (b *BlobStore) Put(dat []byte, namespace, object, id string) error {
+	return b.f.Put(bytes.NewBuffer(dat), nil, namespace, object, id)
 }
