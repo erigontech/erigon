d703c3cd8e70555054b93807abf46c515def227b
Alex Sharov <AskAlexSharov@gmail.com>
2023-06-06 15:51:04 +0700

    e3: Fix integration tests, step 2 (#7673)

--- eth/stagedsync/stage_bodies.go
+++ eth/stagedsync/stage_bodies.go
@@ -10,8 +10,6 @@ import (
 	libcommon "github.com/ledgerwatch/erigon-lib/common"
 	"github.com/ledgerwatch/erigon-lib/common/dbg"
 	"github.com/ledgerwatch/erigon-lib/kv"
-	"github.com/ledgerwatch/erigon-lib/kv/rawdbv3"
-	"github.com/ledgerwatch/erigon/common/dbutils"
 	"github.com/ledgerwatch/erigon/core/rawdb"
 	"github.com/ledgerwatch/erigon/core/rawdb/blockio"
 	"github.com/ledgerwatch/log/v3"
@@ -208,14 +206,9 @@ func BodiesForward(
 				return false, fmt.Errorf("WriteRawBodyIfNotExists: %w", err)
 			}
 			if cfg.historyV3 && ok {
-				body, _ := rawdb.ReadBodyForStorageByKey(tx, dbutils.BlockBodyKey(blockHeight, header.Hash()))
-				lastTxnID := body.BaseTxId + uint64(body.TxAmount) - 1
-				if err := rawdbv3.TxNums.Append(tx, blockHeight, lastTxnID); err != nil {
+				if err := rawdb.AppendCanonicalTxNums(tx, blockHeight); err != nil {
 					return false, err
 				}
-				//if err := rawdb.AppendCanonicalTxNums(tx, blockHeight); err != nil {
-				//	return false, err
-				//}
 			}
 			if ok {
 				dataflow.BlockBodyDownloadStates.AddChange(blockHeight, dataflow.BlockBodyCleared)
