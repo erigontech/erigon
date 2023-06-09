7690c4cd563c219b652bd445b64ac9eb382a49bb
alex.sharov <AskAlexSharov@gmail.com>
2023-06-09 13:27:55 +0700

    save

--- consensus/bor/snapshot.go
+++ consensus/bor/snapshot.go
@@ -5,7 +5,7 @@ import (
 	"context"
 	"encoding/json"
 
-	lru "github.com/hashicorp/golang-lru/v2"
+	lru "github.com/hashicorp/golang-lru/arc/v2"
 	"github.com/ledgerwatch/erigon-lib/chain"
 	"github.com/ledgerwatch/erigon-lib/common"
 	"github.com/ledgerwatch/erigon-lib/kv"
