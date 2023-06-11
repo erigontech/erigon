f672b33f66d1c25eb86fae4ff9287147e07012bc
alex.sharov <AskAlexSharov@gmail.com>
2023-06-11 10:11:12 +0700

    save

--- cmd/rpcdaemon/rpcdaemontest/test_util.go
+++ cmd/rpcdaemon/rpcdaemontest/test_util.go
@@ -90,25 +90,25 @@ func CreateTestSentry(t *testing.T) (*stages.MockSentry, *core.ChainPack, []*cor
 	defer contractBackend.Close()
 
 	// Generate empty chain to have some orphaned blocks for tests
-	orphanedChain, err := core.GenerateChain(m.ChainConfig, m.Genesis, m.Engine, m.DB, 5, func(i int, block *core.BlockGen) {
-	}, true)
-	if err != nil {
-		t.Fatal(err)
-	}
+	//orphanedChain, err := core.GenerateChain(m.ChainConfig, m.Genesis, m.Engine, m.DB, 5, func(i int, block *core.BlockGen) {
+	//}, true)
+	//if err != nil {
+	//	t.Fatal(err)
+	//}
 
 	chain, err := getChainInstance(&addresses, m.ChainConfig, m.Genesis, m.Engine, m.DB, contractBackend)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if err = m.InsertChain(orphanedChain); err != nil {
-		t.Fatal(err)
-	}
+	//if err = m.InsertChain(orphanedChain); err != nil {
+	//	t.Fatal(err)
+	//}
 	if err = m.InsertChain(chain); err != nil {
 		t.Fatal(err)
 	}
 
-	return m, chain, []*core.ChainPack{orphanedChain}
+	return m, chain, []*core.ChainPack{}
 }
 
 var chainInstance *core.ChainPack
