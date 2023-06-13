ff2baff57308656aaa3a96e44d072bb51aeac435
alex.sharov <AskAlexSharov@gmail.com>
2023-06-13 09:35:23 +0700

    save

--- tests/state_test_util.go
+++ tests/state_test_util.go
@@ -191,20 +191,14 @@ func (t *StateTest) RunNoVerify(tx kv.RwTx, subtest StateSubtest, vmconfig vm.Co
 	readBlockNr := block.NumberU64()
 	writeBlockNr := readBlockNr + 1
 
-	_, err = MakePreState(&chain.Rules{}, tx, t.json.Pre, readBlockNr)
+	_, err = MakePreState(&chain.Rules{}, tx, t.json.Pre, readBlockNr, ethconfig.EnableHistoryV4InTest)
 	if err != nil {
 		return nil, libcommon.Hash{}, UnsupportedForkError{subtest.Fork}
 	}
 
-	r := rpchelper.NewLatestStateReader(tx)
+	r := rpchelper.NewLatestStateReader(tx, ethconfig.EnableHistoryV4InTest)
+	w := rpchelper.NewLatestStateWriter(tx, writeBlockNr, ethconfig.EnableHistoryV4InTest)
 	statedb := state.New(r)
-
-	var w state.StateWriter
-	if ethconfig.EnableHistoryV4InTest {
-		w = state.NewWriterV4(tx.(kv.TemporalTx))
-	} else {
-		w = state.NewPlainStateWriter(tx, nil, writeBlockNr)
-	}
 
 	var baseFee *big.Int
 	if config.IsLondon(0) {
@@ -318,12 +312,7 @@ func (t *StateTest) RunNoVerify(tx kv.RwTx, subtest StateSubtest, vmconfig vm.Co
 }
 
 func MakePreState(rules *chain.Rules, tx kv.RwTx, accounts types.GenesisAlloc, blockNr uint64, histV3 bool) (*state.IntraBlockState, error) {
-	var r state.StateReader
-	if histV3 {
-		r = state.NewReaderV4(tx.(kv.TemporalTx))
-	} else {
-		r = state.NewPlainStateReader(tx)
-	}
+	r := rpchelper.NewLatestStateReader(tx, histV3)
 	statedb := state.New(r)
 	for addr, a := range accounts {
 		statedb.SetCode(addr, a.Code)
