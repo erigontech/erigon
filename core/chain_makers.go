1c9e18cac156dc7aef979f52e886478680c0a145
alex.sharov <AskAlexSharov@gmail.com>
2023-06-22 17:14:13 +0700

    generate_chains root calc

--- core/chain_makers.go
+++ core/chain_makers.go
@@ -18,6 +18,7 @@ package core
 
 import (
 	"context"
+	"encoding/binary"
 	"fmt"
 	"math/big"
 
@@ -417,7 +418,7 @@ func GenerateChain(config *chain.Config, parent *types.Block, engine consensus.E
 	return &ChainPack{Headers: headers, Blocks: blocks, Receipts: receipts, TopBlock: blocks[n-1]}, nil
 }
 
-func hashKV(k []byte, h *common.Hasher) (newK []byte, err error) {
+func hashKeyAndAddIncarnation(k []byte, h *common.Hasher) (newK []byte, err error) {
 	if len(k) == length.Addr {
 		newK = make([]byte, length.Hash)
 	} else {
@@ -428,11 +429,18 @@ func hashKV(k []byte, h *common.Hasher) (newK []byte, err error) {
 	h.Sha.Write(k[:length.Addr])
 	//nolint:errcheck
 	h.Sha.Read(newK[:length.Hash])
-	if len(k) > length.Addr {
+	if len(k) == length.Addr+length.Incarnation+length.Hash { // PlainState storage
 		copy(newK[length.Hash:], k[length.Addr:length.Addr+length.Incarnation])
 		h.Sha.Reset()
 		//nolint:errcheck
 		h.Sha.Write(k[length.Addr+length.Incarnation:])
+		//nolint:errcheck
+		h.Sha.Read(newK[length.Hash+length.Incarnation:])
+	} else if len(k) == length.Addr+length.Hash { // e4 Domain storage
+		binary.BigEndian.PutUint64(newK[length.Hash:], 1)
+		h.Sha.Reset()
+		//nolint:errcheck
+		h.Sha.Write(k[len(k)-length.Hash:])
 		//nolint:errcheck
 		h.Sha.Read(newK[length.Hash+length.Incarnation:])
 	}
@@ -478,7 +486,7 @@ func CalcHashRootForTests(tx kv.RwTx, header *types.Header, histV3 bool) (hashRo
 					return hashRoot, fmt.Errorf("interate over plain state: %w", err)
 				}
 			}
-			newK, err := hashKV(k, h)
+			newK, err := hashKeyAndAddIncarnation(k, h)
 			if err != nil {
 				return hashRoot, fmt.Errorf("clear HashedAccounts bucket: %w", err)
 			}
@@ -496,7 +504,7 @@ func CalcHashRootForTests(tx kv.RwTx, header *types.Header, histV3 bool) (hashRo
 			if err != nil {
 				return hashRoot, fmt.Errorf("interate over plain state: %w", err)
 			}
-			newK, err := hashKV(k, h)
+			newK, err := hashKeyAndAddIncarnation(k, h)
 			if err != nil {
 				return hashRoot, fmt.Errorf("clear HashedStorage bucket: %w", err)
 			}
@@ -520,7 +528,7 @@ func CalcHashRootForTests(tx kv.RwTx, header *types.Header, histV3 bool) (hashRo
 		if err != nil {
 			return hashRoot, fmt.Errorf("interate over plain state: %w", err)
 		}
-		newK, err := hashKV(k, h)
+		newK, err := hashKeyAndAddIncarnation(k, h)
 		if err != nil {
 			return hashRoot, fmt.Errorf("insert hashed key: %w", err)
 		}
