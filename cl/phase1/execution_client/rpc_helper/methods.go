676e63b3cd347afa260b9bf48971b44edc82f1c5
Enrique Jose Avila Asapche <eavilaasapche@gmail.com>
2023-07-02 19:01:40 +0300

    ops

--- cl/phase1/execution_client/rpc_helper/methods.go
+++ cl/phase1/execution_client/rpc_helper/methods.go
@@ -1,1 +1,8 @@
-package rpchelper
+package rpc_helper
+
+const EngineNewPayloadV1 = "engine_newPayloadV1"
+const EngineNewPayloadV2 = "engine_newPayloadV2"
+const EngineNewPayloadV3 = "engine_newPayloadV3"
+
+const ForkChoiceUpdatedV1 = "engine_forkchoiceUpdatedV1"
+const ForkChoiceUpdatedV2 = "engine_forkchoiceUpdatedV2"
