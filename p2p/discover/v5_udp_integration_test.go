f3b89059601dab5898dc50e7dadccc8682596d58
alex.sharov <AskAlexSharov@gmail.com>
2023-07-05 10:44:06 +0700

    save

--- p2p/discover/v5_udp_integration_test.go
+++ p2p/discover/v5_udp_integration_test.go
@@ -13,6 +13,47 @@ import (
 	"github.com/ledgerwatch/log/v3"
 )
 
+// This test checks that pending calls are re-sent when a handshake happens.
+func TestUDPv5_callResend(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("fix me on win please")
+	}
+	t.Parallel()
+	logger := log.New()
+	test := newUDPV5Test(t, logger)
+	t.Cleanup(test.close)
+
+	remote := test.getNode(test.remotekey, test.remoteaddr, logger).Node()
+	done := make(chan error, 2)
+	go func() {
+		_, err := test.udp.ping(remote)
+		done <- err
+	}()
+	go func() {
+		_, err := test.udp.ping(remote)
+		done <- err
+	}()
+
+	// Ping answered by WHOAREYOU.
+	test.waitPacketOut(func(p *v5wire.Ping, addr *net.UDPAddr, nonce v5wire.Nonce) {
+		test.packetIn(&v5wire.Whoareyou{Nonce: nonce}, logger)
+	})
+	// Ping should be re-sent.
+	test.waitPacketOut(func(p *v5wire.Ping, addr *net.UDPAddr, _ v5wire.Nonce) {
+		test.packetIn(&v5wire.Pong{ReqID: p.ReqID}, logger)
+	})
+	// Answer the other ping.
+	test.waitPacketOut(func(p *v5wire.Ping, addr *net.UDPAddr, _ v5wire.Nonce) {
+		test.packetIn(&v5wire.Pong{ReqID: p.ReqID}, logger)
+	})
+	if err := <-done; err != nil {
+		t.Fatalf("unexpected ping error: %v", err)
+	}
+	if err := <-done; err != nil {
+		t.Fatalf("unexpected ping error: %v", err)
+	}
+}
+
 // This test checks that calls with n replies may take up to n * respTimeout.
 func TestUDPv5_callTimeoutReset(t *testing.T) {
 	if runtime.GOOS == "windows" {
