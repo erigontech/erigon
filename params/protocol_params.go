d341cd2cc404387a9912160449bc1c379ad3eb5c
yperbasis <andrey.ashikhmin@gmail.com>
2023-07-11 09:18:59 +0200

    Use EIP-4844 constants from erigon-lib

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -16,11 +16,7 @@
 
 package params
 
-import (
-	"math/big"
-
-	"github.com/ledgerwatch/erigon-lib/chain"
-)
+import "math/big"
 
 const (
 	GasLimitBoundDivisor uint64 = 1024               // The bound divisor of the gas limit, used in update calculations.
@@ -167,10 +163,8 @@ const (
 	RefundQuotientEIP3529 uint64 = 5
 
 	// stuff from EIP-4844
-	FieldElementsPerBlob       = 4096 // each field element is 32 bytes
 	MinDataGasPrice            = 1
 	DataGasPriceUpdateFraction = 3338477
-	MaxBlobsPerBlock           = chain.MaxDataGasPerBlock / chain.DataGasPerBlob
 
 	BlobVerificationGas      uint64 = 1800000
 	BlobCommitmentVersionKZG uint8  = 0x01
