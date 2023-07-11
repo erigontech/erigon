99788728c28c4e8546fbdfc3ef06fd3079c2e13c
gladcow <jahr@yandex.ru>
2023-07-11 18:19:19 +0300

    expand home dir from tilda

--- cmd/integration/commands/root.go
+++ cmd/integration/commands/root.go
@@ -2,7 +2,9 @@ package commands
 
 import (
 	"context"
+	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/c2h5oh/datasize"
 	"github.com/ledgerwatch/log/v3"
@@ -22,12 +24,28 @@ import (
 	"github.com/ledgerwatch/erigon/turbo/logging"
 )
 
+func expandHomeDir(dirpath string) string {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		panic(err)
+	}
+	if strings.HasPrefix(dirpath, "~/") {
+		return filepath.Join(home, dirpath[2:])
+	} else if dirpath == "~" {
+		return home
+	}
+	return dirpath
+}
+
 var rootCmd = &cobra.Command{
 	Use:   "integration",
 	Short: "long and heavy integration tests for Erigon",
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		datadirCli = expandHomeDir(datadirCli)
 		if chaindata == "" {
 			chaindata = filepath.Join(datadirCli, "chaindata")
+		} else {
+			chaindata = expandHomeDir(chaindata)
 		}
 	},
 	PersistentPostRun: func(cmd *cobra.Command, args []string) {
