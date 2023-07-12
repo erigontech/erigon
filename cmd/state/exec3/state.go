7800d1fd85d695eb075622c01b9281ba2206c7b4
awskii <artem.tsskiy@gmail.com>
2023-07-12 23:37:42 +0100

    save

--- cmd/state/exec3/state.go
+++ cmd/state/exec3/state.go
@@ -147,9 +147,6 @@ func (rw *Worker) RunTxTaskNoLock(txTask *exec22.TxTask) {
 		//fmt.Printf("txNum=%d, blockNum=%d, DAO fork\n", txTask.TxNum, txTask.BlockNum)
 		misc.ApplyDAOHardFork(ibs)
 		ibs.SoftFinalise()
-		//if err = ibs.FinalizeTx(rules, rw.stateWriter); err != nil {
-		//	panic(err)
-		//}
 	case txTask.TxIndex == -1:
 		if txTask.BlockNum == 0 {
 			// Genesis block
