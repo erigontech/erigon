814984dffbd586caa4d877f553f81d64c872f0c6
Giulio rebuffo <giulio.rebuffo@gmail.com>
2023-07-14 16:38:17 +0200

    Implementation of execution.proto: added getters (#7898)

---
+++ turbo/execution/eth1/ethereum_execution.go
@@ -0,0 +1,28 @@
+package eth1
+
+import (
+	"github.com/ledgerwatch/erigon-lib/gointerfaces/execution"
+
+	"github.com/ledgerwatch/erigon-lib/kv"
+	"github.com/ledgerwatch/erigon/turbo/services"
+)
+
+// EthereumExecutionModule describes ethereum execution logic and indexing.
+type EthereumExecutionModule struct {
+	// Snapshots + MDBX
+	blockReader services.FullBlockReader
+
+	// MDBX database
+	db kv.RwDB
+
+	execution.UnimplementedExecutionServer
+}
+
+// Remaining
+
+// func (execution.UnimplementedExecutionServer).InsertBodies(context.Context, *execution.InsertBodiesRequest) (*execution.EmptyMessage, error)
+// func (execution.UnimplementedExecutionServer).InsertHeaders(context.Context, *execution.InsertHeadersRequest) (*execution.EmptyMessage, error)
+
+// func (execution.UnimplementedExecutionServer).AssembleBlock(context.Context, *execution.EmptyMessage) (*types.ExecutionPayload, error)
+// func (execution.UnimplementedExecutionServer).UpdateForkChoice(context.Context, *types.H256) (*execution.ForkChoiceReceipt, error)
+// func (execution.UnimplementedExecutionServer).ValidateChain(context.Context, *types.H256) (*execution.ValidationReceipt, error)
