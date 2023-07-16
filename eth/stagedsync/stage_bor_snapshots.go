da9959343412dd1ebd1d373c9779677cad0644c4
Alex Sharp <alexsharp@Alexs-MacBook-Pro-2.local>
2023-07-16 09:53:36 +0100

    More

--- eth/stagedsync/stage_bor_snapshots.go
+++ eth/stagedsync/stage_bor_snapshots.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/ledgerwatch/erigon-lib/kv"
+	"github.com/ledgerwatch/erigon/eth/stagedsync/stages"
 	"github.com/ledgerwatch/log/v3"
 )
 
@@ -18,8 +19,36 @@ func BorSnapshotsForward(
 	if cfg.chainConfig.Bor == nil {
 		return
 	}
+	useExternalTx := tx != nil
+	if !useExternalTx {
+		tx, err = cfg.db.BeginRw(ctx)
+		if err != nil {
+			return err
+		}
+		defer tx.Rollback()
+	}
 	if err := DownloadAndIndexBorSnapshotsIfNeed(s, ctx, tx, cfg, initialCycle, logger); err != nil {
 		return err
+	}
+	var minProgress uint64
+	for _, stage := range []stages.SyncStage{stages.Headers, stages.Bodies, stages.Senders, stages.TxLookup} {
+		progress, err := stages.GetStageProgress(tx, stage)
+		if err != nil {
+			return err
+		}
+		if minProgress == 0 || progress < minProgress {
+			minProgress = progress
+		}
+	}
+	if minProgress > s.BlockNumber {
+		if err = s.Update(tx, minProgress); err != nil {
+			return err
+		}
+	}
+	if !useExternalTx {
+		if err := tx.Commit(); err != nil {
+			return err
+		}
 	}
 	return
 }
@@ -40,6 +69,22 @@ func DownloadAndIndexBorSnapshotsIfNeed(s *StageState, ctx context.Context, tx k
 func BorSnapshotsPrune(s *PruneState, initialCycle bool, cfg SnapshotsCfg, ctx context.Context, tx kv.RwTx) (err error) {
 	if cfg.chainConfig.Bor == nil {
 		return
+	}
+	useExternalTx := tx != nil
+	if !useExternalTx {
+		tx, err = cfg.db.BeginRw(ctx)
+		if err != nil {
+			return err
+		}
+		defer tx.Rollback()
+	}
+	freezingCfg := cfg.blockReader.FreezingCfg()
+	if freezingCfg.Enabled && freezingCfg.Produce {
+	}
+	if !useExternalTx {
+		if err := tx.Commit(); err != nil {
+			return err
+		}
 	}
 	return
 }
