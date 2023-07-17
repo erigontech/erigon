e528e71105c2032f7bcd22fb8858c54adbd19afa
alex.sharov <AskAlexSharov@gmail.com>
2023-07-17 18:12:11 +0700

    save

--- state/domain.go
+++ state/domain.go
@@ -838,6 +838,7 @@ func (d *Domain) collate(ctx context.Context, step, txFrom, txTo uint64, roTx kv
 	)
 
 	eg, _ := errgroup.WithContext(ctx)
+	defer eg.Wait()
 	eg.Go(func() (errInternal error) {
 		valCount, errInternal = d.writeCollationPair(valuesComp, pairs)
 		return errInternal
