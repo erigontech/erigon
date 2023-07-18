06235ef74294613c9a3f1c7787cfc249642574b7
alex.sharov <AskAlexSharov@gmail.com>
2023-07-18 12:53:44 +0700

    save

--- turbo/backup/backup.go
+++ turbo/backup/backup.go
@@ -41,7 +41,7 @@ func OpenPair(from, to string, label kv.Label, targetPageSize datasize.ByteSize,
 		Label(label).
 		PageSize(targetPageSize.Bytes()).
 		MapSize(datasize.ByteSize(info.Geo.Upper)).
-		Flags(func(flags uint) uint { return flags | mdbx.NoMemInit | mdbx.WriteMap }).
+		Flags(func(flags uint) uint { return flags | mdbx.WriteMap }).
 		WithTableCfg(func(_ kv.TableCfg) kv.TableCfg { return kv.TablesCfgByLabel(label) }).
 		MustOpen()
 	return src, dst
