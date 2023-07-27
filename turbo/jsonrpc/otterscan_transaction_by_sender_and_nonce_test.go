eb093fb9e3ad90374486714692715f807c7b61c2
Willian Mitsuda <wmitsuda@gmail.com>
2023-07-27 16:48:14 -0300

    Fix tests

--- turbo/jsonrpc/otterscan_transaction_by_sender_and_nonce_test.go
+++ turbo/jsonrpc/otterscan_transaction_by_sender_and_nonce_test.go
@@ -12,7 +12,7 @@ import (
 func TestGetTransactionBySenderAndNonce(t *testing.T) {
 	m, _, _ := rpcdaemontest.CreateTestSentry(t)
 	agg := m.HistoryV3Components()
-	api := NewOtterscanAPI(NewBaseApi(nil, nil, m.BlockReader, agg, false, rpccfg.DefaultEvmCallTimeout, m.Engine, m.Dirs), m.DB)
+	api := NewOtterscanAPI(NewBaseApi(nil, nil, m.BlockReader, agg, false, rpccfg.DefaultEvmCallTimeout, m.Engine, m.Dirs), m.DB, 25)
 
 	addr := common.HexToAddress("0x537e697c7ab75a26f9ecf0ce810e3154dfcaaf44")
 	expectCreator := common.HexToAddress("0x71562b71999873db5b286df957af199ec94617f7")
