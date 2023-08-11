d2bde8c096e95c18014cf5c366d1c7d77b136ced
Alex Sharov <AskAlexSharov@gmail.com>
2023-08-11 11:54:59 +0600

    Recsplit: cancelable build (#7994)

--- turbo/snapshotsync/freezeblocks/block_snapshots.go
+++ turbo/snapshotsync/freezeblocks/block_snapshots.go
@@ -1795,7 +1795,7 @@ RETRY:
 		return fmt.Errorf("TransactionsIdx: at=%d-%d, post index building, expect: %d, got %d", blockFrom, blockTo, expectedCount, i)
 	}
 
-	if err := txnHashIdx.Build(); err != nil {
+	if err := txnHashIdx.Build(ctx); err != nil {
 		if errors.Is(err, recsplit.ErrCollision) {
 			logger.Warn("Building recsplit. Collision happened. It's ok. Restarting with another salt...", "err", err)
 			txnHashIdx.ResetNextSalt()
@@ -1804,7 +1804,7 @@ RETRY:
 		}
 		return fmt.Errorf("txnHashIdx: %w", err)
 	}
-	if err := txnHash2BlockNumIdx.Build(); err != nil {
+	if err := txnHash2BlockNumIdx.Build(ctx); err != nil {
 		if errors.Is(err, recsplit.ErrCollision) {
 			logger.Warn("Building recsplit. Collision happened. It's ok. Restarting with another salt...", "err", err)
 			txnHashIdx.ResetNextSalt()
@@ -1895,13 +1895,14 @@ func Idx(ctx context.Context, d *compress.Decompressor, firstDataID uint64, tmpD
 	var idxFilePath = segmentFileName[0:len(segmentFileName)-len(extension)] + ".idx"
 
 	rs, err := recsplit.NewRecSplit(recsplit.RecSplitArgs{
-		KeyCount:   d.Count(),
-		Enums:      true,
-		BucketSize: 2000,
-		LeafSize:   8,
-		TmpDir:     tmpDir,
-		IndexFile:  idxFilePath,
-		BaseDataID: firstDataID,
+		KeyCount:    d.Count(),
+		Enums:       true,
+		BucketSize:  2000,
+		LeafSize:    8,
+		TmpDir:      tmpDir,
+		IndexFile:   idxFilePath,
+		BaseDataID:  firstDataID,
+		EtlBufLimit: etl.BufferOptimalSize / 2,
 	}, logger)
 	if err != nil {
 		return err
@@ -1928,7 +1929,7 @@ RETRY:
 		default:
 		}
 	}
-	if err = rs.Build(); err != nil {
+	if err = rs.Build(ctx); err != nil {
 		if errors.Is(err, recsplit.ErrCollision) {
 			logger.Info("Building recsplit. Collision happened. It's ok. Restarting with another salt...", "err", err)
 			rs.ResetNextSalt()
