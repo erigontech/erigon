9a5b482b77eb55bd3f88deb9a80df581ab9be44e
yperbasis <andrey.ashikhmin@gmail.com>
2023-08-13 21:52:53 +0200

    blobGasUsed in transaction receipt should be 0x hex string

--- turbo/jsonrpc/eth_receipts.go
+++ turbo/jsonrpc/eth_receipts.go
@@ -775,7 +775,8 @@ func marshalReceipt(receipt *types.Receipt, txn types.Transaction, chainConfig *
 				log.Error(err.Error())
 			}
 			fields["blobGasPrice"] = blobGasPrice
-			fields["blobGasUsed"] = misc.GetBlobGasUsed(numBlobs)
+			blobGasUsed := misc.GetBlobGasUsed(numBlobs)
+			fields["blobGasUsed"] = hexutil.Uint64(blobGasUsed)
 		}
 	}
 	return fields
