e8ecf4502a780d25b4664579ab6c18ef7cd5e9bb
Somnath <somnathb@nmodulo.com>
2023-08-14 01:35:27 +0530

    Revert commented code, already fixed

--- core/types/transaction_signing.go
+++ core/types/transaction_signing.go
@@ -254,10 +254,9 @@ func (sg Signer) SenderWithContext(context *secp256k1.Context, tx Transaction) (
 		V.Add(&t.V, u256.Num27)
 		R, S = &t.R, &t.S
 	case *BlobTx:
-		// TODO: Fix the commented out code below
-		// if !sg.blob {
-		// 	return libcommon.Address{}, fmt.Errorf("blob tx is not supported by signer %s", sg)
-		// }
+		if !sg.blob {
+			return libcommon.Address{}, fmt.Errorf("blob tx is not supported by signer %s", sg)
+		}
 		if t.ChainID == nil {
 			if !sg.chainID.IsZero() {
 				return libcommon.Address{}, ErrInvalidChainId
