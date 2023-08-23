13bac6342f398b16e2be8e3c3e3f8a9d56692b07
yperbasis <andrey.ashikhmin@gmail.com>
2023-08-23 15:13:40 +0200

    Revert "EIP-4788: set nonce of beacon root history address to nonzero (#7952)"
    This reverts commit e4612fd6e6ef5df7230a0c2eb99021a89008168b.

--- consensus/misc/eip4788.go
+++ consensus/misc/eip4788.go
@@ -4,7 +4,6 @@ import (
 	"github.com/holiman/uint256"
 
 	libcommon "github.com/ledgerwatch/erigon-lib/common"
-
 	"github.com/ledgerwatch/erigon/consensus"
 	"github.com/ledgerwatch/erigon/core/state"
 	"github.com/ledgerwatch/erigon/core/types"
@@ -21,11 +20,4 @@ func ApplyBeaconRootEip4788(chain consensus.ChainHeaderReader, header *types.Hea
 	parentBeaconBlockRootInt := *uint256.NewInt(0).SetBytes(header.ParentBeaconBlockRoot.Bytes())
 	state.SetState(historyStorageAddress, &timestampIndex, *uint256.NewInt(header.Time))
 	state.SetState(historyStorageAddress, &rootIndex, parentBeaconBlockRootInt)
-
-	// Ensure that the historyStorageAddress has nonzero nonce to prevent wipe-out of its storage stipulated by EIP-161
-	// (when the historyStorageAddress has zero balance).
-	// See https://github.com/ethereum/EIPs/pull/7431
-	if state.GetNonce(historyStorageAddress) == 0 {
-		state.SetNonce(historyStorageAddress, 1)
-	}
 }
