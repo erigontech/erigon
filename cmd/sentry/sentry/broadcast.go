2185ebb05476d833a7a6b4d3b890e7da61852f5f
battlmonstr <battlmonstr@users.noreply.github.com>
2023-08-23 12:13:14 +0200

    send to all sentries in both BroadcastNewBlock and PropagateNewBlockHashes

--- cmd/sentry/sentry/broadcast.go
+++ cmd/sentry/sentry/broadcast.go
@@ -3,13 +3,10 @@ package sentry
 import (
 	"context"
 	"errors"
-	"math"
 	"math/big"
-	"math/rand"
 	"strings"
 	"syscall"
 
-	"github.com/ledgerwatch/erigon-lib/direct"
 	proto_sentry "github.com/ledgerwatch/erigon-lib/gointerfaces/sentry"
 	"github.com/ledgerwatch/log/v3"
 	"google.golang.org/grpc"
@@ -22,48 +19,35 @@ import (
 	"github.com/ledgerwatch/erigon/turbo/stages/headerdownload"
 )
 
-// Methods of sentry called by Core
-
-const (
-	// This is the target size for the packs of transactions or announcements. A
-	// pack can get larger than this if a single transactions exceeds this size.
-	maxTxPacketSize = 100 * 1024
-)
-
 func (cs *MultiClient) PropagateNewBlockHashes(ctx context.Context, announces []headerdownload.Announce) {
 	cs.lock.RLock()
 	defer cs.lock.RUnlock()
+
 	typedRequest := make(eth.NewBlockHashesPacket, len(announces))
 	for i := range announces {
 		typedRequest[i].Hash = announces[i].Hash
 		typedRequest[i].Number = announces[i].Number
 	}
+
 	data, err := rlp.EncodeToBytes(&typedRequest)
 	if err != nil {
 		log.Error("propagateNewBlockHashes", "err", err)
 		return
 	}
-	var req66 *proto_sentry.OutboundMessageData
-	// Send the block to a subset of our peers
-	sendToAmount := int(math.Sqrt(float64(len(cs.sentries))))
-	for i, sentry := range cs.sentries {
+
+	req66 := proto_sentry.OutboundMessageData{
+		Id:   proto_sentry.MessageId_NEW_BLOCK_HASHES_66,
+		Data: data,
+	}
+
+	for _, sentry := range cs.sentries {
 		if !sentry.Ready() {
 			continue
 		}
-		if i > sendToAmount { //TODO: send to random sentries, not just to fi
-			break
-		}
 
-		if req66 == nil {
-			req66 = &proto_sentry.OutboundMessageData{
-				Id:   proto_sentry.MessageId_NEW_BLOCK_HASHES_66,
-				Data: data,
-			}
-
-			_, err = sentry.SendMessageToAll(ctx, req66, &grpc.EmptyCallOption{})
-			if err != nil {
-				log.Error("propagateNewBlockHashes", "err", err)
-			}
+		_, err = sentry.SendMessageToAll(ctx, &req66, &grpc.EmptyCallOption{})
+		if err != nil {
+			log.Error("propagateNewBlockHashes", "err", err)
 		}
 	}
 }
@@ -71,6 +55,7 @@ func (cs *MultiClient) PropagateNewBlockHashes(ctx context.Context, announces []
 func (cs *MultiClient) BroadcastNewBlock(ctx context.Context, header *types.Header, body *types.RawBody, td *big.Int) {
 	cs.lock.RLock()
 	defer cs.lock.RUnlock()
+
 	txs := make([]types.Transaction, len(body.Transactions))
 	for i, tx := range body.Transactions {
 		var err error
@@ -79,6 +64,7 @@ func (cs *MultiClient) BroadcastNewBlock(ctx context.Context, header *types.Head
 			return
 		}
 	}
+
 	data, err := rlp.EncodeToBytes(&eth.NewBlockPacket{
 		Block: types.NewBlock(header, txs, body.Uncles, nil, body.Withdrawals),
 		TD:    td,
@@ -86,49 +72,30 @@ func (cs *MultiClient) BroadcastNewBlock(ctx context.Context, header *types.Head
 	if err != nil {
 		log.Error("broadcastNewBlock", "err", err)
 	}
-	var req66 *proto_sentry.SendMessageToRandomPeersRequest
 
 	maxPeers := uint64(1024)
-
-	if bor, ok := cs.Engine.(*bor.Bor); ok {
-		if sendAll, _ := bor.IsValidator(header); sendAll {
+	if borEngine, ok := cs.Engine.(*bor.Bor); ok {
+		if sendAll, _ := borEngine.IsValidator(header); sendAll {
 			// max peers is overloaded a zero value is interpreted as send all
 			maxPeers = 0
 		}
 	}
 
-	// Send the block to a subset of our peers
-	sendToAmount := int(math.Sqrt(float64(len(cs.sentries))))
+	req66 := proto_sentry.SendMessageToRandomPeersRequest{
+		MaxPeers: maxPeers,
+		Data: &proto_sentry.OutboundMessageData{
+			Id:   proto_sentry.MessageId_NEW_BLOCK_66,
+			Data: data,
+		},
+	}
 
-	sentries := make([]direct.SentryClient, len(cs.sentries))
-	copy(sentries, cs.sentries)
-
-	rand.Shuffle(len(sentries), func(i int, j int) {
-		sentries[i], sentries[j] = sentries[j], sentries[i]
-	})
-
-	sent := 0
-
-	for i, sentry := range sentries {
+	for _, sentry := range cs.sentries {
 		if !sentry.Ready() {
 			continue
 		}
-		sent++
 
-		if maxPeers > 0 && i > sendToAmount {
-			break
-		}
-
-		if req66 == nil {
-			req66 = &proto_sentry.SendMessageToRandomPeersRequest{
-				MaxPeers: maxPeers,
-				Data: &proto_sentry.OutboundMessageData{
-					Id:   proto_sentry.MessageId_NEW_BLOCK_66,
-					Data: data,
-				},
-			}
-		}
-		if _, err = sentry.SendMessageToRandomPeers(ctx, req66, &grpc.EmptyCallOption{}); err != nil {
+		_, err = sentry.SendMessageToRandomPeers(ctx, &req66, &grpc.EmptyCallOption{})
+		if err != nil {
 			if isPeerNotFoundErr(err) || networkTemporaryErr(err) {
 				log.Debug("broadcastNewBlock", "err", err)
 				continue
