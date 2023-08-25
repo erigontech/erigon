a6d9d26fe915838883fea516f1d4f806ed58386e
Andrew Ashikhmin <34320705+yperbasis@users.noreply.github.com>
2023-08-25 08:06:59 +0200

    Fix opSelfdestruct6780 (#8066)
    also upgrade execution-spec-tests to
    [v1.0.2](https://github.com/ethereum/execution-spec-tests/releases/tag/v1.0.2)

--- tests/exec_spec_test.go
+++ tests/exec_spec_test.go
@@ -15,15 +15,12 @@ func TestExecutionSpec(t *testing.T) {
 
 	dir := filepath.Join(".", "execution-spec-tests")
 
-	// Probably failing due to pre-Byzantium receipts
-	bt.skipLoad(`^frontier/`)
-	bt.skipLoad(`^homestead/`)
-
 	// TODO(yperbasis): fix me
-	bt.skipLoad(`^cancun/`)
+	bt.skipLoad(`^cancun/eip4844_blobs/blob_txs/`)
+	bt.skipLoad(`^cancun/eip4844_blobs/blob_txs_full/`)
+	bt.skipLoad(`^cancun/eip4844_blobs/excess_blob_gas/`)
 
-	// TODO(yperbasis): re-enable checkStateRoot
-	checkStateRoot := false
+	checkStateRoot := true
 
 	bt.walk(t, dir, func(t *testing.T, name string, test *BlockTest) {
 		// import pre accounts & construct test genesis block & state root
