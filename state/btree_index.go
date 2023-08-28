f0fc0ff5d803c05dbe2126ba19d86a79898509b2
alex.sharov <AskAlexSharov@gmail.com>
2023-08-28 09:11:52 +0700

    save

--- state/btree_index.go
+++ state/btree_index.go
@@ -12,11 +12,13 @@ import (
 	"path"
 	"path/filepath"
 	"sort"
+	"strings"
 	"time"
 
 	"github.com/c2h5oh/datasize"
 	"github.com/edsrzf/mmap-go"
 	"github.com/ledgerwatch/log/v3"
+	"github.com/spaolacci/murmur3"
 
 	"github.com/ledgerwatch/erigon-lib/common"
 	"github.com/ledgerwatch/erigon-lib/common/background"
@@ -26,7 +28,7 @@ import (
 	"github.com/ledgerwatch/erigon-lib/recsplit/eliasfano32"
 )
 
-var UseBpsTree bool = false
+var UseBpsTree bool = true
 
 const BtreeLogPrefix = "btree"
 
@@ -803,16 +805,16 @@ func BuildBtreeIndexWithDecompressor(indexPath string, kv *compress.Decompressor
 	defer ps.Delete(p)
 
 	defer kv.EnableReadAhead().DisableReadAhead()
-	//bloomPath := strings.TrimSuffix(indexPath, ".bt") + ".bl"
-	//var bloom *bloomFilter
-	//var err error
-	//if kv.Count() > 0 {
-	//	bloom, err = NewBloom(uint64(kv.Count()/2), bloomPath)
-	//	if err != nil {
-	//		return err
-	//	}
-	//}
-	//hasher := murmur3.New128WithSeed(0)
+	bloomPath := strings.TrimSuffix(indexPath, ".bt") + ".ibl"
+	var bloom *bloomFilter
+	var err error
+	if kv.Count() > 0 {
+		bloom, err = NewBloom(uint64(kv.Count()/2), bloomPath)
+		if err != nil {
+			return err
+		}
+	}
+	hasher := murmur3.New128WithSeed(0)
 
 	args := BtIndexWriterArgs{
 		IndexFile: indexPath,
@@ -838,10 +840,10 @@ func BuildBtreeIndexWithDecompressor(indexPath string, kv *compress.Decompressor
 		if err != nil {
 			return err
 		}
-		//hasher.Reset()
-		//hasher.Write(key) //nolint:errcheck
-		//hi, _ := hasher.Sum128()
-		//bloom.AddHash(hi)
+		hasher.Reset()
+		hasher.Write(key) //nolint:errcheck
+		hi, _ := hasher.Sum128()
+		bloom.AddHash(hi)
 
 		pos, _ = getter.Skip()
 		//if pos-kp == 1 {
@@ -857,11 +859,11 @@ func BuildBtreeIndexWithDecompressor(indexPath string, kv *compress.Decompressor
 	}
 	iw.Close()
 
-	//if bloom != nil {
-	//	if _, err := bloom.WriteFile(bloomPath); err != nil {
-	//		return err
-	//	}
-	//}
+	if bloom != nil {
+		if _, err := bloom.WriteFile(bloomPath); err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
