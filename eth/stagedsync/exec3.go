a549c459a92b56c78afc283379a34d60a92ef45a
alex.sharov <AskAlexSharov@gmail.com>
2023-09-07 14:24:23 +0700

    save

--- eth/stagedsync/exec3.go
+++ eth/stagedsync/exec3.go
@@ -9,7 +9,6 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
-	"runtime/debug"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -165,13 +164,13 @@ func ExecV3(ctx context.Context,
 	agg, engine := cfg.agg, cfg.engine
 	chainConfig, genesis := cfg.chainConfig, cfg.genesis
 
-	defer func() {
-		if err := recover(); err != nil {
-			log.Error("panic", "err", err)
-			debug.PrintStack()
-			panic(err)
-		}
-	}()
+	//defer func() {
+	//	if err := recover(); err != nil {
+	//		log.Error("panic", "err", err)
+	//		debug.PrintStack()
+	//		panic(err)
+	//	}
+	//}()
 
 	useExternalTx := applyTx != nil
 	if initialCycle || !useExternalTx {
