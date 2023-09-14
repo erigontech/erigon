9bc8b707ad5de1dafd919dfbf5c74936fab38133
Giulio <giulio.rebuffo@gmail.com>
2023-09-14 17:03:51 +0200

    save

--- cl/persistence/sql_migrations/migrations.go
+++ cl/persistence/sql_migrations/migrations.go
@@ -23,7 +23,6 @@ var migrations = []string{
 	`CREATE INDEX idx_parent_block_root ON beacon_indicies (parent_block_root);`,
 	`CREATE TABLE IF NOT EXISTS data_config (
 		prune_depth INTEGER NOT NULL,
-		full_blocks BOOLEAN NOT NULL
 	);`,
 }
 
