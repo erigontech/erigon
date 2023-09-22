1637b15177dd954ca5ccbb572faf9fd73e867e79
awskii <artem.tsskiy@gmail.com>
2023-09-22 23:39:50 +0200

    save

--- eth/stagedsync/stages/stages.go
+++ eth/stagedsync/stages/stages.go
@@ -39,7 +39,6 @@ var (
 	Execution           SyncStage = "Execution"       // Executing each block w/o buildinf a trie
 	Translation         SyncStage = "Translation"     // Translation each marked for translation contract (from EVM to TEVM)
 	VerkleTrie          SyncStage = "VerkleTrie"
-	PatriciaTrie        SyncStage = "PatriciaTrie"        // PatriciaTrie is a stage for evaluating HashPatriciaTrie
 	IntermediateHashes  SyncStage = "IntermediateHashes"  // Generate intermediate hashes, calculate the state root hash
 	HashState           SyncStage = "HashState"           // Apply Keccak256 to all the keys in the state
 	AccountHistoryIndex SyncStage = "AccountHistoryIndex" // Generating history index for accounts
