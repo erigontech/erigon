79860297ee965a0d0f1bd088acda03d62522744a
Giulio <giulio.rebuffo@gmail.com>
2023-09-23 21:42:43 +0200

    save

--- erigon-lib/pedersen_hash/hash.go
+++ erigon-lib/pedersen_hash/hash.go
@@ -30,6 +30,7 @@ func Hash(input1, input2 string) (string, error) {
 	in1 := C.CBytes(input1Dec)
 	in2 := C.CBytes(input2Dec)
 	var o [1024]byte
+	//nolint:gocritic
 	out := C.CBytes(o[:])
 	upIn1 := in1
 	upIn2 := in2
