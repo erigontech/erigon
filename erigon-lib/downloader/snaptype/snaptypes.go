fb18af743db7750e6216b5ba9e68a384208cfd4a
alex.sharov <AskAlexSharov@gmail.com>
2023-10-03 10:24:17 +0700

    save

--- erigon-lib/downloader/snaptype/snaptypes.go
+++ erigon-lib/downloader/snaptype/snaptypes.go
@@ -2,10 +2,8 @@ package snaptype
 
 import (
 	"net/url"
-	"strings"
 
 	"github.com/anacrolix/torrent/metainfo"
-	"github.com/ledgerwatch/log/v3"
 )
 
 // Each provider can provide only 1 WebSeed url per file
@@ -14,21 +12,3 @@ type WebSeedsFromProvider map[string]string // fileName -> Url, can be Http/Ftp
 
 type WebSeedUrls map[string]metainfo.UrlList // fileName -> []Url, can be Http/Ftp
 type TorrentUrls map[string][]*url.URL
-
-func NewWebSeedUrls(list []WebSeedsFromProvider) (wsu WebSeedUrls, tu TorrentUrls) {
-	for _, urls := range list {
-		for name, wUrl := range urls {
-			if strings.HasSuffix(name, ".torrent") {
-				uri, err := url.ParseRequestURI(wUrl)
-				if err != nil {
-					log.Debug("[downloader] url is invalid", "url", wUrl, "err", err)
-					continue
-				}
-				tu[name] = append(tu[name], uri)
-				continue
-			}
-			wsu[name] = append(wsu[name], wUrl)
-		}
-	}
-	return wsu, tu
-}
