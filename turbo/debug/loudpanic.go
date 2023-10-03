2ef000bd1e857867938e8eed6da83a4dfd04c42d
alex.sharov <AskAlexSharov@gmail.com>
2023-10-03 14:31:21 +0700

    save

--- turbo/debug/loudpanic.go
+++ turbo/debug/loudpanic.go
@@ -14,8 +14,6 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-//go:build go1.6
-
 package debug
 
 // LoudPanic panics in a way that gets all goroutine stacks printed on stderr.
