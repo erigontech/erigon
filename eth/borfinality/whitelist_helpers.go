d30ef450097ff2f59f2e6b1e7244f2e838ec79d5
Manav Darji <manavdarji.india@gmail.com>
2023-10-04 17:12:42 +0530

    use config.logger

--- eth/borfinality/whitelist_helpers.go
+++ eth/borfinality/whitelist_helpers.go
@@ -33,18 +33,18 @@ func fetchWhitelistCheckpoint(ctx context.Context, heimdallClient heimdall.IHeim
 	// fetch the latest checkpoint from Heimdall
 	checkpoint, err := heimdallClient.FetchCheckpoint(ctx, -1)
 	if err != nil {
-		log.Debug("Failed to fetch latest checkpoint for whitelisting", "err", err)
+		config.logger.Debug("Failed to fetch latest checkpoint for whitelisting", "err", err)
 		return blockNum, blockHash, errCheckpoint
 	}
 
-	log.Info("Got new checkpoint from heimdall", "start", checkpoint.StartBlock.Uint64(), "end", checkpoint.EndBlock.Uint64(), "rootHash", checkpoint.RootHash.String())
+	config.logger.Info("Got new checkpoint from heimdall", "start", checkpoint.StartBlock.Uint64(), "end", checkpoint.EndBlock.Uint64(), "rootHash", checkpoint.RootHash.String())
 
 	// Verify if the checkpoint fetched can be added to the local whitelist entry or not
 	// If verified, it returns the hash of the end block of the checkpoint. If not,
 	// it will return appropriate error.
 	hash, err := verifier.verify(ctx, config, checkpoint.StartBlock.Uint64(), checkpoint.EndBlock.Uint64(), checkpoint.RootHash.String()[2:], true)
 	if err != nil {
-		log.Warn("Failed to whitelist checkpoint", "err", err)
+		config.logger.Warn("Failed to whitelist checkpoint", "err", err)
 		return blockNum, blockHash, err
 	}
 
@@ -65,16 +65,16 @@ func fetchWhitelistMilestone(ctx context.Context, heimdallClient heimdall.IHeimd
 	// fetch latest milestone
 	milestone, err := heimdallClient.FetchMilestone(ctx)
 	if errors.Is(err, heimdall.ErrServiceUnavailable) {
-		log.Debug("Failed to fetch latest milestone for whitelisting", "err", err)
+		config.logger.Debug("Failed to fetch latest milestone for whitelisting", "err", err)
 		return num, hash, errMilestone
 	}
 
 	if err != nil {
-		log.Error("Failed to fetch latest milestone for whitelisting", "err", err)
+		config.logger.Error("Failed to fetch latest milestone for whitelisting", "err", err)
 		return num, hash, errMilestone
 	}
 
-	log.Info("Got new milestone from heimdall", "start", milestone.StartBlock.Uint64(), "end", milestone.EndBlock.Uint64(), "hash", milestone.Hash.String())
+	config.logger.Info("Got new milestone from heimdall", "start", milestone.StartBlock.Uint64(), "end", milestone.EndBlock.Uint64(), "hash", milestone.Hash.String())
 
 	num = milestone.EndBlock.Uint64()
 	hash = milestone.Hash
@@ -91,40 +91,40 @@ func fetchWhitelistMilestone(ctx context.Context, heimdallClient heimdall.IHeimd
 	return num, hash, nil
 }
 
-func fetchNoAckMilestone(ctx context.Context, heimdallClient heimdall.IHeimdallClient) (string, error) {
+func fetchNoAckMilestone(ctx context.Context, heimdallClient heimdall.IHeimdallClient, logger log.Logger) (string, error) {
 	var (
 		milestoneID string
 	)
 
 	milestoneID, err := heimdallClient.FetchLastNoAckMilestone(ctx)
 	if errors.Is(err, heimdall.ErrServiceUnavailable) {
-		log.Debug("Failed to fetch latest no-ack milestone", "err", err)
+		logger.Debug("Failed to fetch latest no-ack milestone", "err", err)
 		return milestoneID, errMilestone
 	}
 
 	if err != nil {
-		log.Error("Failed to fetch latest no-ack milestone", "err", err)
+		logger.Error("Failed to fetch latest no-ack milestone", "err", err)
 		return milestoneID, errMilestone
 	}
 
 	return milestoneID, nil
 }
 
-func fetchNoAckMilestoneByID(ctx context.Context, heimdallClient heimdall.IHeimdallClient, milestoneID string) error {
+func fetchNoAckMilestoneByID(ctx context.Context, heimdallClient heimdall.IHeimdallClient, milestoneID string, logger log.Logger) error {
 	err := heimdallClient.FetchNoAckMilestone(ctx, milestoneID)
 	if errors.Is(err, heimdall.ErrServiceUnavailable) {
-		log.Debug("Failed to fetch no-ack milestone by ID", "milestoneID", milestoneID, "err", err)
+		logger.Debug("Failed to fetch no-ack milestone by ID", "milestoneID", milestoneID, "err", err)
 		return err
 	}
 
 	// fixme: handle different types of errors
 	if errors.Is(err, ErrNotInRejectedList) {
-		log.Warn("MilestoneID not in rejected list", "milestoneID", milestoneID, "err", err)
+		logger.Warn("MilestoneID not in rejected list", "milestoneID", milestoneID, "err", err)
 		return err
 	}
 
 	if err != nil {
-		log.Error("Failed to fetch no-ack milestone by ID ", "milestoneID", milestoneID, "err", err)
+		logger.Error("Failed to fetch no-ack milestone by ID ", "milestoneID", milestoneID, "err", err)
 		return errMilestone
 	}
 
