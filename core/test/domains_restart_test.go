d250fc7b960dee0fc2bb4b6d382e7725ec12dde2
alex.sharov <AskAlexSharov@gmail.com>
2023-10-09 15:45:11 +0700

    save

--- core/test/domains_restart_test.go
+++ core/test/domains_restart_test.go
@@ -499,7 +499,7 @@ func TestCommit(t *testing.T) {
 		//err = domains.UpdateAccountData(ad, buf, nil)
 		//require.NoError(t, err)
 		//
-		err = domains.WriteAccountStorage(ad, loc1, []byte("0401"), nil)
+		err = domains.DomainPut(kv.StorageDomain, ad, loc1, []byte("0401"), nil)
 		require.NoError(t, err)
 	}
 
