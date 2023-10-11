72e30c116cdd62eb09b692ab594c26e50976fd98
alex.sharov <AskAlexSharov@gmail.com>
2023-10-11 09:04:33 +0700

    save

--- erigon-lib/kv/mdbx/kv_mdbx.go
+++ erigon-lib/kv/mdbx/kv_mdbx.go
@@ -144,7 +144,7 @@ func (opts MdbxOpts) InMem(tmpDir string) MdbxOpts {
 	}
 	opts.path = path
 	opts.inMem = true
-	opts.flags = mdbx.UtterlyNoSync | mdbx.NoMetaSync | mdbx.LifoReclaim | mdbx.NoMemInit
+	opts.flags = mdbx.UtterlyNoSync | mdbx.NoMetaSync | mdbx.NoMemInit
 	opts.growthStep = 2 * datasize.MB
 	opts.mapSize = 512 * datasize.MB
 	opts.shrinkThreshold = 0 // disable
