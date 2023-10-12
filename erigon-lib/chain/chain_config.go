f69f3a8846cbe8ec8a71ae7d64a9cdf4df1d882b
yperbasis <andrey.ashikhmin@gmail.com>
2023-10-12 23:49:38 +0200

    comments

--- erigon-lib/chain/chain_config.go
+++ erigon-lib/chain/chain_config.go
@@ -215,28 +215,28 @@ func (c *Config) GetMinBlobGasPrice() uint64 {
 	if c.MinBlobGasPrice != nil {
 		return *c.MinBlobGasPrice
 	}
-	return 1
+	return 1 // MIN_BLOB_GASPRICE (EIP-4844)
 }
 
 func (c *Config) GetMaxBlobGasPerBlock() uint64 {
 	if c.MaxBlobGasPerBlock != nil {
 		return *c.MaxBlobGasPerBlock
 	}
-	return 786432
+	return 786432 // MAX_BLOB_GAS_PER_BLOCK (EIP-4844)
 }
 
 func (c *Config) GetTargetBlobGasPerBlock() uint64 {
 	if c.TargetBlobGasPerBlock != nil {
 		return *c.TargetBlobGasPerBlock
 	}
-	return 393216
+	return 393216 // TARGET_BLOB_GAS_PER_BLOCK (EIP-4844)
 }
 
 func (c *Config) GetBlobGasPriceUpdateFraction() uint64 {
 	if c.BlobGasPriceUpdateFraction != nil {
 		return *c.BlobGasPriceUpdateFraction
 	}
-	return 3338477
+	return 3338477 // BLOB_GASPRICE_UPDATE_FRACTION (EIP-4844)
 }
 
 // CheckCompatible checks whether scheduled fork transitions have been imported
