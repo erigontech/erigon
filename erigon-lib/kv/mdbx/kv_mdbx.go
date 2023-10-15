e0f21cf69a70df33ea2236b5a0549b27362a9bfd
alex.sharov <AskAlexSharov@gmail.com>
2023-10-15 07:58:14 +0700

    save

--- erigon-lib/kv/mdbx/kv_mdbx.go
+++ erigon-lib/kv/mdbx/kv_mdbx.go
@@ -453,7 +453,7 @@ func (db *MdbxKV) Accede() bool     { return db.opts.HasFlag(mdbx.Accede) }
 // it allow open DB from another process - even if main process holding long RW transaction
 func (db *MdbxKV) openDBIs(buckets []string) error {
 	if db.ReadOnly() {
-		if err := db.View(context.Background(), func(tx kv.Tx) error {
+		return db.View(context.Background(), func(tx kv.Tx) error {
 			for _, name := range buckets {
 				if db.buckets[name].IsDeprecated {
 					continue
@@ -463,25 +463,20 @@ func (db *MdbxKV) openDBIs(buckets []string) error {
 				}
 			}
 			return tx.Commit() // when open db as read-only, commit of this RO transaction is required
-		}); err != nil {
-			return err
-		}
-	} else {
-		if err := db.Update(context.Background(), func(tx kv.RwTx) error {
-			for _, name := range buckets {
-				if db.buckets[name].IsDeprecated {
-					continue
-				}
-				if err := tx.(kv.BucketMigrator).CreateBucket(name); err != nil {
-					return err
-				}
-			}
-			return nil
-		}); err != nil {
-			return err
-		}
+		})
 	}
-	return nil
+
+	return db.Update(context.Background(), func(tx kv.RwTx) error {
+		for _, name := range buckets {
+			if db.buckets[name].IsDeprecated {
+				continue
+			}
+			if err := tx.(kv.BucketMigrator).CreateBucket(name); err != nil {
+				return err
+			}
+		}
+		return nil
+	})
 }
 
 // Close closes db
@@ -740,7 +735,7 @@ func (tx *MdbxTx) CreateBucket(name string) error {
 
 	var flags = tx.db.buckets[name].Flags
 	var nativeFlags uint
-	if !tx.db.ReadOnly() {
+	if !tx.db.ReadOnly() && !tx.db.Accede() {
 		nativeFlags |= mdbx.Create
 	}
 
