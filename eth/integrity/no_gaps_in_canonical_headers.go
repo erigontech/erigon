93181bee4313bb13d156e05f0653ed431d694774
alex.sharov <AskAlexSharov@gmail.com>
2023-10-16 12:58:52 +0700

    save

--- eth/integrity/no_gaps_in_canonical_headers.go
+++ eth/integrity/no_gaps_in_canonical_headers.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/ledgerwatch/erigon-lib/common"
 	"github.com/ledgerwatch/erigon-lib/kv"
 	"github.com/ledgerwatch/erigon/core/rawdb"
 	"github.com/ledgerwatch/erigon/eth/stagedsync/stages"
@@ -18,6 +19,11 @@ func NoGapsInCanonicalHeaders(tx kv.Tx, ctx context.Context, br services.BlockRe
 		panic(err)
 	}
 	for i := firstBlockInDB; i < lastBlockNum; i++ {
+		hash, err := rawdb.ReadCanonicalHash(tx, i)
+		if hash == (common.Hash{}) {
+			err = fmt.Errorf("canonical marker not found: %d\n", i)
+			panic(err)
+		}
 		header := rawdb.ReadHeaderByNumber(tx, i)
 		if header == nil {
 			err = fmt.Errorf("header not found: %d\n", i)
