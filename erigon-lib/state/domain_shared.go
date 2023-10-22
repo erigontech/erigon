ddbe33945a7182196a40e3f6fbd8f1419d673fd2
alex.sharov <AskAlexSharov@gmail.com>
2023-10-22 09:24:09 +0700

    save

--- erigon-lib/state/domain_shared.go
+++ erigon-lib/state/domain_shared.go
@@ -7,7 +7,6 @@ import (
 	"encoding/binary"
 	"fmt"
 	math2 "math"
-	"sort"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -621,20 +620,19 @@ func (sd *SharedDomains) ComputeCommitment(ctx context.Context, saveStateAfter, 
 
 	defer func(t time.Time) { mxCommitmentWriteTook.UpdateDuration(t) }(time.Now())
 
-	keys := make([][]byte, 0, len(branchNodeUpdates))
-	for k := range branchNodeUpdates {
-		keys = append(keys, []byte(k))
-	}
-	sort.SliceStable(keys, func(i, j int) bool { return bytes.Compare(keys[i], keys[j]) < 0 })
+	//keys := make([][]byte, 0, len(branchNodeUpdates))
+	//for k := range branchNodeUpdates {
+	//	keys = append(keys, []byte(k))
+	//}
+	//sort.SliceStable(keys, func(i, j int) bool { return bytes.Compare(keys[i], keys[j]) < 0 })
 
-	for _, key := range keys {
+	for key, update := range branchNodeUpdates {
 		select {
 		case <-ctx.Done():
 			return nil, ctx.Err()
 		default:
 		}
-		prefix := key
-		update := branchNodeUpdates[string(prefix)]
+		prefix := []byte(key)
 
 		stateValue, err := sd.LatestCommitment(prefix)
 		if err != nil {
