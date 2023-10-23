1df08b4fcc6aab39c0011e130068f7936f2b7107
alex.sharov <AskAlexSharov@gmail.com>
2023-10-23 16:09:49 +0700

    save

--- eth/stagedsync/exec3.go
+++ eth/stagedsync/exec3.go
@@ -593,7 +593,7 @@ func ExecV3(ctx context.Context,
 	var b *types.Block
 	//var err error
 
-	//fmt.Printf("exec: %d -> %d\n", blockNum, maxBlockNum)
+	fmt.Printf("exec: %d -> %d\n", blockNum, maxBlockNum)
 Loop:
 	for ; blockNum <= maxBlockNum; blockNum++ {
 		if blockNum >= blocksInSnapshots {
