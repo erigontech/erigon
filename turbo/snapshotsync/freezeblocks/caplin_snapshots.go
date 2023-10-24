a8446a43863c83ee7ca2f7bc4702cd15fe80a9f8
Giulio <giulio.rebuffo@gmail.com>
2023-10-24 13:32:20 +0200

    save

--- turbo/snapshotsync/freezeblocks/caplin_snapshots.go
+++ turbo/snapshotsync/freezeblocks/caplin_snapshots.go
@@ -127,6 +127,7 @@ func BeaconBlocksIdx(ctx context.Context, sn snaptype.FileInfo, segmentFilePath 
 		}
 		if i == 0 {
 			fmt.Println(offset, i)
+			fmt.Println(word[:140])
 		}
 		p.Processed.Add(1)
 		n := binary.PutUvarint(num, i)
