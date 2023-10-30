8a86c8c893a3be7188ed27f4d339a277403679bd
Giulio <giulio.rebuffo@gmail.com>
2023-10-30 10:36:26 +0100

    save

--- cl/persistence/format/snapshot_format/blocks.go
+++ cl/persistence/format/snapshot_format/blocks.go
@@ -20,7 +20,7 @@ var metadataSlabs = sync.Pool{
 	New: func() interface{} {
 		// 33 bytes is the size of the metadata slab
 		// body root + version
-		return [33]byte{}
+		return &[33]byte{}
 	},
 }
 
@@ -141,8 +141,8 @@ func ReadBlockFromSnapshot(r io.Reader, executionReader ExecutionBlockReaderByNu
 }
 
 func ReadRawBlockFromSnapshot(r io.Reader, out io.Writer, executionReader ExecutionBlockReaderByNumber, cfg *clparams.BeaconChainConfig) (clparams.StateVersion, error) {
-	metadataSlab := metadataSlabs.Get().([33]byte)
-	defer metadataSlabs.Put(&metadataSlab)
+	metadataSlab := metadataSlabs.Get().(*[33]byte)
+	defer metadataSlabs.Put(metadataSlab)
 	// Metadata section is just the current hardfork of the block. TODO(give it a useful purpose)
 	v, err := readMetadataForBlock(r, metadataSlab[:])
 	if err != nil {
