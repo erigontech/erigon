cc6c3f11091d25e3e4d15ba3e804d890f28787ae
alex.sharov <AskAlexSharov@gmail.com>
2023-10-30 09:57:03 +0700

    save

--- erigon-lib/downloader/webseed.go
+++ erigon-lib/downloader/webseed.go
@@ -56,9 +56,10 @@ func (d *WebSeeds) downloadWebseedTomlFromProviders(ctx context.Context, s3Provi
 		}
 		response, err := d.callHttpProvider(ctx, webSeedProviderURL)
 		if err != nil { // don't fail on error
-			d.logger.Debug("[snapshots] downloadWebseedTomlFromProviders", "err", err, "url", webSeedProviderURL.EscapedPath())
+			d.logger.Debug("[snapshots.webseed] get from HTTP provider", "err", err, "url", webSeedProviderURL.EscapedPath())
 			continue
 		}
+		d.logger.Debug("[snapshots.webseed] get from HTTP provider", "urls", len(response))
 		list = append(list, response)
 	}
 	for _, webSeedProviderURL := range s3Providers {
@@ -69,19 +70,20 @@ func (d *WebSeeds) downloadWebseedTomlFromProviders(ctx context.Context, s3Provi
 		}
 		response, err := d.callS3Provider(ctx, webSeedProviderURL)
 		if err != nil { // don't fail on error
-			d.logger.Debug("[snapshots] downloadWebseedTomlFromProviders", "err", err, "url", "s3")
+			d.logger.Debug("[snapshots.webseed] get from S3 provider", "err", err)
 			continue
 		}
+		d.logger.Debug("[snapshots.webseed] get from S3 provider", "urls", len(response))
 		list = append(list, response)
 	}
 	// add to list files from disk
 	for _, webSeedFile := range diskProviders {
 		response, err := d.readWebSeedsFile(webSeedFile)
 		if err != nil { // don't fail on error
-			_, fileName := filepath.Split(webSeedFile)
-			d.logger.Debug("[snapshots] downloadWebseedTomlFromProviders", "err", err, "file", fileName)
+			d.logger.Debug("[snapshots.webseed] get from File provider", "err", err)
 			continue
 		}
+		d.logger.Debug("[snapshots.webseed] get from File provider", "urls", len(response))
 		if len(diskProviders) > 0 {
 			d.logger.Log(d.verbosity, "[snapshots] see webseed.toml file", "files", webSeedFile)
 		}
@@ -189,12 +191,12 @@ func (d *WebSeeds) callHttpProvider(ctx context.Context, webSeedProviderUrl *url
 	request = request.WithContext(ctx)
 	resp, err := http.DefaultClient.Do(request)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("webseed.http: host=%s, url=%s, %w", webSeedProviderUrl.Hostname(), webSeedProviderUrl.EscapedPath(), err)
 	}
 	defer resp.Body.Close()
 	response := snaptype.WebSeedsFromProvider{}
 	if err := toml.NewDecoder(resp.Body).Decode(&response); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("webseed.http: host=%s, url=%s, %w", webSeedProviderUrl.Hostname(), webSeedProviderUrl.EscapedPath(), err)
 	}
 	return response, nil
 }
@@ -235,12 +237,12 @@ func (d *WebSeeds) callS3Provider(ctx context.Context, token string) (snaptype.W
 	//  }
 	resp, err := client.GetObject(ctx, &s3.GetObjectInput{Bucket: &bucketName, Key: &fileName})
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("webseed.s3: bucket=%s, %w", bucketName, err)
 	}
 	defer resp.Body.Close()
 	response := snaptype.WebSeedsFromProvider{}
 	if err := toml.NewDecoder(resp.Body).Decode(&response); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("webseed.s3: bucket=%s, %w", bucketName, err)
 	}
 	return response, nil
 }
@@ -252,7 +254,7 @@ func (d *WebSeeds) callTorrentHttpProvider(ctx context.Context, url *url.URL) ([
 	request = request.WithContext(ctx)
 	resp, err := http.DefaultClient.Do(request)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("webseed.downloadTorrentFile: host=%s, url=%s, %w", url.Hostname(), url.EscapedPath(), err)
 	}
 	defer resp.Body.Close()
 	//protect against too small and too big data
@@ -261,10 +263,10 @@ func (d *WebSeeds) callTorrentHttpProvider(ctx context.Context, url *url.URL) ([
 	}
 	res, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("webseed.downloadTorrentFile: host=%s, url=%s, %w", url.Hostname(), url.EscapedPath(), err)
 	}
 	if err = validateTorrentBytes(res, url.Path); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("webseed.downloadTorrentFile: host=%s, url=%s, %w", url.Hostname(), url.EscapedPath(), err)
 	}
 	return res, nil
 }
@@ -276,13 +278,14 @@ func validateTorrentBytes(b []byte, url string) error {
 	return nil
 }
 func (d *WebSeeds) readWebSeedsFile(webSeedProviderPath string) (snaptype.WebSeedsFromProvider, error) {
+	_, fileName := filepath.Split(webSeedProviderPath)
 	data, err := os.ReadFile(webSeedProviderPath)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("webseed.readWebSeedsFile: file=%s, %d", fileName, err)
 	}
 	response := snaptype.WebSeedsFromProvider{}
 	if err := toml.Unmarshal(data, &response); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("webseed.readWebSeedsFile: file=%s, %d", fileName, err)
 	}
 	return response, nil
 }
