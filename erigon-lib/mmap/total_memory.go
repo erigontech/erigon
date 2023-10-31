81f85e7be28ffe18df4994f4bc60bc769f946eca
battlmonstr <battlmonstr@users.noreply.github.com>
2023-10-31 09:30:37 +0100

    config: avoid OOM in docker using cgroups v2 limit (#6646)

---
+++ erigon-lib/mmap/total_memory.go
@@ -0,0 +1,21 @@
+package mmap
+
+import (
+	"github.com/ledgerwatch/erigon-lib/common/cmp"
+	"github.com/pbnjay/memory"
+	"runtime/debug"
+)
+
+func TotalMemory() uint64 {
+	mem := memory.TotalMemory()
+
+	if cgroupsMemLimit, err := cgroupsMemoryLimit(); (err == nil) && (cgroupsMemLimit > 0) {
+		mem = cmp.Min(mem, cgroupsMemLimit)
+	}
+
+	if goMemLimit := debug.SetMemoryLimit(-1); goMemLimit > 0 {
+		mem = cmp.Min(mem, uint64(goMemLimit))
+	}
+
+	return mem
+}
