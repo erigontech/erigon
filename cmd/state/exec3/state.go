3c8130f62f785a618d7d74e94ffa8c3b63eed9b6
alex.sharov <AskAlexSharov@gmail.com>
2023-11-10 20:24:34 +0300

    save

--- cmd/state/exec3/state.go
+++ cmd/state/exec3/state.go
@@ -2,6 +2,7 @@ package exec3
 
 import (
 	"context"
+	"fmt"
 	"math/big"
 	"sync"
 	"sync/atomic"
@@ -157,6 +158,7 @@ func (rw *Worker) RunTxTaskNoLock(txTask *state.TxTask) {
 	} else if !txTask.HistoryExecution && rw.historyMode.Load() {
 		rw.SetReader(state.NewStateReaderV3(rw.rs))
 	}
+	fmt.Printf("HistoryExecution: %d, %t\n", txTask.TxNum, txTask.HistoryExecution)
 
 	if rw.background && rw.chainTx == nil {
 		var err error
