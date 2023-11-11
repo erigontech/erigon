f5a55a6cc0648d37325e34a00075d6dd18574df3
alex.sharov <AskAlexSharov@gmail.com>
2023-11-12 00:27:07 +0300

    save

--- erigon-lib/state/inverted_index_test.go
+++ erigon-lib/state/inverted_index_test.go
@@ -187,21 +187,21 @@ func TestInvIndexAfterPrune(t *testing.T) {
 
 	ii.integrateFiles(sf, 0, 16)
 
-	tx, err = db.BeginRw(ctx)
-	require.NoError(t, err)
-	defer tx.Rollback()
-
-	from, to := ii.stepsRangeInDB(tx)
-	require.Equal(t, "0.1", fmt.Sprintf("%.1f", from))
-	require.Equal(t, "0.4", fmt.Sprintf("%.1f", to))
 	ic.Close()
+	err = db.Update(ctx, func(tx kv.RwTx) error {
+		from, to := ii.stepsRangeInDB(tx)
+		require.Equal(t, "0.1", fmt.Sprintf("%.1f", from))
+		require.Equal(t, "0.4", fmt.Sprintf("%.1f", to))
 
-	ic = ii.MakeContext()
-	defer ic.Close()
+		ic = ii.MakeContext()
+		defer ic.Close()
 
-	err = ic.Prune(ctx, tx, 0, 16, math.MaxUint64, logEvery)
+		err = ic.Prune(ctx, tx, 0, 16, math.MaxUint64, logEvery)
+		require.NoError(t, err)
+		return nil
+	})
 	require.NoError(t, err)
-	err = tx.Commit()
+
 	require.NoError(t, err)
 	tx, err = db.BeginRw(ctx)
 	require.NoError(t, err)
@@ -218,7 +218,7 @@ func TestInvIndexAfterPrune(t *testing.T) {
 		require.Nil(t, k, table)
 	}
 
-	from, to = ii.stepsRangeInDB(tx)
+	from, to := ii.stepsRangeInDB(tx)
 	require.Equal(t, float64(0), from)
 	require.Equal(t, float64(0), to)
 }
