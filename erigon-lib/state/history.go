19e5e23870562c774708f1bc521a6d54a50d74b6
alex.sharov <AskAlexSharov@gmail.com>
2023-11-29 11:11:32 +0700

    save

--- erigon-lib/state/history.go
+++ erigon-lib/state/history.go
@@ -641,6 +641,7 @@ func (h *History) collate(ctx context.Context, step, txFrom, txTo uint64, roTx k
 	indexBitmaps := map[string]*roaring64.Bitmap{}
 	var txKey [8]byte
 	binary.BigEndian.PutUint64(txKey[:], txFrom)
+	i := 0
 	for k, v, err := keysCursor.Seek(txKey[:]); err == nil && k != nil; k, v, err = keysCursor.Next() {
 		if err != nil {
 			return HistoryCollation{}, fmt.Errorf("iterate over %s history cursor: %w", h.filenameBase, err)
@@ -661,6 +662,13 @@ func (h *History) collate(ctx context.Context, step, txFrom, txTo uint64, roTx k
 		case <-ctx.Done():
 			return HistoryCollation{}, ctx.Err()
 		default:
+		}
+
+		i++
+		if i%1_000_000 == 0 {
+			for _, bm := range indexBitmaps {
+				bm.RunOptimize()
+			}
 		}
 	}
 	keys := make([]string, 0, len(indexBitmaps))
