8c3c83b3d0352154217df9b93ea8278f6204cc3d
alex.sharov <AskAlexSharov@gmail.com>
2023-11-29 14:38:32 +0700

    less logs

--- eth/stagedsync/exec3.go
+++ eth/stagedsync/exec3.go
@@ -329,7 +329,7 @@ func ExecV3(ctx context.Context,
 
 	commitThreshold := batchSize.Bytes()
 	progress := NewProgress(blockNum, commitThreshold, workerCount, execStage.LogPrefix(), logger)
-	logEvery := time.NewTicker(5 * time.Second)
+	logEvery := time.NewTicker(20 * time.Second)
 	defer logEvery.Stop()
 	pruneEvery := time.NewTicker(2 * time.Second)
 	defer pruneEvery.Stop()
