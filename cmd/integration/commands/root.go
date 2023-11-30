eb5396daadeec09695899bc8e893c68eaf0dde80
alex.sharov <AskAlexSharov@gmail.com>
2023-11-30 17:27:20 +0700

    save

--- cmd/integration/commands/root.go
+++ cmd/integration/commands/root.go
@@ -65,6 +65,7 @@ func dbCfg(label kv.Label, path string) kv2.MdbxOpts {
 	// integration tool don't intent to create db, then easiest way to open db - it's pass mdbx.Accede flag, which allow
 	// to read all options from DB, instead of overriding them
 	opts = opts.Accede()
+
 	if databaseVerbosity != -1 {
 		opts = opts.DBVerbosity(kv.DBVerbosityLvl(databaseVerbosity))
 	}
