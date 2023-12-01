8f505203a82b2b12f4f35de0bac25bfbfd08a3f2
alex.sharov <AskAlexSharov@gmail.com>
2023-12-01 18:00:09 +0700

    save

--- erigon-lib/downloader/downloader.go
+++ erigon-lib/downloader/downloader.go
@@ -485,7 +485,7 @@ func (d *Downloader) AddNewSeedableFile(ctx context.Context, name string) error 
 	// if we don't have the torrent file we build it if we have the .seg file
 	torrentFilePath, err := BuildTorrentIfNeed(ctx, name, d.SnapDir())
 	if err != nil {
-		return err
+		return fmt.Errorf("AddNewSeedableFile: %w", err)
 	}
 	ts, err := loadTorrent(torrentFilePath)
 	if err != nil {
