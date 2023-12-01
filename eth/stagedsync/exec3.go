aa1dcdb72e61b54b0eaf6c181888d3f4c2049920
alex.sharov <AskAlexSharov@gmail.com>
2023-12-01 15:11:29 +0700

    save

--- eth/stagedsync/exec3.go
+++ eth/stagedsync/exec3.go
@@ -259,11 +259,11 @@ func ExecV3(ctx context.Context,
 			inputTxNum = _min
 			fmt.Printf("alex2: %d -> %d\n", doms.TxNum(), _min)
 		} else {
-			fmt.Printf("alex3: %d, %d\n", doms.TxNum(), _max)
 			_min, err := rawdbv3.TxNums.Min(applyTx, blockNum)
 			if err != nil {
 				return err
 			}
+			fmt.Printf("alex3: %d, %d, %d\n", doms.TxNum(), _min, _max)
 
 			if doms.TxNum() > _min {
 				// if stopped in the middle of the block: start from beginning of block.
