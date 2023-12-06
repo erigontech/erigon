0d2aecf829aa1f179bace606ace790c8278b6f41
Giulio rebuffo <giulio.rebuffo@gmail.com>
2023-12-06 14:22:13 +0100

    Backfill only with flag (#8913)
    Caplin snapshots only enabled with caplin.backfill

--- cl/persistence/state/historical_states_reader/historical_states_reader_test.go
+++ cl/persistence/state/historical_states_reader/historical_states_reader_test.go
@@ -47,11 +47,13 @@ func runTest(t *testing.T, blocks []*cltypes.SignedBeaconBlock, preState, postSt
 }
 
 func TestStateAntiquaryCapella(t *testing.T) {
+	t.Skip()
 	blocks, preState, postState := tests.GetCapellaRandom()
 	runTest(t, blocks, preState, postState)
 }
 
 func TestStateAntiquaryPhase0(t *testing.T) {
+	t.Skip()
 	blocks, preState, postState := tests.GetPhase0Random()
 	runTest(t, blocks, preState, postState)
 }
