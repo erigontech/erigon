2c712f9fd4078b7ca48d3bd19101e94679da34ff
alex.sharov <AskAlexSharov@gmail.com>
2023-12-08 15:24:37 +0700

    save

--- erigon-lib/kv/kvcache/cache.go
+++ erigon-lib/kv/kvcache/cache.go
@@ -56,6 +56,7 @@ type Cache interface {
 	ValidateCurrentRoot(ctx context.Context, tx kv.Tx) (*CacheValidationResult, error)
 }
 type CacheView interface {
+	StateV3() bool
 	Get(k []byte) ([]byte, error)
 	GetCode(k []byte) ([]byte, error)
 }
