2370fde090a060b94b84b09e14bf471fe0704a58
alex.sharov <AskAlexSharov@gmail.com>
2023-12-13 17:32:39 +0700

    save

--- turbo/snapshotsync/freezeblocks/caplin_snapshots.go
+++ turbo/snapshotsync/freezeblocks/caplin_snapshots.go
@@ -442,6 +442,12 @@ func (s *CaplinSnapshots) BuildMissingIndices(ctx context.Context, logger log.Lo
 }
 
 func (s *CaplinSnapshots) ReadHeader(slot uint64) (*cltypes.SignedBeaconBlockHeader, uint64, libcommon.Hash, error) {
+	defer func() {
+		if rec := recover(); rec != nil {
+			panic(fmt.Sprintf("ReadHeader(%d), %s, %s\n", slot, rec, dbg.Stack()))
+		}
+	}()
+
 	view := s.View()
 	defer view.Close()
 
@@ -455,6 +461,8 @@ func (s *CaplinSnapshots) ReadHeader(slot uint64) (*cltypes.SignedBeaconBlockHea
 	if seg.idxSlot == nil {
 		return nil, 0, libcommon.Hash{}, nil
 	}
+
+	log.Info("[dbg] seg.idxSlot", "slot", slot, "seg.idxSlot.BaseDataID()", seg.idxSlot.BaseDataID(), "seg.idxSlot.KeyCount()", seg.idxSlot.KeyCount())
 	blockOffset := seg.idxSlot.OrdinalLookup(slot - seg.idxSlot.BaseDataID())
 
 	gg := seg.seg.MakeGetter()
