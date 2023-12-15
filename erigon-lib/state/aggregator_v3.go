44ef2301804f1a15b21928d87fffe030660b45e6
Alex Sharov <AskAlexSharov@gmail.com>
2023-12-15 13:06:56 +0700

    Revert "e35: minmax exclude" (#8995)
    Reverts ledgerwatch/erigon#8950

--- erigon-lib/state/aggregator_v3.go
+++ erigon-lib/state/aggregator_v3.go
@@ -912,9 +912,6 @@ func (a *AggregatorV3) recalcMaxTxNum() {
 	if txNum := a.tracesTo.endTxNumMinimax(); txNum < min {
 		min = txNum
 	}
-	if min > 0 {
-		min-- //[startTxNum, endTxNum)
-	}
 	a.minimaxTxNumInFiles.Store(min)
 }
 
