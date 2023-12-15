4bf900ff70806d413732060280a364b1d5adf3a1
alex.sharov <AskAlexSharov@gmail.com>
2023-12-15 09:19:03 +0700

    save

--- eth/backend.go
+++ eth/backend.go
@@ -1191,7 +1191,7 @@ func (s *Ethereum) setUpSnapDownloader(ctx context.Context, downloaderCfg *downl
 		s.downloaderClient, err = downloadergrpc.NewClient(ctx, s.config.Snapshot.DownloaderAddr)
 	} else {
 		// start embedded Downloader
-		s.downloader, err = downloader3.New(ctx, downloaderCfg, s.config.Dirs, s.logger, log.LvlWarn, discover)
+		s.downloader, err = downloader3.New(ctx, downloaderCfg, s.config.Dirs, s.logger, log.LvlDebug, discover)
 		if err != nil {
 			return err
 		}
