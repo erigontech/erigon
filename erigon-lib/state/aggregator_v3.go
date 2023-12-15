d7638755b9dcd6829aea66889e91521771ad2bbc
Alex Sharov <AskAlexSharov@gmail.com>
2023-12-15 09:49:59 +0700

    e35: minmax exclude (#8950)

--- erigon-lib/state/aggregator_v3.go
+++ erigon-lib/state/aggregator_v3.go
@@ -912,6 +912,9 @@ func (a *AggregatorV3) recalcMaxTxNum() {
 	if txNum := a.tracesTo.endTxNumMinimax(); txNum < min {
 		min = txNum
 	}
+	if min > 0 {
+		min-- //[startTxNum, endTxNum)
+	}
 	a.minimaxTxNumInFiles.Store(min)
 }
 
