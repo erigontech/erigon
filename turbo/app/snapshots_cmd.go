9fc4ddda75c73574cf980b961386f8f95790aa40
alex.sharov <AskAlexSharov@gmail.com>
2023-12-18 10:21:49 +0700

    "erigon snap index" support bor

--- turbo/app/snapshots_cmd.go
+++ turbo/app/snapshots_cmd.go
@@ -15,6 +15,8 @@ import (
 	"time"
 
 	"github.com/c2h5oh/datasize"
+	"github.com/ledgerwatch/erigon/core/state/temporal"
+	"github.com/ledgerwatch/erigon/core/systemcontracts"
 	"github.com/ledgerwatch/log/v3"
 	"github.com/urfave/cli/v2"
 
@@ -586,10 +588,6 @@ func doRetireCommand(cliCtx *cli.Context) error {
 	if err := br.BuildMissedIndicesIfNeed(ctx, "retire", nil, chainConfig); err != nil {
 		return err
 	}
-	//db, err = temporal.New(db, agg, systemcontracts.SystemContractCodeLookup[cc.ChainName])
-	//if err != nil {
-	//	return err
-	//}
 
 	//agg.KeepStepsInDB(0)
 
@@ -606,8 +604,10 @@ func doRetireCommand(cliCtx *cli.Context) error {
 		}
 	}
 
+	withBor := chainConfig.Bor != nil
+
 	logger.Info("Params", "from", from, "to", to, "every", every)
-	if err := br.RetireBlocks(ctx, forwardProgress, true, log.LvlInfo, nil, nil); err != nil {
+	if err := br.RetireBlocks(ctx, forwardProgress, withBor, log.LvlInfo, nil, nil); err != nil {
 		return err
 	}
 
@@ -625,7 +625,7 @@ func doRetireCommand(cliCtx *cli.Context) error {
 
 	for j := 0; j < 10_000; j++ { // prune happens by small steps, so need many runs
 		if err := db.UpdateNosync(ctx, func(tx kv.RwTx) error {
-			if err := br.PruneAncientBlocks(tx, 100, true /* includeBor */); err != nil {
+			if err := br.PruneAncientBlocks(tx, 100, withBor /* includeBor */); err != nil {
 				return err
 			}
 			return nil
@@ -638,6 +638,10 @@ func doRetireCommand(cliCtx *cli.Context) error {
 		return nil
 	}
 
+	db, err = temporal.New(db, agg, systemcontracts.SystemContractCodeLookup[chainConfig.ChainName])
+	if err != nil {
+		return err
+	}
 	logger.Info("Compute commitment")
 	if err = db.Update(ctx, func(tx kv.RwTx) error {
 		if casted, ok := tx.(kv.CanWarmupDB); ok {
