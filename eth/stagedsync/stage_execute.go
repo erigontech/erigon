ffdcc15b385f21af697704d1614ab7b09fe2f2b1
alex.sharov <AskAlexSharov@gmail.com>
2023-12-19 13:44:23 +0700

    save

--- eth/stagedsync/stage_execute.go
+++ eth/stagedsync/stage_execute.go
@@ -310,7 +310,7 @@ func reconstituteBlock(agg *libstate.AggregatorV3, db kv.RoDB, tx kv.Tx) (n uint
 var ErrTooDeepUnwind = fmt.Errorf("too deep unwind")
 
 func unwindExec3(u *UnwindState, s *StageState, tx kv.RwTx, ctx context.Context, accumulator *shards.Accumulator, logger log.Logger) (err error) {
-	fmt.Printf("unwindv3: %d -> %d\n", u.CurrentBlockNumber, u.UnwindPoint)
+	//fmt.Printf("unwindv3: %d -> %d\n", u.CurrentBlockNumber, u.UnwindPoint)
 	//txTo, err := rawdbv3.TxNums.Min(tx, u.UnwindPoint+1)
 	//if err != nil {
 	//      return err
