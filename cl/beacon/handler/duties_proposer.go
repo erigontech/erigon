9d38446e855776ad589cee90a36db72ecd0a77b9
Giulio <giulio.rebuffo@gmail.com>
2023-12-24 18:42:42 +0100

    save

--- cl/beacon/handler/duties_proposer.go
+++ cl/beacon/handler/duties_proposer.go
@@ -7,9 +7,12 @@ import (
 	"sync"
 
 	"github.com/ledgerwatch/erigon/cl/beacon/beaconhttp"
+	"github.com/ledgerwatch/erigon/cl/persistence/base_encoding"
+	state_accessors "github.com/ledgerwatch/erigon/cl/persistence/state"
 	shuffling2 "github.com/ledgerwatch/erigon/cl/phase1/core/state/shuffling"
 
 	libcommon "github.com/ledgerwatch/erigon-lib/common"
+	"github.com/ledgerwatch/erigon-lib/kv"
 )
 
 type proposerDuties struct {
@@ -26,14 +29,41 @@ func (a *ApiHandler) getDutiesProposer(r *http.Request) (*beaconResponse, error)
 	}
 
 	if epoch < a.forkchoiceStore.FinalizedCheckpoint().Epoch() {
-		return nil, beaconhttp.NewEndpointError(http.StatusBadRequest, "invalid epoch")
+		tx, err := a.indiciesDB.BeginRo(r.Context())
+		if err != nil {
+			return nil, err
+		}
+		defer tx.Rollback()
+		key := base_encoding.Encode64ToBytes4(epoch)
+		indiciesBytes, err := tx.GetOne(kv.Proposers, key[:])
+		if err != nil {
+			return nil, err
+		}
+		if len(indiciesBytes) != int(a.beaconChainCfg.SlotsPerEpoch*4) {
+			return nil, beaconhttp.NewEndpointError(http.StatusInternalServerError, "proposer duties is corrupted")
+		}
+		duties := make([]proposerDuties, a.beaconChainCfg.SlotsPerEpoch)
+		for i := uint64(0); i < a.beaconChainCfg.SlotsPerEpoch; i++ {
+			validatorIndex := binary.BigEndian.Uint32(indiciesBytes[i*4 : i*4+4])
+			var pk libcommon.Bytes48
+			pk, err := state_accessors.ReadPublicKeyByIndex(tx, uint64(validatorIndex))
+			if err != nil {
+				return nil, err
+			}
+			duties[i] = proposerDuties{
+				Pubkey:         pk,
+				ValidatorIndex: uint64(validatorIndex),
+				Slot:           epoch*a.beaconChainCfg.SlotsPerEpoch + i,
+			}
+		}
+		return newBeaconResponse(duties).withFinalized(true).withVersion(a.beaconChainCfg.GetCurrentStateVersion(epoch)), nil
 	}
 
 	// We need to compute our duties
 	state, cancel := a.syncedData.HeadState()
 	defer cancel()
 	if state == nil {
-		return nil, beaconhttp.NewEndpointError(http.StatusInternalServerError, "beacon node is syncing")
+		return nil, beaconhttp.NewEndpointError(http.StatusServiceUnavailable, "beacon node is syncing")
 
 	}
 
@@ -90,5 +120,4 @@ func (a *ApiHandler) getDutiesProposer(r *http.Request) (*beaconResponse, error)
 	wg.Wait()
 
 	return newBeaconResponse(duties).withFinalized(false).withVersion(a.beaconChainCfg.GetCurrentStateVersion(epoch)), nil
-
 }
