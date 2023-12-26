26913fa3ad9acb3eb8fe3a9260f773d244503955
alex.sharov <AskAlexSharov@gmail.com>
2023-12-26 08:11:40 +0700

    save

--- eth/integrity/e3_history_no_system_txs.go
+++ eth/integrity/e3_history_no_system_txs.go
@@ -17,58 +17,60 @@ import (
 func E3HistoryNoSystemTxs(ctx context.Context, chainDB kv.RoDB, agg *state.AggregatorV3) error {
 	g := &errgroup.Group{}
 	for j := 0; j < 255; j++ {
-		j := j
-		g.Go(func() error {
-			tx, err := chainDB.BeginRo(ctx)
-			if err != nil {
-				return err
-			}
-			defer tx.Rollback()
+		for jj := 0; jj < 255; jj++ {
+			j, jj := j, jj
+			g.Go(func() error {
+				tx, err := chainDB.BeginRo(ctx)
+				if err != nil {
+					return err
+				}
+				defer tx.Rollback()
 
-			var minStep uint64 = math.MaxUint64
-			view := agg.MakeContext()
-			defer view.Close()
-			keys, err := view.DomainRangeLatest(tx, kv.AccountsDomain, []byte{byte(j)}, []byte{byte(j + 1)}, -1)
-			if err != nil {
-				return err
-			}
-			for keys.HasNext() {
-				key, _, err := keys.Next()
+				var minStep uint64 = math.MaxUint64
+				view := agg.MakeContext()
+				defer view.Close()
+				keys, err := view.DomainRangeLatest(tx, kv.AccountsDomain, []byte{byte(j), byte(jj)}, []byte{byte(j + 1), byte(jj + 1)}, -1)
 				if err != nil {
 					return err
 				}
-				it, err := view.IndexRange(kv.AccountsHistoryIdx, key, -1, -1, order.Asc, -1, tx)
-				if err != nil {
-					return err
-				}
-				for it.HasNext() {
-					txNum, _ := it.Next()
-					ok, blockNum, err := rawdbv3.TxNums.FindBlockNum(tx, txNum)
+				for keys.HasNext() {
+					key, _, err := keys.Next()
 					if err != nil {
 						return err
 					}
-					if !ok {
-						panic(txNum)
+					it, err := view.IndexRange(kv.AccountsHistoryIdx, key, -1, -1, order.Asc, -1, tx)
+					if err != nil {
+						return err
 					}
-					if blockNum == 0 {
-						continue
-					}
-					_min, _ := rawdbv3.TxNums.Min(tx, blockNum)
-					if txNum == _min {
-						minStep = min(minStep, txNum/agg.StepSize())
-						log.Warn(fmt.Sprintf("[dbg] minStep=%d, step=%d, txNum=%d, blockNum=%d, key=%x", minStep, txNum/agg.StepSize(), txNum, blockNum, key))
-						break
-					}
+					for it.HasNext() {
+						txNum, _ := it.Next()
+						ok, blockNum, err := rawdbv3.TxNums.FindBlockNum(tx, txNum)
+						if err != nil {
+							return err
+						}
+						if !ok {
+							panic(txNum)
+						}
+						if blockNum == 0 {
+							continue
+						}
+						_min, _ := rawdbv3.TxNums.Min(tx, blockNum)
+						if txNum == _min {
+							minStep = min(minStep, txNum/agg.StepSize())
+							log.Warn(fmt.Sprintf("[dbg] minStep=%d, step=%d, txNum=%d, blockNum=%d, key=%x", minStep, txNum/agg.StepSize(), txNum, blockNum, key))
+							break
+						}
 
+					}
+					if casted, ok := it.(kv.Closer); ok {
+						casted.Close()
+					}
 				}
-				if casted, ok := it.(kv.Closer); ok {
-					casted.Close()
-				}
-			}
-			log.Warn(fmt.Sprintf("[dbg] step=%d", minStep))
+				log.Warn(fmt.Sprintf("[dbg] step=%d", minStep))
 
-			return nil
-		})
+				return nil
+			})
+		}
 	}
 	if err := g.Wait(); err != nil {
 		return err
