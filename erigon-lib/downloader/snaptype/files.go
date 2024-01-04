fe6b01286cb3a8041205c899f899dbe1bb54b196
alex.sharov <AskAlexSharov@gmail.com>
2024-01-04 11:45:43 +0700

    save

--- erigon-lib/downloader/snaptype/files.go
+++ erigon-lib/downloader/snaptype/files.go
@@ -257,9 +257,6 @@ func ParseDir(dir string, version uint8) (res []FileInfo, err error) {
 		res = append(res, meta)
 	}
 	slices.SortFunc(res, func(i, j FileInfo) int {
-		if i.Version != j.Version {
-			return cmp.Compare(i.Version, j.Version)
-		}
 		if i.From != j.From {
 			return cmp.Compare(i.From, j.From)
 		}
@@ -268,6 +265,9 @@ func ParseDir(dir string, version uint8) (res []FileInfo, err error) {
 		}
 		if i.T != j.T {
 			return cmp.Compare(i.T, j.T)
+		}
+		if i.Version != j.Version {
+			return cmp.Compare(i.Version, j.Version)
 		}
 		return cmp.Compare(i.Ext, j.Ext)
 	})
