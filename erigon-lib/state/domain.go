4a478b615a32adb36b95bd4abd38d5ce5259be9b
alex.sharov <AskAlexSharov@gmail.com>
2024-01-05 11:07:05 +0700

    save

--- erigon-lib/state/domain.go
+++ erigon-lib/state/domain.go
@@ -1679,8 +1679,8 @@ func (dc *DomainContext) getLatestFromFiles(filekey []byte) (v []byte, found boo
 			if traceGetLatest == dc.d.filenameBase {
 				fmt.Printf("GetLatest(%s, %x) -> not found in file %s (false positive existence idx)\n", dc.d.filenameBase, filekey, dc.files[i].src.decompressor.FileName())
 				fmt.Printf("bloom false-positive probability: %s, %f, a-b=%d-%d\n", dc.files[i].src.existence.FileName, dc.files[i].src.existence.filter.FalsePosititveProbability(), A, B)
-				m := bloomfilter.OptimalM(dc.files[i].src.existence.filter.N()*2, 0.01)
-				k := bloomfilter.OptimalK(m, dc.files[i].src.existence.filter.N()*2)
+				m := bloomfilter.OptimalM(dc.files[i].src.existence.filter.N()*10, 0.01)
+				k := bloomfilter.OptimalK(m, dc.files[i].src.existence.filter.N()*10)
 				fmt.Printf("recommended: m=%d,k=%d, have m=%d,k=%d\n", m, k, dc.files[i].src.existence.filter.M(), dc.files[i].src.existence.filter.K())
 			}
 			//	LatestStateReadGrindNotFound.ObserveDuration(t)
