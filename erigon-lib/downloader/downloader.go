74bb8dd5acc81f4ecc2bd48bdbc904be930db57d
alex.sharov <AskAlexSharov@gmail.com>
2024-01-08 11:23:25 +0700

    save

--- erigon-lib/downloader/downloader.go
+++ erigon-lib/downloader/downloader.go
@@ -489,25 +489,16 @@ func getPeersRatesForlogs(peersOfThisFile []*torrent.PeerConn, fName string) ([]
 }
 
 func ScheduleVerifyFile(ctx context.Context, t *torrent.Torrent, completePieces *atomic.Uint64) error {
-	select {
-	case <-ctx.Done():
-		return ctx.Err()
-	case <-t.GotInfo():
-	}
-
-	defer func(tt time.Time) { fmt.Printf("downloader.go:498: %s, %s\n", time.Since(tt), t.Name()) }(time.Now())
-
 	for i := 0; i < t.NumPieces(); i++ {
-		i := i
+		t.Piece(i).VerifyData()
+
+		completePieces.Add(1)
+
 		select {
 		case <-ctx.Done():
 			return ctx.Err()
 		default:
 		}
-
-		t.Piece(i).VerifyData()
-
-		completePieces.Add(1)
 	}
 	return nil
 }
