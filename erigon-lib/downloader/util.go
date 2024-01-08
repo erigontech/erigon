942dd7ace5bcfd2b052891be0f1562891f791d5b
alex.sharov <AskAlexSharov@gmail.com>
2024-01-08 11:05:49 +0700

    save

--- erigon-lib/downloader/util.go
+++ erigon-lib/downloader/util.go
@@ -425,7 +425,6 @@ func VerifyFileFailFast(ctx context.Context, t *torrent.Torrent, root string, co
 		if !good {
 			return fmt.Errorf("hash mismatch at piece %d", i)
 		}
-		//fmt.Printf("%d: %v: %v\n", i, p.Hash(), good)
 		completePieces.Add(1)
 	}
 	return nil
