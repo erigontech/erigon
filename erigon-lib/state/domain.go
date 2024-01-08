cf1203efb31e132376b974223f35b6bb99814bce
alex.sharov <AskAlexSharov@gmail.com>
2024-01-08 12:28:32 +0700

    more debug info

--- erigon-lib/state/domain.go
+++ erigon-lib/state/domain.go
@@ -481,10 +481,13 @@ func (d *Domain) removeFilesAfterStep(lowerBound uint64, readonly bool) {
 		return true
 	})
 	for _, item := range toDelete {
-		log.Debug(fmt.Sprintf("[snapshots] delete %s, because step %d has not enough files (was not complete). stack: %s", item.decompressor.FileName(), lowerBound, dbg.Stack()))
 		d.files.Delete(item)
 		if !readonly {
+			log.Debug(fmt.Sprintf("[snapshots] delete %s, because step %d has not enough files (was not complete). stack: %s", item.decompressor.FileName(), lowerBound, dbg.Stack()))
 			item.closeFilesAndRemove()
+		} else {
+			log.Debug(fmt.Sprintf("[snapshots] closing %s, because step %d has not enough files (was not complete). stack: %s", item.decompressor.FileName(), lowerBound, dbg.Stack()))
+
 		}
 	}
 
