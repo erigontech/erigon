6e6a7b54ea9d366ce8c0b336d9e86183f7358274
yperbasis <andrey.ashikhmin@gmail.com>
2024-01-10 10:38:33 +0100

    Fix DenebForkEpoch for Chiado

--- cl/clparams/config.go
+++ cl/clparams/config.go
@@ -883,7 +883,7 @@ func chiadoConfig() BeaconChainConfig {
 	cfg.BellatrixForkVersion = 0x0200006f
 	cfg.CapellaForkEpoch = 244224
 	cfg.CapellaForkVersion = 0x0300006f
-	cfg.DenebForkEpoch = 8265728
+	cfg.DenebForkEpoch = 516608
 	cfg.DenebForkVersion = 0x0400006f
 	cfg.TerminalTotalDifficulty = "231707791542740786049188744689299064356246512"
 	cfg.DepositContractAddress = "0xb97036A26259B7147018913bD58a774cf91acf25"
