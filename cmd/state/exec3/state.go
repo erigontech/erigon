7a182eab7728a5a0a2b318011f2aad4bdc6f0b98
alex.sharov <AskAlexSharov@gmail.com>
2024-01-14 12:48:19 +0700

    save

--- cmd/state/exec3/state.go
+++ cmd/state/exec3/state.go
@@ -179,7 +179,7 @@ func (rw *Worker) RunTxTaskNoLock(txTask *state.TxTask) {
 
 	rw.ibs.Reset()
 	ibs := rw.ibs
-	ibs.SetTrace(true)
+	//ibs.SetTrace(true)
 
 	rules := txTask.Rules
 	var err error
@@ -206,7 +206,7 @@ func (rw *Worker) RunTxTaskNoLock(txTask *state.TxTask) {
 		syscall := func(contract libcommon.Address, data []byte, ibs *state.IntraBlockState, header *types.Header, constCall bool) ([]byte, error) {
 			return core.SysCallContract(contract, data, rw.chainConfig, ibs, header, rw.engine, constCall /* constCall */)
 		}
-		ibs.SetTrace(true)
+		//ibs.SetTrace(true)
 		rw.engine.Initialize(rw.chainConfig, rw.chain, header, ibs, syscall, rw.logger)
 		txTask.Error = ibs.FinalizeTx(rules, noop)
 	case txTask.Final:
