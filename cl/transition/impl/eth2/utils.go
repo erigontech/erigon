9738593e1dfdcd9138d204324e71e084b893e064
Giulio <giulio.rebuffo@gmail.com>
2024-01-18 00:25:31 +0100

    save

--- cl/transition/impl/eth2/utils.go
+++ cl/transition/impl/eth2/utils.go
@@ -8,7 +8,6 @@ import (
 	"github.com/ledgerwatch/erigon/cl/abstract"
 	"github.com/ledgerwatch/erigon/cl/cltypes"
 	"github.com/ledgerwatch/erigon/cl/utils"
-	"github.com/ledgerwatch/erigon/core/types"
 )
 
 const VERSIONED_HASH_VERSION_KZG byte = byte(1)
@@ -22,40 +21,6 @@ func kzgCommitmentToVersionedHash(kzgCommitment *cltypes.KZGCommitment) (libcomm
 	copy(versionedHash[:], buf)
 
 	return versionedHash, nil
-}
-
-func txPeekBlobVersionedHashes(txBytes []byte) []libcommon.Hash {
-	if txBytes[0] != types.BlobTxType {
-		return []libcommon.Hash{}
-	}
-
-	messageOffset := 1 + binary.LittleEndian.Uint32(txBytes[1:5])
-
-	/*
-		https://gist.github.com/protolambda/23bd106b66f6d4bb854ce46044aa3ca3
-		chain_id: 32 bytes
-		nonce: 8 bytes
-		priority_fee_per_gas: 32 bytes
-		max_basefee_per_gas: 32 bytes
-		gas: 8 bytes
-		to: 4 bytes - offset to B (relative to A)
-		value: 32 bytes
-		data: 4 bytes - offset to C (relative to A)
-		access_list: 4 bytes - offset to D (relative to A)
-		max_fee_per_blob_gas: 32 bytes
-		blob_versioned_hashes: 4 bytes - offset to E (relative to A)
-	*/
-	// field offset: 32 + 8 + 32 + 32 + 8 + 4 + 32 + 4 + 4 + 32 = 188
-	blobVersionedHashes := messageOffset + binary.LittleEndian.Uint32(txBytes[messageOffset+188:messageOffset+192])
-
-	versionedHashes := make([]libcommon.Hash, len(txBytes[blobVersionedHashes:])/32)
-	for pos, i := blobVersionedHashes, 0; int(pos) < len(txBytes) && i < len(versionedHashes); pos += 32 {
-		versionedHash := libcommon.BytesToHash(txBytes[pos : pos+32])
-		versionedHashes[i] = versionedHash
-		i++
-	}
-
-	return versionedHashes
 }
 
 func computeSigningRootEpoch(epoch uint64, domain []byte) (libcommon.Hash, error) {
