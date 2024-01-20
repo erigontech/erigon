502bf7fece68c9d35568fd8c213f58ae8aff2b04
alex.sharov <AskAlexSharov@gmail.com>
2024-01-20 16:30:20 +0700

    save

--- turbo/snapshotsync/freezeblocks/bor_snapshots.go
+++ turbo/snapshotsync/freezeblocks/bor_snapshots.go
@@ -1148,8 +1148,10 @@ func (m *BorMerger) Merge(ctx context.Context, snapshots *BorRoSnapshots, mergeR
 				continue
 			}
 
-			if err := onDelete(toMerge[t]); err != nil {
-				return err
+			if onDelete != nil {
+				if err := onDelete(toMerge[t]); err != nil {
+					return err
+				}
 			}
 
 		}
