28f9b0eacb906a820af99a15adf8cc481a7f40d4
alex.sharov <AskAlexSharov@gmail.com>
2024-01-26 18:03:09 +0700

    save

--- eth/integrity/snap_blocks_read.go
+++ eth/integrity/snap_blocks_read.go
@@ -3,12 +3,17 @@ package integrity
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/ledgerwatch/erigon-lib/kv"
 	"github.com/ledgerwatch/erigon/turbo/services"
+	"github.com/ledgerwatch/log/v3"
 )
 
 func SnapBlocksRead(db kv.RoDB, blockReader services.FullBlockReader, ctx context.Context) error {
+	logEvery := time.NewTicker(10 * time.Second)
+	defer logEvery.Stop()
+
 	maxBlockNum := blockReader.Snapshots().SegmentsMax()
 	for i := uint64(0); i < maxBlockNum; i += 10_000 {
 		if err := db.View(ctx, func(tx kv.Tx) error {
@@ -22,6 +27,14 @@ func SnapBlocksRead(db kv.RoDB, blockReader services.FullBlockReader, ctx contex
 			return nil
 		}); err != nil {
 			return err
+		}
+
+		select {
+		case <-ctx.Done():
+			return nil
+		case <-logEvery.C:
+			log.Info("[integrity] SnapBlocksRead", "blockNum", fmt.Sprintf("%dK/%dK", i/1000, maxBlockNum/1000))
+		default:
 		}
 	}
 	return nil
