547f9e21a5afecc68ae4b89999f050f202657eaa
bayram98 <bayramguwanjow@gmail.com>
2024-01-30 00:54:51 +0900

    fix: resolve how to initiate blobSidecar

--- cl/cltypes/blob_sidecar.go
+++ cl/cltypes/blob_sidecar.go
@@ -1,8 +1,6 @@
 package cltypes
 
 import (
-	"fmt"
-
 	libcommon "github.com/ledgerwatch/erigon-lib/common"
 	"github.com/ledgerwatch/erigon-lib/types/clonable"
 	"github.com/ledgerwatch/erigon/cl/cltypes/solid"
@@ -11,6 +9,7 @@ import (
 )
 
 const MAX_BLOBS_PER_BLOCK = 4096
+const BRANCH_SIZE = 17
 const (
 	MAX_REQUEST_BLOCKS_DENEB              = 128
 	MAX_REQUEST_BLOB_SIDECARS             = MAX_REQUEST_BLOCKS_DENEB * MAX_BLOBS_PER_BLOCK
@@ -28,43 +27,31 @@ type BlobSidecar struct {
 }
 
 func NewBlobSidecar(index uint64, blob Blob, kzgCommitment libcommon.Bytes48, kzgProof libcommon.Bytes48, signedBlockHeader *SignedBeaconBlockHeader, commitmentInclusionProof solid.HashVectorSSZ) *BlobSidecar {
-	fmt.Println("NewBlobSidecar:", index, kzgCommitment, kzgProof, signedBlockHeader, commitmentInclusionProof)
-
 	return &BlobSidecar{
 		Index:                    index,
 		Blob:                     blob,
 		KzgCommitment:            kzgCommitment,
 		KzgProof:                 kzgProof,
-		SignedBlockHeader:        signedBlockHeader,
-		CommitmentInclusionProof: commitmentInclusionProof,
+		SignedBlockHeader:        new(SignedBeaconBlockHeader),
+		CommitmentInclusionProof: solid.NewHashVector(BRANCH_SIZE),
 	}
 }
 
-// EncodeSSZ encodes the BlobSidecar in SSZ format
 func (b *BlobSidecar) EncodeSSZ(buf []byte) ([]byte, error) {
-	fmt.Println("EncodeSSZ:", b.SignedBlockHeader)
 	return ssz2.MarshalSSZ(buf, b.getSchema()...)
 }
 
-// encodingSize
 func (b *BlobSidecar) EncodingSizeSSZ() int {
 	size := 8 + 4096*32 + 48 + 48
-	fmt.Println("EncodingSizeSSZ:", size)
-	fmt.Println("EncodingSizeSSZ:", b.SignedBlockHeader)
-	fmt.Println("EncodingSizeSSZ:", b.CommitmentInclusionProof)
-
 	if b.SignedBlockHeader != nil {
-		fmt.Println("Signed:", b.SignedBlockHeader)
 		size += b.SignedBlockHeader.EncodingSizeSSZ()
 	}
 	if b.CommitmentInclusionProof != nil {
-		fmt.Println("Signed:", b.CommitmentInclusionProof)
 		size += b.CommitmentInclusionProof.EncodingSizeSSZ()
 	}
 	return size
 }
 
-// DecodeSSZ decodes BlobSidecar in SSZ format.
 func (b *BlobSidecar) DecodeSSZ(buf []byte, version int) error {
 	return ssz2.UnmarshalSSZ(buf, version, b.getSchema()...)
 }
@@ -100,22 +87,18 @@ func NewBlobIdentifier(blockRoot libcommon.Hash, index uint64) *BlobIdentifier {
 	}
 }
 
-// EncodeSSZ encodes the BlobIdentifier in SSZ format
 func (b *BlobIdentifier) EncodeSSZ(buf []byte) ([]byte, error) {
 	return ssz2.MarshalSSZ(buf, b.getSchema()...)
 }
 
-// encodingSize
 func (b *BlobIdentifier) EncodingSizeSSZ() int {
 	return 32 + 8
 }
 
-// DecodeSSZ decodes BlobIdentifier in SSZ format.
 func (b *BlobIdentifier) DecodeSSZ(buf []byte, version int) error {
 	return ssz2.UnmarshalSSZ(buf, version, b.getSchema()...)
 }
 
-// HashSSZ encodes the BlobIdentifier in SSZ tree format.
 func (b *BlobIdentifier) HashSSZ() ([32]byte, error) {
 	return merkle_tree.HashTreeRoot(b.getSchema()...)
 }
