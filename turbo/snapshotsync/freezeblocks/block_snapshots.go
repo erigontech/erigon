d639da733be80c770ac4a2f04a6a3059516d38f2
alex.sharov <AskAlexSharov@gmail.com>
2024-01-30 14:32:24 +0700

    save

--- turbo/snapshotsync/freezeblocks/block_snapshots.go
+++ turbo/snapshotsync/freezeblocks/block_snapshots.go
@@ -1483,7 +1483,7 @@ func (br *BlockRetire) RetireBlocks(ctx context.Context, minBlockNum uint64, max
 
 	if includeBor {
 		// "bor snaps" can be behind "block snaps", it's ok: for example because of `kill -9` in the middle of merge
-		log.Warn("[dbg] d0", "br.blockReader.FrozenBorBlocks()", br.blockReader.FrozenBorBlocks(), "br.blockReader.FrozenBlocks()", br.blockReader.FrozenBlocks())
+		log.Warn("[dbg] d0", "br.blockReader.FrozenBorBlocks()", br.blockReader.FrozenBorBlocks(), "minBlockNum", minBlockNum)
 		for br.blockReader.FrozenBorBlocks() < minBlockNum {
 			log.Warn("[dbg] d1", "br.blockReader.FrozenBorBlocks()", br.blockReader.FrozenBorBlocks(), "maxBlockNum", maxBlockNum)
 			ok, err := br.retireBorBlocks(ctx, br.blockReader.FrozenBorBlocks(), maxBlockNum, lvl, seedNewSnapshots, onDeleteSnapshots)
@@ -1498,6 +1498,7 @@ func (br *BlockRetire) RetireBlocks(ctx context.Context, minBlockNum uint64, max
 
 	var ok, okBor bool
 	for {
+		log.Warn("[dbg] d20", "br.blockReader.FrozenBlocks()", br.blockReader.FrozenBlocks())
 		if frozen := br.blockReader.FrozenBlocks(); frozen > minBlockNum {
 			minBlockNum = frozen
 		}
