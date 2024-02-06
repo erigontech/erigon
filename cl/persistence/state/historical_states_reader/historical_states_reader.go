947842c6db33214c2fc0110ccd34f94b9226ca4f
Giulio <giulio.rebuffo@gmail.com>
2024-02-06 16:59:23 +0100

    save

--- cl/persistence/state/historical_states_reader/historical_states_reader.go
+++ cl/persistence/state/historical_states_reader/historical_states_reader.go
@@ -579,6 +579,7 @@ func (r *HistoricalStatesReader) readPendingEpochs(tx kv.Tx, slot uint64, curren
 	if err != nil {
 		return nil, nil, err
 	}
+	fmt.Println(previousEpochAttestations, currentEpochAttestations)
 	previousEpochAttestations.Truncate(int(previousEpochAttestationsLength))
 	currentEpochAttestations.Truncate(int(currentEpochAttestationsLength))
 	return currentEpochAttestations, previousEpochAttestations, nil
