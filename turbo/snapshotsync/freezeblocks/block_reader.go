4676874d890ded05947bd835cca4545731e9e287
alex.sharov <AskAlexSharov@gmail.com>
2024-02-06 09:30:18 +0700

    save

--- turbo/snapshotsync/freezeblocks/block_reader.go
+++ turbo/snapshotsync/freezeblocks/block_reader.go
@@ -1094,15 +1094,14 @@ func (r *BlockReader) EventsByBlock(ctx context.Context, tx kv.Tx, hash common.H
 	var buf []byte
 	result := []rlp.RawValue{}
 	fmt.Printf("[dbg] EventsByBlock01 last: %s, %s\n", segments[len(segments)-1].seg.FileName(), segments[0].seg.FileName())
-	for i := len(segments) - 1; i >= 0; i-- {
+	for i := len(segments); i >= 0; i-- {
 		sn := segments[i]
 		if sn.from > blockHeight {
 			fmt.Printf("[dbg] EventsByBlock2 skip1: %s, %d\n", sn.seg.FileName(), sn.from)
 			continue
 		}
 		if sn.to <= blockHeight {
-			fmt.Printf("[dbg] EventsByBlock2 skip2: %s, %d\n", sn.seg.FileName(), sn.to)
-			continue
+			break
 		}
 		if sn.IdxBorTxnHash == nil {
 			fmt.Printf("[dbg] EventsByBlock2 skip3: %s, %d\n", sn.seg.FileName(), sn.to)
