1f96fed66ae3422d0f39c0f73870442760ed40f8
alex.sharov <AskAlexSharov@gmail.com>
2024-02-06 11:33:56 +0700

    save

--- turbo/snapshotsync/freezeblocks/bor_snapshots.go
+++ turbo/snapshotsync/freezeblocks/bor_snapshots.go
@@ -481,6 +481,7 @@ RETRY:
 		word, nextPos = g.Next(word[:0])
 		i++
 		if first || !bytes.Equal(blockNumBuf[:], word[length.Hash:length.Hash+length.BlockNum]) {
+			fmt.Printf("add: %x, %d\n", word[:length.Hash], i)
 			if err = rs.AddKey(word[:length.Hash], offset); err != nil {
 				return err
 			}
