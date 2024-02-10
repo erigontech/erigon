4eda12a7199803b6e4be13d6dd13bd6cdc49f8fc
Giulio <giulio.rebuffo@gmail.com>
2024-02-10 14:00:09 +0100

    save

--- cl/persistence/state/historical_states_reader/historical_states_reader.go
+++ cl/persistence/state/historical_states_reader/historical_states_reader.go
@@ -673,7 +673,7 @@ func (r *HistoricalStatesReader) ReadPartecipations(tx kv.Tx, slot uint64) (*sol
 				return false
 			}
 			prevIdxs := isCurrentEpoch && currentEpoch != prevEpoch
-			fmt.Println(attestingIndicies)
+			fmt.Println(participationFlagsIndicies)
 			// apply the flags
 			for _, idx := range attestingIndicies {
 				for _, flagIndex := range participationFlagsIndicies {
