ce78ba69e538e0d632babd78b59e206c4c2c8835
Mark Holt <135143369+mh0lt@users.noreply.github.com>
2024-02-11 14:37:53 +0000

    Add lock for downloader version map (#9419)
    Fix for:
    
    fatal error: concurrent map read and map write
    
    goroutine 213 [running]:
    github.com/ledgerwatch/erigon-lib/downloader.initSnapshotLock.func1()
    github.com/ledgerwatch/erigon-lib@v1.0.0/downloader/downloader.go:329
    +0x3a7

--- erigon-lib/downloader/downloader.go
+++ erigon-lib/downloader/downloader.go
@@ -286,6 +286,7 @@ func initSnapshotLock(ctx context.Context, cfg *downloadercfg.Cfg, db kv.RoDB, l
 	// is no matching version just use the one discovered for the file
 
 	versionedCfg := map[snaptype.Version]*snapcfg.Cfg{}
+	versionedCfgLock := sync.Mutex{}
 
 	snapDir := cfg.Dirs.Snap
 
@@ -328,12 +329,19 @@ func initSnapshotLock(ctx context.Context, cfg *downloadercfg.Cfg, db kv.RoDB, l
 						downloadMap.Set(fileInfo.Name(), snapcfg.PreverifiedItem{Name: fileInfo.Name(), Hash: hash})
 					}
 				} else {
-					versioned, ok := versionedCfg[fileInfo.Version]
+					versioned := func() *snapcfg.Cfg {
+						versionedCfgLock.Lock()
+						defer versionedCfgLock.Unlock()
 
-					if !ok {
-						versioned = snapcfg.VersionedCfg(cfg.ChainName, fileInfo.Version, fileInfo.Version)
-						versionedCfg[fileInfo.Version] = versioned
-					}
+						versioned, ok := versionedCfg[fileInfo.Version]
+
+						if !ok {
+							versioned = snapcfg.VersionedCfg(cfg.ChainName, fileInfo.Version, fileInfo.Version)
+							versionedCfg[fileInfo.Version] = versioned
+						}
+
+						return versioned
+					}()
 
 					hashBytes, err := localHashBytes(ctx, fileInfo, db, logger)
 
