f36e50532ad89bb363b5da1668da8497ca86f6eb
racytech <racytech0x0@gmail.com>
2024-02-12 22:29:33 +0600

    import PoS blocks impl

--- eth/backend.go
+++ eth/backend.go
@@ -1483,6 +1483,10 @@ func (s *Ethereum) TxpoolServer() txpool_proto.TxpoolServer {
 	return s.txPoolGrpcServer
 }
 
+func (s *Ethereum) ExecutionModule() *eth1.EthereumExecutionModule {
+	return s.eth1ExecutionServer
+}
+
 // RemoveContents is like os.RemoveAll, but preserve dir itself
 func RemoveContents(dir string) error {
 	d, err := os.Open(dir)
