a31828f52f18aa14ef7c781390487bc0f1ff6945
Giulio <giulio.rebuffo@gmail.com>
2024-02-13 01:01:44 +0100

    save

--- cl/phase1/stages/clstages.go
+++ cl/phase1/stages/clstages.go
@@ -442,15 +442,18 @@ func ConsensusClStages(ctx context.Context,
 							return err
 						case blocks := <-respCh:
 							for _, block := range blocks.Data {
+								fmt.Println("Hey")
 								// we can ignore this error because the block would not process if the hashssz failed
 								blockRoot, _ := block.HashSSZ()
 								if _, ok := cfg.forkChoice.GetHeader(blockRoot); ok {
 									continue
 								}
+								start := time.Now()
 								if err := processBlock(tx, block, true, true); err != nil {
 									log.Error("bad blocks segment received", "err", err)
 									continue
 								}
+								fmt.Println("block", block.Block.Slot, "took", time.Since(start))
 
 								// publish block to event handler
 								cfg.emitter.Publish("block", map[string]any{
