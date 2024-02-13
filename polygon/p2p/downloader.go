5c2994080106237a6aa8be41b18e60196a97180e
taratorio <94537774+taratorio@users.noreply.github.com>
2024-02-13 12:18:42 +0000

    tidy todos and reqRespTimeout

--- polygon/p2p/downloader.go
+++ polygon/p2p/downloader.go
@@ -14,6 +14,8 @@ import (
 	"github.com/ledgerwatch/erigon/rlp"
 )
 
+const reqRespTimeout = 5 * time.Second
+
 var invalidDownloadHeadersRangeErr = errors.New("invalid download headers range")
 
 type RequestIdGenerator func() uint64
@@ -58,9 +60,14 @@ func (d *downloader) DownloadHeaders(ctx context.Context, start uint64, end uint
 	d.messageListener.RegisterBlockHeaders66(observer)
 	defer d.messageListener.UnregisterBlockHeaders66(observer)
 
-	ctx, cancel := context.WithTimeout(ctx, 30*time.Second)
+	ctx, cancel := context.WithTimeout(ctx, reqRespTimeout)
 	defer cancel()
 	g, ctx := errgroup.WithContext(ctx)
+
+	//
+	// TODO chunk request into smaller ranges if needed to fit in the 10 MiB response size
+	// TODO peer should return <= amount, check for > amount and penalize peer
+	//
 
 	g.Go(func() error {
 		for {
@@ -86,39 +93,6 @@ func (d *downloader) DownloadHeaders(ctx context.Context, start uint64, end uint
 				if pkt.RequestId != requestId {
 					continue
 				}
-
-				//
-				// TODO what sort of validation if any should we do here? check sentry_multi_client & refactor if necessary
-				// Looks like sentry_multi_client does not actually do any validations
-				// corresponding to the spec at this layer (p2p message processing).
-				//
-				// We should make a decision which of the following validations we want to do here
-				// and which at higher layers.
-				//
-				// From the spec:
-				/*
-					The validity of block headers depends on the context in which they are used. For a single block header,
-					only the validity of the proof-of-work seal (mix-digest, block-nonce) can be verified. When a header is
-					used to extend the client's local chain, or multiple headers are processed in sequence during chain
-					synchronization, the following rules apply:
-
-					- Headers must form a chain where block numbers are consecutive and the parent-hash of each header
-					matches the hash of the preceding header.
-					- When extending the locally-stored chain, implementations must also verify that the values of
-					difficulty, gas-limit and time are within the bounds of protocol rules given in the Yellow Paper.
-					- The gas-used header field must be less than or equal to the gas-limit.
-					- basefee-per-gas must be present in headers after the London hard fork. It must be absent for earlier
-					blocks. This rule was added by EIP-1559.
-					- For PoS blocks after The Merge, ommers-hash must be the empty keccak256 hash since no ommer headers
-					can exist.
-					- withdrawals-root must be present in headers after the Shanghai fork. The field must be absent for
-					blocks before the fork. This rule was added by EIP-4895.
-				*/
-
-				//
-				// TODO also limit on size of message 10MiB and penalize?
-				// TODO should we check for too large of a range for download headers?
-				//
 
 				headers = pkt.BlockHeadersPacket
 				return nil
