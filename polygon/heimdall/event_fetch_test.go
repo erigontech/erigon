9fc37a3e5d579d503538b4205c0e59c014226016
Mark Holt <mark@distributed.vision>
2024-02-22 12:34:43 +0000

    dec start number

--- polygon/heimdall/event_fetch_test.go
+++ polygon/heimdall/event_fetch_test.go
@@ -13,13 +13,13 @@ func TestOver50EventBlockFetch(t *testing.T) {
 
 	// block      := 48077376
 	// block time := Sep-28-2023 08:13:58 AM
-	events, err := heimdallClient.FetchStateSyncEvents(context.Background(), 2774291, time.Unix(1695888838-128, 0), 0)
+	events, err := heimdallClient.FetchStateSyncEvents(context.Background(), 2774290, time.Unix(1695888838-128, 0), 0)
 
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if len(events) != 114 {
-		t.Fatal("Unexpected event count, exptected: 114, got:", len(events))
+	if len(events) != 112 {
+		t.Fatal("Unexpected event count, exptected: 113, got:", len(events))
 	}
 }
