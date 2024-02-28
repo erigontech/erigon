d7700d8b31e57f4e0d834ca55c6998d0afd2f7a4
Somnath Banerjee <snb895@outlook.com>
2024-02-28 14:26:08 +0400

    Move default prune.r check inside the block

--- ethdb/prune/storage_mode.go
+++ ethdb/prune/storage_mode.go
@@ -259,12 +259,12 @@ func EnsureNotChanged(tx kv.GetPut, pruneMode Mode) (Mode, error) {
 		return pruneMode, err
 	}
 
-	// Don't change from previous default as default for Receipts pruning has now changed
-	if pruneMode.Receipts.useDefaultValue() {
-		pruneMode.Receipts = pm.Receipts
-	}
-
 	if pruneMode.Initialised {
+		// Don't change from previous default as default for Receipts pruning has now changed
+		if pruneMode.Receipts.useDefaultValue() {
+			pruneMode.Receipts = pm.Receipts
+		}
+
 		// If storage mode is not explicitly specified, we take whatever is in the database
 		if !reflect.DeepEqual(pm, pruneMode) {
 			if bytes.Equal(pm.Receipts.dbType(), kv.PruneTypeOlder) && bytes.Equal(pruneMode.Receipts.dbType(), kv.PruneTypeBefore) {
