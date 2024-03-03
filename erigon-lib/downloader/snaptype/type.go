75e01dac1173d947ca33ed704929cd5343f3d745
Giulio rebuffo <giulio.rebuffo@gmail.com>
2024-03-03 18:21:03 +0100

    Caplin: Added blob's snapshots to Caplin (#9573)
    Antiquates blobs to snapshot format.

--- erigon-lib/downloader/snaptype/type.go
+++ erigon-lib/downloader/snaptype/type.go
@@ -45,7 +45,8 @@ var Indexes = struct {
 	TxnHash2BlockNum,
 	BorTxnHash,
 	BorSpanId,
-	BeaconBlockSlot Index
+	BeaconBlockSlot,
+	BlobSidecarSlot Index
 }{
 	Unknown:          -1,
 	HeaderHash:       0,
@@ -55,6 +56,7 @@ var Indexes = struct {
 	BorTxnHash:       4,
 	BorSpanId:        5,
 	BeaconBlockSlot:  6,
+	BlobSidecarSlot:  7,
 }
 
 func (i Index) Offset() int {
@@ -82,6 +84,8 @@ func (i Index) String() string {
 		return Enums.BorSpans.String()
 	case Indexes.BeaconBlockSlot:
 		return Enums.BeaconBlocks.String()
+	case Indexes.BlobSidecarSlot:
+		return Enums.BlobSidecars.String()
 	default:
 		panic(fmt.Sprintf("unknown index: %d", i))
 	}
@@ -189,6 +193,7 @@ var Enums = struct {
 	BorEvents,
 	BorSpans,
 	BeaconBlocks Enum
+	BlobSidecars Enum
 }{
 	Unknown:      -1,
 	Headers:      0,
@@ -197,6 +202,7 @@ var Enums = struct {
 	BorEvents:    3,
 	BorSpans:     4,
 	BeaconBlocks: 5,
+	BlobSidecars: 6,
 }
 
 func (ft Enum) String() string {
@@ -213,6 +219,8 @@ func (ft Enum) String() string {
 		return "borspans"
 	case Enums.BeaconBlocks:
 		return "beaconblocks"
+	case Enums.BlobSidecars:
+		return "blobsidecars"
 	default:
 		panic(fmt.Sprintf("unknown file type: %d", ft))
 	}
@@ -232,6 +240,8 @@ func (ft Enum) Type() Type {
 		return BorSpans
 	case Enums.BeaconBlocks:
 		return BeaconBlocks
+	case Enums.BlobSidecars:
+		return BlobSidecars
 	default:
 		return nil
 	}
@@ -260,6 +270,8 @@ func ParseEnum(s string) (Enum, bool) {
 		return Enums.BorSpans, true
 	case "beaconblocks":
 		return Enums.BeaconBlocks, true
+	case "blobsidecars":
+		return Enums.BlobSidecars, true
 	default:
 		return Enums.Unknown, false
 	}
@@ -318,6 +330,14 @@ var (
 			MinSupported: 1,
 		},
 		indexes: []Index{Indexes.BeaconBlockSlot},
+	}
+	BlobSidecars = snapType{
+		enum: Enums.BlobSidecars,
+		versions: Versions{
+			Current:      1,
+			MinSupported: 1,
+		},
+		indexes: []Index{Indexes.BlobSidecarSlot},
 	}
 
 	BlockSnapshotTypes = []Type{Headers, Bodies, Transactions}
