1115d3615c1f59084114715e662ceb0b100c15e3
Giulio <giulio.rebuffo@gmail.com>
2024-03-03 16:35:36 +0100

    save

--- eth/stagedsync/stage_snapshots.go
+++ eth/stagedsync/stage_snapshots.go
@@ -234,6 +234,7 @@ func DownloadAndIndexSnapshotsIfNeed(s *StageState, ctx context.Context, tx kv.R
 		}
 	} else {
 		if err := snapshotsync.WaitForDownloader(ctx, s.LogPrefix(), cfg.historyV3, cfg.blobs, cstate, cfg.agg, tx, cfg.blockReader, &cfg.chainConfig, cfg.snapshotDownloader, s.state.StagesIdsList()); err != nil {
+			return err
 		}
 	}
 	// It's ok to notify before tx.Commit(), because RPCDaemon does read list of files by gRPC (not by reading from db)
