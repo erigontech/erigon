72ce8bfb44cbe97361aa040bd0892e0e017c4fdd
bayram98 <bayramguwanjow@gmail.com>
2024-03-04 18:17:44 +0900

    feat: add validation

--- cl/phase1/forkchoice/on_operations.go
+++ cl/phase1/forkchoice/on_operations.go
@@ -2,6 +2,7 @@ package forkchoice
 
 import (
 	"bytes"
+	"encoding/binary"
 	"errors"
 	"fmt"
 
@@ -207,7 +208,83 @@ func (f *ForkChoiceStore) OnSignedContributionAndProof(signedChange *cltypes.Sig
 		f.emitters.Publish("contribution_and_proof", signedChange)
 		return nil
 	}
-	// TODO: implement the validation logic for the handler
+
+	contributionAndProof := signedChange.Message
+	signiture := signedChange.Signature
+	fmt.Println("Printing Debug for contributionAndProof", contributionAndProof)
+	fmt.Println("Printing Debug for signiture", signiture)
+
+	//Take lock as we interact with state.
+	s := f.syncedDataManager.HeadState()
+	if s == nil {
+		return fmt.Errorf("no head state avaible")
+	}
+	// https: //github.com/ethereum/consensus-specs/blob/dev/specs/altair/p2p-interface.md#sync_committee_contribution_and_proof
+
+	// [IGNORE] The contribution's slot is for the current slot (with a MAXIMUM_GOSSIP_CLOCK_DISPARITY allowance), i.e. contribution.slot == current_slot.
+	maximumGossipClockDisparity := uint64(clparams.NetworkConfigs[clparams.MainnetNetwork].MaximumGossipClockDisparity)
+	currentSlot := s.Slot()
+	if contributionAndProof.Contribution.Slot != currentSlot && contributionAndProof.Contribution.Slot > currentSlot+maximumGossipClockDisparity {
+		return nil
+	}
+
+	// [REJECT] The subcommittee index is in the allowed range, i.e. contribution.subcommittee_index < SYNC_COMMITTEE_SUBNET_COUNT.
+	if contributionAndProof.Contribution.SubcommitteeIndex >= clparams.MainnetBeaconConfig.SyncCommitteeSubnetCount {
+		return fmt.Errorf("subcommittee index is out of range")
+	}
+
+	// [REJECT] The contribution has participants -- that is, any(contribution.aggregation_bits).
+	aggregationBits := contributionAndProof.Contribution.AggregationBits
+	if aggregationBits == nil {
+		return fmt.Errorf("contribution has no participants")
+	}
+
+	found := false
+	for _, bit := range contributionAndProof.Contribution.AggregationBits {
+		if bit != 0 {
+			found = true
+			break
+		}
+	}
+	if aggregationBits == nil || !found {
+		return fmt.Errorf("contribution has no participants")
+	}
+
+	// [REJECT] contribution_and_proof.selection_proof selects the validator as an aggregator for the slot -- i.e. is_sync_committee_aggregator(contribution_and_proof.selection_proof) returns True.
+	selectionProof := contributionAndProof.SelectionProof
+	if len(selectionProof) != 96 {
+		return fmt.Errorf("incorrect signiture length")
+	}
+	modulo := utils.Max64(1, f.beaconCfg.SyncCommitteeSize/f.beaconCfg.SyncCommitteeSubnetCount/f.beaconCfg.TargetAggregatorsPerSyncSubcommittee)
+	hashSignature := utils.Sha256(selectionProof[:])
+	if binary.LittleEndian.Uint64(hashSignature[:8])%modulo != 0 {
+		return fmt.Errorf("selects the validator as an aggregator")
+	}
+
+	// [REJECT] The aggregator's validator index is in the declared subcommittee of the current sync committee
+	// -- i.e. state.validators[contribution_and_proof.aggregator_index].pubkey in get_sync_subcommittee_pubkeys(state, contribution.subcommittee_index).
+	committee := s.CurrentSyncCommittee()
+	committeeKeys := committee.GetCommittee()
+	aggregatorPubKey, err := s.ValidatorPublicKey(int(contributionAndProof.AggregatorIndex))
+	if err != nil {
+		return err
+	}
+	inSubcommittee := false
+	for _, pubKey := range committeeKeys {
+		if bytes.Equal(pubKey[:], aggregatorPubKey[:]) {
+			inSubcommittee = true
+			break
+		}
+	}
+	if !inSubcommittee {
+		return fmt.Errorf("aggregator's validator index is not in subcommittee")
+	}
+
+	// [IGNORE] A valid sync committee contribution with equal slot, beacon_block_root and subcommittee_index whose aggregation_bits is non-strict superset has not already been seen.
+	// [IGNORE] The sync committee contribution is the first valid contribution received for the aggregator with index contribution_and_proof.aggregator_index for the slot contribution.slot and subcommittee index contribution.subcommittee_index (this requires maintaining a cache of size SYNC_COMMITTEE_SIZE for this topic that can be flushed after each slot).
+	// [REJECT] The contribution_and_proof.selection_proof is a valid signature of the SyncAggregatorSelectionData derived from the contribution by the validator with index contribution_and_proof.aggregator_index.
+	// [REJECT] The aggregator signature, signed_contribution_and_proof.signature, is valid.
+	// [REJECT] The aggregate signature is valid for the message beacon_block_root and aggregate pubkey derived from the participation info in aggregation_bits for the subcommittee specified by the contribution.subcommittee_index.
 
 	// Insert in the pool
 	f.operationsPool.SignedContributionAndProofPool.Insert(signedChange.Signature, signedChange)
