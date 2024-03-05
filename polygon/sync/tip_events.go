31c9782bbf297d86d4621ec38c40042be0ad89f8
battlmonstr <battlmonstr@users.noreply.github.com>
2024-03-05 15:25:42 +0100

    polygon/sync: event channel (#9596)

--- polygon/sync/tip_events.go
+++ polygon/sync/tip_events.go
@@ -1,9 +1,7 @@
 package sync
 
 import (
-	"container/list"
 	"context"
-	"sync"
 	"time"
 
 	"github.com/ledgerwatch/erigon/core/types"
@@ -69,12 +67,7 @@ func (e Event) AsNewSpan() EventNewSpan {
 }
 
 type TipEvents struct {
-	events    chan Event
-	pollDelay time.Duration
-
-	queue      *list.List
-	queueCap   int
-	queueMutex sync.Mutex
+	events *EventChannel[Event]
 
 	p2pService      p2p.Service
 	heimdallService heimdall.HeimdallNoStore
@@ -84,43 +77,18 @@ func NewTipEvents(
 	p2pService p2p.Service,
 	heimdallService heimdall.HeimdallNoStore,
 ) *TipEvents {
-	return &TipEvents{
-		events:    make(chan Event),
-		pollDelay: time.Second,
+	eventsCapacity := uint(1000) // more than 3 milestones
 
-		queue:    list.New(),
-		queueCap: 1000, // more than 3 milestones
+	return &TipEvents{
+		events: NewEventChannel[Event](eventsCapacity, time.Second),
 
 		p2pService:      p2pService,
 		heimdallService: heimdallService,
 	}
 }
 
-func (te *TipEvents) Events() chan Event {
-	return te.events
-}
-
-func (te *TipEvents) pushEvent(e Event) {
-	te.queueMutex.Lock()
-	defer te.queueMutex.Unlock()
-
-	if te.queue.Len() == te.queueCap {
-		te.queue.Remove(te.queue.Front())
-	}
-
-	te.queue.PushBack(e)
-}
-
-func (te *TipEvents) takeEvent() (Event, bool) {
-	te.queueMutex.Lock()
-	defer te.queueMutex.Unlock()
-
-	if elem := te.queue.Front(); elem != nil {
-		e := te.queue.Remove(elem).(Event)
-		return e, true
-	} else {
-		return Event{}, false
-	}
+func (te *TipEvents) Events() <-chan Event {
+	return te.events.Events()
 }
 
 func (te *TipEvents) Run(ctx context.Context) error {
@@ -129,7 +97,7 @@ func (te *TipEvents) Run(ctx context.Context) error {
 			return
 		}
 		block := message.Decoded.Block
-		te.pushEvent(Event{
+		te.events.PushEvent(Event{
 			Type: EventTypeNewHeader,
 			newHeader: EventNewHeader{
 				NewHeader: block.Header(),
@@ -143,7 +111,7 @@ func (te *TipEvents) Run(ctx context.Context) error {
 		if message.Decoded == nil {
 			return
 		}
-		te.pushEvent(Event{
+		te.events.PushEvent(Event{
 			Type: EventTypeNewHeaderHashes,
 			newHeaderHashes: EventNewHeaderHashes{
 				NewHeaderHashes: *message.Decoded,
@@ -154,7 +122,7 @@ func (te *TipEvents) Run(ctx context.Context) error {
 	defer newBlockHashesObserverCancel()
 
 	err := te.heimdallService.OnMilestoneEvent(ctx, func(milestone *heimdall.Milestone) {
-		te.pushEvent(Event{
+		te.events.PushEvent(Event{
 			Type:         EventTypeNewMilestone,
 			newMilestone: milestone,
 		})
@@ -164,7 +132,7 @@ func (te *TipEvents) Run(ctx context.Context) error {
 	}
 
 	err = te.heimdallService.OnSpanEvent(ctx, func(span *heimdall.Span) {
-		te.pushEvent(Event{
+		te.events.PushEvent(Event{
 			Type:    EventTypeNewSpan,
 			newSpan: span,
 		})
@@ -173,23 +141,5 @@ func (te *TipEvents) Run(ctx context.Context) error {
 		return err
 	}
 
-	// pump events from the ring buffer to the events channel
-	for {
-		e, ok := te.takeEvent()
-		if !ok {
-			pollDelayTimer := time.NewTimer(te.pollDelay)
-			select {
-			case <-pollDelayTimer.C:
-				continue
-			case <-ctx.Done():
-				return ctx.Err()
-			}
-		}
-
-		select {
-		case te.events <- e:
-		case <-ctx.Done():
-			return ctx.Err()
-		}
-	}
+	return te.events.Run(ctx)
 }
