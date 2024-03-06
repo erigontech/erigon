7bdfb63c897eb8ba5a1cfca84b761267f0d439f3
alex.sharov <AskAlexSharov@gmail.com>
2024-03-06 11:00:58 +0700

    save

--- turbo/snapshotsync/freezeblocks/block_snapshots.go
+++ turbo/snapshotsync/freezeblocks/block_snapshots.go
@@ -828,7 +828,7 @@ func buildMissedIndices(logPrefix string, ctx context.Context, dirs datadir.Dirs
 				defer notifySegmentIndexingFinished(info.Name())
 				defer ps.Delete(p)
 				if err := buildIdx(gCtx, info, chainConfig, tmpDir, p, log.LvlInfo, logger); err != nil {
-					return fmt.Errorf("%s: %w", segment.FileName(), err)
+					return fmt.Errorf("%s: %w", info.Name(), err)
 				}
 				return nil
 			})
