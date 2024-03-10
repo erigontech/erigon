8bb29e7c0eb84f902f16bdf52134869e7570f396
pcw109550 <pcw109550@gmail.com>
2024-03-10 22:57:31 +0900

    Use pointer receiver type because of atomic.Value
    We must use receiver type for tx methods because if not,
    value receivers will copy the struct containing atomic.Value field.
    TransactionMisc struct is embedded to to every tx type, so
    we must use pointer receiver for every tx methods.

--- core/types/blob_tx.go
+++ core/types/blob_tx.go
@@ -24,7 +24,7 @@ type BlobTx struct {
 }
 
 // copy creates a deep copy of the transaction data and initializes all fields.
-func (stx BlobTx) copy() *BlobTx {
+func (stx *BlobTx) copy() *BlobTx {
 	cpy := &BlobTx{
 		DynamicFeeTransaction: *stx.DynamicFeeTransaction.copy(),
 		MaxFeePerBlobGas:      new(uint256.Int),
@@ -37,17 +37,17 @@ func (stx BlobTx) copy() *BlobTx {
 	return cpy
 }
 
-func (stx BlobTx) Type() byte { return BlobTxType }
+func (stx *BlobTx) Type() byte { return BlobTxType }
 
-func (stx BlobTx) GetBlobHashes() []libcommon.Hash {
+func (stx *BlobTx) GetBlobHashes() []libcommon.Hash {
 	return stx.BlobVersionedHashes
 }
 
-func (stx BlobTx) GetBlobGas() uint64 {
+func (stx *BlobTx) GetBlobGas() uint64 {
 	return fixedgas.BlobGasPerBlob * uint64(len(stx.BlobVersionedHashes))
 }
 
-func (stx BlobTx) AsMessage(s Signer, baseFee *big.Int, rules *chain.Rules) (Message, error) {
+func (stx *BlobTx) AsMessage(s Signer, baseFee *big.Int, rules *chain.Rules) (Message, error) {
 	msg := Message{
 		nonce:      stx.Nonce,
 		gasLimit:   stx.Gas,
@@ -92,7 +92,7 @@ func (stx *BlobTx) Sender(signer Signer) (libcommon.Address, error) {
 	return addr, nil
 }
 
-func (stx BlobTx) Hash() libcommon.Hash {
+func (stx *BlobTx) Hash() libcommon.Hash {
 	if hash := stx.hash.Load(); hash != nil {
 		return *hash.(*libcommon.Hash)
 	}
@@ -114,7 +114,7 @@ func (stx BlobTx) Hash() libcommon.Hash {
 	return hash
 }
 
-func (stx BlobTx) SigningHash(chainID *big.Int) libcommon.Hash {
+func (stx *BlobTx) SigningHash(chainID *big.Int) libcommon.Hash {
 	return prefixedRlpHash(
 		BlobTxType,
 		[]interface{}{
@@ -132,7 +132,7 @@ func (stx BlobTx) SigningHash(chainID *big.Int) libcommon.Hash {
 		})
 }
 
-func (stx BlobTx) payloadSize() (payloadSize, nonceLen, gasLen, accessListLen, blobHashesLen int) {
+func (stx *BlobTx) payloadSize() (payloadSize, nonceLen, gasLen, accessListLen, blobHashesLen int) {
 	payloadSize, nonceLen, gasLen, accessListLen = stx.DynamicFeeTransaction.payloadSize()
 	// size of MaxFeePerBlobGas
 	payloadSize++
@@ -156,7 +156,7 @@ func encodeBlobVersionedHashes(hashes []libcommon.Hash, w io.Writer, b []byte) e
 	return nil
 }
 
-func (stx BlobTx) encodePayload(w io.Writer, b []byte, payloadSize, nonceLen, gasLen, accessListLen, blobHashesLen int) error {
+func (stx *BlobTx) encodePayload(w io.Writer, b []byte, payloadSize, nonceLen, gasLen, accessListLen, blobHashesLen int) error {
 	// prefix
 	if err := EncodeStructSizePrefix(payloadSize, w, b); err != nil {
 		return err
@@ -232,7 +232,7 @@ func (stx BlobTx) encodePayload(w io.Writer, b []byte, payloadSize, nonceLen, ga
 	return nil
 }
 
-func (stx BlobTx) EncodeRLP(w io.Writer) error {
+func (stx *BlobTx) EncodeRLP(w io.Writer) error {
 	payloadSize, nonceLen, gasLen, accessListLen, blobHashesLen := stx.payloadSize()
 	// size of struct prefix and TxType
 	envelopeSize := 1 + rlp2.ListPrefixLen(payloadSize) + payloadSize
@@ -252,7 +252,7 @@ func (stx BlobTx) EncodeRLP(w io.Writer) error {
 	return nil
 }
 
-func (stx BlobTx) MarshalBinary(w io.Writer) error {
+func (stx *BlobTx) MarshalBinary(w io.Writer) error {
 	payloadSize, nonceLen, gasLen, accessListLen, blobHashesLen := stx.payloadSize()
 	var b [33]byte
 	// encode TxType
