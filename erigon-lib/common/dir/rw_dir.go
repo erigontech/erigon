6fec9e677aa84aa878c819fdc8bbc988677f362c
alex.sharov <AskAlexSharov@gmail.com>
2024-03-10 14:54:47 +0700

    save

--- erigon-lib/common/dir/rw_dir.go
+++ erigon-lib/common/dir/rw_dir.go
@@ -143,7 +143,6 @@ func ListFiles(dir string, extensions ...string) (paths []string, err error) {
 			}
 		}
 		if !match {
-			fmt.Printf("[dbg] ListFiles skip %s\n", f.Name())
 			continue
 		}
 		paths = append(paths, filepath.Join(dir, f.Name()))
