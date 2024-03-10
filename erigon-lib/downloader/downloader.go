ade09c9b19d809aa88e2edf04d50acbf8fcc7046
alex.sharov <AskAlexSharov@gmail.com>
2024-03-10 15:46:07 +0700

    save

--- erigon-lib/downloader/downloader.go
+++ erigon-lib/downloader/downloader.go
@@ -1145,7 +1145,7 @@ func (d *Downloader) mainLoop(silent bool) error {
 }
 
 func localHashCompletionCheck(ctx context.Context, t *torrent.Torrent, fileInfo snaptype.FileInfo, statusChan chan downloadStatus) ([]byte, bool) {
-	defer func(t time.Time) { fmt.Printf("downloader.go:1148: %s\n", time.Since(t)) }(time.Now())
+	defer func(t time.Time) { fmt.Printf("downloader.go:1148: %s, %s\n", time.Since(t), fileInfo.Name()) }(time.Now())
 	localHash, err := fileHashBytes(ctx, fileInfo)
 
 	if err == nil {
