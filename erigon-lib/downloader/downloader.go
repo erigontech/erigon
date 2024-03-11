359eb19991ce921499ffae92e5129a5de65e6763
alex.sharov <AskAlexSharov@gmail.com>
2024-03-11 09:52:51 +0700

    save

--- erigon-lib/downloader/downloader.go
+++ erigon-lib/downloader/downloader.go
@@ -697,6 +697,9 @@ func (d *Downloader) mainLoop(silent bool) error {
 
 								go func(fileInfo snaptype.FileInfo, infoHash infohash.T, length int64, completionTime time.Time) {
 									checkGroup.Go(func() error {
+										defer func(t time.Time) {
+											fmt.Printf("downloader.go:699: fileHashBytes %s, %s\n", time.Since(t), fileInfo.Name())
+										}(time.Now())
 										fileHashBytes, _ := fileHashBytes(d.ctx, fileInfo)
 
 										if bytes.Equal(infoHash.Bytes(), fileHashBytes) {
