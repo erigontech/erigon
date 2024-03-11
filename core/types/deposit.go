690262f0e2dbf70c3afafc570f9fa240663e31a0
racytech <racytech0x0@gmail.com>
2024-03-11 18:17:38 +0500

    deposits: encode/decode RLP

---
+++ core/types/deposit.go
@@ -0,0 +1,130 @@
+package types
+
+import (
+	"fmt"
+	"io"
+
+	libcommon "github.com/ledgerwatch/erigon-lib/common"
+	"github.com/ledgerwatch/erigon/rlp"
+)
+
+const (
+	pLen = 48 // pubkey size
+	wLen = 32 // withdrawalCredentials size
+	sLen = 96 // signature size
+)
+
+type Deposit struct {
+	Pubkey                [pLen]byte
+	WithdrawalCredentials libcommon.Hash
+	Amount                uint64
+	Signature             [sLen]byte
+	Index                 uint64
+}
+
+func (d *Deposit) EncodingSize() (encodingSize int) {
+	// // pubkey
+	// encodingSize++       // 0x80..0xb7 1
+	// encodingSize += pLen // string itself 48
+	// // withdrawalCredentials
+	// encodingSize++       // 0x80..0xb7 1
+	// encodingSize += wLen // string itself 32
+	// amount
+	encodingSize++
+	encodingSize += rlp.IntLenExcludingHead(d.Amount)
+	// // signature
+	// encodingSize++       // 0xb8..0xbf 1
+	// encodingSize++       // string size (1 byte) 1
+	// encodingSize += sLen // 96
+	// index
+	encodingSize++
+	encodingSize += rlp.IntLenExcludingHead(d.Index)
+
+	encodingSize += 1 + 48 + 1 + 32 + 1 + 1 + 96
+	return encodingSize
+}
+
+func (d *Deposit) EncodeRLP(w io.Writer) error {
+	encodingSize := d.EncodingSize()
+
+	var b [128]byte
+	if err := EncodeStructSizePrefix(encodingSize, w, b[:]); err != nil {
+		return err
+	}
+
+	b[0] = 0x80 + pLen
+	if _, err := w.Write(b[:1]); err != nil {
+		return err
+	}
+	if _, err := w.Write(d.Pubkey[:]); err != nil {
+		return err
+	}
+
+	b[0] = 0x80 + wLen
+	if _, err := w.Write(b[:1]); err != nil {
+		return err
+	}
+	if _, err := w.Write(d.WithdrawalCredentials[:]); err != nil {
+		return err
+	}
+
+	if err := rlp.EncodeInt(d.Amount, w, b[:]); err != nil {
+		return err
+	}
+
+	b[0] = 0xb8
+	if _, err := w.Write(b[:1]); err != nil {
+		return err
+	}
+	b[0] = sLen
+	if _, err := w.Write(b[:1]); err != nil {
+		return err
+	}
+	if _, err := w.Write(d.Signature[:]); err != nil {
+		return err
+	}
+
+	return rlp.EncodeInt(d.Index, w, b[:])
+}
+
+func (d *Deposit) DecodeRLP(s *rlp.Stream) error {
+
+	_, err := s.List()
+	if err != nil {
+		return fmt.Errorf("list err: %s", err)
+	}
+	var b []byte
+	if b, err = s.Bytes(); err != nil {
+		return fmt.Errorf("deposit: pubkey decode error: %w", err)
+	}
+	if len(b) != pLen {
+		return fmt.Errorf("deposit: pubkey error: expected length is %d, got: %d", pLen, len(b))
+	}
+	copy(d.Pubkey[:], b)
+
+	if b, err = s.Bytes(); err != nil {
+		return fmt.Errorf("deposit: withdrawalCridentials decode error: %w", err)
+	}
+	if len(b) != wLen {
+		return fmt.Errorf("deposit: withdrawalCridentials error: expected length is %d, got: %d", pLen, len(b))
+	}
+	copy(d.WithdrawalCredentials[:], b)
+
+	if d.Amount, err = s.Uint(); err != nil {
+		return fmt.Errorf("deposit: amount error:: %w", err)
+	}
+
+	if b, err = s.Bytes(); err != nil {
+		return fmt.Errorf("deposit: signature decode error: %w", err)
+	}
+	if len(b) != sLen {
+		return fmt.Errorf("deposit: signature error: expected length is %d, got: %d", pLen, len(b))
+	}
+	copy(d.Signature[:], b)
+
+	if d.Index, err = s.Uint(); err != nil {
+		return fmt.Errorf("deposit: amount error:: %w", err)
+	}
+
+	return s.ListEnd()
+}
---
+++ core/types/deposit_test.go
@@ -0,0 +1,126 @@
+package types
+
+import (
+	"bytes"
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/ledgerwatch/erigon/rlp"
+)
+
+const RUNS = 100
+
+func randBytes(rnd *rand.Rand, size int) []byte {
+	arr := make([]byte, 128)
+
+	for i := 0; i < size; i++ {
+		arr[i] = byte(rnd.Intn(256))
+	}
+
+	return arr
+}
+
+func randDeposit(rnd *rand.Rand) *Deposit {
+	d := &Deposit{
+		Pubkey:                [pLen]byte(randBytes(rnd, pLen)),
+		WithdrawalCredentials: [wLen]byte(randBytes(rnd, wLen)),
+		Amount:                rand.Uint64(),
+		Signature:             [sLen]byte(randBytes(rnd, sLen)),
+		Index:                 rand.Uint64(),
+	}
+
+	return d
+}
+
+func isEqualBytes(a, b []byte) bool {
+
+	for i, _ := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func TestDepositEncodeDecodeRLP(t *testing.T) {
+	src := rand.NewSource(time.Now().UnixNano())
+	rnd := rand.New(src)
+
+	var enc *Deposit
+
+	for i := 0; i < RUNS; i++ {
+		enc = randDeposit(rnd)
+		var buf bytes.Buffer
+		if err := enc.EncodeRLP(&buf); err != nil {
+			t.Error(err)
+		}
+
+		dec := Deposit{}
+
+		s := rlp.NewStream(bytes.NewReader(buf.Bytes()), 0)
+		if err := dec.DecodeRLP(s); err != nil {
+			t.Error(err)
+		}
+
+		if !isEqualBytes(enc.Pubkey[:], dec.Pubkey[:]) {
+			t.Errorf("deposit decode: not equal Pubkeys: expected: %v, got: %v", enc.Pubkey, dec.Pubkey)
+		}
+
+		if !isEqualBytes(enc.WithdrawalCredentials[:], dec.WithdrawalCredentials[:]) {
+			t.Errorf("deposit decode: not equal WithdrawalCridentials: expected: %v, got: %v", enc.WithdrawalCredentials, dec.WithdrawalCredentials)
+		}
+
+		if enc.Amount != dec.Amount {
+			t.Errorf("deposit decode: not equal Amounts: expected: %v, got: %v", enc.Amount, dec.Amount)
+		}
+
+		if !isEqualBytes(enc.Signature[:], dec.Signature[:]) {
+			t.Errorf("deposit decode: not equal Signature: expected: %v, got: %v", enc.Signature, dec.Signature)
+		}
+
+		if enc.Index != dec.Index {
+			t.Errorf("deposit decode: not equal Indexes: expected: %v, got: %v", enc.Index, dec.Index)
+		}
+	}
+
+	for i := 0; i < RUNS; i++ {
+		enc = randDeposit(rnd)
+		enc.Amount = uint64(i)
+		enc.Index = uint64(i)
+		var buf bytes.Buffer
+		if err := enc.EncodeRLP(&buf); err != nil {
+			t.Error(err)
+		}
+		fmt.Println("BUF LENGTH: ", buf.Len())
+		dec := Deposit{}
+
+		s := rlp.NewStream(bytes.NewReader(buf.Bytes()), 0)
+		if err := dec.DecodeRLP(s); err != nil {
+			t.Error(err)
+		}
+
+		if !isEqualBytes(enc.Pubkey[:], dec.Pubkey[:]) {
+			t.Errorf("deposit decode: not equal Pubkeys: expected: %v, got: %v", enc.Pubkey, dec.Pubkey)
+		}
+
+		if !isEqualBytes(enc.WithdrawalCredentials[:], dec.WithdrawalCredentials[:]) {
+			t.Errorf("deposit decode: not equal WithdrawalCridentials: expected: %v, got: %v", enc.WithdrawalCredentials, dec.WithdrawalCredentials)
+		}
+
+		if enc.Amount != dec.Amount {
+			t.Errorf("deposit decode: not equal Amounts: expected: %v, got: %v", enc.Amount, dec.Amount)
+		}
+
+		if !isEqualBytes(enc.Signature[:], dec.Signature[:]) {
+			t.Errorf("deposit decode: not equal Signature: expected: %v, got: %v", enc.Signature, dec.Signature)
+		}
+
+		if enc.Index != dec.Index {
+			t.Errorf("deposit decode: not equal Indexes: expected: %v, got: %v", enc.Index, dec.Index)
+		}
+	}
+
+}
