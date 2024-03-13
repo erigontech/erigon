facb0ef926d4dbeb34aa1b5deb7dac6f7209fe1c
hexoscott <70711990+hexoscott@users.noreply.github.com>
2024-03-13 14:29:53 +0000

    allow 'input' in place of 'data' for eth_call (#186)

--- turbo/adapter/ethapi/api.go
+++ turbo/adapter/ethapi/api.go
@@ -46,6 +46,7 @@ type CallArgs struct {
 	Value                *hexutil.Big       `json:"value"`
 	Nonce                *hexutil.Uint64    `json:"nonce"`
 	Data                 *hexutility.Bytes  `json:"data"`
+	Input                *hexutility.Bytes  `json:"input"`
 	AccessList           *types2.AccessList `json:"accessList"`
 	ChainID              *hexutil.Big       `json:"chainId,omitempty"`
 }
@@ -139,6 +140,8 @@ func (args *CallArgs) ToMessage(globalGasCap uint64, baseFee *uint256.Int) (type
 	var data []byte
 	if args.Data != nil {
 		data = *args.Data
+	} else if args.Input != nil {
+		data = *args.Input
 	}
 	var accessList types2.AccessList
 	if args.AccessList != nil {
