5e7f42e427aae11bf584d345642137418d177958
alex.sharov <AskAlexSharov@gmail.com>
2024-03-13 14:36:08 +0700

    save

--- erigon-lib/downloader/util.go
+++ erigon-lib/downloader/util.go
@@ -327,6 +327,12 @@ func addTorrentFile(ctx context.Context, ts *torrent.TorrentSpec, torrentClient 
 }
 
 func _addTorrentFile(ctx context.Context, ts *torrent.TorrentSpec, torrentClient *torrent.Client, db kv.RwDB, webseeds *WebSeeds) (t *torrent.Torrent, ok bool, err error) {
+	defer func(t time.Time) {
+		if time.Since(t) > 10*time.Second {
+			fmt.Printf("util.go:330: %s, %s\n", time.Since(t), ts.DisplayName)
+		}
+	}(time.Now())
+
 	select {
 	case <-ctx.Done():
 		return nil, false, ctx.Err()
