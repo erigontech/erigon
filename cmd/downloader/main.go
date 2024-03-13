812e7882d21ea52adabdf7ae5e4c784092ae0846
alex.sharov <AskAlexSharov@gmail.com>
2024-03-13 14:28:36 +0700

    save

--- cmd/downloader/main.go
+++ cmd/downloader/main.go
@@ -204,7 +204,7 @@ func Downloader(ctx context.Context, logger log.Logger) error {
 		return err
 	}
 
-	cfg.ClientConfig.PieceHashersPerTorrent = 128
+	cfg.ClientConfig.PieceHashersPerTorrent = 256
 	cfg.ClientConfig.DisableIPv6 = disableIPV6
 	cfg.ClientConfig.DisableIPv4 = disableIPV4
 
