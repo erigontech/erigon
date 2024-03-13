a94938ca027cc6d3e40d2d588364da898ddad048
alex.sharov <AskAlexSharov@gmail.com>
2024-03-13 17:01:01 +0700

    save

--- erigon-lib/downloader/downloader.go
+++ erigon-lib/downloader/downloader.go
@@ -2254,8 +2254,8 @@ func openClient(ctx context.Context, dbDir, snapDir string, cfg *torrent.ClientC
 		PageSize(uint64(4 * datasize.KB)).
 		WriteMap().
 		LifoReclaim().
-		RoTxsLimiter(semaphore.NewWeighted(9_000))
-	Path(dbDir).
+		RoTxsLimiter(semaphore.NewWeighted(9_000)).
+		Path(dbDir).
 		Open(ctx)
 	if err != nil {
 		return nil, nil, nil, nil, fmt.Errorf("torrentcfg.openClient: %w", err)
