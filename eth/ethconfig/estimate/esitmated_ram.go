37147c4221832d5f13e3eb52cb29f16af4f771eb
taratorio <94537774+taratorio@users.noreply.github.com>
2024-03-15 11:22:23 +0000

    workers ignoring cpus

--- eth/ethconfig/estimate/esitmated_ram.go
+++ eth/ethconfig/estimate/esitmated_ram.go
@@ -13,7 +13,6 @@ type EstimatedRamPerWorker datasize.ByteSize
 
 // Workers - return max workers amount based on total Memory/CPU's and estimated RAM per worker
 func (r EstimatedRamPerWorker) Workers() int {
-	// 50% of TotalMemory. Better don't count on 100% because OOM Killer may have aggressive defaults and other software may need RAM
 	maxWorkersForGivenMemory := r.WorkersIgnoringCPUs()
 	res := cmp.Min(AlmostAllCPUs(), maxWorkersForGivenMemory)
 	return cmp.Max(1, res) // must have at-least 1 worker
@@ -27,6 +26,7 @@ func (r EstimatedRamPerWorker) WorkersQuarter() int {
 	return cmp.Max(1, r.Workers()/4)
 }
 
+// WorkersIgnoringCPUs - return max workers amount based on total Memory and estimated RAM per worker
 func (r EstimatedRamPerWorker) WorkersIgnoringCPUs() int {
 	// 50% of TotalMemory. Better don't count on 100% because OOM Killer may have aggressive defaults and other software may need RAM
 	return int((mmap.TotalMemory() / 2) / uint64(r))
