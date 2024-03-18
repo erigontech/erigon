2ff3df49c53d2a7bd1607a6067afdd5816329b21
Shoham Chakraborty <shhmchk@gmail.com>
2024-03-18 18:39:27 +0800

    Remove duplicate stats

--- erigon-lib/common/disk/disk.go
+++ erigon-lib/common/disk/disk.go
@@ -11,17 +11,11 @@ import (
 )
 
 var (
-	writeBytes = metrics.NewGauge(`system_disk_writebytes`)
-	readBytes  = metrics.NewGauge(`system_disk_readbytes`)
+	writeBytes = metrics.NewGauge(`process_io_storage_written_bytes_total`)
+	readBytes  = metrics.NewGauge(`process_io_storage_read_bytes_total`)
 
-	writeBytes2 = metrics.NewGauge(`process_io_storage_written_bytes_total`)
-	readBytes2  = metrics.NewGauge(`process_io_storage_read_bytes_total`)
-
-	writeCount = metrics.NewGauge(`ru_outblock`)
-	readCount  = metrics.NewGauge(`ru_inblock`)
-
-	writeCount2 = metrics.NewGauge(`process_io_write_syscalls_total`)
-	readCount2  = metrics.NewGauge(`process_io_read_syscalls_total`)
+	writeCount = metrics.NewGauge(`process_io_write_syscalls_total`)
+	readCount  = metrics.NewGauge(`process_io_read_syscalls_total`)
 
 	cgoCount = metrics.NewGauge(`go_cgo_calls_count`)
 )
@@ -40,14 +34,8 @@ func UpdatePrometheusDiskStats() error {
 	writeBytes.SetUint64(counters.WriteBytes)
 	readBytes.SetUint64(counters.ReadBytes)
 
-	writeBytes2.SetUint64(counters.WriteBytes)
-	readBytes2.SetUint64(counters.ReadBytes)
-
 	writeCount.SetUint64(counters.WriteCount)
 	readCount.SetUint64(counters.ReadCount)
-
-	writeCount2.SetUint64(counters.WriteCount)
-	readCount2.SetUint64(counters.ReadCount)
 
 	cgoCount.SetUint64(uint64(runtime.NumCgoCall()))
 
--- erigon-lib/common/disk/disk_linux.go
+++ erigon-lib/common/disk/disk_linux.go
@@ -15,20 +15,11 @@ var (
 	diskMajorFaults = metrics.NewGauge(`process_major_pagefaults_total`)
 	diskMinorFaults = metrics.NewGauge(`process_minor_pagefaults_total`)
 
-	diskMajorFaults2 = metrics.NewGauge(`ru_majflt`)
-	diskMinorFaults2 = metrics.NewGauge(`ru_minflt`)
+	writeCount = metrics.NewGauge(`process_io_write_syscalls_total`)
+	readCount  = metrics.NewGauge(`process_io_read_syscalls_total`)
 
-	writeCount = metrics.NewGauge(`ru_outblock`)
-	readCount  = metrics.NewGauge(`ru_inblock`)
-
-	writeCount2 = metrics.NewGauge(`process_io_write_syscalls_total`)
-	readCount2  = metrics.NewGauge(`process_io_read_syscalls_total`)
-
-	writeBytes = metrics.NewGauge(`system_disk_writebytes`)
-	readBytes  = metrics.NewGauge(`system_disk_readbytes`)
-
-	writeBytes2 = metrics.NewGauge(`process_io_storage_written_bytes_total`)
-	readBytes2  = metrics.NewGauge(`process_io_storage_read_bytes_total`)
+	writeBytes = metrics.NewGauge(`process_io_storage_written_bytes_total`)
+	readBytes  = metrics.NewGauge(`process_io_storage_read_bytes_total`)
 
 	cgoCount = metrics.NewGauge(`go_cgo_calls_count`)
 )
@@ -50,22 +41,13 @@ func UpdatePrometheusDiskStats() error {
 	}
 
 	diskMajorFaults.SetUint64(faults.MajorFaults)
-	diskMajorFaults2.SetUint64(faults.MajorFaults)
-
 	diskMinorFaults.SetUint64(faults.MinorFaults)
-	diskMinorFaults2.SetUint64(faults.MinorFaults)
 
 	writeCount.SetUint64(counters.WriteCount)
 	readCount.SetUint64(counters.ReadCount)
 
-	writeCount2.SetUint64(counters.WriteCount)
-	readCount2.SetUint64(counters.ReadCount)
-
 	writeBytes.SetUint64(counters.WriteBytes)
 	readBytes.SetUint64(counters.ReadBytes)
-
-	writeBytes2.SetUint64(counters.WriteBytes)
-	readBytes2.SetUint64(counters.ReadBytes)
 
 	cgoCount.SetUint64(uint64(runtime.NumCgoCall()))
 
