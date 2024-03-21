3f4a34fc66de702fc5b0cef28396e240f09a3c95
Shoham Chakraborty <shhmchk@gmail.com>
2024-03-21 12:26:43 +0800

    metrics: Restore disk statistics (#9672)
    Grafana preview
    
    <img width="1711" alt="Screenshot 2024-03-18 at 6 39 55 PM"
    src="https://github.com/ledgerwatch/erigon/assets/7486955/92676df0-9db0-4050-94f6-41df0b95a026">

--- eth/backend.go
+++ eth/backend.go
@@ -33,6 +33,7 @@ import (
 	"sync/atomic"
 	"time"
 
+	"github.com/ledgerwatch/erigon-lib/common/disk"
 	"github.com/ledgerwatch/erigon-lib/common/mem"
 
 	"github.com/erigontech/mdbx-go/mdbx"
@@ -490,6 +491,7 @@ func New(ctx context.Context, stack *node.Node, config *ethconfig.Config, logger
 
 	// setup periodic logging and prometheus updates
 	go mem.LogMemStats(ctx, logger)
+	go disk.UpdateDiskStats(ctx, logger)
 
 	var currentBlock *types.Block
 	if err := chainKv.View(context.Background(), func(tx kv.Tx) error {
