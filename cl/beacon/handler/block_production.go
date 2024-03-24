fd8e543693b0e67434712d3a33b163b1c1973b29
Giulio <giulio.rebuffo@gmail.com>
2024-03-24 02:30:03 +0100

    save

--- cl/beacon/handler/block_production.go
+++ cl/beacon/handler/block_production.go
@@ -286,6 +286,8 @@ func (a *ApiHandler) produceBeaconBody(ctx context.Context, apiVersion int, base
 				executionPayload.Extra = payload.Extra
 				executionPayload.BlobGasUsed = payload.BlobGasUsed
 				executionPayload.ExcessBlobGas = payload.ExcessBlobGas
+				payload.BaseFeePerGas = payload.BaseFeePerGas
+				// Reset the limit of withdrawals
 				executionPayload.Withdrawals = solid.NewStaticListSSZ[*cltypes.Withdrawal](int(a.beaconChainCfg.MaxWithdrawalsPerPayload), 44)
 				payload.Withdrawals.Range(func(index int, value *cltypes.Withdrawal, length int) bool {
 					executionPayload.Withdrawals.Append(value)
