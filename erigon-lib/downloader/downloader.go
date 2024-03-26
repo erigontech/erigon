5317eca6889d0d507a2fe2839b3fe6ae5b6a79e2
alex.sharov <AskAlexSharov@gmail.com>
2024-03-26 12:09:57 +0700

    save

--- erigon-lib/downloader/downloader.go
+++ erigon-lib/downloader/downloader.go
@@ -2168,12 +2168,10 @@ func (d *Downloader) AddMagnetLink(ctx context.Context, infoHash metainfo.Hash, 
 		}
 
 		//TODO: remove whitelist check - Erigon may send us new seedable files
-		if !d.snapshotLock.Downloads.Contains(name) {
-			mi := t.Metainfo()
-			if err := CreateTorrentFileIfNotExists(d.SnapDir(), t.Info(), &mi, d.torrentFiles); err != nil {
-				d.logger.Warn("[snapshots] create torrent file", "err", err)
-				return
-			}
+		mi := t.Metainfo()
+		if err := CreateTorrentFileIfNotExists(d.SnapDir(), t.Info(), &mi, d.torrentFiles); err != nil {
+			d.logger.Warn("[snapshots] create torrent file", "err", err)
+			return
 		}
 
 		urls, ok := d.webseeds.ByFileName(t.Name())
