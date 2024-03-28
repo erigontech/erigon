6b0a70ba64115f5212fc09bc364b65dbcae2d511
awskii <artem.tsskiy@gmail.com>
2024-03-28 19:39:28 +0000

    Revert "save"
    7d8c3ee7663801df9b7b4e72423aaf4c5fcb8f14

--- erigon-lib/state/history.go
+++ erigon-lib/state/history.go
@@ -528,9 +528,6 @@ func (hc *HistoryContext) newWriter(tmpdir string, discard bool) *historyBuffere
 
 		ii: hc.ic.newWriter(tmpdir, discard),
 	}
-	if !discard && hc.h.dontProduceFiles {
-		w.discard = true
-	}
 	w.historyVals.LogLvl(log.LvlTrace)
 	w.historyVals.SortAndFlushInBackground(true)
 	return w
