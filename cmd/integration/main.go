7f819cc495cb8c276d9ece1fbc65b24d8ac80e0d
alex.sharov <AskAlexSharov@gmail.com>
2024-04-03 22:23:47 +0200

    save

--- cmd/integration/main.go
+++ cmd/integration/main.go
@@ -5,18 +5,12 @@ import (
 	"os"
 
 	"github.com/ledgerwatch/erigon-lib/common"
-	"github.com/ledgerwatch/erigon-lib/common/disk"
-	"github.com/ledgerwatch/erigon-lib/common/mem"
 	"github.com/ledgerwatch/erigon/cmd/integration/commands"
-	"github.com/ledgerwatch/log/v3"
 )
 
 func main() {
 	rootCmd := commands.RootCommand()
 	ctx, _ := common.RootContext()
-
-	go mem.LogMemStats(ctx, log.New())
-	go disk.UpdateDiskStats(ctx, log.New())
 
 	if err := rootCmd.ExecuteContext(ctx); err != nil {
 		fmt.Println(err)
