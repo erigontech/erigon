3c0bcbf013a406bfc1218758480b2f126ae0bd75
Giulio <giulio.rebuffo@gmail.com>
2024-04-05 12:11:24 +0200

    save

--- cl/sentinel/gossip.go
+++ cl/sentinel/gossip.go
@@ -412,6 +412,9 @@ func (s *GossipSubscription) run(ctx context.Context, sub *pubsub.Subscription, 
 				log.Warn("[Sentinel] fail to decode gossip packet", "err", err, "topicName", topicName)
 				return
 			}
+			if gossip.IsTopicBlobSidecar(topicName) || topicName == gossip.TopicNameBeaconBlock {
+				fmt.Println(topicName)
+			}
 
 			if msg.ReceivedFrom == s.host {
 				continue
