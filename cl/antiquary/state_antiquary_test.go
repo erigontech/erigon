faf7a431516d760217677b729a191ed324d1d9da
Giulio rebuffo <giulio.rebuffo@gmail.com>
2024-04-08 17:21:42 +0200

    Caplin: Refactored state antiquary (#9876)
    This PR separates the data collection layer from the `Antiquary` object
    to the `beaconStatesCollector`, which hopefully makes the code cleaner.
    It is still messy, but now we have a proper separation between the logic
    and the data collection layer.
    
    The reason why we have internal buffers here is that MDBX locks the
    entire database when writing to it, so we need to minimize the time
    spent in the write lock. So instead of writing the historical states on
    write transactions and keeping the lock for hours, we accumulate them in
    memory and write them in a single write transaction.

--- cl/antiquary/state_antiquary_test.go
+++ cl/antiquary/state_antiquary_test.go
@@ -1,4 +1,4 @@
-//go:build prevent
+//go:build integration
 
 package antiquary
 
@@ -15,20 +15,17 @@ import (
 	state_accessors "github.com/ledgerwatch/erigon/cl/persistence/state"
 	"github.com/ledgerwatch/erigon/cl/phase1/core/state"
 	"github.com/ledgerwatch/log/v3"
-	"github.com/spf13/afero"
 	"github.com/stretchr/testify/require"
 )
 
 func runTest(t *testing.T, blocks []*cltypes.SignedBeaconBlock, preState, postState *state.CachingBeaconState) {
 	db := memdb.NewTestDB(t)
-	reader, _ := tests.LoadChain(blocks, postState, db, t)
+	reader := tests.LoadChain(blocks, postState, db, t)
 
 	ctx := context.Background()
 	vt := state_accessors.NewStaticValidatorTable()
-	f := afero.NewMemMapFs()
-	a := NewAntiquary(ctx, preState, vt, &clparams.MainnetBeaconConfig, datadir.New("/tmp"), nil, db, nil, reader, log.New(), true, true, f)
+	a := NewAntiquary(ctx, nil, preState, vt, &clparams.MainnetBeaconConfig, datadir.New("/tmp"), nil, db, nil, reader, log.New(), true, true, true)
 	require.NoError(t, a.IncrementBeaconState(ctx, blocks[len(blocks)-1].Block.Slot+33))
-	// TODO: add more meaning here, like checking db values, will do so once i see some bugs
 }
 
 func TestStateAntiquaryCapella(t *testing.T) {
