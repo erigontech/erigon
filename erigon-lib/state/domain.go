7d6a8fb19d312825bbff76f09c43c6cab625e8ff
alex.sharov <AskAlexSharov@gmail.com>
2024-04-10 13:57:26 +0700

    move filesItem to files_item.go

--- erigon-lib/state/domain.go
+++ erigon-lib/state/domain.go
@@ -23,7 +23,6 @@ import (
 	"encoding/binary"
 	"fmt"
 	"math"
-	"os"
 	"path/filepath"
 	"regexp"
 	"strconv"
@@ -43,7 +42,6 @@ import (
 	"github.com/ledgerwatch/erigon-lib/common/dir"
 	"github.com/ledgerwatch/erigon-lib/etl"
 	"github.com/ledgerwatch/erigon-lib/kv"
-	"github.com/ledgerwatch/erigon-lib/kv/bitmapdb"
 	"github.com/ledgerwatch/erigon-lib/kv/iter"
 	"github.com/ledgerwatch/erigon-lib/kv/order"
 	"github.com/ledgerwatch/erigon-lib/metrics"
@@ -97,122 +95,6 @@ var (
 	traceGetAsOf     = dbg.EnvString("AGG_TRACE_GET_AS_OF", "")
 	tracePutWithPrev = dbg.EnvString("AGG_TRACE_PUT_WITH_PREV", "")
 )
-
-// filesItem corresponding to a pair of files (.dat and .idx)
-type filesItem struct {
-	decompressor         *seg.Decompressor
-	index                *recsplit.Index
-	bindex               *BtIndex
-	bm                   *bitmapdb.FixedSizeBitmaps
-	existence            *ExistenceFilter
-	startTxNum, endTxNum uint64 //[startTxNum, endTxNum)
-
-	// Frozen: file of size StepsInColdFile. Completely immutable.
-	// Cold: file of size < StepsInColdFile. Immutable, but can be closed/removed after merge to bigger file.
-	// Hot: Stored in DB. Providing Snapshot-Isolation by CopyOnWrite.
-	frozen   bool         // immutable, don't need atomic
-	refcount atomic.Int32 // only for `frozen=false`
-
-	// file can be deleted in 2 cases: 1. when `refcount == 0 && canDelete == true` 2. on app startup when `file.isSubsetOfFrozenFile()`
-	// other processes (which also reading files, may have same logic)
-	canDelete atomic.Bool
-}
-
-func newFilesItem(startTxNum, endTxNum, stepSize uint64) *filesItem {
-	startStep := startTxNum / stepSize
-	endStep := endTxNum / stepSize
-	frozen := endStep-startStep == StepsInColdFile
-	return &filesItem{startTxNum: startTxNum, endTxNum: endTxNum, frozen: frozen}
-}
-
-// isSubsetOf - when `j` covers `i` but not equal `i`
-func (i *filesItem) isSubsetOf(j *filesItem) bool {
-	return (j.startTxNum <= i.startTxNum && i.endTxNum <= j.endTxNum) && (j.startTxNum != i.startTxNum || i.endTxNum != j.endTxNum)
-}
-func (i *filesItem) isBefore(j *filesItem) bool { return i.endTxNum <= j.startTxNum }
-
-func filesItemLess(i, j *filesItem) bool {
-	if i.endTxNum == j.endTxNum {
-		return i.startTxNum > j.startTxNum
-	}
-	return i.endTxNum < j.endTxNum
-}
-func (i *filesItem) closeFilesAndRemove() {
-	if i.decompressor != nil {
-		i.decompressor.Close()
-		// paranoic-mode on: don't delete frozen files
-		if !i.frozen {
-			if err := os.Remove(i.decompressor.FilePath()); err != nil {
-				log.Trace("remove after close", "err", err, "file", i.decompressor.FileName())
-			}
-			if err := os.Remove(i.decompressor.FilePath() + ".torrent"); err != nil {
-				log.Trace("remove after close", "err", err, "file", i.decompressor.FileName()+".torrent")
-			}
-		}
-		i.decompressor = nil
-	}
-	if i.index != nil {
-		i.index.Close()
-		// paranoic-mode on: don't delete frozen files
-		if !i.frozen {
-			if err := os.Remove(i.index.FilePath()); err != nil {
-				log.Trace("remove after close", "err", err, "file", i.index.FileName())
-			}
-		}
-		i.index = nil
-	}
-	if i.bindex != nil {
-		i.bindex.Close()
-		if err := os.Remove(i.bindex.FilePath()); err != nil {
-			log.Trace("remove after close", "err", err, "file", i.bindex.FileName())
-		}
-		i.bindex = nil
-	}
-	if i.bm != nil {
-		i.bm.Close()
-		if err := os.Remove(i.bm.FilePath()); err != nil {
-			log.Trace("remove after close", "err", err, "file", i.bm.FileName())
-		}
-		i.bm = nil
-	}
-	if i.existence != nil {
-		i.existence.Close()
-		if err := os.Remove(i.existence.FilePath); err != nil {
-			log.Trace("remove after close", "err", err, "file", i.existence.FileName)
-		}
-		i.existence = nil
-	}
-}
-
-type DomainStats struct {
-	MergesCount          uint64
-	LastCollationTook    time.Duration
-	LastPruneTook        time.Duration
-	LastPruneHistTook    time.Duration
-	LastFileBuildingTook time.Duration
-	LastCollationSize    uint64
-	LastPruneSize        uint64
-
-	FilesQueries *atomic.Uint64
-	TotalQueries *atomic.Uint64
-	EfSearchTime time.Duration
-	DataSize     uint64
-	IndexSize    uint64
-	FilesCount   uint64
-}
-
-func (ds *DomainStats) Accumulate(other DomainStats) {
-	if other.FilesQueries != nil {
-		ds.FilesQueries.Add(other.FilesQueries.Load())
-	}
-	if other.TotalQueries != nil {
-		ds.TotalQueries.Add(other.TotalQueries.Load())
-	}
-	ds.EfSearchTime += other.EfSearchTime
-	ds.IndexSize += other.IndexSize
-	ds.DataSize += other.DataSize
-	ds.FilesCount += other.FilesCount
-}
 
 // Domain is a part of the state (examples are Accounts, Storage, Code)
 // Domain should not have any go routines or locks
@@ -781,20 +663,6 @@ func (ch *CursorHeap) Pop() interface{} {
 	*ch = old[0 : n-1]
 	return x
 }
-
-// filesItem corresponding to a pair of files (.dat and .idx)
-type ctxItem struct {
-	getter     *seg.Getter
-	reader     *recsplit.IndexReader
-	startTxNum uint64
-	endTxNum   uint64
-
-	i   int
-	src *filesItem
-}
-
-func (i *ctxItem) isSubSetOf(j *ctxItem) bool { return i.src.isSubsetOf(j.src) } //nolint
-func (i *ctxItem) isSubsetOf(j *ctxItem) bool { return i.src.isSubsetOf(j.src) } //nolint
 
 // DomainContext allows accesing the same domain from multiple go-routines
 type DomainContext struct {
@@ -2409,4 +2277,34 @@ func (mf MergedFiles) Close() {
 			}
 		}
 	}
+}
+
+type DomainStats struct {
+	MergesCount          uint64
+	LastCollationTook    time.Duration
+	LastPruneTook        time.Duration
+	LastPruneHistTook    time.Duration
+	LastFileBuildingTook time.Duration
+	LastCollationSize    uint64
+	LastPruneSize        uint64
+
+	FilesQueries *atomic.Uint64
+	TotalQueries *atomic.Uint64
+	EfSearchTime time.Duration
+	DataSize     uint64
+	IndexSize    uint64
+	FilesCount   uint64
+}
+
+func (ds *DomainStats) Accumulate(other DomainStats) {
+	if other.FilesQueries != nil {
+		ds.FilesQueries.Add(other.FilesQueries.Load())
+	}
+	if other.TotalQueries != nil {
+		ds.TotalQueries.Add(other.TotalQueries.Load())
+	}
+	ds.EfSearchTime += other.EfSearchTime
+	ds.IndexSize += other.IndexSize
+	ds.DataSize += other.DataSize
+	ds.FilesCount += other.FilesCount
 }
