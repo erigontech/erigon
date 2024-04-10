eb7b6f5463988caae5232a1f4a47246a009c1cea
awskii <artem.tsskiy@gmail.com>
2024-04-10 16:45:49 +0100

    save

--- common/debug/log_panic.go
+++ common/debug/log_panic.go
@@ -2,16 +2,22 @@ package debug
 
 import (
 	"os"
+	"sync"
 	"syscall"
 
 	"github.com/ledgerwatch/erigon-lib/common/dbg"
 	"github.com/ledgerwatch/log/v3"
 )
 
-var sigc chan os.Signal
+var (
+	sigm sync.Mutex
+	sigc chan os.Signal
+)
 
 func GetSigC(sig *chan os.Signal) {
+	sigm.Lock()
 	sigc = *sig
+	sigm.Unlock()
 }
 
 // LogPanic - does log panic to logger and to <datadir>/crashreports then stops the process
@@ -22,6 +28,9 @@ func LogPanic() {
 	}
 
 	log.Error("catch panic", "err", panicResult, "stack", dbg.Stack())
+
+	sigm.Lock()
+	defer sigm.Unlock()
 	if sigc != nil {
 		sigc <- syscall.SIGINT
 	}
