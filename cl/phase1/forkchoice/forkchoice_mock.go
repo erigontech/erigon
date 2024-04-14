71a830495c8bff8e8682a96bcbe6bc54097cb9f9
Giulio <giulio.rebuffo@gmail.com>
2024-04-14 15:59:46 +0200

    save

--- cl/phase1/forkchoice/forkchoice_mock.go
+++ cl/phase1/forkchoice/forkchoice_mock.go
@@ -192,11 +192,6 @@ func (f *ForkChoiceStorageMock) ForkNodes() []ForkNode {
 	return f.WeightsMock
 }
 
-// func (f *ForkChoiceStorageMock) OnAggregateAndProof(aggregateAndProof *cltypes.SignedAggregateAndProof, test bool) error {
-// 	f.Pool.AttestationsPool.Insert(aggregateAndProof.Message.Aggregate.Signature(), aggregateAndProof.Message.Aggregate)
-// 	return nil
-// }
-
 func (f *ForkChoiceStorageMock) Synced() bool {
 	return true
 }
