35f18c9723be27439d127fe1eb0845350e8b1178
Giulio <giulio.rebuffo@gmail.com>
2024-04-17 22:18:12 +0200

    save

--- cl/phase1/network/services/attestation_service.go
+++ cl/phase1/network/services/attestation_service.go
@@ -100,8 +100,6 @@ func (s *attestationService) ProcessMessage(ctx context.Context, subnet *uint64,
 		for j := 0; j < 8; j++ {
 			if bits[i]&(1<<uint(j)) != 0 {
 				if i*8+j >= len(beaconCommitte) {
-					// turn off the bit
-					bits[i] &= ^(1 << uint(j))
 					continue
 				}
 				setBits++
