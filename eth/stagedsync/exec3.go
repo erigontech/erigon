610838e347e6d0bc63956c220186bd786371afb5
alex.sharov <AskAlexSharov@gmail.com>
2024-04-19 12:36:17 +0700

    save

--- eth/stagedsync/exec3.go
+++ eth/stagedsync/exec3.go
@@ -559,7 +559,7 @@ func ExecV3(ctx context.Context,
 	}
 
 	if useExternalTx && blockNum < cfg.blockReader.FrozenBlocks() {
-		defer agg.KeepStepsInDB(0).KeepStepsInDB(1)
+		//defer agg.KeepStepsInDB(0).KeepStepsInDB(1)
 	}
 
 	getHeaderFunc := func(hash common.Hash, number uint64) (h *types.Header) {
