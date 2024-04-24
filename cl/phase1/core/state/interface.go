831c48d426ca7c0f88a9706c7bd1f072a4a53f68
Kewei <kewei.train@gmail.com>
2024-04-25 00:56:39 +0800

    standardize mock file name (#10043)

--- cl/phase1/core/state/interface.go
+++ cl/phase1/core/state/interface.go
@@ -4,7 +4,7 @@ import libcommon "github.com/ledgerwatch/erigon-lib/common"
 
 // BeaconStateReader is an interface for reading the beacon state.
 //
-//go:generate mockgen -destination=./mock_services/beacon_state_reader.go -package=mock_services . BeaconStateReader
+//go:generate mockgen -destination=./mock_services/beacon_state_reader_mock.go -package=mock_services . BeaconStateReader
 type BeaconStateReader interface {
 	ValidatorPublicKey(index int) (libcommon.Bytes48, error)
 	GetDomain(domainType [4]byte, epoch uint64) ([]byte, error)
