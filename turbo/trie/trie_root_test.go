49b7a9713c3831e5dc2c298a4dc905e17510c0bf
goofylfg <165781272+goofylfg@users.noreply.github.com>
2024-04-25 03:42:20 +0800

    chore: remove repetitive words (#10044)

--- turbo/trie/trie_root_test.go
+++ turbo/trie/trie_root_test.go
@@ -473,7 +473,7 @@ func FuzzTrieRootAccounts(f *testing.F) {
 // populates the trie tables. Then, it modifies the database of accounts but
 // does not update the trie tables.  Finally, for every seeded account (modified
 // and not), it computes a proof for that key and verifies that the proof
-// matches the one as computed by the naive trie implementation and that that
+// matches the one as computed by the naive trie implementation and that
 // proof is valid.
 func FuzzTrieRootAccountProofs(f *testing.F) {
 	addFuzzTrieSeeds(f)
@@ -554,7 +554,7 @@ func FuzzTrieRootAccountProofs(f *testing.F) {
 // modifies the database of storage but does not update the trie tables.
 // Finally, for every seeded storage key (modified and not), it computes a proof for
 // that key and verifies that the proof matches the one as computed by the naive
-// trie implementation and that that proof is valid.
+// trie implementation and that proof is valid.
 func FuzzTrieRootStorageProofs(f *testing.F) {
 	addFuzzTrieSeeds(f)
 
