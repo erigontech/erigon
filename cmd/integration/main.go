00cf11a4db300eca7ef62127f8914386245e1d62
taratorio <94537774+taratorio@users.noreply.github.com>
2024-04-25 16:02:06 +0300

    cmd/integration: run kv tables init func in cmd/integration

--- cmd/integration/main.go
+++ cmd/integration/main.go
@@ -6,6 +6,9 @@ import (
 
 	"github.com/ledgerwatch/erigon-lib/common"
 	"github.com/ledgerwatch/erigon/cmd/integration/commands"
+
+	// needed so that erigon-lib/kv init func is run
+	_ "github.com/ledgerwatch/erigon-lib/kv"
 )
 
 func main() {
