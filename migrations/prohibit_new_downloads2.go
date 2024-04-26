d34cbcaf927212e55399fae984331d9f312d5c33
Giulio <giulio.rebuffo@gmail.com>
2024-04-26 11:27:00 +0200

    save

---
+++ migrations/prohibit_new_downloads2.go
@@ -0,0 +1,60 @@
+package migrations
+
+import (
+	"context"
+	"encoding/json"
+	"io/fs"
+	"os"
+	"path/filepath"
+
+	"github.com/ledgerwatch/erigon-lib/common/datadir"
+	"github.com/ledgerwatch/erigon-lib/common/dir"
+	"github.com/ledgerwatch/erigon-lib/downloader"
+	"github.com/ledgerwatch/erigon-lib/downloader/snaptype"
+	"github.com/ledgerwatch/erigon-lib/kv"
+	"github.com/ledgerwatch/erigon/eth/stagedsync/stages"
+	"github.com/ledgerwatch/log/v3"
+)
+
+// Switch to the second version of download.lock.
+var ProhibitNewDownloadsLock2 = Migration{
+	Name: "prohibit_new_downloads_lock",
+	Up: func(db kv.RwDB, dirs datadir.Dirs, progress []byte, BeforeCommit Callback, logger log.Logger) (err error) {
+		tx, err := db.BeginRw(context.Background())
+		if err != nil {
+			return err
+		}
+		defer tx.Rollback()
+		fPath := filepath.Join(dirs.Snap, downloader.ProhibitNewDownloadsFileName)
+		if dir.FileExist(fPath) {
+			content, err := os.ReadFile(fPath)
+			if err != nil {
+				return err
+			}
+			if len(
+				content,
+			) == 0 { // old format, need to change to all snaptypes except blob sidecars
+				locked := []string{}
+				ts := snaptype.AllTypes
+				for _, t := range ts {
+					if t.String() != snaptype.BlobSidecars.String() {
+						ts = append(ts, t)
+					}
+				}
+				newContent, err := json.Marshal(ts)
+				if err != nil {
+					return err
+				}
+				if err := os.WriteFile(fPath, newContent, fs.FileMode(os.O_TRUNC|os.O_WRONLY)); err != nil {
+					return err
+				}
+			}
+
+		}
+		// This migration is no-op, but it forces the migration mechanism to apply it and thus write the DB schema version info
+		if err := BeforeCommit(tx, nil, true); err != nil {
+			return err
+		}
+		return tx.Commit()
+	},
+}
