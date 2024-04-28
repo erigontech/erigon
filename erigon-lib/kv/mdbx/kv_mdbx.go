87a219d033794c172b7b031293ee570641eb4183
alex.sharov <AskAlexSharov@gmail.com>
2024-04-28 10:07:32 +0700

    save

--- erigon-lib/kv/mdbx/kv_mdbx.go
+++ erigon-lib/kv/mdbx/kv_mdbx.go
@@ -459,11 +459,6 @@ func (opts MdbxOpts) Open(ctx context.Context) (kv.RwDB, error) {
 	}
 	db.path = opts.path
 	addToPathDbMap(opts.path, db)
-	if dbg.MdbxLockInRam() {
-		if err := db.View(ctx, func(tx kv.Tx) error { return tx.(*MdbxTx).LockDBInRam() }); err != nil {
-			return nil, err
-		}
-	}
 	return db, nil
 }
 
