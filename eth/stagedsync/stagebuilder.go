34753782f5b835496d22a3d4d7b94aca7f338270
Giulio <giulio.rebuffo@gmail.com>
2024-04-29 23:39:00 +0200

    save

--- eth/stagedsync/stagebuilder.go
+++ eth/stagedsync/stagebuilder.go
@@ -68,33 +68,6 @@ func MiningStages(
 			Prune: func(firstCycle bool, u *PruneState, tx kv.RwTx, logger log.Logger) error { return nil },
 		},
 		{
-			ID:          stages.HashState,
-			Description: "Hash the key in the state",
-			Forward: func(firstCycle bool, badBlockUnwind bool, s *StageState, u Unwinder, txc wrap.TxContainer, logger log.Logger) error {
-				return SpawnHashStateStage(s, txc.Tx, hashStateCfg, ctx, logger)
-			},
-			Unwind: func(firstCycle bool, u *UnwindState, s *StageState, txc wrap.TxContainer, logger log.Logger) error {
-				return nil
-			},
-			Prune: func(firstCycle bool, u *PruneState, tx kv.RwTx, logger log.Logger) error { return nil },
-		},
-		{
-			ID:          stages.IntermediateHashes,
-			Description: "Generate intermediate hashes and computing state root",
-			Forward: func(firstCycle bool, badBlockUnwind bool, s *StageState, u Unwinder, txc wrap.TxContainer, logger log.Logger) error {
-				stateRoot, err := SpawnIntermediateHashesStage(s, u, txc.Tx, trieCfg, ctx, logger)
-				if err != nil {
-					return err
-				}
-				createBlockCfg.miner.MiningBlock.Header.Root = stateRoot
-				return nil
-			},
-			Unwind: func(firstCycle bool, u *UnwindState, s *StageState, txc wrap.TxContainer, logger log.Logger) error {
-				return nil
-			},
-			Prune: func(firstCycle bool, u *PruneState, tx kv.RwTx, logger log.Logger) error { return nil },
-		},
-		{
 			ID:          stages.MiningFinish,
 			Description: "Mining: create and propagate valid block",
 			Forward: func(firstCycle bool, badBlockUnwind bool, s *StageState, u Unwinder, txc wrap.TxContainer, logger log.Logger) error {
