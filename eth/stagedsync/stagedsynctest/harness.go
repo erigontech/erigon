cf090464c462b9e385efbd050eb9d17ca903a3f2
Giulio <giulio.rebuffo@gmail.com>
2024-04-30 17:42:38 +0200

    save

--- eth/stagedsync/stagedsynctest/harness.go
+++ eth/stagedsync/stagedsynctest/harness.go
@@ -92,9 +92,9 @@ func InitHarness(ctx context.Context, t *testing.T, cfg HarnessCfg) Harness {
 		ctx,
 		stagedsync.MiningCreateBlockCfg{},
 		bhCfg,
+		stagedsync.ExecuteBlockCfg{},
+		stagedsync.SendersCfg{},
 		stagedsync.MiningExecCfg{},
-		stagedsync.HashStateCfg{},
-		stagedsync.TrieCfg{},
 		stagedsync.MiningFinishCfg{},
 	)
 	miningSync := stagedsync.New(
