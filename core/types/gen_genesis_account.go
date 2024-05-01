746c99c641761200aa7d8cc0c8be3beda49522e2
taratorio <94537774+taratorio@users.noreply.github.com>
2024-05-01 17:20:06 +0300

    make gen run for clean state

--- core/types/gen_genesis_account.go
+++ core/types/gen_genesis_account.go
@@ -7,9 +7,8 @@ import (
 	"errors"
 	"math/big"
 
-	libcommon "github.com/ledgerwatch/erigon-lib/common"
+	"github.com/ledgerwatch/erigon-lib/common"
 	"github.com/ledgerwatch/erigon-lib/common/hexutility"
-
 	"github.com/ledgerwatch/erigon/common/math"
 )
 
@@ -61,9 +60,9 @@ func (g *GenesisAccount) UnmarshalJSON(input []byte) error {
 		g.Code = *dec.Code
 	}
 	if dec.Storage != nil {
-		g.Storage = make(map[libcommon.Hash]libcommon.Hash, len(dec.Storage))
+		g.Storage = make(map[common.Hash]common.Hash, len(dec.Storage))
 		for k, v := range dec.Storage {
-			g.Storage[libcommon.Hash(k)] = libcommon.Hash(v)
+			g.Storage[common.Hash(k)] = common.Hash(v)
 		}
 	}
 	if dec.Balance == nil {
