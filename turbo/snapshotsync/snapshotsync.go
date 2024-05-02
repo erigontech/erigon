026a740fabf9aa98d78570729a7134fb03f28496
Giulio <giulio.rebuffo@gmail.com>
2024-05-03 01:38:41 +0200

    save

--- turbo/snapshotsync/snapshotsync.go
+++ turbo/snapshotsync/snapshotsync.go
@@ -200,10 +200,8 @@ func buildBlackListForPruning(pruneMode bool, stepPrune, minBlockToDownload, blo
 func getMinimumBlocksToDownload(tx kv.Tx, blockReader services.FullBlockReader, minStep uint64) (uint64, error) {
 	frozenBlocks := blockReader.FrozenBlocks()
 	minToDownload := uint64(math.MaxUint64)
-	fmt.Println(frozenBlocks)
 	stateTxNum := minStep * config3.HistoryV3AggregationStep
 	if err := blockReader.IterateFrozenBodies(func(blockNum, baseTxNum, txAmount uint64) error {
-		fmt.Println(blockNum, baseTxNum, txAmount)
 		if stateTxNum <= baseTxNum { // If blocks is behind state then we can consider a starting point
 			return nil
 		}
