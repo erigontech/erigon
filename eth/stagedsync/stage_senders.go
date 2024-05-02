ab08635af57b9c16f956fce815423b361384d9e0
alex.sharov <AskAlexSharov@gmail.com>
2024-05-02 12:33:10 +0700

    save

--- eth/stagedsync/stage_senders.go
+++ eth/stagedsync/stage_senders.go
@@ -9,6 +9,7 @@ import (
 	"sync"
 	"time"
 
+	"github.com/ledgerwatch/erigon-lib/common/dbg"
 	"github.com/ledgerwatch/erigon-lib/kv/dbutils"
 	"github.com/ledgerwatch/erigon/eth/ethconfig"
 
@@ -231,6 +232,7 @@ Loop:
 			return err
 		}
 		if body == nil {
+			_, _ = cfg.blockReader.BodyWithTransactions(dbg.ContextWithDebug(ctx, true), tx, blockHash, blockNumber)
 			logger.Warn(fmt.Sprintf("[%s] ReadBodyWithTransactions can't find block", logPrefix), "num", blockNumber, "hash", blockHash)
 			continue
 		}
