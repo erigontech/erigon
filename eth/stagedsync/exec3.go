5212d51378c137e712cbdb186c457ab031034a19
awskii <artem.tsskiy@gmail.com>
2024-05-14 16:49:22 +0100

    remove ctx

--- eth/stagedsync/exec3.go
+++ eth/stagedsync/exec3.go
@@ -903,9 +903,6 @@ Loop:
 							agg.BuildFilesInBackground(outputTxNum.Load())
 						}
 
-						aggCtx := agg.BeginFilesRo()
-						defer aggCtx.Close()
-
 						applyTx, err = cfg.db.BeginRw(context.Background()) //nolint
 						if err != nil {
 							return err
