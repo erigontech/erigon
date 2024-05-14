25d8b8c4689e8ffdfbedfadc7c2306184961f7c1
Giulio <giulio.rebuffo@gmail.com>
2024-05-14 10:42:37 +0200

    save

--- eth/stagedsync/exec3.go
+++ eth/stagedsync/exec3.go
@@ -881,7 +881,7 @@ Loop:
 
 				if err := func() error {
 					doms.Close()
-					if _, err := applyTx.(state2.HasAggTx).AggTx().(*state2.AggregatorRoTx).PruneSmallBatches(ctx, 10*time.Minute, applyTx); err != nil {
+					if _, err := applyTx.(state2.HasAggTx).AggTx().(*state2.AggregatorRoTx).PruneSmallBatches(ctx, 1*time.Minute, applyTx); err != nil {
 						return err
 					}
 					if err = execStage.Update(applyTx, outputBlockNum.GetValueUint64()); err != nil {
@@ -902,14 +902,14 @@ Loop:
 						defer aggCtx.Close()
 
 						tt = time.Now()
-						// for haveMoreToPrune := true; haveMoreToPrune; {
-						// 	//very aggressive prune, because:
-						// 	// if prune is slow - means DB > RAM and skip pruning will only make things worse
-						// 	// db will grow -> prune will get slower -> db will grow -> ...
-						// 	if haveMoreToPrune, err = aggCtx.PruneSmallBatchesDb(ctx, 10*time.Minute, chainDb); err != nil {
-						// 		return err
-						// 	}
-						// }
+						for haveMoreToPrune := true; haveMoreToPrune; {
+							//very aggressive prune, because:
+							// if prune is slow - means DB > RAM and skip pruning will only make things worse
+							// db will grow -> prune will get slower -> db will grow -> ...
+							if haveMoreToPrune, err = aggCtx.PruneSmallBatchesDb(ctx, 10*time.Minute, chainDb); err != nil {
+								return err
+							}
+						}
 
 						t3 = time.Since(tt)
 
