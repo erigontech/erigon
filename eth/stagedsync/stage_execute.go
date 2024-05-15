4001ad90bf01d12e46f416be73a825ed0bb6fba7
Giulio <giulio.rebuffo@gmail.com>
2024-05-15 21:10:15 +0200

    save

--- eth/stagedsync/stage_execute.go
+++ eth/stagedsync/stage_execute.go
@@ -284,16 +284,12 @@ func ExecBlockV3(s *StageState, u Unwinder, txc wrap.TxContainer, toBlock uint64
 		return err
 	}
 
-	logPrefix := s.LogPrefix()
 	var to = prevStageProgress
 	if toBlock > 0 {
 		to = cmp.Min(prevStageProgress, toBlock)
 	}
 	if to < s.BlockNumber {
 		return nil
-	}
-	if to > s.BlockNumber+16 {
-		logger.Info(fmt.Sprintf("[%s] Blocks execution", logPrefix), "from", s.BlockNumber, "to", to)
 	}
 
 	parallel := txc.Tx == nil
