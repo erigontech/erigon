d881854c2aba38069827c050b3f09a7d05465c1a
yperbasis <andrey.ashikhmin@gmail.com>
2024-05-15 16:21:08 +0200

    start adding official tests

--- core/vm/contracts_test.go
+++ core/vm/contracts_test.go
@@ -343,6 +343,15 @@ func TestPrecompiledBLS12381PairingFail(t *testing.T)    { testJsonFail("blsPair
 func TestPrecompiledBLS12381MapG1Fail(t *testing.T)      { testJsonFail("blsMapG1", "11", t) }
 func TestPrecompiledBLS12381MapG2Fail(t *testing.T)      { testJsonFail("blsMapG2", "12", t) }
 
+// Tests from https://github.com/ethereum/EIPs/tree/master/assets/eip-2537
+func TestPrecompiledBLS12381G1AddEip(t *testing.T)     { testJson("blsG1Add-eip", "0a", t) }
+func TestPrecompiledBLS12381G2AddEip(t *testing.T)     { testJson("blsG2Add-eip", "0d", t) }
+func TestPrecompiledBLS12381G1AddFailEip(t *testing.T) { testJsonFail("blsG1Add-eip", "0a", t) }
+func TestPrecompiledBLS12381G1MulFailEip(t *testing.T) { testJsonFail("blsG1Mul-eip", "0b", t) }
+func TestPrecompiledBLS12381G2AddFailEip(t *testing.T) { testJsonFail("blsG2Add-eip", "0d", t) }
+func TestPrecompiledBLS12381MapG1FailEip(t *testing.T) { testJsonFail("blsMapG1-eip", "11", t) }
+func TestPrecompiledBLS12381MapG2FailEip(t *testing.T) { testJsonFail("blsMapG2-eip", "12", t) }
+
 func loadJson(name string) ([]precompiledTest, error) {
 	data, err := os.ReadFile(fmt.Sprintf("testdata/precompiles/%v.json", name))
 	if err != nil {
