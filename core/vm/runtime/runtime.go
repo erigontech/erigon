463cd5da70bb3ea764d31a6eecdb90dc9972e6d2
Arun Dhyani <dhyaniarun7@gmail.com>
2024-05-16 19:42:19 +0530

    fix nil checks in runtime

--- core/vm/runtime/runtime.go
+++ core/vm/runtime/runtime.go
@@ -136,7 +136,7 @@ func Execute(code, input []byte, cfg *Config, bn uint64) ([]byte, *state.IntraBl
 		sender  = vm.AccountRef(cfg.Origin)
 		rules   = vmenv.ChainRules()
 	)
-	if cfg.EVMConfig.Tracer != nil {
+	if cfg.EVMConfig.Tracer != nil && cfg.EVMConfig.Tracer.OnTxStart != nil {
 		cfg.EVMConfig.Tracer.OnTxStart(vmenv.GetVMContext(), types.NewTransaction(0, address, cfg.Value, cfg.GasLimit, cfg.GasPrice, input), cfg.Origin)
 	}
 	cfg.State.Prepare(rules, cfg.Origin, cfg.Coinbase, &address, vm.ActivePrecompiles(rules), nil)
@@ -183,7 +183,7 @@ func Create(input []byte, cfg *Config, blockNr uint64) ([]byte, libcommon.Addres
 		sender = vm.AccountRef(cfg.Origin)
 		rules  = vmenv.ChainRules()
 	)
-	if cfg.EVMConfig.Tracer != nil {
+	if cfg.EVMConfig.Tracer != nil && cfg.EVMConfig.Tracer.OnTxStart != nil {
 		cfg.EVMConfig.Tracer.OnTxStart(vmenv.GetVMContext(), types.NewContractCreation(0, cfg.Value, cfg.GasLimit, cfg.GasPrice, input), cfg.Origin)
 	}
 	cfg.State.Prepare(rules, cfg.Origin, cfg.Coinbase, nil, vm.ActivePrecompiles(rules), nil)
@@ -211,7 +211,7 @@ func Call(address libcommon.Address, input []byte, cfg *Config) ([]byte, uint64,
 	sender := cfg.State.GetOrNewStateObject(cfg.Origin)
 	statedb := cfg.State
 	rules := vmenv.ChainRules()
-	if cfg.EVMConfig.Tracer != nil {
+	if cfg.EVMConfig.Tracer != nil && cfg.EVMConfig.Tracer.OnTxStart != nil {
 		cfg.EVMConfig.Tracer.OnTxStart(vmenv.GetVMContext(), types.NewTransaction(0, address, cfg.Value, cfg.GasLimit, cfg.GasPrice, input), cfg.Origin)
 	}
 	statedb.Prepare(rules, cfg.Origin, cfg.Coinbase, &address, vm.ActivePrecompiles(rules), nil)
