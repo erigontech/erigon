fe588041dfea771ba0252bebace44b891e2877cd
battlmonstr <battlmonstr@users.noreply.github.com>
2024-05-16 08:36:45 +0200

    align entity receivers

--- polygon/heimdall/milestone.go
+++ polygon/heimdall/milestone.go
@@ -10,52 +10,53 @@ import (
 	"github.com/ledgerwatch/erigon-lib/kv"
 )
 
-var _ Waypoint = Milestone{}
-
 type MilestoneId uint64
 
-// milestone defines a response object type of bor milestone
+// Milestone defines a response object type of bor milestone
 type Milestone struct {
 	Id     MilestoneId
 	Fields WaypointFields
 }
 
-func (m Milestone) RawId() uint64 {
+var _ Entity = &Milestone{}
+var _ Waypoint = &Milestone{}
+
+func (m *Milestone) RawId() uint64 {
 	return uint64(m.Id)
 }
 
-func (m Milestone) StartBlock() *big.Int {
+func (m *Milestone) StartBlock() *big.Int {
 	return m.Fields.StartBlock
 }
 
-func (m Milestone) EndBlock() *big.Int {
+func (m *Milestone) EndBlock() *big.Int {
 	return m.Fields.EndBlock
 }
 
-func (m Milestone) BlockNumRange() ClosedRange {
+func (m *Milestone) BlockNumRange() ClosedRange {
 	return ClosedRange{
 		Start: m.StartBlock().Uint64(),
 		End:   m.EndBlock().Uint64(),
 	}
 }
 
-func (m Milestone) RootHash() libcommon.Hash {
+func (m *Milestone) RootHash() libcommon.Hash {
 	return m.Fields.RootHash
 }
 
-func (m Milestone) Timestamp() uint64 {
+func (m *Milestone) Timestamp() uint64 {
 	return m.Fields.Timestamp
 }
 
-func (m Milestone) Length() uint64 {
+func (m *Milestone) Length() uint64 {
 	return m.Fields.Length()
 }
 
-func (m Milestone) CmpRange(n uint64) int {
+func (m *Milestone) CmpRange(n uint64) int {
 	return m.Fields.CmpRange(n)
 }
 
-func (m Milestone) String() string {
+func (m *Milestone) String() string {
 	return fmt.Sprintf(
 		"Milestone {%v (%d:%d) %v %v %v}",
 		m.Fields.Proposer.String(),
@@ -88,7 +89,6 @@ func (m *Milestone) MarshalJSON() ([]byte, error) {
 }
 
 func (m *Milestone) UnmarshalJSON(b []byte) error {
-
 	// TODO - do we want to handle milestone_id ?
 	// (example format: 043353d6-d83f-47f8-a38f-f5062e82a6d4 - 0x142987cad41cf7111b2f186da6ab89e460037f7f)
 	dto := struct {
