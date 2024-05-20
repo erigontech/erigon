a19b3aabdb16991a15da0a3bf650161a66c61bcd
awskii <awskii@users.noreply.github.com>
2024-05-20 13:26:50 +0100

    tip

--- erigon-lib/kv/mdbx/kv_mdbx.go
+++ erigon-lib/kv/mdbx/kv_mdbx.go
@@ -1002,7 +1002,7 @@ func (tx *MdbxTx) CreateBucket(name string) error {
 	dbi, err = tx.tx.OpenDBI(name, nativeFlags, nil, nil)
 
 	if err != nil {
-		return fmt.Errorf("db-talbe doesn't exists: %s, %w. recommendation: can try run `integration run_migrations` to create non-existing tables", name, err)
+		return fmt.Errorf("db-talbe doesn't exists: %s, %w. Tip: try run `integration run_migrations` to create non-existing tables", name, err)
 	}
 	cnfCopy.DBI = kv.DBI(dbi)
 
