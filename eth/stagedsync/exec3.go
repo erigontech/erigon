4093966ee637a021f9bcbec5ed1840488730fa69
Giulio <giulio.rebuffo@gmail.com>
2024-05-21 13:42:25 +0200

    save

--- eth/stagedsync/exec3.go
+++ eth/stagedsync/exec3.go
@@ -642,7 +642,7 @@ Loop:
 			return f(n)
 		}
 		blockContext := core.NewEVMBlockContext(header, getHashFn, engine, nil /* author */)
-
+		fmt.Println("B", blockNum, "txs", len(txs), "txNum", inputTxNum, "outputTxNum", outputTxNum.Load(), "blockComplete", blockComplete.Load())
 		if parallel {
 			select {
 			case err := <-rwLoopErrCh:
