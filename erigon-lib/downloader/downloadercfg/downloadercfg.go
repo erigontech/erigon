cabd6dc9b864468c43bf45fc76eab7b83cd8d859
Mark Holt <mark@distributed.vision>
2024-05-29 13:50:29 +0100

    change default hashers to numcpu's - 2

--- erigon-lib/downloader/downloadercfg/downloadercfg.go
+++ erigon-lib/downloader/downloadercfg/downloadercfg.go
@@ -30,6 +30,7 @@ import (
 	"github.com/anacrolix/torrent"
 	"github.com/c2h5oh/datasize"
 	"github.com/ledgerwatch/erigon-lib/chain/snapcfg"
+	"github.com/ledgerwatch/erigon-lib/common/cmp"
 	"github.com/ledgerwatch/erigon-lib/common/datadir"
 	"github.com/ledgerwatch/erigon-lib/common/dir"
 	"github.com/ledgerwatch/log/v3"
@@ -65,7 +66,7 @@ func Default() *torrent.ClientConfig {
 	// better don't increase because erigon periodically producing "new seedable files" - and adding them to downloader.
 	// it must not impact chain tip sync - so, limit resources to minimum by default.
 	// but when downloader is started as a separated process - rise it to max
-	//torrentConfig.PieceHashersPerTorrent = cmp.Max(1, runtime.NumCPU()-1)
+	torrentConfig.PieceHashersPerTorrent = cmp.Max(1, runtime.NumCPU()-2)
 
 	torrentConfig.MinDialTimeout = 6 * time.Second    //default: 3s
 	torrentConfig.HandshakesTimeout = 8 * time.Second //default: 4s
