8b92231e91264f73b07bdb71095a02068d325371
alex.sharov <AskAlexSharov@gmail.com>
2024-05-31 11:45:35 +0700

    save

--- erigon-lib/kv/rawdbv3/txnum.go
+++ erigon-lib/kv/rawdbv3/txnum.go
@@ -20,7 +20,6 @@ import (
 	"encoding/binary"
 	"errors"
 	"fmt"
-	"github.com/ledgerwatch/log/v3"
 	"sort"
 
 	"github.com/ledgerwatch/erigon-lib/common/dbg"
@@ -182,7 +181,7 @@ func (txNums) FindBlockNum(tx kv.Tx, endTxNumMinimax uint64) (ok bool, blockNum 
 		if len(v) != 8 {
 			_lb, _lt, _ := TxNums.Last(tx)
 			err = fmt.Errorf("FindBlockNum(%d): seems broken TxNum value: %x -> (%x, %x); last in db: (%d, %d)", endTxNumMinimax, seek, found, v, _lb, _lt)
-			log.Warn("[dbg] FindBlockNum", "err", err, "stack", dbg.Stack())
+			//log.Warn("[dbg] FindBlockNum", "err", err, "stack", dbg.Stack())
 			return true
 		}
 		return binary.BigEndian.Uint64(v) >= endTxNumMinimax
