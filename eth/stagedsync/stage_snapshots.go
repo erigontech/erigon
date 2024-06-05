47459798fb78bf83de132acbc913125473340424
alex.sharov <AskAlexSharov@gmail.com>
2024-06-05 12:21:48 +0700

    save

--- eth/stagedsync/stage_snapshots.go
+++ eth/stagedsync/stage_snapshots.go
@@ -372,7 +372,6 @@ func FillDBFromSnapshots(logPrefix string, ctx context.Context, tx kv.RwTx, dirs
 
 		case stages.Bodies:
 			firstTxNum := blockReader.FirstTxnNumNotInSnapshots()
-			log.Info("[dbg] FillDBFromSnapshots", "blockReader.FirstTxnNumNotInSnapshots()", firstTxNum)
 			// ResetSequence - allow set arbitrary value to sequence (for example to decrement it to exact value)
 			if err := rawdb.ResetSequence(tx, kv.EthTx, firstTxNum); err != nil {
 				return err
