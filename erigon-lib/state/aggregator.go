33dfd1f2a9f42c4419ad7523b94d452040bac2e7
alex.sharov <AskAlexSharov@gmail.com>
2024-06-05 12:10:13 +0700

    save

--- erigon-lib/state/aggregator.go
+++ erigon-lib/state/aggregator.go
@@ -785,11 +785,17 @@ func (ac *AggregatorRoTx) CanUnwindBeforeBlockNum(blockNum uint64, tx kv.Tx) (ui
 	blockNumWithCommitment, _, _, err := domains.LatestCommitmentState(tx, ac.CanUnwindDomainsToTxNum(), unwindToTxNum)
 	if err != nil {
 		_minBlockNum, _ := ac.MinUnwindDomainsBlockNum(tx)
-		return _minBlockNum, blockNum >= _minBlockNum, nil //nolint
+		if blockNum >= _minBlockNum {
+			return blockNum, true, nil
+		}
+		return _minBlockNum, false, nil //nolint
 	}
 	if blockNumWithCommitment == 0 && ac.CanUnwindDomainsToTxNum() > 0 { // don't allow unwind beyond files progress
 		_minBlockNum, _ := ac.MinUnwindDomainsBlockNum(tx)
-		return _minBlockNum, blockNum >= _minBlockNum, nil
+		if blockNum >= _minBlockNum {
+			return blockNum, true, nil
+		}
+		return _minBlockNum, false, nil
 	}
 
 	return blockNumWithCommitment, true, nil
