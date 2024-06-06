5cd3b5e1b16d4c8fc580b6ac8758a13d611fae2d
Alex Sharov <AskAlexSharov@gmail.com>
2024-06-06 17:18:56 +0700

    Remove GetAsOf tests (#10642)

--- turbo/jsonrpc/storage_range.go
+++ turbo/jsonrpc/storage_range.go
@@ -1,8 +1,6 @@
 package jsonrpc
 
 import (
-	"fmt"
-
 	"github.com/holiman/uint256"
 	libcommon "github.com/ledgerwatch/erigon-lib/common"
 	"github.com/ledgerwatch/erigon-lib/kv"
@@ -22,28 +20,6 @@ type storageMap map[libcommon.Hash]StorageEntry
 type StorageEntry struct {
 	Key   *libcommon.Hash `json:"key"`
 	Value libcommon.Hash  `json:"value"`
-}
-
-type walker interface {
-	ForEachStorage(addr libcommon.Address, startLocation libcommon.Hash, cb func(key, seckey libcommon.Hash, value uint256.Int) bool, maxResults int) error
-}
-
-func storageRangeAt(stateReader walker, contractAddress libcommon.Address, start []byte, maxResult int) (StorageRangeResult, error) {
-	result := StorageRangeResult{Storage: storageMap{}}
-	resultCount := 0
-
-	if err := stateReader.ForEachStorage(contractAddress, libcommon.BytesToHash(start), func(key, seckey libcommon.Hash, value uint256.Int) bool {
-		if resultCount < maxResult {
-			result.Storage[seckey] = StorageEntry{Key: &key, Value: value.Bytes32()}
-		} else {
-			result.NextKey = &key
-		}
-		resultCount++
-		return resultCount < maxResult
-	}, maxResult+1); err != nil {
-		return StorageRangeResult{}, fmt.Errorf("error walking over storage: %w", err)
-	}
-	return result, nil
 }
 
 func storageRangeAtV3(ttx kv.TemporalTx, contractAddress libcommon.Address, start []byte, txNum uint64, maxResult int) (StorageRangeResult, error) {
