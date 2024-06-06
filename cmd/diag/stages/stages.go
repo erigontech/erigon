06936601a22a316d63fbb9c6bbb36c19c9416bf4
dvovk <vovk.dimon@gmail.com>
2024-06-06 13:39:08 +0100

    updated stage cmd to support substages

--- cmd/diag/stages/stages.go
+++ cmd/diag/stages/stages.go
@@ -29,16 +29,41 @@ var Command = cli.Command{
 }
 
 func printCurentStage(cliCtx *cli.Context) error {
-	var data diagnostics.SyncStatistics
+	/*var data diagnostics.SyncStatistics
 	url := "http://" + cliCtx.String(flags.DebugURLFlag.Name) + flags.ApiPath + "/snapshot-sync"
 
 	err := util.MakeHttpGetCall(cliCtx.Context, url, &data)
 	if err != nil {
 		util.RenderError(err)
 		return nil
+	}*/
+
+	testData := []diagnostics.SyncStage{
+		{
+			ID:    "Snapshots",
+			State: diagnostics.Running,
+			SubStages: []diagnostics.SyncSubStage{
+				{ID: "Download header-chain", State: diagnostics.Running},
+				{ID: "Download snapshots", State: diagnostics.Queued},
+				{ID: "Indexing", State: diagnostics.Queued},
+				{ID: "Fill DB", State: diagnostics.Queued},
+			},
+		},
+		{ID: "BlockHashes", State: diagnostics.Queued},
+		{ID: "Senders", State: diagnostics.Queued},
+		{ID: "Execution", State: diagnostics.Queued},
+		{ID: "HashState", State: diagnostics.Queued},
+		{ID: "IntermediateHashes", State: diagnostics.Queued},
+		{ID: "CallTraces", State: diagnostics.Queued},
+		{ID: "AccountHistoryIndex", State: diagnostics.Queued},
+		{ID: "StorageHistoryIndex", State: diagnostics.Queued},
+		{ID: "LogIndex", State: diagnostics.Queued},
+		{ID: "TxLookup", State: diagnostics.Queued},
+		{ID: "Finish", State: diagnostics.Queued},
 	}
 
-	stagesRows := getStagesRows(data.SyncStages)
+	//stagesRows := getStagesRows(data.SyncStages)
+	stagesRows := getStagesRows(testData)
 
 	switch cliCtx.String(flags.OutputFlag.Name) {
 	case "json":
@@ -47,7 +72,7 @@ func printCurentStage(cliCtx *cli.Context) error {
 	case "text":
 		util.RenderTableWithHeader(
 			"Sync stages:",
-			table.Row{"Stage", "Status"},
+			table.Row{"Stage", "SubStage", "Status"},
 			stagesRows,
 		)
 	}
@@ -60,18 +85,23 @@ func getStagesRows(stages []diagnostics.SyncStage) []table.Row {
 	for _, stage := range stages {
 		stageRow := table.Row{
 			stage.ID,
+			"",
 			stage.State.String(),
 		}
+		rows = append(rows, stageRow)
 
 		for _, substage := range stage.SubStages {
 			subStageRow := table.Row{
+				"",
 				substage.ID,
 				substage.State.String(),
 			}
 			rows = append(rows, subStageRow)
 		}
 
-		rows = append(rows, stageRow)
+		if len(stage.SubStages) != 0 {
+			rows = append(rows, table.Row{"", "", ""})
+		}
 	}
 
 	return rows
