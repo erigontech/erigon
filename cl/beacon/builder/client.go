98de3e12ba9fe1219ef386976bb40ce5f4b94c1c
kewei <kewei.train@gmail.com>
2024-06-11 03:09:17 +0800

    update

--- cl/beacon/builder/client.go
+++ cl/beacon/builder/client.go
@@ -50,9 +50,9 @@ func (b *builderClient) RegisterValidator(ctx context.Context, registers []*clty
 	}
 	_, err = httpCall[json.RawMessage](ctx, b.httpClient, http.MethodPost, url, nil, bytes.NewBuffer(payload))
 	if err != nil {
-		log.Warn("[mev builder] httpCall error", "err", err, "payload", string(payload))
+		log.Warn("[mev builder] httpCall error", "err", err)
 	} else {
-		log.Trace("[mev builder] RegisterValidator", "url", url, "payload", string(payload))
+		log.Trace("[mev builder] RegisterValidator", "payload", string(payload))
 	}
 	return err
 }
