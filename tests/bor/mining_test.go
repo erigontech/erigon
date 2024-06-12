519ab6e69fca29e85d083b7ce4187328207fe845
alex.sharov <AskAlexSharov@gmail.com>
2024-06-12 16:07:22 +0700

    save

--- tests/bor/mining_test.go
+++ tests/bor/mining_test.go
@@ -55,9 +55,9 @@ var (
 // Example : CGO_CFLAGS="-D__BLST_PORTABLE__" go test -run ^TestMiningBenchmark$ github.com/ledgerwatch/erigon/tests/bor -v -count=1
 // In TestMiningBenchmark, we will test the mining performance. We will initialize a single node devnet and fire 5000 txs. We will measure the time it takes to include all the txs. This can be made more advcanced by increasing blockLimit and txsInTxpool.
 func TestMiningBenchmark(t *testing.T) {
-	//if config3.EnableHistocloseFilesAfterStepryV4InTest {
-	//	t.Skip("TODO: [e4] implement me")
-	//}
+	if config3.EnableHistocloseFilesAfterStepryV4InTest {
+		t.Skip("TODO: [e4] implement me")
+	}
 
 	log.Root().SetHandler(log.LvlFilterHandler(log.LvlWarn, log.StreamHandler(os.Stderr, log.TerminalFormat())))
 	fdlimit.Raise(2048)
