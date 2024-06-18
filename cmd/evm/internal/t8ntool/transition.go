736b1b27e9d93f87b7e9de68b1115f97b61a281c
Ilya Mikheev <54912776+JkLondon@users.noreply.github.com>
2024-06-18 19:50:52 +0400

    eliminated calcroot (#10791)
    closes #10640
    moved from CalcRoot to ComputeCommitment
    all tests are passed but I'm not sure how to test it more
    
    ---------
    
    Co-authored-by: Ilya Miheev <ilya.miheev@lamoda.ru>

--- cmd/evm/internal/t8ntool/transition.go
+++ cmd/evm/internal/t8ntool/transition.go
@@ -40,7 +40,7 @@ import (
 	"github.com/ledgerwatch/erigon-lib/common/hexutility"
 	"github.com/ledgerwatch/erigon-lib/common/length"
 	"github.com/ledgerwatch/erigon-lib/kv"
-	state3 "github.com/ledgerwatch/erigon-lib/state"
+	libstate "github.com/ledgerwatch/erigon-lib/state"
 	"github.com/ledgerwatch/erigon/common/math"
 	"github.com/ledgerwatch/erigon/consensus/ethash"
 	"github.com/ledgerwatch/erigon/consensus/merge"
@@ -53,7 +53,6 @@ import (
 	"github.com/ledgerwatch/erigon/rlp"
 	"github.com/ledgerwatch/erigon/tests"
 	"github.com/ledgerwatch/erigon/turbo/jsonrpc"
-	"github.com/ledgerwatch/erigon/turbo/trie"
 )
 
 const (
@@ -308,7 +307,7 @@ func Main(ctx *cli.Context) error {
 	}
 	defer tx.Rollback()
 
-	sd, err := state3.NewSharedDomains(tx, log.New())
+	sd, err := libstate.NewSharedDomains(tx, log.New())
 	if err != nil {
 		return err
 	}
@@ -618,6 +617,12 @@ func CalculateStateRoot(tx kv.RwTx) (*libcommon.Hash, error) {
 	}
 	h := libcommon.NewHasher()
 	defer libcommon.ReturnHasherToPool(h)
+	domains, err := libstate.NewSharedDomains(tx, log.New())
+	if err != nil {
+		return nil, fmt.Errorf("NewSharedDomains: %w", err)
+	}
+	defer domains.Close()
+
 	for k, v, err := c.First(); k != nil; k, v, err = c.Next() {
 		if err != nil {
 			return nil, fmt.Errorf("interate over plain state: %w", err)
@@ -650,10 +655,12 @@ func CalculateStateRoot(tx kv.RwTx) (*libcommon.Hash, error) {
 		}
 	}
 	c.Close()
-	root, err := trie.CalcRoot("", tx)
+	root, err := domains.ComputeCommitment(context.Background(), true, domains.BlockNum(), "")
 	if err != nil {
 		return nil, err
 	}
+	hashRoot := libcommon.Hash{}
+	hashRoot.SetBytes(root)
 
-	return &root, nil
+	return &hashRoot, nil
 }
