1cf240b1c3b3158668eb1b6f6b0d3183e2fad534
yperbasis <andrey.ashikhmin@gmail.com>
2024-06-18 17:53:20 +0200

    small simplifications

--- core/bor_fee_log.go
+++ core/bor_fee_log.go
@@ -11,7 +11,6 @@ import (
 var transferLogSig = libcommon.HexToHash("0xe6497e3ee548a3372136af2fcb0696db31fc6cf20260707645068bd3fe97f3c4")
 var transferFeeLogSig = libcommon.HexToHash("0x4dfe1bbbcf077ddc3e01291eea2d5c70c2b422b415d95645b9adcfd678cb1d63")
 var feeAddress = libcommon.HexToAddress("0x0000000000000000000000000000000000001010")
-var zero = uint256.NewInt(0)
 
 // AddTransferLog adds transfer log into state
 func AddTransferLog(
@@ -85,7 +84,7 @@ func addTransferLog(
 	output2 *uint256.Int,
 ) {
 	// ignore if amount is 0
-	if amount.Cmp(zero) <= 0 {
+	if amount.IsZero() {
 		return
 	}
 
