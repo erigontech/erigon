aadb2348c0eac5858a99f2de0479c6d284398aa1
Ilya Miheev <ilya.miheev@lamoda.ru>
2024-06-21 14:07:15 +0400

    replaced encoding

--- erigon-lib/kv/mdbx/kv_mdbx_test.go
+++ erigon-lib/kv/mdbx/kv_mdbx_test.go
@@ -1123,8 +1123,8 @@ func BenchmarkDB_Get(b *testing.B) {
 
 	// Ensure data is correct.
 	if err := db.View(context.Background(), func(tx kv.Tx) error {
-		b.ResetTimer()
 		key := u64tob(uint64(1))
+		b.ResetTimer()
 		for i := 1; i <= b.N; i++ {
 			v, err := tx.GetOne(table, key)
 			if err != nil {
