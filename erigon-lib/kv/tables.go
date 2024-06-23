0c6f428f6c5ef7c1b7b4b171ecc2587e677e9cd0
alex.sharov <AskAlexSharov@gmail.com>
2024-06-23 17:39:51 +0700

    save

--- erigon-lib/kv/tables.go
+++ erigon-lib/kv/tables.go
@@ -969,8 +969,9 @@ const (
 )
 
 const (
-	ReceiptsAppendable Appendable = 0
-	AppendableLen      Appendable = 1
+	//ReceiptsAppendable Appendable = 0
+	//AppendableLen      Appendable = 1
+	AppendableLen Appendable = 0
 )
 
 func (iip InvertedIdxPos) String() string {
@@ -1020,8 +1021,8 @@ func String2Domain(in string) (Domain, error) {
 
 func (iip Appendable) String() string {
 	switch iip {
-	case ReceiptsAppendable:
-		return "receipts"
+	//case ReceiptsAppendable:
+	//	return "receipts"
 	default:
 		return "unknown Appendable"
 	}
@@ -1029,8 +1030,8 @@ func (iip Appendable) String() string {
 
 func String2Appendable(in string) (Appendable, error) {
 	switch in {
-	case "receipts":
-		return ReceiptsAppendable, nil
+	//case "receipts":
+	//	return ReceiptsAppendable, nil
 	default:
 		return 0, fmt.Errorf("unknown Appendable name: %s", in)
 	}
