d5245d512b8a4b4c3ac4d61ffbc99dad7acdf5c0
Minhyuk Kim <kimminhyuk1004@gmail.com>
2024-06-24 17:32:59 +0900

    Fix gas fee calculation for debug calls

--- turbo/adapter/ethapi/api.go
+++ turbo/adapter/ethapi/api.go
@@ -112,7 +112,7 @@ func (args *CallArgs) ToMessage(globalGasCap uint64, baseFee *uint256.Int) (type
 			gasFeeCap, gasTipCap = gasPrice, gasPrice
 		} else {
 			// User specified 1559 gas fields (or none), use those
-			gasFeeCap = uint256.MustFromBig(baseFee.ToBig())
+			gasFeeCap = new(uint256.Int)
 			if args.MaxFeePerGas != nil {
 				overflow := gasFeeCap.SetFromBig(args.MaxFeePerGas.ToInt())
 				if overflow {
