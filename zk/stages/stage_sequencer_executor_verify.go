da394079604d926ccb461af22dd2e46f5bab920e
Kamen Stoykov <24619432+kstoykov@users.noreply.github.com>
2024-06-24 19:43:11 +0300

    Panic on failed batch verification (#649)
    * panic-on-failed-batch-verification
    
    * type
    
    * stop the node from creating blocks

--- zk/stages/stage_sequencer_executor_verify.go
+++ zk/stages/stage_sequencer_executor_verify.go
@@ -3,6 +3,7 @@ package stages
 import (
 	"context"
 	"math"
+	"time"
 
 	"bytes"
 	"errors"
@@ -228,7 +229,11 @@ func SpawnSequencerExecutorVerifyStage(
 				cfg.verifier.CancelAllRequestsUnsafe()
 				return nil
 			} else {
-				log.Info(fmt.Sprintf("[%s] identified an invalid batch but limbo is disabled so mark is as valid anyway and continue", s.LogPrefix()), "batch", response.BatchNumber)
+				// this infinite loop will make the node to print the error once every minute therefore preventing it for creating new blocks
+				for {
+					time.Sleep(time.Minute)
+					log.Error(fmt.Sprintf("[%s] identified an invalid batch with number %d", s.LogPrefix(), response.BatchNumber))
+				}
 			}
 		}
 
