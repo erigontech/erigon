382f1a9f38dbd19ead5fdb7bdc71466ab5768da2
Kewei <kewei.train@gmail.com>
2024-06-25 16:58:29 +0800

    Support external mev builder (#10492)
    - enable builder mode by configuring `--beacon.api=builder` and setup
    `--mev-relay-url` to run in builder mode
    ([code](https://github.com/ledgerwatch/erigon/pull/10492/files#diff-aca4b9462813c7b1d2ff5d7929408096522e963a92a092fcb1ffc3240c9e43b9R111))
    
    - Set mev relay url by `--caplin.mev-relay-url` as run erigon if would
    like to run caplin in builder mode.
    eg:
    ```
    --caplin.mev-relay-url=https://boost-relay-sepolia.flashbots.net/
    ```
    - Follow builder specs
    [here](https://ethereum.github.io/builder-specs/#/) to impl [builder
    client](https://github.com/ledgerwatch/erigon/pull/10492/files#diff-d2932cc9922a090127fd132ae180802b3686ab83e26f1946c985f4c0da6ffc23R12)
    
    - validator registration api `/v1/validator/register_validator`
    
    - choose proposed block (either blinded or unblinded block) api
    `/v3/validator/blocks/{slot}`
    
    - post blinded block api `/v1/beacon/blinded_blocks`
    `/v2/beacon/blinded_blocks`

--- cl/beacon/handler/handler.go
+++ cl/beacon/handler/handler.go
@@ -14,6 +14,7 @@ import (
 	"github.com/ledgerwatch/erigon/cl/beacon/beacon_router_configuration"
 	"github.com/ledgerwatch/erigon/cl/beacon/beaconevents"
 	"github.com/ledgerwatch/erigon/cl/beacon/beaconhttp"
+	"github.com/ledgerwatch/erigon/cl/beacon/builder"
 	"github.com/ledgerwatch/erigon/cl/beacon/synced_data"
 	"github.com/ledgerwatch/erigon/cl/clparams"
 	"github.com/ledgerwatch/erigon/cl/cltypes"
@@ -87,6 +88,7 @@ type ApiHandler struct {
 	voluntaryExitService             services.VoluntaryExitService
 	blsToExecutionChangeService      services.BLSToExecutionChangeService
 	proposerSlashingService          services.ProposerSlashingService
+	builderClient                    builder.BuilderClient
 }
 
 func NewApiHandler(
@@ -119,6 +121,7 @@ func NewApiHandler(
 	voluntaryExitService services.VoluntaryExitService,
 	blsToExecutionChangeService services.BLSToExecutionChangeService,
 	proposerSlashingService services.ProposerSlashingService,
+	builderClient builder.BuilderClient,
 ) *ApiHandler {
 	blobBundles, err := lru.New[common.Bytes48, BlobBundle]("blobs", maxBlobBundleCacheSize)
 	if err != nil {
@@ -159,6 +162,7 @@ func NewApiHandler(
 		voluntaryExitService:             voluntaryExitService,
 		blsToExecutionChangeService:      blsToExecutionChangeService,
 		proposerSlashingService:          proposerSlashingService,
+		builderClient:                    builderClient,
 	}
 }
 
@@ -211,6 +215,9 @@ func (a *ApiHandler) init() {
 			}
 			if a.routerCfg.Beacon {
 				r.Route("/beacon", func(r chi.Router) {
+					if a.routerCfg.Builder {
+						r.Post("/blinded_blocks", beaconhttp.HandleEndpointFunc(a.PostEthV1BlindedBlocks))
+					}
 					r.Route("/rewards", func(r chi.Router) {
 						r.Post("/sync_committee/{block_id}", beaconhttp.HandleEndpointFunc(a.PostEthV1BeaconRewardsSyncCommittees))
 						r.Get("/blocks/{block_id}", beaconhttp.HandleEndpointFunc(a.GetEthV1BeaconRewardsBlocks))
@@ -270,7 +277,7 @@ func (a *ApiHandler) init() {
 						r.Get("/proposer/{epoch}", beaconhttp.HandleEndpointFunc(a.getDutiesProposer))
 						r.Post("/sync/{epoch}", beaconhttp.HandleEndpointFunc(a.getSyncDuties))
 					})
-					r.Get("/blinded_blocks/{slot}", http.NotFound)
+					r.Get("/blinded_blocks/{slot}", http.NotFound) // deprecated
 					r.Get("/attestation_data", beaconhttp.HandleEndpointFunc(a.GetEthV1ValidatorAttestationData))
 					r.Get("/aggregate_attestation", beaconhttp.HandleEndpointFunc(a.GetEthV1ValidatorAggregateAttestation))
 					r.Post("/aggregate_and_proofs", a.PostEthV1ValidatorAggregatesAndProof)
@@ -280,6 +287,9 @@ func (a *ApiHandler) init() {
 					r.Post("/contribution_and_proofs", a.PostEthV1ValidatorContributionsAndProofs)
 					r.Post("/prepare_beacon_proposer", a.PostEthV1ValidatorPrepareBeaconProposal)
 					r.Post("/liveness/{epoch}", beaconhttp.HandleEndpointFunc(a.liveness))
+					if a.routerCfg.Builder {
+						r.Post("/register_validator", beaconhttp.HandleEndpointFunc(a.PostEthV1BuilderRegisterValidator))
+					}
 				})
 			}
 
@@ -297,6 +307,9 @@ func (a *ApiHandler) init() {
 				r.Route("/beacon", func(r chi.Router) {
 					r.Get("/blocks/{block_id}", beaconhttp.HandleEndpointFunc(a.GetEthV1BeaconBlock))
 					r.Post("/blocks", a.PostEthV2BeaconBlocks)
+					if a.routerCfg.Builder {
+						r.Post("/blinded_blocks", beaconhttp.HandleEndpointFunc(a.PostEthV2BlindedBlocks))
+					}
 				})
 			}
 			if a.routerCfg.Validator {
