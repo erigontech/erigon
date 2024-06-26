970b8b2ae1d99eaf9159721e00c98e332088e5d1
Andrew Ashikhmin <34320705+yperbasis@users.noreply.github.com>
2024-06-26 20:18:14 +0200

    Update EIP-2935 for Prague devnet1

--- consensus/misc/eip2935.go
+++ consensus/misc/eip2935.go
@@ -18,20 +18,6 @@ func StoreBlockHashesEip2935(header *types.Header, state *state.IntraBlockState,
 		return
 	}
 	storeHash(headerNum-1, header.ParentHash, state)
-	// If this is the fork block, add the parent's direct `HISTORY_SERVE_WINDOW - 1` ancestors as well
-	parent := headerReader.GetHeader(header.ParentHash, headerNum-1)
-	if parent.Time < config.PragueTime.Uint64() {
-		p := headerNum - 1
-		window := params.BlockHashHistoryServeWindow - 1
-		if p < window {
-			window = p
-		}
-		for i := window; i > 0; i-- {
-			p = p - 1
-			storeHash(p, parent.ParentHash, state)
-			parent = headerReader.GetHeader(parent.ParentHash, p)
-		}
-	}
 }
 
 func storeHash(num uint64, hash libcommon.Hash, state *state.IntraBlockState) {
--- core/vm/eips.go
+++ core/vm/eips.go
@@ -37,7 +37,6 @@ var activators = map[int]func(*JumpTable){
 	3855: enable3855,
 	3529: enable3529,
 	3198: enable3198,
-	2935: enable2935,
 	2929: enable2929,
 	2200: enable2200,
 	1884: enable1884,
@@ -321,9 +320,4 @@ func enable7516(jt *JumpTable) {
 		numPop:      0,
 		numPush:     1,
 	}
-}
-
-// enable2935 applies EIP-2935 (Historical block hashes in state)
-func enable2935(jt *JumpTable) {
-	jt[BLOCKHASH].execute = opBlockhash2935
 }
