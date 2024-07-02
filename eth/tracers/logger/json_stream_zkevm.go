a302eef6ddad2c8efe3a9693a69f608a30ab8579
Valentin Staykov <79150443+V-Staykov@users.noreply.github.com>
2024-07-02 14:09:10 +0300

    685 rpc debug tracetransaction memory fields contains extra zeroed memory values (#686)
    * added test log contract
    
    * don't print empty byte lines in trace memory after the last one with a value != 0

--- eth/tracers/logger/json_stream_zkevm.go
+++ eth/tracers/logger/json_stream_zkevm.go
@@ -200,26 +200,38 @@ func (l *JsonStreamLogger_ZkEvm) writeMemory(memory *vm.Memory) {
 	if !l.cfg.DisableMemory {
 		memData := memory.Data()
 
-		//[zkevm] don't print empty bytes in memory array
-		isMemoryEmpty := true
-		for i := 0; i < len(memData); i++ {
-			if memData[i] != 0 {
-				isMemoryEmpty = false
-				break
+		//[zkevm] don't print empty bytes in memory array after the last non-empty byte line
+		filteredByteLines := [][]byte{}
+		foundValueLine := false
+		for i := len(memData); i-32 >= 0; i -= 32 {
+			bytes := memData[i-32 : i]
+
+			isEmpty := true
+			if !foundValueLine {
+				for _, b := range bytes {
+					if b != 0 {
+						isEmpty = false
+						foundValueLine = true
+						break
+					}
+				}
+			}
+
+			if !isEmpty || foundValueLine {
+				filteredByteLines = append(filteredByteLines, bytes)
 			}
 		}
 
 		l.stream.WriteMore()
 		l.stream.WriteObjectField("memory")
 		l.stream.WriteArrayStart()
-		if !isMemoryEmpty {
-			for i := 0; i+32 <= len(memData); i += 32 {
-				if i > 0 {
-					l.stream.WriteMore()
-				}
-				l.stream.WriteString(string(l.hexEncodeBuf[0:hex.Encode(l.hexEncodeBuf[:], memData[i:i+32])]))
+		for i := len(filteredByteLines) - 1; i >= 0; i-- {
+			if i != len(filteredByteLines)-1 {
+				l.stream.WriteMore()
 			}
+			l.stream.WriteString(string(l.hexEncodeBuf[0:hex.Encode(l.hexEncodeBuf[:], filteredByteLines[i])]))
 		}
+
 		l.stream.WriteArrayEnd()
 	}
 }
--- zk/debug_tools/test-contracts/scripts/emitlog.js
+++ zk/debug_tools/test-contracts/scripts/emitlog.js
@@ -1,17 +1,17 @@
 async function main() {
 try {
    // Get the ContractFactory of your SimpleContract
-   const SimpleContract = await hre.ethers.getContractFactory("MulMod");
+   const SimpleContract = await hre.ethers.getContractFactory("EmitLog");
 
    // Deploy the contract
    const contract = await SimpleContract.deploy();
    // Wait for the deployment transaction to be mined
    await contract.waitForDeployment();
 
-   console.log(`MulMod deployed to: ${await contract.getAddress()}`);
+   console.log(`EmitLog deployed to: ${await contract.getAddress()}`);
 
-   const result = await contract.modx();
-   console.log(result);
+   const result = await contract.emitLogs();
+   console.log('emitLogs method call transaction: ', result.hash);
  } catch (error) {
    console.error(error);
    process.exit(1);
