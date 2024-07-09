ac3928c2af3c7006759d3400811f7393924afbde
alex.sharov <AskAlexSharov@gmail.com>
2024-07-09 12:10:26 +0700

    save

--- erigon-lib/state/aggregator.go
+++ erigon-lib/state/aggregator.go
@@ -34,6 +34,7 @@ import (
 
 	"github.com/RoaringBitmap/roaring/roaring64"
 	"github.com/c2h5oh/datasize"
+	btree2 "github.com/tidwall/btree"
 	rand2 "golang.org/x/exp/rand"
 	"golang.org/x/sync/errgroup"
 	"golang.org/x/sync/semaphore"
@@ -393,38 +394,25 @@ func (a *Aggregator) Files() []string {
 	return ac.Files()
 }
 func (a *Aggregator) LS() {
+	doLS := func(dirtyFiles *btree2.BTreeG[*filesItem]) {
+		dirtyFiles.Walk(func(items []*filesItem) bool {
+			for _, item := range items {
+				if item.decompressor == nil {
+					continue
+				}
+				log.Info("[agg] ", "f", item.decompressor.FileName(), "words", item.decompressor.Count())
+			}
+			return true
+		})
+	}
 	for _, d := range a.d {
-		d.dirtyFiles.Walk(func(items []*filesItem) bool {
-			for _, item := range items {
-				if item.decompressor == nil {
-					continue
-				}
-				log.Info("[agg] ", "f", item.decompressor.FileName(), "words", item.decompressor.Count())
-			}
-			return true
-		})
+		doLS(d.dirtyFiles)
 	}
-	for _, d := range a.iis {
-		d.dirtyFiles.Walk(func(items []*filesItem) bool {
-			for _, item := range items {
-				if item.decompressor == nil {
-					continue
-				}
-				log.Info("[agg] ", "f", item.decompressor.FileName(), "words", item.decompressor.Count())
-			}
-			return true
-		})
+	for _, ii := range a.iis {
+		doLS(ii.dirtyFiles)
 	}
-	for _, d := range a.ap {
-		d.dirtyFiles.Walk(func(items []*filesItem) bool {
-			for _, item := range items {
-				if item.decompressor == nil {
-					continue
-				}
-				log.Info("[agg] ", "f", item.decompressor.FileName(), "words", item.decompressor.Count())
-			}
-			return true
-		})
+	for _, ap := range a.ap {
+		doLS(ap.dirtyFiles)
 	}
 }
 
