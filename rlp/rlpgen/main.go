9be8724666e79d82e1d91c1f0e6c99a650d4566e
alex.sharov <AskAlexSharov@gmail.com>
2024-07-11 15:37:04 +0700

    save

--- rlp/rlpgen/main.go
+++ rlp/rlpgen/main.go
@@ -27,7 +27,7 @@ import (
 	"golang.org/x/tools/go/packages"
 )
 
-const pathOfPackageRLP = "github.com/ethereum/go-ethereum/rlp"
+const pathOfPackageRLP = "github.com/ledgerwatch/erigon/rlp"
 
 func main() {
 	var (
