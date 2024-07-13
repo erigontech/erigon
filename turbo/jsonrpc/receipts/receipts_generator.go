6d3b7da11ac96f6577d61b066ba47cc921d6a6c9
JkLondon <ilya@mikheev.fun>
2024-07-13 18:13:56 +0300

    save

--- turbo/jsonrpc/receipts/receipts_generator.go
+++ turbo/jsonrpc/receipts/receipts_generator.go
@@ -64,7 +64,7 @@ func (g *Generator) GetReceipts(ctx context.Context, cfg *chain.Config, tx kv.Tx
 		}
 		return h
 	}
-	header := block.Header()
+	header := block.HeaderNoCopy()
 	for i, txn := range block.Transactions() {
 		ibs.SetTxContext(txn.Hash(), block.Hash(), i)
 		receipt, _, err := core.ApplyTransaction(cfg, core.GetHashFn(header, getHeader), engine, nil, gp, ibs, noopWriter, header, txn, usedGas, usedBlobGas, vm.Config{})
