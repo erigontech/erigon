a212cf2e238663d8dd2add7cc75dcc1247fec888
alex.sharov <AskAlexSharov@gmail.com>
2024-07-16 09:29:15 +0700

    save

--- tests/init_test.go
+++ tests/init_test.go
@@ -247,7 +247,7 @@ func (tm *testMatcher) runTestFile(t *testing.T, path, name string, runTest inte
 			name := name + "/" + key
 			subTestName := key
 			if len(subTestName) > 100 {
-				subTestName = fmt.Sprintf("%d", i)
+				subTestName = fmt.Sprintf("%s...%d", key[:90], i)
 			}
 			t.Run(subTestName, func(t *testing.T) {
 				if r, _ := tm.findSkip(name); r != "" {
