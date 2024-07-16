2a11fd325d3e0a783acd694ed7cbf9c64e205c3c
dvovk <vovk.dimon@gmail.com>
2024-07-16 14:37:12 +0100

    fix

--- erigon-lib/diagnostics/network.go
+++ erigon-lib/diagnostics/network.go
@@ -263,7 +263,5 @@ func (d *DiagnosticClient) runCollectPeersStatistics(rootCtx context.Context) {
 }
 
 func (d *DiagnosticClient) Peers() map[string]PeerStatistics {
-	d.mu.Lock()
-	defer d.mu.Unlock()
-	return d.peersStats.getPeers()
+	return d.peersStats.GetPeers()
 }
