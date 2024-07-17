34770f102ed815472e5b42912026be038a1f5920
antonis19 <antonis19@users.noreply.github.com>
2024-07-17 12:51:42 +0300

    gofmt

--- eth/stagedsync/stage_execute.go
+++ eth/stagedsync/stage_execute.go
@@ -200,10 +200,10 @@ func unwindExec3(u *UnwindState, s *StageState, txc wrap.TxContainer, ctx contex
 
 	if changeset == nil {
 		if u.CurrentBlockNumber < u.UnwindPoint {
-		   return fmt.Errorf("can't unwind(%d -> %d) because forward-unwind requested", u.CurrentBlockNumber, u.UnwindPoint)
+			return fmt.Errorf("can't unwind(%d -> %d) because forward-unwind requested", u.CurrentBlockNumber, u.UnwindPoint)
 		}
 		if u.CurrentBlockNumber == u.UnwindPoint {
-		   return fmt.Errorf("can't unwind(%d -> %d) because 0 blocks unwind requested", u.CurrentBlockNumber, u.UnwindPoint)
+			return fmt.Errorf("can't unwind(%d -> %d) because 0 blocks unwind requested", u.CurrentBlockNumber, u.UnwindPoint)
 		}
 		return fmt.Errorf("can't unwind(%d -> %d) because no changesets in db", u.CurrentBlockNumber, u.UnwindPoint)
 	}
