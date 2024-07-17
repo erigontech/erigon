1b1079926ce17a8439b0e86e8bde7c2bbc13b061
Andrew Ashikhmin <34320705+yperbasis@users.noreply.github.com>
2024-07-17 14:55:03 +0200

    move secp256k1 to erigontech (#11209)

--- crypto/signature_cgo.go
+++ crypto/signature_cgo.go
@@ -26,8 +26,9 @@ import (
 	"crypto/elliptic"
 	"fmt"
 
+	"github.com/erigontech/secp256k1"
+
 	"github.com/ledgerwatch/erigon/common/math"
-	"github.com/ledgerwatch/secp256k1"
 )
 
 // Ecrecover returns the uncompressed public key that created the given signature.
