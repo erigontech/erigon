bd01d60c3310508ecfbbbf4b00f04bc1ba818b50
taratorio <94537774+taratorio@users.noreply.github.com>
2024-07-19 19:15:59 +0100

    cosmetic

--- polygon/heimdall/span_accum_producer_priorities.go
+++ polygon/heimdall/span_accum_producer_priorities.go
@@ -1,8 +1,22 @@
+// Copyright 2024 The Erigon Authors
+// This file is part of Erigon.
+//
+// Erigon is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Erigon is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Erigon. If not, see <http://www.gnu.org/licenses/>.
+
 package heimdall
 
 import "github.com/erigontech/erigon/polygon/bor/valset"
-
-var _ Entity = (*SpanAccumProducerPriorities)(nil)
 
 // SpanAccumProducerPriorities represents the block producer selection at each epoch
 // with their corresponding accumulated ProposerPriority.
@@ -31,6 +45,8 @@ type SpanAccumProducerPriorities struct {
 	EndBlock   uint64
 	Producers  []*valset.Validator
 }
+
+var _ Entity = (*SpanAccumProducerPriorities)(nil)
 
 func (s *SpanAccumProducerPriorities) RawId() uint64 {
 	return uint64(s.SpanId)
