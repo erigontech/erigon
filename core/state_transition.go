0770b1b928bb379499682c3830f92850cddb5cbd
lupin012 <58134934+lupin012@users.noreply.github.com>
2024-07-20 10:21:46 +0200

    tracer: add support bailout on evm.create() (#11237)
    Add bailout mgt on evm.create() method (it is already mgt on evm.cal()).
    When approved I will create PR on rel 2.60.x

--- core/state_transition.go
+++ core/state_transition.go
@@ -419,7 +419,7 @@ func (st *StateTransition) TransitionDb(refunds bool, gasBailout bool) (*Executi
 		// nonce to calculate the address of the contract that is being created
 		// It does get incremented inside the `Create` call, after the computation
 		// of the contract's address, but before the execution of the code.
-		ret, _, st.gasRemaining, vmerr = st.evm.Create(sender, st.data, st.gasRemaining, st.value)
+		ret, _, st.gasRemaining, vmerr = st.evm.Create(sender, st.data, st.gasRemaining, st.value, bailout)
 	} else {
 		// Increment the nonce for the next transaction
 		st.state.SetNonce(msg.From(), st.state.GetNonce(sender.Address())+1)
