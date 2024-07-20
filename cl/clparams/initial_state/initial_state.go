dead2f7864858d945e25c85baaa80cf5e4246619
Giulio <giulio.rebuffo@gmail.com>
2024-07-20 22:18:38 +0200

    save

--- cl/clparams/initial_state/initial_state.go
+++ cl/clparams/initial_state/initial_state.go
@@ -74,7 +74,7 @@ func GetGenesisState(network clparams.NetworkType) (*state.CachingBeaconState, e
 		if err != nil {
 			return nil, err
 		}
-		if err := returnState.DecodeSSZ(encodedState, int(clparams.HoleskyNetwork)); err != nil {
+		if err := returnState.DecodeSSZ(encodedState, int(clparams.BellatrixVersion)); err != nil {
 			return nil, err
 		}
 	default:
