erigontech/erigon

commit 2d3db5bcb849d72eda1db5570b39920186c8b805

up goreleaser version (#10726)

Makefile
@@ -267,7 +267,7 @@ install:
 	@ls -al "$(DIST)"
 
 PACKAGE_NAME          := github.com/ledgerwatch/erigon
-GOLANG_CROSS_VERSION  ?= v1.21.6
+GOLANG_CROSS_VERSION  ?= v1.22.4
 
 .PHONY: release-dry-run
 release-dry-run: git-submodules

commit e2df67877c5faaca25a6f66387f486a4519c3f01

fix goreleaser after update (#11281)
Fixes
https://github.com/erigontech/erigon/actions/runs/10041015251/job/27748204997#step:9:72
(caused by PR #10726 apparently). See also
https://github.com/goreleaser/goreleaser-cross-example/blob/master/Makefile

Makefile
@@ -292,7 +292,7 @@ release-dry-run: git-submodules
 		-v `pwd`:/go/src/$(PACKAGE_NAME) \
 		-w /go/src/$(PACKAGE_NAME) \
 		ghcr.io/goreleaser/goreleaser-cross:${GOLANG_CROSS_VERSION} \
-		--clean --skip-validate --skip-publish
+		--clean --skip=validate --skip=publish
 
 .PHONY: release
 release: git-submodules
@@ -307,7 +307,7 @@ release: git-submodules
 		-v `pwd`:/go/src/$(PACKAGE_NAME) \
 		-w /go/src/$(PACKAGE_NAME) \
 		ghcr.io/goreleaser/goreleaser-cross:${GOLANG_CROSS_VERSION} \
-		--clean --skip-validate
+		--clean --skip=validate
 
 	@docker image push --all-tags thorax/erigon
 	@docker image push --all-tags ghcr.io/erigontech/erigon