04a82a84aba6746d688710d57391a744297511a2
Giulio <giulio.rebuffo@gmail.com>
2024-07-22 21:17:49 +0200

    save

--- turbo/cli/flags.go
+++ turbo/cli/flags.go
@@ -152,7 +152,7 @@ var (
 	SyncLoopBlockLimitFlag = cli.UintFlag{
 		Name:  "sync.loop.block.limit",
 		Usage: "Sets the maximum number of blocks to process per loop iteration",
-		Value: 5_000,
+		Value: 1_000,
 	}
 
 	UploadLocationFlag = cli.StringFlag{
