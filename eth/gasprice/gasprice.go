0e88a11ae4bf72f8c00ae95ab6b9cc6edf79e090
Manav Darji <manavdarji.india@gmail.com>
2024-07-23 22:35:58 +0530

    PIP-35: enforce 25gwei gas config for all polygon chains for erigon 2 (#11295)
    Cherry-picked from https://github.com/erigontech/erigon/pull/11294
    
    > Remove the checks for amoy added previously to prepare for mainnet
    release.
    > Sets `txpool.pricelimit`, `miner.gasprice` and `gpo.ignoreprice` to
    25gwei for all polygon based networks.

--- eth/gasprice/gasprice.go
+++ eth/gasprice/gasprice.go
@@ -26,7 +26,6 @@ import (
 	"github.com/ledgerwatch/erigon-lib/chain"
 	libcommon "github.com/ledgerwatch/erigon-lib/common"
 	"github.com/ledgerwatch/erigon/eth/gasprice/gaspricecfg"
-	"github.com/ledgerwatch/erigon/params"
 	"github.com/ledgerwatch/log/v3"
 
 	"github.com/ledgerwatch/erigon/core/types"
@@ -286,9 +285,8 @@ func (s *sortingHeap) Pop() interface{} {
 }
 
 // setBorDefaultGpoIgnorePrice enforces gpo IgnorePrice to be equal to BorDefaultGpoIgnorePrice (25gwei by default)
-// only for polygon amoy network.
 func setBorDefaultGpoIgnorePrice(chainConfig *chain.Config, gasPriceConfig gaspricecfg.Config) {
-	if chainConfig.Bor != nil && chainConfig.ChainID.Cmp(params.AmoyChainConfig.ChainID) == 0 && gasPriceConfig.IgnorePrice != gaspricecfg.BorDefaultGpoIgnorePrice {
+	if chainConfig.Bor != nil && gasPriceConfig.IgnorePrice != gaspricecfg.BorDefaultGpoIgnorePrice {
 		log.Warn("Sanitizing invalid bor gasprice oracle ignore price", "provided", gasPriceConfig.IgnorePrice, "updated", gaspricecfg.BorDefaultGpoIgnorePrice)
 		gasPriceConfig.IgnorePrice = gaspricecfg.BorDefaultGpoIgnorePrice
 	}
