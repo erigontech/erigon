6e0e6cc0e6a0cdec682c68210b5b5b350f9acdd0
JkLondon <ilya@mikheev.fun>
2024-07-24 13:11:09 +0300

    save

--- eth/backend_test.go
+++ eth/backend_test.go
@@ -2,6 +2,7 @@ package eth
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/require"
 	"os"
 	"path/filepath"
 	"testing"
@@ -9,64 +10,42 @@ import (
 
 func TestRemoveContents(t *testing.T) {
 	tmpDirName := t.TempDir()
-	err := os.RemoveAll(tmpDirName)
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = os.Mkdir(tmpDirName, 0750)
-	if err != nil {
-		t.Fatal(err)
-	}
 	//t.Logf("creating %s/root...", rootName)
 	rootName := filepath.Join(tmpDirName, "root")
-	err = os.Mkdir(rootName, 0750)
-	if err != nil {
-		t.Fatal(err)
-	}
+	err := os.Mkdir(rootName, 0750)
+	require.NoError(t, err)
 	//fmt.Println("OK")
 	for i := 0; i < 3; i++ {
 		outerName := filepath.Join(rootName, fmt.Sprintf("outer_%d", i+1))
 		//t.Logf("creating %s... ", outerName)
 		err = os.Mkdir(outerName, 0750)
-		if err != nil {
-			t.Fatal(err)
-		}
+		require.NoError(t, err)
 		//t.Logf("OK")
 		for j := 0; j < 2; j++ {
 			innerName := filepath.Join(outerName, fmt.Sprintf("inner_%d", j+1))
 			//t.Logf("creating %s... ", innerName)
 			err = os.Mkdir(innerName, 0750)
-			if err != nil {
-				t.Fatal(err)
-			}
+			require.NoError(t, err)
 			//t.Log("OK")
 			for k := 0; k < 2; k++ {
 				innestName := filepath.Join(innerName, fmt.Sprintf("innest_%d", k+1))
 				//t.Logf("creating %s... ", innestName)
-				err := os.Mkdir(innestName, 0750)
-				if err != nil {
-					t.Fatal(err)
-				}
+				err = os.Mkdir(innestName, 0750)
+				require.NoError(t, err)
 				//t.Log("OK")
 			}
 		}
 	}
 	list, err := os.ReadDir(rootName)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if len(list) != 3 {
-		t.Fatal("expected 3 dirs got ", len(list))
-	}
+	require.NoError(t, err)
+
+	require.Len(t, list, 3)
+
 	err = RemoveContents(rootName)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
+
 	list, err = os.ReadDir(rootName)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if len(list) != 0 {
-		t.Fatal("expected 0 dirs got ", len(list))
-	}
+	require.NoError(t, err)
+
+	require.Len(t, list, 0)
 }
