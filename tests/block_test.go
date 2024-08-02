e1406c502152783318cd7de831ae0169ba04dee4
sudeepdino008 <sudeepdino008@gmail.com>
2024-08-02 13:17:34 +0530

    minor change for testing

--- tests/block_test.go
+++ tests/block_test.go
@@ -45,7 +45,7 @@ func TestBlockchain(t *testing.T) {
 	bt.skipLoad(`^TransitionTests/bcArrowGlacierToMerge/powToPosBlockRejection\.json`)
 	bt.skipLoad(`^TransitionTests/bcFrontierToHomestead/blockChainFrontierWithLargerTDvsHomesteadBlockchain\.json`)
 
-	// TODO: HistoryV3: doesn't produce receipts on execution by design. But maybe we can Generate them on-the fly (on history) and enable this tests
+	// TODO: HistoryV3: doesn't produce receipts on execution by design. But maybe w can Generate them on-the fly (on history) and enable this tests
 	bt.skipLoad(`^InvalidBlocks/bcInvalidHeaderTest/log1_wrongBloom\.json`)
 	bt.skipLoad(`^InvalidBlocks/bcInvalidHeaderTest/wrongReceiptTrie\.json`)
 	bt.skipLoad(`^InvalidBlocks/bcInvalidHeaderTest/wrongGasUsed\.json`)
