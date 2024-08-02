92495b15c1184a4ed9f74b089e21b30ee7ba065e
kewei <kewei.train@gmail.com>
2024-08-02 23:19:16 +0800

    log

--- cl/beacon/beaconevents/state_feed.go
+++ cl/beacon/beaconevents/state_feed.go
@@ -1,6 +1,9 @@
 package beaconevents
 
-import ethevent "github.com/erigontech/erigon/event"
+import (
+	"github.com/erigontech/erigon-lib/log/v3"
+	ethevent "github.com/erigontech/erigon/event"
+)
 
 type stateFeed struct {
 	feed *ethevent.Feed
@@ -17,6 +20,7 @@ func (f *stateFeed) Subscribe(channel chan *EventStream) ethevent.Subscription {
 }
 
 func (f *stateFeed) SendHead(value *HeadData) int {
+	log.Info("[test] send head", "value", value)
 	return f.feed.Send(&EventStream{
 		Event: StateHead,
 		Data:  value,
