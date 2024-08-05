f74d14c715045497c8cae3251a556bb319c2c829
alex.sharov <AskAlexSharov@gmail.com>
2024-08-05 13:24:06 +0700

    save

--- erigon-lib/state/domain.go
+++ erigon-lib/state/domain.go
@@ -714,7 +714,7 @@ type DomainRoTx struct {
 	lEachCacheHit, lEachCacheTotal [LevelsWithLRU]int
 }
 
-const LevelsWithLRU = 1
+const LevelsWithLRU = 2
 
 func domainReadMetric(name kv.Domain, level int) metrics.Summary {
 	if level > 4 {
