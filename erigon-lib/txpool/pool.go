59fb3ef8884938e5300f97bbd6386e5b0711390c
alex.sharov <AskAlexSharov@gmail.com>
2024-08-06 12:05:03 +0700

    save

--- erigon-lib/txpool/pool.go
+++ erigon-lib/txpool/pool.go
@@ -703,7 +703,7 @@ func (p *TxPool) getCachedBlobTxnLocked(tx kv.Tx, hash []byte) (*metaTx, error) 
 
 	_, err = parseCtx.ParseTransaction(txRlp, 0, txSlot, nil, false, true, nil)
 	if err != nil {
-		log.Warn("[dbg] getCachedBlobTxnLocked", "err", err)
+		log.Warn("[dbg] getCachedBlobTxnLocked", "err", err, "len(v)", Len(v))
 	}
 
 	return newMetaTx(txSlot, false, 0), nil
