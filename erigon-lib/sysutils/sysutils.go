88569bd21c445c9eb7430c64986ccd6022266e8a
dvovk <vovk.dimon@gmail.com>
2024-08-07 18:15:05 +0100

    added collecting info about all processes

--- erigon-lib/sysutils/sysutils.go
+++ erigon-lib/sysutils/sysutils.go
@@ -17,21 +17,24 @@
 package sysutils
 
 import (
-	"fmt"
-	"sort"
 	"time"
 
 	"github.com/erigontech/erigon-lib/log/v3"
-	"github.com/shirou/gopsutil/v4/cpu"
 	"github.com/shirou/gopsutil/v4/process"
 )
 
 type ProcessInfo struct {
-	Pid       int32
-	Name      string
-	CPUUsage  float64
-	Memory    float32
-	StartTime int64
+	Pid      int32
+	Name     string
+	CPUUsage float64
+	Memory   float32
+}
+
+type ProcessMerge struct {
+	CPUUsage float64
+	Memory   float32
+	Times    int
+	Name     string
 }
 
 const (
@@ -40,126 +43,128 @@ const (
 	usageThreshold = 0.05
 )
 
-func AllProcessesCPU() {
+func GetProcessesInfo() []*ProcessInfo {
 	procs, err := process.Processes()
 	if err != nil {
-		log.Fatalf("Error retrieving processes: %v", err)
+		log.Debug("[Sysutil] Error retrieving processes: %v", err)
 	}
 
-	// Collect processes and calculate average stats.
-	allProcsRepeats := make([][]ProcessInfo, 0, iterations)
+	return averageProceses(procs)
+}
 
-	// Collect all processes 5 times with a delay of 2 seconds to calculate average stats.
+func AverageProceses(procs []*process.Process) []*ProcessInfo {
+	return averageProceses(procs)
+}
+
+func averageProceses(procs []*process.Process) []*ProcessInfo {
+	// Collect processes and calculate average stats.
+	allProcsRepeats := make([][]*ProcessInfo, 0, iterations)
+
+	// Collect all processes N times with a delay of N seconds to calculate average stats.
 	for i := 0; i < iterations; i++ {
-		processCPUUsage := allProcesses(procs)
-		allProcsRepeats = append(allProcsRepeats, processCPUUsage)
+		processes := allProcesses(procs)
+		allProcsRepeats = append(allProcsRepeats, processes)
 		time.Sleep(sleepSeconds * time.Second)
 	}
 
 	// Calculate average stats.
 	averageProcs := mergeProcesses(allProcsRepeats)
+	averageProcs = removeProcessesAboveThreshold(averageProcs, usageThreshold)
 
-	// Sort by CPU usage in descending order and remove processes with negligible CPU and memory usage.
-	sort.Slice(averageProcs, func(i, j int) bool {
-		return averageProcs[i].CPUUsage > averageProcs[j].CPUUsage
-	})
-
-	// Filter out processes with negligible CPU or memory usage.
-	toDisplay := make([]ProcessInfo, 0, len(averageProcs))
-	for _, proc := range averageProcs {
-		if proc.CPUUsage > usageThreshold || proc.Memory > usageThreshold {
-			toDisplay = append(toDisplay, proc)
-		}
-	}
-
-	//print all
-	for idx, proc := range toDisplay {
-		fmt.Printf("#:%d PID: %d Name: %s CPU Usage: %.2f%% Memory: %.2f%% Start Time: %s\n", idx, proc.Pid, proc.Name, proc.CPUUsage, proc.Memory, time.Unix(proc.StartTime, 0))
-	}
-
-	// Optionally, you can also print total CPU usage for reference
-	totalCPUPercent, err := cpu.Percent(time.Second, false)
-	if err != nil {
-		log.Fatalf("Error retrieving total CPU usage: %v", err)
-	}
-	fmt.Printf("Total CPU Usage: %.2f%%\n", totalCPUPercent[0])
+	return averageProcs
 }
 
-// merge all processes infos into one array with average values
-func mergeProcesses(allProcsRepeats [][]ProcessInfo) []ProcessInfo {
+func RemoveProcessesAboveThreshold(processes []*ProcessInfo, treshold float64) []*ProcessInfo {
+	return removeProcessesAboveThreshold(processes, treshold)
+}
+
+func removeProcessesAboveThreshold(processes []*ProcessInfo, treshold float64) []*ProcessInfo {
+	// remove processes with CPU or Memory usage less than threshold
+	for i := 0; i < len(processes); i++ {
+		if processes[i].CPUUsage < treshold && processes[i].Memory < float32(treshold) {
+			processes = append(processes[:i], processes[i+1:]...)
+			i--
+		}
+	}
+	return processes
+}
+
+func MergeProcesses(allProcsRepeats [][]*ProcessInfo) []*ProcessInfo {
+	return mergeProcesses(allProcsRepeats)
+}
+
+func mergeProcesses(allProcsRepeats [][]*ProcessInfo) []*ProcessInfo {
 	if len(allProcsRepeats) == 0 || len(allProcsRepeats[0]) == 0 {
 		return nil
 	}
 
 	repeats := len(allProcsRepeats)
-	allProcessLength := len(allProcsRepeats[0])
-	resultArray := make([]ProcessInfo, 0, allProcessLength)
+	if repeats == 1 {
+		return allProcsRepeats[0]
+	}
 
-	for i := 0; i < allProcessLength; i++ {
-		firstProcess := allProcsRepeats[0][i]
-		totalCPUUsage := firstProcess.CPUUsage
-		totalMemory := firstProcess.Memory
+	prcmap := make(map[int32]*ProcessMerge)
 
-		if repeats > 1 {
-			for j := 1; j < repeats; j++ {
-				totalCPUUsage += allProcsRepeats[j][i].CPUUsage
-				totalMemory += allProcsRepeats[j][i].Memory
+	for _, procList := range allProcsRepeats {
+		for _, proc := range procList {
+			if prc, exists := prcmap[proc.Pid]; exists {
+				prc.CPUUsage += proc.CPUUsage
+				prc.Memory += proc.Memory
+				prc.Times++
+			} else {
+				prcmap[proc.Pid] = &ProcessMerge{
+					CPUUsage: proc.CPUUsage,
+					Memory:   proc.Memory,
+					Times:    1,
+					Name:     proc.Name,
+				}
 			}
-
-			totalCPUUsage /= float64(repeats)
-			totalMemory /= float32(repeats)
 		}
+	}
 
-		resultArray = append(resultArray, ProcessInfo{
-			Pid:       firstProcess.Pid,
-			Name:      firstProcess.Name,
-			CPUUsage:  totalCPUUsage,
-			Memory:    totalMemory,
-			StartTime: firstProcess.StartTime,
+	resultArray := make([]*ProcessInfo, 0, len(prcmap))
+
+	for pid, prc := range prcmap {
+		resultArray = append(resultArray, &ProcessInfo{
+			Pid:      pid,
+			Name:     prc.Name,
+			CPUUsage: prc.CPUUsage / float64(prc.Times),
+			Memory:   prc.Memory / float32(prc.Times),
 		})
 	}
 
 	return resultArray
 }
 
-func allProcesses(procs []*process.Process) []ProcessInfo {
-	// add process cpu usage data to array in order to sort it later
-	processCPUUsage := make([]ProcessInfo, 0)
+func allProcesses(procs []*process.Process) []*ProcessInfo {
+	processes := make([]*ProcessInfo, 0)
 
 	for _, proc := range procs {
-		// Get process ID
 		pid := proc.Pid
-		// Get process name
 		name, err := proc.Name()
 		if err != nil {
 			name = "Unknown"
 		}
 
-		// Get CPU percent
+		//remove gopls process as it is what we use to get info
+		if name == "gopls" {
+			continue
+		}
+
 		cpuPercent, err := proc.CPUPercent()
 		if err != nil {
-			//log.Printf("Error retrieving CPU percent for PID %d: %v Name: %s", pid, err, name)
+			log.Trace("[Sysutil] Error retrieving CPU percent for PID %d: %v Name: %s", pid, err, name)
 			continue
 		}
 
-		// Get memory percent
 		memPercent, err := proc.MemoryPercent()
 		if err != nil {
-			//log.Printf("Error retrieving memory percent for PID %d: %v Name: %s", pid, err, name)
+			log.Trace("[Sysutil] Error retrieving memory percent for PID %d: %v Name: %s", pid, err, name)
 			continue
 		}
 
-		// Get process start time
-		createTime, err := proc.CreateTime()
-		if err != nil {
-			//log.Printf("Error retrieving create time for PID %d: %v Name: %s", pid, err, name)
-			continue
-		}
-
-		// memory info
-
-		processCPUUsage = append(processCPUUsage, ProcessInfo{Pid: pid, Name: name, CPUUsage: cpuPercent, Memory: memPercent, StartTime: createTime})
+		processes = append(processes, &ProcessInfo{Pid: pid, Name: name, CPUUsage: cpuPercent, Memory: memPercent})
 	}
 
-	return processCPUUsage
+	return processes
 }
