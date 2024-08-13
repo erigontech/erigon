17a595f82b120694dca71cb4e2ad5885ad440fed
Alex Sharov <AskAlexSharov@gmail.com>
2024-08-13 09:52:16 +0700

    chain-tip perf: share worker between execs (#11542)

--- eth/stagedsync/exec3.go
+++ eth/stagedsync/exec3.go
@@ -356,8 +356,10 @@ func ExecV3(ctx context.Context,
 	rwsConsumed := make(chan struct{}, 1)
 	defer close(rwsConsumed)
 
-	execWorkers, applyWorker, rws, stopWorkers, waitWorkers := exec3.NewWorkersPool(lock.RLocker(), accumulator, logger, ctx, parallel, chainDb, rs, in, blockReader, chainConfig, genesis, engine, workerCount+1, cfg.dirs)
+	execWorkers, _, rws, stopWorkers, waitWorkers := exec3.NewWorkersPool(lock.RLocker(), accumulator, logger, ctx, parallel, chainDb, rs, in, blockReader, chainConfig, genesis, engine, workerCount+1, cfg.dirs)
 	defer stopWorkers()
+	applyWorker := cfg.applyWorker
+	applyWorker.ResetState(rs, accumulator)
 	applyWorker.DiscardReadList()
 
 	commitThreshold := batchSize.Bytes()
