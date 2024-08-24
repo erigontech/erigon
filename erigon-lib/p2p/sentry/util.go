c97bc379da30ebec4bdbe0284ccd935e6a94fc65
Mark Holt <mark@distributed.vision>
2024-08-24 09:46:59 +0100

    protocol by peer test

--- erigon-lib/p2p/sentry/util.go
+++ erigon-lib/p2p/sentry/util.go
@@ -7,65 +7,43 @@ import (
 	"strings"
 
 	"github.com/erigontech/erigon-lib/gointerfaces/sentryproto"
-	"github.com/erigontech/erigon-lib/types"
+	"github.com/erigontech/erigon-lib/gointerfaces/typesproto"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
-func Protocols(sentry sentryproto.SentryClient, peer types.PeerID) []byte {
-	if peer != nil {
-		if reply, err := sentry.PeerById(context.Background(), &sentryproto.PeerByIdRequest{PeerId: peer}); err == nil {
-			info := reply.GetPeer()
-			var protocols []byte
+func PeerProtocols(sentry sentryproto.SentryClient, peer *typesproto.H512) []byte {
+	if reply, err := sentry.PeerById(context.Background(), &sentryproto.PeerByIdRequest{PeerId: peer}); err == nil {
+		info := reply.GetPeer()
+		var protocols []byte
 
-			for _, cap := range info.Caps {
-				parts := strings.Split(cap, "/")
-				if len(parts) > 1 && strings.EqualFold(parts[0], "ETH") {
-					if version, err := strconv.Atoi(parts[1]); err == nil {
-						protocols = append(protocols, byte(version))
-					}
+		for _, cap := range info.Caps {
+			parts := strings.Split(cap, "/")
+			if len(parts) > 1 && strings.EqualFold(parts[0], "ETH") {
+				if version, err := strconv.Atoi(parts[1]); err == nil {
+					protocols = append(protocols, byte(version))
 				}
 			}
-
-			return protocols
 		}
-	} else {
-		switch sentry := sentry.(type) {
-		case interface{ Protocol() uint }:
-			return []byte{byte(sentry.Protocol())}
-		case *sentryMultiplexer:
-			if infos, err := sentry.NodeInfos(context.Background()); err == nil {
-				var protocols []byte
-				var seen map[byte]struct{} = map[byte]struct{}{}
-				for _, info := range infos {
-					var rawProtocols map[string]json.RawMessage
-					json.Unmarshal(info.Protocols, &rawProtocols)
 
-					if rawJson, ok := rawProtocols["eth"]; ok {
-						protocol := struct {
-							Name    string `json:"name"`
-							Version uint   `json:"version"`
-						}{}
+		return protocols
+	}
 
-						json.Unmarshal(rawJson, &protocol)
+	return nil
+}
 
-						if protocol.Version > 0 {
-							p := byte(protocol.Version)
-							if _, ok := seen[p]; !ok {
-								protocols = append(protocols, p)
-								seen[p] = struct{}{}
-							}
-						}
-					}
+func Protocols(sentry sentryproto.SentryClient) []byte {
+	switch sentry := sentry.(type) {
+	case interface{ Protocol() uint }:
+		return []byte{byte(sentry.Protocol())}
+	case *sentryMultiplexer:
+		if infos, err := sentry.NodeInfos(context.Background()); err == nil {
+			var protocols []byte
+			var seen map[byte]struct{} = map[byte]struct{}{}
+			for _, info := range infos {
+				var rawProtocols map[string]json.RawMessage
+				json.Unmarshal(info.Protocols, &rawProtocols)
 
-				}
-				return protocols
-			}
-		default:
-			if info, err := sentry.NodeInfo(context.Background(), &emptypb.Empty{}); err == nil {
-				var protocols map[string]json.RawMessage
-				json.Unmarshal(info.Protocols, &protocols)
-
-				if rawJson, ok := protocols["eth"]; ok {
+				if rawJson, ok := rawProtocols["eth"]; ok {
 					protocol := struct {
 						Name    string `json:"name"`
 						Version uint   `json:"version"`
@@ -74,8 +52,32 @@ func Protocols(sentry sentryproto.SentryClient, peer types.PeerID) []byte {
 					json.Unmarshal(rawJson, &protocol)
 
 					if protocol.Version > 0 {
-						return []byte{byte(protocol.Version)}
+						p := byte(protocol.Version)
+						if _, ok := seen[p]; !ok {
+							protocols = append(protocols, p)
+							seen[p] = struct{}{}
+						}
 					}
+				}
+
+			}
+			return protocols
+		}
+	default:
+		if info, err := sentry.NodeInfo(context.Background(), &emptypb.Empty{}); err == nil {
+			var protocols map[string]json.RawMessage
+			json.Unmarshal(info.Protocols, &protocols)
+
+			if rawJson, ok := protocols["eth"]; ok {
+				protocol := struct {
+					Name    string `json:"name"`
+					Version uint   `json:"version"`
+				}{}
+
+				json.Unmarshal(rawJson, &protocol)
+
+				if protocol.Version > 0 {
+					return []byte{byte(protocol.Version)}
 				}
 			}
 		}
--- erigon-lib/p2p/sentry/util_test.go
+++ erigon-lib/p2p/sentry/util_test.go
@@ -5,9 +5,10 @@ import (
 	"testing"
 
 	"github.com/erigontech/erigon-lib/direct"
+	"github.com/erigontech/erigon-lib/gointerfaces"
 	"github.com/erigontech/erigon-lib/gointerfaces/sentryproto"
 	"github.com/erigontech/erigon-lib/gointerfaces/typesproto"
-	"github.com/erigontech/erigon-lib/p2p/sentry"
+	sentry "github.com/erigontech/erigon-lib/p2p/sentry"
 	"github.com/erigontech/erigon/eth/protocols/eth"
 	"github.com/erigontech/erigon/p2p"
 	"github.com/stretchr/testify/require"
@@ -64,7 +65,7 @@ func TestProtocols(t *testing.T) {
 	direct := newClient(ctrl, 0, []p2p.Protocol{eth67})
 	direct.EXPECT().Protocol().Return(67)
 
-	p := sentry.Protocols(direct, nil)
+	p := sentry.Protocols(direct)
 
 	require.Len(t, p, 1)
 	require.Equal(t, byte(67), p[0])
@@ -73,7 +74,7 @@ func TestProtocols(t *testing.T) {
 		mock: newClient(ctrl, 1, []p2p.Protocol{eth68}),
 	}
 
-	p = sentry.Protocols(base, nil)
+	p = sentry.Protocols(base)
 
 	require.Len(t, p, 1)
 	require.Equal(t, byte(68), p[0])
@@ -81,9 +82,38 @@ func TestProtocols(t *testing.T) {
 	mux := sentry.NewSentryMultiplexer([]sentryproto.SentryClient{direct, base})
 	require.NotNil(t, mux)
 
-	p = sentry.Protocols(mux, nil)
+	p = sentry.Protocols(mux)
 
 	require.Len(t, p, 2)
 	require.Contains(t, p, byte(67))
 	require.Contains(t, p, byte(68))
+}
+
+func TestProtocolsByPeerId(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	peerId := gointerfaces.ConvertHashToH512([64]byte{})
+
+	direct := newClient(ctrl, 0, []p2p.Protocol{eth67})
+	direct.EXPECT().PeerById(gomock.Any(), gomock.Any(), gomock.Any()).
+		Return(&sentryproto.PeerByIdReply{
+			Peer: &typesproto.PeerInfo{
+				Id:   peerId.String(),
+				Caps: []string{"eth/67"},
+			},
+		}, nil).AnyTimes()
+
+	p := sentry.PeerProtocols(direct, peerId)
+
+	require.Len(t, p, 1)
+	require.Equal(t, byte(67), p[0])
+
+	mux := sentry.NewSentryMultiplexer([]sentryproto.SentryClient{direct})
+	require.NotNil(t, mux)
+
+	p = sentry.PeerProtocols(mux, peerId)
+
+	require.Len(t, p, 1)
+	require.Equal(t, byte(67), p[0])
 }
