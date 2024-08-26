aa2a2cde4f6ec938c76cf6c60ed44487658dc381
Kamen Stoykov <24619432+kstoykov@users.noreply.github.com>
2024-08-26 18:21:07 +0300

    Duplicate values in debug timer (#1033)
    * duplicate-values-in-debug-timer
    
    * format duration only in ms
    
    * use int ms

--- zk/utils/debug_timer.go
+++ zk/utils/debug_timer.go
@@ -33,13 +33,17 @@ func (t *Timer) LogTimer() {
 	}
 
 	t.elapsed = time.Since(t.start)
+	keysAndValues := make([]interface{}, 0, 2+2*len(t.taskNames))
+
 	logMessage := fmt.Sprintf("duration: %s, task: %s", t.elapsed, t.taskNames[0])
+	keysAndValues = append(keysAndValues, "duration-ms", fmt.Sprintf("%d", t.elapsed/time.Millisecond), "task", t.taskNames[0])
 
 	for i, task := range t.taskNames[1:] {
 		logMessage += fmt.Sprintf(", subtask%d: %s", i+1, task)
+		keysAndValues = append(keysAndValues, fmt.Sprintf("subtask%d", i+1), task)
 	}
 
-	log.Info("[cdk-metric] " + logMessage)
+	log.Info("[cdk-metric] "+logMessage, keysAndValues...)
 }
 
 func (t *Timer) Elapsed() time.Duration {
