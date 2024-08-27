a346cbbb744705de3ccccf1995a7542366dc5f34
kewei <kewei.train@gmail.com>
2024-08-28 02:40:36 +0800

    exp

--- cl/phase1/network/gossip_manager.go
+++ cl/phase1/network/gossip_manager.go
@@ -164,6 +164,9 @@ func (g *GossipManager) routeAndProcess(ctx context.Context, data *sentinel.Goss
 		if err := obj.DecodeSSZ(data.Data, int(version)); err != nil {
 			return err
 		}
+		if obj.Block.Slot != g.ethClock.GetCurrentSlot() {
+			return fmt.Errorf("block slot is not current slot")
+		}
 		log.Debug("[test] Received block via gossip", "slot", obj.Block.Slot, "peer", data.Peer.Pid)
 		return g.blockService.ProcessMessage(ctx, data.SubnetId, obj)
 	case gossip.TopicNameSyncCommitteeContributionAndProof:
