3bc2c41448d5fc2bb450b1081187e745cdeed168
alex.sharov <AskAlexSharov@gmail.com>
2024-08-29 09:31:55 +0700

    save

--- erigon-lib/state/domain.go
+++ erigon-lib/state/domain.go
@@ -128,7 +128,7 @@ var DomainCompressCfg = seg.Cfg{
 	DictReducerSoftLimit: 2000000,
 	MinPatternLen:        20,
 	MaxPatternLen:        32,
-	SamplingFactor:       1,
+	SamplingFactor:       4,
 	MaxDictPatterns:      64 * 1024 * 2,
 	Workers:              1,
 }
