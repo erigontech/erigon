50e49f4ef7a1ed2deea08350c045dba94daaa2e3
taratorio <94537774+taratorio@users.noreply.github.com>
2024-08-30 17:14:29 +0100

    polygon/heimdall: fix errors.Is typo for ErrNotInMilestoneList

--- polygon/heimdall/service.go
+++ polygon/heimdall/service.go
@@ -115,7 +115,7 @@ func newCheckpointFetcher(client HeimdallClient, logger log.Logger) entityFetche
 func newMilestoneFetcher(client HeimdallClient, logger log.Logger) entityFetcher[*Milestone] {
 	fetchEntity := func(ctx context.Context, number int64) (*Milestone, error) {
 		milestone, err := client.FetchMilestone(ctx, number)
-		if errors.Is(ErrNotInMilestoneList, err) {
+		if errors.Is(err, ErrNotInMilestoneList) {
 			// this is needed because there may be an unfortunate edge case where FetchFirstMilestoneNum returned 10
 			// but by the time our request reaches heimdall milestone=10 has been already pruned
 			return milestone, fmt.Errorf("%w: %w", errTransientEntityFetcherFailure, err)
