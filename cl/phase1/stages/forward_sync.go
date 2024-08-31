f7cc1b2734df6afc49f0959ecc482f903eec1022
Giulio <giulio.rebuffo@gmail.com>
2024-08-31 23:29:25 +0200

    save

--- cl/phase1/stages/forward_sync.go
+++ cl/phase1/stages/forward_sync.go
@@ -121,10 +121,10 @@ func processDownloadedBlockBatches(ctx context.Context, cfg *Cfg, highestBlockPr
 			return
 		}
 
-		if !hasSignedHeaderInDB {
+		if !hasSignedHeaderInDB && block.Block.Slot%(cfg.beaconCfg.SlotsPerEpoch*2) == 0 {
 			// Perform post-processing on the block
 			st, err = cfg.forkChoice.GetStateAtBlockRoot(blockRoot, false)
-			if err == nil && block.Block.Slot%(cfg.beaconCfg.SlotsPerEpoch*2) == 0 && st != nil {
+			if err == nil && st != nil {
 				// Dump the beacon state on disk if conditions are met
 				if err = cfg.forkChoice.DumpBeaconStateOnDisk(st); err != nil {
 					// Return an error if dumping the state fails
