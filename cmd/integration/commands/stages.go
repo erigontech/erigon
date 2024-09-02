5267a3952d7fe4eb197a51e0e056a9fcf3858241
Shoham Chakraborty <shhmchk@gmail.com>
2024-09-02 10:03:07 +0800

    Set state for PolygonSync

--- cmd/integration/commands/stages.go
+++ cmd/integration/commands/stages.go
@@ -888,15 +888,11 @@ func stagePolygonSync(db kv.RwDB, ctx context.Context, logger log.Logger) error 
 			return reset2.ResetPolygonSync(tx, db, agg, blockReader, blockWriter, dirs, *chainConfig, logger)
 		}
 
-		stageState := stage(stageSync, tx, nil, stages.Senders)
+		stageState := stage(stageSync, tx, nil, stages.PolygonSync)
 		cfg := stagedsync.NewPolygonSyncStageCfg(logger, chainConfig, nil, heimdallClient, nil, 0, nil, blockReader, nil, 0) // we only need blockReader and blockWriter (blockWriter is constructed in NewPolygonSyncStageCfg)
 		if unwind > 0 {
-			u := stageSync.NewUnwindState(stages.Senders, stageState.BlockNumber-unwind, stageState.BlockNumber, true, false)
+			u := stageSync.NewUnwindState(stageState.ID, stageState.BlockNumber-unwind, stageState.BlockNumber, true, false)
 			if err := stagedsync.UnwindPolygonSyncStage(ctx, tx, u, cfg); err != nil {
-				return err
-			}
-
-			if err := stages.SaveStageProgress(tx, stages.Snapshots, stageState.BlockNumber-unwind); err != nil {
 				return err
 			}
 		}
