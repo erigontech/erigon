d73bc9e9cb8a733a2b8d019e440ca769a7250840
JkLondon <ilya@mikheev.fun>
2024-09-05 15:35:20 +0400

    commit

--- turbo/jsonrpc/trace_adhoc.go
+++ turbo/jsonrpc/trace_adhoc.go
@@ -1323,7 +1323,7 @@ func (api *TraceAPIImpl) doCallMany(ctx context.Context, dbtx kv.Tx, msgs []type
 			gp := new(core.GasPool).AddGas(msg.Gas()).AddBlobGas(msg.BlobGas())
 
 			execResult, err = core.ApplyMessage(evm, msg, gp, true /* refunds */, gasBailout /* gasBailout */)
-			println("%+v", execResult)
+			println(fmt.Sprintf("%+v", execResult))
 			if execResult.Reverted {
 				println(string(execResult.Revert()))
 			}
