a799b58189ead863f83ebf828a45ac6043984ffe
Giulio <giulio.rebuffo@gmail.com>
2024-09-08 23:43:32 +0200

    save

--- erigon-lib/downloader/snaptype/files.go
+++ erigon-lib/downloader/snaptype/files.go
@@ -222,7 +222,7 @@ func IsSeedableExtension(name string) bool {
 	return false
 }
 
-const Erigon3SeedableSteps = 320
+const Erigon3SeedableSteps = 64
 
 // Use-cases:
 //   - produce and seed snapshots earlier on chain tip. reduce depnedency on "good peers with history" at p2p-network.
