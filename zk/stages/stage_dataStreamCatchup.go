a3435bca8a270aeb36dfcbebd8f547885296a4aa
Valentin Staykov <79150443+V-Staykov@users.noreply.github.com>
2024-09-10 13:49:17 +0300

    datastream catchup get executed block if datastream block is 0 (#1154)

--- zk/stages/stage_dataStreamCatchup.go
+++ zk/stages/stage_dataStreamCatchup.go
@@ -11,8 +11,8 @@ import (
 	"github.com/ledgerwatch/erigon/eth/stagedsync/stages"
 	"github.com/ledgerwatch/erigon/zk/datastream/server"
 	"github.com/ledgerwatch/erigon/zk/hermez_db"
-	"github.com/ledgerwatch/log/v3"
 	"github.com/ledgerwatch/erigon/zk/sequencer"
+	"github.com/ledgerwatch/log/v3"
 )
 
 type DataStreamCatchupCfg struct {
@@ -90,6 +90,15 @@ func CatchupDatastream(ctx context.Context, logPrefix string, tx kv.RwTx, stream
 		finalBlockNumber, err = stages.GetStageProgress(tx, stages.DataStream)
 		if err != nil {
 			return 0, err
+		}
+
+		// this handles a case where the node was synced in RPC mode without a datastream
+		// in this case the datastream progress will be 0 but it still should catch up
+		if finalBlockNumber == 0 {
+			finalBlockNumber, err = stages.GetStageProgress(tx, stages.Execution)
+			if err != nil {
+				return 0, err
+			}
 		}
 	} else {
 		finalBlockNumber, err = stages.GetStageProgress(tx, stages.Execution)
