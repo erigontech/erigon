1404bdfc2f072dff4f87df3d6d29967f2e2fd56c
kewei <kewei.train@gmail.com>
2024-09-16 17:16:26 +0800

    update

--- cl/phase1/network/services/batch_signature_verification.go
+++ cl/phase1/network/services/batch_signature_verification.go
@@ -77,7 +77,7 @@ func (b *BatchSignatureVerifier) start(inputCh <-chan *AggregateVerificationData
 				b.processSignatureVerification(aggregateVerificationData)
 				ticker.Reset(batchCheckInterval)
 				// clear the slice
-				aggregateVerificationData = make([]*AggregateVerificationData, 0, batchSignatureVerificationThreshold)
+				clear(aggregateVerificationData)
 			}
 		case <-ticker.C:
 			if len(aggregateVerificationData) == 0 {
@@ -85,7 +85,7 @@ func (b *BatchSignatureVerifier) start(inputCh <-chan *AggregateVerificationData
 			}
 			b.processSignatureVerification(aggregateVerificationData)
 			// clear the slice
-			aggregateVerificationData = make([]*AggregateVerificationData, 0, batchSignatureVerificationThreshold)
+			clear(aggregateVerificationData)
 		}
 	}
 }
