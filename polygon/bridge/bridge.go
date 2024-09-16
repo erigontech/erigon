046aa8b5adc1e6abc7b40abc9d782b327a83a167
milen <94537774+taratorio@users.noreply.github.com>
2024-09-16 09:50:04 +0100

    polygon: fix more transient errors for astrid (#11996)
    fixes
    ```
    DBUG[09-14|13:18:45.703] [p2p.peerTracker] peer connected         peerId=da870922d3a430bc9e0c71663884bb17968ceda35bf01f9f3d6318eb3c434a4e3ccf36eb1f6deb66a6037b9acf5b1473e2c94fbdcab98b4fe861134af08c8d05
    WARN[09-14|13:18:45.920] [bor.heimdall] an error while fetching   path=clerk/event-record/list queryParams="from-id=2899&to-time=1726316275&limit=50" attempt=5 err="Get \"https://heimdall-api-amoy.polygon.technology/clerk/event-record/list?from-id=2899&to-time=1726316275&limit=50\": context deadline exceeded"
    WARN[09-14|13:18:45.920] [bridge] scraper transient err occurred  from=2899 to=2024-09-14T13:17:55+01:00 err="Get \"https://heimdall-api-amoy.polygon.technology/clerk/event-record/list?from-id=2899&to-time=1726316275&limit=50\": context deadline exceeded"
    WARN[09-14|13:18:45.990] [bor.heimdall] an error while fetching   path=/checkpoints/count queryParams= attempt=5 err="Get \"https://heimdall-api-amoy.polygon.technology/checkpoints/count\": context deadline exceeded"
    WARN[09-14|13:18:45.991] [bor.heimdall] an error while fetching   path=/milestone/count queryParams= attempt=5 err="Get \"https://heimdall-api-amoy.polygon.technology/milestone/count\": context canceled"
    DBUG[09-14|13:18:45.991] [bor.heimdall] request canceled          reason="context canceled" path=/milestone/count queryParams= attempt=5
    WARN[09-14|13:18:45.991] [bor.heimdall] an error while fetching   path=bor/latest-span queryParams= attempt=5 err="Get \"https://heimdall-api-amoy.polygon.technology/bor/latest-span\": context canceled"
    WARN[09-14|13:18:45.991] [bor.heimdall] an error while fetching   path=clerk/event-record/list queryParams="from-id=2899&to-time=1726316325&limit=50" attempt=1 err="Get \"https://heimdall-api-amoy.polygon.technology/clerk/event-record/list?from-id=2899&to-time=1726316325&limit=50\": context canceled"
    DBUG[09-14|13:18:45.992] [bor.heimdall] request canceled          reason="context canceled" path=clerk/event-record/list queryParams="from-id=2899&to-time=1726316325&limit=50" attempt=1
    DBUG[09-14|13:18:48.992] Error while executing stage              err="[2/6 PolygonSync] stopped: Get \"https://heimdall-api-amoy.polygon.technology/checkpoints/count\": context deadline exceeded"
    EROR[09-14|13:18:48.992] [2/6 PolygonSync] stopping node          err="Get \"https://heimdall-api-amoy.polygon.technology/checkpoints/count\": context deadline exceeded"
    ```

--- polygon/bridge/bridge.go
+++ polygon/bridge/bridge.go
@@ -31,6 +31,7 @@ import (
 	"github.com/erigontech/erigon/polygon/polygoncommon"
 
 	libcommon "github.com/erigontech/erigon-lib/common"
+	liberrors "github.com/erigontech/erigon-lib/common/errors"
 	"github.com/erigontech/erigon/core/types"
 	"github.com/erigontech/erigon/polygon/bor/borcfg"
 	"github.com/erigontech/erigon/polygon/heimdall"
@@ -63,6 +64,7 @@ func NewBridge(store Store, logger log.Logger, borConfig *borcfg.BorConfig, even
 		eventFetcher:                 eventFetcher,
 		stateReceiverContractAddress: libcommon.HexToAddress(borConfig.StateReceiverContract),
 		reader:                       reader,
+		transientErrors:              []error{context.DeadlineExceeded, heimdall.ErrBadGateway},
 		fetchedEventsSignal:          make(chan struct{}),
 		processedBlocksSignal:        make(chan struct{}),
 	}
@@ -75,6 +77,7 @@ type Bridge struct {
 	eventFetcher                 eventFetcher
 	stateReceiverContractAddress libcommon.Address
 	reader                       *Reader
+	transientErrors              []error
 	// internal state
 	reachedTip             atomic.Bool
 	fetchedEventsSignal    chan struct{}
@@ -132,12 +135,12 @@ func (b *Bridge) Run(ctx context.Context) error {
 		default:
 		}
 
-		// start scrapping events
+		// start scraping events
 		from := lastFetchedEventID + 1
 		to := time.Now()
 		events, err := b.eventFetcher.FetchStateSyncEvents(ctx, from, to, heimdall.StateEventsFetchLimit)
 		if err != nil {
-			if errors.Is(err, context.DeadlineExceeded) {
+			if liberrors.IsOneOf(err, b.transientErrors) {
 				b.logger.Warn(
 					bridgeLogPrefix("scraper transient err occurred"),
 					"from", from,
