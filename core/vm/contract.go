3a95cf018c94e86d12e133c2e548711a243d8a2e
alex.sharov <AskAlexSharov@gmail.com>
2024-09-20 11:24:55 +0700

    save

--- core/vm/contract.go
+++ core/vm/contract.go
@@ -78,7 +78,7 @@ type JumpDestCache struct {
 }
 
 var (
-	jumpDestCacheLimit = dbg.EnvInt("JD_LRU", 512)
+	jumpDestCacheLimit = dbg.EnvInt("JD_LRU", 128)
 	jumpDestCacheTrace = dbg.EnvBool("JD_LRU_TRACE", false)
 )
 
