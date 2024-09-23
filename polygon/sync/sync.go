d60353e5796b5d5f58ff2a40fed818dd10dd5eda
taratorio <94537774+taratorio@users.noreply.github.com>
2024-09-23 19:37:31 +0100

    polygon/sync: add bridge unwind logic for astrid

--- polygon/sync/sync.go
+++ polygon/sync/sync.go
@@ -37,6 +37,7 @@ type heimdallSynchronizer interface {
 
 type bridgeSynchronizer interface {
 	Synchronize(ctx context.Context, blockNum uint64) error
+	Unwind(ctx context.Context, blockNum uint64) error
 }
 
 type Sync struct {
@@ -246,13 +247,22 @@ func (s *Sync) applyNewBlockOnTip(
 		return nil
 	}
 
+	newTip := ccBuilder.Tip()
+	firstConnected := newConnectedHeaders[0]
+	if newTip != oldTip && oldTip.Hash() != firstConnected.ParentHash {
+		// forks have changed, we need to unwind unwindable data
+		blockNum := max(1, firstConnected.Number.Uint64()) - 1
+		if err := s.bridgeSync.Unwind(ctx, blockNum); err != nil {
+			return err
+		}
+	}
+
 	// len(newConnectedHeaders) is always <= len(blockChain)
 	newConnectedBlocks := blockChain[len(blockChain)-len(newConnectedHeaders):]
 	if err := s.store.InsertBlocks(ctx, newConnectedBlocks); err != nil {
 		return err
 	}
 
-	newTip := ccBuilder.Tip()
 	if newTip == oldTip {
 		return nil
 	}
