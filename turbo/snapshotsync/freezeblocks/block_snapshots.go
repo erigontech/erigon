0409e53420501705566317c6b70441ee48149396
alex.sharov <AskAlexSharov@gmail.com>
2024-09-24 10:04:20 +0200

    save

--- turbo/snapshotsync/freezeblocks/block_snapshots.go
+++ turbo/snapshotsync/freezeblocks/block_snapshots.go
@@ -663,7 +663,9 @@ func (s *RoSnapshots) idxAvailability() uint64 {
 		if !s.HasType(segtype.Type()) {
 			return true
 		}
-		maxIdx = value.maxVisibleBlock.Load()
+		if len(value.VisibleSegments) > 0 {
+			maxIdx = value.VisibleSegments[len(value.VisibleSegments)-1].to - 1
+		}
 		return false // all types of segments have the same height. stop here
 	})
 
