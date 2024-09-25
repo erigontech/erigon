5374523ec49dbedf90fe184cfc949290a1155ed0
JkLondon <ilya@mikheev.fun>
2024-09-25 17:21:22 +0200

    commit

--- eth/stagedsync/exec3.go
+++ eth/stagedsync/exec3.go
@@ -349,6 +349,8 @@ func ExecV3(ctx context.Context,
 	if int64(maxBlockNum-blockNum) > 16 {
 		log.Info(fmt.Sprintf("[%s] starting", execStage.LogPrefix()),
 			"from", blockNum, "to", maxBlockNum, "fromTxNum", doms.TxNum(), "offsetFromBlockBeginning", offsetFromBlockBeginning, "initialCycle", initialCycle, "useExternalTx", useExternalTx)
+	} else {
+		println("here!")
 	}
 
 	agg.BuildFilesInBackground(outputTxNum.Load())
