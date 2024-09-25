55bda26ab566cc2aa9ea74fcfedb75acdae625c9
awskii <artem.tsskiy@gmail.com>
2024-09-25 19:06:17 +0100

    save

--- erigon-lib/config3/config3.go
+++ erigon-lib/config3/config3.go
@@ -17,8 +17,8 @@
 package config3
 
 // AggregationStep number of transactions in smallest static file
-// const HistoryV3AggregationStep = 1_562_500 // = 100M / 64. Dividers: 2, 5, 10, 20, 50, 100, 500
-const HistoryV3AggregationStep = 1_562_500 / 100
+const HistoryV3AggregationStep = 1_562_500 // = 100M / 64. Dividers: 2, 5, 10, 20, 50, 100, 500
+//const HistoryV3AggregationStep = 1_562_500 / 10
 
 const EnableHistoryV4InTest = true
 
