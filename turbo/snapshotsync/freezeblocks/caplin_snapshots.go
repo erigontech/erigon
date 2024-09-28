9544e6b2aae9f3ac9ae58a7fe8e35c000e9dbeb8
Giulio <giulio.rebuffo@gmail.com>
2024-09-28 15:18:50 +0200

    save

--- turbo/snapshotsync/freezeblocks/caplin_snapshots.go
+++ turbo/snapshotsync/freezeblocks/caplin_snapshots.go
@@ -202,7 +202,7 @@ Loop:
 			continue
 		}
 		var processed bool = true
-		fmt.Println(f.Type.Enum())
+		fmt.Println(f.Type.Enum(), fName)
 		switch f.Type.Enum() {
 		case snaptype.CaplinEnums.BeaconBlocks:
 			var sn *DirtySegment
