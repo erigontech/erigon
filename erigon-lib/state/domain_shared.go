6a6b386cb83a67161185af0ab54c7ffca97e2ca7
awskii <artem.tsskiy@gmail.com>
2024-10-01 17:32:34 +0100

    g

--- erigon-lib/state/domain_shared.go
+++ erigon-lib/state/domain_shared.go
@@ -1366,7 +1366,7 @@ func (sdc *SharedDomainsCommitmentContext) ComputeCommitment(ctx context.Context
 
 	// data accessing functions should be set when domain is opened/shared context updated
 	sdc.patriciaTrie.SetTrace(sdc.sharedDomains.trace)
-	sdc.Reset()
+	// sdc.Reset()					//
 
 	rootHash, err = sdc.patriciaTrie.Process(ctx, sdc.updates, logPrefix)
 	if err != nil {
