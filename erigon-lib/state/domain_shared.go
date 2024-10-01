40a9bebca456b1b4b92d9c89032ee60b7c01b8e6
awskii <artem.tsskiy@gmail.com>
2024-10-01 17:17:26 +0100

    save

--- erigon-lib/state/domain_shared.go
+++ erigon-lib/state/domain_shared.go
@@ -260,7 +260,7 @@ func (sd *SharedDomains) RebuildCommitmentRange(ctx context.Context, rwTx kv.RwT
 
 	sd.SetTx(rwTx)
 	sd.SetTxNum(uint64(to - 1))
-	sd.sdCtx.SetLimitReadAsOfTxNum(sd.TxNum())
+	sd.sdCtx.SetLimitReadAsOfTxNum(sd.TxNum() + 1)
 
 	keyCountByDomains := sd.KeyCountInDomainRange(uint64(from), uint64(to))
 	totalKeys := keyCountByDomains[kv.AccountsDomain] + keyCountByDomains[kv.StorageDomain]
