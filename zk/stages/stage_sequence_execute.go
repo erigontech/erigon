c3ad9bd5d04cf93526c3423289c2c1f19dc31ae9
Kamen Stoykov <24619432+kstoykov@users.noreply.github.com>
2024-10-01 11:32:18 +0300

    feat(sequencer): add-checked-regarding-ds-execution (#1249)

--- zk/stages/stage_sequence_execute.go
+++ zk/stages/stage_sequence_execute.go
@@ -47,40 +47,7 @@ func SpawnSequencingStage(
 	}
 
 	if lastBatch < highestBatchInDs {
-		if !cfg.zk.SequencerResequence {
-			panic(fmt.Sprintf("[%s] The node need re-sequencing but this option is disabled.", s.LogPrefix()))
-		}
-
-		log.Info(fmt.Sprintf("[%s] Last batch %d is lower than highest batch in datastream %d, resequencing...", s.LogPrefix(), lastBatch, highestBatchInDs))
-
-		batches, err := cfg.datastreamServer.ReadBatches(lastBatch+1, highestBatchInDs)
-		if err != nil {
-			return err
-		}
-
-		if err = cfg.datastreamServer.UnwindToBatchStart(lastBatch + 1); err != nil {
-			return err
-		}
-
-		log.Info(fmt.Sprintf("[%s] Resequence from batch %d to %d in data stream", s.LogPrefix(), lastBatch+1, highestBatchInDs))
-		for _, batch := range batches {
-			batchJob := NewResequenceBatchJob(batch)
-			subBatchCount := 0
-			for batchJob.HasMoreBlockToProcess() {
-				if err = sequencingBatchStep(s, u, ctx, cfg, historyCfg, batchJob); err != nil {
-					return err
-				}
-
-				subBatchCount += 1
-			}
-
-			log.Info(fmt.Sprintf("[%s] Resequenced original batch %d with %d batches", s.LogPrefix(), batchJob.batchToProcess[0].BatchNumber, subBatchCount))
-			if cfg.zk.SequencerResequenceStrict && subBatchCount != 1 {
-				return fmt.Errorf("strict mode enabled, but resequenced batch %d has %d sub-batches", batchJob.batchToProcess[0].BatchNumber, subBatchCount)
-			}
-		}
-
-		return nil
+		return resequence(s, u, ctx, cfg, historyCfg, lastBatch, highestBatchInDs)
 	}
 
 	if cfg.zk.SequencerResequence {
@@ -103,6 +70,11 @@ func sequencingBatchStep(
 	logPrefix := s.LogPrefix()
 	log.Info(fmt.Sprintf("[%s] Starting sequencing stage", logPrefix))
 	defer log.Info(fmt.Sprintf("[%s] Finished sequencing stage", logPrefix))
+
+	// at this point of time the datastream could not be ahead of the executor
+	if err = validateIfDatastreamIsAheadOfExecution(s, ctx, cfg); err != nil {
+		return err
+	}
 
 	sdb, err := newStageDb(ctx, cfg.db)
 	if err != nil {
