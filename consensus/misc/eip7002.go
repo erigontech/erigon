72737b1701b501444d733b33419b24e6aed5e26e
Andrew Ashikhmin <34320705+yperbasis@users.noreply.github.com>
2024-10-02 12:32:49 +0200

    Fix compilation (#12171)

--- consensus/misc/eip7002.go
+++ consensus/misc/eip7002.go
@@ -3,8 +3,10 @@ package misc
 import (
 	"encoding/binary"
 
+	"github.com/ledgerwatch/log/v3"
+
 	"github.com/ledgerwatch/erigon-lib/common"
-	"github.com/ledgerwatch/erigon-lib/log/v3"
+
 	"github.com/ledgerwatch/erigon/consensus"
 	"github.com/ledgerwatch/erigon/core/types"
 	"github.com/ledgerwatch/erigon/params"
