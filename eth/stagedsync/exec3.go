4a4c109c3c2e4b329196735135e36e30a8cd7a21
Mark Holt <mark.holt@erigon.tech>
2024-10-08 15:04:22 +0100

    fix panic due to nill receipt

--- eth/stagedsync/exec3.go
+++ eth/stagedsync/exec3.go
@@ -896,7 +896,14 @@ Loop:
 
 				if txTask.Final {
 					if !isMining && !inMemExec && !execStage.CurrentSyncCycle.IsInitialCycle {
-						cfg.notifications.RecentLogs.Add(blockReceipts)
+						// add will fail if not all txs have receipts
+						receipts := make(types.Receipts, 0, len(blockReceipts))
+						for _, receipt := range blockReceipts {
+							if receipt != nil {
+								receipts = append(receipts, receipt)
+							}
+						}
+						cfg.notifications.RecentLogs.Add(receipts)
 					}
 					checkReceipts := !cfg.vmConfig.StatelessExec && chainConfig.IsByzantium(txTask.BlockNum) && !cfg.vmConfig.NoReceipts && !isMining
 					if txTask.BlockNum > 0 && !skipPostEvaluation { //Disable check for genesis. Maybe need somehow improve it in future - to satisfy TestExecutionSpec
