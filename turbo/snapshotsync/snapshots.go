cba2228e906e37226fe88beb4f156b4be7050ca8
Mark Holt <mark.holt@erigon.tech>
2024-10-08 15:04:07 +0100

    remove printer

--- turbo/snapshotsync/snapshots.go
+++ turbo/snapshotsync/snapshots.go
@@ -995,10 +995,6 @@ func TypedSegments(dir string, minBlock uint64, types []snaptype.Type, allowGaps
 				log.Debug("[snapshots] see gap", "type", segType, "from", lst.from)
 			}
 			res = append(res, l...)
-			if len(m) > 0 {
-				lst := m[len(m)-1]
-				log.Debug("[snapshots] see gap", "type", segType, "from", lst.from)
-			}
 
 			missingSnapshots = append(missingSnapshots, m...)
 		}
