erigontech/erigon

commit 253f73721c4855e697aae9a8a7219f96e54062e2

blockfiles: split dirty files and visible files (#11557)
This PR for  #11417 includes:
1. splitting segments into dirtySegments and visibleSegments
2. dirtySegments are updated in background and not accessible for APP.
3. dirtySegments will be added to visibleSegments when:
    - there's no gap/overlap/garbage 
    - all types of segments are created and indexed  at that height
4. add unit test:  `TestCalculateVisibleSegments`

---------

Co-authored-by: lupin012 <58134934+lupin012@users.noreply.github.com>
Co-authored-by: Alex Sharov <AskAlexSharov@gmail.com>
Co-authored-by: Ilya Mikheev <54912776+JkLondon@users.noreply.github.com>
Co-authored-by: JkLondon <ilya@mikheev.fun>
Co-authored-by: shashiy <shaashiiy@gmail.com>
Co-authored-by: Elias Rad <146735585+nnsW3@users.noreply.github.com>
Co-authored-by: awskii <awskii@users.noreply.github.com>
Co-authored-by: blxdyx <125243069+blxdyx@users.noreply.github.com>
Co-authored-by: Giulio rebuffo <giulio.rebuffo@gmail.com>
Co-authored-by: Shota <silagadzeshota@gmail.com>
Co-authored-by: shota.silagadze <shota.silagadze@taal.com>
Co-authored-by: Dmytro Vovk <vovk.dimon@gmail.com>
Co-authored-by: Massa <massarinoaa@gmail.com>

turbo/snapshotsync/freezeblocks/block_snapshots_test.go
@@ -212,7 +212,7 @@ func TestMergeSnapshots(t *testing.T) {
 	for i := uint64(0); i < N; i++ {
 		createFile(i*10_000, (i+1)*10_000)
 	}
-	s := NewRoSnapshots(ethconfig.BlocksFreezing{}, dir, 0, logger)
+	s := NewRoSnapshots(ethconfig.BlocksFreezing{ChainName: networkname.MainnetChainName}, dir, 0, logger)
 	defer s.Close()
 	require.NoError(s.ReopenFolder())
 	{
@@ -308,7 +308,7 @@ func TestDeleteSnapshots(t *testing.T) {
 	for i := uint64(0); i < N; i++ {
 		createFile(i*10_000, (i+1)*10_000)
 	}
-	s := NewRoSnapshots(ethconfig.BlocksFreezing{}, dir, 0, logger)
+	s := NewRoSnapshots(ethconfig.BlocksFreezing{ChainName: networkname.MainnetChainName}, dir, 0, logger)
 	defer s.Close()
 	retireFiles := []string{
 		"v1-000000-000010-bodies.seg",
@@ -331,6 +331,12 @@ func TestRemoveOverlaps(t *testing.T) {
 		}
 	}
 
+	// 0 - 10_000, ... , 40_000 - 50_000 => 5 files
+	// 0 - 100_000 => 1 file
+	// 130_000 - 140_000, ... , 180_000 - 190_000 => 5 files
+	// 100_000 - 200_000 => 1 file
+	// 200_000 - 210_000, ... , 220_000 - 230_000 => 3 files
+
 	for i := uint64(0); i < 5; i++ {
 		createFile(i*10_000, (i+1)*10_000)
 	}
@@ -347,7 +353,7 @@ func TestRemoveOverlaps(t *testing.T) {
 		createFile(200_000+i*10_000, 200_000+(i+1)*10_000)
 	}
 
-	s := NewRoSnapshots(ethconfig.BlocksFreezing{}, dir, 0, logger)
+	s := NewRoSnapshots(ethconfig.BlocksFreezing{ChainName: networkname.MainnetChainName}, dir, 0, logger)
 
 	defer s.Close()
 	require.NoError(s.ReopenSegments(coresnaptype.BlockSnapshotTypes, false))
@@ -395,11 +401,14 @@ func TestOpenAllSnapshot(t *testing.T) {
 	logger := log.New()
 	baseDir, require := t.TempDir(), require.New(t)
 
-	for _, chain := range []string{networkname.MainnetChainName, networkname.AmoyChainName} {
+	steps := []uint64{500_000, 100_000}
+
+	for i, chain := range []string{networkname.MainnetChainName, networkname.AmoyChainName} {
+		step := steps[i]
 		dir := filepath.Join(baseDir, chain)
 		chainSnapshotCfg := snapcfg.KnownCfg(chain)
 		chainSnapshotCfg.ExpectBlocks = math.MaxUint64
-		cfg := ethconfig.BlocksFreezing{}
+		cfg := ethconfig.BlocksFreezing{ChainName: chain}
 		createFile := func(from, to uint64, name snaptype.Type) {
 			createTestSegmentFile(t, from, to, name.Enum(), dir, 1, logger)
 		}
@@ -412,49 +421,49 @@ func TestOpenAllSnapshot(t *testing.T) {
 			res, _ := s.segments.Get(e)
 			return res
 		}
-		require.Equal(0, len(getSegs(coresnaptype.Enums.Headers).segments))
+		require.Equal(0, len(getSegs(coresnaptype.Enums.Headers).VisibleSegments))
 		s.Close()
 
-		createFile(500_000, 1_000_000, coresnaptype.Bodies)
+		createFile(step, step*2, coresnaptype.Bodies)
 		s = NewRoSnapshots(cfg, dir, 0, logger)
 		defer s.Close()
 		require.NotNil(getSegs(coresnaptype.Enums.Bodies))
-		require.Equal(0, len(getSegs(coresnaptype.Enums.Bodies).segments))
+		require.Equal(0, len(getSegs(coresnaptype.Enums.Bodies).VisibleSegments))
 		s.Close()
 
-		createFile(500_000, 1_000_000, coresnaptype.Headers)
-		createFile(500_000, 1_000_000, coresnaptype.Transactions)
+		createFile(step, step*2, coresnaptype.Headers)
+		createFile(step, step*2, coresnaptype.Transactions)
 		s = NewRoSnapshots(cfg, dir, 0, logger)
 		err = s.ReopenFolder()
 		require.NoError(err)
 		require.NotNil(getSegs(coresnaptype.Enums.Headers))
 		s.ReopenSegments(coresnaptype.BlockSnapshotTypes, false)
-		require.Equal(1, len(getSegs(coresnaptype.Enums.Headers).segments))
+		// require.Equal(1, len(getSegs(coresnaptype.Enums.Headers).visibleSegments))
 		s.Close()
 
-		createFile(0, 500_000, coresnaptype.Bodies)
-		createFile(0, 500_000, coresnaptype.Headers)
-		createFile(0, 500_000, coresnaptype.Transactions)
+		createFile(0, step, coresnaptype.Bodies)
+		createFile(0, step, coresnaptype.Headers)
+		createFile(0, step, coresnaptype.Transactions)
 		s = NewRoSnapshots(cfg, dir, 0, logger)
 		defer s.Close()
 
 		err = s.ReopenFolder()
 		require.NoError(err)
 		require.NotNil(getSegs(coresnaptype.Enums.Headers))
-		require.Equal(2, len(getSegs(coresnaptype.Enums.Headers).segments))
+		require.Equal(2, len(getSegs(coresnaptype.Enums.Headers).VisibleSegments))
 
 		view := s.View()
 		defer view.Close()
 
 		seg, ok := view.TxsSegment(10)
 		require.True(ok)
-		require.Equal(int(seg.to), 500_000)
+		require.Equal(seg.to, step)
 
-		seg, ok = view.TxsSegment(500_000)
+		seg, ok = view.TxsSegment(step)
 		require.True(ok)
-		require.Equal(int(seg.to), 1_000_000)
+		require.Equal(seg.to, step*2)
 
-		_, ok = view.TxsSegment(1_000_000)
+		_, ok = view.TxsSegment(step * 2)
 		require.False(ok)
 
 		// Erigon may create new snapshots by itself - with high bigger than hardcoded ExpectedBlocks
@@ -465,11 +474,11 @@ func TestOpenAllSnapshot(t *testing.T) {
 		require.NoError(err)
 		defer s.Close()
 		require.NotNil(getSegs(coresnaptype.Enums.Headers))
-		require.Equal(2, len(getSegs(coresnaptype.Enums.Headers).segments))
+		require.Equal(2, len(getSegs(coresnaptype.Enums.Headers).VisibleSegments))
 
-		createFile(500_000, 900_000, coresnaptype.Headers)
-		createFile(500_000, 900_000, coresnaptype.Bodies)
-		createFile(500_000, 900_000, coresnaptype.Transactions)
+		createFile(step, step*2-step/5, coresnaptype.Headers)
+		createFile(step, step*2-step/5, coresnaptype.Bodies)
+		createFile(step, step*2-step/5, coresnaptype.Transactions)
 		chainSnapshotCfg.ExpectBlocks = math.MaxUint64
 		s = NewRoSnapshots(cfg, dir, 0, logger)
 		defer s.Close()
@@ -515,4 +524,78 @@ func TestParseCompressedFileName(t *testing.T) {
 	require.Equal(f.Type.Enum(), coresnaptype.Bodies.Enum())
 	require.Equal(1_000, int(f.From))
 	require.Equal(2_000, int(f.To))
+}
+
+func getSeg(s *RoSnapshots, e snaptype.Enum) *segments {
+	res, _ := s.segments.Get(e)
+	return res
+}
+
+func TestCalculateVisibleSegments(t *testing.T) {
+	logger := log.New()
+	dir, require := t.TempDir(), require.New(t)
+	createFile := func(from, to uint64, name snaptype.Type) {
+		createTestSegmentFile(t, from, to, name.Enum(), dir, 1, logger)
+	}
+
+	for i := uint64(0); i < 7; i++ {
+		createFile(i*500_000, (i+1)*500_000, coresnaptype.Headers)
+	}
+	for i := uint64(0); i < 6; i++ {
+		createFile(i*500_000, (i+1)*500_000, coresnaptype.Bodies)
+	}
+	for i := uint64(0); i < 5; i++ {
+		createFile(i*500_000, (i+1)*500_000, coresnaptype.Transactions)
+	}
+	cfg := ethconfig.BlocksFreezing{ChainName: networkname.MainnetChainName}
+	s := NewRoSnapshots(cfg, dir, 0, logger)
+	defer s.Close()
+
+	{
+		require.NoError(s.ReopenFolder())
+		idx := s.idxAvailability()
+		require.Equal(2_500_000-1, int(idx))
+
+		require.Equal(5, len(getSeg(s, coresnaptype.Enums.Headers).VisibleSegments))
+		require.Equal(5, len(getSeg(s, coresnaptype.Enums.Bodies).VisibleSegments))
+		require.Equal(5, len(getSeg(s, coresnaptype.Enums.Transactions).VisibleSegments))
+
+		require.Equal(7, getSeg(s, coresnaptype.Enums.Headers).DirtySegments.Len())
+		require.Equal(6, getSeg(s, coresnaptype.Enums.Bodies).DirtySegments.Len())
+		require.Equal(5, getSeg(s, coresnaptype.Enums.Transactions).DirtySegments.Len())
+	}
+
+	// gap in transactions: [5*500_000 - 6*500_000]
+	{
+		createFile(6*500_000, 7*500_000, coresnaptype.Transactions)
+
+		require.NoError(s.ReopenFolder())
+		idx := s.idxAvailability()
+		require.Equal(2_500_000-1, int(idx))
+
+		require.Equal(5, len(getSeg(s, coresnaptype.Enums.Headers).VisibleSegments))
+		require.Equal(5, len(getSeg(s, coresnaptype.Enums.Bodies).VisibleSegments))
+		require.Equal(5, len(getSeg(s, coresnaptype.Enums.Transactions).VisibleSegments))
+
+		require.Equal(7, getSeg(s, coresnaptype.Enums.Headers).DirtySegments.Len())
+		require.Equal(6, getSeg(s, coresnaptype.Enums.Bodies).DirtySegments.Len())
+		require.Equal(5, getSeg(s, coresnaptype.Enums.Transactions).DirtySegments.Len())
+	}
+
+	// overlap in transactions: [4*500_000 - 4.5*500_000]
+	{
+		createFile(4*500_000, 4*500_000+250_000, coresnaptype.Transactions)
+
+		require.NoError(s.ReopenFolder())
+		idx := s.idxAvailability()
+		require.Equal(2_500_000-1, int(idx))
+
+		require.Equal(5, len(getSeg(s, coresnaptype.Enums.Headers).VisibleSegments))
+		require.Equal(5, len(getSeg(s, coresnaptype.Enums.Bodies).VisibleSegments))
+		require.Equal(5, len(getSeg(s, coresnaptype.Enums.Transactions).VisibleSegments))
+
+		require.Equal(7, getSeg(s, coresnaptype.Enums.Headers).DirtySegments.Len())
+		require.Equal(6, getSeg(s, coresnaptype.Enums.Bodies).DirtySegments.Len())
+		require.Equal(5, getSeg(s, coresnaptype.Enums.Transactions).DirtySegments.Len())
+	}
 }

cmd/devnet/services/polygon/heimdallsim/heimdall_simulator_test.go
@@ -94,6 +94,9 @@ func TestSimulatorEvents(t *testing.T) {
 		t.Skip("fix me on win")
 	}
 
+	// the number of events included in v1-000000-000500-borevents.seg
+	eventsCount := 23
+
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
@@ -101,7 +104,7 @@ func TestSimulatorEvents(t *testing.T) {
 
 	res, err := sim.FetchStateSyncEvents(ctx, 0, time.Now(), 100)
 	assert.NoError(t, err)
-	assert.Equal(t, 100, len(res))
+	assert.Equal(t, eventsCount, len(res))
 
 	resLimit, err := sim.FetchStateSyncEvents(ctx, 0, time.Now(), 2)
 	assert.NoError(t, err)
@@ -117,7 +120,7 @@ func TestSimulatorEvents(t *testing.T) {
 	lastTime := res[len(res)-1].Time
 	resTime, err := sim.FetchStateSyncEvents(ctx, 0, lastTime.Add(-1*time.Second), 100)
 	assert.NoError(t, err)
-	assert.Equal(t, 99, len(resTime))
+	assert.Equal(t, eventsCount-1, len(resTime))
 	assert.Equal(t, res[:len(res)-1], resTime)
 }
 

cmd/integration/commands/stages.go
@@ -1325,7 +1325,8 @@ func allSnapshots(ctx context.Context, db kv.RoDB, logger log.Logger) (*freezebl
 	openSnapshotOnce.Do(func() {
 		dirs := datadir.New(datadirCli)
 
-		snapCfg := ethconfig.NewSnapCfg(true, true, true)
+		chainConfig := fromdb.ChainConfig(db)
+		snapCfg := ethconfig.NewSnapCfg(true, true, true, chainConfig.ChainName)
 
 		_allSnapshotsSingleton = freezeblocks.NewRoSnapshots(snapCfg, dirs.Snap, 0, logger)
 		_allBorSnapshotsSingleton = freezeblocks.NewBorRoSnapshots(snapCfg, dirs.Snap, 0, logger)

erigon-lib/direct/sentry_client.go
@@ -26,7 +26,7 @@ import (
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/emptypb"
 
-	sentryproto "github.com/erigontech/erigon-lib/gointerfaces/sentryproto"
+	"github.com/erigontech/erigon-lib/gointerfaces/sentryproto"
 	types "github.com/erigontech/erigon-lib/gointerfaces/typesproto"
 	libsentry "github.com/erigontech/erigon-lib/p2p/sentry"
 )

erigon-lib/downloader/snaptype/files.go
@@ -250,6 +250,9 @@ func (f FileInfo) Name() string { return f.name }
 func (f FileInfo) Dir() string  { return filepath.Dir(f.Path) }
 func (f FileInfo) Len() uint64  { return f.To - f.From }
 
+func (f FileInfo) GetRange() (from, to uint64) { return f.From, f.To }
+func (f FileInfo) GetType() Type               { return f.Type }
+
 func (f FileInfo) CompareTo(o FileInfo) int {
 	if res := cmp.Compare(f.From, o.From); res != 0 {
 		return res

erigon-lib/gointerfaces/downloaderproto/downloader_client_mock.go
@@ -217,7 +217,7 @@ func (c *MockDownloaderClientVerifyCall) DoAndReturn(f func(context.Context, *Ve
 	return c
 }
 
-//SetLogPrefix mocks base method.
+// SetLogPrefix mocks base method.
 func (m *MockDownloaderClient) SetLogPrefix(arg0 context.Context, arg1 *SetLogPrefixRequest, arg2 ...grpc.CallOption) (*emptypb.Empty, error) {
 	m.ctrl.T.Helper()
 	varargs := []any{arg0, arg1}

erigon-lib/p2p/sentry/util_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/erigontech/erigon-lib/gointerfaces"
 	"github.com/erigontech/erigon-lib/gointerfaces/sentryproto"
 	"github.com/erigontech/erigon-lib/gointerfaces/typesproto"
-	sentry "github.com/erigontech/erigon-lib/p2p/sentry"
+	"github.com/erigontech/erigon-lib/p2p/sentry"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/mock/gomock"
 	"google.golang.org/grpc"

commit 05fb0239a10e7dbb0dc859b783593addb4daf75e

freezeblocks: fix max visible segment when there is a gap in .idx files (#12263)
We have a gap in our bor mainnet snapshot toml between
'v1-051900-052000-headers.idx' =
'd97b3c598f520888eb67244ba28d994998584e65'
'v1-054000-054100-headers.idx' =
'a20efd5aca03ebb528215b24ce6aa0c20cc90c55'

When syncing bor mainnet from scratch the node fails to sync due to
that. We will fix the gap in our toml however the node should
auto-recover by regenerating the .idx files

After digging further it seems like we have a regression in
https://github.com/erigontech/erigon/pull/11557
idxAvailability used to stop when finding a seg which is not indexed
after that PR it does not, but instead it keeps on going so idxMax gets
set to 62 mil although there is a gap (it should have been set to 52 mil
where the gap is)

Tested on my node and it recreated the gap of indices

```
[INFO] [10-09|16:32:27.574] [snapshots:missed-idx] Stat              blocks=62.44M indices=52.00M alloc=26.8GB sys=27.4GB
[DBUG] [10-09|16:32:27.957] [index] created                          file=/home/admin/erigon-data/e3-bor-heimdall-stage-mainnet/snapshots/v1-052500-052600-headers.idx.tmp
[DBUG] [10-09|16:32:27.957] [index] calculating                      file=v1-052500-052600-headers.idx
[DBUG] [10-09|16:32:27.977] [index] created                          file=/home/admin/erigon-data/e3-bor-heimdall-stage-mainnet/snapshots/v1-053200-053300-headers.idx.tmp
[DBUG] [10-09|16:32:27.977] [index] calculating                      file=v1-053200-053300-headers.idx
[DBUG] [10-09|16:32:27.978] [index] created                          file=/home/admin/erigon-data/e3-bor-heimdall-stage-mainnet/snapshots/v1-052800-052900-headers.idx.tmp
[DBUG] [10-09|16:32:27.978] [index] calculating                      file=v1-052800-052900-headers.idx

turbo/snapshotsync/freezeblocks/block_snapshots.go
@@ -609,7 +609,7 @@ func (s *RoSnapshots) recalcVisibleFiles() {
 					continue
 				}
 				if seg.indexes == nil {
-					continue
+					break
 				}
 				for len(newVisibleSegments) > 0 && newVisibleSegments[len(newVisibleSegments)-1].src.isSubSetOf(seg) {
 					newVisibleSegments[len(newVisibleSegments)-1].src = nil

turbo/snapshotsync/freezeblocks/block_snapshots_test.go
@@ -18,6 +18,7 @@ package freezeblocks
 
 import (
 	"context"
+	"os"
 	"path/filepath"
 	"testing"
 	"testing/fstest"
@@ -26,6 +27,7 @@ import (
 	"golang.org/x/exp/slices"
 
 	"github.com/erigontech/erigon-lib/log/v3"
+	"github.com/erigontech/erigon/turbo/testlog"
 
 	"github.com/erigontech/erigon-lib/chain/networkname"
 	"github.com/erigontech/erigon-lib/chain/snapcfg"
@@ -598,4 +600,33 @@ func TestCalculateVisibleSegments(t *testing.T) {
 		require.Equal(6, getSeg(s, coresnaptype.Enums.Bodies).DirtySegments.Len())
 		require.Equal(5, getSeg(s, coresnaptype.Enums.Transactions).DirtySegments.Len())
 	}
+}
+
+func TestCalculateVisibleSegmentsWhenGapsInIdx(t *testing.T) {
+	logger := testlog.Logger(t, log.LvlCrit)
+	dir, require := t.TempDir(), require.New(t)
+	createFile := func(from, to uint64, name snaptype.Type) {
+		createTestSegmentFile(t, from, to, name.Enum(), dir, 1, logger)
+	}
+
+	for i := uint64(0); i < 3; i++ {
+		createFile(i*500_000, (i+1)*500_000, coresnaptype.Headers)
+		createFile(i*500_000, (i+1)*500_000, coresnaptype.Bodies)
+		createFile(i*500_000, (i+1)*500_000, coresnaptype.Transactions)
+	}
+
+	missingIdxFile := filepath.Join(dir, snaptype.IdxFileName(1, 500_000, 1_000_000, coresnaptype.Headers.Name()))
+	err := os.Remove(missingIdxFile)
+	require.NoError(err)
+
+	cfg := ethconfig.BlocksFreezing{ChainName: networkname.MainnetChainName}
+	s := NewRoSnapshots(cfg, dir, 0, logger)
+	defer s.Close()
+
+	require.NoError(s.ReopenFolder())
+	idx := s.idxAvailability()
+	require.Equal(500_000-1, int(idx))
+
+	require.Equal(1, len(getSeg(s, coresnaptype.Enums.Headers).VisibleSegments))
+	require.Equal(3, getSeg(s, coresnaptype.Enums.Headers).DirtySegments.Len())
 }