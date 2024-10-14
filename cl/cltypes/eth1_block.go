093b4dbcb1913842f62eb5ccfef77f817996b2a1
kewei <kewei.train@gmail.com>
2024-10-14 23:29:02 +0800

    unittest

--- cl/cltypes/eth1_block.go
+++ cl/cltypes/eth1_block.go
@@ -63,10 +63,8 @@ type Eth1Block struct {
 // NewEth1Block creates a new Eth1Block.
 func NewEth1Block(version clparams.StateVersion, beaconCfg *clparams.BeaconChainConfig) *Eth1Block {
 	return &Eth1Block{
-		version:      version,
-		beaconCfg:    beaconCfg,
-		Transactions: &solid.TransactionsSSZ{},
-		Withdrawals:  solid.NewStaticListSSZ[*Withdrawal](int(beaconCfg.MaxWithdrawalsPerPayload), 44),
+		version:   version,
+		beaconCfg: beaconCfg,
 	}
 }
 
