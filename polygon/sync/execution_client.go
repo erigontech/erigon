b46552ded13154e30546ce84cef51705e3ff556f
milen <94537774+taratorio@users.noreply.github.com>
2024-10-17 17:43:57 +0100

    polygon/sync: handle bad blocks on chain tip (#12320)
    closes https://github.com/erigontech/erigon/issues/11533
    
    High level:
    - when we get a `ErrForkChoiceUpdateBadBlock` on the chain tip due to a
    invalid root hash/receipts mismatch there are 2 scenarios - either we
    are wrong (due to a bug in our execution or us running on a wrong fork)
    or the peer which sent us the new block/new block hashes event is wrong
    - when we get a `ErrForkChoiceUpdateBadBlock` when handling a new
    milestone that means we are the one who is wrong
    
    With this in mind a way to handle `ErrForkChoiceUpdateBadBlock` on tip
    is to:
    - if the error happens when handing a new milestone mismatch and/or when
    in "sync to tip mode" using checkpoints & milestones on initial start -
    then terminate the node if we have a fork choice bad block err since we
    know the issue is with us and there is no point in looping
    - if the error happens while we are in "chain tip mode" and are building
    our canonical chain after the latest milestone has been correctly
    processed then we do not know if it us who is at fault or the block our
    peer has sent - in this case we mark the block as bad in an lru cache
    and penalise the peer (if this is after a NewBlockHashes event) and move
    on. Any future blocks that link to bad blocks in the lru get discarded
    with warn messages. The idea behind this is that 1) if it was indeed our
    peer who was wrong then we did the right thing & 2) if our peer was
    correct but we were wrong then we will eventually penalise all our peers
    and will stop receiving events from p2p - when this happens we will
    eventually get a new milestone event from heimdall, our canonical chain
    tip wouldn't match, we will try to download all blocks for the milestone
    from any peer that connects to us afterwards, will execute those blocks
    and we will again fail with a bad block err in which case this will be
    treated as a terminal failure and the node will exit unexpectedly
    
    Whenever we handle a bad block err we make sure to unwind the bridge
    back to the last known valid block height, so that it is in a consistent
    state for the next valid block and after shutdown for a clean restarts
    that would not require manual intervention.
    
    As part of this PR I noticed a few gaps in the logic around unwinding
    the bridge upon change of forks. Addressed that as part of this change
    so that we can close the "handle unwinding at tip" ticket.

--- polygon/sync/execution_client.go
+++ polygon/sync/execution_client.go
@@ -32,7 +32,8 @@ import (
 	eth1utils "github.com/erigontech/erigon/turbo/execution/eth1/eth1_utils"
 )
 
-var errForkChoiceUpdateFailure = errors.New("fork choice update failed")
+var ErrForkChoiceUpdateFailure = errors.New("fork choice update failure")
+var ErrForkChoiceUpdateBadBlock = errors.New("fork choice update bad block")
 
 type ExecutionClient interface {
 	InsertBlocks(ctx context.Context, blocks []*types.Block) error
@@ -102,15 +103,24 @@ func (e *executionClient) UpdateForkChoice(ctx context.Context, tip *types.Heade
 		latestValidHash = gointerfaces.ConvertH256ToHash(response.LatestValidHash)
 	}
 
-	if len(response.ValidationError) > 0 {
-		return latestValidHash, fmt.Errorf("%w: validationErr=%s", errForkChoiceUpdateFailure, response.Status)
+	switch response.Status {
+	case executionproto.ExecutionStatus_Success:
+		return latestValidHash, nil
+	case executionproto.ExecutionStatus_BadBlock:
+		return latestValidHash, fmt.Errorf(
+			"%w: status=%d, validationErr='%s'",
+			ErrForkChoiceUpdateBadBlock,
+			response.Status,
+			response.ValidationError,
+		)
+	default:
+		return latestValidHash, fmt.Errorf(
+			"%w: status=%d, validationErr='%s'",
+			ErrForkChoiceUpdateFailure,
+			response.Status,
+			response.ValidationError,
+		)
 	}
-
-	if response.Status != executionproto.ExecutionStatus_Success {
-		return latestValidHash, fmt.Errorf("%w: status=%s", errForkChoiceUpdateFailure, response.Status)
-	}
-
-	return latestValidHash, nil
 }
 
 func (e *executionClient) CurrentHeader(ctx context.Context) (*types.Header, error) {
