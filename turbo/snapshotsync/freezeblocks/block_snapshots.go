f079aa1bd238e606a9f6993ac06ae2ebd158f066
alex.sharov <AskAlexSharov@gmail.com>
2024-10-18 14:58:12 +0700

    save

--- turbo/snapshotsync/freezeblocks/block_snapshots.go
+++ turbo/snapshotsync/freezeblocks/block_snapshots.go
@@ -370,16 +370,6 @@ func (s *DirtySegment) reopenIdx(dir string) (err error) {
 	for len(s.indexes) < len(s.Type().Indexes()) {
 		s.indexes = append(s.indexes, nil)
 	}
-	//for i, index := range s.Type().Indexes() {
-	//
-	//	fileName := s.Type().IdxFileName(s.version, s.from, s.to, index)
-	//	index, err := recsplit.OpenIndex(filepath.Join(dir, fileName))
-	//	if err != nil {
-	//		return fmt.Errorf("%w, fileName: %s", err, fileName)
-	//	}
-	//
-	//	s.indexes[i] = index
-	//}
 
 	for i, fileName := range s.Type().IdxFileNames(s.version, s.from, s.to) {
 		if s.indexes[i] != nil {
