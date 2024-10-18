8e9a4d8073596622659d7707d239bf396d4e8a54
alex.sharov <AskAlexSharov@gmail.com>
2024-10-18 15:44:32 +0700

    save

--- cmd/pics/state.go
+++ cmd/pics/state.go
@@ -437,7 +437,7 @@ func initialState1() error {
 		return err
 	}
 
-	emptyKv := memdb.New("")
+	emptyKv := memdb.New("", kv.ChainDB)
 	if err = stateDatabaseComparison(emptyKv, m.DB, 0); err != nil {
 		return err
 	}
