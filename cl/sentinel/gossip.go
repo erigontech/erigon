35f94f8c34460b2454a7a8d43fefff74c3c2d0db
Giulio <giulio.rebuffo@gmail.com>
2024-10-20 02:04:32 +0200

    save

--- cl/sentinel/gossip.go
+++ cl/sentinel/gossip.go
@@ -651,8 +651,21 @@ func (s *GossipSubscription) run(ctx context.Context, sub *pubsub.Subscription, 
 }
 
 func (g *GossipSubscription) Publish(data []byte) error {
-	if len(g.topic.ListPeers()) == 0 {
+	listTopicsLen := len(g.topic.ListPeers())
+	if g.sub == nil {
+		log.Debug("[Gossip] No subscription to publish to for topic", "topic", g.topic.String())
+		return nil
+	}
+	supportThreadedPublishing := gossip.IsTopicBeaconAttestation(g.sub.Topic()) || gossip.IsTopicSyncCommittee(g.sub.Topic())
+
+	if listTopicsLen == 0 && supportThreadedPublishing {
 		log.Debug("[Gossip] No peers to publish to for topic", "topic", g.topic.String())
+		go func() {
+			if err := g.topic.Publish(g.ctx, data, pubsub.WithReadiness(pubsub.MinTopicSize(1))); err != nil {
+				g.s.logger.Debug("[Gossip] Published to topic", "topic", g.topic.String(), "err", err)
+			}
+		}()
+		return nil
 	}
 
 	return g.topic.Publish(g.ctx, data)
