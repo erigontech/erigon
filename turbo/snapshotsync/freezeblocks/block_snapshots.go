009fc192817ebf736aaf571c1fca6ade8b882901
steven <wangpeculiar@gmail.com>
2024-10-24 11:42:53 +0800

    speed up closeWhatNotInList (#12114)
    1. accelerate scanning files by avoiding doubly nested loop
    2. ~~do not reopen~~ (achieved by #12332 and #12362, so remove relevant
    code here)

--- turbo/snapshotsync/freezeblocks/block_snapshots.go
+++ turbo/snapshotsync/freezeblocks/block_snapshots.go
@@ -933,23 +933,22 @@ func (s *RoSnapshots) Close() {
 }
 
 func (s *RoSnapshots) closeWhatNotInList(l []string) {
+	protectFiles := make(map[string]struct{}, len(l))
+	for _, f := range l {
+		protectFiles[f] = struct{}{}
+	}
 	toClose := make(map[snaptype.Enum][]*DirtySegment, 0)
 	s.segments.Scan(func(segtype snaptype.Enum, value *segments) bool {
 		value.DirtySegments.Walk(func(segs []*DirtySegment) bool {
-
-		Loop1:
 			for _, seg := range segs {
-				for _, fName := range l {
-					if fName == seg.FileName() {
-						continue Loop1
-					}
+				if _, ok := protectFiles[seg.FileName()]; ok {
+					continue
 				}
 				if _, ok := toClose[seg.segType.Enum()]; !ok {
 					toClose[segtype] = make([]*DirtySegment, 0)
 				}
 				toClose[segtype] = append(toClose[segtype], seg)
 			}
-
 			return true
 		})
 		return true
