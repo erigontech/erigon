c9954b5974542abbfce990a5b3681b95c8fee7fd
Andrew Ashikhmin <34320705+yperbasis@users.noreply.github.com>
2024-10-24 14:05:09 +0200

    Flatten EIP-7002 withdrawal requests encoding (#12456)
    Cherry pick #12138 into `release/2.61`
    
    Co-authored-by: Somnath <snb895@outlook.com>

--- consensus/misc/eip7002.go
+++ consensus/misc/eip7002.go
@@ -1,11 +1,7 @@
 package misc
 
 import (
-	"encoding/binary"
-
 	"github.com/ledgerwatch/log/v3"
-
-	"github.com/ledgerwatch/erigon-lib/common"
 
 	"github.com/ledgerwatch/erigon/consensus"
 	"github.com/ledgerwatch/erigon/core/types"
@@ -16,54 +12,8 @@ import (
 // (May have to move it to config json later for cross-chain compatibility)
 // TODO @somnathb1 Probably not needed outside of EVM
 const (
-	ExcessWithdrawalReqsSlot        = 0
-	WithdrawalReqCountSlot          = 1
-	WithdrawalReqQueueHeadSlot      = 2
-	WithdrawalReqQueueTailSlot      = 3
-	WithdrawalReqQueueStorageOffset = 4
-	MaxWithdrawalReqsPerBlock       = 16
-	TargetWithdrawalReqsPerBlock    = 2
-	MinWithdrawalReqFee             = 1
-	WithdrawalReqFeeUpdFraction     = 17
+	WithdrawalRequestDataLen = 76 // addr + pubkey + amt
 )
-
-// const abiStr = `[
-// 	{
-// 		"inputs": [],
-// 		"name": "read_withdrawal_requests",
-// 		"outputs": [
-// 			{
-// 				"components": [
-// 					{
-// 						"internalType": "bytes20",
-// 						"name": "sourceAddress",
-// 						"type": "bytes20"
-// 					},
-// 					{
-// 						"internalType": "bytes32",
-// 						"name": "validatorPubKey1",
-// 						"type": "bytes32"
-// 					},
-// 					{
-// 						"internalType": "bytes16",
-// 						"name": "validatorPubKey2",
-// 						"type": "bytes16"
-// 					},
-// 					{
-// 						"internalType": "uint64",
-// 						"name": "amount",
-// 						"type": "uint64"
-// 					}
-// 				],
-// 				"internalType": "struct WithdrawalContract.ValidatorWithdrawalRequest[]",
-// 				"name": "",
-// 				"type": "tuple[]"
-// 			}
-// 		],
-// 		"stateMutability": "nonpayable",
-// 		"type": "function"
-// 	}
-// ]`
 
 func DequeueWithdrawalRequests7002(syscall consensus.SystemCall) types.Requests {
 	res, err := syscall(params.WithdrawalRequestAddress, nil)
@@ -71,38 +21,14 @@ func DequeueWithdrawalRequests7002(syscall consensus.SystemCall) types.Requests 
 		log.Warn("Err with syscall to WithdrawalRequestAddress", "err", err)
 		return nil
 	}
-	// Parse out the exits - using the bytes array returned
+	// Just append the contract outputs
 	var reqs types.Requests
-	lenPerReq := 20 + 48 + 8 // addr + pubkey + amt
-	for i := 0; i <= len(res)-lenPerReq; i += lenPerReq {
-		var pubkey [48]byte
-		copy(pubkey[:], res[i+20:i+68])
+	for i := 0; i <= len(res)-WithdrawalRequestDataLen; i += WithdrawalRequestDataLen {
+
 		wr := &types.WithdrawalRequest{
-			SourceAddress:   common.BytesToAddress(res[i : i+20]),
-			ValidatorPubkey: pubkey,
-			Amount:          binary.BigEndian.Uint64(res[i+68:]),
+			RequestData: [WithdrawalRequestDataLen]byte(res[i : i+WithdrawalRequestDataLen]),
 		}
 		reqs = append(reqs, wr)
 	}
 	return reqs
-
-	// Alternatively unpack using the abi methods
-	// wAbi, _ := abi.JSON(strings.NewReader(abiStr))
-	// wAbi.Unpack("read_withdrawal_requests", wrs)
-
-	// type R struct {
-	// 	sourceAddress [20]byte
-	// 	validatorPubKey1 [32] byte
-	// 	validatorPubKey2 [16] byte
-	// 	amount uint64
-	// }
-	// Ret := make([]R, 0)
-	// wAbi.UnpackIntoInterface(Ret, "read_withdrawal_requests", wrs)
-
-	// reqs := make(types.Requests, 0)
-
-	// for r := range(Ret) {
-	// 	req := types.NewRequest(Ret)
-	// 	reqs = append(reqs, types.NewRequest())
-	// }
 }
