44fc37f58c95228fb9edd5cd5e7f6b010d9cfb79
Giulio <giulio.rebuffo@gmail.com>
2024-10-26 22:45:30 +0200

    save

--- turbo/snapshotsync/freezeblocks/caplin_state_snapshots.go
+++ turbo/snapshotsync/freezeblocks/caplin_state_snapshots.go
@@ -654,7 +654,7 @@ func (s *CaplinStateSnapshots) Get(tbl string, slot uint64) ([]byte, error) {
 
 	seg, ok := view.VisibleSegment(slot, strings.ToLower(tbl))
 	if !ok {
-		fmt.Println("not ok", strings.ToLower(tbl), iew.roTxs)
+		fmt.Println("not ok", strings.ToLower(tbl), view.roTxs)
 		return nil, nil
 	}
 
