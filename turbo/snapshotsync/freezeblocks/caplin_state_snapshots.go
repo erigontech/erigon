207138188b8258b2d7122e566a3feb7de4c05cca
Giulio <giulio.rebuffo@gmail.com>
2024-10-26 16:52:25 +0200

    save

--- turbo/snapshotsync/freezeblocks/caplin_state_snapshots.go
+++ turbo/snapshotsync/freezeblocks/caplin_state_snapshots.go
@@ -204,6 +204,7 @@ func (s *CaplinStateSnapshots) Close() {
 // OpenList stops on optimistic=false, continue opening files on optimistic=true
 func (s *CaplinStateSnapshots) OpenList(fileNames []string, optimistic bool) error {
 	defer s.recalcVisibleFiles()
+	fmt.Println(fileNames)
 
 	s.dirtySegmentsLock.Lock()
 	defer s.dirtySegmentsLock.Unlock()
