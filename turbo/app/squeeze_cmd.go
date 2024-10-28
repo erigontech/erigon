da83d73db9e7fedc2bbe62b1de51485807221e09
awskii <awskii@users.noreply.github.com>
2024-10-28 07:35:02 +0000

    update for SqueezeCommitment (#12497)
    #12496

--- turbo/app/squeeze_cmd.go
+++ turbo/app/squeeze_cmd.go
@@ -98,7 +98,7 @@ func squeezeCommitment(ctx context.Context, dirs datadir.Dirs, logger log.Logger
 	}
 	ac := agg.BeginFilesRo()
 	defer ac.Close()
-	if err := ac.SqueezeCommitmentFiles(ac); err != nil {
+	if err := ac.SqueezeCommitmentFiles(); err != nil {
 		return err
 	}
 	ac.Close()
@@ -152,7 +152,7 @@ func squeezeStorage(ctx context.Context, dirs datadir.Dirs, logger log.Logger) e
 	acOld := aggOld.BeginFilesRo()
 	defer acOld.Close()
 
-	if err = acOld.SqueezeCommitmentFiles(ac); err != nil {
+	if err = acOld.SqueezeCommitmentFiles(); err != nil {
 		return err
 	}
 	acOld.Close()
