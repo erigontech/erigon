29f9e628ab1a58f23d6339ba51c13b1ce07b0ecb
alex.sharov <AskAlexSharov@gmail.com>
2024-11-04 16:08:32 +0700

    save

--- turbo/jsonrpc/eth_receipts.go
+++ turbo/jsonrpc/eth_receipts.go
@@ -229,6 +229,7 @@ func (api *BaseAPI) getLogsV3(ctx context.Context, tx kv.TemporalTx, begin, end 
 		if err != nil {
 			return nil, err
 		}
+		fmt.Printf("[dbg] iter: %d, %d\n", blockNum, txIndex)
 		if isFinalTxn {
 			continue
 		}
@@ -260,6 +261,7 @@ func (api *BaseAPI) getLogsV3(ctx context.Context, tx kv.TemporalTx, begin, end 
 			return nil, err
 		}
 		if txn == nil {
+			fmt.Printf("[dbg] txn not found: %d, %d\n", blockNum, txIndex)
 			continue
 		}
 
