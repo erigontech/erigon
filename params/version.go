b746f010dfb8be5e865af51883437ddaeb44ff14
yperbasis <andrey.ashikhmin@gmail.com>
2024-11-06 09:56:04 +0100

    params: v2.61.0-beta1

--- params/version.go
+++ params/version.go
@@ -31,10 +31,10 @@ var (
 
 // see https://calver.org
 const (
-	VersionMajor       = 2     // Major version component of the current release
-	VersionMinor       = 61    // Minor version component of the current release
-	VersionMicro       = 0     // Patch version component of the current release
-	VersionModifier    = "dev" // Modifier component of the current release
+	VersionMajor       = 2       // Major version component of the current release
+	VersionMinor       = 61      // Minor version component of the current release
+	VersionMicro       = 0       // Patch version component of the current release
+	VersionModifier    = "beta1" // Modifier component of the current release
 	VersionKeyCreated  = "ErigonVersionCreated"
 	VersionKeyFinished = "ErigonVersionFinished"
 )
