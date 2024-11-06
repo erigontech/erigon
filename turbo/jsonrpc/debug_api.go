1b7739c5d53aba1a210cae10a4f1d320f4d071d5
alex.sharov <AskAlexSharov@gmail.com>
2024-11-06 10:00:33 +0700

    save

--- turbo/jsonrpc/debug_api.go
+++ turbo/jsonrpc/debug_api.go
@@ -210,7 +210,7 @@ func (api *PrivateDebugAPIImpl) GetModifiedAccountsByNumber(ctx context.Context,
 	if err != nil {
 		return nil, err
 	}
-	endTxNum, err := txNumsReader.Max(tx, endNum)
+	endTxNum, err := txNumsReader.Min(tx, endNum-1)
 	if err != nil {
 		return nil, err
 	}
