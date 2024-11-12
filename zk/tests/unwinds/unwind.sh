8884d2b033429e747c892db4a44b937b044749ef
Scott Fairclough <70711990+hexoscott@users.noreply.github.com>
2024-11-12 10:49:21 +0000

    increase timeouts for unwind tests (#1445)
    * increase timeouts for unwind tests
    
    * remove timeout on unwind stream host

--- zk/tests/unwinds/unwind.sh
+++ zk/tests/unwinds/unwind.sh
@@ -15,8 +15,8 @@ dataPath="./datadir"
 firstStop=11204
 stopBlock=11315
 unwindBatch=70
-firstTimeout=150s
-secondTimeout=150s
+firstTimeout=300s
+secondTimeout=300s
 
 rm -rf "$dataPath/rpc-datadir"
 rm -rf "$dataPath/phase1-dump1"
@@ -27,10 +27,10 @@ rm -rf "$dataPath/phase1-diffs"
 rm -rf "$dataPath/phase2-diffs"  
 
 # run datastream server
-timeout 600s go run ./zk/debug_tools/datastream-host --file="$(pwd)/zk/tests/unwinds/datastream/hermez-dynamic-integration8-datastream/data-stream.bin" &
+go run ./zk/debug_tools/datastream-host --file="$(pwd)/zk/tests/unwinds/datastream/hermez-dynamic-integration8-datastream/data-stream.bin" &
 
 # in order to start the datastream server
-sleep 5
+sleep 10
 
 # run erigon for a while to sync to the unwind point to capture the dump
 timeout $firstTimeout ./build/bin/cdk-erigon \
