d62a26629c16f3e4869e94c0c1b214aff013f989
Giulio <giulio.rebuffo@gmail.com>
2024-11-13 10:05:06 +0100

    save

--- erigon-lib/downloader/webseed.go
+++ erigon-lib/downloader/webseed.go
@@ -356,6 +356,7 @@ func (d *WebSeeds) VerifyManifestedBucket(ctx context.Context, webSeedProviderUR
 }
 
 func (d *WebSeeds) Discover(ctx context.Context, files []string, rootDir string) {
+	fmt.Println("A")
 	listsOfFiles := d.constructListsOfFiles(ctx, d.seeds, files)
 	torrentMap := d.makeTorrentUrls(listsOfFiles)
 	webSeedMap := d.downloadTorrentFilesFromProviders(ctx, rootDir, torrentMap)
