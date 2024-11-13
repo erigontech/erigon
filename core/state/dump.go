5f29db618a13ccf1e13cc89957f5fe2106e4d9d2
JkLondon <ilya@mikheev.fun>
2024-11-13 12:20:08 +0100

    commit

--- core/state/dump.go
+++ core/state/dump.go
@@ -191,7 +191,7 @@ func (d *Dumper) DumpToCollector(c DumpCollector, excludeCode, excludeStorage bo
 			Storage:  make(map[string]string),
 		}
 		if acc.CodeHash != emptyCodeHash {
-			println("here for account ", account.Address.Hex())
+			println("here for account ", libcommon.BytesToAddress(k).Hex())
 			if account.CodeHash != nil {
 				println("account codehash:", account.CodeHash.String())
 			} else {
