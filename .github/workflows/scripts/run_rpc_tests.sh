d6ad01f24017dda8ddb1f6662b789cd626e77116
lupin012 <58134934+lupin012@users.noreply.github.com>
2024-11-14 09:47:01 +0100

    rpc-test: update exception list (#12679)
    Co-authored-by: alex.sharov <AskAlexSharov@gmail.com>

--- .github/workflows/scripts/run_rpc_tests.sh
+++ .github/workflows/scripts/run_rpc_tests.sh
@@ -6,39 +6,15 @@ set +e # Disable exit on error
 disabled_tests=(
     # Erigon2 and Erigon3 never supported this api methods
     trace_rawTransaction
-    # remove these lines after https://github.com/erigontech/rpc-tests/pull/284 and https://github.com/erigontech/erigon/pull/12655
-    eth_feeHistory/test_12.json
-    eth_feeHistory/test_13.json
-    eth_feeHistory/test_15.json
-    eth_feeHistory/test_16.json
-    eth_feeHistory/test_17.json
-    eth_feeHistory/test_18.json
-    eth_feeHistory/test_19.json
-    eth_feeHistory/test_20.json
-    # total difficulty field was removed, then added back
-    # remove this line after https://github.com/erigontech/rpc-tests/pull/282
-    eth_getBlockByHash/test_10.json
-    eth_getBlockByNumber/test_12.json
     # Erigon bugs
     debug_accountRange
-    # need update rpc-test - because Erigon is correct (@AskAlexSharov will do after https://github.com/erigontech/erigon/pull/12634)
-    # remove this line after https://github.com/erigontech/rpc-tests/pull/273
+    # changed in last commits
+    debug_storageRangeAt/test_11.json
+    # needs investigations
     debug_getModifiedAccountsByHash
     debug_getModifiedAccountsByNumber
     # Erigon bug https://github.com/erigontech/erigon/issues/12603
     erigon_getLatestLogs
-    erigon_getLogsByHash/test_04.json
-    # Erigon bug https://github.com/erigontech/erigon/issues/12637
-    debug_traceBlockByNumber/test_05.tar
-    debug_traceBlockByNumber/test_08.tar
-    debug_traceBlockByNumber/test_09.tar
-    debug_traceBlockByNumber/test_10.tar
-    debug_traceBlockByNumber/test_11.tar
-    debug_traceBlockByNumber/test_12.tar
-    # modified expected in case of empty rsp
-    debug_storageRangeAt/test_11.json
-    # remove this line after https://github.com/erigontech/rpc-tests/pull/281
-    parity_getBlockReceipts
     parity_listStorageKeys/test_12.json
     # created task https://github.com/erigontech/erigon/issues/12668
     debug_traceCallMany/test_02.tar
@@ -53,7 +29,7 @@ disabled_tests=(
     engine_exchangeTransitionConfigurationV1/test_01.json
     engine_getClientVersionV1/test_1.json
     trace_replayBlockTransactions/test_29.tar
-    # do these perhaps require Erigon up?
+    # these tests requires Erigon active
     admin_nodeInfo/test_01.json
     admin_peers/test_01.json
     erigon_nodeInfo/test_1.json
