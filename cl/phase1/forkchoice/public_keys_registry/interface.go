65f7bdf0a38ffbcbc48d8cdd5021c0a4f48ad569
Giulio <giulio.rebuffo@gmail.com>
2024-11-15 10:26:34 +0100

    save

---
+++ cl/phase1/forkchoice/public_keys_registry/interface.go
@@ -0,0 +1,16 @@
+package public_keys_registry
+
+import (
+	"github.com/erigontech/erigon-lib/common"
+	"github.com/erigontech/erigon/cl/abstract"
+	"github.com/erigontech/erigon/cl/cltypes/solid"
+)
+
+// PublicKeyRegistry is a registry of public keys
+// It is used to store public keys and their indices
+type PublicKeyRegistry interface {
+	ResetAnchor(s abstract.BeaconState)
+	VerifyAggregateSignature(checkpoint solid.Checkpoint, pubkeysIdxs *solid.RawUint64List, message []byte, signature common.Bytes96) (bool, error)
+	AddState(checkpoint solid.Checkpoint, s abstract.BeaconState)
+	Prune(epoch uint64)
+}
