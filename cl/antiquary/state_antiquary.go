554011758fa0434d299ebbac50eb425e622c60ba
Giulio rebuffo <giulio.rebuffo@gmail.com>
2024-11-15 02:49:13 +0100

    Caplin: make archive states downloadable (#12713)

--- cl/antiquary/state_antiquary.go
+++ cl/antiquary/state_antiquary.go
@@ -488,11 +488,15 @@ func (s *Antiquary) IncrementBeaconState(ctx context.Context, to uint64) error {
 	if err != nil {
 		return err
 	}
+
 	log.Info("Historical states antiquated", "slot", s.currentState.Slot(), "root", libcommon.Hash(stateRoot), "latency", endTime)
-	if s.snapgen {
+	if s.stateSn != nil {
 		if err := s.stateSn.OpenFolder(); err != nil {
 			return err
 		}
+	}
+
+	if s.snapgen {
 
 		// Keep gnosis out for a bit
 		if s.currentState.BeaconConfig().ConfigName == "gnosis" {
