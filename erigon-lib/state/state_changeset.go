f3889566ab277f226f4d8d759fc936b104be0a0f
alex.sharov <AskAlexSharov@gmail.com>
2024-11-18 19:37:42 +0700

    save

--- erigon-lib/state/state_changeset.go
+++ erigon-lib/state/state_changeset.go
@@ -267,7 +267,7 @@ func DeserializeKeys(in []byte) [kv.DomainLen][]DomainEntryDiff {
 	return ret
 }
 
-const diffChunkLen = 4*1024 - 32
+const diffChunkLen = 4*1024 - 48
 
 func WriteDiffSet(tx kv.RwTx, blockNumber uint64, blockHash common.Hash, diffSet *StateChangeSet) error {
 	// Write the diffSet to the database
