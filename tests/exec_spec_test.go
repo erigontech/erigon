5caf83055b10b3b9f2cc4ad0f4f62824aade9fba
Somnath Banerjee <snb895@outlook.com>
2024-11-20 14:48:44 +0400

    Undo spec test change

--- tests/exec_spec_test.go
+++ tests/exec_spec_test.go
@@ -20,7 +20,7 @@ func TestExecutionSpec(t *testing.T) {
 
 	bt := new(testMatcher)
 
-	dir := filepath.Join(".", "execution-spec-tests", "prague", "eip6110_deposits")
+	dir := filepath.Join(".", "execution-spec-tests")
 	checkStateRoot := true
 
 	bt.walk(t, dir, func(t *testing.T, name string, test *BlockTest) {
