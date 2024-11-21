6e6b5bbe1b7aa688d4d6e506d359f49ba3dab210
Max Revitt <max.revitt@gmail.com>
2024-11-21 09:40:36 +0000

    fix(ds): client - defer set streaming (#1492)

--- zk/datastream/client/stream_client.go
+++ zk/datastream/client/stream_client.go
@@ -509,6 +509,10 @@ func (c *StreamClient) handleSocketError(socketErr error) bool {
 // reads entries to the end of the stream
 // at end will wait for new entries to arrive
 func (c *StreamClient) readAllEntriesToChannel() (err error) {
+	defer func() {
+		c.setStreaming(false)
+	}()
+
 	c.setStreaming(true)
 	c.stopReadingToChannel.Store(false)
 
