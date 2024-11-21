430a325f3d9531f9038ccb6a3af05ba42dafc6a0
Giulio <giulio.rebuffo@gmail.com>
2024-11-21 17:29:06 +0100

    save

--- cl/beacon/handler/block_production.go
+++ cl/beacon/handler/block_production.go
@@ -187,8 +187,7 @@ func (a *ApiHandler) GetEthV1ValidatorAttestationData(
 			*slot,
 			*committeeIndex,
 		)
-		x, _ := json.Marshal(attestationData)
-		fmt.Println(string(x))
+
 		if err == attestation_producer.ErrHeadStateBehind {
 			return beaconhttp.NewEndpointError(
 				http.StatusServiceUnavailable,
