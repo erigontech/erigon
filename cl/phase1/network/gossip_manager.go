b88a5fcb066fffb8ff200bf3c1b1a8193b2e667e
Giulio <giulio.rebuffo@gmail.com>
2024-11-21 16:48:31 +0100

    save

--- cl/phase1/network/gossip_manager.go
+++ cl/phase1/network/gossip_manager.go
@@ -105,7 +105,7 @@ func NewGossipReceiver(
 		voluntaryExitService:         voluntaryExitService,
 		blsToExecutionChangeService:  blsToExecutionChangeService,
 		proposerSlashingService:      proposerSlashingService,
-		attestationsLimiter:          newTimeBasedRateLimiter(4*time.Second, 125),
+		attestationsLimiter:          newTimeBasedRateLimiter(6*time.Second, 125),
 		aggregateAndProofLimiter:     newTimeBasedRateLimiter(4*time.Second, 75),
 	}
 }
