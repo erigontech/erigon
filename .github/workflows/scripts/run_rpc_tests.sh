937fdff90f0bd94f4cc82f2b590efc245e1ed018
lupin012 <58134934+lupin012@users.noreply.github.com.>
2024-11-23 18:40:01 +0100

    add again these API because require access to erigon server

--- .github/workflows/scripts/run_rpc_tests.sh
+++ .github/workflows/scripts/run_rpc_tests.sh
@@ -8,7 +8,7 @@ disabled_tests=(
     trace_rawTransaction
     # Erigon bug https://github.com/erigontech/erigon/issues/12603
     erigon_getLatestLogs
-    # created task https://github.com/erigontech/erigon/issues/12668
+    # created task https://github.com/erigontech/erigon/issues/12668 is merged
     debug_traceCallMany/test_02.tar
     debug_traceCallMany/test_04.tar
     debug_traceCallMany/test_05.tar
@@ -20,6 +20,7 @@ disabled_tests=(
     engine_exchangeCapabilities/test_1.json
     engine_exchangeTransitionConfigurationV1/test_01.json
     engine_getClientVersionV1/test_1.json
+    # waiting PR https://github.com/erigontech/rpc-tests/pull/298 to be merged
     trace_replayBlockTransactions/test_29.tar
     # these tests requires Erigon active
     admin_nodeInfo/test_01.json
@@ -27,8 +28,10 @@ disabled_tests=(
     erigon_nodeInfo/test_1.json
     eth_coinbase/test_01.json
     eth_createAccessList/test_16.json
+    eth_getTransactionByHash/test_02.json
     eth_getWork/test_01.json
     eth_mining/test_01.json
+    eth_protocolVersion/test_1.json
     eth_submitHashrate/test_1.json
     eth_submitWork/test_1.json
     net_peerCount/test_1.json
