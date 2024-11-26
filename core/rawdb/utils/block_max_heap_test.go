07e1d00e170212a91c4e701a4bb02a092db490aa
sudeepdino008 <sudeepdino008@gmail.com>
2024-11-26 16:48:07 +0530

    -

--- core/rawdb/utils/block_max_heap_test.go
+++ core/rawdb/utils/block_max_heap_test.go
@@ -8,23 +8,14 @@ import (
 	"github.com/erigontech/erigon/core/rawdb/utils"
 )
 
-func NewB(id uint64, hash []byte) *utils.BlockId {
-	return &utils.BlockId{Number: id, Hash: common.BytesToHash(hash)}
-}
-
-func LastByte(b *utils.BlockId) byte {
-	by := b.Hash.Bytes()
-	return by[len(by)-1]
-}
-
 func TestPush(t *testing.T) {
 	h := utils.NewBlockMaxHeap(-1)
-	heap.Push(h, NewB(1, []byte{}))
+	heap.Push(h, newB(1, []byte{}))
 	if h.Len() != 1 {
 		t.Fatal("expected 1")
 	}
 
-	heap.Push(h, NewB(2, []byte{}))
+	heap.Push(h, newB(2, []byte{}))
 	if h.Len() != 2 {
 		t.Fatal("expected 2")
 	}
@@ -32,51 +23,60 @@ func TestPush(t *testing.T) {
 
 func TestPop(t *testing.T) {
 	h := utils.NewBlockMaxHeap(-1)
-	heap.Push(h, NewB(4, []byte{0x01}))
-	heap.Push(h, NewB(99, []byte{0x02}))
-	heap.Push(h, NewB(3, []byte{0x03}))
+	heap.Push(h, newB(4, []byte{0x01}))
+	heap.Push(h, newB(99, []byte{0x02}))
+	heap.Push(h, newB(3, []byte{0x03}))
 
 	x := heap.Pop(h).(*utils.BlockId)
-	if x.Number != 3 || LastByte(x) != 0x03 {
+	if x.Number != 3 || lastByte(x) != 0x03 {
 		t.Fatal("unexpected")
 	}
 
 	x = heap.Pop(h).(*utils.BlockId)
-	if x.Number != 4 || LastByte(x) != 0x01 {
+	if x.Number != 4 || lastByte(x) != 0x01 {
 		t.Fatal("unexpected")
 	}
 
 	x = h.Pop().(*utils.BlockId)
-	if x.Number != 99 || LastByte(x) != 0x02 {
+	if x.Number != 99 || lastByte(x) != 0x02 {
 		t.Fatal("unexpected")
 	}
 }
 
 func TestPopWithLimits(t *testing.T) {
 	h := utils.NewBlockMaxHeap(2)
-	heap.Push(h, NewB(4, []byte{0x01}))
-	heap.Push(h, NewB(2, []byte{0x04}))
-	heap.Push(h, NewB(99, []byte{0x02}))
-	heap.Push(h, NewB(3, []byte{0x03}))
+	heap.Push(h, newB(4, []byte{0x01}))
+	heap.Push(h, newB(2, []byte{0x04}))
+	heap.Push(h, newB(99, []byte{0x02}))
+	heap.Push(h, newB(3, []byte{0x03}))
 
 	if h.Len() != 2 {
 		t.Fatal("expected 2 got ", h.Len())
 	}
 	x := heap.Pop(h).(*utils.BlockId)
-	if x.Number != 4 || LastByte(x) != 0x01 {
+	if x.Number != 4 || lastByte(x) != 0x01 {
 		t.Fatal("unexpected")
 	}
 
 	x = heap.Pop(h).(*utils.BlockId)
-	if x.Number != 99 || LastByte(x) != 0x02 {
+	if x.Number != 99 || lastByte(x) != 0x02 {
 		t.Fatal("unexpected")
 	}
 }
 
 func TestPopWithEmptyHeap(t *testing.T) {
 	h := utils.NewBlockMaxHeap(0)
-	heap.Push(h, NewB(4, []byte{0x01}))
+	heap.Push(h, newB(4, []byte{0x01}))
 	if h.Len() != 0 {
 		t.Fatal("expected 0")
 	}
+}
+
+func newB(id uint64, hash []byte) *utils.BlockId {
+	return &utils.BlockId{Number: id, Hash: common.BytesToHash(hash)}
+}
+
+func lastByte(b *utils.BlockId) byte {
+	by := b.Hash.Bytes()
+	return by[len(by)-1]
 }
