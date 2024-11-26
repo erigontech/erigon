175102fb82c6ef4e15f58a9c833c1bd500b95f2c
alex.sharov <AskAlexSharov@gmail.com>
2024-11-26 13:17:44 +0700

    save

--- polygon/bridge/snapshot_store.go
+++ polygon/bridge/snapshot_store.go
@@ -263,6 +263,7 @@ func (s *SnapshotStore) BlockEventIdsRange(ctx context.Context, blockNum uint64)
 		}
 	}
 
+	panic(fmt.Errorf("%w: %d", ErrEventIdRangeNotFound, blockNum))
 	return 0, 0, fmt.Errorf("%w: %d", ErrEventIdRangeNotFound, blockNum)
 }
 
