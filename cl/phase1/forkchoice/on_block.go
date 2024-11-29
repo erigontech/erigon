7cd41c31c04ace729adc1ef8a1d93471f2d61e63
Giulio <giulio.rebuffo@gmail.com>
2024-11-29 10:51:22 +0100

    save

--- cl/phase1/forkchoice/on_block.go
+++ cl/phase1/forkchoice/on_block.go
@@ -209,7 +209,7 @@ func (f *ForkChoiceStore) OnBlock(ctx context.Context, block *cltypes.SignedBeac
 			if err := f.optimisticStore.ValidateBlock(block.Block); err != nil {
 				return fmt.Errorf("failed to validate block in optimistic store: %v", err)
 			}
-			f.verifiedExecutionPayload.Add(block.Block.Body.ExecutionPayload.BlockHash, struct{}{})
+			f.verifiedExecutionPayload.Add(blockRoot, struct{}{})
 		}
 		if err != nil {
 			return fmt.Errorf("newPayload failed: %v", err)
