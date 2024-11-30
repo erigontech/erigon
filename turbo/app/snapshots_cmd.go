3b6a6cea77e3c81d1e48e47fc6c2f977c9430849
Willian Mitsuda <wmitsuda@gmail.com>
2024-11-30 02:20:54 -0300

    Rename constant to DefaultStepSize (#12928)
    I always forget that constant name when I need to check the value, let's
    rename it to something more direct.
    
    I think we could also rename/move the package "config3" to some other
    place with global constants, but no idea of good place.

--- turbo/app/snapshots_cmd.go
+++ turbo/app/snapshots_cmd.go
@@ -1473,7 +1473,7 @@ func dbCfg(label kv.Label, path string) mdbx.MdbxOpts {
 		Accede(true) // integration tool: open db without creation and without blocking erigon
 }
 func openAgg(ctx context.Context, dirs datadir.Dirs, chainDB kv.RwDB, logger log.Logger) *libstate.Aggregator {
-	agg, err := libstate.NewAggregator(ctx, dirs, config3.HistoryV3AggregationStep, chainDB, logger)
+	agg, err := libstate.NewAggregator(ctx, dirs, config3.DefaultStepSize, chainDB, logger)
 	if err != nil {
 		panic(err)
 	}
