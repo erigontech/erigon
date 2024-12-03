46a6266c5391a3a53d6f83ae8bee6bf9e5611ec4
yperbasis <andrey.ashikhmin@gmail.com>
2024-12-03 12:57:02 +0100

    post-revert fix

--- core/state/intra_block_state.go
+++ core/state/intra_block_state.go
@@ -292,7 +292,7 @@ func (sdb *IntraBlockState) GetCodeSize(addr libcommon.Address) int {
 		return len(stateObject.code)
 	}
 	if stateObject.data.CodeHash == emptyCodeHashH {
-		return 0, nil
+		return 0
 	}
 	l, err := sdb.stateReader.ReadAccountCodeSize(addr, stateObject.data.Incarnation)
 	if err != nil {
