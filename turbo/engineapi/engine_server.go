erigontech/erigon

commit 5d999358f09e7e7c73bfd7c906d3cc66ac349b96

engineapi: Add version-based check for requests list (#12979)
As per the
[specs](https://github.com/ethereum/execution-apis/blob/main/src/engine/prague.md#engine_newpayloadv4)
`V4`-Prague follows on from `V3`-Cancun, plus the added changes. So, in
my interpretation, the following checks should hold:
- Params validation precedes over fork-version check. 
- Param validation for the additional param `executionRequests` should
have the same semantics as point (1) from "Specification" section of
[`newPayloadV3`](https://github.com/ethereum/execution-apis/blob/main/src/engine/cancun.md#engine_getpayloadv3)

(This may break some opinionated hive tests, and potentially some spec
tests)

turbo/engineapi/engine_server.go
@@ -138,17 +138,16 @@ func (s *EngineServer) checkWithdrawalsPresence(time uint64, withdrawals types.W
 	return nil
 }
 
-func (s *EngineServer) checkRequestsPresence(time uint64, executionRequests []hexutility.Bytes) error {
-	if !s.config.IsPrague(time) {
-		if executionRequests != nil {
-			return &rpc.InvalidParamsError{Message: "requests before Prague"}
-		}
+func (s *EngineServer) checkRequestsPresence(version clparams.StateVersion, executionRequests []hexutility.Bytes) error {
+	if version < clparams.ElectraVersion && executionRequests != nil {
+		return &rpc.InvalidParamsError{Message: "requests in EngineAPI not supported before Prague"}
 	}
-	// if s.config.IsPrague(time) {
-	//   if len(executionRequests) < 3 {
-	// 		return &rpc.InvalidParamsError{Message: "missing requests list"}
-	// 	}
-	// }
+	if executionRequests == nil {
+		return &rpc.InvalidParamsError{Message: "missing requests list"}
+	}
+	if len(executionRequests) != len(types.KnownRequestTypes) {
+		return &rpc.InvalidParamsError{Message: "invalid requests lists"}
+	}
 	return nil
 }
 
@@ -204,7 +203,7 @@ func (s *EngineServer) newPayload(ctx context.Context, req *engine_types.Executi
 	}
 
 	var requests types.FlatRequests
-	if err := s.checkRequestsPresence(header.Time, executionRequests); err != nil {
+	if err := s.checkRequestsPresence(version, executionRequests); err != nil {
 		return nil, err
 	}
 	if version >= clparams.ElectraVersion {

commit e56550f637a40e1ecd4927094c59d325eddb92c5

Fix bug on pre-Pectra `newPayload` (#12998)
Fixes bug introduced in https://github.com/erigontech/erigon/pull/12979

turbo/engineapi/engine_server.go
@@ -139,14 +139,17 @@ func (s *EngineServer) checkWithdrawalsPresence(time uint64, withdrawals types.W
 }
 
 func (s *EngineServer) checkRequestsPresence(version clparams.StateVersion, executionRequests []hexutility.Bytes) error {
-	if version < clparams.ElectraVersion && executionRequests != nil {
-		return &rpc.InvalidParamsError{Message: "requests in EngineAPI not supported before Prague"}
-	}
-	if executionRequests == nil {
-		return &rpc.InvalidParamsError{Message: "missing requests list"}
-	}
-	if len(executionRequests) != len(types.KnownRequestTypes) {
-		return &rpc.InvalidParamsError{Message: "invalid requests lists"}
+	if version < clparams.ElectraVersion {
+		if executionRequests != nil {
+			return &rpc.InvalidParamsError{Message: "requests in EngineAPI not supported before Prague"}
+		}
+	} else {
+		if executionRequests == nil {
+			return &rpc.InvalidParamsError{Message: "missing requests list"}
+		}
+		if len(executionRequests) != len(types.KnownRequestTypes) {
+			return &rpc.InvalidParamsError{Message: "invalid requests lists"}
+		}
 	}
 	return nil
 }