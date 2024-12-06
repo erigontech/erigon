f5646b347a47f1b12337bd23474a298ca6946375
Andrew Ashikhmin <34320705+yperbasis@users.noreply.github.com>
2024-12-06 21:50:14 +0100

    Deduplicate code in BlockReader.blockWithSenders

--- turbo/snapshotsync/freezeblocks/block_reader.go
+++ turbo/snapshotsync/freezeblocks/block_reader.go
@@ -712,32 +712,23 @@ func (r *BlockReader) blockWithSenders(ctx context.Context, tx kv.Getter, hash c
 		}
 		return
 	}
-	if txsAmount == 0 {
-		block = types.NewBlockFromStorage(hash, h, nil, b.Uncles, b.Withdrawals)
-		if len(senders) != block.Transactions().Len() {
-			if dbgLogs {
-				log.Info(dbgPrefix + fmt.Sprintf("found block with %d transactions, but %d senders", block.Transactions().Len(), len(senders)))
-			}
-			return block, senders, nil // no senders is fine - will recover them on the fly
-		}
-		block.SendersToTxs(senders)
-		return block, senders, nil
-	}
 
-	txnSeg, ok, release := r.sn.ViewSingleFile(coresnaptype.Transactions, blockHeight)
-	if !ok {
-		if dbgLogs {
-			log.Info(dbgPrefix+"no transactions file for this block num", "r.sn.BlocksAvailable()", r.sn.BlocksAvailable(), "r.sn.indicesReady", r.sn.indicesReady.Load())
-		}
-		return
-	}
-	defer release()
 	var txs []types.Transaction
-	txs, senders, err = r.txsFromSnapshot(baseTxnId, txsAmount, txnSeg, buf)
-	if err != nil {
-		return nil, nil, err
+	if txsAmount != 0 {
+		txnSeg, ok, release := r.sn.ViewSingleFile(coresnaptype.Transactions, blockHeight)
+		if !ok {
+			if dbgLogs {
+				log.Info(dbgPrefix+"no transactions file for this block num", "r.sn.BlocksAvailable()", r.sn.BlocksAvailable(), "r.sn.indicesReady", r.sn.indicesReady.Load())
+			}
+			return
+		}
+		defer release()
+		txs, senders, err = r.txsFromSnapshot(baseTxnId, txsAmount, txnSeg, buf)
+		if err != nil {
+			return nil, nil, err
+		}
+		release()
 	}
-	release()
 
 	block = types.NewBlockFromStorage(hash, h, txs, b.Uncles, b.Withdrawals)
 	if len(senders) != block.Transactions().Len() {
