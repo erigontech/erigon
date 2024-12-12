04f7466627aca731f8984ad9b596b6be4d379859
alex.sharov <AskAlexSharov@gmail.com>
2024-12-12 09:39:10 +0700

    merge main

--- eth/stagedsync/stage_custom_trace.go
+++ eth/stagedsync/stage_custom_trace.go
@@ -173,9 +173,9 @@ func customTraceBatch(ctx context.Context, cfg *exec3.ExecArgs, tx kv.TemporalRw
 	var m runtime.MemStats
 	if err := exec3.CustomTraceMapReduce(fromBlock, toBlock, exec3.TraceConsumer{
 		NewTracer: func() exec3.GenericTracer { return nil },
-		Reduce: func(txTask *state.TxTask, tx kv.Tx) (err error) {
+		Reduce: func(txTask *state.TxTask, tx kv.Tx) error {
 			if txTask.Error != nil {
-				return err
+				return txTask.Error
 			}
 
 			if txTask.Tx != nil {
