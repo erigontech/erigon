c8b767b0c82f7b2a3f13f5306d991e43ee099359
ChengenH <hce19970702@gmail.com>
2024-12-13 16:16:09 +0800

    refactor: unify the error handling methods in the crypto package that are different from the project style
    Signed-off-by: ChengenH <hce19970702@gmail.com>

--- cl/beacon/handler/block_production.go
+++ cl/beacon/handler/block_production.go
@@ -212,7 +212,7 @@ func (a *ApiHandler) GetEthV1ValidatorAttestationData(
 			*committeeIndex,
 		)
 
-		if err == attestation_producer.ErrHeadStateBehind {
+		if errors.Is(attestation_producer.ErrHeadStateBehind, err) {
 			return beaconhttp.NewEndpointError(
 				http.StatusServiceUnavailable,
 				synced_data.ErrNotSynced,
