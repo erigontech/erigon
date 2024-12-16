5ff2c08402f058bcdedda4be289417ab103856ec
alex.sharov <AskAlexSharov@gmail.com>
2024-12-16 13:49:59 +0700

    merge main

--- core/rawdb/rawtemporaldb/accessors_receipt.go
+++ core/rawdb/rawtemporaldb/accessors_receipt.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/erigontech/erigon-lib/common"
 	"github.com/erigontech/erigon-lib/kv"
-	"github.com/erigontech/erigon-lib/kv/order"
 	"github.com/erigontech/erigon/core/types"
 )
 
