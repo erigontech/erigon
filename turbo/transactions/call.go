628b2eabfbb035f042a84e7fed1a50e53bd942c6
alex.sharov <AskAlexSharov@gmail.com>
2024-12-17 15:14:10 +0700

    save

--- turbo/transactions/call.go
+++ turbo/transactions/call.go
@@ -3,6 +3,7 @@ package transactions
 import (
 	"context"
 	"fmt"
+	"math"
 	"time"
 
 	"github.com/holiman/uint256"
@@ -92,7 +93,12 @@ func DoCall(
 		evm.Cancel()
 	}()
 
-	gp := new(core.GasPool).AddGas(msg.Gas()).AddBlobGas(msg.BlobGas())
+	gp := new(core.GasPool)
+	if gasCap == 0 {
+		gp.AddGas(math.MaxUint64)
+	} else {
+		gp.AddGas(gasCap)
+	}
 	result, err := core.ApplyMessage(evm, msg, gp, true /* refunds */, false /* gasBailout */)
 	if err != nil {
 		return nil, err
