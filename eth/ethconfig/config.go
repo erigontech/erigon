7e42c42ca5c4b0b7a2013ac2cfa9af9762d7a835
taratorio <94537774+taratorio@users.noreply.github.com>
2024-12-20 21:49:23 +0200

    txpool: tidy NoGossip config

--- eth/ethconfig/config.go
+++ eth/ethconfig/config.go
@@ -257,8 +257,6 @@ type Config struct {
 	SilkwormRpcLogDumpResponse   bool
 	SilkwormRpcNumWorkers        uint32
 	SilkwormRpcJsonCompatibility bool
-
-	DisableTxPoolGossip bool
 }
 
 type Sync struct {
