a8d35b8f0ef4a805f354388eef57356055174d75
alex.sharov <AskAlexSharov@gmail.com>
2024-12-21 13:25:16 +0700

    save

--- core/rawdb/accessors_indexes.go
+++ core/rawdb/accessors_indexes.go
@@ -24,6 +24,7 @@ import (
 
 	libcommon "github.com/erigontech/erigon-lib/common"
 	"github.com/erigontech/erigon-lib/kv"
+	"github.com/erigontech/erigon-lib/log/v3"
 	"github.com/erigontech/erigon/core/types"
 )
 
