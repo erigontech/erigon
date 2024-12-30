e877e50acfba1fbb4cfab3153e68585a4f2ec498
alex.sharov <AskAlexSharov@gmail.com>
2024-12-30 11:25:48 +0700

    save

--- erigon-lib/recsplit/recsplit_test.go
+++ erigon-lib/recsplit/recsplit_test.go
@@ -197,51 +197,9 @@ func BenchmarkTwoLayerIndex(b *testing.B) {
 	tmpDir := b.TempDir()
 	indexFile := filepath.Join(tmpDir, "index")
 	salt := uint32(1)
-	N := 1_000_000
+	N := 10_000_000
 	const BucketSize = 10
 
-	//b.Run("0", func(b *testing.B) {
-	//	rs, err := NewRecSplit(RecSplitArgs{
-	//		KeyCount:   N,
-	//		BucketSize: BucketSize,
-	//		Salt:       &salt,
-	//		TmpDir:     tmpDir,
-	//
-	//		IndexFile:          indexFile,
-	//		LeafSize:           16,
-	//		Enums:              true,
-	//		LessFalsePositives: true,
-	//	}, logger)
-	//	defer rs.Close()
-	//	// failsCnt=25251323, LeafSize=24
-	//	// failsCnt=13944472, LeafSize=16
-	//	// failsCnt=105941, LeafSize=8
-	//	// failsCnt=7210, LeafSize=4
-	//	require.NoError(b, err)
-	//	for i := 0; i < N; i++ {
-	//		err = rs.AddKey([]byte(fmt.Sprintf("key %d", i)), uint64(i*17))
-	//		require.NoError(b, err)
-	//	}
-	//	err = rs.Build(context.Background())
-	//	require.NoError(b, err)
-	//
-	//	idx := MustOpen(indexFile)
-	//	defer idx.Close()
-	//	reader := NewIndexReader(idx)
-	//
-	//	hi := make([]uint64, N)
-	//	lo := make([]uint64, N)
-	//	for j := 0; j < N; j++ {
-	//		hi[j], lo[j] = reader.Sum([]byte(fmt.Sprintf("key %d", j)))
-	//	}
-	//
-	//	b.ResetTimer()
-	//	for i := 0; i < b.N; i++ {
-	//		for j := 0; j < N; j++ {
-	//			_, _ = reader.TwoLayerLookupByHash(hi[j], lo[j])
-	//		}
-	//	}
-	//})
 	b.Run("1", func(b *testing.B) {
 		rs, err := NewRecSplit(RecSplitArgs{
 			KeyCount:           N,
