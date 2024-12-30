08a0eebb08b3abab3cd52b83c24a9921aff471ec
alex.sharov <AskAlexSharov@gmail.com>
2024-12-30 10:08:13 +0700

    save

--- cmd/integration/commands/stages.go
+++ cmd/integration/commands/stages.go
@@ -1133,16 +1133,30 @@ func stageExec(db kv.TemporalRwDB, ctx context.Context, logger log.Logger) error
 			block = sendersProgress
 		}
 
-		if err := db.Update(ctx, func(tx kv.RwTx) error {
+		if noCommit {
+			tx, err := db.BeginTemporalRw(ctx)
+			if err != nil {
+				return err
+			}
+			defer tx.Rollback()
 			for bn := execProgress; bn < block; bn++ {
 				txc.Tx = tx
 				if err := stagedsync.SpawnExecuteBlocksStage(s, sync, txc, bn, ctx, cfg, logger); err != nil {
 					return err
 				}
 			}
-			return nil
-		}); err != nil {
-			return err
+		} else {
+			if err := db.Update(ctx, func(tx kv.RwTx) error {
+				for bn := execProgress; bn < block; bn++ {
+					txc.Tx = tx
+					if err := stagedsync.SpawnExecuteBlocksStage(s, sync, txc, bn, ctx, cfg, logger); err != nil {
+						return err
+					}
+				}
+				return nil
+			}); err != nil {
+				return err
+			}
 		}
 		return nil
 	}
