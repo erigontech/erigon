a9301cd2e9b2da9281f5d37515f07742bb0719c3
alex.sharov <AskAlexSharov@gmail.com>
2024-12-30 11:25:00 +0700

    save

--- erigon-lib/recsplit/recsplit_test.go
+++ erigon-lib/recsplit/recsplit_test.go
@@ -280,10 +280,85 @@ func BenchmarkTwoLayerIndex(b *testing.B) {
 		}
 	})
 
-	b.Run("4", func(b *testing.B) {
+	b.Run("3", func(b *testing.B) {
 		rs, err := NewRecSplit(RecSplitArgs{
 			KeyCount:           N,
 			BucketSize:         100,
+			Salt:               &salt,
+			TmpDir:             tmpDir,
+			IndexFile:          indexFile,
+			LeafSize:           8,
+			Enums:              true,
+			LessFalsePositives: true,
+		}, logger)
+		defer rs.Close()
+		require.NoError(b, err)
+		for i := 0; i < N; i++ {
+			err = rs.AddKey([]byte(fmt.Sprintf("key %d", i)), uint64(i*17))
+			require.NoError(b, err)
+		}
+		err = rs.Build(context.Background())
+		require.NoError(b, err)
+
+		idx := MustOpen(indexFile)
+		defer idx.Close()
+		reader := NewIndexReader(idx)
+
+		hi := make([]uint64, N)
+		lo := make([]uint64, N)
+		for j := 0; j < N; j++ {
+			hi[j], lo[j] = reader.Sum([]byte(fmt.Sprintf("key %d", j)))
+		}
+
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			for j := 0; j < N; j++ {
+				_, _ = reader.TwoLayerLookupByHash(hi[j], lo[j])
+			}
+		}
+	})
+
+	b.Run("4", func(b *testing.B) {
+		rs, err := NewRecSplit(RecSplitArgs{
+			KeyCount:           N,
+			BucketSize:         500,
+			Salt:               &salt,
+			TmpDir:             tmpDir,
+			IndexFile:          indexFile,
+			LeafSize:           8,
+			Enums:              true,
+			LessFalsePositives: true,
+		}, logger)
+		defer rs.Close()
+		require.NoError(b, err)
+		for i := 0; i < N; i++ {
+			err = rs.AddKey([]byte(fmt.Sprintf("key %d", i)), uint64(i*17))
+			require.NoError(b, err)
+		}
+		err = rs.Build(context.Background())
+		require.NoError(b, err)
+
+		idx := MustOpen(indexFile)
+		defer idx.Close()
+		reader := NewIndexReader(idx)
+
+		hi := make([]uint64, N)
+		lo := make([]uint64, N)
+		for j := 0; j < N; j++ {
+			hi[j], lo[j] = reader.Sum([]byte(fmt.Sprintf("key %d", j)))
+		}
+
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			for j := 0; j < N; j++ {
+				_, _ = reader.TwoLayerLookupByHash(hi[j], lo[j])
+			}
+		}
+	})
+	b.Run("5", func(b *testing.B) {
+		rs, err := NewRecSplit(RecSplitArgs{
+			KeyCount:           N,
+			BucketSize:         512,
 			Salt:               &salt,
 			TmpDir:             tmpDir,
 			IndexFile:          indexFile,
