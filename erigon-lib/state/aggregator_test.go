48c9fac146308d0f4e3175641a1afae1d786b053
alex.sharov <AskAlexSharov@gmail.com>
2024-12-30 17:58:36 +0700

    save

--- erigon-lib/state/aggregator_test.go
+++ erigon-lib/state/aggregator_test.go
@@ -1273,9 +1273,24 @@ func TestAggregator_RebuildCommitmentBasedOnFiles(t *testing.T) {
 	compression := ac.d[kv.CommitmentDomain].d.compression
 	fnames := []string{}
 	for _, f := range ac.d[kv.CommitmentDomain].files {
-		k, stateVal, _, found, err := f.src.bindex.Get(keyCommitmentState, seg.NewReader(f.src.decompressor.MakeGetter(), compression))
-		require.NoError(t, err)
-		require.True(t, found)
+		var k, stateVal []byte
+		if ac.d[kv.CommitmentDomain].d.indexList&withHashMap == 0 {
+			idx := f.src.index.GetReaderFromPool()
+			r := seg.NewReader(f.src.decompressor.MakeGetter(), compression)
+
+			offset, ok := idx.TwoLayerLookup(keyCommitmentState)
+			require.True(t, ok)
+			r.Reset(offset)
+			k, _ = r.Next(nil)
+			stateVal, _ = r.Next(nil)
+		} else {
+			var found bool
+			var err error
+			k, stateVal, _, found, err = f.src.bindex.Get(keyCommitmentState, seg.NewReader(f.src.decompressor.MakeGetter(), compression))
+			require.NoError(t, err)
+			require.True(t, found)
+			require.EqualValues(t, keyCommitmentState, k)
+		}
 		require.EqualValues(t, keyCommitmentState, k)
 		rh, err := commitment.HexTrieExtractStateRoot(stateVal)
 		require.NoError(t, err)
