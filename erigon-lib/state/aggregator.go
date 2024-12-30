a0a5a4d932696d020f7f77078d7aee2de02bf0ac
alex.sharov <AskAlexSharov@gmail.com>
2024-12-30 17:07:36 +0700

    save

--- erigon-lib/state/aggregator.go
+++ erigon-lib/state/aggregator.go
@@ -126,9 +126,7 @@ func domainIntegrityCheck(name kv.Domain, dirs datadir.Dirs, fromStep, toStep ui
 	}
 }
 
-var dbgCommBtIndex = dbg.EnvBool("AGG_COMMITMENT_BT", false)
-
-var Schema = map[kv.Domain]*domainCfg{
+var Schema = map[kv.Domain]domainCfg{
 	kv.AccountsDomain: {
 		name: kv.AccountsDomain, valuesTable: kv.TblAccountVals,
 
@@ -197,7 +195,7 @@ var Schema = map[kv.Domain]*domainCfg{
 	kv.CommitmentDomain: {
 		name: kv.CommitmentDomain, valuesTable: kv.TblCommitmentVals,
 
-		indexList:   withHashMap,
+		indexList:   withBTree | withExistence,
 		compression: seg.CompressKeys,
 		compressCfg: DomainCompressCfg,
 
@@ -246,9 +244,6 @@ func NewAggregator(ctx context.Context, dirs datadir.Dirs, aggregationStep uint6
 		return nil, err
 	}
 
-	if dbgCommBtIndex {
-		Schema[kv.CommitmentDomain].indexList = withBTree | withExistence
-	}
 	ctx, ctxCancel := context.WithCancel(ctx)
 	a := &Aggregator{
 		ctx:                    ctx,
@@ -356,7 +351,7 @@ func (a *Aggregator) registerDomain(name kv.Domain, salt *uint32, dirs datadir.D
 	cfg.hist.iiCfg.salt = salt
 	cfg.hist.iiCfg.dirs = dirs
 	cfg.hist.iiCfg.aggregationStep = aggregationStep
-	a.d[name], err = NewDomain(*cfg, logger)
+	a.d[name], err = NewDomain(cfg, logger)
 	if err != nil {
 		return err
 	}
