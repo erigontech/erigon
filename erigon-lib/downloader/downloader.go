da1cdce3c5798d17fafbf286e752e5aa1b7ef7c1
Dmytro Vovk <vovk.dimon@gmail.com>
2025-01-02 22:27:17 +0000

    save

--- erigon-lib/downloader/downloader.go
+++ erigon-lib/downloader/downloader.go
@@ -2293,7 +2293,7 @@ func (d *Downloader) ReCalcStats(interval time.Duration) {
 	d.lock.Unlock()
 
 	if !stats.Completed {
-		logger.Debug("[snapshots] downloading",
+		logger.Info("[snapshots] downloading",
 			"len", len(torrents),
 			"webTransfers", webTransfers,
 			"torrent", torrentInfo,
