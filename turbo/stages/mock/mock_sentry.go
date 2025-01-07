776d0824fb1ec1e299de0b6bad8b3a568d5e744e
milen <94537774+taratorio@users.noreply.github.com>
2025-01-07 15:57:14 +0000

    txpool: streamline initialisation (#13202)
    Attempt to simplify how we initialise and run the`TxPool`. 
    
    Observations:
    1. Currently the package exports many sub-components to backend.go such
    as `Fetch`, `Sender`, `PoolDB`, `newTxs chan txpool.Announcements`. I
    see these all as internal components of the `TxPool` that `backend.go`
    and other initialisers should not care about. In reality, the only
    things `backend.go` cares about are the `TxPool` and the
    `TxPoolGrpcServer`.
    2. Additionally, logic about starting and closing the tx pool leaks to
    backend.go (ie the creator of tx pool needs to know what sub-components
    need to be started and disposed).
    
    This PR addresses the above by:
    1. Moving the p2p fetcher, sender and pool db as internal attributes of
    TxPool.
    2. Introducing the `Run(ctx)` pattern that we are using in Astrid and
    Shutter where each runnable component is responsible for starting its
    own background goroutines, loops, listeners, etc. and also is
    responsible for shutting them all down by being ctx-aware (ie when ctx
    is cancelled all sub components gets disposed of/shutdown)

--- turbo/stages/mock/mock_sentry.go
+++ turbo/stages/mock/mock_sentry.go
@@ -29,8 +29,9 @@ import (
 
 	"github.com/c2h5oh/datasize"
 	lru "github.com/hashicorp/golang-lru/arc/v2"
-	"github.com/holiman/uint256"
+	"github.com/stretchr/testify/require"
 	"go.uber.org/mock/gomock"
+	"golang.org/x/sync/errgroup"
 	"golang.org/x/sync/semaphore"
 	"google.golang.org/protobuf/types/known/emptypb"
 
@@ -44,6 +45,7 @@ import (
 	proto_downloader "github.com/erigontech/erigon-lib/gointerfaces/downloaderproto"
 	execution "github.com/erigontech/erigon-lib/gointerfaces/executionproto"
 	proto_sentry "github.com/erigontech/erigon-lib/gointerfaces/sentryproto"
+	"github.com/erigontech/erigon-lib/gointerfaces/txpoolproto"
 	ptypes "github.com/erigontech/erigon-lib/gointerfaces/typesproto"
 	"github.com/erigontech/erigon-lib/kv"
 	"github.com/erigontech/erigon-lib/kv/kvcache"
@@ -125,11 +127,8 @@ type MockSentry struct {
 	Notifications *shards.Notifications
 
 	// TxPool
-	TxPoolFetch      *txpool.Fetch
-	TxPoolSend       *txpool.Send
-	TxPoolGrpcServer *txpool.GrpcServer
 	TxPool           *txpool.TxPool
-	txPoolDB         kv.RwDB
+	TxPoolGrpcServer txpoolproto.TxpoolServer
 
 	HistoryV3      bool
 	agg            *libstate.Aggregator
@@ -137,13 +136,11 @@ type MockSentry struct {
 	BlockReader    services.FullBlockReader
 	ReceiptsReader *receipts.Generator
 	posStagedSync  *stagedsync.Sync
+	bgComponentsEg errgroup.Group
 }
 
 func (ms *MockSentry) Close() {
 	ms.cancel()
-	if ms.txPoolDB != nil {
-		ms.txPoolDB.Close()
-	}
 	if ms.Engine != nil {
 		ms.Engine.Close()
 	}
@@ -155,6 +152,9 @@ func (ms *MockSentry) Close() {
 	}
 	if ms.DB != nil {
 		ms.DB.Close()
+	}
+	if err := ms.bgComponentsEg.Wait(); err != nil {
+		require.Equal(ms.tb, context.Canceled, err) // upon waiting for clean exit we should get ctx cancelled
 	}
 }
 
@@ -318,6 +318,17 @@ func MockWithEverything(tb testing.TB, gspec *types.Genesis, key *ecdsa.PrivateK
 	if tb != nil {
 		tb.Cleanup(mock.Close)
 	}
+
+	// Committed genesis will be shared between download and mock sentry
+	_, mock.Genesis, err = core.CommitGenesisBlock(mock.DB, gspec, datadir.New(tmpdir), mock.Log)
+	if _, ok := err.(*chain.ConfigCompatError); err != nil && !ok {
+		if tb != nil {
+			tb.Fatal(err)
+		} else {
+			panic(err)
+		}
+	}
+
 	blockWriter := blockio.NewBlockWriter()
 
 	mock.Address = crypto.PubkeyToAddress(mock.Key.PublicKey)
@@ -333,49 +344,33 @@ func MockWithEverything(tb testing.TB, gspec *types.Genesis, key *ecdsa.PrivateK
 	blockPropagator := func(Ctx context.Context, header *types.Header, body *types.RawBody, td *big.Int) {}
 	if !cfg.TxPool.Disable {
 		poolCfg := txpoolcfg.DefaultConfig
-		newTxs := make(chan txpool.Announcements, 1024)
-		if tb != nil {
-			tb.Cleanup(func() {
-				close(newTxs)
-			})
-		}
-		chainID, _ := uint256.FromBig(mock.ChainConfig.ChainID)
-		shanghaiTime := mock.ChainConfig.ShanghaiTime
-		cancunTime := mock.ChainConfig.CancunTime
-		pragueTime := mock.ChainConfig.PragueTime
-		maxBlobsPerBlock := mock.ChainConfig.GetMaxBlobsPerBlock()
-		mock.txPoolDB = memdb.NewWithLabel(tmpdir, kv.TxPoolDB)
-		mock.TxPool, err = txpool.New(newTxs, mock.txPoolDB, mock.DB, poolCfg, kvcache.NewDummy(), *chainID, shanghaiTime, nil /* agraBlock */, cancunTime, pragueTime, maxBlobsPerBlock, nil, logger)
+		stateChangesClient := direct.NewStateDiffClientDirect(erigonGrpcServeer)
+		mock.TxPool, mock.TxPoolGrpcServer, err = txpool.Assemble(
+			ctx,
+			poolCfg,
+			mock.DB,
+			kvcache.NewDummy(),
+			sentries,
+			stateChangesClient,
+			func() {}, /* builderNotifyNewTxns */
+			logger,
+			txpool.WithP2PFetcherWg(&mock.ReceiveWg),
+			txpool.WithP2PSenderWg(nil),
+			txpool.WithFeeCalculator(nil),
+			txpool.WithPoolDBInitializer(func(_ context.Context, _ txpoolcfg.Config, _ log.Logger) (kv.RwDB, error) {
+				return memdb.NewWithLabel(tmpdir, kv.TxPoolDB), nil
+			}),
+		)
 		if err != nil {
 			tb.Fatal(err)
 		}
 
-		stateChangesClient := direct.NewStateDiffClientDirect(erigonGrpcServeer)
-
-		mock.TxPoolFetch = txpool.NewFetch(mock.Ctx, sentries, mock.TxPool, stateChangesClient, mock.DB, mock.txPoolDB, *chainID, logger)
-		mock.TxPoolFetch.SetWaitGroup(&mock.ReceiveWg)
-		mock.TxPoolSend = txpool.NewSend(mock.Ctx, sentries, mock.TxPool, logger)
-		mock.TxPoolGrpcServer = txpool.NewGrpcServer(mock.Ctx, mock.TxPool, mock.txPoolDB, *chainID, logger)
-
-		mock.TxPoolFetch.ConnectCore()
 		mock.StreamWg.Add(1)
-		mock.TxPoolFetch.ConnectSentries()
+		mock.bgComponentsEg.Go(func() error { return mock.TxPool.Run(ctx) })
 		mock.StreamWg.Wait()
-
-		go txpool.MainLoop(mock.Ctx, mock.TxPool, newTxs, mock.TxPoolSend, mock.TxPoolGrpcServer.NewSlotsStreams, func() {})
 	}
 
-	// Committed genesis will be shared between download and mock sentry
-	_, mock.Genesis, err = core.CommitGenesisBlock(mock.DB, gspec, datadir.New(tmpdir), mock.Log)
-	if _, ok := err.(*chain.ConfigCompatError); err != nil && !ok {
-		if tb != nil {
-			tb.Fatal(err)
-		} else {
-			panic(err)
-		}
-	}
 	latestBlockBuiltStore := builder.NewLatestBlockBuiltStore()
-
 	inMemoryExecution := func(txc wrap.TxContainer, header *types.Header, body *types.RawBody, unwindPoint uint64, headersChain []*types.Header, bodiesChain []*types.RawBody,
 		notifications *shards.Notifications) error {
 		terseLogger := log.New()
