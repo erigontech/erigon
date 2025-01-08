0aaed4b51f682ec1cbb031a2710ffabebcb9cbdf
Tornado Contrib <164305046+tornadocontrib@users.noreply.github.com>
2025-01-08 23:48:15 +0000

    Update engine_api_methods.go

--- turbo/engineapi/engine_api_methods.go
+++ turbo/engineapi/engine_api_methods.go
@@ -140,17 +140,20 @@ func (e *EngineServer) GetClientVersionV1(ctx context.Context, callerVersion *en
 	if callerVersion != nil {
 		e.logger.Info("[GetClientVersionV1] Received request from" + callerVersion.String())
 	}
+	/**
 	commitBytes := [4]byte{}
 	c := []byte(params.GitCommit)
 	if len(c) >= 4 {
 		copy(commitBytes[:], c[0:4])
 	}
+        **/
 	result := make([]engine_types.ClientVersionV1, 1)
 	result[0] = engine_types.ClientVersionV1{
 		Code:    params.ClientCode,
 		Name:    params.ClientName,
 		Version: params.Version,
-		Commit:  commitBytes,
+		//Commit:  commitBytes,
+		Commit:  params.Version,
 	}
 	return result, nil
 }
