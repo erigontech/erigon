20e61f11db97fb4561a78236e084fb3efaf3cc86
Somnath <snb895@outlook.com>
2025-01-11 14:08:39 +0400

    Implement calldata cost increase per EIP-7623 (#13080)
    See https://github.com/ethereum/EIPs/blob/master/EIPS/eip-7623.md
    
    Issue board - https://github.com/erigontech/erigon/issues/12401

--- tests/transaction_test_util.go
+++ tests/transaction_test_util.go
@@ -78,7 +78,10 @@ func (tt *TransactionTest) Run(chainID *big.Int) error {
 		if stx, ok := tx.(*types.SetCodeTransaction); ok {
 			authorizationsLen = uint64(len(stx.GetAuthorizations()))
 		}
-		requiredGas, err := core.IntrinsicGas(msg.Data(), msg.AccessList(), msg.To() == nil, rules.IsHomestead, rules.IsIstanbul, rules.IsShanghai, authorizationsLen)
+		requiredGas, floorGas, err := core.IntrinsicGas(msg.Data(), msg.AccessList(), msg.To() == nil, rules.IsHomestead, rules.IsIstanbul, rules.IsShanghai, rules.IsPrague, authorizationsLen)
+		if rules.IsPrague && floorGas > requiredGas {
+			requiredGas = floorGas
+		}
 		if err != nil {
 			return nil, nil, 0, err
 		}
