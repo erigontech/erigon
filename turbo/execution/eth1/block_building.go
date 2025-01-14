30f945355be9ea432ffd802802b44a6173901361
Somnath <snb895@outlook.com>
2025-01-14 18:14:49 +0400

    Update Empty requests list behaviour for Pectra-5 (#12985)
    The updated EIP-7685 says requests with empty `request_data` should be
    dropped from `executionRequests` field in the API and ignored for hash
    calculation.
    See https://github.com/ethereum/EIPs/pull/8989,
    https://github.com/ethereum/execution-apis/pull/599
    
    Issue board: https://github.com/erigontech/erigon/issues/12401

--- turbo/execution/eth1/block_building.go
+++ turbo/execution/eth1/block_building.go
@@ -199,21 +199,15 @@ func (e *EthereumExecutionModule) GetAssembledBlock(ctx context.Context, req *ex
 		}
 	}
 
-	var requestsBundle types2.RequestsBundle
+	var requestsBundle *types2.RequestsBundle
 	if blockWithReceipts.Requests != nil {
-		requests := make([][]byte, len(types.KnownRequestTypes))
-		if len(blockWithReceipts.Requests) == len(types.KnownRequestTypes) {
-			for i, r := range blockWithReceipts.Requests {
-				requests[i] = make([]byte, 0)
-				requests[i] = append(requests[i], r.RequestData...)
-			}
-		} else {
-			e.logger.Error("Requests len SHOULD BE", "equal to", len(types.KnownRequestTypes), "got", len(blockWithReceipts.Requests))
-			for i := 0; i < len(types.KnownRequestTypes); i++ {
-				requests[i] = make([]byte, 0)
-			}
+		requestsBundle = &types2.RequestsBundle{}
+		requests := make([][]byte, 0)
+		for i, r := range blockWithReceipts.Requests {
+			requests[i] = make([]byte, 0)
+			requests[i] = append(requests[i], r.RequestData...)
 		}
-		requestsBundle = types2.RequestsBundle{Requests: requests}
+		requestsBundle.Requests = requests
 	}
 
 	return &execution.GetAssembledBlockResponse{
@@ -221,7 +215,7 @@ func (e *EthereumExecutionModule) GetAssembledBlock(ctx context.Context, req *ex
 			ExecutionPayload: payload,
 			BlockValue:       gointerfaces.ConvertUint256IntToH256(blockValue),
 			BlobsBundle:      blobsBundle,
-			Requests:         &requestsBundle,
+			Requests:         requestsBundle,
 		},
 		Busy: false,
 	}, nil
