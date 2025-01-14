135af7c71d880309f88c862ebd52c978365ccd21
JkLondon <ilya@mikheev.fun>
2025-01-14 22:39:17 +0300

    commit

--- turbo/jsonrpc/eth_receipts.go
+++ turbo/jsonrpc/eth_receipts.go
@@ -340,7 +340,7 @@ func (api *BaseAPI) getLogsV3(ctx context.Context, tx kv.TemporalTx, begin, end 
 				log.TxHash = txn.Hash()
 			}
 		} else {
-			println("receipt", r.CumulativeGasUsed, r.FirstLogIndexWithinBlock)
+			println("receipt:", "txnum", txNum, "cumGasUsed", r.CumulativeGasUsed, "firstLog", r.FirstLogIndexWithinBlock)
 			filtered = r.Logs.Filter(addrMap, crit.Topics, 0)
 		}
 
