cdf7f4c1591853d10dad82e6be7b779462d15427
awskii <artem.tsskiy@gmail.com>
2025-01-22 14:29:35 +0000

    new inmem db helper

--- core/rawdb/targets.go
+++ core/rawdb/targets.go
@@ -1,3 +1,16 @@
 package rawdb
 
+import (
+	"os"
+
+	"github.com/erigontech/erigon-lib/kv"
+	"github.com/erigontech/erigon-lib/kv/memdb"
+)
+
 var CodePrefix = []byte("c") // CodePrefix + code hash -> account code
+
+func NewMemoryDatabase() kv.RwDB {
+	tmp := os.TempDir()
+
+	return memdb.New(tmp, kv.ChainDB)
+}
