c10870e5c3efd5fbb3ceb5ae03cfedcc4386bca2
sudeepdino008 <sudeepdino008@gmail.com>
2025-01-23 15:29:45 +0530

    save

--- erigon-lib/state/appendables/ap_interfaces.go
+++ erigon-lib/state/appendables/ap_interfaces.go
@@ -1,9 +1,13 @@
 package appendables
 
 import (
+	"bytes"
 	"context"
+	"fmt"
 
 	"github.com/erigontech/erigon-lib/kv"
+	"github.com/erigontech/erigon/core/types"
+	"github.com/ethereum/go-ethereum/common"
 )
 
 // 3 appendable categories:
@@ -51,19 +55,104 @@ type Appendable interface {
 // for which it is not relevant. Plus, sometimes base appendable tsNum should also be managed...
 
 // each appendable kind has a different query pattern
+
+// 1:1
 type PointQueries interface {
 	Get(tsNum TsNum, tx kv.Tx) (VVType, error)
 	Put(tsNum TsNum, value VVType, tx kv.RwTx) error
 }
 
+// many:1
 type RangedQueries interface {
 	Get(tsNum TsNum, tx kv.Tx) (VVType, error)
 	Put(tsNum TsNum, value VVType, tx kv.RwTx) error
-	PutEntityEnd(tsNum TsNum, startBaseTsNum TsNum, tx kv.RwTx) error
+	PutEntityEnd(startBaseTsNum TsNum, tsNum TsNum, tx kv.RwTx) error
 }
 
+// 1:many
+type BindQueries interface {
+	Get(tsNum TsNum, tx kv.Tx) (VVType, error)
+	Put(tsId TsId, value VVType, tx kv.RwTx) error
+	PutMaxTsNum(baseTsNum TsNum, maxTsNum TsNum, tx kv.RwTx) error
+}
+
+// canonicalTbl + valTbl
+// headers, bodies, beaconblocks
 type MarkedQueries interface {
 	Get(tsNum TsNum, tx kv.Tx) (VVType, error)
-	GetNc(tsId TsId, forkId []byte, tx kv.Tx) (VVType, error)
-	Put(tsId TsId, forkId []byte, value VVType, tx kv.RwTx) error
+	GetNc(tsNum TsNum, forkId []byte, tx kv.Tx) (VVType, error)
+	Put(tsNum TsNum, forkId []byte, value VVType, tx kv.RwTx) error
+}
+
+type AppEnum string
+
+const (
+	Headers      AppEnum = "headers"
+	Bodies       AppEnum = "bodies"
+	Transactions AppEnum = "transactions"
+)
+
+type TemporalRwTx interface {
+	kv.RwTx
+	kv.TemporalTx
+	AggTx(baseAppendable AppEnum) AggTx // gets aggtx for entity-set represented by baseAppendable
+}
+
+type AggTx interface {
+	// pick out the right one; else runtime failure
+	// user needs to be anyway aware of what set of queries
+	// he can interact with. So is fine.
+	RangedQueries(app AppEnum) RangedQueries
+	PointQueries(app AppEnum) PointQueries
+	MarkedQueries(app AppEnum) MarkedQueries
+	BindQueries(app AppEnum) BindQueries
+}
+
+func WriteRawBody(db TemporalRwTx, hash common.Hash, number uint64, body *types.RawBody) error {
+	baseTxnID, err := db.IncrementSequence(kv.EthTx, uint64(types.TxCountToTxAmount(len(body.Transactions))))
+	if err != nil {
+		return err
+	}
+
+	data := types.BodyForStorage{
+		BaseTxnID:   types.BaseTxnID(baseTxnID),
+		TxCount:     types.TxCountToTxAmount(len(body.Transactions)), /*system txs*/
+		Uncles:      body.Uncles,
+		Withdrawals: body.Withdrawals,
+	}
+
+	if err = WriteBodyForStorage(db, hash, number, &data); err != nil {
+		return fmt.Errorf("WriteBodyForStorage: %w", err)
+	}
+	if err = WriteRawTransactions(db, body.Transactions, data.BaseTxnID.First()); err != nil {
+		return fmt.Errorf("WriteRawTransactions: %w", err)
+	}
+	return nil
+}
+
+func WriteBodyForStorage(db TemporalRwTx, hash common.Hash, number uint64, body *types.BodyForStorage) error {
+	aggTx := db.AggTx(Headers) // or temporalTx.AggTx(baseAppendableEnum); gives aggtx for entityset
+
+	b := bytes.Buffer{}
+	if err := body.EncodeRLP(&b); err != nil {
+		panic(err)
+	}
+
+	markedQueries := aggTx.MarkedQueries(Bodies)
+
+	// write bodies
+	markedQueries.Put(TsNum(number), hash, b.Bytes(), db)
+	return nil
+}
+
+func WriteRawTransactions(db TemporalRwTx, txs [][]byte, baseTxnID uint64) error {
+	aggTx := db.AggTx(Headers) // or temporalTx.AggTx(baseAppendableEnum); gives aggtx for entityset
+	stx := baseTxnID
+	txq := aggTx.BindQueries(Transactions)
+
+	for _, txn := range txs {
+		txq.Put(TsId(stx), VVType(txn), db)
+		stx++
+	}
+	return nil
 }
