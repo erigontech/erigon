a218ebb39c5c7a31332edd49e64a447b3054378c
Giulio <giulio.rebuffo@gmail.com>
2025-01-24 21:08:30 +0100

    save

--- turbo/engineapi/engine_logs_spammer/engine_log_spammer.go
+++ turbo/engineapi/engine_logs_spammer/engine_log_spammer.go
@@ -40,7 +40,7 @@ func (e *EngineLogsSpammer) Start(ctx context.Context) {
 				return
 			case <-intervalSpam.C:
 				if time.Since(e.lastRequestTime.Load().(time.Time)) > logSpamInterval {
-					e.logger.Warn("flag --externalcl was provided, but no CL seems to be connected.")
+					e.logger.Warn("externalcl is needed, but no CL seems to be connected.")
 				}
 			}
 		}
