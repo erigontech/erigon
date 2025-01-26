74faa8815dcb2e63abc7aec8b64ca7640abe4b08
Shoham Chakraborty <shhmchk@gmail.com>
2025-01-26 19:46:36 +0800

    Fix order

--- turbo/execution/eth1/forkchoice.go
+++ turbo/execution/eth1/forkchoice.go
@@ -519,7 +519,7 @@ func (e *EthereumExecutionModule) updateForkChoice(ctx context.Context, original
 		var m runtime.MemStats
 		dbg.ReadMemStats(&m)
 		blockTimings := e.forkValidator.GetTimings(blockHash)
-		logArgs := []interface{}{"hash", blockHash, "age", "number", fcuHeader.Number.Uint64(), common.PrettyAge(time.Unix(int64(fcuHeader.Time), 0))}
+		logArgs := []interface{}{"hash", blockHash, "number", fcuHeader.Number.Uint64(), "age", common.PrettyAge(time.Unix(int64(fcuHeader.Time), 0))}
 		if flushExtendingFork {
 			totalTime := blockTimings[engine_helpers.BlockTimingsValidationIndex]
 			if !e.syncCfg.ParallelStateFlushing {
