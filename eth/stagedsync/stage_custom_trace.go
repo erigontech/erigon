85a1437772d5433d39dbac5a1c555f05d8f4a57e
alex.sharov <AskAlexSharov@gmail.com>
2025-01-31 17:41:36 +0700

    save

--- eth/stagedsync/stage_custom_trace.go
+++ eth/stagedsync/stage_custom_trace.go
@@ -142,7 +142,7 @@ func customTraceBatchProduce(ctx context.Context, cfg *exec3.ExecArgs, db kv.RwD
 		if toBlock > 0 {
 			toBlock-- // [fromBlock,toBlock)
 		}
-		lastTxNumInDB := tx.(kv.TemporalTx).(state2.HasAggTx).AggTx().(*state2.AggregatorRoTx).DbgDomain(kv.ReceiptDomain).DbgMaxTxNumInDB(tx)
+		toTxNum := tx.(kv.TemporalTx).(state2.HasAggTx).AggTx().(*state2.AggregatorRoTx).DbgDomain(kv.ReceiptDomain).DbgMaxTxNumInDB(tx)
 		//toTxNum, err := txNumsReader.Max(tx, toBlock)
 		//if err != nil {
 		//	return err
