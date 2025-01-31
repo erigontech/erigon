8bf06952bb95ad827806a5907a03fcdb6db8533f
alex.sharov <AskAlexSharov@gmail.com>
2025-01-31 21:26:56 +0700

    save

--- cmd/state/exec3/historical_trace_worker.go
+++ cmd/state/exec3/historical_trace_worker.go
@@ -297,7 +297,7 @@ func doHistoryReduce(consumer TraceConsumer, db kv.TemporalRoDB, ctx context.Con
 	}
 	log.Warn("[dbg] reduceDone", "outputTxNum", outputTxNum.Load(), "toTxNum", toTxNum)
 	if outputTxNum.Load() != toTxNum {
-		return fmt.Errorf("but not all txnums proceeded: toTxNum=%d, outputTxNum=%d", toTxNum, outputTxNum)
+		return fmt.Errorf("not all txnums proceeded: toTxNum=%d, outputTxNum=%d", toTxNum, outputTxNum)
 	}
 	return nil
 }
