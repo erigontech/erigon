3c5fccb6ca0c0be0361b106fbb47bbcae97bd8ad
alex.sharov <AskAlexSharov@gmail.com>
2025-01-31 16:57:45 +0700

    save

--- eth/stagedsync/stage_custom_trace.go
+++ eth/stagedsync/stage_custom_trace.go
@@ -252,22 +252,22 @@ func customTraceBatch(ctx context.Context, cfg *exec3.ExecArgs, tx kv.TemporalRw
 
 			doms.SetTx(tx)
 			doms.SetTxNum(txTask.TxNum)
-			if txTask.TxNum < 50 {
-				fmt.Printf("[dbg.exec] rec: %d, %d, %t\n", txTask.BlockNum, txTask.TxNum, txTask.Final)
-			}
+			//if txTask.TxNum < 50 {
+			//fmt.Printf("[dbg.exec] rec: %d, %d, %t\n", txTask.BlockNum, txTask.TxNum, txTask.Final)
+			//}
 
 			if !txTask.Final {
 				var receipt *types.Receipt
 				if txTask.TxIndex >= 0 && !txTask.Final {
 					receipt = txTask.BlockReceipts[txTask.TxIndex]
 				}
-				if txTask.TxNum < 50 {
-					if receipt != nil {
-						fmt.Printf("[dbg.exec] append: %d, %d, %d\n", txTask.BlockNum, txTask.TxNum, receipt.CumulativeGasUsed)
-					} else {
-						fmt.Printf("[dbg.exec] append: %d, %d, 0\n", txTask.BlockNum, txTask.TxNum)
-					}
-				}
+				//if txTask.TxNum < 50 {
+				//	if receipt != nil {
+				//		fmt.Printf("[dbg.exec] append: %d, %d, %d\n", txTask.BlockNum, txTask.TxNum, receipt.CumulativeGasUsed)
+				//	} else {
+				//		fmt.Printf("[dbg.exec] append: %d, %d, 0\n", txTask.BlockNum, txTask.TxNum)
+				//	}
+				//}
 				if err := rawtemporaldb.AppendReceipt(doms, receipt, cumulativeBlobGasUsedInBlock, txTask.TxNum); err != nil {
 					return err
 				}
