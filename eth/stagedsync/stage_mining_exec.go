7852e6f65be38a0b616352bf2e288a8a55a6576b
Giulio <giulio.rebuffo@gmail.com>
2025-02-06 18:00:38 +0100

    save

--- eth/stagedsync/stage_mining_exec.go
+++ eth/stagedsync/stage_mining_exec.go
@@ -477,6 +477,7 @@ func addTransactionsToMiningBlock(
 	noop := state.NewNoopWriter()
 
 	var miningCommitTx = func(txn types.Transaction, coinbase libcommon.Address, vmConfig *vm.Config, chainConfig chain.Config, ibs *state.IntraBlockState, current *MiningBlock) ([]*types.Log, error) {
+		ibs.Reset()
 		ibs.SetTxContext(txnIdx)
 		gasSnap := gasPool.Gas()
 		blobGasSnap := gasPool.BlobGas()
