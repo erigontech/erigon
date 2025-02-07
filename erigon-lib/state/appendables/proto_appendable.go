bd630b9ca5d6668da4ac9f8d9ebe65531ea6e0fb
sudeepdino008 <sudeepdino008@gmail.com>
2025-02-07 10:48:55 +0530

    empty files

--- erigon-lib/state/appendables/proto_appendable.go
+++
@@ -1,9 +0,0 @@
-package appendables
-
-import "github.com/tidwall/btree"
-
-type ProtoAppendable struct {
-	freezer freezer
-
-	dirtyFiles *btree.BTreeG[*dirtyFiles]
-}
--- erigon-lib/state/appendables/relational_appendable.go
+++
@@ -1,8 +0,0 @@
-package appendables
-
-import "github.com/erigontech/erigon-lib/kv"
-
-type RelationI interface {
-	RootNum2Id(from RootNum, tx kv.Tx) (Id, error)
-	Num2Id(from Num, tx kv.Tx) (Id, error)
-}
