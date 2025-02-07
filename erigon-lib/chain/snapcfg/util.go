182bdc015e01f348d8b5c6f0a923f13eee14c3a6
Giulio <giulio.rebuffo@gmail.com>
2025-02-07 15:41:14 +0100

    save

--- erigon-lib/chain/snapcfg/util.go
+++ erigon-lib/chain/snapcfg/util.go
@@ -20,7 +20,6 @@ import (
 	"context"
 	_ "embed"
 	"encoding/json"
-	"fmt"
 	"path/filepath"
 	"slices"
 	"sort"
@@ -402,10 +401,6 @@ func (c Cfg) Seedable(info snaptype.FileInfo) bool {
 
 // IsFrozen - can't be merged to bigger files
 func (c Cfg) IsFrozen(info snaptype.FileInfo) bool {
-	fmt.Println(info.TypeString)
-	if strings.Contains(info.Name(), "caplin") {
-		return true
-	}
 	mergeLimit := c.MergeLimit(info.Type.Enum(), info.From)
 	return info.To-info.From == mergeLimit
 }
