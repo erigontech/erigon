13bc320f2979d9f39fa52d48399288de998b5064
JkLondon <ilya@mikheev.fun>
2025-02-19 19:11:51 +0100

    commit

--- core/gdbme/gdbme.go
+++ core/gdbme/gdbme.go
@@ -15,14 +15,12 @@ import (
 func RestartWithGDB(argc int, argv []string) {
 	fmt.Println("Restarting with gdb...")
 
-	// Convert Go args to C
 	cArgv := make([]*C.char, len(argv))
 	for i, arg := range argv {
 		cArgv[i] = C.CString(arg)
 		defer C.free(unsafe.Pointer(cArgv[i]))
 	}
 
-	// Ensure C API gets the correct argument list
 	var cArgvPtr **C.char
 	if len(cArgv) > 0 {
 		cArgvPtr = &cArgv[0]
