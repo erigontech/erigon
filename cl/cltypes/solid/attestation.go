bcf900db16bd026751104e8623631c9c0536f597
kewei <kewei.train@gmail.com>
2025-02-19 14:32:30 +0800

    update

--- cl/cltypes/solid/attestation.go
+++ cl/cltypes/solid/attestation.go
@@ -173,8 +173,8 @@ func (a *Attestation) UnmarshalJSON(data []byte) error {
 //	data: AttestationData
 //	signature: BLSSignature
 type SingleAttestation struct {
-	CommitteeIndex uint64            `json:"committee_index"`
-	AttesterIndex  uint64            `json:"attester_index"`
+	CommitteeIndex uint64            `json:"committee_index,string"`
+	AttesterIndex  uint64            `json:"attester_index,string"`
 	Data           *AttestationData  `json:"data"`
 	Signature      libcommon.Bytes96 `json:"signature"`
 }
