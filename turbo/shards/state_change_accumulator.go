4d73ef5fc2a75ecd0ff1299376d015379aab7d57
milen <94537774+taratorio@users.noreply.github.com>
2025-02-19 08:55:07 +0000

    shards: add block time to state changes (#13860)
    add `BlockTime` info to `StateChange` message
    this is needed in the Shutter pool to be able to calculate the slot
    number associated with the given block notification
    
    used in https://github.com/erigontech/erigon/pull/13864
    relates to https://github.com/erigontech/erigon/issues/13386

--- turbo/shards/state_change_accumulator.go
+++ turbo/shards/state_change_accumulator.go
@@ -22,6 +22,7 @@ import (
 	libcommon "github.com/erigontech/erigon-lib/common"
 	"github.com/erigontech/erigon-lib/gointerfaces"
 	remote "github.com/erigontech/erigon-lib/gointerfaces/remoteproto"
+	"github.com/erigontech/erigon/core/types"
 )
 
 // Accumulator collects state changes in a form that can then be delivered to the RPC daemon
@@ -63,11 +64,12 @@ func (a *Accumulator) SetStateID(stateID uint64) {
 }
 
 // StartChange begins accumulation of changes for a new block
-func (a *Accumulator) StartChange(blockHeight uint64, blockHash libcommon.Hash, txs [][]byte, unwind bool) {
+func (a *Accumulator) StartChange(h *types.Header, txs [][]byte, unwind bool) {
 	a.changes = append(a.changes, &remote.StateChange{})
 	a.latestChange = a.changes[len(a.changes)-1]
-	a.latestChange.BlockHeight = blockHeight
-	a.latestChange.BlockHash = gointerfaces.ConvertHashToH256(blockHash)
+	a.latestChange.BlockHeight = h.Number.Uint64()
+	a.latestChange.BlockHash = gointerfaces.ConvertHashToH256(h.Hash())
+	a.latestChange.BlockTime = h.Time
 	if unwind {
 		a.latestChange.Direction = remote.Direction_UNWIND
 	} else {
