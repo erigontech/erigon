1a1b5bf4c024f9561799b4814f2db90f61dcf04b
alex.sharov <AskAlexSharov@gmail.com>
2025-02-21 17:47:45 +0700

    lint

--- turbo/jsonrpc/eth_receipts.go
+++ turbo/jsonrpc/eth_receipts.go
@@ -286,7 +286,8 @@ func (api *BaseAPI) getLogsV3(ctx context.Context, tx kv.TemporalTx, begin, end 
 		return logs, err
 	}
 
-	fmt.Printf("[dbg] dbg10: it1.count=%d\n", stream.Count(txNumbers))
+	cnt, _ := stream.Count(txNumbers)
+	fmt.Printf("[dbg] dbg10: it1.count=%d\n", cnt)
 	it := rawdbv3.TxNums2BlockNums(tx,
 		txNumsReader,
 		txNumbers, order.Asc)
