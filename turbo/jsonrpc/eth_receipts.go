835101da3733527c7d0563855cb1b2d814cb2db8
Shoham Chakraborty <shhmchk@gmail.com>
2025-02-24 17:18:18 +0800

    Add check

--- turbo/jsonrpc/eth_receipts.go
+++ turbo/jsonrpc/eth_receipts.go
@@ -296,31 +296,33 @@ func (api *BaseAPI) getLogsV3(ctx context.Context, tx kv.TemporalTx, begin, end 
 			return nil, err
 		}
 		if isFinalTxn {
-			// check for state sync event logs
-			events, err := api.stateSyncEvents(ctx, tx, header.Hash(), blockNum, chainConfig)
-			if err != nil {
-				return logs, err
-			}
+			if chainConfig.Bor != nil {
+				// check for state sync event logs
+				events, err := api.stateSyncEvents(ctx, tx, header.Hash(), blockNum, chainConfig)
+				if err != nil {
+					return logs, err
+				}
 
-			borLogs, err := api.borReceiptGenerator.GenerateBorLogs(ctx, events, txNumsReader, tx, header, chainConfig, txIndex, len(logs))
-			if err != nil {
-				return logs, err
-			}
+				borLogs, err := api.borReceiptGenerator.GenerateBorLogs(ctx, events, txNumsReader, tx, header, chainConfig, txIndex, len(logs))
+				if err != nil {
+					return logs, err
+				}
 
-			borLogs = borLogs.Filter(addrMap, crit.Topics, 0)
-			for _, filteredLog := range borLogs {
-				logs = append(logs, &types.ErigonLog{
-					Address:     filteredLog.Address,
-					Topics:      filteredLog.Topics,
-					Data:        filteredLog.Data,
-					BlockNumber: filteredLog.BlockNumber,
-					TxHash:      filteredLog.TxHash,
-					TxIndex:     filteredLog.TxIndex,
-					BlockHash:   filteredLog.BlockHash,
-					Index:       filteredLog.Index,
-					Removed:     filteredLog.Removed,
-					Timestamp:   header.Time,
-				})
+				borLogs = borLogs.Filter(addrMap, crit.Topics, 0)
+				for _, filteredLog := range borLogs {
+					logs = append(logs, &types.ErigonLog{
+						Address:     filteredLog.Address,
+						Topics:      filteredLog.Topics,
+						Data:        filteredLog.Data,
+						BlockNumber: filteredLog.BlockNumber,
+						TxHash:      filteredLog.TxHash,
+						TxIndex:     filteredLog.TxIndex,
+						BlockHash:   filteredLog.BlockHash,
+						Index:       filteredLog.Index,
+						Removed:     filteredLog.Removed,
+						Timestamp:   header.Time,
+					})
+				}
 			}
 
 			continue
