60a1c11d1445ee771f35358324a221ddd642cb24
Giulio <giulio.rebuffo@gmail.com>
2025-02-24 13:51:19 +0100

    save

--- cl/validator/sync_contribution_pool/sync_contribution_pool.go
+++ cl/validator/sync_contribution_pool/sync_contribution_pool.go
@@ -21,13 +21,13 @@ import (
 	"errors"
 	"sync"
 
-	"github.com/Giulio2002/bls"
 	"github.com/erigontech/erigon-lib/common"
 	"github.com/erigontech/erigon/cl/clparams"
 	"github.com/erigontech/erigon/cl/cltypes"
 	"github.com/erigontech/erigon/cl/cltypes/solid"
 	"github.com/erigontech/erigon/cl/phase1/core/state"
 	"github.com/erigontech/erigon/cl/utils"
+	"github.com/erigontech/erigon/cl/utils/bls"
 )
 
 type syncContributionKey struct {
