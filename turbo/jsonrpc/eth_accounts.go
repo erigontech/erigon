60355855bd3914730e84541c8be0f17f021e4fb9
Giulio <giulio.rebuffo@gmail.com>
2025-02-24 19:20:38 +0100

    save

--- turbo/jsonrpc/eth_accounts.go
+++ turbo/jsonrpc/eth_accounts.go
@@ -121,7 +121,6 @@ func (api *APIImpl) GetCode(ctx context.Context, address libcommon.Address, bloc
 // GetStorageAt implements eth_getStorageAt. Returns the value from a storage position at a given address.
 func (api *APIImpl) GetStorageAt(ctx context.Context, address libcommon.Address, index string, blockNrOrHash rpc.BlockNumberOrHash) (string, error) {
 	var empty []byte
-
 	indexBytes := hexutility.FromHex(index)
 	if len(indexBytes) > 32 {
 		return "", errors.New("unable to decode storage key: hex string too long, want at most 32 bytes")
