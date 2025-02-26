c16585036578ae79eb9147ba7a7b6943ec8f7e15
taratorio <94537774+taratorio@users.noreply.github.com>
2025-02-26 21:40:50 +0200

    simplify cond mutexes

--- txnprovider/shutter/decrypted_txns_pool.go
+++ txnprovider/shutter/decrypted_txns_pool.go
@@ -35,15 +35,13 @@ type TxnBatch struct {
 }
 
 type DecryptedTxnsPool struct {
-	mu             *sync.Mutex
 	decryptedTxns  map[DecryptionMark]TxnBatch
 	decryptionCond *sync.Cond
 }
 
 func NewDecryptedTxnsPool() *DecryptedTxnsPool {
-	mu := sync.Mutex{}
+	var mu sync.Mutex
 	return &DecryptedTxnsPool{
-		mu:             &mu,
 		decryptedTxns:  make(map[DecryptionMark]TxnBatch),
 		decryptionCond: sync.NewCond(&mu),
 	}
@@ -54,8 +52,8 @@ func (p *DecryptedTxnsPool) Wait(ctx context.Context, mark DecryptionMark) error
 	go func() {
 		defer close(done)
 
-		p.mu.Lock()
-		defer p.mu.Unlock()
+		p.decryptionCond.L.Lock()
+		defer p.decryptionCond.L.Unlock()
 
 		for _, ok := p.decryptedTxns[mark]; !ok && ctx.Err() == nil; _, ok = p.decryptedTxns[mark] {
 			p.decryptionCond.Wait()
@@ -75,15 +73,15 @@ func (p *DecryptedTxnsPool) Wait(ctx context.Context, mark DecryptionMark) error
 }
 
 func (p *DecryptedTxnsPool) DecryptedTxns(mark DecryptionMark) (TxnBatch, bool) {
-	p.mu.Lock()
-	defer p.mu.Unlock()
+	p.decryptionCond.L.Lock()
+	defer p.decryptionCond.L.Unlock()
 	txnBatch, ok := p.decryptedTxns[mark]
 	return txnBatch, ok
 }
 
 func (p *DecryptedTxnsPool) AddDecryptedTxns(mark DecryptionMark, txnBatch TxnBatch) {
-	p.mu.Lock()
-	defer p.mu.Unlock()
+	p.decryptionCond.L.Lock()
+	defer p.decryptionCond.L.Unlock()
 	p.decryptedTxns[mark] = txnBatch
 	p.decryptionCond.Broadcast()
 	txnsLen := float64(len(txnBatch.Transactions))
@@ -93,8 +91,8 @@ func (p *DecryptedTxnsPool) AddDecryptedTxns(mark DecryptionMark, txnBatch TxnBa
 }
 
 func (p *DecryptedTxnsPool) DeleteDecryptedTxnsUpToSlot(slot uint64) (markDeletions, txnDeletions uint64) {
-	p.mu.Lock()
-	defer p.mu.Unlock()
+	p.decryptionCond.L.Lock()
+	defer p.decryptionCond.L.Unlock()
 
 	var totalBytes int64
 	for mark, txnBatch := range p.decryptedTxns {
