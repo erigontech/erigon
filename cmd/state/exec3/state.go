6161afa987d6a4dbfaf9c75fbddecb38cc24be0f
awskii <artem.tsskiy@gmail.com>
2025-02-27 18:20:21 +0000

    init arbos in worker

--- cmd/state/exec3/state.go
+++ cmd/state/exec3/state.go
@@ -25,6 +25,9 @@ import (
 
 	"github.com/erigontech/erigon-lib/common/dbg"
 	"github.com/erigontech/nitro-erigon/arbos"
+	"github.com/erigontech/nitro-erigon/arbos/arbosState"
+	"github.com/erigontech/nitro-erigon/arbos/arbostypes"
+	"github.com/erigontech/nitro-erigon/statetransfer"
 
 	"github.com/erigontech/erigon-lib/log/v3"
 
@@ -200,7 +203,28 @@ func (rw *Worker) RunTxTaskNoLock(txTask *state.TxTask, isMining bool) {
 	txTask.Error = nil
 	if rw.chainConfig.IsArbitrum() {
 		// core.ReadyEVMForL2(rw.evm, &txTask.TxAsMessage)
-		rw.evm.IntraBlockState()
+		// rw.evm.IntraBlockState()
+		ibsa := state.NewArbitrum(rw.ibs)
+		accountsPerSync := uint(100000)
+		initMessage, err := arbostypes.GetSepoliaRollupInitMessage()
+		if err != nil {
+			rw.logger.Error("Failed to get Sepolia Rollup init message", "err", err)
+			return
+		}
+
+		initData := statetransfer.ArbosInitializationInfo{
+			NextBlockNumber: 0,
+		}
+		initReader := statetransfer.NewMemoryInitDataReader(&initData)
+
+		stateRoot, err := arbosState.InitializeArbosInDatabase(ibsa, rw.rs.Domains(), initReader, rw.chainConfig, initMessage, rw.evm.Context.Time, accountsPerSync)
+		if err != nil {
+			rw.logger.Error("Failed to init ArbOS", "err", err)
+			return
+		}
+		_ = stateRoot
+		rw.logger.Info("ArbOS initialized", "stateRoot", stateRoot)
+
 		rw.evm.ProcessingHook = arbos.NewTxProcessorIBS(rw.evm, rw.ibs, &txTask.TxAsMessage)
 	}
 
