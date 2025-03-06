f0c731c44190a9bd995b28c825e278b722c49b78
Giulio <giulio.rebuffo@gmail.com>
2025-03-06 20:22:28 +0100

    save

--- consensus/merge/merge.go
+++ consensus/merge/merge.go
@@ -200,7 +200,7 @@ func (s *Merge) Finalize(config *chain.Config, header *types.Header, state *stat
 			// evaluation must be skipped to prevent accessing logs from an invalid receipt.
 			if rec == nil {
 				skipEvaluation = true
-				logger.Info("nil receipt found in block receipts, skipping evaluation")
+				logger.Info("nil receipt found in block receipts, skipping evaluation of receiptsHash")
 				break
 			}
 			allLogs = append(allLogs, rec.Logs...)
