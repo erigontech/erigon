22d38dd0703f752fc6169beeddcb5d4ef9ec232a
Giulio <giulio.rebuffo@gmail.com>
2025-03-12 18:32:18 +0100

    save

--- cl/antiquary/state_antiquary.go
+++ cl/antiquary/state_antiquary.go
@@ -391,9 +391,9 @@ func (s *Antiquary) IncrementBeaconState(ctx context.Context, to uint64) error {
 		if err := transition.TransitionState(s.currentState, block, blockRewardsCollector, fullValidation); err != nil {
 			return err
 		}
-		// if s.currentState.Slot() == 3000010 {
-		// 	s.dumpFullBeaconState()
-		// }
+		if s.currentState.Slot() == 3751254 {
+			s.dumpFullBeaconState()
+		}
 		blocksProcessed++
 
 		first = false
