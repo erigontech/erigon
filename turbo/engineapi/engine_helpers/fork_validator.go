b8802850b93e8dc500d1270d161780998c5fe957
milen <94537774+taratorio@users.noreply.github.com>
2025-03-18 13:28:31 +0000

    execution: fix missing log notifications when flushing extending fork (#14192)
    found in https://github.com/erigontech/erigon/pull/13983
    
    we weren't getting any new log notifications (rpc log subscriptions api)
    when the EL was going via the `e.forkValidator.FlushExtendingFork` code
    path which does an in-memory flush of the previously cached execution of
    `EthereumExecutionModule.ValidateChain`

--- turbo/engineapi/engine_helpers/fork_validator.go
+++ turbo/engineapi/engine_helpers/fork_validator.go
@@ -143,7 +143,7 @@ func (fv *ForkValidator) NotifyCurrentHeight(currentHeight uint64) {
 }
 
 // FlushExtendingFork flush the current extending fork if fcu chooses its head hash as the its forkchoice.
-func (fv *ForkValidator) FlushExtendingFork(tx kv.RwTx, accumulator *shards.Accumulator) error {
+func (fv *ForkValidator) FlushExtendingFork(tx kv.RwTx, accumulator *shards.Accumulator, recentLogs *shards.RecentLogs) error {
 	fv.lock.Lock()
 	defer fv.lock.Unlock()
 	start := time.Now()
@@ -162,6 +162,7 @@ func (fv *ForkValidator) FlushExtendingFork(tx kv.RwTx, accumulator *shards.Accu
 	timings[BlockTimingsFlushExtendingFork] = time.Since(start)
 	fv.timingsCache.Add(fv.extendingForkHeadHash, timings)
 	fv.extendingForkNotifications.Accumulator.CopyAndReset(accumulator)
+	fv.extendingForkNotifications.RecentLogs.CopyAndReset(recentLogs)
 	// Clean extending fork data
 	fv.sharedDom = nil
 
