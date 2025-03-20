8421001e72a73dbe8c198a605f14af166706d707
Dmytro Vovk <vovk.dimon@gmail.com>
2025-03-20 17:52:50 +0000

    save

--- erigon-lib/diagnostics/client.go
+++ erigon-lib/diagnostics/client.go
@@ -59,6 +59,8 @@ type DiagnosticClient struct {
 	conn                *websocket.Conn
 
 	notificationMessages []DiagMessages
+	removes              uint64
+	adds                 uint64
 }
 
 func NewDiagnosticClient(ctx context.Context, metricsMux *http.ServeMux, dataDirPath string, speedTest bool, webseedsList []string) (*DiagnosticClient, error) {
@@ -91,6 +93,8 @@ func NewDiagnosticClient(ctx context.Context, metricsMux *http.ServeMux, dataDir
 		peersStats:           NewPeerStats(1000), // 1000 is the limit of peers; TODO: make it configurable through a flag
 		webseedsList:         webseedsList,
 		notificationMessages: []DiagMessages{},
+		removes:              0,
+		adds:                 0,
 	}, nil
 }
 
