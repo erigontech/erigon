bc08ad6bb1843955bdf630f502f3323f83479f43
Shoham Chakraborty <shhmchk@gmail.com>
2025-03-25 12:54:58 +0800

    Fix failing test

--- core/vm/runtime/runtime.go
+++ core/vm/runtime/runtime.go
@@ -39,8 +39,8 @@ import (
 	"github.com/erigontech/erigon-lib/kv/temporal"
 	"github.com/erigontech/erigon-lib/log/v3"
 	state3 "github.com/erigontech/erigon-lib/state"
-
 	"github.com/erigontech/erigon/core/state"
+	"github.com/erigontech/erigon/core/tracing"
 	"github.com/erigontech/erigon/core/vm"
 )
 
@@ -166,6 +166,9 @@ func Execute(code, input []byte, cfg *Config, tempdir string) ([]byte, *state.In
 	// set the receiver's (the executing contract) code for execution.
 	cfg.State.SetCode(address, code)
 	// Call the code with the given configuration.
+	if cfg.EVMConfig.Tracer != nil && cfg.EVMConfig.Tracer.OnTxStart != nil {
+		cfg.EVMConfig.Tracer.OnTxStart(&tracing.VMContext{IntraBlockState: cfg.State}, nil, libcommon.Address{})
+	}
 	ret, _, err := vmenv.Call(
 		sender,
 		libcommon.BytesToAddress([]byte("contract")),
@@ -174,6 +177,9 @@ func Execute(code, input []byte, cfg *Config, tempdir string) ([]byte, *state.In
 		cfg.Value,
 		false, /* bailout */
 	)
+	if cfg.EVMConfig.Tracer != nil && cfg.EVMConfig.Tracer.OnTxEnd != nil {
+		cfg.EVMConfig.Tracer.OnTxEnd(nil, err)
+	}
 
 	return ret, cfg.State, err
 }
