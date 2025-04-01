bd04f4c09d7f383f26b491651a2b27bb279ccb2e
sudeepdino008 <sudeepdino008@gmail.com>
2025-04-01 12:36:09 +0530

    wip: files registry

---
+++ erigon-lib/state/files_registry.go
@@ -0,0 +1,71 @@
+package state
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/elastic/go-freelru"
+	"github.com/erigontech/erigon-lib/log/v3"
+	btree2 "github.com/tidwall/btree"
+)
+
+// manages dirtyfiles and visible files,
+// providing safe access
+// and initiation inside a folder
+// configuration for files creation/merging
+type FilesRegistry struct {
+	dirtyFiles *btree2.BTreeG[*filesItem]
+	_visible   visibleFiles
+	name       string
+	dir        string
+}
+
+// files seek cache
+
+type FilesSeekCache[V any] struct {
+	*freelru.LRU[uint64, V]
+
+	hit, total, limit int
+	enabled, trace    bool
+}
+
+func NewFilesSeekCache[V any](limit uint32, enabled bool, trace bool,
+	hash freelru.HashKeyCallback[uint64]) *FilesSeekCache[V] {
+	c, err := freelru.New[uint64, V](limit, hash)
+	if err != nil {
+		panic(err)
+	}
+	return &FilesSeekCache[V]{LRU: c, enabled: enabled, trace: trace}
+}
+
+// caching files registry
+
+type CachingFilesRegistry[V any] struct {
+	*FilesRegistry
+	enabled bool
+	caches  *sync.Pool
+}
+
+func NewCachingFilesRegistry[V any](dir string, limit uint32, enabled bool, trace bool,
+	hash freelru.HashKeyCallback[uint64]) *CachingFilesRegistry[V] {
+	f := &CachingFilesRegistry[V]{
+		FilesRegistry: &FilesRegistry{
+			dirtyFiles: btree2.NewBTreeGOptions(filesItemLess, btree2.Options{Degree: 128, NoLocks: false}),
+			dir:        dir,
+		},
+		enabled: enabled,
+		caches:  &sync.Pool{New: func() any { return NewFilesSeekCache[V](limit, enabled, trace, hash) }},
+	}
+	//f.files = f.newVisibleFiles()
+	return f
+}
+
+func (c *FilesSeekCache[V]) LogStats(fileBaseName string) {
+	if c == nil || !c.trace {
+		return
+	}
+	m := c.Metrics()
+	log.Warn("[dbg] FilesSeekCache", "a", fileBaseName, "ratio",
+		fmt.Sprintf("%.2f", float64(c.hit)/float64(c.total)), "hit", c.hit, "collisions", m.Collisions,
+		"evictions", m.Evictions, "inserts", m.Inserts, "removals", m.Removals, "limit", c.limit)
+}
