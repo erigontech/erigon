0ced0d5106e57edd79437b0cdf884a90865de762
Dmytro Vovk <vovk.dimon@gmail.com>
2025-04-01 23:08:48 +0100

    save

--- erigon-lib/diagnostics/client.go
+++ erigon-lib/diagnostics/client.go
@@ -101,9 +101,10 @@ func NewDiagnosticClient(ctx context.Context, metricsMux *http.ServeMux, dataDir
 			resourcesUsage: ResourcesUsage{
 				MemoryUsage: []MemoryStats{},
 			},
-			peersStats:   NewPeerStats(1000), // 1000 is the limit of peers; TODO: make it configurable through a flag
-			webseedsList: webseedsList,
+			peersStats:           NewPeerStats(1000), // 1000 is the limit of peers; TODO: make it configurable through a flag
+			webseedsList:         webseedsList,
 			notificationMessages: []DiagMessages{},
+		}
 	})
 
 	return instance, nil
