291c6110f416d0a6d3333ab52aa755a2fd3eacf6
Andrew Ashikhmin <34320705+yperbasis@users.noreply.github.com>
2025-04-01 11:31:49 +0200

    Engine API: log requests on block hash mismatch (#14379)
    Cherry pick PR #14338 into `release/3.0`

--- turbo/engineapi/engine_server.go
+++ turbo/engineapi/engine_server.go
@@ -262,7 +262,8 @@ func (s *EngineServer) newPayload(ctx context.Context, req *engine_types.Executi
 
 	blockHash := req.BlockHash
 	if header.Hash() != blockHash {
-		s.logger.Error("[NewPayload] invalid block hash", "stated", blockHash, "actual", header.Hash())
+		s.logger.Error("[NewPayload] invalid block hash", "stated", blockHash, "actual", header.Hash(),
+			"payload", req, "parentBeaconBlockRoot", parentBeaconBlockRoot, "requests", executionRequests)
 		return &engine_types.PayloadStatus{
 			Status:          engine_types.InvalidStatus,
 			ValidationError: engine_types.NewStringifiedErrorFromString("invalid block hash"),
