749efae883f83d6ba00a972460a70b8a3f5cfdbe
sudeepdino008 <sudeepdino008@gmail.com>
2025-04-02 09:25:38 +0530

    save

--- erigon-lib/state/files_item.go
+++ erigon-lib/state/files_item.go
@@ -31,7 +31,6 @@ import (
 	"github.com/erigontech/erigon-lib/log/v3"
 	"github.com/erigontech/erigon-lib/recsplit"
 	"github.com/erigontech/erigon-lib/seg"
-	ae "github.com/erigontech/erigon-lib/state/appendable_extras"
 )
 
 // filesItem is "dirty" file - means file which can be:
@@ -74,10 +73,6 @@ var _ FilesItem = (*filesItem)(nil)
 
 func newFilesItem(startTxNum, endTxNum, stepSize uint64) *filesItem {
 	return newFilesItemWithFrozenSteps(startTxNum, endTxNum, stepSize, config3.StepsInFrozenFile)
-}
-
-func newFilesItemWithSnapConfig(startTxNum, endTxNum uint64, snapConfig *ae.SnapshotConfig) *filesItem {
-	return newFilesItemWithFrozenSteps(startTxNum, endTxNum, snapConfig.RootNumPerStep, snapConfig.StepsInFrozenFile())
 }
 
 func newFilesItemWithFrozenSteps(startTxNum, endTxNum, stepSize uint64, stepsInFrozenFile uint64) *filesItem {
