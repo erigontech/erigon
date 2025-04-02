6fb6bbd710d02003fdd6036802a9cd49cb91b567
Giulio <giulio.rebuffo@gmail.com>
2025-04-02 16:37:55 +0200

    save

--- erigon-lib/chain/chain_config.go
+++ erigon-lib/chain/chain_config.go
@@ -63,6 +63,7 @@ type Config struct {
 	TerminalTotalDifficulty       *big.Int `json:"terminalTotalDifficulty,omitempty"`       // The merge happens when terminal total difficulty is reached
 	TerminalTotalDifficultyPassed bool     `json:"terminalTotalDifficultyPassed,omitempty"` // Disable PoW sync for networks that have already passed through the Merge
 	MergeNetsplitBlock            *big.Int `json:"mergeNetsplitBlock,omitempty"`            // Virtual fork after The Merge to use as a network splitter; see FORK_NEXT_VALUE in EIP-3675
+	MergeBlock                    *big.Int `json:"mergeBlock,omitempty"`                    // The Merge block number
 
 	// Mainnet fork scheduling switched from block numbers to timestamps after The Merge
 	ShanghaiTime *big.Int `json:"shanghaiTime,omitempty"`
@@ -630,5 +631,5 @@ func isForked(s *big.Int, head uint64) bool {
 }
 
 func (c *Config) IsPreMerge(blockNumber uint64) bool {
-	return c.MergeNetsplitBlock != nil && blockNumber < c.MergeNetsplitBlock.Uint64()
+	return c.MergeBlock != nil && blockNumber < c.MergeBlock.Uint64()
 }
