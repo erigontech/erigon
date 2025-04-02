a13bac5543921605fb44eec21cf782298c276319
alex.sharov <AskAlexSharov@gmail.com>
2025-04-02 16:04:12 +0700

    save

--- turbo/jsonrpc/receipts/receipts_generator.go
+++ turbo/jsonrpc/receipts/receipts_generator.go
@@ -13,6 +13,7 @@ import (
 	"github.com/erigontech/erigon-lib/log/v3"
 	"github.com/erigontech/erigon/consensus"
 	"github.com/erigontech/erigon/core"
+	"github.com/erigontech/erigon/core/rawdb"
 	"github.com/erigontech/erigon/core/rawdb/rawtemporaldb"
 	"github.com/erigontech/erigon/core/state"
 	"github.com/erigontech/erigon/core/types"
@@ -179,8 +180,15 @@ func (g *Generator) GetReceipt(ctx context.Context, cfg *chain.Config, tx kv.Tem
 
 func (g *Generator) GetReceipts(ctx context.Context, cfg *chain.Config, tx kv.TemporalTx, block *types.Block) (types.Receipts, error) {
 	blockHash := block.Hash()
-	mu := g.blockExecMutex.lock(blockHash)
+	mu := g.blockExecMutex.lock(blockHash) // parallel requests of same blockNum will executed only once
 	defer g.blockExecMutex.unlock(mu, blockHash)
+
+	//if can find in DB - then don't need store in `receiptsCache` - because DB it's already kind-of cache (small, mmaped, hot file)
+	if receipts, err := rawdb.ReadReceipts(tx, blockHash, block.NumberU64()); err != nil {
+		return nil, err
+	} else if len(receipts) > 0 {
+		return receipts, nil
+	}
 
 	if receipts, ok := g.receiptsCache.Get(blockHash); ok {
 		return receipts, nil
