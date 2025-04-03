3b64d6d6bb13e7342ecd13776c2312010eb870bd
alex.sharov <AskAlexSharov@gmail.com>
2025-04-03 10:45:31 +0700

    save

--- eth/ethutils/receipt.go
+++ eth/ethutils/receipt.go
@@ -21,11 +21,10 @@ import (
 
 	"github.com/holiman/uint256"
 
-	"github.com/erigontech/erigon-lib/log/v3"
-
 	"github.com/erigontech/erigon-lib/chain"
 	"github.com/erigontech/erigon-lib/common"
 	"github.com/erigontech/erigon-lib/common/hexutil"
+	"github.com/erigontech/erigon-lib/log/v3"
 	"github.com/erigontech/erigon/consensus/misc"
 	"github.com/erigontech/erigon/core/types"
 )
