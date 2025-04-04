6744b4af1614bec735ae73dbbb8ae41ab14fe06a
alex.sharov <AskAlexSharov@gmail.com>
2025-04-04 15:35:01 +0700

    save

--- core/rawdb/accessors_chain.go
+++ core/rawdb/accessors_chain.go
@@ -1300,6 +1300,7 @@ func PruneReceiptsCache(tx kv.RwTx, toBlockNum uint64, pruneLimit int) error {
 
 // WriteReceiptsCache stores all the transaction receipts belonging to a block.
 func WriteReceiptsCache(tx kv.RwTx, blockNum uint64, blockHash common.Hash, receipts types.Receipts) error {
+	return nil
 	if ok, err := HasReceiptCache(tx, blockNum, blockHash, 0); err != nil { // if exists don't write
 		return err
 	} else if ok {
