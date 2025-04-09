2d949cde416115fc1852e36cafd10a140be1e23c
Giulio <giulio.rebuffo@gmail.com>
2025-04-09 22:00:17 +0200

    save

--- turbo/jsonrpc/eth_call.go
+++ turbo/jsonrpc/eth_call.go
@@ -350,7 +350,7 @@ func (api *APIImpl) getProof(ctx context.Context, roTx kv.Tx, address libcommon.
 		return nil, fmt.Errorf("block number is in the future latest=%d requested=%d", latestBlock, blockNrOrHash.BlockNumber.Uint64())
 	}
 	if blockNrOrHash.BlockNumber.Uint64() < latestBlock {
-		lastTxnInBlock, err := rawdbv3.TxNums.Min(roTx, header.Number.Uint64()+1)
+		lastTxnInBlock, err := rawdbv3.TxNums.WithCustomReadTxNumFunc(freezeblocks.ReadTxNumFuncFromBlockReader(ctx, api._blockReader)).Min(tx, blockNrOrHash.BlockNumber.Uint64())
 		if err != nil {
 			return nil, err
 		}
