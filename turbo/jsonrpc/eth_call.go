30776107a3965b8c2cf304bbfcb1235c34481c21
Giulio <giulio.rebuffo@gmail.com>
2025-04-09 21:53:20 +0200

    save

--- turbo/jsonrpc/eth_call.go
+++ turbo/jsonrpc/eth_call.go
@@ -355,7 +355,7 @@ func (api *APIImpl) getProof(ctx context.Context, roTx kv.Tx, address libcommon.
 			return nil, err
 		}
 		commitmentStartingTxNum := tx.HistoryStartFrom(kv.CommitmentDomain)
-		fmt.Println("commitmentStartingTxNum", commitmentStartingTxNum, lastTxnInBlock)
+		fmt.Println("commitmentStartingTxNum", commitmentStartingTxNum, lastTxnInBlock, header.Number.Uint64())
 		if lastTxnInBlock < commitmentStartingTxNum {
 			return nil, state.PrunedError
 		}
