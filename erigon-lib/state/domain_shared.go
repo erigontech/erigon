c219986b4e4636925cf3a7ff05e1e473da7b44a7
awskii <artem.tsskiy@gmail.com>
2025-04-09 20:04:12 +0100

    save

--- erigon-lib/state/domain_shared.go
+++ erigon-lib/state/domain_shared.go
@@ -34,7 +34,6 @@ import (
 	"github.com/erigontech/erigon-lib/common/assert"
 	"github.com/erigontech/erigon-lib/common/dbg"
 	"github.com/erigontech/erigon-lib/common/length"
-	"github.com/erigontech/erigon-lib/crypto"
 	"github.com/erigontech/erigon-lib/kv"
 	"github.com/erigontech/erigon-lib/kv/order"
 	"github.com/erigontech/erigon-lib/kv/rawdbv3"
