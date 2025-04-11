90ec0c15c4f9931f1a4311d075746801f989c83b
yperbasis <andrey.ashikhmin@gmail.com>
2025-04-11 06:23:00 +0200

    Clean up TestFiltersDeadlock

--- rpc/rpchelper/filters_deadlock_test.go
+++ rpc/rpchelper/filters_deadlock_test.go
@@ -14,27 +14,23 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with Erigon. If not, see <http://www.gnu.org/licenses/>.
 
-package rpchelper_test
+package rpchelper
 
 import (
 	"context"
 	"testing"
 
 	libcommon "github.com/erigontech/erigon-lib/common"
-	"github.com/erigontech/erigon-lib/gointerfaces"
-	remote "github.com/erigontech/erigon-lib/gointerfaces/remoteproto"
-	types2 "github.com/erigontech/erigon-lib/gointerfaces/typesproto"
 	"github.com/erigontech/erigon-lib/log/v3"
 	"github.com/erigontech/erigon/core/types"
 	"github.com/erigontech/erigon/eth/filters"
-	"github.com/erigontech/erigon/rpc/rpchelper"
 )
 
-func TestFiltersDeadlock_Test(t *testing.T) {
+func TestFiltersDeadlock(t *testing.T) {
 	t.Parallel()
 	logger := log.New()
-	config := rpchelper.FiltersConfig{}
-	f := rpchelper.New(context.TODO(), config, nil, nil, nil, func() {}, logger)
+	config := FiltersConfig{}
+	f := New(context.TODO(), config, nil, nil, nil, func() {}, logger)
 	crit := filters.FilterCriteria{
 		Addresses: nil,
 		Topics:    [][]libcommon.Hash{},
@@ -42,7 +38,7 @@ func TestFiltersDeadlock_Test(t *testing.T) {
 	subCount := 20
 	logCount := 100
 	type sub struct {
-		id rpchelper.LogsSubID
+		id LogsSubID
 		ch <-chan *types.Log
 	}
 	ctx, cancel := context.WithCancel(context.TODO())
@@ -69,19 +65,5 @@ func TestFiltersDeadlock_Test(t *testing.T) {
 		log := createLog()
 		// this will deadlock
 		f.OnNewLogs(log)
-	}
-}
-
-func createLog() *remote.SubscribeLogsReply {
-	return &remote.SubscribeLogsReply{
-		Address:          gointerfaces.ConvertAddressToH160([20]byte{}),
-		BlockHash:        gointerfaces.ConvertHashToH256([32]byte{}),
-		BlockNumber:      0,
-		Data:             []byte{},
-		LogIndex:         0,
-		Topics:           []*types2.H256{gointerfaces.ConvertHashToH256([32]byte{99, 99})},
-		TransactionHash:  gointerfaces.ConvertHashToH256([32]byte{}),
-		TransactionIndex: 0,
-		Removed:          false,
 	}
 }
