753a1bac0e9a10d0c14ae6a79e67cf2257358d0f
alex.sharov <AskAlexSharov@gmail.com>
2025-04-11 10:23:33 +0700

    save

--- cmd/downloader/recompress.sh
+++ cmd/downloader/recompress.sh
@@ -9,6 +9,8 @@ do
    to=$dir/snapshots/$file.new
    echo "file: $file"
 
+./build/bin/erigon snapshots uncompress /erigon-data/sepolia_remove_me/snapshots/history/v1-commitment.0-2.v | ./build/bin/erigon seg compress /erigon-data/sepolia_remove_me/snapshots/history/v99-commitment.0-2.v  --datadir=/erigon-data/sepolia_remove_me
+./build/bin/erigon snapshots uncompress $from | ./build/bin/erigon seg compress $to --datadir=$dir
    ./build/bin/erigon_old snapshots uncompress $from | ./build/bin/erigon seg compress $to --datadir=$dir
    a=$(du -h $from | awk '{print $1;}')
    b=$(du -h $to | awk '{print $1;}')
