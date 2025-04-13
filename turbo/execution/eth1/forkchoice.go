288491ef467d28f7cfd134850b02645b02d26a12
Giulio <giulio.rebuffo@gmail.com>
2025-04-13 18:06:27 +0200

    more truncates

--- turbo/execution/eth1/forkchoice.go
+++ turbo/execution/eth1/forkchoice.go
@@ -99,6 +99,7 @@ func (e *EthereumExecutionModule) fixCanonicalChainIfNecessary(ctx context.Conte
 		if parentHeader == nil {
 			return nil // no need to fix the chain if the header is not found
 		}
+		fmt.Println("comparing", currHeader.ParentHash, parentHeader.Hash())
 		if currHeader.ParentHash != parentHeader.Hash() {
 			e.logger.Warn("fixCanonicalChainIfNecessary: canonical chain is broken.", "currHeader.ParentHash", currHeader.ParentHash, "parentHeader.Hash", parentHeader.Hash(), "number", i)
 			// canonical chain is broken, fix it
