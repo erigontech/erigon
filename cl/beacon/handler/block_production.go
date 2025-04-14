f5fe791349c8cf82c0bf5ca170c9df22f9eb016d
kewei <kewei.train@gmail.com>
2025-04-14 19:29:44 +0700

    update

--- cl/beacon/handler/block_production.go
+++ cl/beacon/handler/block_production.go
@@ -1334,7 +1334,8 @@ func (a *ApiHandler) electraMergedAttestationCandidates(s abstract.BeaconState) 
 	for root := range pool {
 		for committee := range pool[root] {
 			att := pool[root][committee][0]
-			log.Info("Pool data", "root", root, "slot", att.Data.Slot, "committee", committee, "attestations", len(pool[root][committee]))
+			attBytes, _ := json.Marshal(att)
+			log.Info("Pool data", "root", root, "slot", att.Data.Slot, "committee", committee, "attestations", len(pool[root][committee]), "attestation", string(attBytes))
 			for _, att := range pool[root][committee] {
 				// Convert aggregation bits to binary string representation
 				bits := att.AggregationBits.Bytes()
