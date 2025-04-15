62a10896368bc11c90b15bbca644f5acc1fac4e0
alex.sharov <AskAlexSharov@gmail.com>
2025-04-15 09:27:06 +0700

    save

--- eth/ethutils/utils.go
+++ eth/ethutils/utils.go
@@ -18,11 +18,15 @@ package ethutils
 
 import (
 	"errors"
+	"fmt"
 	"reflect"
 
 	libcommon "github.com/erigontech/erigon-lib/common"
+	"github.com/erigontech/erigon-lib/common/datadir"
 	"github.com/erigontech/erigon-lib/crypto/kzg"
+	"github.com/erigontech/erigon-lib/kv"
 	"github.com/erigontech/erigon-lib/log/v3"
+	"github.com/erigontech/erigon/core/rawdb"
 	"github.com/erigontech/erigon/core/types"
 	"github.com/erigontech/erigon/execution/consensus"
 )
@@ -81,5 +85,43 @@ func ValidateBlobs(blobGasUsed, maxBlobsGas, maxBlobsPerBlock uint64, expectedBl
 	if !reflect.DeepEqual(actualBlobHashes, expectedBlobHashes) {
 		return ErrMismatchBlobHashes
 	}
+	return nil
+}
+
+func CheckAndSetCommitmentHistoryFlag(tx kv.RwTx, logger log.Logger, dirs datadir.Dirs, keepExecutionProofs bool) error {
+	isCommitmentHistoryEnabled, ok, err := rawdb.ReadDBCommitmentHistoryEnabled(tx)
+	if err != nil {
+		return err
+	}
+	if !ok {
+		if !keepExecutionProofs {
+			if err := rawdb.WriteDBCommitmentHistoryEnabled(tx, keepExecutionProofs); err != nil {
+				return err
+			}
+			return nil
+		}
+		// we need to make sure we do not run from an old version so check amount of keys in kv.AccountDomain
+		c, err := tx.Count(kv.TblAccountVals)
+		if err != nil {
+			return fmt.Errorf("failed to count keys in kv.AccountDomain: %w", err)
+		}
+		if c > 0 {
+			return fmt.Errorf("commitment history is not enabled in the database. restart erigon after deleting the chaindata folder: %s", dirs.Chaindata)
+		}
+
+		if err := rawdb.WriteDBCommitmentHistoryEnabled(tx, keepExecutionProofs); err != nil {
+			return err
+		}
+		return nil
+	}
+	if keepExecutionProofs != isCommitmentHistoryEnabled {
+		return fmt.Errorf(
+			"commitment history flag mismatch from db and config. db: %v, config: %v. please restart erigon with the same flag or delete the chaindata folder: %s",
+			isCommitmentHistoryEnabled, keepExecutionProofs, dirs.Chaindata)
+	}
+	if err := rawdb.WriteDBCommitmentHistoryEnabled(tx, keepExecutionProofs); err != nil {
+		return err
+	}
+	logger.Warn("enabling commitment history. this is an experimental flag so run at your own risk!", "enabled", keepExecutionProofs)
 	return nil
 }
