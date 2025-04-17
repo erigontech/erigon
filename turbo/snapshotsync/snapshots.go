a0dd682243a7f40e447b812469e7d0202a2840de
alex.sharov <AskAlexSharov@gmail.com>
2025-04-17 14:54:47 +0700

    save

--- turbo/snapshotsync/snapshots.go
+++ turbo/snapshotsync/snapshots.go
@@ -121,10 +121,6 @@ func findOverlaps[T SortedRange](in []T) (res []T, overlapped []T) {
 	for i := 0; i < len(in); i++ {
 		f := in[i]
 		iFrom, iTo := f.GetRange()
-		if f.GetType().Name() == "headers" {
-			fmt.Printf("[dbg] findOverlaps: %s, %d-%d\n", f.GetType().Name(), iFrom, iTo)
-		}
-
 		if iFrom == iTo {
 			overlapped = append(overlapped, f)
 			continue
