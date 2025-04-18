411c1b5a0ad88f28bd9331d4b7aa34eb6bae6555
sudeep <sudeepdino008@gmail.com>
2025-04-18 07:18:47 +0530

    fix use of dirtyFiles in BuildMissedAccessors (#14613)
    **BeginDirtyFilesRoTx**
    - because BuildMissedAccessors need dirtyFiles to be open, and currently
    there's no mechanic to protect these dirtyFile from getting
    removed/munmapp-ed, while it's being read.
    - `BeginDirtyFilesRoTx` is kept open for the duration of Building missed
    accessors.
    
    
    **consistent naming**
    -  "missing" accessors/files -> "missed" accessors/files
    - `BuildMissedIndices` -> `BuildMissedAccessors` (because we already use
    `Accessors` bitmap)

--- erigon-lib/state/history.go
+++ erigon-lib/state/history.go
@@ -312,11 +312,15 @@ func (ht *HistoryRoTx) Files() (res []string) {
 	return append(res, ht.iit.Files()...)
 }
 
-func (h *History) missedMapAccessors() (l []*filesItem) {
+func (h *History) MissedMapAccessors() (l []*filesItem) {
+	return h.missedMapAccessors(h.dirtyFiles.Items())
+}
+
+func (h *History) missedMapAccessors(source []*filesItem) (l []*filesItem) {
 	if !h.indexList.Has(AccessorHashMap) {
 		return nil
 	}
-	return fileItemsWithMissingAccessors(h.dirtyFiles, h.aggregationStep, func(fromStep, toStep uint64) []string {
+	return fileItemsWithMissedAccessors(source, h.aggregationStep, func(fromStep, toStep uint64) []string {
 		return []string{
 			h.vAccessorFilePath(fromStep, toStep),
 		}
@@ -427,9 +431,9 @@ func (h *History) buildVI(ctx context.Context, historyIdxPath string, hist, efHi
 	return historyIdxPath, nil
 }
 
-func (h *History) BuildMissedAccessors(ctx context.Context, g *errgroup.Group, ps *background.ProgressSet) {
-	h.InvertedIndex.BuildMissedAccessors(ctx, g, ps)
-	for _, item := range h.missedMapAccessors() {
+func (h *History) BuildMissedAccessors(ctx context.Context, g *errgroup.Group, ps *background.ProgressSet, historyFiles *MissedAccessorHistoryFiles) {
+	h.InvertedIndex.BuildMissedAccessors(ctx, g, ps, historyFiles.ii)
+	for _, item := range historyFiles.missedMapAccessors() {
 		item := item
 		g.Go(func() error {
 			return h.buildVi(ctx, item, ps)
