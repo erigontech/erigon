339ea3f04e2635deb53c694058f5b958e5bccf84
Mark Holt <mark.holt@erigon.tech>
2025-04-19 12:10:28 +0100

    print deps

--- polygon/bor/bor.go
+++ polygon/bor/bor.go
@@ -1757,6 +1757,9 @@ func (c *Bor) TxDependencies(h *types.Header) [][]int {
 
 	c.Dependencies.Add(h.Hash(), dependencies)
 
+	if len(dependencies) > 0 {
+		fmt.Println(h.Number.Int64(), dependencies)
+	}
 	return dependencies
 }
 
