9dca4a9defa8944803f23df6379e8db848cad9f2
Giulio <giulio.rebuffo@gmail.com>
2025-04-20 23:51:01 +0200

    safu

--- core/vm/interpreter.go
+++ core/vm/interpreter.go
@@ -253,6 +253,7 @@ func (in *EVMInterpreter) Run(contract *Contract, input []byte, readOnly bool) (
 	// Arbitrum: handle Stylus programs
 	if in.evm.chainRules.IsStylus && state.IsStylusProgram(contract.Code) {
 		ret, err = in.evm.ProcessingHook.ExecuteWASM(callContext, input, in)
+		fmt.Println(err)
 		return
 	}
 
