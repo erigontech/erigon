e4cff5b31f70b005538ab0ac5e6af49804f31467
alex.sharov <AskAlexSharov@gmail.com>
2025-04-22 11:27:50 +0700

    save

--- core/state/rw_v3.go
+++ core/state/rw_v3.go
@@ -262,34 +262,6 @@ func (rs *ParallelExecutionState) ApplyLogsAndTraces(txTask *TxTask, domains *li
 		}
 	}
 
-	//if !txTask.Final {
-	//	var receipt *types.Receipt
-	//	if txTask.TxIndex >= 0 {
-	//		receipt = txTask.BlockReceipts[txTask.TxIndex]
-	//	}
-	//	if err := rawtemporaldb.AppendReceipt(se.doms, receipt, se.blobGasUsed); err != nil {
-	//		return false, err
-	//	}
-	//} else {
-	//	if se.cfg.chainConfig.Bor != nil && txTask.TxIndex >= 1 {
-	//		// get last receipt and store the last log index + 1
-	//		lastReceipt := txTask.BlockReceipts[txTask.TxIndex-1]
-	//		if lastReceipt == nil {
-	//			return false, fmt.Errorf("receipt is nil but should be populated, txIndex=%d, block=%d", txTask.TxIndex-1, txTask.BlockNum)
-	//		}
-	//		if len(lastReceipt.Logs) > 0 {
-	//			firstIndex := lastReceipt.Logs[len(lastReceipt.Logs)-1].Index + 1
-	//			receipt := types.Receipt{
-	//				CumulativeGasUsed:        lastReceipt.CumulativeGasUsed,
-	//				FirstLogIndexWithinBlock: uint32(firstIndex),
-	//			}
-	//			if err := rawtemporaldb.AppendReceipt(se.doms, &receipt, se.blobGasUsed); err != nil {
-	//				return false, err
-	//			}
-	//		}
-	//	}
-	//}
-
 	return nil
 }
 
