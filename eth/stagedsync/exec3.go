f46c5ad31b87bf89fc558a388624a5b1e01eb9c4
alex.sharov <AskAlexSharov@gmail.com>
2025-04-23 13:49:29 +0700

    save

--- eth/stagedsync/exec3.go
+++ eth/stagedsync/exec3.go
@@ -885,9 +885,6 @@ func flushAndCheckCommitmentV3(ctx context.Context, header *types.Header, applyT
 		if err := doms.Flush(ctx, applyTx); err != nil {
 			return false, err
 		}
-		if err = applyTx.(state2.HasAggTx).AggTx().(*state2.AggregatorRoTx).PruneCommitHistory(ctx, applyTx, nil); err != nil {
-			return false, err
-		}
 	}
 	return true, nil
 
