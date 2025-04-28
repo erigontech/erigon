90edcdc327c8d7282d00bdef673c91a220e60ee9
GarmashAlex <garmasholeksii@gmail.com>
2025-04-28 17:28:48 +0300

    compress: update test hashes for the new DictionaryBuilder implementation

--- erigon-lib/seg/compress_test.go
+++ erigon-lib/seg/compress_test.go
@@ -177,7 +177,7 @@ func TestCompressDict1(t *testing.T) {
 		i++
 	}
 
-	if cs := checksum(d.filePath); cs != 3153486123 {
+	if cs := checksum(d.filePath); cs != 3126908344 {
 		// it's ok if hash changed, but need re-generate all existing snapshot hashes
 		// in https://github.com/erigontech/erigon-snapshot
 		t.Errorf("result file hash changed, %d", cs)
@@ -247,7 +247,7 @@ func TestCompressDictCmp(t *testing.T) {
 		i++
 	}
 
-	if cs := checksum(d.filePath); cs != 3153486123 {
+	if cs := checksum(d.filePath); cs != 3126908344 {
 		// it's ok if hash changed, but need re-generate all existing snapshot hashes
 		// in https://github.com/erigontech/erigon-snapshot
 		t.Errorf("result file hash changed, %d", cs)
