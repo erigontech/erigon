d33b53c4f6e8fa5f85e91594f9411417fd2d5c95
alex.sharov <AskAlexSharov@gmail.com>
2025-04-30 20:00:48 +0700

    save

--- erigon-lib/state/stats/agg_log_stats.go
+++ erigon-lib/state/stats/agg_log_stats.go
@@ -22,16 +22,6 @@ func LogStats(at *state.AggregatorRoTx, tx kv.Tx, logger log.Logger, tx2block fu
 		return
 	}
 
-	var lastCommitmentBlockNum, lastCommitmentTxNum uint64
-	commFiles := at.DomainFiles(kv.CommitmentDomain)
-	if len(commFiles) > 0 {
-		lastCommitmentTxNum = commFiles[len(commFiles)-1].EndRootNum()
-		lastCommitmentBlockNum, err = tx2block(lastCommitmentTxNum)
-		if err != nil {
-			logger.Warn("[snapshots:history] Stat", "err", err)
-			return
-		}
-	}
 	firstHistoryIndexBlockInDB, err := tx2block(at.MinStepInDb(tx, kv.AccountsDomain) * at.StepSize())
 	if err != nil {
 		logger.Warn("[snapshots:history] Stat", "err", err)
@@ -44,10 +34,6 @@ func LogStats(at *state.AggregatorRoTx, tx kv.Tx, logger log.Logger, tx2block fu
 		"blocks", common2.PrettyCounter(domainBlockNumProgress+1),
 		"txs", common2.PrettyCounter(at.Agg().EndTxNumMinimax()),
 		"first_history_idx_in_db", firstHistoryIndexBlockInDB,
-		"last_commitment_block", lastCommitmentBlockNum,
-		"last_commitment_tx_num", lastCommitmentTxNum,
-		//"cnt_in_files", strings.Join(str2, ","),
-		//"used_files", strings.Join(at.Files(), ","),
 		"alloc", common2.ByteCount(m.Alloc), "sys", common2.ByteCount(m.Sys))
 
 }
