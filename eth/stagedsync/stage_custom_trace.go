909d9ddcc4f298ce879c336aeb20eacac83af929
alex.sharov <AskAlexSharov@gmail.com>
2025-05-01 09:10:12 +0700

    save

--- eth/stagedsync/stage_custom_trace.go
+++ eth/stagedsync/stage_custom_trace.go
@@ -149,7 +149,7 @@ func SpawnCustomTrace(cfg CustomTraceCfg, ctx context.Context, logger log.Logger
 
 	log.Info("SpawnCustomTrace", "startBlock", startBlock, "endBlock", endBlock)
 
-	batchSize := uint64(100_000)
+	batchSize := uint64(50_000)
 	for ; startBlock < endBlock; startBlock += batchSize {
 		to := min(endBlock+1, startBlock+batchSize)
 		if err := customTraceBatchProduce(ctx, cfg.Produce, cfg.ExecArgs, cfg.db, startBlock, to, "custom_trace", producingDomain, logger); err != nil {
