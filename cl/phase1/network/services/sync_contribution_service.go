7ada16046f05f8a86971d8f6b2c6c5f4f4bed02a
kewei <kewei.train@gmail.com>
2025-05-05 14:25:50 +0800

    log

--- cl/phase1/network/services/sync_contribution_service.go
+++ cl/phase1/network/services/sync_contribution_service.go
@@ -173,7 +173,8 @@ func (s *syncContributionService) ProcessMessage(ctx context.Context, subnet *ui
 					F:          func() {},
 				}
 				if err := s.batchSignatureVerifier.ImmediateVerification(&d); err != nil {
-					log.Warn("immediate verification failed", "err", err, "index", i, "aggregation_bits", contributionAndProof.Contribution.AggregationBits, "subcommittee_index", contributionAndProof.Contribution.SubcommitteeIndex)
+					log.Warn("immediate verification failed", "err", err, "index", i, "aggregation_bits", contributionAndProof.Contribution.AggregationBits, "subcommittee_index", contributionAndProof.Contribution.SubcommitteeIndex,
+						"slot", contributionAndProof.Contribution.Slot, "aggregator_index", contributionAndProof.AggregatorIndex, "subcommiteePubsKeys", len(subcommiteePubsKeys))
 				}
 			}
 
