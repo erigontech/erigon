21dd54f011c0dd9588c4994e08487e49d683752a
awskii <artem.tsskiy@gmail.com>
2025-05-05 09:12:21 +0100

    fix error text

--- ethdb/prune/storage_mode.go
+++ ethdb/prune/storage_mode.go
@@ -211,7 +211,7 @@ func EnsureNotChanged(tx kv.GetPut, pruneMode Mode) (Mode, error) {
 
 		// If storage mode is not explicitly specified, we take whatever is in the database
 		if !reflect.DeepEqual(pm, pruneMode) {
-			return pm, errors.New("not allowed change of --prune flag, last time you used: " + pm.String())
+			return pm, errors.New("changing --prune.* flags is prohibited, last time you used: --prune.mode=" + pm.String())
 		}
 	}
 	return pm, nil
