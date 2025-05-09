4fc25597f294ade66c926df3e6c309c3ad1c188a
Alex Sharov <AskAlexSharov@gmail.com>
2025-05-09 08:37:35 +0700

    Revert "[3.0 pick] downloader: same NetworkChunkSize as in Erigon2" (#14958)
    Reverts erigontech/erigon#14642

--- erigon-lib/downloader/downloadercfg/downloadercfg.go
+++ erigon-lib/downloader/downloadercfg/downloadercfg.go
@@ -50,7 +50,7 @@ const DefaultPieceSize = 2 * 1024 * 1024
 
 // DefaultNetworkChunkSize - how much data request per 1 network call to peer.
 // default: 16Kb
-const DefaultNetworkChunkSize = 256 * 1024
+const DefaultNetworkChunkSize = 8 * 1024 * 1024
 
 type Cfg struct {
 	ClientConfig  *torrent.ClientConfig
