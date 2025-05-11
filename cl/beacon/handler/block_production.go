373d26cf256c05910678402dfb6258731907a457
Giulio <giulio.rebuffo@gmail.com>
2025-05-11 02:02:40 +0200

    save

--- cl/beacon/handler/block_production.go
+++ cl/beacon/handler/block_production.go
@@ -310,7 +310,6 @@ func (a *ApiHandler) GetEthV3ValidatorBlock(
 		"proposerIndex", block.ProposerIndex,
 		"slot", targetSlot,
 		"state_root", block.StateRoot,
-		"attestations", block.BeaconBody.Attestations.Len(),
 		"execution_value", block.GetExecutionValue().Uint64(),
 		"version", block.Version(),
 		"blinded", block.IsBlinded(),
