b19ee66a0468e556f8e38a96ebd324977d803dde
alex.sharov <AskAlexSharov@gmail.com>
2025-05-13 12:27:49 +0700

    save

--- erigon-db/rawdb/accessors_chain.go
+++ erigon-db/rawdb/accessors_chain.go
@@ -1296,8 +1296,8 @@ func ReadReceiptsCacheV2(tx kv.TemporalTx, block *types.Block, txNumReader rawdb
 		return
 	}
 
-	for txnID := _min; txnID < _max+1; txnID++ {
-		v, ok, err := tx.HistorySeek(kv.RCacheDomain, receiptCacheKey, txnID-1)
+	for txnID := _min + 1; txnID < _max+1; txnID++ {
+		v, ok, err := tx.HistorySeek(kv.RCacheDomain, receiptCacheKey, txnID+1)
 		if err != nil {
 			return nil, fmt.Errorf("unexpected error, couldn't find changeset: txNum=%d, %w", txnID, err)
 		}
