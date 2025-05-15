a782351dc73a487b01e7a6a254f4ba30711bce67
sudeepdino008 <sudeepdino008@gmail.com>
2025-05-15 20:10:33 +0530

    save

--- erigon-lib/state/entity_extras/entity_integrity_check.go
+++ erigon-lib/state/entity_extras/entity_integrity_check.go
@@ -1,13 +1,16 @@
 package entity_extras
 
-import "github.com/erigontech/erigon-lib/kv"
+import (
+	"github.com/erigontech/erigon-lib/common/datadir"
+	"github.com/erigontech/erigon-lib/kv"
+)
 
 // this is an equivalent of snap_integrity_check.go, but for
 // current dhii (which don't have SnapNameSchema).
 // dependency/referred: account/storage
 // dependent/referencing: commitment
 type EntityIntegrityChecker interface {
-	CheckAnyDependentPresent(dependencyDomain kv.Domain, fromStep, toStep uint64) (IsPresent bool)
+	CheckAllDependentPresent(dependencyDomain kv.Domain, fromStep, toStep uint64) (IsPresent bool)
 }
 
 type KvFileNameGen func(fromStep, toStep uint64) string
@@ -18,42 +21,51 @@ type KvFileNameGen func(fromStep, toStep uint64) string
 // instance should be held by dependency domain
 // (accounts in this example)
 type DependencyIntegrityChecker struct {
-	dependencyMap map[kv.Domain][]*dependencyInfo
+	dependencyMap map[kv.Domain][]*dependentInfo
+	dirs          datadir.Dirs
 }
 
-type dependencyInfo struct {
-	filenameGen     KvFileNameGen
+type dependentInfo struct {
 	dependentDomain kv.Domain
 	dependentGen    KvFileNameGen
 }
 
-func NewDependencyIntegrityChecker() *DependencyIntegrityChecker {
-	return &DependencyIntegrityChecker{}
+func NewDependencyIntegrityChecker(dirs datadir.Dirs) *DependencyIntegrityChecker {
+	return &DependencyIntegrityChecker{dependencyMap: make(map[kv.Domain][]*dependentInfo), dirs: dirs}
 }
 
-func (d *DependencyIntegrityChecker) AddDependency(dependency kv.Domain, dependencyGen KvFileNameGen, dependent kv.Domain, dependentGen KvFileNameGen) {
+func (d *DependencyIntegrityChecker) AddDependency(dependency kv.Domain, dependent kv.Domain, dependentGen KvFileNameGen) {
 	arr, ok := d.dependencyMap[dependency]
 	if !ok {
-		arr = make([]*dependencyInfo, 0)
+		arr = make([]*dependentInfo, 0)
 	}
-	arr = append(arr, &dependencyInfo{
-		filenameGen:     dependencyGen,
+	arr = append(arr, &dependentInfo{
 		dependentDomain: dependent,
 		dependentGen:    dependentGen,
 	})
 	d.dependencyMap[dependency] = arr
 }
 
-func (d *DependencyIntegrityChecker) CheckAnyDependentPresent(dependency kv.Domain, fromStep, toStep uint64) (IsPresent bool) {
+// dependency: account
+// is commitment.0-2 present? if no, don't delete account.0-2, also don't use it for visibleFiles...
+func (d *DependencyIntegrityChecker) CheckAllDependentPresent(dependency kv.Domain, fromStep, toStep uint64) (IsPresent bool) {
 	arr, ok := d.dependencyMap[dependency]
 	if !ok {
 		return false
 	}
 
-	for _, dependency := range arr {
-
+	for _, dependent := range arr {
+		dependentName := dependent.dependentGen(fromStep, toStep)
+		if !d.checkFilePresent(d.dirs.Snap, dependentName) {
+			return false
+		}
 	}
 
+	return true
+}
+
+func (d *DependencyIntegrityChecker) checkFilePresent(dir, filename string) bool {
+	return true
 }
 
 // 1.should delete this file? -- only care about the file (no index etc.)
