erigontech/erigon

commit bf3b30f571526f335783029b5aaca79ba379b525

Minsup improvements (#15065)
added eq method

added ok as return parameter to `FindFilesWithVersionsByPattern`

---------

Co-authored-by: JkLondon <ilya@mikheev.fun>

erigon-lib/state/inverted_index.go
@@ -286,8 +286,8 @@ func (ii *InvertedIndex) openDirtyFiles() error {
 			fromStep, toStep := item.startTxNum/ii.aggregationStep, item.endTxNum/ii.aggregationStep
 			if item.decompressor == nil {
 				fPathPattern := ii.efFilePathMask(fromStep, toStep)
-				fPath, fileVer, err := version.FindFilesWithVersionsByPattern(fPathPattern)
-				if err != nil && !errors.Is(err, os.ErrNotExist) {
+				fPath, fileVer, ok, err := version.FindFilesWithVersionsByPattern(fPathPattern)
+				if err != nil {
 					_, fName := filepath.Split(fPath)
 					ii.logger.Debug("[agg] InvertedIndex.openDirtyFiles: FindFilesWithVersionsByPattern error", "f", fName, "err", err)
 					invalidFileItemsLock.Lock()
@@ -296,7 +296,7 @@ func (ii *InvertedIndex) openDirtyFiles() error {
 					continue
 				}
 
-				if errors.Is(err, os.ErrNotExist) {
+				if ok {
 					_, fName := filepath.Split(fPath)
 					ii.logger.Debug("[agg] InvertedIndex.openDirtyFiles: file does not exists", "f", fName)
 					invalidFileItemsLock.Lock()
@@ -305,7 +305,7 @@ func (ii *InvertedIndex) openDirtyFiles() error {
 					continue
 				}
 
-				if fileVer.Cmp(ii.version.DataEF.Current) != 0 {
+				if !fileVer.Eq(ii.version.DataEF.Current) {
 					if !fileVer.Less(ii.version.DataEF.MinSupported) {
 						ii.version.DataEF.Current = fileVer
 					} else {
@@ -331,14 +331,14 @@ func (ii *InvertedIndex) openDirtyFiles() error {
 
 			if item.index == nil {
 				fPathPattern := ii.efAccessorFilePathMask(fromStep, toStep)
-				fPath, fileVer, err := version.FindFilesWithVersionsByPattern(fPathPattern)
-				if err != nil && !errors.Is(err, os.ErrNotExist) {
+				fPath, fileVer, ok, err := version.FindFilesWithVersionsByPattern(fPathPattern)
+				if err != nil {
 					_, fName := filepath.Split(fPath)
 					ii.logger.Warn("[agg] InvertedIndex.openDirtyFiles", "err", err, "f", fName)
 					// don't interrupt on error. other files may be good
 				}
-				if !errors.Is(err, os.ErrNotExist) {
-					if fileVer.Cmp(ii.version.AccessorEFI.Current) != 0 {
+				if ok {
+					if !fileVer.Eq(ii.version.AccessorEFI.Current) {
 						if !fileVer.Less(ii.version.AccessorEFI.MinSupported) {
 							ii.version.AccessorEFI.Current = fileVer
 						} else {

erigon-lib/state/history.go
@@ -25,7 +25,6 @@ import (
 	"fmt"
 	"github.com/erigontech/erigon-lib/version"
 	"math"
-	"os"
 	"path/filepath"
 	"sync"
 	"time"
@@ -227,8 +226,8 @@ func (h *History) openDirtyFiles() error {
 			fromStep, toStep := item.startTxNum/h.aggregationStep, item.endTxNum/h.aggregationStep
 			if item.decompressor == nil {
 				fPathMask := h.vFilePathMask(fromStep, toStep)
-				fPath, fileVer, err := version.FindFilesWithVersionsByPattern(fPathMask)
-				if err != nil && !errors.Is(err, os.ErrNotExist) {
+				fPath, fileVer, ok, err := version.FindFilesWithVersionsByPattern(fPathMask)
+				if err != nil {
 					_, fName := filepath.Split(fPath)
 					h.logger.Debug("[agg] History.openDirtyFiles: FileExist", "f", fName, "err", err)
 					invalidFilesMu.Lock()
@@ -236,7 +235,7 @@ func (h *History) openDirtyFiles() error {
 					invalidFilesMu.Unlock()
 					continue
 				}
-				if errors.Is(err, os.ErrNotExist) {
+				if !ok {
 					_, fName := filepath.Split(fPath)
 					h.logger.Debug("[agg] History.openDirtyFiles: file does not exists", "f", fName)
 					invalidFilesMu.Lock()
@@ -244,7 +243,7 @@ func (h *History) openDirtyFiles() error {
 					invalidFilesMu.Unlock()
 					continue
 				}
-				if fileVer.Cmp(h.version.DataV.Current) != 0 {
+				if !fileVer.Eq(h.version.DataV.Current) {
 					if !fileVer.Less(h.version.DataV.MinSupported) {
 						h.version.DataV.Current = fileVer
 					} else {
@@ -283,13 +282,13 @@ func (h *History) openDirtyFiles() error {
 
 			if item.index == nil {
 				fPathMask := h.vAccessorFilePathMask(fromStep, toStep)
-				fPath, fileVer, err := version.FindFilesWithVersionsByPattern(fPathMask)
-				if err != nil && !errors.Is(err, os.ErrNotExist) {
+				fPath, fileVer, ok, err := version.FindFilesWithVersionsByPattern(fPathMask)
+				if err != nil {
 					_, fName := filepath.Split(fPath)
 					h.logger.Warn("[agg] History.openDirtyFiles", "err", err, "f", fName)
 				}
-				if !errors.Is(err, os.ErrNotExist) {
-					if fileVer.Cmp(h.version.AccessorVI.Current) != 0 {
+				if ok {
+					if !fileVer.Eq(h.version.AccessorVI.Current) {
 						if !fileVer.Less(h.version.AccessorVI.MinSupported) {
 							h.version.AccessorVI.Current = fileVer
 						} else {

erigon-lib/state/snap_repo.go
@@ -1,9 +1,7 @@
 package state
 
 import (
-	"errors"
 	"fmt"
-	"os"
 	"path/filepath"
 	"sync"
 
@@ -317,10 +315,10 @@ func (f *SnapshotRepo) openDirtyFiles() error {
 			if item.decompressor == nil {
 				fPathGen := p.DataFile(version.V1_0, ee.RootNum(item.startTxNum), ee.RootNum(item.endTxNum))
 				fPathMask, _ := version.ReplaceVersionWithMask(fPathGen)
-				fPath, _, err := version.FindFilesWithVersionsByPattern(fPathMask)
-				if err != nil {
+				fPath, _, ok, err := version.FindFilesWithVersionsByPattern(fPathMask)
+				if err != nil || !ok {
 					_, fName := filepath.Split(fPath)
-					if errors.Is(err, os.ErrNotExist) {
+					if err == nil {
 						f.logger.Debug("SnapshotRepo.openDirtyFiles: file doesn't exist", "f", fName)
 					} else {
 						f.logger.Debug("SnapshotRepo.openDirtyFiles: FileExist", "f", fName, "err", err)
@@ -345,13 +343,13 @@ func (f *SnapshotRepo) openDirtyFiles() error {
 			if item.index == nil && accessors.Has(AccessorHashMap) {
 				fPathGen := p.AccessorIdxFile(version.V1_0, ee.RootNum(item.startTxNum), ee.RootNum(item.endTxNum), 0)
 				fPathMask, _ := version.ReplaceVersionWithMask(fPathGen)
-				fPath, _, err := version.FindFilesWithVersionsByPattern(fPathMask)
-				if err != nil && !errors.Is(err, os.ErrNotExist) {
+				fPath, _, ok, err := version.FindFilesWithVersionsByPattern(fPathMask)
+				if err != nil {
 					_, fName := filepath.Split(fPath)
 					f.logger.Debug("SnapshotRepo.openDirtyFiles: FileExist", "f", fName, "err", err)
 				}
 
-				if !errors.Is(err, os.ErrNotExist) {
+				if ok {
 					if item.index, err = recsplit.OpenIndex(fPath); err != nil {
 						_, fName := filepath.Split(fPath)
 						f.logger.Error("SnapshotRepo.openDirtyFiles", "err", err, "f", fName)
@@ -363,12 +361,12 @@ func (f *SnapshotRepo) openDirtyFiles() error {
 			if item.bindex == nil && accessors.Has(AccessorBTree) {
 				fPathGen := p.BtIdxFile(version.V1_0, ee.RootNum(item.startTxNum), ee.RootNum(item.endTxNum))
 				fPathMask, _ := version.ReplaceVersionWithMask(fPathGen)
-				fPath, _, err := version.FindFilesWithVersionsByPattern(fPathMask)
-				if err != nil && !errors.Is(err, os.ErrNotExist) {
+				fPath, _, ok, err := version.FindFilesWithVersionsByPattern(fPathMask)
+				if err != nil {
 					_, fName := filepath.Split(fPath)
 					f.logger.Warn("[agg] SnapshotRepo.openDirtyFiles", "err", err, "f", fName)
 				}
-				if !errors.Is(err, os.ErrNotExist) {
+				if ok {
 					if item.bindex, err = OpenBtreeIndexWithDecompressor(fPath, DefaultBtreeM, item.decompressor, p.DataFileCompression()); err != nil {
 						_, fName := filepath.Split(fPath)
 						f.logger.Error("SnapshotRepo.openDirtyFiles", "err", err, "f", fName)
@@ -379,12 +377,12 @@ func (f *SnapshotRepo) openDirtyFiles() error {
 			if item.existence == nil && accessors.Has(AccessorExistence) {
 				fPathGen := p.ExistenceFile(version.V1_0, ee.RootNum(item.startTxNum), ee.RootNum(item.endTxNum))
 				fPathMask, _ := version.ReplaceVersionWithMask(fPathGen)
-				fPath, _, err := version.FindFilesWithVersionsByPattern(fPathMask)
-				if err != nil && !errors.Is(err, os.ErrNotExist) {
+				fPath, _, ok, err := version.FindFilesWithVersionsByPattern(fPathMask)
+				if err != nil {
 					_, fName := filepath.Split(fPath)
 					f.logger.Debug("SnapshotRepo.openDirtyFiles: FileExist", "f", fName, "err", err)
 				}
-				if !errors.Is(err, os.ErrNotExist) {
+				if ok {
 					if item.existence, err = existence.OpenFilter(fPath); err != nil {
 						_, fName := filepath.Split(fPath)
 						f.logger.Error("SnapshotRepo.openDirtyFiles", "err", err, "f", fName)

erigon-lib/version/file_version.go
@@ -3,7 +3,6 @@ package version
 import (
 	"errors"
 	"fmt"
-	"os"
 	"path/filepath"
 	"sort"
 	"strconv"
@@ -47,6 +46,15 @@ func (v Version) Cmp(rhd Version) int {
 
 	// Both Major and Minor are equal
 	return 0
+}
+
+func (v Version) Eq(rhd Version) bool {
+	if v.Major == rhd.Major {
+		if v.Minor == rhd.Minor {
+			return true
+		}
+	}
+	return false
 }
 
 func (v Version) Downgrade() Version {
@@ -114,14 +122,14 @@ func (v Versions) String() string {
 }
 
 // FindFilesWithVersionsByPattern return an filepath by pattern
-func FindFilesWithVersionsByPattern(pattern string) (string, Version, error) {
+func FindFilesWithVersionsByPattern(pattern string) (string, Version, bool, error) {
 	matches, err := filepath.Glob(pattern)
 	if err != nil {
-		return "", Version{}, fmt.Errorf("invalid pattern: %w", err)
+		return "", Version{}, false, fmt.Errorf("invalid pattern: %w", err)
 	}
 
 	if len(matches) == 0 {
-		return "", Version{}, os.ErrNotExist
+		return "", Version{}, false, nil
 	}
 	if len(matches) > 1 {
 		sort.Slice(matches, func(i, j int) bool {
@@ -136,11 +144,11 @@ func FindFilesWithVersionsByPattern(pattern string) (string, Version, error) {
 		_, fName := filepath.Split(matches[len(matches)-1])
 		ver, _ := ParseVersion(fName)
 
-		return matches[len(matches)-1], ver, nil
+		return matches[len(matches)-1], ver, true, nil
 	}
 	_, fName := filepath.Split(matches[0])
 	ver, _ := ParseVersion(fName)
-	return matches[0], ver, nil
+	return matches[0], ver, true, nil
 }
 
 func ReplaceVersionWithMask(path string) (string, error) {

commit 5eae3a7add7fe097168adf869a7760c5248802e5

Inverted Index: fix check for file existence (#15073)
I noticed that RPC Integration Tests started failing systematically
after #15065, so I checked the changes in such PR and spotted this.

RPC Integration Tests are OK here:
https://github.com/erigontech/erigon/actions/runs/15052494612

erigon-lib/state/inverted_index.go
@@ -296,7 +296,7 @@ func (ii *InvertedIndex) openDirtyFiles() error {
 					continue
 				}
 
-				if ok {
+				if !ok {
 					_, fName := filepath.Split(fPath)
 					ii.logger.Debug("[agg] InvertedIndex.openDirtyFiles: file does not exists", "f", fName)
 					invalidFileItemsLock.Lock()