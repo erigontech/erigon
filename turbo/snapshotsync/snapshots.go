3409a6d27e45be77f2eb7315f66c7057bebc22f5
alex.sharov <AskAlexSharov@gmail.com>
2025-05-15 15:49:20 +0700

    save

--- turbo/snapshotsync/snapshots.go
+++ turbo/snapshotsync/snapshots.go
@@ -933,7 +933,7 @@ func (s *RoSnapshots) Ls() {
 			if seg.src == nil || seg.src.Decompressor == nil {
 				continue
 			}
-			log.Info("[snapshots] ", "f", seg.src.FileName(), "from", seg.from, "to", seg.to, "count", seg.src.Decompressor.Count())
+			log.Info("[snapshots] ", "f", seg.src.FileName(), xseg.src.Decompressor.Count())
 		}
 	}
 }
