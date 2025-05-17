e3524a905ef0d2fe7f810b03659e5da3ea40e994
alex.sharov <AskAlexSharov@gmail.com>
2025-05-17 12:44:34 +0700

    save

--- turbo/jsonrpc/receipts/receipts_generator.go
+++ turbo/jsonrpc/receipts/receipts_generator.go
@@ -157,7 +157,7 @@ func (g *Generator) GetReceipt(ctx context.Context, cfg *chain.Config, tx kv.Tem
 	}
 
 	//if can find in DB - then don't need store in `receiptsCache` - because DB it's already kind-of cache (small, mmaped, hot file)
-	receiptFromDB, ok, err = rawdb.ReadReceiptCacheV2(tx, blockNum, blockHash, uint32(index), txnHash, g.txNumReader)
+	receiptFromDB, ok, err = rawdb.ReadReceiptCacheV2(tx, blockNum, blockHash, txNum, txnHash)
 	if err != nil {
 		return nil, err
 	}
