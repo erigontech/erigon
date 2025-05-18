7623aaf3cd1673e8700611ea21ee4ae332b3ca9a
alex.sharov <AskAlexSharov@gmail.com>
2025-05-18 11:55:57 +0700

    save

--- core/vm/program/program_test.go
+++ core/vm/program/program_test.go
@@ -21,8 +21,8 @@ import (
 	"math/big"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/vm"
+	"github.com/erigontech/erigon-lib/common"
+	"github.com/erigontech/erigon/core/vm"
 	"github.com/holiman/uint256"
 )
 
