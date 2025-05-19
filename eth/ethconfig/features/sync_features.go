e0c73e6e4be88a3dbcbc01e98cc082ede9ecd4e9
alex.sharov <AskAlexSharov@gmail.com>
2025-05-19 11:56:40 +0700

    save

---
+++ eth/ethconfig/features/sync_features.go
@@ -0,0 +1,32 @@
+package features
+
+import (
+	"context"
+
+	"github.com/erigontech/erigon-lib/kv"
+	"github.com/erigontech/erigon-lib/kv/kvcfg"
+	"github.com/erigontech/erigon-lib/state"
+	"github.com/erigontech/erigon/core/rawdb"
+	"github.com/erigontech/erigon/eth/ethconfig"
+)
+
+func EnableSyncCfg(chainDB kv.RoDB, syncCfg ethconfig.Sync) (ethconfig.Sync, error) {
+	err := chainDB.View(context.Background(), func(tx kv.Tx) (err error) {
+		syncCfg.KeepExecutionProofs, _, err = rawdb.ReadDBCommitmentHistoryEnabled(tx)
+		if err != nil {
+			return err
+		}
+		if syncCfg.KeepExecutionProofs {
+			state.EnableHistoricalCommitment()
+		}
+		syncCfg.PersistReceiptsCacheV2, err = kvcfg.PersistReceipts.Enabled(tx)
+		if err != nil {
+			return err
+		}
+		if syncCfg.PersistReceiptsCacheV2 {
+			state.EnableHistoricalRCache()
+		}
+		return nil
+	})
+	return syncCfg, err
+}
