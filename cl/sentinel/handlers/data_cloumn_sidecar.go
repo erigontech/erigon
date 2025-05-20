d50f295c9f82794f4e432fe19f810ee162ec1f8f
kewei <kewei.train@gmail.com>
2025-05-21 00:52:05 +0800

    update

---
+++ cl/sentinel/handlers/data_cloumn_sidecar.go
@@ -0,0 +1,11 @@
+package handlers
+
+import "github.com/libp2p/go-libp2p/core/network"
+
+func (c *ConsensusHandlers) dataColumnSidecarsByRangeHandler(s network.Stream) error {
+	return nil
+}
+
+func (c *ConsensusHandlers) dataColumnSidecarsByRootHandler(s network.Stream) error {
+	return nil
+}
