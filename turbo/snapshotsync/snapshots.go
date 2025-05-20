9011cd98293d96a90acb66e603c74dd9f65e9ce5
sudeepdino008 <sudeepdino008@gmail.com>
2025-05-20 15:53:09 +0530

    save

--- turbo/snapshotsync/snapshots.go
+++ turbo/snapshotsync/snapshots.go
@@ -1294,7 +1294,7 @@ type snapshotNotifier interface {
 }
 
 func (s *RoSnapshots) BuildMissedIndices(ctx context.Context, logPrefix string, notifier snapshotNotifier, dirs datadir.Dirs, cc *chain.Config, logger log.Logger) error {
-	s.logger.Info("ye...", "im", s.IndicesMax(), "segmax", s.SegmentsMax(), "producee2", !s.Cfg().ProduceE2, "cfg", s.Cfg())
+	s.logger.Info("ye...", "im", s.IndicesMax(), "segmax", s.SegmentsMax(), "producee2", !s.Cfg().ProduceE2, "cfg", s.Cfg().String())
 	if s.IndicesMax() >= s.SegmentsMax() {
 		return nil
 	}
