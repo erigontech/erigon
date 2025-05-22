e0d1d65466e49212de0c5a6682bd76781a32d1ea
alex.sharov <AskAlexSharov@gmail.com>
2025-05-22 13:59:50 +0700

    save

--- cmd/rpcdaemon/rpcservices/eth_backend.go
+++ cmd/rpcdaemon/rpcservices/eth_backend.go
@@ -17,7 +17,6 @@
 package rpcservices
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
 	"errors"
@@ -205,7 +204,7 @@ func (back *RemoteBackend) PendingBlock(ctx context.Context) (*types.Block, erro
 	}
 
 	var block types.Block
-	err = rlp.Decode(bytes.NewReader(blockRlp.BlockRlp), &block)
+	err = rlp.DecodeBytes(blockRlp.BlockRlp, &block)
 	if err != nil {
 		return nil, fmt.Errorf("decoding block from %x: %w", blockRlp.BlockRlp, err)
 	}
