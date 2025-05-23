081eec22dc0d49f92c7f8ccfa0cd986738584d1b
alex.sharov <AskAlexSharov@gmail.com>
2025-05-23 20:05:32 +0700

    save

--- rpc/jsonrpc/eth_receipts.go
+++ rpc/jsonrpc/eth_receipts.go
@@ -312,6 +312,7 @@ func (api *BaseAPI) getLogsV3(ctx context.Context, tx kv.TemporalTx, begin, end 
 		filtered := r.Logs.Filter(addrMap, crit.Topics, 0)
 
 		for _, filteredLog := range filtered {
+			log.Warn("[dbg] bn", "bn", blockNum, "tx", filteredLog.TxIndex, "li", filteredLog.Index)
 			logs = append(logs, &types.ErigonLog{
 				Address:     filteredLog.Address,
 				Topics:      filteredLog.Topics,
