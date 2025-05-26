9612978478c0161eaba635cdb939fce519cbc948
alex.sharov <AskAlexSharov@gmail.com>
2025-05-26 22:29:39 +0700

    save

--- wmake.ps1
+++ wmake.ps1
@@ -397,7 +397,7 @@ if (!Test-Path -Path [string](Join-Path $MyContext.Directory "\.git") -PathType 
   Error !
   Directory $MyContext.Directory does not seem to be a properly cloned Erigon repository
   Please clone it using 
-  git clone --recurse-submodules -j8 https://github.com/erigontech/erigon.git
+  git clone -j8 https://github.com/erigontech/erigon.git
 
 "@
     exit 1
