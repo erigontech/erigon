52e3973ecea4417e5dacbc41da3c554482f68f17
sudeepdino008 <sudeepdino008@gmail.com>
2025-05-26 15:42:34 +0530

    save

--- erigon-lib/state/gc_test.go
+++ erigon-lib/state/gc_test.go
@@ -148,6 +148,13 @@ func TestDomainGCReadAfterRemoveFile(t *testing.T) {
 			lastOnFs, _ := h.dirtyFiles.Max()
 			require.False(lastOnFs.frozen) // prepared dataset must have some non-frozen files. or it's bad dataset.
 			h.integrateMergedDirtyFiles([]*filesItem{lastOnFs}, nil, nil, nil, nil, nil)
+
+			{
+				hc2 := h.BeginFilesRo()
+				hc2.cleanAfterMerge(lastOnFs, nil, nil)
+				hc2.Close()
+			}
+
 			require.NotNil(lastOnFs.decompressor)
 			h.reCalcVisibleFiles(h.dirtyFilesEndTxNumMinimax())
 
@@ -189,6 +196,11 @@ func TestDomainGCReadAfterRemoveFile(t *testing.T) {
 			lastOnFs, _ := h.dirtyFiles.Max()
 			require.False(lastOnFs.frozen) // prepared dataset must have some non-frozen files. or it's bad dataset.
 			h.integrateMergedDirtyFiles([]*filesItem{lastOnFs}, nil, nil, nil, nil, nil)
+			{
+				hc2 := h.BeginFilesRo()
+				hc2.cleanAfterMerge(lastOnFs, nil, nil)
+				hc2.Close()
+			}
 			h.reCalcVisibleFiles(h.dirtyFilesEndTxNumMinimax())
 
 			require.NotNil(lastOnFs.decompressor)
