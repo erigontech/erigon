c81f623ee25708477d4ea53d09f2ed4fc1608395
kewei <kewei.train@gmail.com>
2025-05-27 17:24:31 +0700

    update

--- cl/rpc/rpc.go
+++ cl/rpc/rpc.go
@@ -217,6 +217,62 @@ func (b *BeaconRpcP2P) sendBlobsSidecar(ctx context.Context, topic string, reqDa
 	return responsePacket, message.Peer.Pid, nil
 }
 
+func (b *BeaconRpcP2P) SendColumnSidecarsByRootIdentifierReq(
+	ctx context.Context,
+	req *solid.ListSSZ[*cltypes.DataColumnsByRootIdentifier],
+) ([]*cltypes.DataColumnSidecar, string, error) {
+	var buffer buffer.Buffer
+	if err := ssz_snappy.EncodeAndWrite(&buffer, req); err != nil {
+		return nil, "", err
+	}
+
+	responsePacket, pid, err := b.sendRequest(ctx, communication.DataColumnSidecarsByRootProtocolV1, buffer.Bytes(), uint64(req.Len()))
+	if err != nil {
+		return nil, pid, err
+	}
+
+	ColumnSidecars := []*cltypes.DataColumnSidecar{}
+	for _, data := range responsePacket {
+		columnSidecar := &cltypes.DataColumnSidecar{}
+		if err := columnSidecar.DecodeSSZ(data.raw, data.version); err != nil {
+			return nil, pid, err
+		}
+		ColumnSidecars = append(ColumnSidecars, columnSidecar)
+	}
+	return ColumnSidecars, pid, nil
+}
+
+func (b *BeaconRpcP2P) SendColumnSidecarsByRangeReqV1(
+	ctx context.Context,
+	start, count uint64,
+	columns []uint64,
+) ([]*cltypes.DataColumnSidecar, string, error) {
+	req := &cltypes.ColumnSidecarsByRangeRequest{
+		StartSlot: start,
+		Count:     count,
+		Columns:   *solid.NewListSSZUint64(columns),
+	}
+	var buffer buffer.Buffer
+	if err := ssz_snappy.EncodeAndWrite(&buffer, req); err != nil {
+		return nil, "", err
+	}
+
+	responsePacket, pid, err := b.sendRequest(ctx, communication.DataColumnSidecarsByRangeProtocolV1, buffer.Bytes(), count)
+	if err != nil {
+		return nil, pid, err
+	}
+
+	ColumnSidecars := []*cltypes.DataColumnSidecar{}
+	for _, data := range responsePacket {
+		columnSidecar := &cltypes.DataColumnSidecar{}
+		if err := columnSidecar.DecodeSSZ(data.raw, data.version); err != nil {
+			return nil, pid, err
+		}
+		ColumnSidecars = append(ColumnSidecars, columnSidecar)
+	}
+	return ColumnSidecars, pid, nil
+}
+
 // SendBeaconBlocksByRangeReq retrieves blocks range from beacon chain.
 func (b *BeaconRpcP2P) SendBlobsSidecarByIdentifierReq(ctx context.Context, req *solid.ListSSZ[*cltypes.BlobIdentifier]) ([]*cltypes.BlobSidecar, string, error) {
 	var buffer buffer.Buffer
@@ -298,4 +354,85 @@ func (b *BeaconRpcP2P) SetStatus(finalizedRoot common.Hash, finalizedEpoch uint6
 
 func (b *BeaconRpcP2P) BanPeer(pid string) {
 	b.sentinel.BanPeer(b.ctx, &sentinel.Peer{Pid: pid})
+}
+
+// responseData is a helper struct to store the version and the raw data of the response for each data container.
+type responseData struct {
+	version int
+	raw     []byte
+}
+
+// sendRequest sends a request to the sentinel and helps with decoding the response.
+func (b *BeaconRpcP2P) sendRequest(
+	ctx context.Context,
+	topic string,
+	reqPayload []byte,
+	dataCount uint64,
+) ([]responseData, string, error) {
+	ctx, cn := context.WithTimeout(ctx, time.Second*2)
+	defer cn()
+	message, err := b.sentinel.SendRequest(ctx, &sentinel.RequestData{
+		Data:  reqPayload,
+		Topic: topic,
+	})
+	if err != nil {
+		return nil, "", err
+	}
+	if message.Error {
+		rd := snappy.NewReader(bytes.NewBuffer(message.Data))
+		errBytes, _ := io.ReadAll(rd)
+		log.Trace("received range req error", "err", string(errBytes), "raw", string(message.Data))
+		return nil, message.Peer.Pid, nil
+	}
+
+	responsePacket := []responseData{}
+	r := bytes.NewReader(message.Data)
+	for i := 0; i < int(dataCount); i++ {
+		forkDigest := make([]byte, 4)
+		if _, err := r.Read(forkDigest); err != nil {
+			if err == io.EOF {
+				break
+			}
+			return nil, message.Peer.Pid, err
+		}
+
+		// Read varint for length of message.
+		encodedLn, _, err := ssz_snappy.ReadUvarint(r)
+		if err != nil {
+			return nil, message.Peer.Pid, fmt.Errorf("unable to read varint from message prefix: %w", err)
+		}
+		// Sanity check for message size.
+		if encodedLn > uint64(maxMessageLength) {
+			return nil, message.Peer.Pid, errors.New("received message too big")
+		}
+
+		// Read bytes using snappy into a new raw buffer of side encodedLn.
+		raw := make([]byte, encodedLn)
+		sr := snappy.NewReader(r)
+		bytesRead := 0
+		for bytesRead < int(encodedLn) {
+			n, err := sr.Read(raw[bytesRead:])
+			if err != nil {
+				return nil, message.Peer.Pid, fmt.Errorf("read error: %w", err)
+			}
+			bytesRead += n
+		}
+		// Fork digests
+		respForkDigest := binary.BigEndian.Uint32(forkDigest)
+		if respForkDigest == 0 {
+			return nil, message.Peer.Pid, errors.New("null fork digest")
+		}
+
+		version, err := b.ethClock.StateVersionByForkDigest(utils.Uint32ToBytes4(respForkDigest))
+		if err != nil {
+			return nil, message.Peer.Pid, err
+		}
+		responsePacket = append(responsePacket, responseData{
+			version: int(version),
+			raw:     raw,
+		})
+		// TODO(issues/5884): figure out why there is this extra byte.
+		r.ReadByte()
+	}
+	return responsePacket, message.Peer.Pid, nil
 }
