b605ef07c1285d01e1ac08311074f003401ff2ec
Giulio rebuffo <giulio.rebuffo@gmail.com>
2025-05-27 14:16:16 +0000

    Engine-API: Fixed `context canceled` (#15277)
    also skip making a goroutine in the average case

--- turbo/snapshotsync/snapshots.go
+++ turbo/snapshotsync/snapshots.go
@@ -674,7 +674,7 @@ func (s *RoSnapshots) HasType(in snaptype.Type) bool {
 type ready struct {
 	mu     sync.Mutex
 	on     chan struct{}
-	state  bool
+	state  atomic.Bool
 	inited bool
 }
 
@@ -697,15 +697,21 @@ func (r *ready) set() {
 	r.mu.Lock()
 	defer r.mu.Unlock()
 	r.init()
-	if r.state {
+	if r.state.Load() {
 		return
 	}
-	r.state = true
-	close(r.on)
+	if r.state.CompareAndSwap(false, true) {
+		close(r.on)
+	}
 }
 
 func (s *RoSnapshots) Ready(ctx context.Context) <-chan error {
 	errc := make(chan error)
+
+	if s.ready.state.Load() {
+		close(errc)
+		return errc
+	}
 
 	go func() {
 		select {
