erigontech/erigon

commit 836ce249ac1a64d4e01769849a67231ded272a06

GetLatest: single key param (#13022)

erigon-lib/kv/kv_interface.go
@@ -458,7 +458,7 @@ type (
 )
 
 type TemporalGetter interface {
-	GetLatest(name Domain, k, k2 []byte) (v []byte, step uint64, err error)
+	GetLatest(name Domain, k []byte) (v []byte, step uint64, err error)
 }
 type TemporalTx interface {
 	Tx

commit dd794510c1b260c09b98bca852ecf4467d8435c7

kv/temporal: fixes and tests for RangeAsOf and HasPrefix (#14826)
part of https://github.com/erigontech/erigon/issues/11075
follow up after https://github.com/erigontech/erigon/pull/14739

- adds unit tests for `TemporalTx.HasPrefix`, `SharedDomains.HasPrefix`
and `TemporalTx.RangeAsOf` (it will be used by `HistoryReaderV3` to
implement historical `HasStorage`)
- adds a `firstVal` return value to `HasPrefix` interface since it will
be used in `ReaderParallelV3` (I missed this in earlier PR)
- fixes a regression in `SharedDomains.DomainPut`and
`SharedDomains.DomainDel` which was found in `RangeAsOf` tests - the
issues is that when calling `sd.GetLatest` to get the `prevVal` and
`prevStep` needed for the history writers we didn't pass a composite key
(problematic only for the Storage domain) to `GetLatest` which caused
empty values to be returned for `prevVal` and `prevStep` - since those
are used by the history writers I think this might cause wrong
historical values for the Storage domain - this regression was probably
introduced in December 2024 by
https://github.com/erigontech/erigon/commit/836ce249ac1a64d4e01769849a67231ded272a06

---------

Co-authored-by: alex.sharov <AskAlexSharov@gmail.com>

erigon-lib/kv/kv_interface.go
@@ -541,7 +541,7 @@ type (
 
 type TemporalGetter interface {
 	GetLatest(name Domain, k []byte) (v []byte, step uint64, err error)
-	HasPrefix(name Domain, prefix []byte) (firstKey []byte, ok bool, err error)
+	HasPrefix(name Domain, prefix []byte) (firstKey []byte, firstVal []byte, hasPrefix bool, err error)
 }
 type TemporalTx interface {
 	Tx

erigon-lib/kv/membatchwithdb/memory_mutation.go
@@ -735,7 +735,7 @@ func (m *MemoryMutation) GetAsOf(name kv.Domain, k []byte, ts uint64) (v []byte,
 	return m.db.(kv.TemporalTx).GetAsOf(name, k, ts)
 }
 
-func (m *MemoryMutation) HasPrefix(name kv.Domain, prefix []byte) (firstKey []byte, ok bool, err error) {
+func (m *MemoryMutation) HasPrefix(name kv.Domain, prefix []byte) ([]byte, []byte, bool, error) {
 	return m.db.(kv.TemporalTx).HasPrefix(name, prefix)
 }
 

erigon-lib/kv/remotedb/kv_remote.go
@@ -667,7 +667,7 @@ func (tx *tx) GetLatest(name kv.Domain, k []byte) (v []byte, step uint64, err er
 	return reply.V, 0, nil
 }
 
-func (tx *tx) HasPrefix(domain kv.Domain, prefix []byte) (firstKey []byte, ok bool, err error) {
+func (tx *tx) HasPrefix(name kv.Domain, prefix []byte) ([]byte, []byte, bool, error) {
 	//TODO implement me
 	panic("implement me")
 }

erigon-lib/kv/temporal/kv_temporal.go
@@ -389,31 +389,36 @@ func (tx *tx) getLatest(name kv.Domain, dbTx kv.Tx, k []byte) (v []byte, step ui
 	return v, step, err
 }
 
-func (tx *Tx) HasPrefix(name kv.Domain, prefix []byte) ([]byte, bool, error) {
+func (tx *Tx) HasPrefix(name kv.Domain, prefix []byte) ([]byte, []byte, bool, error) {
 	return tx.hasPrefix(name, tx.Tx, prefix)
 }
 
-func (tx *RwTx) HasPrefix(name kv.Domain, prefix []byte) ([]byte, bool, error) {
+func (tx *RwTx) HasPrefix(name kv.Domain, prefix []byte) ([]byte, []byte, bool, error) {
 	return tx.hasPrefix(name, tx.RwTx, prefix)
 }
 
-func (tx *tx) hasPrefix(name kv.Domain, dbTx kv.Tx, prefix []byte) ([]byte, bool, error) {
-	it, err := tx.rangeLatest(name, dbTx, prefix, nil, 1)
+func (tx *tx) hasPrefix(name kv.Domain, dbTx kv.Tx, prefix []byte) ([]byte, []byte, bool, error) {
+	to, ok := kv.NextSubtree(prefix)
+	if !ok {
+		to = nil
+	}
+
+	it, err := tx.rangeLatest(name, dbTx, prefix, to, 1)
 	if err != nil {
-		return nil, false, err
+		return nil, nil, false, err
 	}
 
 	defer it.Close()
 	if !it.HasNext() {
-		return nil, false, nil
+		return nil, nil, false, nil
 	}
 
-	k, _, err := it.Next()
+	k, v, err := it.Next()
 	if err != nil {
-		return nil, false, err
+		return nil, nil, false, err
 	}
 
-	return k, true, nil
+	return k, v, true, nil
 }
 
 func (tx *Tx) GetLatest(name kv.Domain, k []byte) (v []byte, step uint64, err error) {

erigon-lib/state/domain_shared.go
@@ -160,7 +160,7 @@ func (gt *temporalGetter) GetLatest(name kv.Domain, k []byte) (v []byte, step ui
 	return gt.sd.GetLatest(name, gt.tx, k)
 }
 
-func (gt *temporalGetter) HasPrefix(name kv.Domain, prefix []byte) (firstKey []byte, ok bool, err error) {
+func (gt *temporalGetter) HasPrefix(name kv.Domain, prefix []byte) (firstKey []byte, firstVal []byte, ok bool, err error) {
 	return gt.sd.HasPrefix(name, prefix, gt.tx)
 }
 
@@ -413,15 +413,16 @@ func (sd *SharedDomains) SetTrace(b bool) {
 	sd.trace = b
 }
 
-func (sd *SharedDomains) HasPrefix(domain kv.Domain, prefix []byte, roTx kv.Tx) ([]byte, bool, error) {
-	var firstKey []byte
+func (sd *SharedDomains) HasPrefix(domain kv.Domain, prefix []byte, roTx kv.Tx) ([]byte, []byte, bool, error) {
+	var firstKey, firstVal []byte
 	var hasPrefix bool
 	err := sd.IteratePrefix(domain, prefix, sd.txNum, roTx, func(k []byte, v []byte, step uint64) (bool, error) {
 		firstKey = common.CopyBytes(k)
+		firstVal = common.CopyBytes(v)
 		hasPrefix = true
 		return false, nil // do not continue, end on first occurrence
 	})
-	return firstKey, hasPrefix, err
+	return firstKey, firstVal, hasPrefix, err
 }
 
 // IterateStoragePrefix iterates over key-value pairs of the storage domain that start with given prefix

erigon-lib/kv/temporal/kv_temporal_test.go
@@ -1,0 +1,354 @@
+package temporal
+
+import (
+	"context"
+	"encoding/binary"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/erigontech/erigon-lib/common"
+	"github.com/erigontech/erigon-lib/common/datadir"
+	"github.com/erigontech/erigon-lib/kv"
+	"github.com/erigontech/erigon-lib/kv/memdb"
+	"github.com/erigontech/erigon-lib/kv/order"
+	"github.com/erigontech/erigon-lib/log/v3"
+	"github.com/erigontech/erigon-lib/state"
+)
+
+func TestTemporalTx_HasPrefix_StorageDomain(t *testing.T) {
+	t.Parallel()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	t.Cleanup(cancel)
+
+	logger := log.New()
+	logger.SetHandler(log.LvlFilterHandler(log.LvlCrit, log.StderrHandler))
+
+	mdbxDb := memdb.NewTestDB(t, kv.ChainDB)
+	dirs := datadir.New(t.TempDir())
+	_, err := state.GetStateIndicesSalt(dirs, true /* genNew */, logger) // gen salt needed by aggregator
+	require.NoError(t, err)
+	aggStep := uint64(1)
+	agg, err := state.NewAggregator(ctx, dirs, aggStep, mdbxDb, logger)
+	require.NoError(t, err)
+	t.Cleanup(agg.Close)
+	temporalDb, err := New(mdbxDb, agg)
+	require.NoError(t, err)
+	t.Cleanup(temporalDb.Close)
+
+	rwTtx1, err := temporalDb.BeginTemporalRw(ctx)
+	require.NoError(t, err)
+	t.Cleanup(rwTtx1.Rollback)
+	sd, err := state.NewSharedDomains(rwTtx1, logger)
+	require.NoError(t, err)
+	t.Cleanup(sd.Close)
+
+	acc1 := common.HexToAddress("0x1234567890123456789012345678901234567890")
+	acc1slot1 := common.HexToHash("0x0000000000000000000000000000000000000000000000000000000000000001")
+	storageK1 := append(append([]byte{}, acc1.Bytes()...), acc1slot1.Bytes()...)
+	acc2 := common.HexToAddress("0x1234567890123456789012345678901234567891")
+	acc2slot2 := common.HexToHash("0x0000000000000000000000000000000000000000000000000000000000000002")
+	storageK2 := append(append([]byte{}, acc2.Bytes()...), acc2slot2.Bytes()...)
+
+	// --- check 1: non-existing storage ---
+	{
+		firstKey, firstVal, ok, err := rwTtx1.HasPrefix(kv.StorageDomain, acc1.Bytes())
+		require.NoError(t, err)
+		require.False(t, ok)
+		require.Nil(t, firstKey)
+		require.Nil(t, firstVal)
+	}
+
+	// --- check 2: storage exists in DB - TemporalTx.HasPrefix should catch this ---
+	{
+		// write to storage
+		err = sd.DomainPut(kv.StorageDomain, rwTtx1, storageK1, []byte{1}, 1, nil, 0)
+		require.NoError(t, err)
+		err = sd.Flush(ctx, rwTtx1)
+		require.NoError(t, err)
+		err = rwTtx1.Commit()
+		require.NoError(t, err)
+
+		// make sure it is indeed in db using a db tx
+		dbRoTx1, err := mdbxDb.BeginRo(ctx)
+		require.NoError(t, err)
+		t.Cleanup(dbRoTx1.Rollback)
+		c1, err := dbRoTx1.CursorDupSort(kv.TblStorageVals)
+		require.NoError(t, err)
+		t.Cleanup(c1.Close)
+		k, v, err := c1.Next()
+		require.NoError(t, err)
+		require.Equal(t, append(append([]byte{}, acc1.Bytes()...), acc1slot1.Bytes()...), k)
+		wantValueBytes := make([]byte, 8)                      // 8 bytes for uint64 step num
+		binary.BigEndian.PutUint64(wantValueBytes, ^uint64(1)) // step num
+		wantValueBytes = append(wantValueBytes, byte(1))       // value we wrote to the storage slot
+		require.Equal(t, wantValueBytes, v)
+		k, v, err = c1.Next()
+		require.NoError(t, err)
+		require.Nil(t, k)
+		require.Nil(t, v)
+
+		// all good
+		// now move on to temporal tx
+		roTtx1, err := temporalDb.BeginTemporalRo(ctx)
+		require.NoError(t, err)
+		t.Cleanup(roTtx1.Rollback)
+
+		// make sure there are no files yet and we are only hitting the DB
+		require.Equal(t, uint64(0), roTtx1.Debug().TxNumsInFiles(kv.StorageDomain))
+
+		// finally, verify TemporalTx.HasPrefix returns true
+		firstKey, firstVal, ok, err := roTtx1.HasPrefix(kv.StorageDomain, acc1.Bytes())
+		require.NoError(t, err)
+		require.True(t, ok)
+		require.Equal(t, append(append([]byte{}, acc1.Bytes()...), acc1slot1.Bytes()...), firstKey)
+		require.Equal(t, []byte{1}, firstVal)
+
+		// check some other non-existing storages for non-existence after write operation
+		firstKey, firstVal, ok, err = roTtx1.HasPrefix(kv.StorageDomain, acc2.Bytes())
+		require.NoError(t, err)
+		require.False(t, ok)
+		require.Nil(t, firstKey)
+		require.Nil(t, firstVal)
+	}
+
+	// --- check 3: storage exists in files only - TemporalTx.HasPrefix should catch this
+	{
+		// move data to files and trigger prune (need one more step for prune so write to some other storage)
+		rwTtx2, err := temporalDb.BeginTemporalRw(ctx)
+		require.NoError(t, err)
+		t.Cleanup(rwTtx2.Rollback)
+		err = sd.DomainPut(kv.StorageDomain, rwTtx2, storageK2, []byte{2}, 2, nil, 0)
+		require.NoError(t, err)
+		err = sd.Flush(ctx, rwTtx2)
+		require.NoError(t, err)
+		err = rwTtx2.Commit()
+		require.NoError(t, err)
+
+		// build files
+		err = agg.BuildFiles(2)
+		require.NoError(t, err)
+		rwTtx3, err := temporalDb.BeginTemporalRw(ctx)
+		require.NoError(t, err)
+		t.Cleanup(rwTtx3.Rollback)
+
+		// prune
+		haveMore, err := rwTtx3.PruneSmallBatches(ctx, time.Minute)
+		require.NoError(t, err)
+		require.False(t, haveMore)
+		err = rwTtx3.Commit()
+		require.NoError(t, err)
+
+		// double check acc1 storage data not in the mdbx DB
+		dbRoTx2, err := mdbxDb.BeginRo(ctx)
+		require.NoError(t, err)
+		t.Cleanup(dbRoTx2.Rollback)
+		c2, err := dbRoTx2.CursorDupSort(kv.TblStorageVals)
+		require.NoError(t, err)
+		t.Cleanup(c2.Close)
+		k, v, err := c2.Next() // acc2 storage from step 2 will be there
+		require.NoError(t, err)
+		require.Equal(t, append(append([]byte{}, acc2.Bytes()...), acc2slot2.Bytes()...), k)
+		wantValueBytes := make([]byte, 8)                      // 8 bytes for uint64 step num
+		binary.BigEndian.PutUint64(wantValueBytes, ^uint64(2)) // step num
+		wantValueBytes = append(wantValueBytes, byte(2))       // value we wrote to the storage slot
+		require.Equal(t, wantValueBytes, v)
+		k, v, err = c2.Next() // acc1 storage from step 1 must not be there
+		require.NoError(t, err)
+		require.Nil(t, k)
+		require.Nil(t, v)
+
+		// double check files for 2 steps have been created
+		roTtx2, err := temporalDb.BeginTemporalRo(ctx)
+		require.NoError(t, err)
+		t.Cleanup(roTtx2.Rollback)
+		require.Equal(t, uint64(2), roTtx2.Debug().TxNumsInFiles(kv.StorageDomain))
+
+		// finally, verify TemporalTx.HasPrefix returns true
+		firstKey, firstVal, ok, err := roTtx2.HasPrefix(kv.StorageDomain, acc1.Bytes())
+		require.NoError(t, err)
+		require.True(t, ok)
+		require.Equal(t, append(append([]byte{}, acc1.Bytes()...), acc1slot1.Bytes()...), firstKey)
+		require.Equal(t, []byte{1}, firstVal)
+	}
+
+	// --- check 4: delete storage - TemporalTx.HasPrefix should catch this and say it does not exist
+	{
+		rwTtx4, err := temporalDb.BeginTemporalRw(ctx)
+		require.NoError(t, err)
+		t.Cleanup(rwTtx4.Rollback)
+		err = sd.DomainDelPrefix(kv.StorageDomain, rwTtx4, acc1.Bytes(), 3)
+		require.NoError(t, err)
+		err = sd.Flush(ctx, rwTtx4)
+		require.NoError(t, err)
+		err = rwTtx4.Commit()
+		require.NoError(t, err)
+
+		roTtx3, err := temporalDb.BeginTemporalRo(ctx)
+		require.NoError(t, err)
+		t.Cleanup(roTtx3.Rollback)
+
+		firstKey, firstVal, ok, err := roTtx3.HasPrefix(kv.StorageDomain, acc1.Bytes())
+		require.NoError(t, err)
+		require.False(t, ok)
+		require.Nil(t, firstKey)
+		require.Nil(t, firstVal)
+	}
+
+	// --- check 5: write to it again after deletion - TemporalTx.HasPrefix should catch
+	{
+		rwTtx5, err := temporalDb.BeginTemporalRw(ctx)
+		require.NoError(t, err)
+		t.Cleanup(rwTtx5.Rollback)
+		err = sd.DomainPut(kv.StorageDomain, rwTtx5, storageK1, []byte{3}, 4, nil, 0)
+		require.NoError(t, err)
+		err = sd.Flush(ctx, rwTtx5)
+		require.NoError(t, err)
+		err = rwTtx5.Commit()
+		require.NoError(t, err)
+
+		roTtx4, err := temporalDb.BeginTemporalRo(ctx)
+		require.NoError(t, err)
+		t.Cleanup(roTtx4.Rollback)
+
+		firstKey, firstVal, ok, err := roTtx4.HasPrefix(kv.StorageDomain, acc1.Bytes())
+		require.NoError(t, err)
+		require.True(t, ok)
+		require.Equal(t, append(append([]byte{}, acc1.Bytes()...), acc1slot1.Bytes()...), firstKey)
+		require.Equal(t, []byte{3}, firstVal)
+	}
+}
+
+func TestTemporalTx_RangeAsOf_StorageDomain(t *testing.T) {
+	t.Parallel()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	t.Cleanup(cancel)
+
+	logger := log.New()
+	logger.SetHandler(log.LvlFilterHandler(log.LvlCrit, log.StderrHandler))
+
+	mdbxDb := memdb.NewTestDB(t, kv.ChainDB)
+	dirs := datadir.New(t.TempDir())
+	_, err := state.GetStateIndicesSalt(dirs, true /* genNew */, logger) // gen salt needed by aggregator
+	require.NoError(t, err)
+	aggStep := uint64(1)
+	agg, err := state.NewAggregator(ctx, dirs, aggStep, mdbxDb, logger)
+	require.NoError(t, err)
+	t.Cleanup(agg.Close)
+	temporalDb, err := New(mdbxDb, agg)
+	require.NoError(t, err)
+	t.Cleanup(temporalDb.Close)
+
+	// empty range when nothing has been written yet
+	acc1 := common.HexToAddress("0x1234567890123456789012345678901234567890")
+	acc1slot1 := common.HexToHash("0x0000000000000000000000000000000000000000000000000000000000000001")
+	storageK1 := append(append([]byte{}, acc1.Bytes()...), acc1slot1.Bytes()...)
+	nextSubTree, ok := kv.NextSubtree(acc1.Bytes())
+	require.True(t, ok)
+
+	// write storage at txn num 1, update it at txn num 2, then delete it at txn num 3, then write to it again
+	// txn num 1
+	rwTtx1, err := temporalDb.BeginTemporalRw(ctx)
+	require.NoError(t, err)
+	t.Cleanup(rwTtx1.Rollback)
+	sd, err := state.NewSharedDomains(rwTtx1, logger)
+	require.NoError(t, err)
+	t.Cleanup(sd.Close)
+	err = sd.DomainPut(kv.StorageDomain, rwTtx1, storageK1, []byte{1}, 1, nil, 0)
+	require.NoError(t, err)
+	err = sd.Flush(ctx, rwTtx1)
+	require.NoError(t, err)
+	err = rwTtx1.Commit()
+	require.NoError(t, err)
+	// txn num 2
+	rwTtx2, err := temporalDb.BeginTemporalRw(ctx)
+	require.NoError(t, err)
+	t.Cleanup(rwTtx2.Rollback)
+	err = sd.DomainPut(kv.StorageDomain, rwTtx2, storageK1, []byte{2}, 2, nil, 0)
+	require.NoError(t, err)
+	err = sd.Flush(ctx, rwTtx2)
+	require.NoError(t, err)
+	err = rwTtx2.Commit()
+	require.NoError(t, err)
+	// txn num 3
+	rwTtx3, err := temporalDb.BeginTemporalRw(ctx)
+	require.NoError(t, err)
+	t.Cleanup(rwTtx3.Rollback)
+	err = sd.DomainDelPrefix(kv.StorageDomain, rwTtx3, acc1.Bytes(), 3)
+	require.NoError(t, err)
+	err = sd.Flush(ctx, rwTtx3)
+	require.NoError(t, err)
+	err = rwTtx3.Commit()
+	require.NoError(t, err)
+	// txn num 4
+	rwTtx4, err := temporalDb.BeginTemporalRw(ctx)
+	require.NoError(t, err)
+	t.Cleanup(rwTtx4.Rollback)
+	err = sd.DomainPut(kv.StorageDomain, rwTtx4, storageK1, []byte{3}, 4, nil, 0)
+	require.NoError(t, err)
+	err = sd.Flush(ctx, rwTtx4)
+	require.NoError(t, err)
+	err = rwTtx4.Commit()
+	require.NoError(t, err)
+
+	// empty value at txn 0
+	roTtx1, err := temporalDb.BeginTemporalRo(ctx)
+	require.NoError(t, err)
+	t.Cleanup(roTtx1.Rollback)
+	it1, err := roTtx1.RangeAsOf(kv.StorageDomain, acc1.Bytes(), nextSubTree, 1, order.Asc, kv.Unlim)
+	require.NoError(t, err)
+	t.Cleanup(it1.Close)
+	require.True(t, it1.HasNext())
+	k, v, err := it1.Next()
+	require.NoError(t, err)
+	require.Equal(t, append(append([]byte{}, acc1.Bytes()...), acc1slot1.Bytes()...), k)
+	require.Len(t, v, 0)
+	require.False(t, it1.HasNext())
+
+	// value 1 at txn num 1
+	it2, err := roTtx1.RangeAsOf(kv.StorageDomain, acc1.Bytes(), nextSubTree, 2, order.Asc, kv.Unlim)
+	require.NoError(t, err)
+	t.Cleanup(it2.Close)
+	require.True(t, it2.HasNext())
+	k, v, err = it2.Next()
+	require.NoError(t, err)
+	require.Equal(t, append(append([]byte{}, acc1.Bytes()...), acc1slot1.Bytes()...), k)
+	require.Equal(t, []byte{1}, v)
+	require.False(t, it2.HasNext())
+
+	// value 2 at txn num 2
+	it3, err := roTtx1.RangeAsOf(kv.StorageDomain, acc1.Bytes(), nextSubTree, 3, order.Asc, kv.Unlim)
+	require.NoError(t, err)
+	t.Cleanup(it3.Close)
+	require.True(t, it3.HasNext())
+	k, v, err = it3.Next()
+	require.NoError(t, err)
+	require.Equal(t, append(append([]byte{}, acc1.Bytes()...), acc1slot1.Bytes()...), k)
+	require.Equal(t, []byte{2}, v)
+	require.False(t, it3.HasNext())
+
+	// empty value at txn num 3
+	it4, err := roTtx1.RangeAsOf(kv.StorageDomain, acc1.Bytes(), nextSubTree, 4, order.Asc, kv.Unlim)
+	require.NoError(t, err)
+	t.Cleanup(it4.Close)
+	require.True(t, it4.HasNext())
+	k, v, err = it4.Next()
+	require.NoError(t, err)
+	require.Equal(t, append(append([]byte{}, acc1.Bytes()...), acc1slot1.Bytes()...), k)
+	require.Len(t, v, 0)
+	require.False(t, it4.HasNext())
+
+	// value 3 at txn num 4 - note under the hood this will use latest vals instead of historical
+	it5, err := roTtx1.RangeAsOf(kv.StorageDomain, acc1.Bytes(), nextSubTree, 5, order.Asc, kv.Unlim)
+	require.NoError(t, err)
+	t.Cleanup(it5.Close)
+	require.True(t, it5.HasNext())
+	k, v, err = it5.Next()
+	require.NoError(t, err)
+	require.Equal(t, append(append([]byte{}, acc1.Bytes()...), acc1slot1.Bytes()...), k)
+	require.Equal(t, []byte{3}, v)
+	require.False(t, it5.HasNext())
+}

erigon-lib/state/domain_shared_test.go
@@ -27,8 +27,10 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/erigontech/erigon-lib/common"
+	"github.com/erigontech/erigon-lib/common/datadir"
 	"github.com/erigontech/erigon-lib/common/length"
 	"github.com/erigontech/erigon-lib/kv"
+	"github.com/erigontech/erigon-lib/kv/memdb"
 	"github.com/erigontech/erigon-lib/kv/rawdbv3"
 	"github.com/erigontech/erigon-lib/log/v3"
 	accounts3 "github.com/erigontech/erigon-lib/types/accounts"
@@ -524,4 +526,242 @@ func TestSharedDomain_StorageIter(t *testing.T) {
 	err = domains.Flush(ctx, rwTx)
 	require.NoError(t, err)
 	rwTx.Rollback()
+}
+
+func TestSharedDomain_HasPrefix_StorageDomain(t *testing.T) {
+	t.Parallel()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	t.Cleanup(cancel)
+
+	logger := log.New()
+	logger.SetHandler(log.LvlFilterHandler(log.LvlTrace, log.StderrHandler))
+
+	mdbxDb := memdb.NewTestDB(t, kv.ChainDB)
+	dirs := datadir.New(t.TempDir())
+	_, err := GetStateIndicesSalt(dirs, true /* genNew */, logger) // gen salt needed by aggregator
+	require.NoError(t, err)
+	aggStep := uint64(1)
+	agg, err := NewAggregator(ctx, dirs, aggStep, mdbxDb, logger)
+	require.NoError(t, err)
+	t.Cleanup(agg.Close)
+	temporalDb, err := New(mdbxDb, agg)
+	require.NoError(t, err)
+	t.Cleanup(temporalDb.Close)
+
+	rwTtx1, err := temporalDb.BeginTemporalRw(ctx)
+	require.NoError(t, err)
+	t.Cleanup(rwTtx1.Rollback)
+	sd, err := NewSharedDomains(rwTtx1, logger)
+	require.NoError(t, err)
+	t.Cleanup(sd.Close)
+
+	acc1 := common.HexToAddress("0x1234567890123456789012345678901234567890")
+	acc1slot1 := common.HexToHash("0x0000000000000000000000000000000000000000000000000000000000000001")
+	storageK1 := append(append([]byte{}, acc1.Bytes()...), acc1slot1.Bytes()...)
+	acc2 := common.HexToAddress("0x1234567890123456789012345678901234567891")
+	acc2slot2 := common.HexToHash("0x0000000000000000000000000000000000000000000000000000000000000002")
+	storageK2 := append(append([]byte{}, acc2.Bytes()...), acc2slot2.Bytes()...)
+
+	// --- check 1: non-existing storage ---
+	{
+		firstKey, firstVal, ok, err := sd.HasPrefix(kv.StorageDomain, acc1.Bytes(), rwTtx1)
+		require.NoError(t, err)
+		require.False(t, ok)
+		require.Nil(t, firstKey)
+		require.Nil(t, firstVal)
+	}
+
+	// --- check 2: storage exists in DB - SharedDomains.HasPrefix should catch this ---
+	{
+		// write to storage
+		err = sd.DomainPut(kv.StorageDomain, rwTtx1, storageK1, []byte{1}, 1, nil, 0)
+		require.NoError(t, err)
+		// check before flush
+		firstKey, firstVal, ok, err := sd.HasPrefix(kv.StorageDomain, acc1.Bytes(), rwTtx1)
+		require.NoError(t, err)
+		require.True(t, ok)
+		require.Equal(t, append(append([]byte{}, acc1.Bytes()...), acc1slot1.Bytes()...), firstKey)
+		require.Equal(t, []byte{1}, firstVal)
+		// check after flush
+		err = sd.Flush(ctx, rwTtx1)
+		require.NoError(t, err)
+		err = rwTtx1.Commit()
+		require.NoError(t, err)
+
+		// make sure it is indeed in db using a db tx
+		dbRoTx1, err := mdbxDb.BeginRo(ctx)
+		require.NoError(t, err)
+		t.Cleanup(dbRoTx1.Rollback)
+		c1, err := dbRoTx1.CursorDupSort(kv.TblStorageVals)
+		require.NoError(t, err)
+		t.Cleanup(c1.Close)
+		k, v, err := c1.Next()
+		require.NoError(t, err)
+		require.Equal(t, append(append([]byte{}, acc1.Bytes()...), acc1slot1.Bytes()...), k)
+		wantValueBytes := make([]byte, 8)                      // 8 bytes for uint64 step num
+		binary.BigEndian.PutUint64(wantValueBytes, ^uint64(1)) // step num
+		wantValueBytes = append(wantValueBytes, byte(1))       // value we wrote to the storage slot
+		require.Equal(t, wantValueBytes, v)
+		k, v, err = c1.Next()
+		require.NoError(t, err)
+		require.Nil(t, k)
+		require.Nil(t, v)
+
+		// all good
+		// now move on to SharedDomains
+		roTtx1, err := temporalDb.BeginTemporalRo(ctx)
+		require.NoError(t, err)
+		t.Cleanup(roTtx1.Rollback)
+
+		// make sure there are no files yet and we are only hitting the DB
+		require.Equal(t, uint64(0), roTtx1.Debug().TxNumsInFiles(kv.StorageDomain))
+
+		// finally, verify SharedDomains.HasPrefix returns true
+		sd.SetTxNum(2) // needed for HasPrefix (in-mem has to be ahead of tx num)
+		firstKey, firstVal, ok, err = sd.HasPrefix(kv.StorageDomain, acc1.Bytes(), roTtx1)
+		require.NoError(t, err)
+		require.True(t, ok)
+		require.Equal(t, append(append([]byte{}, acc1.Bytes()...), acc1slot1.Bytes()...), firstKey)
+		require.Equal(t, []byte{1}, firstVal)
+
+		// check some other non-existing storages for non-existence after write operation
+		firstKey, firstVal, ok, err = sd.HasPrefix(kv.StorageDomain, acc2.Bytes(), roTtx1)
+		require.NoError(t, err)
+		require.False(t, ok)
+		require.Nil(t, firstKey)
+		require.Nil(t, firstVal)
+
+		roTtx1.Rollback()
+	}
+
+	// --- check 3: storage exists in files only - SharedDomains.HasPrefix should catch this
+	{
+		// move data to files and trigger prune (need one more step for prune so write to some other storage)
+		rwTtx2, err := temporalDb.BeginTemporalRw(ctx)
+		require.NoError(t, err)
+		t.Cleanup(rwTtx2.Rollback)
+		err = sd.DomainPut(kv.StorageDomain, rwTtx2, storageK2, []byte{2}, 2, nil, 0)
+		require.NoError(t, err)
+		// check before flush
+		firstKey, firstVal, ok, err := sd.HasPrefix(kv.StorageDomain, acc1.Bytes(), rwTtx2)
+		require.NoError(t, err)
+		require.True(t, ok)
+		require.Equal(t, append(append([]byte{}, acc1.Bytes()...), acc1slot1.Bytes()...), firstKey)
+		require.Equal(t, []byte{1}, firstVal)
+		// check after flush
+		err = sd.Flush(ctx, rwTtx2)
+		require.NoError(t, err)
+		err = rwTtx2.Commit()
+		require.NoError(t, err)
+
+		// build files
+		err = agg.BuildFiles(2)
+		require.NoError(t, err)
+		rwTtx3, err := temporalDb.BeginTemporalRw(ctx)
+		require.NoError(t, err)
+		t.Cleanup(rwTtx3.Rollback)
+
+		// prune
+		haveMore, err := rwTtx3.PruneSmallBatches(ctx, time.Minute)
+		require.NoError(t, err)
+		require.False(t, haveMore)
+		err = rwTtx3.Commit()
+		require.NoError(t, err)
+
+		// double check acc1 storage data not in the mdbx DB
+		dbRoTx2, err := mdbxDb.BeginRo(ctx)
+		require.NoError(t, err)
+		t.Cleanup(dbRoTx2.Rollback)
+		c2, err := dbRoTx2.CursorDupSort(kv.TblStorageVals)
+		require.NoError(t, err)
+		t.Cleanup(c2.Close)
+		k, v, err := c2.Next() // acc2 storage from step 2 will be there
+		require.NoError(t, err)
+		require.Equal(t, append(append([]byte{}, acc2.Bytes()...), acc2slot2.Bytes()...), k)
+		wantValueBytes := make([]byte, 8)                      // 8 bytes for uint64 step num
+		binary.BigEndian.PutUint64(wantValueBytes, ^uint64(2)) // step num
+		wantValueBytes = append(wantValueBytes, byte(2))       // value we wrote to the storage slot
+		require.Equal(t, wantValueBytes, v)
+		k, v, err = c2.Next() // acc1 storage from step 1 must not be there
+		require.NoError(t, err)
+		require.Nil(t, k)
+		require.Nil(t, v)
+
+		// double check files for 2 steps have been created
+		roTtx2, err := temporalDb.BeginTemporalRo(ctx)
+		require.NoError(t, err)
+		t.Cleanup(roTtx2.Rollback)
+		require.Equal(t, uint64(2), roTtx2.Debug().TxNumsInFiles(kv.StorageDomain))
+
+		// finally, verify SharedDomains.HasPrefix returns true
+		firstKey, firstVal, ok, err = sd.HasPrefix(kv.StorageDomain, acc1.Bytes(), roTtx2)
+		require.NoError(t, err)
+		require.True(t, ok)
+		require.Equal(t, append(append([]byte{}, acc1.Bytes()...), acc1slot1.Bytes()...), firstKey)
+		require.Equal(t, []byte{1}, firstVal)
+		roTtx2.Rollback()
+	}
+
+	// --- check 4: delete storage - SharedDomains.HasPrefix should catch this and say it does not exist
+	{
+		rwTtx4, err := temporalDb.BeginTemporalRw(ctx)
+		require.NoError(t, err)
+		t.Cleanup(rwTtx4.Rollback)
+		err = sd.DomainDelPrefix(kv.StorageDomain, rwTtx4, acc1.Bytes(), 3)
+		require.NoError(t, err)
+		// check before flush
+		firstKey, firstVal, ok, err := sd.HasPrefix(kv.StorageDomain, acc1.Bytes(), rwTtx4)
+		require.NoError(t, err)
+		require.False(t, ok)
+		require.Nil(t, firstKey)
+		require.Nil(t, firstVal)
+		// check after flush
+		err = sd.Flush(ctx, rwTtx4)
+		require.NoError(t, err)
+		err = rwTtx4.Commit()
+		require.NoError(t, err)
+
+		roTtx3, err := temporalDb.BeginTemporalRo(ctx)
+		require.NoError(t, err)
+		t.Cleanup(roTtx3.Rollback)
+		sd.SetTxNum(4) // needed for HasPrefix (in-mem has to be ahead of tx num)
+		firstKey, firstVal, ok, err = sd.HasPrefix(kv.StorageDomain, acc1.Bytes(), roTtx3)
+		require.NoError(t, err)
+		require.False(t, ok)
+		require.Nil(t, firstKey)
+		require.Nil(t, firstVal)
+		roTtx3.Rollback()
+	}
+
+	// --- check 5: write to it again after deletion - SharedDomains.HasPrefix should catch
+	{
+		rwTtx5, err := temporalDb.BeginTemporalRw(ctx)
+		require.NoError(t, err)
+		t.Cleanup(rwTtx5.Rollback)
+		err = sd.DomainPut(kv.StorageDomain, rwTtx5, storageK1, []byte{3}, 4, nil, 0)
+		require.NoError(t, err)
+		// check before flush
+		firstKey, firstVal, ok, err := sd.HasPrefix(kv.StorageDomain, acc1.Bytes(), rwTtx5)
+		require.NoError(t, err)
+		require.True(t, ok)
+		require.Equal(t, append(append([]byte{}, acc1.Bytes()...), acc1slot1.Bytes()...), firstKey)
+		require.Equal(t, []byte{3}, firstVal)
+		// check after flush
+		err = sd.Flush(ctx, rwTtx5)
+		require.NoError(t, err)
+		err = rwTtx5.Commit()
+		require.NoError(t, err)
+
+		roTtx4, err := temporalDb.BeginTemporalRo(ctx)
+		require.NoError(t, err)
+		t.Cleanup(roTtx4.Rollback)
+		sd.SetTxNum(5) // needed for HasPrefix (in-mem has to be ahead of tx num)
+		firstKey, firstVal, ok, err = sd.HasPrefix(kv.StorageDomain, acc1.Bytes(), roTtx4)
+		require.NoError(t, err)
+		require.True(t, ok)
+		require.Equal(t, append(append([]byte{}, acc1.Bytes()...), acc1slot1.Bytes()...), firstKey)
+		require.Equal(t, []byte{3}, firstVal)
+		roTtx4.Rollback()
+	}
 }

erigon-lib/state/kv_temporal_copy_test.go
@@ -388,31 +388,36 @@ func (tx *tx) getLatest(name kv.Domain, dbTx kv.Tx, k []byte) (v []byte, step ui
 	return v, step, err
 }
 
-func (tx *Tx) HasPrefix(name kv.Domain, prefix []byte) ([]byte, bool, error) {
+func (tx *Tx) HasPrefix(name kv.Domain, prefix []byte) ([]byte, []byte, bool, error) {
 	return tx.hasPrefix(name, tx.Tx, prefix)
 }
 
-func (tx *RwTx) HasPrefix(name kv.Domain, prefix []byte) ([]byte, bool, error) {
+func (tx *RwTx) HasPrefix(name kv.Domain, prefix []byte) ([]byte, []byte, bool, error) {
 	return tx.hasPrefix(name, tx.RwTx, prefix)
 }
 
-func (tx *tx) hasPrefix(name kv.Domain, dbTx kv.Tx, prefix []byte) ([]byte, bool, error) {
-	it, err := tx.rangeLatest(name, dbTx, prefix, nil, 1)
+func (tx *tx) hasPrefix(name kv.Domain, dbTx kv.Tx, prefix []byte) ([]byte, []byte, bool, error) {
+	to, ok := kv.NextSubtree(prefix)
+	if !ok {
+		to = nil
+	}
+
+	it, err := tx.rangeLatest(name, dbTx, prefix, to, 1)
 	if err != nil {
-		return nil, false, err
+		return nil, nil, false, err
 	}
 
 	defer it.Close()
 	if !it.HasNext() {
-		return nil, false, nil
+		return nil, nil, false, nil
 	}
 
-	k, _, err := it.Next()
+	k, v, err := it.Next()
 	if err != nil {
-		return nil, false, err
+		return nil, nil, false, err
 	}
 
-	return k, true, nil
+	return k, v, true, nil
 }
 
 func (tx *Tx) GetLatest(name kv.Domain, k []byte) (v []byte, step uint64, err error) {