fc20f3cbfab2db1f5e82dbcd1f4b8b69546b87f2
alex.sharov <AskAlexSharov@gmail.com>
2025-05-28 18:56:21 +0700

    save

--- cmd/evm/internal/t8ntool/transition.go
+++ cmd/evm/internal/t8ntool/transition.go
@@ -674,7 +674,7 @@ func CalculateStateRoot(tx kv.TemporalRwTx, blockNum uint64, txNum uint64) (*com
 		}
 	}
 	c.Close()
-	root, err := domains.ComputeCommitment(context.Background(), true, blockNum, txNum, "")
+	root, err := domains.ComputeCommitment(context.Background(), tx, true, blockNum, txNum, "")
 	if err != nil {
 		return nil, err
 	}
