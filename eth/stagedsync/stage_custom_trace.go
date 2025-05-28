32f699a92d5711c9afbe2daaf277fe1e62171cd4
alex.sharov <AskAlexSharov@gmail.com>
2025-05-28 13:30:49 +0700

    save

--- eth/stagedsync/stage_custom_trace.go
+++ eth/stagedsync/stage_custom_trace.go
@@ -150,7 +150,7 @@ func SpawnCustomTrace(cfg CustomTraceCfg, ctx context.Context, logger log.Logger
 			_nextBlock -= batchSize / 2
 		}
 		if toStep-fromStep < 1 { // increase small jump
-			_nextBlock += batchSize * 3
+			_nextBlock += batchSize
 		}
 
 		to := min(endBlock+1, _nextBlock)
