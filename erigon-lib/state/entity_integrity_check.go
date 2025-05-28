2fdc3d6946e50acf1e03fe76f61d3650fbfff583
sudeepdino008 <sudeepdino008@gmail.com>
2025-05-28 15:50:47 +0530

    save

--- erigon-lib/state/entity_integrity_check.go
+++ erigon-lib/state/entity_integrity_check.go
@@ -36,7 +36,7 @@ func NewDependencyIntegrityChecker(dirs datadir.Dirs, logger log.Logger) *Depend
 		dependencyMap: make(map[kv.Domain][]*DependentInfo),
 		dirs:          dirs,
 		logger:        logger,
-		trace:         true,
+		//		trace:         true,
 	}
 }
 
