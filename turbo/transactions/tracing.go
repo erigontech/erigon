3d9e86635bae4ed25edf69e528fc4e482786c0f3
canepat <16927169+canepat@users.noreply.github.com>
2025-05-30 14:40:02 +0200

    rpcdaemon: stack-based json stream (#14994)

--- turbo/transactions/tracing.go
+++ turbo/transactions/tracing.go
@@ -24,11 +24,10 @@ import (
 	"fmt"
 	"time"
 
-	jsoniter "github.com/json-iterator/go"
-
 	"github.com/erigontech/erigon-db/interfaces"
 	"github.com/erigontech/erigon-lib/chain"
 	"github.com/erigontech/erigon-lib/common"
+	"github.com/erigontech/erigon-lib/jsonstream"
 	"github.com/erigontech/erigon-lib/kv"
 	"github.com/erigontech/erigon-lib/kv/rawdbv3"
 	"github.com/erigontech/erigon-lib/types"
@@ -100,7 +99,7 @@ func TraceTx(
 	ibs *state.IntraBlockState,
 	config *tracersConfig.TraceConfig,
 	chainConfig *chain.Config,
-	stream *jsoniter.Stream,
+	stream jsonstream.Stream,
 	callTimeout time.Duration,
 ) (gasUsed uint64, err error) {
 	tracer, streaming, cancel, err := AssembleTracer(ctx, config, txCtx.TxHash, blockHash, txnIndex, stream, callTimeout)
@@ -143,7 +142,7 @@ func AssembleTracer(
 	txHash common.Hash,
 	blockHash common.Hash,
 	txnIndex int,
-	stream *jsoniter.Stream,
+	stream jsonstream.Stream,
 	callTimeout time.Duration,
 ) (*tracers.Tracer, bool, context.CancelFunc, error) {
 	// Assemble the structured logger or the JavaScript tracer
@@ -190,7 +189,7 @@ func ExecuteTraceTx(
 	ibs *state.IntraBlockState,
 	config *tracersConfig.TraceConfig,
 	chainConfig *chain.Config,
-	stream *jsoniter.Stream,
+	stream jsonstream.Stream,
 	tracer *tracers.Tracer,
 	streaming bool,
 	execCb func(evm *vm.EVM, refunds bool) (*evmtypes.ExecutionResult, error),
