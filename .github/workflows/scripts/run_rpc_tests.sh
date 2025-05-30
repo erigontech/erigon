3ea7a1d75cc5f3bee1d3fd7381bbb7f222da8f8b
canepat <16927169+canepat@users.noreply.github.com>
2025-05-30 16:41:52 +0200

    qa_tests: execute RPC tests in parallel

--- .github/workflows/scripts/run_rpc_tests.sh
+++ .github/workflows/scripts/run_rpc_tests.sh
@@ -38,6 +38,6 @@ disabled_tests=(
 # Transform the array into a comma-separated string
 disabled_test_list=$(IFS=,; echo "${disabled_tests[*]}")
 
-python3 ./run_tests.py --port 8545 --engine-port 8545 --continue -f --json-diff --serial -x "$disabled_test_list" 
+python3 ./run_tests.py --port 8545 --engine-port 8545 --continue -f --json-diff -x "$disabled_test_list" 
 
 exit $?
