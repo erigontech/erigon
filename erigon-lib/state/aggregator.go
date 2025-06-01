ebca6cab7b8e7e61893f8854e0f49851f4c4ac50
alex.sharov <AskAlexSharov@gmail.com>
2025-06-01 21:40:57 +0700

    save

--- erigon-lib/state/aggregator.go
+++ erigon-lib/state/aggregator.go
@@ -994,7 +994,7 @@ func (at *AggregatorRoTx) PruneSmallBatches(ctx context.Context, timeout time.Du
 			at.a.logger.Info("[snapshots] pruning state",
 				"until commit", time.Until(started.Add(timeout)).String(),
 				"pruneLimit", pruneLimit,
-				"aggregatedStep", at.StepsInFiles(kv.StateDomains...),
+				"aggregatedStep", at.StepsInFiles(kv.AccountsDomain),
 				"stepsRangeInDB", at.stepsRangeInDBAsStr(tx),
 				"pruned", fullStat.String(),
 			)
