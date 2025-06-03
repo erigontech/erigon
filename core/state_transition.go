erigontech/erigon

commit 7c81a1d411b4a802ea5887c810590b9ea97bfa27

Versioned IBS Implementation (#15387)
Versioned IBS is used by parallel processing to keep track of reads and
writes between parallel threads running on the same block.

If the versionedMap is set on the IBS it will use this to mediate read
and write activity to make sure that there dependency clash between
transactions running in parallel. If a dependency is encountered
execution will be stopped with a dependency panic which is handled by
the parallel scheduler which re-schedules execution after the
dependencies have been resolves.

Note that this version of the IBS has a block in in its context. This is
not functional it is purely used for tracing. This is because
differences in state setting can have subtle side effects which can
cause issues several blocks later. Having the block in the trace output
makes it easier to tie together cause and effect.

core/state_transition.go
@@ -215,8 +215,8 @@ func (st *StateTransition) buyGas(gasBailout bool) error {
 		if have, want := balance, balanceCheck; have.Cmp(want) < 0 {
 			return fmt.Errorf("%w: address %v have %v want %v", ErrInsufficientFunds, st.msg.From().Hex(), have, want)
 		}
-		st.state.SubBalance(st.msg.From(), gasVal, tracing.BalanceDecreaseGasBuy)
-		st.state.SubBalance(st.msg.From(), blobGasVal, tracing.BalanceDecreaseGasBuy)
+		st.state.SubBalance(st.msg.From(), *gasVal, tracing.BalanceDecreaseGasBuy)
+		st.state.SubBalance(st.msg.From(), *blobGasVal, tracing.BalanceDecreaseGasBuy)
 	}
 
 	if err := st.gp.SubGas(st.msg.Gas()); err != nil {
@@ -318,12 +318,10 @@ func (st *StateTransition) ApplyFrame() (*evmtypes.ExecutionResult, error) {
 	if err != nil {
 		return nil, fmt.Errorf("%w: %w", ErrStateTransitionFailed, err)
 	}
-	senderInitBalance = senderInitBalance.Clone()
 	coinbaseInitBalance, err := st.state.GetBalance(coinbase)
 	if err != nil {
 		return nil, fmt.Errorf("%w: %w", ErrStateTransitionFailed, err)
 	}
-	coinbaseInitBalance = coinbaseInitBalance.Clone()
 
 	msg := st.msg
 	st.gasRemaining += st.msg.Gas()
@@ -394,12 +392,10 @@ func (st *StateTransition) TransitionDb(refunds bool, gasBailout bool) (*evmtype
 	if err != nil {
 		return nil, fmt.Errorf("%w: %w", ErrStateTransitionFailed, err)
 	}
-	senderInitBalance = senderInitBalance.Clone()
 	coinbaseInitBalance, err := st.state.GetBalance(coinbase)
 	if err != nil {
 		return nil, fmt.Errorf("%w: %w", ErrStateTransitionFailed, err)
 	}
-	coinbaseInitBalance = coinbaseInitBalance.Clone()
 
 	// First check this message satisfies all consensus rules before
 	// applying the message. The rules include these clauses
@@ -519,17 +515,17 @@ func (st *StateTransition) TransitionDb(refunds bool, gasBailout bool) (*evmtype
 	}
 	amount := new(uint256.Int).SetUint64(st.gasUsed())
 	amount.Mul(amount, effectiveTip) // gasUsed * effectiveTip = how much goes to the block producer (miner, validator)
-	if err := st.state.AddBalance(coinbase, amount, tracing.BalanceIncreaseRewardTransactionFee); err != nil {
+	if err := st.state.AddBalance(coinbase, *amount, tracing.BalanceIncreaseRewardTransactionFee); err != nil {
 		return nil, fmt.Errorf("%w: %w", ErrStateTransitionFailed, err)
 	}
 	if !msg.IsFree() && rules.IsLondon {
 		burntContractAddress := st.evm.ChainConfig().GetBurntContract(st.evm.Context.BlockNumber)
 		if burntContractAddress != nil {
 			burnAmount := new(uint256.Int).Mul(new(uint256.Int).SetUint64(st.gasUsed()), st.evm.Context.BaseFee)
-			st.state.AddBalance(*burntContractAddress, burnAmount, tracing.BalanceChangeUnspecified)
+			st.state.AddBalance(*burntContractAddress, *burnAmount, tracing.BalanceChangeUnspecified)
 			if rules.IsAura && rules.IsPrague {
 				// https://github.com/gnosischain/specs/blob/master/network-upgrades/pectra.md#eip-4844-pectra
-				st.state.AddBalance(*burntContractAddress, st.evm.BlobFee, tracing.BalanceChangeUnspecified)
+				st.state.AddBalance(*burntContractAddress, *st.evm.BlobFee, tracing.BalanceChangeUnspecified)
 			}
 		}
 	}
@@ -541,7 +537,7 @@ func (st *StateTransition) TransitionDb(refunds bool, gasBailout bool) (*evmtype
 		ReturnData:          ret,
 		SenderInitBalance:   senderInitBalance,
 		CoinbaseInitBalance: coinbaseInitBalance,
-		FeeTipped:           amount,
+		FeeTipped:           *amount,
 		EvmRefund:           st.state.GetRefund(),
 	}
 
@@ -642,7 +638,7 @@ func (st *StateTransition) verifyAuthorities(auths []types.Authorization, contra
 func (st *StateTransition) refundGas() {
 	// Return ETH for remaining gas, exchanged at the original rate.
 	remaining := new(uint256.Int).Mul(new(uint256.Int).SetUint64(st.gasRemaining), st.gasPrice)
-	st.state.AddBalance(st.msg.From(), remaining, tracing.BalanceIncreaseGasReturn)
+	st.state.AddBalance(st.msg.From(), *remaining, tracing.BalanceIncreaseGasReturn)
 
 	// Also return remaining gas to the block gas counter so it is
 	// available for the next transaction.

polygon/aa/aa_gas.go
@@ -39,7 +39,7 @@ func chargeGas(
 		return fmt.Errorf("%w: RIP-7560 address %v have %v want %v", core.ErrInsufficientFunds, chargeFrom.Hex(), balance, preCharge)
 	}
 
-	if err := ibs.SubBalance(*chargeFrom, preCharge, 0); err != nil {
+	if err := ibs.SubBalance(*chargeFrom, *preCharge, 0); err != nil {
 		return err
 	}
 
@@ -68,7 +68,7 @@ func refundGas(
 
 	chargeFrom := tx.GasPayer()
 
-	if err := ibs.AddBalance(*chargeFrom, refund, tracing.BalanceIncreaseGasReturn); err != nil {
+	if err := ibs.AddBalance(*chargeFrom, *refund, tracing.BalanceIncreaseGasReturn); err != nil {
 		return err
 	}
 
@@ -91,5 +91,5 @@ func payCoinbase(
 
 	amount := new(uint256.Int).SetUint64(gasUsed)
 	amount.Mul(amount, effectiveTip)
-	return ibs.AddBalance(coinbase, amount, tracing.BalanceIncreaseRewardTransactionFee)
+	return ibs.AddBalance(coinbase, *amount, tracing.BalanceIncreaseRewardTransactionFee)
 }

cmd/devnet/services/polygon/proofgenerator_test.go
@@ -170,10 +170,10 @@ func (rg *requestGenerator) GetTransactionReceipt(ctx context.Context, hash comm
 	}
 
 	header := block.Header()
+	blockNum := block.NumberU64()
 
 	for i, txn := range block.Transactions() {
-
-		ibs.SetTxContext(i)
+		ibs.SetTxContext(blockNum, i)
 
 		receipt, _, err := core.ApplyTransaction(chainConfig, core.GetHashFn(header, getHeader), engine, nil, gp, ibs, noopWriter, header, txn, &gasUsed, &usedBlobGas, vm.Config{})
 

cmd/state/commands/opcode_tracer.go
@@ -737,9 +737,10 @@ func runBlock(engine consensus.Engine, ibs *state.IntraBlockState, txnWriter sta
 	usedBlobGas := new(uint64)
 	var receipts types.Receipts
 	core.InitializeBlockExecution(engine, nil, header, chainConfig, ibs, nil, logger, nil)
-	rules := chainConfig.Rules(block.NumberU64(), block.Time())
+	blockNum := block.NumberU64()
+	rules := chainConfig.Rules(blockNum, block.Time())
 	for i, txn := range block.Transactions() {
-		ibs.SetTxContext(i)
+		ibs.SetTxContext(blockNum, i)
 		receipt, _, err := core.ApplyTransaction(chainConfig, core.GetHashFn(header, getHeader), engine, nil, gp, ibs, txnWriter, header, txn, gasUsed, usedBlobGas, vmConfig)
 		if err != nil {
 			return nil, fmt.Errorf("could not apply txn %d [%x] failed: %w", i, txn.Hash(), err)

core/blockchain.go
@@ -124,8 +124,10 @@ func ExecuteBlockEphemerally(
 	var rejectedTxs []*RejectedTx
 	includedTxs := make(types.Transactions, 0, block.Transactions().Len())
 	receipts := make(types.Receipts, 0, block.Transactions().Len())
+	blockNum := block.NumberU64()
+
 	for i, txn := range block.Transactions() {
-		ibs.SetTxContext(i)
+		ibs.SetTxContext(blockNum, i)
 		writeTrace := false
 		if vmConfig.Tracer == nil && getTracer != nil {
 			tracer, err := getTracer(i, txn.Hash())

core/chain_makers.go
@@ -122,7 +122,7 @@ func (b *BlockGen) AddTxWithChain(getHeader func(hash common.Hash, number uint64
 	if b.gasPool == nil {
 		b.SetCoinbase(common.Address{})
 	}
-	b.ibs.SetTxContext(len(b.txs))
+	b.ibs.SetTxContext(b.header.Number.Uint64(), len(b.txs))
 	receipt, _, err := ApplyTransaction(b.config, GetHashFn(b.header, getHeader), engine, &b.header.Coinbase, b.gasPool, b.ibs, state.NewNoopWriter(), b.header, txn, &b.header.GasUsed, b.header.BlobGasUsed, vm.Config{})
 	if err != nil {
 		panic(err)
@@ -138,7 +138,7 @@ func (b *BlockGen) AddFailedTxWithChain(getHeader func(hash common.Hash, number 
 	if b.gasPool == nil {
 		b.SetCoinbase(common.Address{})
 	}
-	b.ibs.SetTxContext(len(b.txs))
+	b.ibs.SetTxContext(b.header.Number.Uint64(), len(b.txs))
 	receipt, _, err := ApplyTransaction(b.config, GetHashFn(b.header, getHeader), engine, &b.header.Coinbase, b.gasPool, b.ibs, state.NewNoopWriter(), b.header, txn, &b.header.GasUsed, b.header.BlobGasUsed, vm.Config{})
 	_ = err // accept failed transactions
 	b.txs = append(b.txs, txn)

core/genesis_write.go
@@ -504,7 +504,7 @@ func GenesisToBlock(g *types.Genesis, dirs datadir.Dirs, logger log.Logger) (*ty
 			if overflow {
 				panic("overflow at genesis allocs")
 			}
-			statedb.AddBalance(addr, balance, tracing.BalanceIncreaseGenesisBalance)
+			statedb.AddBalance(addr, *balance, tracing.BalanceIncreaseGenesisBalance)
 			statedb.SetCode(addr, account.Code)
 			statedb.SetNonce(addr, account.Nonce)
 			for key, value := range account.Storage {

core/state/access_list.go
@@ -24,8 +24,7 @@ import (
 )
 
 type accessList struct {
-	addresses map[common.Address]int
-	slots     []map[common.Hash]struct{}
+	addresses map[common.Address]map[common.Hash]struct{}
 }
 
 // ContainsAddress returns true if the address is in the access list.
@@ -44,23 +43,23 @@ func (al *accessList) ContainsAddress(address common.Address) bool {
 // Contains checks if a slot within an account is present in the access list, returning
 // separate flags for the presence of the account and the slot respectively.
 func (al *accessList) Contains(address common.Address, slot common.Hash) (addressPresent bool, slotPresent bool) {
-	idx, ok := al.addresses[address]
+	slots, ok := al.addresses[address]
 	if !ok {
 		// no such address (and hence zero slots)
 		return false, false
 	}
-	if idx == -1 {
+	if slots == nil {
 		// address yes, but no slots
 		return true, false
 	}
-	_, slotPresent = al.slots[idx][slot]
+	_, slotPresent = slots[slot]
 	return true, slotPresent
 }
 
 // newAccessList creates a new accessList.
 func newAccessList() *accessList {
 	return &accessList{
-		addresses: make(map[common.Address]int),
+		addresses: map[common.Address]map[common.Hash]struct{}{},
 	}
 }
 
@@ -73,16 +72,17 @@ func newAccessList() *accessList {
 func (al *accessList) Copy() *accessList {
 	cp := newAccessList()
 	for k, v := range al.addresses {
-		cp.addresses[k] = v
-	}
-	cp.slots = make([]map[common.Hash]struct{}, len(al.slots))
-	for i, slotMap := range al.slots {
-		newSlotmap := make(map[common.Hash]struct{}, len(slotMap))
-		for k := range slotMap {
-			newSlotmap[k] = struct{}{}
+		if v == nil {
+			cp.addresses[k] = v
+		} else {
+			slots := map[common.Hash]struct{}{}
+			for k := range v {
+				slots[k] = struct{}{}
+			}
+			cp.addresses[k] = slots
 		}
-		cp.slots[i] = newSlotmap
 	}
+
 	return cp
 }
 
@@ -92,7 +92,7 @@ func (al *accessList) AddAddress(address common.Address) bool {
 	if _, present := al.addresses[address]; present {
 		return false
 	}
-	al.addresses[address] = -1
+	al.addresses[address] = nil
 	return true
 }
 
@@ -102,18 +102,14 @@ func (al *accessList) AddAddress(address common.Address) bool {
 // - slot added
 // For any 'true' value returned, a corresponding journal entry must be made.
 func (al *accessList) AddSlot(address common.Address, slot common.Hash) (addrChange bool, slotChange bool) {
-	idx, addrPresent := al.addresses[address]
-	if !addrPresent || idx == -1 {
+	slots, addrPresent := al.addresses[address]
+	if !addrPresent || slots == nil {
 		// Address not present, or addr present but no slots there
-		al.addresses[address] = len(al.slots)
-		slotmap := map[common.Hash]struct{}{slot: {}}
-		al.slots = append(al.slots, slotmap)
+		al.addresses[address] = map[common.Hash]struct{}{slot: {}}
 		return !addrPresent, true
 	}
-	// There is already an (address,slot) mapping
-	slotmap := al.slots[idx]
-	if _, ok := slotmap[slot]; !ok {
-		slotmap[slot] = struct{}{}
+	if _, ok := slots[slot]; !ok {
+		slots[slot] = struct{}{}
 		// Journal add slot change
 		return false, true
 	}
@@ -126,19 +122,17 @@ func (al *accessList) AddSlot(address common.Address, slot common.Hash) (addrCha
 // This method is meant to be used  by the journal, which maintains ordering of
 // operations.
 func (al *accessList) DeleteSlot(address common.Address, slot common.Hash) {
-	idx, addrOk := al.addresses[address]
+	slots, addrOk := al.addresses[address]
 	// There are two ways this can fail
 	if !addrOk {
 		panic("reverting slot change, address not present in list")
 	}
-	slotmap := al.slots[idx]
-	delete(slotmap, slot)
+	delete(slots, slot)
 	// If that was the last (first) slot, remove it
 	// Since additions and rollbacks are always performed in order,
 	// we can delete the item without worrying about screwing up later indices
-	if len(slotmap) == 0 {
-		al.slots = al.slots[:idx]
-		al.addresses[address] = -1
+	if len(slot) == 0 {
+		al.addresses[address] = nil
 	}
 }
 
@@ -147,5 +141,12 @@ func (al *accessList) DeleteSlot(address common.Address, slot common.Hash) {
 // This method is meant to be used  by the journal, which maintains ordering of
 // operations.
 func (al *accessList) DeleteAddress(address common.Address) {
+	slots, addrOk := al.addresses[address]
+	if !addrOk {
+		panic("reverting address change, address not present in list")
+	}
+	if len(slots) > 0 {
+		panic("reverting address change, address has slots")
+	}
 	delete(al.addresses, address)
 }

commit 138120eea90bf1488ddd15442033cf97aa7872ea

Fix insufficient funds balance formatting  (#15417)
Fixes a breaking formatting change introduced in:
https://github.com/erigontech/erigon/pull/15387

core/state_transition.go
@@ -213,7 +213,7 @@ func (st *StateTransition) buyGas(gasBailout bool) error {
 			return err
 		}
 		if have, want := balance, balanceCheck; have.Cmp(want) < 0 {
-			return fmt.Errorf("%w: address %v have %v want %v", ErrInsufficientFunds, st.msg.From().Hex(), have, want)
+			return fmt.Errorf("%w: address %v have %v want %v", ErrInsufficientFunds, st.msg.From().Hex(), &have, want)
 		}
 		st.state.SubBalance(st.msg.From(), *gasVal, tracing.BalanceDecreaseGasBuy)
 		st.state.SubBalance(st.msg.From(), *blobGasVal, tracing.BalanceDecreaseGasBuy)

polygon/aa/aa_gas.go
@@ -36,7 +36,7 @@ func chargeGas(
 	}
 
 	if balance.Cmp(preCharge) < 0 {
-		return fmt.Errorf("%w: RIP-7560 address %v have %v want %v", core.ErrInsufficientFunds, chargeFrom.Hex(), balance, preCharge)
+		return fmt.Errorf("%w: RIP-7560 address %v have %v want %v", core.ErrInsufficientFunds, chargeFrom.Hex(), &balance, preCharge)
 	}
 
 	if err := ibs.SubBalance(*chargeFrom, *preCharge, 0); err != nil {