72cb8021e60577460d666d2343e93d4e2e6361ea
yperbasis <andrey.ashikhmin@gmail.com>
2025-06-04 17:48:50 +0200

    preCheck

--- core/error.go
+++ core/error.go
@@ -110,4 +110,8 @@ var (
 	// ErrSenderNoEOA is returned if the sender of a transaction is a contract.
 	// See EIP-3607: Reject transactions from senders with deployed code.
 	ErrSenderNoEOA = errors.New("sender not an eoa")
+
+	// ErrGasLimitTooHigh if the gas limit of a transaction exceeds MaxTxnGasLimit.
+	// See EIP-7825: Transaction Gas Limit Cap.
+	ErrGasLimitTooHigh = errors.New("gas limit too high")
 )
