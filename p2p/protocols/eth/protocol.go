dd669e3f426d1c677a8349d0779e31d740f7fbb6
taratorio <94537774+taratorio@users.noreply.github.com>
2025-06-06 12:43:44 +0100

    x2 devp2p ProtocolMaxMsgSize

--- p2p/protocols/eth/protocol.go
+++ p2p/protocols/eth/protocol.go
@@ -43,7 +43,7 @@ const ProtocolName = "eth"
 
 // maxMessageSize is the maximum cap on the size of a protocol message.
 const maxMessageSize = 10 * 1024 * 1024
-const ProtocolMaxMsgSize = maxMessageSize
+const ProtocolMaxMsgSize = 2 * maxMessageSize
 
 const (
 	// Protocol messages in eth/64
