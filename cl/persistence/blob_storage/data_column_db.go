4e8ccc3121e542a882da18f192e01d2c2eb848ce
kewei <kewei.train@gmail.com>
2025-06-06 16:28:53 +0800

    DownloadMissingColumnsByBlocks & IsDataAvailable

--- cl/persistence/blob_storage/data_column_db.go
+++ cl/persistence/blob_storage/data_column_db.go
@@ -28,6 +28,7 @@ type DataCloumnStorage interface {
 	RemoveColumnSidecars(ctx context.Context, slot uint64, blockRoot common.Hash) error
 	ReadColumnSidecarByColumnIndex(ctx context.Context, slot uint64, blockRoot common.Hash, columnIndex int64) (*cltypes.DataColumnSidecar, error)
 	WriteStream(w io.Writer, slot uint64, blockRoot common.Hash, idx uint64) error // Used for P2P networking
+	GetExistingColumnIndex(ctx context.Context, blockRoot common.Hash) ([]uint64, error)
 	//KzgCommitmentsCount(ctx context.Context, blockRoot common.Hash) (uint32, error)
 	//Prune() error
 }
@@ -39,7 +40,7 @@ type dataCloumnStorageImpl struct {
 	ethClock          eth_clock.EthereumClock
 	slotsKept         uint64
 
-	mutexes map[uint64]*sync.RWMutex
+	dbMutexes map[uint64]*sync.RWMutex
 }
 
 func NewDataColumnStore(db kv.RwDB, fs afero.Fs, slotsKept uint64, beaconChainConfig *clparams.BeaconChainConfig, ethClock eth_clock.EthereumClock) DataCloumnStorage {
@@ -49,10 +50,10 @@ func NewDataColumnStore(db kv.RwDB, fs afero.Fs, slotsKept uint64, beaconChainCo
 		beaconChainConfig: beaconChainConfig,
 		ethClock:          ethClock,
 		slotsKept:         slotsKept,
-		mutexes:           make(map[uint64]*sync.RWMutex, mutexSize),
+		dbMutexes:         make(map[uint64]*sync.RWMutex, mutexSize),
 	}
 	for i := uint64(0); i < mutexSize; i++ {
-		impl.mutexes[i] = &sync.RWMutex{}
+		impl.dbMutexes[i] = &sync.RWMutex{}
 	}
 	return impl
 }
@@ -65,17 +66,13 @@ func dataColumnFilePath(slot uint64, blockRoot common.Hash, columnIndex uint64) 
 }
 
 func (s *dataCloumnStorageImpl) WriteColumnSidecars(ctx context.Context, blockRoot common.Hash, columnIndex int64, columnData *cltypes.DataColumnSidecar) error {
-	mutex := s.acquireMutexBySlot(columnData.SignedBlockHeader.Header.Slot)
-	mutex.Lock()
-	defer mutex.Unlock()
-
 	dir, filepath := dataColumnFilePath(columnData.SignedBlockHeader.Header.Slot, blockRoot, uint64(columnIndex))
 	if err := s.fs.MkdirAll(dir, 0755); err != nil {
 		return err
 	}
 	if _, err := s.fs.Stat(filepath); err == nil {
 		// File already exists, no need to write again
-		// TODO: Need to check content?
+		// TODO: Need to check content or database?
 		return nil
 	}
 	fh, err := s.fs.Create(filepath)
@@ -93,6 +90,9 @@ func (s *dataCloumnStorageImpl) WriteColumnSidecars(ctx context.Context, blockRo
 		return err
 	}
 
+	mutex := s.acquireMutexBySlot(columnData.SignedBlockHeader.Header.Slot)
+	mutex.Lock()
+	defer mutex.Unlock()
 	// increment the column count and append the column index
 	// | column_count | column_index1 | column_index2 | ... |
 	tx, err := s.db.BeginRw(ctx)
@@ -134,10 +134,6 @@ func (s *dataCloumnStorageImpl) WriteColumnSidecars(ctx context.Context, blockRo
 }
 
 func (s *dataCloumnStorageImpl) ReadColumnSidecarByColumnIndex(ctx context.Context, slot uint64, blockRoot common.Hash, columnIndex int64) (*cltypes.DataColumnSidecar, error) {
-	mutex := s.acquireMutexBySlot(slot)
-	mutex.RLock()
-	defer mutex.RUnlock()
-
 	_, filepath := dataColumnFilePath(slot, blockRoot, uint64(columnIndex))
 	fh, err := s.fs.Open(filepath)
 	if err != nil {
@@ -195,9 +191,32 @@ func (s *dataCloumnStorageImpl) WriteStream(w io.Writer, slot uint64, blockRoot 
 	return err
 }
 
+func (s *dataCloumnStorageImpl) GetExistingColumnIndex(ctx context.Context, blockRoot common.Hash) ([]uint64, error) {
+	// No need to lock the mutex here, as we are only reading from the database
+	tx, err := s.db.BeginRw(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer tx.Rollback()
+
+	bytes, err := tx.GetOne(kv.BlockRootToDataColumnCount, blockRoot[:])
+	if err != nil {
+		return nil, err
+	}
+	if bytes == nil {
+		return nil, nil
+	}
+	count := binary.LittleEndian.Uint32(bytes[0:4])
+	columns := make([]uint64, count)
+	for i := uint32(0); i < count; i++ {
+		columns[i] = uint64(binary.LittleEndian.Uint32(bytes[4+i*4 : 4+(i+1)*4]))
+	}
+	return columns, nil
+}
+
 func (s *dataCloumnStorageImpl) acquireMutexBySlot(slot uint64) *sync.RWMutex {
 	index := slot % mutexSize
-	return s.mutexes[index]
+	return s.dbMutexes[index]
 }
 
 func (s *dataCloumnStorageImpl) Prune() error {
