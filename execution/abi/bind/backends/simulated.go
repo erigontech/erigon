09ad5f0fbc9d028acea326d752d34f8ddb8f5fe2
otc group <coalbocman632@gmail.com>
2025-06-07 16:05:11 +0200

    update link to ErrorCode list

--- execution/abi/bind/backends/simulated.go
+++ execution/abi/bind/backends/simulated.go
@@ -537,7 +537,7 @@ type revertError struct {
 }
 
 // ErrorCode returns the JSON error code for a revert.
-// See: https://github.com/ethereum/wiki/wiki/JSON-RPC-Error-Codes-Improvement-Proposal
+// See: https://github.com/ethereum/eth-wiki/blob/master/json-rpc/error-codes-improvement-proposal.md
 func (e *revertError) ErrorCode() int {
 	return 3
 }
