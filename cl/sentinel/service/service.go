8d0299484f9f97047dda1ddbbbb56ff43020fb88
kewei <kewei.train@gmail.com>
2025-06-12 14:25:54 +0800

    debug

--- cl/sentinel/service/service.go
+++ cl/sentinel/service/service.go
@@ -231,10 +231,10 @@ func (s *SentinelServer) requestPeer(ctx context.Context, pid peer.ID, req *sent
 	if resp.StatusCode < 200 || resp.StatusCode > 399 {
 		errBody, _ := io.ReadAll(resp.Body)
 		errorMessage := fmt.Errorf("SentinelHttp: %s", string(errBody))
-		if strings.Contains(errorMessage.Error(), "Read Code: EOF") {
-			// don't ban the peer.
-			return nil, errorMessage
-		}
+		//if strings.Contains(errorMessage.Error(), "Read Code: EOF") {
+		// don't ban the peer.
+		//	return nil, errorMessage
+		//}
 		if shouldBanOnFail {
 			s.sentinel.Peers().RemovePeer(pid)
 			s.sentinel.Host().Peerstore().RemovePeer(pid)
