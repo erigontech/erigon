502af46efcd8b89fb4efcc8d4a7cf9b9d07dcca5
Giulio rebuffo <giulio.rebuffo@gmail.com>
2025-06-13 10:37:52 +0000

    Add CLZ opcode (#15532)

--- core/vm/interpreter.go
+++ core/vm/interpreter.go
@@ -168,6 +168,8 @@ func copyJumpTable(jt *JumpTable) *JumpTable {
 func NewEVMInterpreter(evm *EVM, cfg Config) *EVMInterpreter {
 	var jt *JumpTable
 	switch {
+	case evm.chainRules.IsOsaka:
+		jt = &osakaInstructionSet
 	case evm.ChainRules().IsBhilai:
 		jt = &bhilaiInstructionSet
 	case evm.ChainRules().IsPrague:
