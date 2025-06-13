2cff1787ea8d67276a36a82c8a5b6f153c435cb6
taratorio <94537774+taratorio@users.noreply.github.com>
2025-06-13 08:34:39 +0200

    fix: TxDataZero

--- erigon-lib/rlp/commitment.go
+++ erigon-lib/rlp/commitment.go
@@ -16,13 +16,16 @@
 
 package rlp
 
-import "io"
+import (
+	"fmt"
+	"io"
+)
 
 // RLP-related utilities necessary for computing commitments for state root hash
 
 // generateRlpPrefixLenDouble calculates the length of RLP prefix to encode a string of bytes of length l "twice",
 // meaning that it is the prefix for rlp(rlp(data))
-func generateRlpPrefixLenDouble(l int, firstByte byte) int {
+func generateRlpPrefixLenDouble(l uint64, firstByte byte) int {
 	if l < 2 {
 		// firstByte only matters when there is 1 byte to encode
 		if firstByte >= 0x80 {
@@ -48,7 +51,10 @@ func generateRlpPrefixLenDouble(l int, firstByte byte) int {
 	if l < 65536 {
 		return 7
 	}
-	return 8
+	if l < (1<<24)-4 {
+		return 8
+	}
+	panic(fmt.Sprintf("generateRlpPrefixLenDouble not implemented for lenght=%d", l))
 }
 
 func multiByteHeaderPrefixOfLen(l int) byte {
@@ -63,7 +69,7 @@ func multiByteHeaderPrefixOfLen(l int) byte {
 	return byte(0xB7 + l)
 }
 
-func generateByteArrayLen(buffer []byte, pos int, l int) int {
+func generateByteArrayLen(buffer []byte, pos int, l uint64) int {
 	if l < 56 {
 		buffer[pos] = byte(0x80 + l)
 		pos++
@@ -81,7 +87,7 @@ func generateByteArrayLen(buffer []byte, pos int, l int) int {
 		pos++
 		buffer[pos] = byte(l & 255)
 		pos++
-	} else {
+	} else if l < (1 << 24) {
 		// len(vn) is encoded as three bytes
 		buffer[pos] = multiByteHeaderPrefixOfLen(3)
 		pos++
@@ -91,11 +97,91 @@ func generateByteArrayLen(buffer []byte, pos int, l int) int {
 		pos++
 		buffer[pos] = byte(l & 255)
 		pos++
+	} else if l < (1 << 32) {
+		// len(vn) is encoded as four bytes
+		buffer[pos] = multiByteHeaderPrefixOfLen(4)
+		pos++
+		buffer[pos] = byte(l >> 24)
+		pos++
+		buffer[pos] = byte((l >> 16) & 255)
+		pos++
+		buffer[pos] = byte((l >> 8) & 255)
+		pos++
+		buffer[pos] = byte(l & 255)
+		pos++
+	} else if l < (1 << 40) {
+		// len(vn) is encoded as five bytes
+		buffer[pos] = multiByteHeaderPrefixOfLen(5)
+		pos++
+		buffer[pos] = byte(l >> 32)
+		pos++
+		buffer[pos] = byte((l >> 24) & 255)
+		pos++
+		buffer[pos] = byte((l >> 16) & 255)
+		pos++
+		buffer[pos] = byte((l >> 8) & 255)
+		pos++
+		buffer[pos] = byte(l & 255)
+		pos++
+	} else if l < (1 << 48) {
+		// len(vn) is encoded as six bytes
+		buffer[pos] = multiByteHeaderPrefixOfLen(6)
+		pos++
+		buffer[pos] = byte(l >> 40)
+		pos++
+		buffer[pos] = byte((l >> 32) & 255)
+		pos++
+		buffer[pos] = byte((l >> 24) & 255)
+		pos++
+		buffer[pos] = byte((l >> 16) & 255)
+		pos++
+		buffer[pos] = byte((l >> 8) & 255)
+		pos++
+		buffer[pos] = byte(l & 255)
+		pos++
+	} else if l < (1 << 56) {
+		// len(vn) is encoded as seven bytes
+		buffer[pos] = multiByteHeaderPrefixOfLen(7)
+		pos++
+		buffer[pos] = byte(l >> 48)
+		pos++
+		buffer[pos] = byte((l >> 40) & 255)
+		pos++
+		buffer[pos] = byte((l >> 32) & 255)
+		pos++
+		buffer[pos] = byte((l >> 24) & 255)
+		pos++
+		buffer[pos] = byte((l >> 16) & 255)
+		pos++
+		buffer[pos] = byte((l >> 8) & 255)
+		pos++
+		buffer[pos] = byte(l & 255)
+		pos++
+	} else {
+		// len(vn) is encoded as eight bytes
+		buffer[pos] = multiByteHeaderPrefixOfLen(8)
+		pos++
+		buffer[pos] = byte(l >> 56)
+		pos++
+		buffer[pos] = byte((l >> 48) & 255)
+		pos++
+		buffer[pos] = byte((l >> 40) & 255)
+		pos++
+		buffer[pos] = byte((l >> 32) & 255)
+		pos++
+		buffer[pos] = byte((l >> 24) & 255)
+		pos++
+		buffer[pos] = byte((l >> 16) & 255)
+		pos++
+		buffer[pos] = byte((l >> 8) & 255)
+		pos++
+		buffer[pos] = byte(l & 255)
+		pos++
 	}
 	return pos
 }
 
-func generateByteArrayLenDouble(buffer []byte, pos int, l int) int {
+func generateByteArrayLenDouble(buffer []byte, pos int, l uint64) int {
 	if l < 55 {
 		// After first wrapping, the length will be l + 1 < 56
 		buffer[pos] = byte(0x80 + l + 1)
@@ -161,7 +247,7 @@ func generateByteArrayLenDouble(buffer []byte, pos int, l int) int {
 		pos++
 		buffer[pos] = byte(l & 255)
 		pos++
-	} else {
+	} else if l < (1<<24)-4 {
 		// Both wrappings are 4 bytes
 		buffer[pos] = multiByteHeaderPrefixOfLen(3)
 		pos++
@@ -179,11 +265,13 @@ func generateByteArrayLenDouble(buffer []byte, pos int, l int) int {
 		pos++
 		buffer[pos] = byte(l & 255)
 		pos++
+	} else {
+		panic(fmt.Sprintf("generateByteArrayLenDouble not implemented for lenght=%d", l))
 	}
 	return pos
 }
 
-func generateRlpPrefixLen(l int) int {
+func generateRlpPrefixLen(l uint64) int {
 	if l < 2 {
 		return 0
 	}
@@ -196,7 +284,22 @@ func generateRlpPrefixLen(l int) int {
 	if l < 65536 {
 		return 3
 	}
-	return 4
+	if l < (1 << 24) {
+		return 4
+	}
+	if l < (1 << 32) {
+		return 5
+	}
+	if l < (1 << 40) {
+		return 6
+	}
+	if l < (1 << 48) {
+		return 7
+	}
+	if l < (1 << 56) {
+		return 8
+	}
+	return 9
 }
 
 // RlpSerializable is a value that can be double-RLP coded.
@@ -220,7 +323,7 @@ func (b RlpSerializableBytes) DoubleRLPLen() int {
 	if len(b) < 1 {
 		return 0
 	}
-	return generateRlpPrefixLenDouble(len(b), b[0]) + len(b)
+	return generateRlpPrefixLenDouble(uint64(len(b)), b[0]) + len(b)
 }
 
 type RlpEncodedBytes []byte
@@ -234,13 +337,13 @@ func (b RlpEncodedBytes) RawBytes() []byte {
 }
 
 func (b RlpEncodedBytes) DoubleRLPLen() int {
-	return generateRlpPrefixLen(len(b)) + len(b)
+	return generateRlpPrefixLen(uint64(len(b))) + len(b)
 }
 
-func encodeBytesAsRlpToWriter(source []byte, w io.Writer, prefixGenFunc func([]byte, int, int) int, prefixBuf []byte) error {
+func encodeBytesAsRlpToWriter(source []byte, w io.Writer, prefixGenFunc func([]byte, int, uint64) int, prefixBuf []byte) error {
 	// > 1 byte, write a prefix or prefixes first
 	if len(source) > 1 || (len(source) == 1 && source[0] >= 0x80) {
-		prefixLen := prefixGenFunc(prefixBuf, 0, len(source))
+		prefixLen := prefixGenFunc(prefixBuf, 0, uint64(len(source)))
 
 		if _, err := w.Write(prefixBuf[:prefixLen]); err != nil {
 			return err
@@ -256,10 +359,10 @@ func EncodeByteArrayAsRlp(raw []byte, w io.Writer, prefixBuf []byte) (int, error
 	if err != nil {
 		return 0, err
 	}
-	return generateRlpPrefixLen(len(raw)) + len(raw), nil
+	return generateRlpPrefixLen(uint64(len(raw))) + len(raw), nil
 }
 
-func GenerateStructLen(buffer []byte, l int) int {
+func GenerateStructLen(buffer []byte, l uint64) int {
 	if l < 56 {
 		buffer[0] = byte(192 + l)
 		return 1
@@ -276,9 +379,59 @@ func GenerateStructLen(buffer []byte, l int) int {
 		buffer[0] = byte(247 + 2)
 		return 3
 	}
-	buffer[3] = byte(l & 255)
-	buffer[2] = byte((l >> 8) & 255)
-	buffer[1] = byte(l >> 16)
-	buffer[0] = byte(247 + 3)
-	return 4
+	if l < (1 << 24) {
+		buffer[3] = byte(l & 255)
+		buffer[2] = byte((l >> 8) & 255)
+		buffer[1] = byte(l >> 16)
+		buffer[0] = byte(247 + 3)
+		return 4
+	}
+	if l < (1 << 32) {
+		buffer[4] = byte(l & 255)
+		buffer[3] = byte((l >> 8) & 255)
+		buffer[2] = byte((l >> 16) & 255)
+		buffer[1] = byte(l >> 24)
+		buffer[0] = byte(247 + 4)
+		return 5
+	}
+	if l < (1 << 40) {
+		buffer[5] = byte(l & 255)
+		buffer[4] = byte((l >> 8) & 255)
+		buffer[3] = byte((l >> 16) & 255)
+		buffer[2] = byte((l >> 24) & 255)
+		buffer[1] = byte(l >> 32)
+		buffer[0] = byte(247 + 5)
+		return 6
+	}
+	if l < (1 << 48) {
+		buffer[6] = byte(l & 255)
+		buffer[5] = byte((l >> 8) & 255)
+		buffer[4] = byte((l >> 16) & 255)
+		buffer[3] = byte((l >> 24) & 255)
+		buffer[2] = byte((l >> 32) & 255)
+		buffer[1] = byte(l >> 40)
+		buffer[0] = byte(247 + 6)
+		return 7
+	}
+	if l < (1 << 56) {
+		buffer[7] = byte(l & 255)
+		buffer[6] = byte((l >> 8) & 255)
+		buffer[5] = byte((l >> 16) & 255)
+		buffer[4] = byte((l >> 24) & 255)
+		buffer[3] = byte((l >> 32) & 255)
+		buffer[2] = byte((l >> 40) & 255)
+		buffer[1] = byte(l >> 48)
+		buffer[0] = byte(247 + 7)
+		return 8
+	}
+	buffer[8] = byte(l & 255)
+	buffer[7] = byte((l >> 8) & 255)
+	buffer[6] = byte((l >> 16) & 255)
+	buffer[5] = byte((l >> 24) & 255)
+	buffer[4] = byte((l >> 32) & 255)
+	buffer[3] = byte((l >> 40) & 255)
+	buffer[2] = byte((l >> 48) & 255)
+	buffer[1] = byte(l >> 56)
+	buffer[0] = byte(247 + 8)
+	return 9
 }
