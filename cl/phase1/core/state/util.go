2df10527124e0be88d634f49c272c63613c00eb4
kewei <kewei.train@gmail.com>
2025-06-19 21:46:26 +0800

    validator custody

--- cl/phase1/core/state/util.go
+++ cl/phase1/core/state/util.go
@@ -177,3 +177,20 @@ func QueueExcessActiveBalance(s abstract.BeaconState, vindex uint64, validator *
 	}
 	return nil
 }
+
+func GetValidatorsCustodyRequirement(s abstract.BeaconState, validatorIndices []uint64) uint64 {
+	totalNodeBalance := uint64(0)
+	for _, index := range validatorIndices {
+		effectiveBalance, err := s.ValidatorEffectiveBalance(int(index))
+		if err != nil {
+			continue
+		}
+		totalNodeBalance += effectiveBalance
+	}
+
+	count := totalNodeBalance / s.BeaconConfig().BalancePerAdditionalCustodyGroup
+	return min(
+		max(count, s.BeaconConfig().ValidatorCustodyRequirement),
+		s.BeaconConfig().NumberOfCustodyGroups,
+	)
+}
