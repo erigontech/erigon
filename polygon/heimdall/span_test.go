0ff6420fe8440c820f7ee6ce1dd54723c8e5a312
Nikita Ostroukhov <gnz.corp@gmail.com>
2025-06-23 13:20:23 +0100

    Fixed checkpoint and span pagination endpoint in heimdallv2 (#15708)

--- polygon/heimdall/span_test.go
+++ polygon/heimdall/span_test.go
@@ -17,12 +17,27 @@
 package heimdall
 
 import (
+	"encoding/json"
 	"testing"
 
 	libcommon "github.com/erigontech/erigon-lib/common"
 	"github.com/erigontech/erigon/polygon/bor/valset"
 	"github.com/erigontech/erigon/polygon/heimdall/heimdalltest"
+	"github.com/stretchr/testify/require"
 )
+
+func TestA(t *testing.T) {
+	output := []byte(`{"span_list":[{"id":"0","start_block":"0","end_block":"255","validator_set":{"validators":[{"val_id":"1","start_epoch":"0","end_epoch":"0","nonce":"1","voting_power":"10000","pub_key":"BD7rDNYnoRW2fonLir583UAzk5oRkJoUSC0J1PK+t0Y3kw6szj775oKPnO7gxzCIlLYWlidt4ixIILu8RebudD8=","signer":"20D8433DC819AF087336A725422DF4CFBEF29710","last_updated":"","jailed":false,"proposer_priority":"-10000"},{"val_id":"2","start_epoch":"0","end_epoch":"0","nonce":"1","voting_power":"10000","pub_key":"BP5UKZ9Pgw+j294NZvMSefMAtoyOhxPqNFRJKL1FxKvAHqbh/vxmo9xtLmsWDLtwCJ8rhzl9IdY3V9qVEzPw6sA=","signer":"735DE19A997EF33A090C873D1AC27F99D77B843C","last_updated":"","jailed":false,"proposer_priority":"10000"}],"proposer":{"val_id":"1","start_epoch":"0","end_epoch":"0","nonce":"1","voting_power":"10000","pub_key":"BD7rDNYnoRW2fonLir583UAzk5oRkJoUSC0J1PK+t0Y3kw6szj775oKPnO7gxzCIlLYWlidt4ixIILu8RebudD8=","signer":"20D8433DC819AF087336A725422DF4CFBEF29710","last_updated":"","jailed":false,"proposer_priority":"-10000"},"total_voting_power":"20000"},"selected_producers":[{"val_id":"1","start_epoch":"0","end_epoch":"0","nonce":"1","voting_power":"10000","pub_key":"BD7rDNYnoRW2fonLir583UAzk5oRkJoUSC0J1PK+t0Y3kw6szj775oKPnO7gxzCIlLYWlidt4ixIILu8RebudD8=","signer":"20D8433DC819AF087336A725422DF4CFBEF29710","last_updated":"","jailed":false,"proposer_priority":"-10000"},{"val_id":"2","start_epoch":"0","end_epoch":"0","nonce":"1","voting_power":"10000","pub_key":"BP5UKZ9Pgw+j294NZvMSefMAtoyOhxPqNFRJKL1FxKvAHqbh/vxmo9xtLmsWDLtwCJ8rhzl9IdY3V9qVEzPw6sA=","signer":"735DE19A997EF33A090C873D1AC27F99D77B843C","last_updated":"","jailed":false,"proposer_priority":"10000"}],"bor_chain_id":"2756"}]}`)
+
+	var v SpanListResponseV2
+
+	err := json.Unmarshal(output, &v)
+	require.Nil(t, err)
+
+	list, err := v.ToList()
+	require.Nil(t, err)
+	require.Len(t, list, 1)
+}
 
 func TestSpanJsonMarshall(t *testing.T) {
 	validators := []*valset.Validator{
