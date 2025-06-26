9f13e8657afe77c36b6d1e42efe0ea226e223825
JkLondon <ilya@mikheev.fun>
2025-06-26 11:04:42 +0200

    commit

--- .github/workflows/scripts/run_rpc_tests.sh
+++ .github/workflows/scripts/run_rpc_tests.sh
@@ -2,6 +2,21 @@
 
 set +e # Disable exit on error
 
+manual=false
+for arg in "$@"; do
+  if [[ $arg == "--manual" ]]; then
+    manual=true
+  fi
+done
+
+if $manual; then
+  echo "Running manual setup…"
+  python3 -m venv .venv
+  source .venv/bin/activate
+  pip3 install -r ../requirements.txt
+  echo "Manual setup complete."
+fi
+
 # Array of disabled tests
 disabled_tests=(
     # Failing after the PR https://github.com/erigontech/erigon/pull/13617 that fixed this incompatibility
@@ -36,6 +51,6 @@ disabled_tests=(
 # Transform the array into a comma-separated string
 disabled_test_list=$(IFS=,; echo "${disabled_tests[*]}")
 
-python3 ./run_tests.py --port 8545 --engine-port 8545 --continue -f --json-diff -x "$disabled_test_list" 
+python3 ./run_tests.py --port 8545 --engine-port 8545 --continue -f --json-diff -x "$disabled_test_list"
 
 exit $?
