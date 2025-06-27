2b56897438ffa82af853a5a7c24cb68006709cec
kewei <kewei.train@gmail.com>
2025-06-28 00:03:18 +0200

    debug

--- cl/utils/eth_clock/ethereum_clock.go
+++ cl/utils/eth_clock/ethereum_clock.go
@@ -135,17 +135,17 @@ func (t *ethereumClockImpl) GetCurrentEpoch() uint64 {
 func (t *ethereumClockImpl) CurrentForkDigest() (common.Bytes4, error) {
 	currentEpoch := t.GetCurrentEpoch()
 	// Retrieve current fork version.
-	/*
-		currentForkVersion := utils.Uint32ToBytes4(uint32(t.beaconCfg.GenesisForkVersion))
-		for _, fork := range forkList(t.beaconCfg.ForkVersionSchedule) {
-			if currentEpoch >= fork.epoch {
-				currentForkVersion = fork.version
-				continue
-			}
-			break
+
+	currentForkVersion := utils.Uint32ToBytes4(uint32(t.beaconCfg.GenesisForkVersion))
+	for _, fork := range forkList(t.beaconCfg.ForkVersionSchedule) {
+		if currentEpoch >= fork.epoch {
+			currentForkVersion = fork.version
+			continue
 		}
-		return t.computeForkDigestForVersion(currentForkVersion)*/
-	return t.ComputeForkDigest(currentEpoch)
+		break
+	}
+	return t.computeForkDigestForVersion(currentForkVersion)
+	//return t.ComputeForkDigest(currentEpoch)
 }
 
 func (t *ethereumClockImpl) NextForkDigest() (common.Bytes4, error) {
