308b1d1253cb49dbf235edc0f0426c79ea5afb72
alex <AskAlexSharov@gmail.com>
2025-06-28 10:17:02 +0700

    save

--- turbo/jsonrpc/eth_receipts.go
+++ turbo/jsonrpc/eth_receipts.go
@@ -475,7 +475,7 @@ func (api *APIImpl) GetTransactionReceipt(ctx context.Context, txnHash common.Ha
 		return nil, nil
 	}
 
-	if txNumMin+2 > txNum { //TODO: what a magic is this "2" and how to avoid it
+	if txNumMin+1 > txNum { //TODO: what a magic is this "2" and how to avoid it
 		return nil, fmt.Errorf("uint underflow txnums error txNum: %d, txNumMin: %d, blockNum: %d", txNum, txNumMin, blockNum)
 	}
 
@@ -484,7 +484,7 @@ func (api *APIImpl) GetTransactionReceipt(ctx context.Context, txnHash common.Ha
 		return nil, err
 	}
 
-	var txnIndex = int(txNum - txNumMin - 2)
+	var txnIndex = int(txNum - txNumMin - 1)
 
 	txn, err := api._blockReader.TxnByIdxInBlock(ctx, tx, header.Number.Uint64(), txnIndex)
 	if err != nil {
