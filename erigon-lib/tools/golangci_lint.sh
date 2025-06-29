272f485d73f1f123bb445eee2e506180697cfdc2
JkLondon <ilya@mikheev.fun>
2025-06-29 15:51:37 +0200

    commit

--- erigon-lib/tools/golangci_lint.sh
+++ erigon-lib/tools/golangci_lint.sh
@@ -2,7 +2,7 @@
 
 scriptDir=$(dirname "${BASH_SOURCE[0]}")
 scriptName=$(basename "${BASH_SOURCE[0]}")
-version="v2.1.6"
+version="v2.2.0"
 
 if [[ "$1" == "--install-deps" ]]
 then
@@ -10,12 +10,13 @@ then
 	exit
 fi
 
-if ! which golangci-lint > /dev/null
-then
-	echo "golangci-lint tool is not found, install it with:"
-	echo "    make lint-deps"
-	echo "or follow https://golangci-lint.run/usage/install/"
-	exit 2
+bin_path="$install_dir/golangci-lint"
+
+if [ ! -x "$bin_path" ]; then
+  echo "golangci-lint tool is not found in $install_dir, install it with:"
+  echo "    make lint-deps"
+  echo "or follow https://golangci-lint.run/usage/install/"
+  exit 2
 fi
 
-golangci-lint run --config ./.golangci.yml
+"$bin_path" run --config ./.golangci.yml
