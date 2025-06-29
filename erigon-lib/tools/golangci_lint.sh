4810bf8f9105b5e573692d69f07cce9777bbc97b
JkLondon <ilya@mikheev.fun>
2025-06-29 15:45:47 +0200

    commit

--- erigon-lib/tools/golangci_lint.sh
+++ erigon-lib/tools/golangci_lint.sh
@@ -2,7 +2,7 @@
 
 scriptDir=$(dirname "${BASH_SOURCE[0]}")
 scriptName=$(basename "${BASH_SOURCE[0]}")
-version="v2.1.5"
+version="v2.2.0"
 
 install_dir="$(go env GOBIN)"
 if [ -z "$install_dir" ]; then
