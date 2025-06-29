907b642eeb612476b920c9fe45f4047b1482b89f
kewei <kewei.train@gmail.com>
2025-06-29 18:16:20 +0200

    lint warning

--- cl/persistence/blob_storage/data_column_db.go
+++ cl/persistence/blob_storage/data_column_db.go
@@ -229,7 +229,7 @@ func (s *dataColumnStorageImpl) RemoveColumnSidecar(ctx context.Context, slot ui
 			countBytes := make([]byte, 4)
 			binary.LittleEndian.PutUint32(countBytes, count)
 			newBytes := make([]byte, 4+count*4)
-			copy(newBytes[:4], countBytes[:])
+			copy(newBytes[:4], countBytes)
 			copy(newBytes[4:], bytes[4:4+i*4])
 			copy(newBytes[4+i*4:], bytes[4+(i+1)*4:])
 			if err := tx.Put(kv.BlockRootToDataColumnCount, blockRoot[:], newBytes); err != nil { // truncate bytes
