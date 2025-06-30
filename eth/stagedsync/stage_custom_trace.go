1dfb7e5ca7ec4030352308ba91653edbdf28ad22
alex <AskAlexSharov@gmail.com>
2025-06-30 15:51:11 +0700

    save

--- eth/stagedsync/stage_custom_trace.go
+++ eth/stagedsync/stage_custom_trace.go
@@ -275,25 +275,6 @@ func customTraceBatchProduce(ctx context.Context, produce Produce, cfg *exec3.Ex
 		if err := tx.Commit(); err != nil {
 			return err
 		}
-
-		if dbg.AssertEnabled {
-			if produce.LogAddr {
-				db.ViewTemporal(ctx, func(tx kv.TemporalTx) error {
-					txNumsReader := cfg.BlockReader.TxnumReader(ctx)
-					_min, _ := txNumsReader.Min(tx, fromBlock)
-					_max, _ := txNumsReader.Max(tx, toBlock)
-
-					for i, _ := range di.logAddrs {
-						it, _ := tx.IndexRange(kv.LogAddrIdx, di.logAddrs[i][:], int(_min), int(_max), true, kv.Unlim)
-						isEmpty := !it.HasNext()
-						if isEmpty {
-							panic(fmt.Sprintf("assert: logAddr index is empty for range %d-%d, fromBlock=%d, toBlock=%d, addr=%x", _min, _min, fromBlock, toBlock, di.logAddrs[i]))
-						}
-					}
-					return nil
-				})
-			}
-		}
 	}
 
 	agg := db.(state2.HasAgg).Agg().(*state2.Aggregator)
