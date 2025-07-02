91bbfc834a568c93d05a7ca29ef78fc184c87abb
kewei <kewei.train@gmail.com>
2025-07-02 14:57:48 +0200

    debug

--- cl/sentinel/communication/ssz_snappy/encoding.go
+++ cl/sentinel/communication/ssz_snappy/encoding.go
@@ -117,12 +117,16 @@ func ReadUvarint(r io.Reader) (x, n uint64, err error) {
 		b := uint64(currByte[0])
 		x |= (b & 0x7F) << shift
 		if (b & 0x80) == 0 {
+			// Check for overflow on the last byte
+			if shift == 63 && b > 1 {
+				return 0, n, errors.New("varint overflows a 64-bit integer")
+			}
 			return x, n, nil
 		}
 	}
 
 	// The number is too large to represent in a 64-bit value.
-	return 0, n, nil
+	return 0, n, errors.New("varint overflows a 64-bit integer")
 }
 
 func DecodeListSSZ(data []byte, count uint64, list []ssz.EncodableSSZ, b *clparams.BeaconChainConfig, ethClock eth_clock.EthereumClock) error {
