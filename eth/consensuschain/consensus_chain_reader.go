39b5d75f046776b39d2964e1dca2830586c76828
alex <AskAlexSharov@gmail.com>
2025-07-04 11:59:10 +0700

    save

--- eth/consensuschain/consensus_chain_reader.go
+++ eth/consensuschain/consensus_chain_reader.go
@@ -28,6 +28,7 @@ import (
 	"github.com/erigontech/erigon-lib/log/v3"
 	"github.com/erigontech/erigon-lib/rlp"
 	"github.com/erigontech/erigon-lib/types"
+	"github.com/erigontech/erigon/polygon/heimdall"
 	"github.com/erigontech/erigon/turbo/services"
 )
 
@@ -124,4 +125,13 @@ func (cr Reader) BorEventsByBlock(hash common.Hash, number uint64) []rlp.RawValu
 		return nil
 	}
 	return events
+}
+
+func (cr Reader) BorSpan(spanId uint64) *heimdall.Span {
+	span, _, err := cr.blockReader.Span(context.Background(), cr.tx, spanId)
+	if err != nil {
+		log.Warn("BorSpan failed", "err", err)
+		return nil
+	}
+	return span
 }
