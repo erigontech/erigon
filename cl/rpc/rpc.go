94a8e28e007493d98c62b191246ccc7488c425e5
Giulio <giulio.rebuffo@gmail.com>
2025-07-07 16:46:52 +0200

    save

--- cl/rpc/rpc.go
+++ cl/rpc/rpc.go
@@ -183,9 +183,8 @@ func (b *BeaconRpcP2P) SendColumnSidecarsByRangeReqV1(
 func (b *BeaconRpcP2P) TestSendColumnSidecarsByRangeReqV1(
 	ctx context.Context,
 ) ([]*cltypes.DataColumnSidecar, string, error) {
-	req := &cltypes.ColumnSidecarsByRangeRequest{
-		StartSlot: 80590,
-		Count:     2,
+	req := &cltypes.DataColumnsByRootIdentifier{
+		BlockRoot: common.HexToHash("0x30f7d463e46398ec0cd7e91ceac838b6f74740f3d804942a3e44feda592a3d9e"),
 		Columns:   solid.NewUint64ListSSZ(int(b.beaconConfig.NumberOfColumns)),
 	}
 	req.Columns.Append(1)
