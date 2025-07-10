65ab95a3fabf0282c92dfe0afc26cf897c43bd1b
Matt Joiner <anacrolix@gmail.com>
2025-07-10 23:43:07 +1000

    Add error recovery code to erigon-lib/common/debug (#15988)
    Had a consistent panic occurring in amoy. I had touched it before to
    make the stack traces more obvious. I abstracted them out as a possible
    pattern for logging and recovering generally.

--- erigon-lib/common/debug/log_panic.go
+++ erigon-lib/common/debug/log_panic.go
@@ -17,12 +17,16 @@
 package debug
 
 import (
+	"bytes"
+	"fmt"
 	"os"
+	"runtime/debug"
 	"sync/atomic"
 	"syscall"
 
 	"github.com/erigontech/erigon-lib/common/dbg"
 	"github.com/erigontech/erigon-lib/log/v3"
+	"golang.org/x/sync/errgroup"
 )
 
 var sigc atomic.Value
@@ -31,7 +35,7 @@ func GetSigC(sig *chan os.Signal) {
 	sigc.Store(*sig)
 }
 
-// LogPanic - does log panic to logger and to <datadir>/crashreports then stops the process
+// LogPanic - does log panic to logger then stops the process
 func LogPanic() {
 	panicResult := recover()
 	if panicResult == nil {
@@ -42,4 +46,54 @@ func LogPanic() {
 	if sl := sigc.Load(); sl != nil {
 		sl.(chan os.Signal) <- syscall.SIGINT
 	}
+}
+
+// Recovers errors, logs the stack trace and sets an error value.
+func RecoverPanicIntoError(logger log.Logger, outErr *error) {
+	if *outErr != nil {
+		// Don't swallow panics if an error is already set. This is an unrecoverable situation.
+		return
+	}
+	r := recover()
+	if r == nil {
+		return
+	}
+	err, ok := r.(error)
+	if !ok {
+		panic(r)
+	}
+	*outErr = err
+	logger.Crit("recovered panic", "err", err, "stack", string(debug.Stack()))
+}
+
+// Runs errgroup.Group.Wait and recovers from errgroup.PanicError.
+func WaitErrGroupRecoverPanicError(eg *errgroup.Group, logger log.Logger) (err error) {
+	defer func() {
+		if err != nil {
+			return
+		}
+		r := recover()
+		if r == nil {
+			return
+		}
+		// Don't check for nested error as it's expected that we are the first to handle the error.
+		pe, ok := r.(errgroup.PanicError)
+		if !ok {
+			panic(r)
+		}
+		// We really don't want to miss the panic stack traces.
+		var buf bytes.Buffer
+		fmt.Fprintf(&buf, "panicked with error: %v\n%s", pe.Recovered, pe.Stack)
+		fmt.Fprintf(&buf, "recovered by %s", debug.Stack())
+		os.Stderr.Write(buf.Bytes())
+		// Do we need context for where the Wait occurred? Because we do the Wait here, we know the
+		// stack trace between the Wait and the callsite is predictable and uninteresting.
+		logger.Crit(
+			"recovered from errgroup.PanicError",
+			"err", pe.Recovered,
+			"panic-stack", string(pe.Stack),
+			"recover-stack", string(debug.Stack()))
+		err = pe.Recovered
+	}()
+	return eg.Wait()
 }
