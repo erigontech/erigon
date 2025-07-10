erigontech/erigon

commit b7ab135e80f629eb945b836a75ab372a3e291d27

Expose debug mux to downstream components (#15982)
This passes the metrics or pprof mux top the downloader for torrents
handling.

Note the handler url is changed to: `/downloader/torrents`

Fixes: https://github.com/erigontech/erigon/issues/14637

cmd/downloader/main.go
@@ -22,6 +22,7 @@ import (
 	"fmt"
 	"io/fs"
 	"net"
+	"net/http"
 	"net/url"
 	"os"
 	"path/filepath"
@@ -302,7 +303,7 @@ func Downloader(ctx context.Context, logger log.Logger) error {
 	defer d.Close()
 	logger.Info("[snapshots] Start bittorrent server", "my_peer_id", fmt.Sprintf("%x", d.TorrentClient().PeerID()))
 
-	d.HandleTorrentClientStatus()
+	d.HandleTorrentClientStatus(http.DefaultServeMux)
 
 	err = d.AddTorrentsFromDisk(ctx)
 	if err != nil {

erigon-db/downloader/downloader.go
@@ -1346,8 +1346,8 @@ func (d *Downloader) Completed() bool {
 
 // Expose torrent client status to HTTP on the public/default serve mux used by GOPPROF=http. Only
 // do this if you have a single instance.
-func (d *Downloader) HandleTorrentClientStatus() {
-	http.HandleFunc("/downloaderTorrentClientStatus", func(w http.ResponseWriter, r *http.Request) {
+func (d *Downloader) HandleTorrentClientStatus(debugMux *http.ServeMux) {
+	debugMux.HandleFunc("/downloader/torrents", func(w http.ResponseWriter, r *http.Request) {
 		d.torrentClient.WriteStatus(w)
 	})
 }

cmd/erigon/main.go
@@ -68,6 +68,14 @@ func runErigon(cliCtx *cli.Context) error {
 		return err
 	}
 
+	debugMux := metricsMux
+
+	if debugMux == nil {
+		debugMux = pprofMux
+	} else {
+		debugMux = http.DefaultServeMux
+	}
+
 	// initializing the node and providing the current git commit there
 
 	logger.Info("Build info", "git_branch", params.GitBranch, "git_tag", params.GitTag, "git_commit", params.GitCommit)
@@ -86,7 +94,7 @@ func runErigon(cliCtx *cli.Context) error {
 	erigonInfoGauge := metrics.GetOrCreateGauge(fmt.Sprintf(`erigon_info{version="%s",commit="%s"}`, params.Version, params.GitCommit))
 	erigonInfoGauge.Set(1)
 
-	nodeCfg, err := node.NewNodConfigUrfave(cliCtx, logger)
+	nodeCfg, err := node.NewNodConfigUrfave(cliCtx, debugMux, logger)
 	if err != nil {
 		return err
 	}

cmd/sentry/main.go
@@ -89,7 +89,7 @@ var rootCmd = &cobra.Command{
 	},
 	RunE: func(cmd *cobra.Command, args []string) error {
 		dirs := datadir.New(datadirCli)
-		nodeConfig := node2.NewNodeConfig()
+		nodeConfig := node2.NewNodeConfig(nil)
 		p2pConfig, err := utils.NewP2PConfig(
 			nodiscover,
 			dirs,

cmd/devnet/devnet/node.go
@@ -175,9 +175,17 @@ func (n *devnetNode) run(ctx *cli.Context) error {
 		return err
 	}
 
+	debugMux := metricsMux
+
+	if debugMux == nil {
+		debugMux = pprofMux
+	} else {
+		debugMux = http.DefaultServeMux
+	}
+
 	logger.Info("Build info", "git_branch", params.GitBranch, "git_tag", params.GitTag, "git_commit", params.GitCommit)
 
-	nodeConf, err := enode.NewNodConfigUrfave(ctx, logger)
+	nodeConf, err := enode.NewNodConfigUrfave(ctx, debugMux, logger)
 	if err != nil {
 		return err
 	}

eth/backend.go
@@ -420,7 +420,7 @@ func New(ctx context.Context, stack *node.Node, config *ethconfig.Config, logger
 	}
 
 	// Can happen in some configurations
-	if err := backend.setUpSnapDownloader(ctx, config.Downloader, chainConfig); err != nil {
+	if err := backend.setUpSnapDownloader(ctx, stack.Config(), config.Downloader, chainConfig); err != nil {
 		return nil, err
 	}
 
@@ -1473,7 +1473,7 @@ func (s *Ethereum) NodesInfo(limit int) (*remote.NodesInfoReply, error) {
 }
 
 // sets up blockReader and client downloader
-func (s *Ethereum) setUpSnapDownloader(ctx context.Context, downloaderCfg *downloadercfg.Cfg, cc *chain.Config) error {
+func (s *Ethereum) setUpSnapDownloader(ctx context.Context, nodeCfg *nodecfg.Config, downloaderCfg *downloadercfg.Cfg, cc *chain.Config) error {
 	var err error
 	s.chainDB.OnFilesChange(func(frozenFileNames []string) {
 		s.logger.Warn("files changed...sending notification")
@@ -1518,7 +1518,7 @@ func (s *Ethereum) setUpSnapDownloader(ctx context.Context, downloaderCfg *downl
 			return err
 		}
 
-		s.downloader.HandleTorrentClientStatus()
+		s.downloader.HandleTorrentClientStatus(nodeCfg.DebugMux)
 
 		bittorrentServer, err := downloader.NewGrpcServer(s.downloader)
 		if err != nil {

node/nodecfg/config.go
@@ -21,6 +21,7 @@ package nodecfg
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -171,6 +172,8 @@ type Config struct {
 	HealthCheck bool
 
 	Http httpcfg.HttpCfg
+
+	DebugMux *http.ServeMux
 }
 
 // IPCEndpoint resolves an IPC endpoint based on a configured value, taking into

turbo/app/import_cmd.go
@@ -80,7 +80,7 @@ func importChain(cliCtx *cli.Context) error {
 		return err
 	}
 
-	nodeCfg, err := turboNode.NewNodConfigUrfave(cliCtx, logger)
+	nodeCfg, err := turboNode.NewNodConfigUrfave(cliCtx, nil, logger)
 	if err != nil {
 		return err
 	}

commit 02ea0c17fafb217e4e2ee21210f5476c1be5b308

Fix panic in debug muxxing and restore GOPPROF handler route
Panic introduced by https://github.com/erigontech/erigon/pull/15982, and route restored per https://github.com/erigontech/erigon/pull/15982#issuecomment-3048460301.

cmd/downloader/main.go
@@ -22,7 +22,6 @@ import (
 	"fmt"
 	"io/fs"
 	"net"
-	"net/http"
 	"net/url"
 	"os"
 	"path/filepath"
@@ -303,7 +302,7 @@ func Downloader(ctx context.Context, logger log.Logger) error {
 	defer d.Close()
 	logger.Info("[snapshots] Start bittorrent server", "my_peer_id", fmt.Sprintf("%x", d.TorrentClient().PeerID()))
 
-	d.HandleTorrentClientStatus(http.DefaultServeMux)
+	d.HandleTorrentClientStatus(nil)
 
 	err = d.AddTorrentsFromDisk(ctx)
 	if err != nil {

erigon-db/downloader/downloader.go
@@ -1344,12 +1344,17 @@ func (d *Downloader) Completed() bool {
 	return d.allTorrentsComplete()
 }
 
-// Expose torrent client status to HTTP on the public/default serve mux used by GOPPROF=http. Only
-// do this if you have a single instance.
+// Expose torrent client status to HTTP on the public/default serve mux used by GOPPROF=http, and
+// the provided "debug" mux if non-nil. Only do this if you have a single instance of a Downloader.
 func (d *Downloader) HandleTorrentClientStatus(debugMux *http.ServeMux) {
-	debugMux.HandleFunc("/downloader/torrents", func(w http.ResponseWriter, r *http.Request) {
+	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		d.torrentClient.WriteStatus(w)
 	})
+	p := "/downloader/torrentClientStatus"
+	http.Handle(p, h)
+	if debugMux != nil {
+		debugMux.Handle(p, h)
+	}
 }
 
 func (d *Downloader) spawn(f func()) {