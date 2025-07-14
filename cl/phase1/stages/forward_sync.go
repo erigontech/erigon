3e81c2b6b0794075c62f3dbde27aaa5e769db27c
kewei <kewei.train@gmail.com>
2025-07-14 20:33:46 +0800

    update

--- cl/phase1/stages/forward_sync.go
+++ cl/phase1/stages/forward_sync.go
@@ -118,14 +118,14 @@ func downloadBlobs(ctx context.Context, logger log.Logger, cfg *Cfg, highestBloc
 			denebBlocks = append(denebBlocks, block)
 		}
 	}
-
-	if len(denebBlocks) > 0 && shouldProcessBlobs(denebBlocks, cfg) {
-		_, err = downloadAndProcessEip4844DA(ctx, logger, cfg, highestBlockProcessed, denebBlocks)
-		if err != nil {
-			logger.Trace("[Caplin] Failed to process blobs", "err", err)
-			return err
-		}
-	}
+	/*
+		if len(denebBlocks) > 0 && shouldProcessBlobs(denebBlocks, cfg) {
+			_, err = downloadAndProcessEip4844DA(ctx, logger, cfg, highestBlockProcessed, denebBlocks)
+			if err != nil {
+				logger.Trace("[Caplin] Failed to process blobs", "err", err)
+				return err
+			}
+		}*/
 
 	if len(fuluBlocks) > 0 && canDownloadColumnData(fuluBlocks, cfg) {
 		wg := sync.WaitGroup{} // TODO: remove this bad fix
