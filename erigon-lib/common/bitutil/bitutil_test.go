b4d728775c8474b1456e353e65d2231bfe1549c0
kilavvy <140459108+kilavvy@users.noreply.github.com>
2025-07-14 23:43:01 +0200

    Update bitutil_test.go

--- erigon-lib/common/bitutil/bitutil_test.go
+++ erigon-lib/common/bitutil/bitutil_test.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Adapted from: https://golang.org/src/crypto/cipher/xor_test.go
+// Adapted from: https://cs.opensource.google/go/go/+/refs/tags/go1.21.5:src/crypto/cipher/xor_test.go
 
 package bitutil
 
