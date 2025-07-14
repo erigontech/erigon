ed2d9e4a1e7b7ffdc7ead6a2e9bb588010becd8a
kilavvy <140459108+kilavvy@users.noreply.github.com>
2025-07-14 23:43:33 +0200

    Update bitutil.go

--- erigon-lib/common/bitutil/bitutil.go
+++ erigon-lib/common/bitutil/bitutil.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Adapted from: https://golang.org/src/crypto/cipher/xor.go
+// Adapted from: https://cs.opensource.google/go/go/+/refs/tags/go1.21.5:src/crypto/cipher/xor.go
 
 // Package bitutil implements fast bitwise operations.
 package bitutil
