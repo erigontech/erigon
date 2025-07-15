e53a566bd0bf0e3eef00f8a8085e730c606e90f3
kewei <kewei.train@gmail.com>
2025-07-16 03:08:02 +0800

    update

--- cl/rpc/rpc.go
+++ cl/rpc/rpc.go
@@ -131,7 +131,7 @@ func (b *BeaconRpcP2P) SendColumnSidecarsByRootIdentifierReq(
 	}
 
 	data := common.CopyBytes(buffer.Bytes())
-	responsePacket, pid, err := b.sendRequestWithPeer(ctx, communication.DataColumnSidecarsByRootProtocolV1, data, pid)
+	responsePacket, _, err := b.sendRequestWithPeer(ctx, communication.DataColumnSidecarsByRootProtocolV1, data, pid)
 	if err != nil {
 		return nil, pid, 0, err
 	}
