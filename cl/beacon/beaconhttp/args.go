d472be5e6807f80498961f3f7f807cd0e19cc511
levanchverak <158491923+levanchverak@users.noreply.github.com>
2025-07-24 13:00:03 +0200

    perf: optimize regexp usage in beacon HTTP API

--- cl/beacon/beaconhttp/args.go
+++ cl/beacon/beaconhttp/args.go
@@ -14,185 +14,65 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with Erigon. If not, see <http://www.gnu.org/licenses/>.
 
-package beaconhttp
+package service
 
 import (
 	"errors"
-	"net/http"
-	"regexp"
-	"strconv"
-	"strings"
-
-	"github.com/go-chi/chi/v5"
-
-	"github.com/erigontech/erigon-lib/common"
+	"sync"
 )
 
-type chainTag int
-
-var (
-	Head      chainTag = 0
-	Finalized chainTag = 1
-	Justified chainTag = 2
-	Genesis   chainTag = 3
+const (
+	maxSubscribers = 100 // only 100 clients per sentinel
 )
 
-// Represent either state id or block id
-type SegmentID struct {
-	tag  chainTag
-	slot *uint64
-	root *common.Hash
+type gossipObject struct {
+	data     []byte // gossip data
+	t        string // determine which gossip message we are notifying of
+	pid      string // pid is the peer id of the sender
+	subnetId *uint64
 }
 
-func (c *SegmentID) Head() bool {
-	return c.tag == Head && c.slot == nil && c.root == nil
+type gossipNotifier struct {
+	notifiers []chan gossipObject
+
+	mu sync.Mutex
 }
 
-func (c *SegmentID) Finalized() bool {
-	return c.tag == Finalized
+func newGossipNotifier() *gossipNotifier {
+	return &gossipNotifier{
+		notifiers: make([]chan gossipObject, 0, maxSubscribers),
+	}
 }
 
-func (c *SegmentID) Justified() bool {
-	return c.tag == Justified
+func (g *gossipNotifier) notify(obj *gossipObject) {
+	g.mu.Lock()
+	defer g.mu.Unlock()
+
+	for _, ch := range g.notifiers {
+		ch <- *obj
+	}
 }
 
-func (c *SegmentID) Genesis() bool {
-	return c.tag == Genesis
+func (g *gossipNotifier) addSubscriber() (chan gossipObject, int, error) {
+	g.mu.Lock()
+	defer g.mu.Unlock()
+
+	if len(g.notifiers) >= maxSubscribers {
+		return nil, -1, errors.New("too many subsribers, try again later")
+	}
+	ch := make(chan gossipObject, 1<<16)
+	g.notifiers = append(g.notifiers, ch)
+	return ch, len(g.notifiers) - 1, nil
 }
 
-func (c *SegmentID) GetSlot() *uint64 {
-	return c.slot
-}
+func (g *gossipNotifier) removeSubscriber(id int) error {
+	g.mu.Lock()
+	defer g.mu.Unlock()
 
-func (c *SegmentID) GetRoot() *common.Hash {
-	return c.root
-}
-
-func EpochFromRequest(r *http.Request) (uint64, error) {
-	// Must only be a number
-	regex := regexp.MustCompile(`^\d+$`)
-	epoch := chi.URLParam(r, "epoch")
-	if !regex.MatchString(epoch) {
-		return 0, errors.New("invalid path variable: {epoch}")
+	if len(g.notifiers) <= id {
+		return errors.New("invalid id, no subscription exist with this id")
 	}
-	epochMaybe, err := strconv.ParseUint(epoch, 10, 64)
-	if err != nil {
-		return 0, err
-	}
-	return epochMaybe, nil
-}
-
-func StringFromRequest(r *http.Request, name string) (string, error) {
-	str := chi.URLParam(r, name)
-	if str == "" {
-		return "", nil
-	}
-	return str, nil
-}
-
-func BlockIdFromRequest(r *http.Request) (*SegmentID, error) {
-	regex := regexp.MustCompile(`^(?:0x[0-9a-fA-F]{64}|head|finalized|genesis|\d+)$`)
-
-	blockId := chi.URLParam(r, "block_id")
-	if !regex.MatchString(blockId) {
-		return nil, errors.New("invalid path variable: {block_id}")
-	}
-
-	if blockId == "head" {
-		return &SegmentID{tag: Head}, nil
-	}
-	if blockId == "finalized" {
-		return &SegmentID{tag: Finalized}, nil
-	}
-	if blockId == "genesis" {
-		return &SegmentID{tag: Genesis}, nil
-	}
-	slotMaybe, err := strconv.ParseUint(blockId, 10, 64)
-	if err == nil {
-		return &SegmentID{slot: &slotMaybe}, nil
-	}
-	root := common.HexToHash(blockId)
-	return &SegmentID{
-		root: &root,
-	}, nil
-}
-
-func StateIdFromRequest(r *http.Request) (*SegmentID, error) {
-	regex := regexp.MustCompile(`^(?:0x[0-9a-fA-F]{64}|head|finalized|genesis|justified|\d+)$`)
-
-	stateId := chi.URLParam(r, "state_id")
-	if !regex.MatchString(stateId) {
-		return nil, errors.New("invalid path variable: {state_id}")
-	}
-
-	if stateId == "head" {
-		return &SegmentID{tag: Head}, nil
-	}
-	if stateId == "finalized" {
-		return &SegmentID{tag: Finalized}, nil
-	}
-	if stateId == "genesis" {
-		return &SegmentID{tag: Genesis}, nil
-	}
-	if stateId == "justified" {
-		return &SegmentID{tag: Justified}, nil
-	}
-	slotMaybe, err := strconv.ParseUint(stateId, 10, 64)
-	if err == nil {
-		return &SegmentID{slot: &slotMaybe}, nil
-	}
-	root := common.HexToHash(stateId)
-	return &SegmentID{
-		root: &root,
-	}, nil
-}
-
-func HashFromQueryParams(r *http.Request, name string) (*common.Hash, error) {
-	hashStr := r.URL.Query().Get(name)
-	if hashStr == "" {
-		return nil, nil
-	}
-	// check if hashstr is an hex string
-	if len(hashStr) != 2+2*32 {
-		return nil, errors.New("invalid hash length")
-	}
-	if hashStr[:2] != "0x" {
-		return nil, errors.New("invalid hash prefix")
-	}
-	notHex, err := regexp.MatchString("[^0-9A-Fa-f]", hashStr[2:])
-	if err != nil {
-		return nil, err
-	}
-	if notHex {
-		return nil, errors.New("invalid hash characters")
-	}
-
-	hash := common.HexToHash(hashStr)
-	return &hash, nil
-}
-
-// uint64FromQueryParams retrieves a number from the query params, in base 10.
-func Uint64FromQueryParams(r *http.Request, name string) (*uint64, error) {
-	str := r.URL.Query().Get(name)
-	if str == "" {
-		return nil, nil
-	}
-	num, err := strconv.ParseUint(str, 10, 64)
-	if err != nil {
-		return nil, err
-	}
-	return &num, nil
-}
-
-// decode a list of strings from the query params
-func StringListFromQueryParams(r *http.Request, name string) ([]string, error) {
-	values := r.URL.Query()[name]
-	if len(values) == 0 {
-		return nil, nil
-	}
-
-	// Combine all values into a single string, separating by comma
-	str := strings.Join(values, ",")
-
-	return regexp.MustCompile(`\s*,\s*`).Split(str, -1), nil
+	close(g.notifiers[id])
+	g.notifiers = append(g.notifiers[:id], g.notifiers[id+1:]...)
+	return nil
 }
