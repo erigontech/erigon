6ed3d302b84f6163b702eba78ddc0afdeb84ef31
Giulio <giulio.rebuffo@gmail.com>
2025-07-26 23:22:46 +0200

    save

--- cl/phase1/stages/stage_history_download.go
+++ cl/phase1/stages/stage_history_download.go
@@ -463,11 +463,11 @@ func downloadBlobHistoryWorker(cfg StageHistoryReconstructionCfg, ctx context.Co
 		// WIP: easy but need to think about this.
 
 		if len(postFuluBlocks) > 0 {
-			peerdas := cfg.forkchoiceState.GetPeerDas()
-			if err := peerdas.DownloadColumnsAndRecoverBlobs(ctx, postFuluBlocks); err != nil {
+			if err := cfg.forkchoiceState.GetPeerDas().DownloadColumnsAndRecoverBlobs(ctx, postFuluBlocks); err != nil {
 				return fmt.Errorf("error downloading blobs for post-fulu blocks: %w", err)
 			}
 		}
+		fmt.Println("[Blobs-Downloader] Downloaded blobs for slot", "blocks", len(batch), "postFuluBlocks", len(postFuluBlocks), "preFuluBlocks", len(preFuluBlocks), "currentSlot", currentSlot)
 		time.Sleep(cfg.backfillingThrottling) // throttle to 0.6 second for backfilling
 	}
 
