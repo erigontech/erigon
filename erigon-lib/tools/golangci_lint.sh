7889c1ca856ff20cec46816d48d6aa44d4e79fd8
alex <AskAlexSharov@gmail.com>
2025-07-26 12:14:46 +0700

    save

--- erigon-lib/tools/golangci_lint.sh
+++ erigon-lib/tools/golangci_lint.sh
@@ -4,24 +4,14 @@ scriptDir=$(dirname "${BASH_SOURCE[0]}")
 scriptName=$(basename "${BASH_SOURCE[0]}")
 version="v2.2.2"
 
-install_dir="$(go env GOBIN)"
-if [ -z "$install_dir" ]; then
-  install_dir="$(go env GOPATH)/bin"
-fi
-
-if [[ "$1" == "--install-deps" ]]
-then
-	curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$install_dir" "$version"
-	exit
-fi
-
+install_dir="./../build/bin"
 bin_path="$install_dir/golangci-lint"
 
-if [ ! -x "$bin_path" ]; then
-  echo "golangci-lint tool is not found in $install_dir, install it with:"
-  echo "    make lint-deps"
-  echo "or follow https://golangci-lint.run/usage/install/"
-  exit 2
+if [ -e $bin_path ]
+then
+  echo "installed"
+else
+	curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$install_dir" "$version"
 fi
 
 "$bin_path" run --config ./.golangci.yml
