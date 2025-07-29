1dc969b0cfcede95de6c6a6f24585a4ad21d1202
Matt Joiner <anacrolix@gmail.com>
2025-07-29 15:24:34 +1000

    Avoid logging zero torrents initializing on adding empty snapshots items

--- erigon-db/downloader/downloader_grpc_server.go
+++ erigon-db/downloader/downloader_grpc_server.go
@@ -54,6 +54,10 @@ type GrpcServer struct {
 // After "download once" - Erigon will produce and seed new files
 // Downloader will able: seed new files (already existing on FS), download uncomplete parts of existing files (if Verify found some bad parts)
 func (s *GrpcServer) Add(ctx context.Context, request *proto_downloader.AddRequest) (*emptypb.Empty, error) {
+	if len(request.Items) == 0 {
+		// Avoid logging initializing 0 torrents.
+		return nil, nil
+	}
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 	defer s.d.ResetLogInterval()
