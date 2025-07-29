3d94371183499c3983cbda61b92b0cad7308107b
yperbasis <andrey.ashikhmin@gmail.com>
2025-07-29 10:13:33 +0200

    postfix

--- cl/sentinel/handlers/test_helpers.go
+++ cl/sentinel/handlers/test_helpers.go
@@ -3,14 +3,14 @@ package handlers
 import (
 	"testing"
 
+	"github.com/erigontech/erigon-lib/chain/networkid"
 	"github.com/erigontech/erigon/cl/clparams/initial_state"
 	"github.com/erigontech/erigon/cl/utils/eth_clock"
-	"github.com/erigontech/erigon/execution/chainspec"
 	"github.com/stretchr/testify/require"
 )
 
 func getEthClock(t *testing.T) eth_clock.EthereumClock {
-	s, err := initial_state.GetGenesisState(chainspec.MainnetChainID)
+	s, err := initial_state.GetGenesisState(networkid.MainnetChainID)
 	require.NoError(t, err)
 	return eth_clock.NewEthereumClock(s.GenesisTime(), s.GenesisValidatorsRoot(), s.BeaconConfig())
 }
