96f55b89dc7b310bb073eff6896c5c9eb346bed9
kewei <kewei.train@gmail.com>
2025-07-29 13:04:36 +0800

    data column sidecar event

--- cl/phase1/forkchoice/mock_services/forkchoice_mock.go
+++ cl/phase1/forkchoice/mock_services/forkchoice_mock.go
@@ -432,3 +432,7 @@ func (f *ForkChoiceStorageMock) GetPendingDeposits(blockRoot common.Hash) (*soli
 func (f *ForkChoiceStorageMock) GetPendingPartialWithdrawals(blockRoot common.Hash) (*solid.ListSSZ[*solid.PendingPartialWithdrawal], bool) {
 	return nil, false
 }
+
+func (f *ForkChoiceStorageMock) GetProposerLookahead(blockRoot common.Hash) (solid.Uint64VectorSSZ, bool) {
+	return nil, false
+}
