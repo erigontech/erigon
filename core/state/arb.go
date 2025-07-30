8548e2f746bb5e47d5a68989c24754e52711c2f3
Bartosz Zawistowski <39065214+bzawisto@users.noreply.github.com>
2025-07-30 16:29:56 +0200

    Fixing issues with solgen (#16370)

--- core/state/arb.go
+++ core/state/arb.go
@@ -357,6 +357,14 @@ func (s *IntraBlockState) GetStorageRoot(addr common.Address) common.Hash {
 func (sdb *IntraBlockState) SetWasmDB(wasmDB wasmdb.WasmIface) {
 	sdb.wasmDB = wasmDB
 }
+
+func (s *IntraBlockState) ExpectBalanceMint(amount *uint256.Int) {
+	if amount.Sign() < 0 {
+		panic(fmt.Sprintf("ExpectBalanceMint called with negative amount %v", amount))
+	}
+	s.arbExtraData.unexpectedBalanceDelta.Sub(s.arbExtraData.unexpectedBalanceDelta, amount)
+}
+
 func (sdb *IntraBlockState) ExpectBalanceBurn(amount *uint256.Int) {
 	if amount.Sign() < 0 {
 		panic(fmt.Sprintf("ExpectBalanceBurn called with negative amount %v", amount))
