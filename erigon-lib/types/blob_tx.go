5f7e61f7e4b787ed0674b7745104aff5f25d2571
yperbasis <andrey.ashikhmin@gmail.com>
2025-07-31 15:44:15 +0200

    fix TestExecutionSpecBlockchainDevnet

--- erigon-lib/types/blob_tx.go
+++ erigon-lib/types/blob_tx.go
@@ -356,9 +356,6 @@ func (stx *BlobTx) DecodeRLP(s *rlp.Stream) error {
 	if len(stx.BlobVersionedHashes) == 0 {
 		return errors.New("a blob stx must contain at least one blob")
 	}
-	if len(stx.BlobVersionedHashes) > params.MaxBlobsPerTxn {
-		return ErrTooManyBlobs
-	}
 	// decode V
 	if b, err = s.Uint256Bytes(); err != nil {
 		return err
