890f8aec388328e4dc478a250aa65f9496d09a97
sudeepdino008 <sudeepdino008@gmail.com>
2025-07-31 10:41:22 +0530

    fix polygon's ii and receipt values for system tx (#16360)
    issue: https://github.com/erigontech/erigon/issues/16339
    
    - enable tracer in final tx exec (for tracesfrom/tracesto)
    - use `ibs.SetTxContext` in final tx exec: this was causing txTask.Logs
    to be incorrectly set to nil.
    - remove unused hooks/tracers from fn signatures
    - need to regen files after this --
    logaddrs/logtopics/tracesfrom/tracesto/receipts.

--- erigon-lib/version/file_version.go
+++ erigon-lib/version/file_version.go
@@ -20,11 +20,15 @@ var (
 	ZeroVersion            = Version{}
 	V1_0          Version  = Version{1, 0}
 	V1_1          Version  = Version{1, 1}
+	V1_2          Version  = Version{1, 2}
 	V2_0          Version  = Version{2, 0}
+	V2_1          Version  = Version{2, 1}
 	V1_0_standart Versions = Versions{V1_0, V1_0}
 	V1_1_standart Versions = Versions{V1_1, V1_0}
+	V1_2_standart Versions = Versions{V1_2, V1_0}
 	V1_1_exact    Versions = Versions{V1_1, V1_1}
 	V2_0_standart Versions = Versions{V2_0, V1_0}
+	V2_1_standart Versions = Versions{V2_1, V1_0}
 )
 
 func (v Version) Less(rhd Version) bool {
