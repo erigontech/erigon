c062b3273ab3e3de631f292c3150f3b5d968c4c8
JkLondon <ilya@mikheev.fun>
2025-08-01 12:48:57 +0200

    commit

--- turbo/app/snapshots_cmd.go
+++ turbo/app/snapshots_cmd.go
@@ -1103,10 +1103,6 @@ func checkIfStateSnapshotsPublishable(dirs datadir.Dirs) error {
 			oldVersion := versioned.GetVersions().II.DataEF.Current
 			expectedFileName := strings.Replace(res.Name(), "accounts", snapType, 1)
 			expectedFileName = version.ReplaceVersion(expectedFileName, res.Version, oldVersion)
-			if snapType == "logtopics" {
-				println("version of schema:", libstate.Schema.LogTopicIdx.GetVersions().II.DataEF.String())
-				println("current:", oldVersion.String())
-			}
 
 			if _, err := os.Stat(filepath.Join(dirs.SnapIdx, expectedFileName)); err != nil {
 				return fmt.Errorf("missing file %s at path %s", expectedFileName, filepath.Join(dirs.SnapIdx, expectedFileName))
