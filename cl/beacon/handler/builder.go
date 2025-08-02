20b91233b360b122a878689fa5d1d43ae6fb90c8
Giulio <giulio.rebuffo@gmail.com>
2025-08-02 14:30:03 +0200

    save

--- cl/beacon/handler/builder.go
+++ cl/beacon/handler/builder.go
@@ -100,7 +100,6 @@ func (a *ApiHandler) GetEth1V1BuilderStatesExpectedWithdrawals(w http.ResponseWr
 }
 
 func (a *ApiHandler) PostEthV1BuilderRegisterValidator(w http.ResponseWriter, r *http.Request) (*beaconhttp.BeaconResponse, error) {
-	//fmt.Println("PostEthV1BuilderRegisterValidator")
 	registerReq := []*cltypes.ValidatorRegistration{}
 	if err := json.NewDecoder(r.Body).Decode(&registerReq); err != nil {
 		return nil, beaconhttp.NewEndpointError(http.StatusBadRequest, err)
