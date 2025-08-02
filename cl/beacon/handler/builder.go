87aa93df86e64a2b0e6247e4ce0a342ca9cd3b7f
Giulio <giulio.rebuffo@gmail.com>
2025-08-02 14:28:43 +0200

    save

--- cl/beacon/handler/builder.go
+++ cl/beacon/handler/builder.go
@@ -19,7 +19,6 @@ package handler
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
 	"net/http"
 
 	"github.com/erigontech/erigon-lib/common"
@@ -101,7 +100,7 @@ func (a *ApiHandler) GetEth1V1BuilderStatesExpectedWithdrawals(w http.ResponseWr
 }
 
 func (a *ApiHandler) PostEthV1BuilderRegisterValidator(w http.ResponseWriter, r *http.Request) (*beaconhttp.BeaconResponse, error) {
-	fmt.Println("PostEthV1BuilderRegisterValidator")
+	//fmt.Println("PostEthV1BuilderRegisterValidator")
 	registerReq := []*cltypes.ValidatorRegistration{}
 	if err := json.NewDecoder(r.Body).Decode(&registerReq); err != nil {
 		return nil, beaconhttp.NewEndpointError(http.StatusBadRequest, err)
