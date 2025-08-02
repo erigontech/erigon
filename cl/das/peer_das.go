719a5e45b2cb693860d921788af7bbf700be0835
Giulio <giulio.rebuffo@gmail.com>
2025-08-02 23:12:16 +0200

    save

--- cl/das/peer_das.go
+++ cl/das/peer_das.go
@@ -115,7 +115,6 @@ func (d *peerdas) IsBlobAlreadyRecovered(blockRoot common.Hash) bool {
 		log.Warn("failed to get kzg commitments count", "err", err, "blockRoot", blockRoot)
 		return false
 	}
-	fmt.Println(blockRoot, "count", count)
 	return count > 0
 }
 
