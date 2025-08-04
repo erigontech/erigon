a043121878e1ca042793add6a9744b6e004384f1
sudeepdino008 <sudeepdino008@gmail.com>
2025-08-04 11:36:09 +0530

    save

--- turbo/app/snapshots_cmd.go
+++ turbo/app/snapshots_cmd.go
@@ -1836,7 +1836,7 @@ func doInspectHistory(cliCtx *cli.Context, dirs datadir.Dirs) error {
 		buf, _ = g.Next(buf[:0])
 		buf2 := bytes.Clone(buf)
 
-		fmt.Println(hexutil.Encode(buf2))
+		fmt.Println(hexutil.Encode(buf2), len(buf2))
 		i++
 		if i > 100 {
 			break
