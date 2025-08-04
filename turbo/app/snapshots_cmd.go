746dfb345f534709e029c7a1a0acca8330b38b03
sudeepdino008 <sudeepdino008@gmail.com>
2025-08-04 17:02:31 +0530

    save

--- turbo/app/snapshots_cmd.go
+++ turbo/app/snapshots_cmd.go
@@ -1932,9 +1932,9 @@ func readAttempt3(vfile, effile, vifile string, dirs datadir.Dirs) error {
 	}
 	defer decomp.Close()
 	i := 0
-	var hv []byte
+	//var hv []byte
 	iiReader := state.Schema.GetDomainCfg(kv.AccountsDomain).GetIINewReader(decomp)
-	hreader, _ := state.Schema.GetDomainCfg(kv.AccountsDomain).GetPagedReader(decomp)
+	//hreader, _ := state.Schema.GetDomainCfg(kv.AccountsDomain).GetPagedReader(decomp)
 	//hreader := state.Schema.GetDomainCfg(kv.AccountsDomain).GetHistNewReader(decomp)
 	seq := &multiencseq.SequenceReader{}
 	for iiReader.HasNext() {
@@ -1967,14 +1967,14 @@ func readAttempt3(vfile, effile, vifile string, dirs datadir.Dirs) error {
 				panic("fails")
 			}
 			fmt.Println("k txNum offset", hexutil.Encode(kc), txNum, offset2)
-			hreader.Reset(offset2)
-			hv, offset2 := hreader.Next(hv[:0])
-			hvc := bytes.Clone(hv)
-			fmt.Println("..............", hexutil.Encode(hvc), len(hvc), offset2)
-			hv, offset2 = hreader.Next(hv[:0])
-			hvc = bytes.Clone(hv)
-			fmt.Println("..............", hexutil.Encode(hvc), len(hvc), offset2)
-			fmt.Println()
+			// 	hreader.Reset(offset2)
+			// 	hv, offset2 := hreader.Next(hv[:0])
+			// 	hvc := bytes.Clone(hv)
+			// 	fmt.Println("..............", hexutil.Encode(hvc), len(hvc), offset2)
+			// 	hv, offset2 = hreader.Next(hv[:0])
+			// 	hvc = bytes.Clone(hv)
+			// 	fmt.Println("..............", hexutil.Encode(hvc), len(hvc), offset2)
+			// 	fmt.Println()
 		}
 		i++
 		fmt.Println("hello-----------------")
