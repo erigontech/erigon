81b347390ce6798eaff020ba536685e9ffd26152
sudeepdino008 <sudeepdino008@gmail.com>
2025-08-04 21:51:15 +0530

    save

--- turbo/app/snapshots_cmd.go
+++ turbo/app/snapshots_cmd.go
@@ -2148,7 +2148,7 @@ func readAttempt3(vfile, effile, vifile string, dirs datadir.Dirs) error {
 			hreader.Reset(offset2)
 			hv, offset2 := hreader.Next(nil)
 			hvc := bytes.Clone(hv)
-			fmt.Println("..............", hexutil.Encode(hvc), len(hvc), offset2, hexutil.Encode(vbyte), len(vbyte))
+			fmt.Println("..............", hexutil.Encode(hvc), len(hvc), offset2)
 			fmt.Println()
 			i++
 		}
