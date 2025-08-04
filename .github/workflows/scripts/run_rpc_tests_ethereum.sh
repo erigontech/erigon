10b109725a1d333221486e1fcb4902f124fb46d2
lupin012 <58134934+lupin012@users.noreply.github.com.>
2025-08-04 18:00:42 +0200

    update rpc test version

--- .github/workflows/scripts/run_rpc_tests_ethereum.sh
+++ .github/workflows/scripts/run_rpc_tests_ethereum.sh
@@ -41,4 +41,4 @@ DISABLED_TEST_LIST=(
 DISABLED_TESTS=$(IFS=,; echo "${DISABLED_TEST_LIST[*]}")
 
 # Call the main test runner script with the required and optional parameters
-"$(dirname "$0")/run_rpc_tests.sh" mainnet v1.71.0 "$DISABLED_TESTS" "$WORKSPACE" "$RESULT_DIR"
+"$(dirname "$0")/run_rpc_tests.sh" mainnet v1.73.0 "$DISABLED_TESTS" "$WORKSPACE" "$RESULT_DIR"
