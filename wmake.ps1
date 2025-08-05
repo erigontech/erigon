5bd051081d4b908eb01868a65dec539719d53e32
antonis19 <antonis19@mail.com>
2025-08-05 11:45:24 +0200

    Run CI tests with `-race` only after PR is merged (#16445)
    Due to `make test-all-race` take more than an hour compared to `make
    test-all` which takes just 13 minutes, we can run `make test-all` on
    PRs, and the longer `make test-all-race` after a PR has been pushed to
    `main` or to a `release/*` branch.
    
    ---------
    
    Co-authored-by: antonis19 <antonis19@users.noreply.github.com>

--- wmake.ps1
+++ wmake.ps1
@@ -544,7 +544,17 @@ if ($BuildTarget -eq "db-tools") {
     } else {
         Write-Host "`n Tests completed"
     }
-
+} elseif ($BuildTarget -eq "test-all-race") {
+     Write-Host " Running all tests ..."
+    $env:GODEBUG = "cgocheck=0"
+    $TestCommand = "go test $($Erigon.BuildFlags) --timeout 60m -race ./..."
+    Invoke-Expression -Command $TestCommand | Out-Host
+    if (!($?)) {
+        Write-Host " ERROR : Tests failed"
+        exit 1
+    } else {
+        Write-Host "`n Tests completed"
+    }
 } else {
 
     # This has a naive assumption every target has a compilation unit with the same name
