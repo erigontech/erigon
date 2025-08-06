9e8b004fb6a71aa01b4c1c22cf2204fc1e6ed0a3
alex <AskAlexSharov@gmail.com>
2025-08-06 11:43:26 +0700

    save

--- turbo/snapshotsync/snapshots.go
+++ turbo/snapshotsync/snapshots.go
@@ -1159,7 +1159,7 @@ func (s *RoSnapshots) OpenFolder() error {
 		s.closeWhatNotInList(list)
 		return s.openSegments(list, true, false)
 	}(); err != nil {
-		return err
+		return fmt.Errorf("OpenFolder: %w", err)
 	}
 
 	s.recalcVisibleFiles(s.alignMin)
