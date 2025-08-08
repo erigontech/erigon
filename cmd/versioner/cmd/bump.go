60c8ccef22089251e9008bfc3f52e6389cab0b23
JkLondon <me@ilyamikheev.com>
2025-08-08 18:24:59 +0200

    commit

--- cmd/versioner/cmd/bump.go
+++ cmd/versioner/cmd/bump.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/erigontech/erigon-lib/state"
 	"github.com/spf13/cobra"
 )
 
@@ -13,17 +14,16 @@ var bumpCmd = &cobra.Command{
 	Use:   "bump",
 	Short: "Bump a schema version in code",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		if domain == "" || facet == "" || newVersion == "" {
-			return fmt.Errorf("--domain, --facet and --to flags are required")
-		}
+		//if domain == "" || facet == "" || newVersion == "" {
+		//	return fmt.Errorf("--domain, --facet and --to flags are required")
+		//}
 		fmt.Printf("Bumping %s.%s to %s\n", domain, facet, newVersion)
-		// TODO: parse version_schema.go AST, update the constant
-		return nil
+		return state.GenerateSchemaVersions("./erigon-lib/state/versions.yaml", "./erigon-lib/state/version_schema_gen.go")
 	},
 }
 
 func init() {
-	bumpCmd.Flags().StringVar(&domain, "domain", "", "Domain name (e.g. accounts)")
-	bumpCmd.Flags().StringVar(&facet, "facet", "", "Facet/key to bump (e.g. data-kv)")
-	bumpCmd.Flags().StringVar(&newVersion, "to", "", "New version (e.g. 2.0.0-standard)")
+	//bumpCmd.Flags().StringVar(&domain, "domain", "", "Domain name (e.g. accounts)")
+	//bumpCmd.Flags().StringVar(&facet, "facet", "", "Facet/key to bump (e.g. data-kv)")
+	//bumpCmd.Flags().StringVar(&newVersion, "to", "", "New version (e.g. 2.0.0-standard)")
 }
