508fc475b765b11d6ff167e51dc9ad6352e2df96
Matt Joiner <anacrolix@gmail.com>
2025-08-11 10:53:48 +1000

    Set default Downloader rate flags
    Fixes #16524.

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -686,18 +686,21 @@ var (
 	}
 	TorrentDownloadRateFlag = cli.StringFlag{
 		Name: "torrent.download.rate",
-		// I'm not sure what we want here. How fast to typical users get with webseeds? Let's try no
-		// limit.
-		Usage: "Bytes per second, example: 32mb. Shared with webseeds unless that rate is set separately.",
+		// Default for 3.1. Try not drain the whole swarm by default.
+		Value: "512mb",
+		Usage: "Bytes per second, example: 32mb. Set Inf for no limit. Shared with webseeds unless that rate is set separately.",
 	}
+	// Decided to not provide a default to keep things simpler (so it shares whatever
+	// TorrentDownloadRateFlag is set to).
 	TorrentWebseedDownloadRateFlag = cli.StringFlag{
 		Name:  "torrent.webseed.download.rate",
-		Usage: "Bytes per second for webseeds, example: 32mb. If not set, rate limit is shared with torrent.download.rate",
+		Usage: "Bytes per second for webseeds, example: 32mb. Set Inf for no limit. If not set, rate limit is shared with torrent.download.rate",
 	}
 	TorrentUploadRateFlag = cli.StringFlag{
-		Name:  "torrent.upload.rate",
-		Value: "32mb",
-		Usage: "Bytes per second, example: 32mb",
+		Name: "torrent.upload.rate",
+		// Agreed in meeting to leave it quite a bit higher than 3.0 unless it becomes a problem.
+		Value: "16mb",
+		Usage: "Bytes per second, example: 32mb. Set Inf for no limit.",
 	}
 	// Deprecated. Shouldn't do anything. TODO: Remove.
 	TorrentDownloadSlotsFlag = cli.IntFlag{
