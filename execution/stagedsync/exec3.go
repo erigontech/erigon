09b9e4d0a59c23cbc013c526e546f62765687276
Mark Holt <mark.holt@erigon.tech>
2025-08-11 17:48:57 +0100

    add comittement check for last output tx

--- execution/stagedsync/exec3.go
+++ execution/stagedsync/exec3.go
@@ -1242,6 +1242,12 @@ func ExecV3(ctx context.Context,
 		dumpPlainStateDebug(applyTx.(kv.TemporalRwTx), executor.domains())
 	}
 
+	if lastTxStep := uint64(outputTxNum.Load()) / doms.StepSize(); lastTxStep <= applyTx.(kv.TemporalRwTx).StepsInFiles(kv.CommitmentDomain) {
+
+		logger.Warn("["+execStage.LogPrefix()+"] + can't persist comittement step too low", "txNum", outputTxNum.Load(), "step", lastTxStep)
+		return fmt.Errorf("can't persist comittement for txNum %d: step %d is frozen", outputTxNum.Load(), lastTxStep)
+	}
+
 	if !useExternalTx && applyTx != nil {
 		if err = applyTx.Commit(); err != nil {
 			return err
