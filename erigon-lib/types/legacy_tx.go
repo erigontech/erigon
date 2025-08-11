06b4b447905bc79548aa6450fcae16e8d9f51ff8
milen <94537774+taratorio@users.noreply.github.com>
2025-08-11 16:40:10 +0300

    tracer: fix prestates for EIP7702 transactions (#16497) (#16555)
    cherry-pick https://github.com/erigontech/erigon/pull/16497 to 3.1
    
    Implements logic from https://github.com/erigontech/erigon/pull/16016 to
    main branch
    
    Co-authored-by: Nebojsa Urosevic <nebojsa94@users.noreply.github.com>

--- erigon-lib/types/legacy_tx.go
+++ erigon-lib/types/legacy_tx.go
@@ -120,6 +120,10 @@ func (tx *LegacyTx) GetAccessList() AccessList {
 	return AccessList{}
 }
 
+func (tx *LegacyTx) GetAuthorizations() []Authorization {
+	return nil
+}
+
 func (tx *LegacyTx) Protected() bool {
 	return isProtectedV(&tx.V)
 }
