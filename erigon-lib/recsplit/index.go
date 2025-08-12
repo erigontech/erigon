c792fe703eefba01e5cba3e2f3134f62b116893b
JkLondon <me@ilyamikheev.com>
2025-08-13 00:54:50 +0200

    commit

--- erigon-lib/recsplit/index.go
+++ erigon-lib/recsplit/index.go
@@ -369,6 +369,10 @@ func (idx *Index) Lookup(bucketHash, fingerprint uint64) (uint64, bool) {
 
 	found := binary.BigEndian.Uint64(idx.data[pos:]) & idx.recMask
 	if idx.lessFalsePositives {
+		if len(idx.existence) == 0 {
+			msg := fmt.Sprintf("existence filter is empty, file %s, len data %d", idx.fileName, len(idx.data))
+			panic(msg)
+		}
 		return found, idx.existence[found] == byte(bucketHash)
 	}
 	return found, true
