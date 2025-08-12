c9b284d512257966e27e5569919086bc91e90a5d
yperbasis <andrey.ashikhmin@gmail.com>
2025-08-12 22:00:14 +0200

    mv datastruct to db

--- db/state/dirty_files.go
+++ db/state/dirty_files.go
@@ -29,10 +29,10 @@ import (
 	btree2 "github.com/tidwall/btree"
 
 	"github.com/erigontech/erigon-lib/common/dir"
-	"github.com/erigontech/erigon-lib/datastruct/existence"
 	"github.com/erigontech/erigon-lib/log/v3"
 	"github.com/erigontech/erigon-lib/version"
 	"github.com/erigontech/erigon/db/config3"
+	"github.com/erigontech/erigon/db/datastruct/existence"
 	"github.com/erigontech/erigon/db/recsplit"
 	"github.com/erigontech/erigon/db/seg"
 )
