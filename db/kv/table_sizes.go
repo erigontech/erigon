765b4c4984bcf85164a65963d1e009ebee7023d1
alex <AskAlexSharov@gmail.com>
2025-08-16 17:16:28 +0700

    save

--- db/kv/table_sizes.go
+++ db/kv/table_sizes.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/erigontech/erigon-lib/common"
 	"github.com/erigontech/erigon-lib/common/dbg"
+	"github.com/erigontech/erigon-lib/log/v3"
 	"github.com/erigontech/erigon-lib/metrics"
 )
 
