5e3bd9a04bc4963509f9bd6864bc9ac9213c9436
sashass1315 <sashass1315@gmail.com>
2025-08-21 16:43:59 +0300

    p2p/nat: upnp: propagate internalAddress error in AddMapping

--- p2p/nat/natupnp.go
+++ p2p/nat/natupnp.go
@@ -87,7 +87,7 @@ func (n *upnp) ExternalIP() (addr net.IP, err error) {
 func (n *upnp) AddMapping(protocol string, extport, intport int, desc string, lifetime time.Duration) error {
 	ip, err := n.internalAddress()
 	if err != nil {
-		return nil
+		return err
 	}
 	protocol = strings.ToUpper(protocol)
 	lifetimeS := uint32(lifetime / time.Second)
