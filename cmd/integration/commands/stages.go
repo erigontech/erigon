99ea507ed60083bb6ca10c2592227032005e0e58
canepat <16927169+canepat@users.noreply.github.com>
2025-08-21 16:00:46 +0200

    integration: fix database not initialized in run_migrations

--- cmd/integration/commands/stages.go
+++ cmd/integration/commands/stages.go
@@ -414,7 +414,24 @@ var cmdRunMigrations = &cobra.Command{
 	Short: "",
 	Run: func(cmd *cobra.Command, args []string) {
 		logger := debug.SetupCobra(cmd, "integration")
-		dbPaths := map[kv.Label]string{kv.ChainDB: chaindata}
+		migrateDB := func(label kv.Label, path string) {
+			logger.Info("Opening DB", "label", label, "path", path)
+			// Non-accede and exclusive mode - to apply creation of new tables if needed.
+			cfg := dbCfg(label, path).RemoveFlags(mdbx.Accede).Exclusive(true)
+			db, err := openDB(cfg, true, logger)
+			if err != nil {
+				logger.Error("Opening DB", "error", err)
+				return
+			}
+			defer db.Close()
+			// Nothing to do, migrations will be applied automatically
+		}
+
+		// Chaindata DB *must* be the first one because guaranteed to contain data in Config table
+		// (see openSnapshotOnce in allSnapshots below).
+		migrateDB(kv.ChainDB, chaindata)
+
+		dbPaths := map[kv.Label]string{}
 		// Migrations must be applied also to the consensus DB because ConsensusTables contain also ChaindataTables
 		// (see kv/tables.go).
 		consensus := strings.Replace(chaindata, "chaindata", "aura", 1)
@@ -436,15 +453,7 @@ var cmdRunMigrations = &cobra.Command{
 			dbPaths[kv.PolygonBridgeDB] = polygonBridge
 		}
 		for dbLabel, dbPath := range dbPaths {
-			//non-accede and exclusive mode - to apply create new tables if need.
-			cfg := dbCfg(dbLabel, dbPath).RemoveFlags(mdbx.Accede).Exclusive(true)
-			db, err := openDB(cfg, true, logger)
-			if err != nil {
-				logger.Error("Opening DB", "error", err)
-				return
-			}
-			defer db.Close()
-			// Nothing to do, migrations will be applied automatically
+			migrateDB(dbLabel, dbPath)
 		}
 	},
 }
