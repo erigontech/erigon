76f89909c81e018376d17f17dc8e8e53721678aa
alex <AskAlexSharov@gmail.com>
2025-08-22 22:00:26 +0700

    save

--- cmd/integration/commands/stages.go
+++ cmd/integration/commands/stages.go
@@ -550,13 +550,6 @@ func init() {
 	//withChaosMonkey(cmdPrintCommitment)
 	rootCmd.AddCommand(cmdPrintCommitment)
 
-	withConfig(cmdPrintCommitment)
-	withDataDir(cmdPrintCommitment)
-	withChain(cmdPrintCommitment)
-	//withHeimdall(cmdPrintCommitment)
-	//withChaosMonkey(cmdPrintCommitment)
-	rootCmd.AddCommand(cmdPrintCommitment)
-
 	withConfig(cmdStageTxLookup)
 	withReset(cmdStageTxLookup)
 	withBlock(cmdStageTxLookup)
