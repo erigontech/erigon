0eedc6f626da7bd01317717212e504e0027fe641
yperbasis <andrey.ashikhmin@gmail.com>
2025-08-22 17:00:41 +0200

    mv compiler to execution/abi

--- cmd/abigen/main.go
+++ cmd/abigen/main.go
@@ -30,12 +30,12 @@ import (
 	"github.com/urfave/cli/v2"
 
 	"github.com/erigontech/erigon-lib/common"
-	"github.com/erigontech/erigon-lib/common/compiler"
 	"github.com/erigontech/erigon-lib/crypto"
 	"github.com/erigontech/erigon-lib/log/v3"
 	"github.com/erigontech/erigon/cmd/utils"
 	"github.com/erigontech/erigon/db/version"
 	"github.com/erigontech/erigon/execution/abi/bind"
+	"github.com/erigontech/erigon/execution/abi/compiler"
 	cli2 "github.com/erigontech/erigon/turbo/cli"
 )
 
