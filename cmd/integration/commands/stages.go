edae41e0a3c3bd70d49a4acbda13a148b01d8c8c
alex <AskAlexSharov@gmail.com>
2025-08-23 15:07:56 +0700

    save

--- cmd/integration/commands/stages.go
+++ cmd/integration/commands/stages.go
@@ -537,6 +537,7 @@ func init() {
 	withSqueeze(cmdCommitmentRebuild)
 	withBlock(cmdCommitmentRebuild)
 	withConcurrentCommitment(cmdCommitmentRebuild)
+	withSqueeze(cmdCommitmentRebuild)
 	withUnwind(cmdCommitmentRebuild)
 	withPruneTo(cmdCommitmentRebuild)
 	withIntegrityChecks(cmdCommitmentRebuild)
