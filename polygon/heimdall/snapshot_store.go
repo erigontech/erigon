a4db24db4ab625c31ec7eb6d2426952b4319c837
sudeepdino008 <sudeepdino008@gmail.com>
2025-08-27 15:22:28 +0530

    save

--- polygon/heimdall/snapshot_store.go
+++ polygon/heimdall/snapshot_store.go
@@ -599,7 +599,7 @@ func validateSnapshots[T Entity](
 				accumulatedErr = errors.New("missing entities")
 			}
 
-			accumulatedErr = fmt.Errorf("%w: snap [%d, %d)", accumulatedErr, expectedId, entity.RawId())
+			accumulatedErr = fmt.Errorf("%w: snap [%d, %d), file: %s", accumulatedErr, expectedId, entity.RawId(), seg.File)
 			if failFast {
 				return accumulatedErr
 			}
