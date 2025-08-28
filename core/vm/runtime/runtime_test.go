8a27ac0588f1a25a1b369de00c8bf1d313e76474
alex <AskAlexSharov@gmail.com>
2025-08-28 10:37:40 +0700

    save

--- core/vm/runtime/runtime_test.go
+++ core/vm/runtime/runtime_test.go
@@ -324,7 +324,7 @@ func BenchmarkEVM_RETURN(b *testing.B) {
 	}
 
 	db := testTemporalDB(b)
-	tx, err := db.BeginTemporalRw(context.Background())
+	tx, err := db.BeginTemporalRo(context.Background())
 	require.NoError(b, err)
 	defer tx.Rollback()
 	domains, err := dbstate.NewSharedDomains(tx, log.New())
@@ -592,7 +592,7 @@ func BenchmarkSimpleLoop(b *testing.B) {
 	p, lbl = program.New().Jumpdest()
 	callEOA := p.
 		Call(nil, 0xE0, 0, 0, 0, 0, 0). // call addr of EOA
-		Op(vm.POP).Jump(lbl).Bytes()    // pop return value and jump to label
+		Op(vm.POP).Jump(lbl).Bytes() // pop return value and jump to label
 
 	p, lbl = program.New().Jumpdest()
 	// Push as if we were making call, then pop it off again, and loop
