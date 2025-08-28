7a37123957ce7658ee80a36fcf3e4ad4357417fc
Kewei <kewei.train@gmail.com>
2025-08-28 12:41:08 +0800

    Update cl/phase1/core/checkpoint_sync/util.go

--- cl/phase1/core/checkpoint_sync/util.go
+++ cl/phase1/core/checkpoint_sync/util.go
@@ -17,7 +17,6 @@ func ReadOrFetchLatestBeaconState(ctx context.Context, dirs datadir.Dirs, beacon
 	var syncer CheckpointSyncer
 
 	isDevnetAndIsDevnetNotSupported := caplinConfig.IsDevnet() && len(clparams.ConfigurableCheckpointsURLs) == 0
-	fmt.Println("isDevnetAndIsDevnetNotSupported", isDevnetAndIsDevnetNotSupported)
 	remoteSync := !caplinConfig.DisabledCheckpointSync && !isDevnetAndIsDevnetNotSupported
 
 	if remoteSync {
