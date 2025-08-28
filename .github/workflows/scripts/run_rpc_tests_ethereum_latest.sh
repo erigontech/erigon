0259d396da25dd87f77118128eda3c91338c8f69
lupin012 <58134934+lupin012@users.noreply.github.com.>
2025-08-28 22:32:45 +0200

    enable do-not-compare-error options

--- .github/workflows/scripts/run_rpc_tests_ethereum_latest.sh
+++ .github/workflows/scripts/run_rpc_tests_ethereum_latest.sh
@@ -29,4 +29,4 @@ DISABLED_TEST_LIST=(
 DISABLED_TESTS=$(IFS=,; echo "${DISABLED_TEST_LIST[*]}")
 
 # Call the main test runner script with the required and optional parameters
-"$(dirname "$0")/run_rpc_tests.sh" mainnet v1.80.0 "$DISABLED_TESTS" "$WORKSPACE" "$RESULT_DIR" "latest" "$REFERENCE_HOST" 
+"$(dirname "$0")/run_rpc_tests.sh" mainnet v1.80.0 "$DISABLED_TESTS" "$WORKSPACE" "$RESULT_DIR" "latest" "$REFERENCE_HOST" "do-not-compare-error-message"
