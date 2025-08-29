754326fab9794a20e85dec1542016c114025f53a
alex <AskAlexSharov@gmail.com>
2025-08-29 13:15:44 +0700

    save

--- tests/block_test.go
+++ tests/block_test.go
@@ -17,6 +17,8 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with Erigon. If not, see <http://www.gnu.org/licenses/>.
 
+//go:build race
+
 package tests
 
 import (
