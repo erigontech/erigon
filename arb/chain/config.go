809ea1a295f733e301a67c19f66971963c063398
awskii <artem.tsskiy@gmail.com>
2025-08-30 02:16:41 +0100

    save

--- arb/chain/config.go
+++ arb/chain/config.go
@@ -16,7 +16,7 @@ var (
 	ArbSepoliaGenesisHash = common.HexToHash("0x77194da4010e549a7028a9c3c51c3e277823be6ac7d138d0bb8a70197b5c004c")
 
 	Arb1ChainConfig = chainspec.ReadChainSpec(chainspecs, "chainspecs/arb1.json")
-	Arb1GenesisHash = common.HexToHash("0xd1882c626699cd19548720713669993ac8f51500056dfbc1afc180496c7f8e2f")
+	Arb1GenesisHash = common.HexToHash("0x7d237dd685b96381544e223f8906e35645d63b89c19983f2246db48568c07986")
 )
 
 func init() {
