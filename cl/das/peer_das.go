7b14749ef40261f5a4c5acc7aa5ff8687cda6842
Giulio Rebuffo <giulio.rebuffo@gmail.com>
2025-08-30 22:49:26 +0200

    save

--- cl/das/peer_das.go
+++ cl/das/peer_das.go
@@ -136,17 +136,7 @@ func (d *peerdas) isMyColumnDataAvailable(slot uint64, blockRoot common.Hash) (b
 	if err != nil {
 		return false, err
 	}
-	if len(expectedCustodies) == 0 {
-		// this case is not reasonable due to empty node ID
-		return len(existingColumns) >= int(d.beaconConfig.NumberOfColumns), nil
-	}
-	nowCustodies := map[cltypes.CustodyIndex]bool{}
-	for _, column := range existingColumns {
-		if _, ok := expectedCustodies[column]; ok {
-			nowCustodies[column] = true
-		}
-	}
-	return len(nowCustodies) >= len(expectedCustodies), nil
+	return len(existingColumns) >= len(expectedCustodies), nil
 }
 
 func (d *peerdas) resubscribeGossip() {
