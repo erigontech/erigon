d7a43d07d8ecf3d61f965bffe2e0b45b998baacf
alex <AskAlexSharov@gmail.com>
2025-09-02 11:17:36 +0700

    save

--- core/vm/runtime/runtime.go
+++ core/vm/runtime/runtime.go
@@ -36,10 +36,7 @@ import (
 	"github.com/erigontech/erigon/core/state"
 	"github.com/erigontech/erigon/core/tracing"
 	"github.com/erigontech/erigon/core/vm"
-	"github.com/erigontech/erigon/db/config3"
 	"github.com/erigontech/erigon/db/datadir"
-	"github.com/erigontech/erigon/db/kv/memdb"
-	"github.com/erigontech/erigon/db/kv/temporal"
 	"github.com/erigontech/erigon/db/kv/temporal/temporaltest"
 	dbstate "github.com/erigontech/erigon/db/state"
 	"github.com/erigontech/erigon/execution/chain"
@@ -129,24 +126,11 @@ func Execute(code, input []byte, cfg *Config, tempdir string) ([]byte, *state.In
 
 	externalState := cfg.State != nil
 	if !externalState {
-		db := memdb.NewStateDB(tempdir)
-		defer db.Close()
 		dirs := datadir.New(tempdir)
-		logger := log.New()
-		salt, err := dbstate.GetStateIndicesSalt(dirs, true, logger)
-		if err != nil {
-			return nil, nil, err
-		}
-		agg, err := dbstate.NewAggregator2(context.Background(), dirs, config3.DefaultStepSize, salt, db, logger)
-		if err != nil {
-			return nil, nil, err
-		}
-		defer agg.Close()
-		_db, err := temporal.New(db, agg)
-		if err != nil {
-			return nil, nil, err
-		}
-		tx, err := _db.BeginTemporalRw(context.Background()) //nolint:gocritic
+		db := temporaltest.NewTestDB(nil, dirs)
+		defer db.Close()
+
+		tx, err := db.BeginTemporalRw(context.Background()) //nolint:gocritic
 		if err != nil {
 			return nil, nil, err
 		}
