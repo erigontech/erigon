b358565e354a3ad5d1b0524c0243326b48dce83b
alex <AskAlexSharov@gmail.com>
2025-09-03 19:34:02 +0700

    save

--- db/state/domain_shared.go
+++ db/state/domain_shared.go
@@ -174,11 +174,6 @@ func (sd *SharedDomains) updateAccountCode(addrS string, code []byte, txNum uint
 	return sd.mem.PutWithPrev(kv.CodeDomain, addr, code, txNum, prevCode, prevStep)
 }
 
-func (sd *SharedDomains) updateCommitmentData(prefix string, data []byte, txNum uint64, prev []byte, prevStep kv.Step) error {
-	sd.mem.DomainPut(kv.CommitmentDomain, prefix, data, txNum)
-	return sd.mem.PutWithPrev(kv.CommitmentDomain, toBytesZeroCopy(prefix), data, txNum, prev, prevStep)
-}
-
 func (sd *SharedDomains) deleteAccount(roTx kv.Tx, addrS string, txNum uint64, prev []byte, prevStep kv.Step) error {
 	addr := toBytesZeroCopy(addrS)
 	if err := sd.DomainDelPrefix(kv.StorageDomain, roTx, addr, txNum); err != nil {
@@ -199,10 +194,6 @@ func (sd *SharedDomains) writeAccountStorage(k string, v []byte, txNum uint64, p
 	return sd.mem.PutWithPrev(kv.StorageDomain, toBytesZeroCopy(k), v, txNum, preVal, prevStep)
 }
 
-func (sd *SharedDomains) delAccountStorage(k string, txNum uint64, preVal []byte, prevStep kv.Step) error {
-	sd.mem.DomainPut(kv.StorageDomain, k, nil, txNum)
-	return sd.mem.PutWithPrev(kv.StorageDomain, toBytesZeroCopy(k), nil, txNum, preVal, prevStep)
-}
 func (sd *SharedDomains) IndexAdd(table kv.InvertedIdx, key []byte, txNum uint64) (err error) {
 	return sd.mem.IndexAdd(table, key, txNum)
 }
@@ -288,7 +279,7 @@ func (sd *SharedDomains) GetLatest(domain kv.Domain, tx kv.Tx, k []byte) (v []by
 //   - if `val == nil` it will call DomainDel
 func (sd *SharedDomains) DomainPut(domain kv.Domain, roTx kv.Tx, k, v []byte, txNum uint64, prevVal []byte, prevStep kv.Step) error {
 	if v == nil {
-		return fmt.Errorf("DomainPut: %s, trying to Put nil value. not allowed", domain)
+		return fmt.Errorf("DomainPut: %s, trying to put nil value. not allowed", domain)
 	}
 
 	if prevVal == nil {
@@ -302,23 +293,19 @@ func (sd *SharedDomains) DomainPut(domain kv.Domain, roTx kv.Tx, k, v []byte, tx
 
 	sd.sdCtx.TouchKey(domain, ks, v)
 	switch domain {
-	case kv.StorageDomain:
-		return sd.writeAccountStorage(ks, v, txNum, prevVal, prevStep)
 	case kv.CodeDomain:
 		if bytes.Equal(prevVal, v) {
 			return nil
 		}
-		return sd.updateAccountCode(ks, v, txNum, prevVal, prevStep)
-	case kv.AccountsDomain, kv.CommitmentDomain, kv.RCacheDomain:
-		sd.mem.DomainPut(domain, ks, v, txNum)
-		return sd.mem.PutWithPrev(domain, k, v, txNum, prevVal, prevStep)
+	case kv.StorageDomain, kv.AccountsDomain, kv.CommitmentDomain, kv.RCacheDomain:
+		//noop
 	default:
 		if bytes.Equal(prevVal, v) {
 			return nil
 		}
-		sd.mem.DomainPut(domain, ks, v, txNum)
-		return sd.mem.PutWithPrev(domain, k, v, txNum, prevVal, prevStep)
 	}
+	sd.mem.DomainPut(domain, ks, v, txNum)
+	return sd.mem.PutWithPrev(domain, k, v, txNum, prevVal, prevStep)
 }
 
 // DomainDel
@@ -338,21 +325,15 @@ func (sd *SharedDomains) DomainDel(domain kv.Domain, tx kv.Tx, k []byte, txNum u
 	ks := string(k)
 	sd.sdCtx.TouchKey(domain, ks, nil)
 	switch domain {
-	case kv.AccountsDomain:
-		return sd.deleteAccount(tx, ks, txNum, prevVal, prevStep)
-	case kv.StorageDomain:
-		return sd.delAccountStorage(ks, txNum, prevVal, prevStep)
 	case kv.CodeDomain:
 		if prevVal == nil {
 			return nil
 		}
-		return sd.updateAccountCode(ks, nil, txNum, prevVal, prevStep)
-	case kv.CommitmentDomain:
-		return sd.updateCommitmentData(ks, nil, txNum, prevVal, prevStep)
 	default:
-		sd.mem.DomainPut(domain, ks, nil, txNum)
-		return sd.mem.PutWithPrev(domain, k, nil, txNum, prevVal, prevStep)
+		//noop
 	}
+	sd.mem.DomainPut(domain, ks, nil, txNum)
+	return sd.mem.PutWithPrev(domain, k, nil, txNum, prevVal, prevStep)
 
 }
 
