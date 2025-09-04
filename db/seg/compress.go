916f208e7faa6220517d78ab03190c5f0fc0a89c
alex <AskAlexSharov@gmail.com>
2025-09-04 13:57:14 +0700

    merge main

--- db/seg/compress.go
+++ db/seg/compress.go
@@ -121,18 +121,7 @@ type Compressor struct {
 	noFsync          bool // fsync is enabled by default, but tests can manually disable
 }
 
-type CompOpts struct {
-	cfg       Cfg
-	lvl       log.Lvl
-	logger    log.Logger
-	logPrefix string
-}
-
-func NewCompressor(ctx context.Context, logPrefix, outputFile, tmpDir string, cfg Cfg, lvl log.Lvl, logger log.Logger) (*CompOpts, error) {
-	newCompressor()
-}
-
-func newCompressor(ctx context.Context, logPrefix, outputFile, tmpDir string, cfg Cfg, lvl log.Lvl, logger log.Logger) (*Compressor, error) {
+func NewCompressor(ctx context.Context, logPrefix, outputFile, tmpDir string, cfg Cfg, lvl log.Lvl, logger log.Logger) (*Compressor, error) {
 	workers := cfg.Workers
 	dir2.MustExist(tmpDir)
 	dir, fileName := filepath.Split(outputFile)
