c096f3b196893551cbc3302414c53dbd87240cae
antonis19 <antonis19@users.noreply.github.com>
2025-09-05 19:04:07 +0200

    remove leftover files

--- execution/chain/spec/bootnodes_mumbai.go
+++ execution/chain/spec/bootnodes_mumbai.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with Erigon. If not, see <http://www.gnu.org/licenses/>.
 
-package params
+package chainspec
 
 var MumbaiBootnodes = []string{
 	"enode://e7a3ec3bebf21c5ee0a6b68a360fcb72594d3bd864b27ad903d0f2e90ff8e64507ff51338db3561db6a06b33a7e741e54ef8ed556b74e66bfffd41125d19924c@34.39.65.209:30303",
