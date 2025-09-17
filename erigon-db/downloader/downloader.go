erigontech/erigon

commit 40ea287b3a43404eaff2559ec9f1b1150b4f2529

Add missing timeouts to metainfo fetching HTTP transport (#16757)
A few recent issues were due to stalls in the HTTP2 stack. In fixing
them the HTTP transport used for fetching metainfos and data from
webseeds drifted apart and some of the timeouts didn't get applied to
the metainfo transport.

I took special care to make sure all flag values should be the same.

I also pulled in the HTTP3 flag at no cost and fixed a missed
`coverprofile` that slows down `test-short`.

erigon-db/downloader/downloader.go
@@ -24,6 +24,7 @@ import (
 	"io/fs"
 	"iter"
 	"math"
+	"net"
 	"net/http"
 	"net/url"
 	"os"
@@ -31,13 +32,13 @@ import (
 	"path/filepath"
 	"runtime"
 	"slices"
-	"strconv"
 	"strings"
 	"sync"
 	"sync/atomic"
 	"time"
 
 	"github.com/erigontech/erigon-lib/common/dir"
+	"github.com/quic-go/quic-go/http3"
 	"golang.org/x/net/http2"
 	"golang.org/x/sync/errgroup"
 	"golang.org/x/sync/semaphore"
@@ -249,66 +250,92 @@ func (r *requestHandler) RoundTrip(req *http.Request) (resp *http.Response, err 
 	return resp, err
 }
 
-func New(ctx context.Context, cfg *downloadercfg.Cfg, logger log.Logger, verbosity log.Lvl) (*Downloader, error) {
-	// Cloudflare, or OS socket overhead seems to limit us to ~100-150MB/s in testing to Cloudflare
-	// buckets. If we could limit HTTP requests to 1 per connection we'd do that, but the HTTP2
-	// config field doesn't do anything yet in Go 1.24 (and 1.25rc1). Disabling HTTP2 is another way
-	// to achieve this.
-	requestTransport := &http.Transport{
-		ReadBufferSize: 2 << 20,
-		// Note this does nothing in go1.24.
-		//HTTP2: &http.HTTP2Config{
-		//	MaxConcurrentStreams: 1,
-		//},
+var (
+	httpDialer = net.Dialer{
+		Timeout: time.Minute,
+	}
+	httpReadBufferSize = initIntFromEnv("DOWNLOADER_HTTP_READ_BUFFER_SIZE", 2<<20, 0)
+	maxConnsPerHost    = initIntFromEnv("DOWNLOADER_MAX_CONNS_PER_HOST", 10, 0)
+	tcpReadBufferSize  = initIntFromEnv("DOWNLOADER_TCP_READ_BUFFER_SIZE", 0, 0)
+	useHttp3           = os.Getenv("DOWNLOADER_HTTP3") != ""
+	forceIpv4          = os.Getenv("DOWNLOADER_FORCE_IPV4") != ""
+)
+
+// Configure a downloader transport (requests and metainfo sources). These now use common settings.
+func makeTransport() http.RoundTripper {
+	if useHttp3 {
+		return &http3.Transport{}
+	}
+	t := &http.Transport{
+		ReadBufferSize: httpReadBufferSize,
 		// Big hammer to achieve one request per connection.
-		DisableKeepAlives: os.Getenv("DOWNLOADER_DISABLE_KEEP_ALIVES") != "",
-		// I see requests get stuck waiting for headers to come back. I suspect Go 1.24 HTTP2
-		// bug.
+		DisableKeepAlives:     os.Getenv("DOWNLOADER_DISABLE_KEEP_ALIVES") != "",
 		ResponseHeaderTimeout: time.Minute,
+		MaxConnsPerHost:       maxConnsPerHost,
+		IdleConnTimeout:       10 * time.Second,
+		DialContext: func(ctx context.Context, network, addr string) (conn net.Conn, err error) {
+			if forceIpv4 {
+				switch network {
+				case "tcp", "tcp6":
+					network = "tcp4"
+				case "tcp4":
+				default:
+					panic(network)
+				}
+			}
+			conn, err = httpDialer.DialContext(ctx, network, addr)
+			if err != nil {
+				return
+			}
+			if tcpReadBufferSize != 0 {
+				err = conn.(*net.TCPConn).SetReadBuffer(tcpReadBufferSize)
+				panicif.Err(err)
+			}
+			return
+		},
 	}
+	configureHttp2(t)
+	return t
+}
 
-	if s := os.Getenv("DOWNLOADER_MAX_CONNS_PER_HOST"); s != "" {
-		var err error
-		i64, err := strconv.ParseInt(s, 10, 0)
-		panicif.Err(err)
-		requestTransport.MaxConnsPerHost = int(i64)
-	}
-
-	requestHandler := requestHandler{
-		RoundTripper: requestTransport,
-	}
-	// Disable HTTP2. See above.
-	if os.Getenv("DOWNLOADER_DISABLE_HTTP2") == "" {
-		// Don't set the http2.Transport as the RoundTripper. It's hooked into the http.Transport by
-		// this call.
-		h2t, err := http2.ConfigureTransports(requestTransport)
-		panicif.Err(err)
-		// Some of these are the defaults, but I really don't trust Go HTTP2 at this point.
-
-		// Will this fix pings from not timing out?
-		h2t.WriteByteTimeout = 15 * time.Second
-		// If we don't read for this long, send a ping.
-		h2t.ReadIdleTimeout = 15 * time.Second
-		h2t.PingTimeout = 15 * time.Second
-		h2t.MaxReadFrameSize = 1 << 20 // Same as net/http.Transport.ReadBufferSize?
-	} else {
+// Configures "Downloader" Transport HTTP2.
+func configureHttp2(t *http.Transport) {
+	if os.Getenv("DOWNLOADER_DISABLE_HTTP2") != "" {
 		// Disable h2 being added automatically.
-		g.MakeMap(&requestTransport.TLSNextProto)
+		g.MakeMap(&t.TLSNextProto)
 	}
+	// Don't set the http2.Transport as the RoundTripper. It's hooked into the http.Transport by
+	// this call. Need to use external http2 library to get access to some config fields that
+	// aren't in std.
+	h2t, err := http2.ConfigureTransports(t)
+	panicif.Err(err)
+	// Some of these are the defaults, but I really don't trust Go HTTP2 at this point.
 
-	// TODO: Add this specifically for webseeds and not as the Client wide HTTP transport.
-	cfg.ClientConfig.WebTransport = &requestHandler
-	metainfoSourcesTransport := http.Transport{
-		MaxConnsPerHost:       10,
-		ResponseHeaderTimeout: time.Minute,
+	// Will this fix pings from not timing out?
+	h2t.WriteByteTimeout = 15 * time.Second
+	// If we don't read for this long, send a ping.
+	h2t.ReadIdleTimeout = 15 * time.Second
+	h2t.PingTimeout = 15 * time.Second
+	h2t.MaxReadFrameSize = 1 << 20 // Same as net/http.Transport.ReadBufferSize?
+}
+
+func New(ctx context.Context, cfg *downloadercfg.Cfg, logger log.Logger, verbosity log.Lvl) (*Downloader, error) {
+	requestHandler := &requestHandler{}
+	{
+		requestHandler.RoundTripper = makeTransport()
+		cfg.ClientConfig.WebTransport = requestHandler
+		// requestHandler.downloader is set later.
 	}
-	// Separate transport so webseed requests and metainfo fetching don't block each other.
-	// Additionally, we can tune for their specific workloads.
-	cfg.ClientConfig.MetainfoSourcesClient = &http.Client{
-		Transport: roundTripperFunc(func(req *http.Request) (*http.Response, error) {
-			insertCloudflareHeaders(req)
-			return metainfoSourcesTransport.RoundTrip(req)
-		}),
+	{
+		metainfoSourcesTransport := makeTransport()
+		// Separate transport so webseed requests and metainfo fetching don't block each other.
+		// Additionally, we can tune for their specific workloads.
+		cfg.ClientConfig.MetainfoSourcesClient = &http.Client{
+			Transport: roundTripperFunc(func(req *http.Request) (*http.Response, error) {
+				insertCloudflareHeaders(req)
+				return metainfoSourcesTransport.RoundTrip(req)
+			}),
+		}
 	}
 
 	db, err := openMdbx(ctx, cfg.Dirs.Downloader, cfg.MdbxWriteMap)

erigon-db/Makefile
@@ -7,7 +7,7 @@ endif
 GOTEST = CGO_CXXFLAGS="$(CGO_CXXFLAGS)" go test -trimpath
 
 test-short:
-	$(GOTEST) -short -coverprofile=coverage-test.out ./...
+	$(GOTEST) -short ./...
 
 test-all:
 	$(GOTEST) -coverprofile=coverage-test-all.out ./...

erigon-db/downloader/env.go
@@ -1,0 +1,27 @@
+package downloader
+
+import (
+	"os"
+	"strconv"
+
+	"github.com/anacrolix/missinggo/v2/panicif"
+	"golang.org/x/exp/constraints"
+)
+
+func initIntFromEnv[T constraints.Signed](key string, defaultValue T, bitSize int) T {
+	return strconvFromEnv(key, defaultValue, bitSize, strconv.ParseInt)
+}
+
+func initUIntFromEnv[T constraints.Unsigned](key string, defaultValue T, bitSize int) T {
+	return strconvFromEnv(key, defaultValue, bitSize, strconv.ParseUint)
+}
+
+func strconvFromEnv[T, U constraints.Integer](key string, defaultValue T, bitSize int, conv func(s string, base, bitSize int) (U, error)) T {
+	s := os.Getenv(key)
+	if s == "" {
+		return defaultValue
+	}
+	i64, err := conv(s, 10, bitSize)
+	panicif.Err(err)
+	return T(i64)
+}

erigon-db/go.mod
@@ -25,12 +25,14 @@ require (
 	github.com/go-quicktest/qt v1.101.0
 	github.com/hashicorp/go-retryablehttp v0.7.8
 	github.com/puzpuzpuz/xsync/v4 v4.1.0
+	github.com/quic-go/quic-go v0.48.2
 	github.com/rs/dnscache v0.0.0-20211102005908-e0241e321417
 	github.com/spaolacci/murmur3 v1.1.0
 	github.com/stretchr/testify v1.10.0
+	golang.org/x/exp v0.0.0-20250606033433-dcc06ee1d476
 	golang.org/x/net v0.43.0
 	golang.org/x/sync v0.16.0
-	golang.org/x/time v0.0.0-20220609170525-579cf78fd858
+	golang.org/x/time v0.5.0
 	google.golang.org/grpc v1.74.2
 	google.golang.org/protobuf v1.36.7
 )
@@ -81,10 +83,12 @@ require (
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
 	github.com/godbus/dbus/v5 v5.0.4 // indirect
 	github.com/gofrs/flock v0.12.1 // indirect
 	github.com/google/btree v1.1.3 // indirect
 	github.com/google/go-cmp v0.7.0 // indirect
+	github.com/google/pprof v0.0.0-20221118152302-e6195bd50e26 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
@@ -107,6 +111,7 @@ require (
 	github.com/multiformats/go-varint v0.0.6 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/nyaosorg/go-windows-shortcut v0.0.0-20220529122037-8b0c89bca4c4 // indirect
+	github.com/onsi/ginkgo/v2 v2.9.5 // indirect
 	github.com/opencontainers/runtime-spec v1.2.0 // indirect
 	github.com/pbnjay/memory v0.0.0-20210728143218-7b4eea64cf58 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
@@ -135,6 +140,7 @@ require (
 	github.com/prometheus/procfs v0.15.1 // indirect
 	github.com/protolambda/ctxlock v0.1.0 // indirect
 	github.com/protolambda/ztyp v0.2.2 // indirect
+	github.com/quic-go/qpack v0.5.1 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	github.com/rogpeppe/go-internal v1.13.1 // indirect
 	github.com/shirou/gopsutil/v4 v4.24.8 // indirect
@@ -154,9 +160,10 @@ require (
 	go.opentelemetry.io/otel/trace v1.36.0 // indirect
 	go.uber.org/mock v0.5.0 // indirect
 	golang.org/x/crypto v0.41.0 // indirect
-	golang.org/x/exp v0.0.0-20250606033433-dcc06ee1d476 // indirect
+	golang.org/x/mod v0.26.0 // indirect
 	golang.org/x/sys v0.35.0 // indirect
 	golang.org/x/text v0.28.0 // indirect
+	golang.org/x/tools v0.35.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250603155806-513f23925822 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	lukechampine.com/blake3 v1.1.6 // indirect

erigon-db/go.sum
@@ -188,6 +188,8 @@ github.com/go-quicktest/qt v1.101.0/go.mod h1:14Bz/f7NwaXPtdYEgzsx46kqSxVwTbzVZs
 github.com/go-stack/stack v1.8.0/go.mod h1:v0f6uXyyMGvRgIKkXu+yp6POWl0qKG85gN/melR3HDY=
 github.com/go-stack/stack v1.8.1 h1:ntEHSVwIt7PNXNpgPmVfMrNhLtgjlmnZha2kOpuRiDw=
 github.com/go-stack/stack v1.8.1/go.mod h1:dcoOX6HbPZSZptuspn9bctJ+N/CnF5gGygcUP3XYfe4=
+github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 h1:tfuBGBXKqDEevZMzYi5KSi8KkcZtzBcTgAUUtapy0OI=
+github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572/go.mod h1:9Pwr4B2jHnOSGXyyzV8ROjYa2ojvAY6HCGYYfMoC3Ls=
 github.com/go-test/deep v1.1.1 h1:0r/53hagsehfO4bzD2Pgr/+RgHqhmf+k1Bpse2cTu1U=
 github.com/go-test/deep v1.1.1/go.mod h1:5C2ZWiW0ErCdrYzpqxLbTX7MG14M9iiw8DgHncVwcsE=
 github.com/godbus/dbus/v5 v5.0.4 h1:9349emZab16e7zQvpmsbtjc18ykshndd8y2PG3sgJbA=
@@ -224,6 +226,8 @@ github.com/google/go-cmp v0.5.9/go.mod h1:17dUlkBOakJ0+DkrSSNjCkIjxS6bF9zb3elmeN
 github.com/google/go-cmp v0.7.0 h1:wk8382ETsv4JYUZwIsn6YpYiWiBsYLSJiTsyBybVuN8=
 github.com/google/go-cmp v0.7.0/go.mod h1:pXiqmnSA92OHEEa9HXL2W4E7lf9JzCmGVUdgjX3N/iU=
 github.com/google/gofuzz v1.0.0/go.mod h1:dBl0BpW6vV/+mYPU4Po3pmUjxk6FQPldtuIdl/M65Eg=
+github.com/google/pprof v0.0.0-20221118152302-e6195bd50e26 h1:Xim43kblpZXfIBQsbuBVKCudVG457BR2GZFIz3uw3hQ=
+github.com/google/pprof v0.0.0-20221118152302-e6195bd50e26/go.mod h1:dDKJzRmX4S37WGHujM7tX//fmj1uioxKzKxz3lo4HJo=
 github.com/google/subcommands v1.2.0/go.mod h1:ZjhPrFU+Olkh9WazFPsl27BQ4UPiG37m3yTrtFlrHVk=
 github.com/google/uuid v1.6.0 h1:NIvaJDMOsjHA8n1jAhLSgzrAzy1Hgr+hNrb57e+94F0=
 github.com/google/uuid v1.6.0/go.mod h1:TIyPZe4MgqvfeYDBFedMoGGpEw/LqOeaOT+nhxU+yHo=
@@ -318,7 +322,11 @@ github.com/nyaosorg/go-windows-shortcut v0.0.0-20220529122037-8b0c89bca4c4 h1:+3
 github.com/nyaosorg/go-windows-shortcut v0.0.0-20220529122037-8b0c89bca4c4/go.mod h1:9YR30vCq/4djj0WO7AvLm48YvNs7M094LWRieEFDE4A=
 github.com/onsi/ginkgo v1.6.0/go.mod h1:lLunBs/Ym6LB5Z9jYTR76FiuTmxDTDusOGeTQH+WWjE=
 github.com/onsi/ginkgo v1.7.0/go.mod h1:lLunBs/Ym6LB5Z9jYTR76FiuTmxDTDusOGeTQH+WWjE=
+github.com/onsi/ginkgo/v2 v2.9.5 h1:+6Hr4uxzP4XIUyAkg61dWBw8lb/gc4/X5luuxN/EC+Q=
+github.com/onsi/ginkgo/v2 v2.9.5/go.mod h1:tvAoo1QUJwNEU2ITftXTpR7R1RbCzoZUOs3RonqW57k=
 github.com/onsi/gomega v1.4.3/go.mod h1:ex+gbHU/CVuBBDIJjb2X0qEXbFg53c61hWP/1CpauHY=
+github.com/onsi/gomega v1.27.6 h1:ENqfyGeS5AX/rlXDd/ETokDz93u0YufY1Pgxuy/PvWE=
+github.com/onsi/gomega v1.27.6/go.mod h1:PIQNjfQwkP3aQAH7lf7j87O/5FiNr+ZR8+ipb+qQlhg=
 github.com/opencontainers/runtime-spec v1.2.0 h1:z97+pHb3uELt/yiAWD691HNHQIF07bE7dzrbT927iTk=
 github.com/opencontainers/runtime-spec v1.2.0/go.mod h1:jwyrGlmzljRJv/Fgzds9SsS/C5hL+LL3ko9hs6T5lQ0=
 github.com/openzipkin/zipkin-go v0.1.6/go.mod h1:QgAqvLzwWbR/WpD4A3cGpPtJrZXNIiJc5AZX7/PBEpw=
@@ -399,6 +407,10 @@ github.com/protolambda/ztyp v0.2.2 h1:rVcL3vBu9W/aV646zF6caLS/dyn9BN8NYiuJzicLNy
 github.com/protolambda/ztyp v0.2.2/go.mod h1:9bYgKGqg3wJqT9ac1gI2hnVb0STQq7p/1lapqrqY1dU=
 github.com/puzpuzpuz/xsync/v4 v4.1.0 h1:x9eHRl4QhZFIPJ17yl4KKW9xLyVWbb3/Yq4SXpjF71U=
 github.com/puzpuzpuz/xsync/v4 v4.1.0/go.mod h1:VJDmTCJMBt8igNxnkQd86r+8KUeN1quSfNKu5bLYFQo=
+github.com/quic-go/qpack v0.5.1 h1:giqksBPnT/HDtZ6VhtFKgoLOWmlyo9Ei6u9PqzIMbhI=
+github.com/quic-go/qpack v0.5.1/go.mod h1:+PC4XFrEskIVkcLzpEkbLqq1uCoxPhQuvK5rH1ZgaEg=
+github.com/quic-go/quic-go v0.48.2 h1:wsKXZPeGWpMpCGSWqOcqpW2wZYic/8T3aqiOID0/KWE=
+github.com/quic-go/quic-go v0.48.2/go.mod h1:yBgs3rWBOADpga7F+jJsb6Ybg1LSYiQvwWlLX+/6HMs=
 github.com/rcrowley/go-metrics v0.0.0-20181016184325-3113b8401b8a/go.mod h1:bCqnVzQkZxMG4s8nGwiZ5l3QUCyqpo9Y+/ZMZ9VjZe4=
 github.com/remyoudompheng/bigfft v0.0.0-20200410134404-eec4a21b6bb0/go.mod h1:qqbHyh8v60DhA7CoWK5oRCqLrMHRGoxYCSS9EjAz6Eo=
 github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec h1:W09IVJc94icq4NjY3clb7Lk8O1qJ8BdBEF8z0ibU0rE=
@@ -438,6 +450,7 @@ github.com/stretchr/testify v1.2.1/go.mod h1:a8OnRcib4nhh0OaRAV+Yts87kKdq0PP7pXf
 github.com/stretchr/testify v1.2.2/go.mod h1:a8OnRcib4nhh0OaRAV+Yts87kKdq0PP7pXfy6kDkUVs=
 github.com/stretchr/testify v1.3.0/go.mod h1:M5WIy9Dh21IEIfnGCwXGc5bZfKNJtfHm1UVUgZn+9EI=
 github.com/stretchr/testify v1.4.0/go.mod h1:j7eGeouHqKxXV5pUuKE4zz7dFj8WfuZ+81PSLYec5m4=
+github.com/stretchr/testify v1.6.1/go.mod h1:6Fq8oRcR53rry900zMqJjRRixrwX3KX962/h/Wwjteg=
 github.com/stretchr/testify v1.7.0/go.mod h1:6Fq8oRcR53rry900zMqJjRRixrwX3KX962/h/Wwjteg=
 github.com/stretchr/testify v1.7.1/go.mod h1:6Fq8oRcR53rry900zMqJjRRixrwX3KX962/h/Wwjteg=
 github.com/stretchr/testify v1.8.0/go.mod h1:yNjHg4UonilssWZ8iaSj1OCr/vHnekPRkoO+kdMU+MU=
@@ -507,6 +520,8 @@ golang.org/x/mod v0.5.1/go.mod h1:5OXOZSfqPIIbmVBIIKWRFfZjPR0E5r58TLhUjH0a2Ro=
 golang.org/x/mod v0.6.0-dev.0.20211013180041-c96bc1413d57/go.mod h1:3p9vT2HGsQu2K1YbXdKPJLVgG5VJdoTa1poYQBtP1AY=
 golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4/go.mod h1:jJ57K6gSWd91VN4djpZkiMVwK6gcyfeH4XE8wZrZaV4=
 golang.org/x/mod v0.8.0/go.mod h1:iBbtSCu2XBx23ZKBPSOrRkjjQPZFPuis4dIYUhu/chs=
+golang.org/x/mod v0.26.0 h1:EGMPT//Ezu+ylkCijjPc+f4Aih7sZvaAr+O3EHBxvZg=
+golang.org/x/mod v0.26.0/go.mod h1:/j6NAhSk8iQ723BGAUyoAcn7SlD7s15Dp9Nd/SfeaFQ=
 golang.org/x/net v0.0.0-20180724234803-3673e40ba225/go.mod h1:mL1N/T3taQHkDXs73rZJwtUhF3w3ftmwwsq0BUmARs4=
 golang.org/x/net v0.0.0-20180826012351-8a410e7b638d/go.mod h1:mL1N/T3taQHkDXs73rZJwtUhF3w3ftmwwsq0BUmARs4=
 golang.org/x/net v0.0.0-20180906233101-161cd47e91fd/go.mod h1:mL1N/T3taQHkDXs73rZJwtUhF3w3ftmwwsq0BUmARs4=
@@ -584,8 +599,8 @@ golang.org/x/text v0.9.0/go.mod h1:e1OnstbJyHTd6l/uOt8jFFHp6TRDWZR/bV3emEE/zU8=
 golang.org/x/text v0.13.0/go.mod h1:TvPlkZtksWOMsz7fbANvkp4WM8x/WCo/om8BMLbz+aE=
 golang.org/x/text v0.28.0 h1:rhazDwis8INMIwQ4tpjLDzUhx6RlXqZNPEM0huQojng=
 golang.org/x/text v0.28.0/go.mod h1:U8nCwOR8jO/marOQ0QbDiOngZVEBB7MAiitBuMjXiNU=
-golang.org/x/time v0.0.0-20220609170525-579cf78fd858 h1:Dpdu/EMxGMFgq0CeYMh4fazTD2vtlZRYE7wyynxJb9U=
-golang.org/x/time v0.0.0-20220609170525-579cf78fd858/go.mod h1:tRJNPiyCQ0inRvYxbN9jk5I+vvW/OXSQhTDSoE431IQ=
+golang.org/x/time v0.5.0 h1:o7cqy6amK/52YcAKIPlM3a+Fpj35zvRj2TP+e1xFSfk=
+golang.org/x/time v0.5.0/go.mod h1:3BpzKBy/shNhVucY/MWOyx10tF3SFh9QdLuxbVysPQM=
 golang.org/x/tools v0.0.0-20180828015842-6cd1fcedba52/go.mod h1:n7NCudcB/nEzxVGmLbDWY5pfWTLqBcC2KZ6jyYvM4mQ=
 golang.org/x/tools v0.0.0-20180917221912-90fa682c2a6e/go.mod h1:n7NCudcB/nEzxVGmLbDWY5pfWTLqBcC2KZ6jyYvM4mQ=
 golang.org/x/tools v0.0.0-20190114222345-bf090417da8b/go.mod h1:n7NCudcB/nEzxVGmLbDWY5pfWTLqBcC2KZ6jyYvM4mQ=
@@ -596,6 +611,8 @@ golang.org/x/tools v0.0.0-20191119224855-298f0cb1881e/go.mod h1:b+2E5dAYhXwXZwtn
 golang.org/x/tools v0.1.8-0.20211029000441-d6a9af8af023/go.mod h1:nABZi5QlRsZVlzPpHl034qft6wpY4eDcsTt5AaioBiU=
 golang.org/x/tools v0.1.12/go.mod h1:hNGJHUnrk76NpqgfD5Aqm5Crs+Hm0VOH/i9J2+nxYbc=
 golang.org/x/tools v0.6.0/go.mod h1:Xwgl3UAJ/d3gWutnCtw505GrjyAbvKui8lOU390QaIU=
+golang.org/x/tools v0.35.0 h1:mBffYraMEf7aa0sB+NuKnuCy8qI/9Bughn8dC2Gu5r0=
+golang.org/x/tools v0.35.0/go.mod h1:NKdj5HkL/73byiZSJjqJgKn3ep7KjFkBOkR/Hps3VPw=
 golang.org/x/xerrors v0.0.0-20190717185122-a985d3407aa7/go.mod h1:I/5z698sn9Ka8TeJc9MKroUUfqBBauWjQqLJ2OPfmY0=
 golang.org/x/xerrors v0.0.0-20191011141410-1b5146add898/go.mod h1:I/5z698sn9Ka8TeJc9MKroUUfqBBauWjQqLJ2OPfmY0=
 golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543/go.mod h1:I/5z698sn9Ka8TeJc9MKroUUfqBBauWjQqLJ2OPfmY0=

commit c47e258ecad8bb43ecdda404923662f0b249f9f2

Fix DOWNLOADER_DISABLE_HTTP2 not working (#16870)
Accidentally broke this in 40ea287b3a43404eaff2559ec9f1b1150b4f2529.

It's not affecting normal operation since most users aren't running with
this flag but it's there as an option.

erigon-db/downloader/downloader.go
@@ -303,6 +303,7 @@ func configureHttp2(t *http.Transport) {
 	if os.Getenv("DOWNLOADER_DISABLE_HTTP2") != "" {
 		// Disable h2 being added automatically.
 		g.MakeMap(&t.TLSNextProto)
+		return
 	}
 	// Don't set the http2.Transport as the RoundTripper. It's hooked into the http.Transport by
 	// this call. Need to use external http2 library to get access to some config fields that