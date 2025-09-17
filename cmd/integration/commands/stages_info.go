1d607829a8f50c6d776af94fc09d3999fbdd7334
JkLondon <me@ilyamikheev.com>
2025-09-17 22:50:28 +0200

    commit

--- cmd/integration/commands/stages_info.go
+++ cmd/integration/commands/stages_info.go
@@ -5,6 +5,7 @@ import (
 	"github.com/erigontech/erigon/db/kv/prune"
 	"github.com/erigontech/erigon/execution/stagedsync/stages"
 	"strings"
+	"time"
 )
 
 type StageProgress struct {
@@ -42,11 +43,11 @@ type StagesInfo struct {
 
 func (info *StagesInfo) Overview() string {
 	return fmt.Sprintf(
-		"Prune mode: %s\nblocks: seg: %d ind: %d\nbor blocks: seg: %d ind: %d\ninfo about last & state.history: txnum: %d, blocknum: %d, steps: %.2f\nEthTxSequence: %d\nIn DB: first header %d, last header %d, first body %d, last body %d",
+		"Prune mode: %s\nblocks: seg: %d ind: %d\nbor blocks: seg: %d ind: %d\ninfo about last & state.history: txnum: %d, blocknum: %d, steps: %.2f\nEthTxSequence: %d\nIn DB: first header %d, last header %d, first body %d, last body %d\nlast update on %s",
 		info.PruneDistance.String(), info.SnapshotInfo.SegMax, info.SnapshotInfo.IndMax,
 		info.BorSnapshotInfo.SegMax, info.BorSnapshotInfo.IndMax,
 		info.LastInfo.TxNum, info.LastInfo.BlockNum, info.LastInfo.IdxSteps,
-		info.EthTxSequence, info.DB.FirstHeader, info.DB.LastHeader, info.DB.FirstBody, info.DB.LastBody)
+		info.EthTxSequence, info.DB.FirstHeader, info.DB.LastHeader, info.DB.FirstBody, info.DB.LastBody, time.Now().Format("15:04:05"))
 }
 
 func (info *StagesInfo) Stages() string {
