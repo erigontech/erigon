7c5cfcdd9532cb899e22621a6125495620d8eaaf
JkLondon <me@ilyamikheev.com>
2025-09-17 21:54:21 +0200

    commit

--- cmd/etui/internals/tui/modules/modules.go
+++ cmd/etui/internals/tui/modules/modules.go
@@ -18,14 +18,19 @@ func Footer() tview.Primitive {
 }
 
 func Body() tview.Primitive {
-	return tview.NewTextView().SetText("starting...")
+	return tview.NewFlex().
+		AddItem(tview.NewTextView().SetText("starting..."), 0, 1, false).
+		AddItem(tview.NewFlex().
+			AddItem(tview.NewTextView().SetText("starting1..."), 0, 1, false).
+			AddItem(tview.NewTextView().SetText("starting2..."), 0, 1, false),
+			0, 5, false)
 }
 
 func TextToBody(app *tview.Application, body *tview.Flex, infoCh <-chan *commands.StagesInfo) {
 	for info := range infoCh {
 		text := strconv.Itoa(rand.Int())
 		app.QueueUpdateDraw(func() {
-			infoView := body.GetItem(1).(*tview.TextView)
+			infoView := body.GetItem(1).(*tview.Flex).GetItem(1).(*tview.Flex).GetItem(0).(*tview.TextView)
 			infoView.Clear()
 			fmt.Fprintf(infoView, "info %+v, text %s", info, text)
 		})
