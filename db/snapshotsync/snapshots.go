faf06c68f627b320c40718a82e31bbcbe6ab6412
Ilya Mikheev <54912776+JkLondon@users.noreply.github.com>
2025-09-18 17:45:21 +0200

    [main] building indexes on old files if have some (cp from 3.1) (#17018)
    cp from #16984
    
    ---------
    
    Co-authored-by: JkLondon <me@ilyamikheev.com>
    Co-authored-by: alex <AskAlexSharov@gmail.com>

--- db/snapshotsync/snapshots.go
+++ db/snapshotsync/snapshots.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"github.com/erigontech/erigon/db/version"
 	"math"
 	"os"
 	"path/filepath"
@@ -464,7 +465,19 @@ func (s *DirtySegment) openIdx(dir string) (err error) {
 		if s.indexes[i] != nil {
 			continue
 		}
-		index, err := recsplit.OpenIndex(filepath.Join(dir, fileName))
+		fPathMask, err := version.ReplaceVersionWithMask(filepath.Join(dir, fileName))
+		if err != nil {
+			return fmt.Errorf("[open index] can't replace with mask in file %s: %w", fileName, err)
+		}
+		fPath, _, ok, err := version.FindFilesWithVersionsByPattern(fPathMask)
+		if err != nil {
+			return fmt.Errorf("%w, fileName: %s", err, fileName)
+		}
+		if !ok {
+			_, fName := filepath.Split(fPath)
+			return fmt.Errorf("[open index] find files by pattern err %w fname %s", os.ErrNotExist, fName)
+		}
+		index, err := recsplit.OpenIndex(fPath)
 
 		if err != nil {
 			return fmt.Errorf("%w, fileName: %s", err, fileName)
