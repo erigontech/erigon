d215b8b098561a763c79da9547be9871df1a6867
Nazarii Denha <dengaaa2002@gmail.com>
2025-09-24 11:17:28 +0200

    minor fix

--- core/state/txtask.go
+++ core/state/txtask.go
@@ -19,6 +19,7 @@ package state
 import (
 	"container/heap"
 	"context"
+	"fmt"
 	"sync"
 	"time"
 
@@ -145,10 +146,12 @@ func (t *TxTask) CreateReceipt(tx kv.TemporalTx) {
 	}
 
 	cumulativeGasUsed += t.GasUsed
-	// if t.GasUsed == 0 { // TODO arbitrum does not need this but others do
-	// 	msg := fmt.Sprintf("assert: no gas used, bn=%d, tn=%d, ti=%d", t.BlockNum, t.TxNum, t.TxIndex)
-	// 	panic(msg)
-	// }
+	if !t.Rules.IsArbitrum {
+		if t.GasUsed == 0 {
+			msg := fmt.Sprintf("assert: no gas used, bn=%d, tn=%d, ti=%d", t.BlockNum, t.TxNum, t.TxIndex)
+			panic(msg)
+		}
+	}
 	r := t.createReceipt(cumulativeGasUsed, firstLogIndex)
 	t.BlockReceipts[t.TxIndex] = r
 }
