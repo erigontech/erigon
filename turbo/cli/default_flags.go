81bad54b20993fee0435a2201700cee4884d27d4
Shoham Chakraborty <shhmchk@gmail.com>
2025-10-01 12:37:40 +0800

    Enable `maxpendpeers` flag

--- turbo/cli/default_flags.go
+++ turbo/cli/default_flags.go
@@ -138,6 +138,7 @@ var DefaultFlags = []cli.Flag{
 	&utils.StaticPeersFlag,
 	&utils.TrustedPeersFlag,
 	&utils.MaxPeersFlag,
+	&utils.MaxPendingPeersFlag,
 	&utils.ChainFlag,
 	&utils.DeveloperPeriodFlag,
 	&utils.VMEnableDebugFlag,
