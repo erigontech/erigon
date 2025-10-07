ddf0c18f8d1d6c651d8dd55c26e6983bc7964055
Mark Holt <135143369+mh0lt@users.noreply.github.com>
2025-10-07 21:37:42 +0100

    Extend blocklimit beyond frozen steps (#17381)
    This PR contains two fixes:
    
    1. Take the txnum for error checking from the domain after comitment
    calculation
    2. Extend initialCycle execution to beyond the frozen state range to
    ensure the state is stored in the DB.
    
    The first fix is actually enough to fix the current qa problem with:
    
    ```
    [EROR] [10-05|02:41:57.717] Could not start execution service        err="[4/6 Execution] can't persist comittement for blockNum 17436067, txNum 54687499: step 34 is frozen"
    ```
    
    But this only became apparent while testing 2, so I have included both
    fixes.

--- execution/stagedsync/exec3.go
+++ execution/stagedsync/exec3.go
@@ -338,8 +338,8 @@ func ExecV3(ctx context.Context,
 					}
 
 					se.lastCommittedBlockNum = lastHeader.Number.Uint64()
-					committedTransactions := inputTxNum - se.lastCommittedTxNum
-					se.lastCommittedTxNum = inputTxNum
+					committedTransactions := se.domains().TxNum() - se.lastCommittedTxNum
+					se.lastCommittedTxNum = se.domains().TxNum()
 
 					commitStart := time.Now()
 					stepsInDb = rawdbhelpers.IdxStepsCountV3(applyTx)
@@ -564,7 +564,7 @@ func (te *txExecutor) onBlockStart(ctx context.Context, blockNum uint64, blockHa
 	}
 }
 
-func (te *txExecutor) executeBlocks(ctx context.Context, tx kv.Tx, blockNum uint64, maxBlockNum uint64, initialTxNum uint64, readAhead chan uint64, applyResults chan applyResult) error {
+func (te *txExecutor) executeBlocks(ctx context.Context, tx kv.TemporalTx, startBlockNum uint64, maxBlockNum uint64, blockLimit uint64, initialTxNum uint64, readAhead chan uint64, initialCycle bool, applyResults chan applyResult) error {
 	inputTxNum, _, offsetFromBlockBeginning, err := restoreTxNum(ctx, &te.cfg, tx, te.doms, maxBlockNum)
 
 	if err != nil {
@@ -586,7 +586,9 @@ func (te *txExecutor) executeBlocks(ctx context.Context, tx kv.Tx, blockNum uint
 			}
 		}()
 
-		for ; blockNum <= maxBlockNum; blockNum++ {
+		lastFrozenStep := tx.StepsInFiles(kv.CommitmentDomain)
+
+		for blockNum := startBlockNum; blockNum <= maxBlockNum; blockNum++ {
 			select {
 			case readAhead <- blockNum:
 			default:
@@ -664,6 +666,16 @@ func (te *txExecutor) executeBlocks(ctx context.Context, tx kv.Tx, blockNum uint
 			if offsetFromBlockBeginning > 0 {
 				// after history execution no offset will be required
 				offsetFromBlockBeginning = 0
+			}
+
+			lastExecutedStep := kv.Step(inputTxNum / te.doms.StepSize())
+
+			// if we're in the initialCycle before we consider the blockLimit we need to make sure we keep executing
+			// until we reach a transaction whose comittement which is writable to the db, otherwise the update will get lost
+			if !initialCycle || lastExecutedStep > 0 && lastExecutedStep > lastFrozenStep && !dbg.DiscardCommitment() {
+				if blockLimit > 0 && startBlockNum-blockNum+1 >= blockLimit {
+					return &ErrLoopExhausted{From: blockNum, To: blockNum, Reason: "block limit reached"}
+				}
 			}
 		}
 
