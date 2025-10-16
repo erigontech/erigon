9b01bf2e0c059cc62c0a9a9ce7e2e4e630291807
antonis19 <antonis19@users.noreply.github.com>
2025-10-16 22:10:33 +0200

    use ibs.Reset()

--- rpc/jsonrpc/trace_adhoc.go
+++ rpc/jsonrpc/trace_adhoc.go
@@ -1413,7 +1413,7 @@ func (api *TraceAPIImpl) doCallBlock(ctx context.Context, dbtx kv.Tx, stateReade
 			sd = &StateDiff{sdMap: sdMap}
 		}
 
-		ibs.SoftReset()
+		ibs.Reset()
 		var finalizeTxStateWriter state.StateWriter
 		if sd != nil {
 			finalizeTxStateWriter = sd
