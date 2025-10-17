bfcfa43164dd6224530fc624a8ad2beba74f6a5c
Andrew Ashikhmin <34320705+yperbasis@users.noreply.github.com>
2025-10-17 03:12:52 +0200

    [r32] version 3.2.1 (#17502)

--- db/version/app.go
+++ db/version/app.go
@@ -31,7 +31,7 @@ var (
 const (
 	Major                    = 3             // Major version component of the current release
 	Minor                    = 2             // Minor version component of the current release
-	Micro                    = 0             // Patch version component of the current release
+	Micro                    = 1             // Patch version component of the current release
 	Modifier                 = ""            // Modifier component of the current release
 	DefaultSnapshotGitBranch = "release/3.1" // Branch of erigontech/erigon-snapshot to use in OtterSync
 	VersionKeyCreated        = "ErigonVersionCreated"
