18d22168a9a63bf745aac7eb9f31d76f4436c694
mh0lt <mark.holt@erigon.tech>
2025-10-18 13:51:54 +0100

    fix compile errors

--- execution/vm/analysis_test.go
+++ execution/vm/analysis_test.go
@@ -94,15 +94,9 @@ func BenchmarkJumpDest(b *testing.B) {
 	contractRef := common.Address{}
 
 	c := NewJumpDestCache(16)
-<<<<<<< HEAD
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		contract := NewContract(contractRef, common.Address{}, common.Address{}, uint256.Int{}, 0, c)
-=======
 
 	for b.Loop() {
-		contract := NewContract(contractRef, common.Address{}, nil, 0, c)
->>>>>>> main
+		contract := NewContract(contractRef, common.Address{}, common.Address{}, uint256.Int{}, 0, c)
 		contract.Code = code
 		contract.CodeHash = hash
 
