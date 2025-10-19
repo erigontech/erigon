5c4fa5025c70c10d20fba2d7cba0542c364cead2
Willian Mitsuda <wmitsuda@gmail.com>
2025-10-19 03:04:11 -0300

    Refactor txNum ranges -> step ranges logic (#17535)
    `FilesItem.startTxNum/endTxNum` -> `from/toStep` logic is used in quite
    a bunch of places in the code.
    
    Although the math is simple, it makes sense to push such behavior into
    FilesItem itself. And the result can be embedded into a function call if
    it expects exactly (from, to), which is most of the places, making the
    code cleaner.
    
    Also there is a trick in how start/endTxNum is calculated at struct
    initialization (and unchecked) that if not respected may lead to
    unexpected results; I added some docs explaining it.

--- db/state/squeeze.go
+++ db/state/squeeze.go
@@ -206,7 +206,7 @@ func SqueezeCommitmentFiles(ctx context.Context, at *AggregatorRoTx, logger log.
 		cf.decompressor.MadvNormal()
 
 		err = func() error {
-			steps := cf.endTxNum/stepSize - cf.startTxNum/stepSize
+			steps := cf.StepCount(stepSize)
 			compression := commitment.d.Compression
 			if steps < DomainMinStepsToCompress {
 				compression = seg.CompressNone
@@ -363,7 +363,9 @@ func RebuildCommitmentFiles(ctx context.Context, rwDb kv.TemporalRwDB, txNumsRea
 
 	ranges := make([]MergeRange, 0)
 	for fi, f := range sf.d[kv.AccountsDomain] {
-		logger.Info(fmt.Sprintf("[commitment_rebuild] shard to build #%d: steps %d-%d (based on %s)", fi, f.startTxNum/a.StepSize(), f.endTxNum/a.StepSize(), f.decompressor.FileName()))
+		fromStep, toStep := f.StepRange(a.StepSize())
+		logger.Info(fmt.Sprintf("[commitment_rebuild] shard to build #%d: steps %d-%d (based on %s)", fi, fromStep, toStep, f.decompressor.FileName()))
+
 		ranges = append(ranges, MergeRange{
 			from: f.startTxNum,
 			to:   f.endTxNum,
