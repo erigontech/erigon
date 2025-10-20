ad7d13274bc1868ef624f7e3f662afa4b7ca0003
Shoham Chakraborty <shhmchk@gmail.com>
2025-10-20 21:46:03 +0800

    publisher

--- execution/p2p/publisher_test.go
+++ execution/p2p/publisher_test.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with Erigon. If not, see <http://www.gnu.org/licenses/>.
 
-package polygonp2p
+package p2p
 
 import (
 	"context"
@@ -33,7 +33,6 @@ import (
 	"github.com/erigontech/erigon/common/event"
 	"github.com/erigontech/erigon/common/log/v3"
 	"github.com/erigontech/erigon/common/testlog"
-	"github.com/erigontech/erigon/execution/p2p"
 	"github.com/erigontech/erigon/execution/types"
 	"github.com/erigontech/erigon/node/direct"
 	"github.com/erigontech/erigon/node/gointerfaces/sentryproto"
@@ -44,35 +43,35 @@ import (
 func TestPublisher(t *testing.T) {
 	newPublisherTest(t).run(func(ctx context.Context, t *testing.T, pt publisherTest) {
 		pt.peerEvent(&sentryproto.PeerEvent{
-			PeerId:  p2p.PeerIdFromUint64(1).H512(),
+			PeerId:  PeerIdFromUint64(1).H512(),
 			EventId: sentryproto.PeerEvent_Connect,
 		})
 		pt.peerEvent(&sentryproto.PeerEvent{
-			PeerId:  p2p.PeerIdFromUint64(2).H512(),
+			PeerId:  PeerIdFromUint64(2).H512(),
 			EventId: sentryproto.PeerEvent_Connect,
 		})
 		pt.peerEvent(&sentryproto.PeerEvent{
-			PeerId:  p2p.PeerIdFromUint64(3).H512(),
+			PeerId:  PeerIdFromUint64(3).H512(),
 			EventId: sentryproto.PeerEvent_Connect,
 		})
 		pt.peerEvent(&sentryproto.PeerEvent{
-			PeerId:  p2p.PeerIdFromUint64(4).H512(),
+			PeerId:  PeerIdFromUint64(4).H512(),
 			EventId: sentryproto.PeerEvent_Connect,
 		})
 		pt.peerEvent(&sentryproto.PeerEvent{
-			PeerId:  p2p.PeerIdFromUint64(5).H512(),
+			PeerId:  PeerIdFromUint64(5).H512(),
 			EventId: sentryproto.PeerEvent_Connect,
 		})
 		pt.peerEvent(&sentryproto.PeerEvent{
-			PeerId:  p2p.PeerIdFromUint64(6).H512(),
+			PeerId:  PeerIdFromUint64(6).H512(),
 			EventId: sentryproto.PeerEvent_Connect,
 		})
 		pt.peerEvent(&sentryproto.PeerEvent{
-			PeerId:  p2p.PeerIdFromUint64(7).H512(),
+			PeerId:  PeerIdFromUint64(7).H512(),
 			EventId: sentryproto.PeerEvent_Connect,
 		})
 		pt.peerEvent(&sentryproto.PeerEvent{
-			PeerId:  p2p.PeerIdFromUint64(8).H512(),
+			PeerId:  PeerIdFromUint64(8).H512(),
 			EventId: sentryproto.PeerEvent_Connect,
 		})
 
@@ -84,24 +83,24 @@ func TestPublisher(t *testing.T) {
 			return func() bool { return len(pt.peerTracker.ListPeersMayMissBlockHash(header1.Hash())) == peersCount }
 		}
 		require.Eventually(t, waitPeersMayMissHash(8), time.Second, 5*time.Millisecond)
-		pt.newBlockEvent(&p2p.DecodedInboundMessage[*eth.NewBlockPacket]{
-			PeerId: p2p.PeerIdFromUint64(1),
+		pt.newBlockEvent(&DecodedInboundMessage[*eth.NewBlockPacket]{
+			PeerId: PeerIdFromUint64(1),
 			Decoded: &eth.NewBlockPacket{
 				Block: block1,
 				TD:    td1,
 			},
 		})
 		require.Eventually(t, waitPeersMayMissHash(7), time.Second, 5*time.Millisecond)
-		pt.newBlockEvent(&p2p.DecodedInboundMessage[*eth.NewBlockPacket]{
-			PeerId: p2p.PeerIdFromUint64(2),
+		pt.newBlockEvent(&DecodedInboundMessage[*eth.NewBlockPacket]{
+			PeerId: PeerIdFromUint64(2),
 			Decoded: &eth.NewBlockPacket{
 				Block: block1,
 				TD:    td1,
 			},
 		})
 		require.Eventually(t, waitPeersMayMissHash(6), time.Second, 5*time.Millisecond)
-		pt.newBlockHashesEvent(&p2p.DecodedInboundMessage[*eth.NewBlockHashesPacket]{
-			PeerId: p2p.PeerIdFromUint64(3),
+		pt.newBlockHashesEvent(&DecodedInboundMessage[*eth.NewBlockHashesPacket]{
+			PeerId: PeerIdFromUint64(3),
 			Decoded: &eth.NewBlockHashesPacket{
 				{
 					Hash:   header1.Hash(),
@@ -110,8 +109,8 @@ func TestPublisher(t *testing.T) {
 			},
 		})
 		require.Eventually(t, waitPeersMayMissHash(5), time.Second, 5*time.Millisecond)
-		pt.newBlockHashesEvent(&p2p.DecodedInboundMessage[*eth.NewBlockHashesPacket]{
-			PeerId: p2p.PeerIdFromUint64(4),
+		pt.newBlockHashesEvent(&DecodedInboundMessage[*eth.NewBlockHashesPacket]{
+			PeerId: PeerIdFromUint64(4),
 			Decoded: &eth.NewBlockHashesPacket{
 				{
 					Hash:   header1.Hash(),
@@ -128,17 +127,17 @@ func TestPublisher(t *testing.T) {
 		}
 		// NewBlock announces should be send to only sqrt(peers) that do not know about this block hash
 		// according to our knowledge: sqrt(4)=2 -> peers 5,6
-		knownSends := map[p2p.PeerId]struct{}{}
-		knownSends[*p2p.PeerIdFromUint64(1)] = struct{}{}
-		knownSends[*p2p.PeerIdFromUint64(2)] = struct{}{}
-		knownSends[*p2p.PeerIdFromUint64(3)] = struct{}{}
-		knownSends[*p2p.PeerIdFromUint64(4)] = struct{}{}
+		knownSends := map[PeerId]struct{}{}
+		knownSends[*PeerIdFromUint64(1)] = struct{}{}
+		knownSends[*PeerIdFromUint64(2)] = struct{}{}
+		knownSends[*PeerIdFromUint64(3)] = struct{}{}
+		knownSends[*PeerIdFromUint64(4)] = struct{}{}
 		require.Eventually(t, waitSends(2), time.Second, 5*time.Millisecond)
-		capturedSend1PeerId := *p2p.PeerIdFromH512(pt.capturedSends()[0].PeerId)
+		capturedSend1PeerId := *PeerIdFromH512(pt.capturedSends()[0].PeerId)
 		_, known := knownSends[capturedSend1PeerId]
 		require.False(t, known)
 		knownSends[capturedSend1PeerId] = struct{}{}
-		capturedSend2PeerId := *p2p.PeerIdFromH512(pt.capturedSends()[1].PeerId)
+		capturedSend2PeerId := *PeerIdFromH512(pt.capturedSends()[1].PeerId)
 		_, known = knownSends[capturedSend2PeerId]
 		require.False(t, known)
 		knownSends[capturedSend2PeerId] = struct{}{}
@@ -147,25 +146,25 @@ func TestPublisher(t *testing.T) {
 		// NewBlockHashes should be sent to all remaining peers that do not already know this block hash
 		// according to our knowledge: peers 7,8
 		require.Eventually(t, waitSends(4), time.Second, 5*time.Millisecond)
-		capturedSend3PeerId := *p2p.PeerIdFromH512(pt.capturedSends()[2].PeerId)
+		capturedSend3PeerId := *PeerIdFromH512(pt.capturedSends()[2].PeerId)
 		_, known = knownSends[capturedSend3PeerId]
 		require.False(t, known)
 		knownSends[capturedSend3PeerId] = struct{}{}
-		capturedSend4PeerId := *p2p.PeerIdFromH512(pt.capturedSends()[3].PeerId)
+		capturedSend4PeerId := *PeerIdFromH512(pt.capturedSends()[3].PeerId)
 		_, known = knownSends[capturedSend4PeerId]
 		require.False(t, known)
 		knownSends[capturedSend4PeerId] = struct{}{}
 		require.Len(t, knownSends, 8)
 		allPeerIds := maps.Keys(knownSends)
-		require.ElementsMatch(t, allPeerIds, []p2p.PeerId{
-			*p2p.PeerIdFromUint64(1),
-			*p2p.PeerIdFromUint64(2),
-			*p2p.PeerIdFromUint64(3),
-			*p2p.PeerIdFromUint64(4),
-			*p2p.PeerIdFromUint64(5),
-			*p2p.PeerIdFromUint64(6),
-			*p2p.PeerIdFromUint64(7),
-			*p2p.PeerIdFromUint64(8),
+		require.ElementsMatch(t, allPeerIds, []PeerId{
+			*PeerIdFromUint64(1),
+			*PeerIdFromUint64(2),
+			*PeerIdFromUint64(3),
+			*PeerIdFromUint64(4),
+			*PeerIdFromUint64(5),
+			*PeerIdFromUint64(6),
+			*PeerIdFromUint64(7),
+			*PeerIdFromUint64(8),
 		})
 
 		// all 8 peers must now know about the hash according to our knowledge
@@ -178,10 +177,10 @@ func newPublisherTest(t *testing.T) publisherTest {
 	t.Cleanup(cancel)
 	logger := testlog.Logger(t, log.LvlCrit)
 	ctrl := gomock.NewController(t)
-	peerEventRegistrar := p2p.NewMockpeerEventRegistrar(ctrl)
-	peerTracker := p2p.NewPeerTracker(logger, peerEventRegistrar, p2p.WithPreservingPeerShuffle)
+	peerEventRegistrar := NewMockpeerEventRegistrar(ctrl)
+	peerTracker := NewPeerTracker(logger, peerEventRegistrar, WithPreservingPeerShuffle)
 	sentryClient := direct.NewMockSentryClient(ctrl)
-	messageSender := p2p.NewMessageSender(sentryClient)
+	messageSender := NewMessageSender(sentryClient)
 	publisher := NewPublisher(logger, messageSender, peerTracker)
 	capturedSends := make([]*sentryproto.SendMessageByIdRequest, 0, 1024)
 	test := publisherTest{
@@ -192,8 +191,8 @@ func newPublisherTest(t *testing.T) publisherTest {
 		peerEventRegistrar:   peerEventRegistrar,
 		publisher:            publisher,
 		peerEventStream:      make(chan *sentryproto.PeerEvent),
-		newBlockHashesStream: make(chan *p2p.DecodedInboundMessage[*eth.NewBlockHashesPacket]),
-		newBlockStream:       make(chan *p2p.DecodedInboundMessage[*eth.NewBlockPacket]),
+		newBlockHashesStream: make(chan *DecodedInboundMessage[*eth.NewBlockHashesPacket]),
+		newBlockStream:       make(chan *DecodedInboundMessage[*eth.NewBlockPacket]),
 		sentryClient:         sentryClient,
 		capturedSendsPtr:     &capturedSends,
 		capturedSendsMu:      &sync.Mutex{},
@@ -210,11 +209,11 @@ type publisherTest struct {
 	ctx                  context.Context
 	ctxCancel            context.CancelFunc
 	t                    *testing.T
-	peerTracker          *p2p.PeerTracker
-	peerEventRegistrar   *p2p.MockpeerEventRegistrar
+	peerTracker          *PeerTracker
+	peerEventRegistrar   *MockpeerEventRegistrar
 	peerEventStream      chan *sentryproto.PeerEvent
-	newBlockHashesStream chan *p2p.DecodedInboundMessage[*eth.NewBlockHashesPacket]
-	newBlockStream       chan *p2p.DecodedInboundMessage[*eth.NewBlockPacket]
+	newBlockHashesStream chan *DecodedInboundMessage[*eth.NewBlockHashesPacket]
+	newBlockStream       chan *DecodedInboundMessage[*eth.NewBlockPacket]
 	sentryClient         *direct.MockSentryClient
 	capturedSendsPtr     *[]*sentryproto.SendMessageByIdRequest
 	capturedSendsMu      *sync.Mutex
@@ -224,7 +223,7 @@ type publisherTest struct {
 func (pt publisherTest) mockPeerEvents(events <-chan *sentryproto.PeerEvent) {
 	pt.peerEventRegistrar.EXPECT().
 		RegisterPeerEventObserver(gomock.Any(), gomock.Any()).
-		DoAndReturn(func(observer event.Observer[*sentryproto.PeerEvent], opts ...p2p.RegisterOpt) p2p.UnregisterFunc {
+		DoAndReturn(func(observer event.Observer[*sentryproto.PeerEvent], opts ...RegisterOpt) UnregisterFunc {
 			ctx, cancel := context.WithCancel(context.Background())
 			go func() {
 				for {
@@ -237,20 +236,20 @@ func (pt publisherTest) mockPeerEvents(events <-chan *sentryproto.PeerEvent) {
 				}
 			}()
 
-			return p2p.UnregisterFunc(cancel)
+			return UnregisterFunc(cancel)
 		}).
 		Times(1)
 }
 
 func (pt publisherTest) peerEvent(e *sentryproto.PeerEvent) {
-	send(pt.ctx, pt.t, pt.peerEventStream, e)
+	sendEvent(pt.ctx, pt.t, pt.peerEventStream, e)
 }
 
-func (pt publisherTest) mockNewBlockHashesEvents(events <-chan *p2p.DecodedInboundMessage[*eth.NewBlockHashesPacket]) {
+func (pt publisherTest) mockNewBlockHashesEvents(events <-chan *DecodedInboundMessage[*eth.NewBlockHashesPacket]) {
 	pt.peerEventRegistrar.EXPECT().
 		RegisterNewBlockHashesObserver(gomock.Any()).
 		DoAndReturn(
-			func(observer event.Observer[*p2p.DecodedInboundMessage[*eth.NewBlockHashesPacket]]) p2p.UnregisterFunc {
+			func(observer event.Observer[*DecodedInboundMessage[*eth.NewBlockHashesPacket]]) UnregisterFunc {
 				ctx, cancel := context.WithCancel(context.Background())
 				go func() {
 					for {
@@ -263,21 +262,21 @@ func (pt publisherTest) mockNewBlockHashesEvents(events <-chan *p2p.DecodedInbou
 					}
 				}()
 
-				return p2p.UnregisterFunc(cancel)
+				return UnregisterFunc(cancel)
 			},
 		).
 		Times(1)
 }
 
-func (pt publisherTest) newBlockHashesEvent(e *p2p.DecodedInboundMessage[*eth.NewBlockHashesPacket]) {
-	send(pt.ctx, pt.t, pt.newBlockHashesStream, e)
+func (pt publisherTest) newBlockHashesEvent(e *DecodedInboundMessage[*eth.NewBlockHashesPacket]) {
+	sendEvent(pt.ctx, pt.t, pt.newBlockHashesStream, e)
 }
 
-func (pt publisherTest) mockNewBlockEvents(events <-chan *p2p.DecodedInboundMessage[*eth.NewBlockPacket]) {
+func (pt publisherTest) mockNewBlockEvents(events <-chan *DecodedInboundMessage[*eth.NewBlockPacket]) {
 	pt.peerEventRegistrar.EXPECT().
 		RegisterNewBlockObserver(gomock.Any()).
 		DoAndReturn(
-			func(observer event.Observer[*p2p.DecodedInboundMessage[*eth.NewBlockPacket]]) p2p.UnregisterFunc {
+			func(observer event.Observer[*DecodedInboundMessage[*eth.NewBlockPacket]]) UnregisterFunc {
 				ctx, cancel := context.WithCancel(context.Background())
 				go func() {
 					for {
@@ -290,14 +289,14 @@ func (pt publisherTest) mockNewBlockEvents(events <-chan *p2p.DecodedInboundMess
 					}
 				}()
 
-				return p2p.UnregisterFunc(cancel)
+				return UnregisterFunc(cancel)
 			},
 		).
 		Times(1)
 }
 
-func (pt publisherTest) newBlockEvent(e *p2p.DecodedInboundMessage[*eth.NewBlockPacket]) {
-	send(pt.ctx, pt.t, pt.newBlockStream, e)
+func (pt publisherTest) newBlockEvent(e *DecodedInboundMessage[*eth.NewBlockPacket]) {
+	sendEvent(pt.ctx, pt.t, pt.newBlockStream, e)
 }
 
 func (pt publisherTest) captureSends(sends *[]*sentryproto.SendMessageByIdRequest) {
@@ -346,7 +345,7 @@ func (pt publisherTest) run(f func(ctx context.Context, t *testing.T, pt publish
 	})
 }
 
-func send[T any](ctx context.Context, t *testing.T, ch chan T, e T) {
+func sendEvent[T any](ctx context.Context, t *testing.T, ch chan T, e T) {
 	ctx, cancel := context.WithTimeout(ctx, time.Second)
 	defer cancel()
 
