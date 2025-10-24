600c4f1239570952f77d5e5861d25a88048818ce
sashaodessa <140454972+sashaodessa@users.noreply.github.com>
2025-10-24 11:38:35 +0200

    Update utils.go

--- node/gointerfaces/grpcutil/utils.go
+++ node/gointerfaces/grpcutil/utils.go
@@ -39,6 +39,7 @@ import (
 	"google.golang.org/grpc/status"
 )
 
+// TLS is deprecated: use ServerTLS or ClientTLS instead.
 func TLS(tlsCACert, tlsCertFile, tlsKeyFile string) (credentials.TransportCredentials, error) {
 	// load peer cert/key, ca cert
 	if tlsCACert == "" {
@@ -63,9 +64,59 @@ func TLS(tlsCACert, tlsCertFile, tlsKeyFile string) (credentials.TransportCreden
 		ClientCAs:    caCertPool,
 		ClientAuth:   tls.RequireAndVerifyClientCert,
 		MinVersion:   tls.VersionTLS12,
-		//nolint:gosec
-		InsecureSkipVerify: true, // This is to make it work when Common Name does not match - remove when procedure is updated for common name
 	}), nil
+}
+
+func ServerTLS(tlsCACert, tlsCertFile, tlsKeyFile string) (credentials.TransportCredentials, error) {
+    if tlsCACert == "" {
+        if tlsCertFile == "" && tlsKeyFile == "" {
+            return nil, nil
+        }
+        return credentials.NewServerTLSFromFile(tlsCertFile, tlsKeyFile)
+    }
+    peerCert, err := tls.LoadX509KeyPair(tlsCertFile, tlsKeyFile)
+    if err != nil {
+        return nil, fmt.Errorf("load peer cert/key error:%w", err)
+    }
+    caCert, err := os.ReadFile(tlsCACert)
+    if err != nil {
+        return nil, fmt.Errorf("read ca cert file error:%w", err)
+    }
+    caCertPool := x509.NewCertPool()
+    caCertPool.AppendCertsFromPEM(caCert)
+    return credentials.NewTLS(&tls.Config{
+        Certificates: []tls.Certificate{peerCert},
+        ClientCAs:    caCertPool,
+        ClientAuth:   tls.RequireAndVerifyClientCert,
+        MinVersion:   tls.VersionTLS12,
+    }), nil
+}
+
+func ClientTLS(tlsCACert, tlsCertFile, tlsKeyFile, serverName string) (credentials.TransportCredentials, error) {
+    var certs []tls.Certificate
+    if tlsCertFile != "" || tlsKeyFile != "" {
+        peerCert, err := tls.LoadX509KeyPair(tlsCertFile, tlsKeyFile)
+        if err != nil {
+            return nil, fmt.Errorf("load peer cert/key error:%w", err)
+        }
+        certs = []tls.Certificate{peerCert}
+    }
+    var rootCAs *x509.CertPool
+    if tlsCACert != "" {
+        caCert, err := os.ReadFile(tlsCACert)
+        if err != nil {
+            return nil, fmt.Errorf("read ca cert file error:%w", err)
+        }
+        rootCAs = x509.NewCertPool()
+        rootCAs.AppendCertsFromPEM(caCert)
+    }
+    cfg := &tls.Config{
+        Certificates: certs,
+        RootCAs:      rootCAs,
+        ServerName:   serverName,
+        MinVersion:   tls.VersionTLS12,
+    }
+    return credentials.NewTLS(cfg), nil
 }
 
 func NewServer(rateLimit uint32, creds credentials.TransportCredentials) *grpc.Server {
