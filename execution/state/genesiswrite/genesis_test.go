92b7a24a1bb7310f62a7b006d110cc79fb7d1de1
yperbasis <andrey.ashikhmin@gmail.com>
2025-11-03 12:54:58 +0100

    mv TestSetupGenesis

--- execution/state/genesiswrite/genesis_test.go
+++ execution/state/genesiswrite/genesis_test.go
@@ -21,8 +21,10 @@ import (
 	"encoding/json"
 	"math/big"
 	"os"
+	"reflect"
 	"testing"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/holiman/uint256"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -34,13 +36,16 @@ import (
 	"github.com/erigontech/erigon/db/kv"
 	"github.com/erigontech/erigon/db/kv/rawdbv3"
 	"github.com/erigontech/erigon/db/kv/temporal/temporaltest"
+	"github.com/erigontech/erigon/db/snapshotsync/freezeblocks"
 	"github.com/erigontech/erigon/execution/chain"
 	"github.com/erigontech/erigon/execution/chain/networkname"
 	chainspec "github.com/erigontech/erigon/execution/chain/spec"
+	"github.com/erigontech/erigon/execution/core"
 	"github.com/erigontech/erigon/execution/stages/mock"
 	"github.com/erigontech/erigon/execution/state"
 	"github.com/erigontech/erigon/execution/state/genesiswrite"
 	"github.com/erigontech/erigon/execution/types"
+	"github.com/erigontech/erigon/node/ethconfig"
 	"github.com/erigontech/erigon/rpc/rpchelper"
 )
 
@@ -180,4 +185,155 @@ func TestDecodeBalance0(t *testing.T) {
 	err = json.Unmarshal(genesisData, genesis)
 	require.NoError(t, err)
 	_ = genesisData
+}
+
+func TestSetupGenesis(t *testing.T) {
+	t.Parallel()
+	var (
+		customghash = common.HexToHash("0x89c99d90b79719238d2645c7642f2c9295246e80775b38cfd162b696817fbd50")
+		customg     = types.Genesis{
+			Config: &chain.Config{ChainID: big.NewInt(1), HomesteadBlock: big.NewInt(3)},
+			Alloc: types.GenesisAlloc{
+				{1}: {Balance: big.NewInt(1), Storage: map[common.Hash]common.Hash{{1}: {1}}},
+			},
+		}
+		oldcustomg = customg
+	)
+	logger := log.New()
+	oldcustomg.Config = &chain.Config{ChainID: big.NewInt(1), HomesteadBlock: big.NewInt(2)}
+	tests := []struct {
+		wantErr    error
+		fn         func(t *testing.T, db kv.RwDB, tmpdir string) (*chain.Config, *types.Block, error)
+		wantConfig *chain.Config
+		name       string
+		wantHash   common.Hash
+	}{
+		{
+			name: "genesis without ChainConfig",
+			fn: func(t *testing.T, db kv.RwDB, tmpdir string) (*chain.Config, *types.Block, error) {
+				return genesiswrite.CommitGenesisBlock(db, new(types.Genesis), datadir.New(tmpdir), logger)
+			},
+			wantErr:    types.ErrGenesisNoConfig,
+			wantConfig: chain.AllProtocolChanges,
+		},
+		{
+			name: "no block in DB, genesis == nil",
+			fn: func(t *testing.T, db kv.RwDB, tmpdir string) (*chain.Config, *types.Block, error) {
+				return genesiswrite.CommitGenesisBlock(db, nil, datadir.New(tmpdir), logger)
+			},
+			wantHash:   chainspec.Mainnet.GenesisHash,
+			wantConfig: chainspec.Mainnet.Config,
+		},
+		{
+			name: "mainnet block in DB, genesis == nil",
+			fn: func(t *testing.T, db kv.RwDB, tmpdir string) (*chain.Config, *types.Block, error) {
+				return genesiswrite.CommitGenesisBlock(db, nil, datadir.New(tmpdir), logger)
+			},
+			wantHash:   chainspec.Mainnet.GenesisHash,
+			wantConfig: chainspec.Mainnet.Config,
+		},
+		{
+			name: "custom block in DB, genesis == nil",
+			fn: func(t *testing.T, db kv.RwDB, tmpdir string) (*chain.Config, *types.Block, error) {
+				genesiswrite.MustCommitGenesis(&customg, db, datadir.New(tmpdir), logger)
+				return genesiswrite.CommitGenesisBlock(db, nil, datadir.New(tmpdir), logger)
+			},
+			wantHash:   customghash,
+			wantConfig: customg.Config,
+		},
+		{
+			name: "custom block in DB, genesis == sepolia",
+			fn: func(t *testing.T, db kv.RwDB, tmpdir string) (*chain.Config, *types.Block, error) {
+				genesiswrite.MustCommitGenesis(&customg, db, datadir.New(tmpdir), logger)
+				return genesiswrite.CommitGenesisBlock(db, chainspec.SepoliaGenesisBlock(), datadir.New(tmpdir), logger)
+			},
+			wantErr:    &genesiswrite.GenesisMismatchError{Stored: customghash, New: chainspec.Sepolia.GenesisHash},
+			wantHash:   chainspec.Sepolia.GenesisHash,
+			wantConfig: chainspec.Sepolia.Config,
+		},
+		{
+			name: "compatible config in DB",
+			fn: func(t *testing.T, db kv.RwDB, tmpdir string) (*chain.Config, *types.Block, error) {
+				genesiswrite.MustCommitGenesis(&oldcustomg, db, datadir.New(tmpdir), logger)
+				return genesiswrite.CommitGenesisBlock(db, &customg, datadir.New(tmpdir), logger)
+			},
+			wantHash:   customghash,
+			wantConfig: customg.Config,
+		},
+		{
+			name: "incompatible config in DB",
+			fn: func(t *testing.T, db kv.RwDB, tmpdir string) (*chain.Config, *types.Block, error) {
+				//if ethconfig.EnableHistoryV4InTest {
+				//	t.Skip("fix me")
+				//}
+				// Commit the 'old' genesis block with Homestead transition at #2.
+				// Advance to block #4, past the homestead transition block of customg.
+				key, _ := crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291")
+				m := mock.MockWithGenesis(t, &oldcustomg, key, false)
+
+				chainBlocks, err := core.GenerateChain(m.ChainConfig, m.Genesis, m.Engine, m.DB, 4, nil)
+				if err != nil {
+					return nil, nil, err
+				}
+				if err = m.InsertChain(chainBlocks); err != nil {
+					return nil, nil, err
+				}
+				// This should return a compatibility error.
+				return genesiswrite.CommitGenesisBlock(m.DB, &customg, datadir.New(tmpdir), logger)
+			},
+			wantHash:   customghash,
+			wantConfig: customg.Config,
+			wantErr: &chain.ConfigCompatError{
+				What:         "Homestead fork block",
+				StoredConfig: big.NewInt(2),
+				NewConfig:    big.NewInt(3),
+				RewindTo:     1,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+			tmpdir := t.TempDir()
+			dirs := datadir.New(tmpdir)
+			db := temporaltest.NewTestDB(t, dirs)
+			//cc := tool.ChainConfigFromDB(db)
+			freezingCfg := ethconfig.Defaults.Snapshot
+			//freezingCfg.ChainName = cc.ChainName //TODO: nil-pointer?
+			blockReader := freezeblocks.NewBlockReader(freezeblocks.NewRoSnapshots(freezingCfg, dirs.Snap, log.New()), nil)
+			config, genesis, err := test.fn(t, db, tmpdir)
+			// Check the return values.
+			if !reflect.DeepEqual(err, test.wantErr) {
+				spew := spew.ConfigState{DisablePointerAddresses: true, DisableCapacities: true}
+				t.Fatalf("%s: returned error %#v, want %#v", test.name, spew.NewFormatter(err), spew.NewFormatter(test.wantErr))
+			}
+			if !reflect.DeepEqual(config, test.wantConfig) {
+				t.Errorf("%s:\nreturned %v\nwant     %v", test.name, config, test.wantConfig)
+			}
+
+			if test.wantHash == (common.Hash{}) {
+				if genesis != nil {
+					t.Fatalf("%s: returned non-nil genesis block, want nil", test.name)
+				}
+				return
+			}
+
+			if genesis.Hash() != test.wantHash {
+
+				t.Errorf("%s: returned hash %s, want %s", test.name, genesis.Hash().Hex(), test.wantHash.Hex())
+			} else if err == nil {
+				if dbErr := db.View(context.Background(), func(tx kv.Tx) error {
+					// Check database content.
+					stored, _, _ := blockReader.BlockWithSenders(context.Background(), tx, test.wantHash, 0)
+					if stored.Hash() != test.wantHash {
+						t.Errorf("%s: block in DB has hash %s, want %s", test.name, stored.Hash(), test.wantHash)
+					}
+					return nil
+				}); dbErr != nil {
+					t.Fatal(dbErr)
+				}
+			}
+		})
+	}
 }
