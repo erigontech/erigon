449cc14ae333460231a3fd6e11f2364edfcf6921
yperbasis <andrey.ashikhmin@gmail.com>
2025-11-03 14:11:27 +0100

    mv accessors_index_test to rawdb

--- db/rawdb/accessors_indexes_test.go
+++ db/rawdb/accessors_indexes_test.go
@@ -17,7 +17,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with Erigon. If not, see <http://www.gnu.org/licenses/>.
 
-package mock_test
+package rawdb_test
 
 import (
 	"context"
