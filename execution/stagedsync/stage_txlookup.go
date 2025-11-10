a71a20035c17eaa34581d60da0898447e3d8de02
Doryu <anna.shuraeva16@gmail.com>
2025-11-10 12:09:34 +0100

    Respect caller context when reading tx numbers (#17816)
    Swap the hard-coded context.TODO() for the caller-provided ctx when
    creating the TxnumReader lets staged sync cancel DB reads promptly
    during shutdown or unwind

--- execution/stagedsync/stage_txlookup.go
+++ execution/stagedsync/stage_txlookup.go
@@ -137,7 +137,7 @@ func SpawnTxLookup(s *StageState, tx kv.RwTx, toBlock uint64, cfg TxLookupCfg, c
 
 // txnLookupTransform - [startKey, endKey)
 func txnLookupTransform(logPrefix string, tx kv.RwTx, blockFrom, blockTo uint64, ctx context.Context, cfg TxLookupCfg, logger log.Logger) (err error) {
-	txNumReader := cfg.blockReader.TxnumReader(context.TODO())
+	txNumReader := cfg.blockReader.TxnumReader(ctx)
 	data := make([]byte, 16)
 	return etl.Transform(logPrefix, tx, kv.HeaderCanonical, kv.TxLookup, cfg.tmpdir, func(k, v []byte, next etl.ExtractNextFunc) error {
 		blocknum, blockHash := binary.BigEndian.Uint64(k), common.CastToHash(v)
