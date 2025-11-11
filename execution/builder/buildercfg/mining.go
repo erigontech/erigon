eb67b7f7ffaded2af773ed03580b127ddc5af893
Andrew Ashikhmin <34320705+yperbasis@users.noreply.github.com>
2025-11-11 11:01:37 +0100

    Remove PoW mining (#17813)

--- execution/builder/buildercfg/mining.go
+++ execution/builder/buildercfg/mining.go
@@ -17,24 +17,14 @@
 package buildercfg
 
 import (
-	"crypto/ecdsa"
-	"math/big"
-	"time"
-
 	"github.com/erigontech/erigon/common"
 	"github.com/erigontech/erigon/common/hexutil"
 )
 
 // MiningConfig is the configuration parameters of mining.
 type MiningConfig struct {
-	Enabled    bool
 	EnabledPOS bool
-	Noverify   bool              // Disable remote mining solution verification(only useful in ethash).
-	Etherbase  common.Address    `toml:",omitempty"` // Public address for block mining rewards
-	SigKey     *ecdsa.PrivateKey // ECDSA private key for signing blocks
-	Notify     []string          `toml:",omitempty"` // HTTP URL list to be notified of new work packages(only useful in ethash).
-	ExtraData  hexutil.Bytes     `toml:",omitempty"` // Block extra data set by the miner
-	GasLimit   *uint64           // Target gas limit for mined blocks.
-	GasPrice   *big.Int          // Minimum gas price for mining a transaction
-	Recommit   time.Duration     // The time interval for miner to re-create mining work.
+	Etherbase  common.Address `toml:",omitempty"` // Public address for block mining rewards
+	ExtraData  hexutil.Bytes  `toml:",omitempty"` // Block extra data set by the miner
+	GasLimit   *uint64        // Target gas limit for mined blocks.
 }
