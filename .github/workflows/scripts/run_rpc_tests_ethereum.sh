bca59583c331dce622d070a3825df1f76cd9ae8e
lupin012 <58134934+lupin012@users.noreply.github.com>
2025-11-17 17:41:48 +0100

    rpcdaemon: add support includeEmpty (#17823)
    This PR include:
    - support IncludeEmpty (new config of Geth)
    - in case diffMode  NOT includes empty account
    - in case diffMode=false and IncludeEmpty=false NOT includes empty
    account
    - in case diffMode=false and IncludeEmpty includes empty account
    
    This behavior is equivalent to Geth
    
    the PR includes new rpc-tests

--- .github/workflows/scripts/run_rpc_tests_ethereum.sh
+++ .github/workflows/scripts/run_rpc_tests_ethereum.sh
@@ -43,4 +43,4 @@ DISABLED_TEST_LIST=(
 DISABLED_TESTS=$(IFS=,; echo "${DISABLED_TEST_LIST[*]}")
 
 # Call the main test runner script with the required and optional parameters
-"$(dirname "$0")/run_rpc_tests.sh" mainnet v1.100.0 "$DISABLED_TESTS" "$WORKSPACE" "$RESULT_DIR"
+"$(dirname "$0")/run_rpc_tests.sh" mainnet v1.101.0 "$DISABLED_TESTS" "$WORKSPACE" "$RESULT_DIR"
