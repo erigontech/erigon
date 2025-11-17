1e471d5e7caafbcfd950e49c00fd093c45efd96f
kewei <kewei.train@gmail.com>
2025-11-18 00:41:50 +0800

    update

--- cl/phase1/network/services/aggregate_and_proof_service.go
+++ cl/phase1/network/services/aggregate_and_proof_service.go
@@ -140,6 +140,7 @@ func (a *aggregateAndProofServiceImpl) ProcessMessage(
 	if aggregateData.Slot > a.syncedDataManager.HeadSlot() {
 		//a.scheduleAggregateForLaterProcessing(aggregateAndProof)
 		return fmt.Errorf("%w: aggregate is for a future slot: %d > %d", ErrIgnore, aggregateData.Slot, a.syncedDataManager.HeadSlot())
+
 	}
 
 	epoch := slot / a.beaconCfg.SlotsPerEpoch
