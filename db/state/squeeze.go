c727c0c9b2d9b7dbb120fed450e21baa2c90d8f4
Ilya Mikheev <54912776+JkLondon@users.noreply.github.com>
2025-11-18 03:25:28 +0100

    unit tests + small parsing unification (#17915)
    closes #16483
    
    ---------
    
    Co-authored-by: JkLondon <me@ilyamikheev.com>

--- db/state/squeeze.go
+++ db/state/squeeze.go
@@ -6,6 +6,7 @@ import (
 	"encoding/hex"
 	"errors"
 	"fmt"
+	"github.com/erigontech/erigon/db/version"
 	"math"
 	"os"
 	"path/filepath"
@@ -57,17 +58,17 @@ func (a *Aggregator) Sqeeze(ctx context.Context, domain kv.Domain) error {
 
 		filesToRemove = append(filesToRemove,
 			tempFileCopy,
-			strings.ReplaceAll(to, ".kv", ".kv.torrent"),
-			strings.ReplaceAll(to, ".kv", ".bt"),
-			strings.ReplaceAll(to, ".kv", ".bt.torrent"),
-			strings.ReplaceAll(to, ".kv", ".kvei"),
-			strings.ReplaceAll(to, ".kv", ".kvei.torrent"),
-			strings.ReplaceAll(to, ".kv", ".kvi"),
-			strings.ReplaceAll(to, ".kv", ".kvi.torrent"))
+			version.MakeMaskedWithExtReplace(to, ".kv.torrent"),
+			version.MakeMaskedWithExtReplace(to, ".bt"),
+			version.MakeMaskedWithExtReplace(to, ".bt.torrent"),
+			version.MakeMaskedWithExtReplace(to, ".kvei"),
+			version.MakeMaskedWithExtReplace(to, ".kvei.torrent"),
+			version.MakeMaskedWithExtReplace(to, ".kvi"),
+			version.MakeMaskedWithExtReplace(to, ".kvi.torrent"))
 	}
 
 	for _, f := range filesToRemove {
-		if err := dir.RemoveFile(f); err != nil {
+		if err := dir.RemoveFilesByMask(f); err != nil {
 			return err
 		}
 	}
