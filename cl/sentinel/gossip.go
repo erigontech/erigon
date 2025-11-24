008b6671f94cd04479a85c4364166804aa35b065
kewei <kewei.train@gmail.com>
2025-11-25 01:20:17 +0800

    update

--- cl/sentinel/gossip.go
+++ cl/sentinel/gossip.go
@@ -24,46 +24,6 @@ import (
 
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	"github.com/libp2p/go-libp2p/core/peer"
-
-	"github.com/erigontech/erigon/cl/gossip"
-)
-
-const (
-	// beaconBlockWeight specifies the scoring weight that we apply to
-	// our beacon block topic.
-	beaconBlockWeight = 0.8
-	// aggregateWeight specifies the scoring weight that we apply to
-	// our aggregate topic.
-	aggregateWeight = 0.5
-	// syncContributionWeight specifies the scoring weight that we apply to
-	// our sync contribution topic.
-	syncContributionWeight = 0.2
-	// attestationTotalWeight specifies the scoring weight that we apply to
-	// our attestation subnet topic.
-	attestationTotalWeight = 1
-	// syncCommitteesTotalWeight specifies the scoring weight that we apply to
-	// our sync subnet topic.
-	syncCommitteesTotalWeight = 0.4
-	// attesterSlashingWeight specifies the scoring weight that we apply to
-	// our attester slashing topic.
-	attesterSlashingWeight = 0.05
-	// proposerSlashingWeight specifies the scoring weight that we apply to
-	// our proposer slashing topic.
-	proposerSlashingWeight = 0.05
-	// voluntaryExitWeight specifies the scoring weight that we apply to
-	// our voluntary exit topic.
-	voluntaryExitWeight = 0.05
-	// blsToExecutionChangeWeight specifies the scoring weight that we apply to
-	// our bls to execution topic.
-	blsToExecutionChangeWeight = 0.05
-
-	// maxInMeshScore describes the max score a peer can attain from being in the mesh.
-	maxInMeshScore = 10
-	// maxFirstDeliveryScore describes the max score a peer can obtain from first deliveries.
-	maxFirstDeliveryScore = 40
-
-	// dampeningFactor reduces the amount by which the various thresholds and caps are created.
-	dampeningFactor = 90
 )
 
 const SSZSnappyCodec = "ssz_snappy"
@@ -71,51 +31,6 @@ const SSZSnappyCodec = "ssz_snappy"
 type GossipTopic struct {
 	Name     string
 	CodecStr string
-}
-
-var BeaconBlockSsz = GossipTopic{
-	Name:     gossip.TopicNameBeaconBlock,
-	CodecStr: SSZSnappyCodec,
-}
-
-var BeaconAggregateAndProofSsz = GossipTopic{
-	Name:     gossip.TopicNameBeaconAggregateAndProof,
-	CodecStr: SSZSnappyCodec,
-}
-
-var VoluntaryExitSsz = GossipTopic{
-	Name:     gossip.TopicNameVoluntaryExit,
-	CodecStr: SSZSnappyCodec,
-}
-
-var ProposerSlashingSsz = GossipTopic{
-	Name:     gossip.TopicNameProposerSlashing,
-	CodecStr: SSZSnappyCodec,
-}
-
-var AttesterSlashingSsz = GossipTopic{
-	Name:     gossip.TopicNameAttesterSlashing,
-	CodecStr: SSZSnappyCodec,
-}
-
-var BlsToExecutionChangeSsz = GossipTopic{
-	Name:     gossip.TopicNameBlsToExecutionChange,
-	CodecStr: SSZSnappyCodec,
-}
-
-var SyncCommitteeContributionAndProofSsz = GossipTopic{
-	Name:     gossip.TopicNameSyncCommitteeContributionAndProof,
-	CodecStr: SSZSnappyCodec,
-}
-
-var LightClientFinalityUpdateSsz = GossipTopic{
-	Name:     gossip.TopicNameLightClientFinalityUpdate,
-	CodecStr: SSZSnappyCodec,
-}
-
-var LightClientOptimisticUpdateSsz = GossipTopic{
-	Name:     gossip.TopicNameLightClientOptimisticUpdate,
-	CodecStr: SSZSnappyCodec,
 }
 
 type GossipManager struct {
@@ -141,43 +56,10 @@ func (s *GossipManager) Recv() <-chan *GossipMessage {
 }
 
 func (s *Sentinel) SubscribeGossip(topic GossipTopic, expiration time.Time, opts ...pubsub.TopicOpt) (sub *GossipSubscription, err error) {
-	/*	digest, err := s.ethClock.CurrentForkDigest()
-		if err != nil {
-			log.Error("[Gossip] Failed to calculate fork choice", "err", err)
-		}
-		var exp atomic.Value
-		exp.Store(expiration)
-		sub = &GossipSubscription{
-			gossip_topic: topic,
-			ch:           s.subManager.ch,
-			host:         s.p2p.Host().ID(),
-			ctx:          s.ctx,
-			expiration:   exp,
-			s:            s,
-		}
-		path := fmt.Sprintf("/eth2/%x/%s/%s", digest, topic.Name, topic.CodecStr)
-		sub.topic, err = s.p2p.Pubsub().Join(path, opts...)
-		if err != nil {
-			return nil, fmt.Errorf("failed to join topic %s, err=%w", path, err)
-		}
-		topicScoreParams := s.topicScoreParams(topic.Name)
-		if topicScoreParams != nil {
-			sub.topic.SetScoreParams(topicScoreParams)
-		}
-		s.subManager.AddSubscription(path, sub)
-
-		return sub, nil*/
 	panic("do not call this")
 }
 
 func (s *Sentinel) Unsubscribe(topic GossipTopic, opts ...pubsub.TopicOpt) (err error) {
-	/*	digest, err := s.ethClock.CurrentForkDigest()
-		if err != nil {
-			log.Error("[Gossip] Failed to calculate fork choice", "err", err)
-		}
-		s.subManager.unsubscribe(fmt.Sprintf("/eth2/%x/%s/%s", digest, topic.Name, topic.CodecStr))
-
-		return nil*/
 	panic("do not call this")
 }
 
@@ -189,38 +71,6 @@ func (g *GossipManager) Close() {
 		return true
 	})
 }
-
-/*
-func (g *GossipManager) Start(ctx context.Context) {
-	go func() {
-		checkingInterval := time.NewTicker(time.Second)
-		dbgLogInterval := time.NewTicker(5 * time.Second)
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			case <-checkingInterval.C:
-				g.subscriptions.Range(func(key, value any) bool {
-					sub := value.(*GossipSubscription)
-					sub.checkIfTopicNeedsToEnabledOrDisabled()
-					return true
-				})
-			case <-dbgLogInterval.C:
-				logArgs := []interface{}{}
-				g.subscriptions.Range(func(key, value any) bool {
-					sub := value.(*GossipSubscription)
-					sub.lock.Lock()
-					if sub.topic != nil {
-						logArgs = append(logArgs, sub.topic.String(), sub.subscribed.Load())
-					}
-					sub.lock.Unlock()
-					return true
-				})
-				log.Trace("[Gossip] Subscriptions", "subscriptions", logArgs)
-			}
-		}
-	}()
-}*/
 
 // GossipSubscription abstracts a gossip subscription to write decoded structs.
 type GossipSubscription struct {
@@ -243,37 +93,6 @@ type GossipSubscription struct {
 	closeOnce sync.Once
 	lock      sync.Mutex
 }
-
-/*
-func (sub *GossipSubscription) checkIfTopicNeedsToEnabledOrDisabled() {
-	sub.lock.Lock()
-	defer sub.lock.Unlock()
-	var err error
-	expirationTime := sub.expiration.Load().(time.Time)
-	if sub.subscribed.Load() && time.Now().After(expirationTime) {
-		sub.stopCh <- struct{}{}
-		sub.subscribed.Store(false)
-		log.Info("[Gossip] Unsubscribed from topic", "topic", sub.sub.Topic())
-		sub.s.updateENROnSubscription(sub.sub.Topic(), false)
-		return
-	}
-	if !sub.subscribed.Load() && time.Now().Before(expirationTime) {
-		sub.stopCh = make(chan struct{}, 3)
-
-		sub.sub, err = sub.topic.Subscribe()
-		if err != nil {
-			log.Warn("[Gossip] failed to begin topic subscription", "err", err)
-			return
-		}
-		var sctx context.Context
-		sctx, sub.cf = context.WithCancel(sub.ctx)
-		go sub.run(sctx, sub.sub, sub.sub.Topic())
-		sub.subscribed.Store(true)
-		sub.s.updateENROnSubscription(sub.sub.Topic(), true)
-		log.Debug("[Gossip] Subscribed to topic", "topic", sub.sub.Topic())
-	}
-
-}*/
 
 func (sub *GossipSubscription) OverwriteSubscriptionExpiry(expiry time.Time) {
 	panic("do not call this")
@@ -310,62 +129,6 @@ type GossipMessage struct {
 	Data      []byte
 }
 
-// this is a helper to begin running the gossip subscription.
-// function should not be used outside of the constructor for gossip subscription
-/*func (s *GossipSubscription) run(ctx context.Context, sub *pubsub.Subscription, topicName string) {
-	defer func() {
-		if r := recover(); r != nil {
-			log.Error("[Sentinel Gossip] Message Handler Crashed", "err", r)
-		}
-	}()
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		case <-s.stopCh:
-			sub.Cancel()
-			return
-		default:
-			msg, err := sub.Next(ctx)
-			if err != nil {
-				if errors.Is(err, context.Canceled) {
-					return
-				}
-				log.Warn("[Sentinel] fail to decode gossip packet", "err", err, "topicName", topicName)
-				return
-			}
-
-			if msg.ReceivedFrom == s.host {
-				continue
-			}
-
-			s.ch <- &GossipMessage{
-				From:      msg.ReceivedFrom,
-				TopicName: topicName,
-				Data:      common.Copy(msg.Data),
-			}
-		}
-	}
-}*/
-
 func (g *GossipSubscription) Publish(data []byte) error {
 	panic("do not call this")
-	/*if len(g.topic.ListPeers()) < 2 {
-		log.Trace("[Gossip] No peers to publish to for topic", "topic", g.topic.String())
-		go func() {
-			if err := g.topic.Publish(g.ctx, data, pubsub.WithReadiness(pubsub.MinTopicSize(1))); err != nil {
-				g.s.logger.Debug("[Gossip] Published to topic", "topic", g.topic.String(), "err", err)
-			}
-		}()
-		if len(g.topic.ListPeers()) == 0 {
-			return errors.New("not enough peers to publish the message")
-		}
-		return nil
-	}
-
-	err := g.topic.Publish(g.ctx, data)
-	if err != nil {
-		return errors.New("failed to publish to topic due to lack of routing capacity (topic too small)")
-	}
-	return nil*/
 }
--- cl/sentinel/sentinel.go
+++ cl/sentinel/sentinel.go
@@ -23,7 +23,6 @@ import (
 	"os/signal"
 	"sync"
 	"syscall"
-	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/libp2p/go-libp2p/core/host"
@@ -49,25 +48,6 @@ import (
 	"github.com/erigontech/erigon/p2p/discover"
 	"github.com/erigontech/erigon/p2p/enode"
 	"github.com/erigontech/erigon/p2p/enr"
-)
-
-const (
-	// overlay parameters
-	gossipSubD    = 4 // topic stable mesh target count
-	gossipSubDlo  = 2 // topic stable mesh low watermark
-	gossipSubDhi  = 6 // topic stable mesh high watermark
-	gossipSubDout = 1 // topic stable mesh target out degree. // Dout must be set below Dlo, and must not exceed D / 2.
-
-	// gossip parameters
-	gossipSubMcacheLen    = 6   // number of windows to retain full messages in cache for `IWANT` responses
-	gossipSubMcacheGossip = 3   // number of windows to gossip about
-	gossipSubSeenTTL      = 550 // number of heartbeat intervals to retain message IDs
-	// heartbeat interval
-	gossipSubHeartbeatInterval = 700 * time.Millisecond // frequency of heartbeat, milliseconds
-
-	// decayToZero specifies the terminal value that we will use when decaying
-	// a value.
-	decayToZero = 0.01
 )
 
 type Sentinel struct {
@@ -101,93 +81,9 @@ type Sentinel struct {
 	metadataLock sync.Mutex
 }
 
-/*
-func (s *Sentinel) createLocalNode(
-	privKey *ecdsa.PrivateKey,
-	ipAddr net.IP,
-	udpPort, tcpPort int,
-	tmpDir string,
-) (*enode.LocalNode, error) {
-	db, err := enode.OpenDB(s.ctx, "", tmpDir, s.logger)
-	if err != nil {
-		return nil, fmt.Errorf("could not open node's peer database: %w", err)
-	}
-	localNode := enode.NewLocalNode(db, privKey, s.logger)
-
-	ipEntry := enr.IP(ipAddr)
-	udpEntry := enr.UDP(udpPort)
-	tcpEntry := enr.TCP(tcpPort)
-
-	localNode.Set(ipEntry)
-	localNode.Set(udpEntry)
-	localNode.Set(tcpEntry)
-
-	localNode.SetFallbackIP(ipAddr)
-	localNode.SetFallbackUDP(udpPort)
-	//s.setupENR(localNode)
-	//go s.updateENR(localNode)
-
-	return localNode, nil
-}*/
-
 func (s *Sentinel) SetStatus(status *cltypes.Status) {
 	s.handshaker.SetStatus(status)
 }
-
-/*
-func (s *Sentinel) createListener() (*discover.UDPv5, error) {
-	var (
-		ipAddr  = s.cfg.IpAddr
-		port    = s.cfg.Port
-		discCfg = s.discoverConfig
-	)
-
-	ip := net.ParseIP(ipAddr)
-	if ip == nil {
-		return nil, fmt.Errorf("bad ip address provided, %s was provided", ipAddr)
-	}
-
-	var bindIP net.IP
-	var networkVersion string
-	// If the IP is an IPv4 address, bind to the correct zero address.
-	if ip.To4() != nil {
-		bindIP, networkVersion = ip.To4(), "udp4"
-	} else {
-		bindIP, networkVersion = ip.To16(), "udp6"
-	}
-
-	udpAddr := &net.UDPAddr{
-		IP:   bindIP,
-		Port: port,
-	}
-	conn, err := net.ListenUDP(networkVersion, udpAddr)
-	if err != nil {
-		return nil, err
-	}
-
-	localNode, err := s.createLocalNode(discCfg.PrivateKey, ip, port, int(s.cfg.TCPPort), s.cfg.TmpDir)
-	if err != nil {
-		return nil, err
-	}
-
-	// Start stream handlers
-	net, err := discover.ListenV5(s.ctx, "any", conn, localNode, discCfg)
-	if err != nil {
-		return nil, err
-	}
-
-	handlers.NewConsensusHandlers(
-		s.ctx,
-		s.blockReader,
-		s.indiciesDB,
-		s.p2p.Host(),
-		s.peers,
-		s.cfg.NetworkConfig,
-		localNode,
-		s.cfg.BeaconConfig, s.ethClock, s.handshaker, s.forkChoiceReader, s.blobStorage, s.dataColumnStorage, s.peerDasStateReader, s.cfg.EnableBlocks).Start()
-
-	return net, err
-}*/
 
 // This is just one of the examples from the libp2p repository.
 func New(
@@ -243,7 +139,6 @@ func New(
 	s.peers = peers.NewPool(s.p2p.Host())
 
 	mux := chi.NewRouter()
-	//	mux := httpreqresp.NewRequestHandler(host)
 	mux.Get("/", httpreqresp.NewRequestHandler(s.p2p.Host()))
 	s.httpApi = mux
 
@@ -312,90 +207,6 @@ func (s *Sentinel) HasTooManyPeers() bool {
 	active, _, _ := s.GetPeersCount()
 	return active >= int(s.cfg.MaxPeerCount)
 }
-
-// func (s *Sentinel) isPeerUsefulForAnySubnet(node *enode.Node) bool {
-// 	ret := false
-
-// 	nodeAttnets := bitfield.NewBitvector64()
-// 	nodeSyncnets := bitfield.NewBitvector4()
-// 	if err := node.Load(enr.WithEntry(s.cfg.NetworkConfig.AttSubnetKey, &nodeAttnets)); err != nil {
-// 		log.Trace("Could not load att subnet", "err", err)
-// 		return false
-// 	}
-// 	if err := node.Load(enr.WithEntry(s.cfg.NetworkConfig.SyncCommsSubnetKey, &nodeSyncnets)); err != nil {
-// 		log.Trace("Could not load sync subnet", "err", err)
-// 		return false
-// 	}
-
-// 	s.subManager.subscriptions.Range(func(key, value any) bool {
-// 		sub := value.(*GossipSubscription)
-// 		sub.lock.Lock()
-// 		defer sub.lock.Unlock()
-// 		if sub.sub == nil {
-// 			return true
-// 		}
-
-// 		if !sub.subscribed.Load() {
-// 			return true
-// 		}
-
-// 		if len(sub.topic.ListPeers()) > peerSubnetTarget {
-// 			return true
-// 		}
-// 		if gossip.IsTopicBeaconAttestation(sub.sub.Topic()) {
-// 			ret = s.isPeerUsefulForAttNet(sub, nodeAttnets)
-// 			return !ret
-// 		}
-
-// 		if gossip.IsTopicSyncCommittee(sub.sub.Topic()) {
-// 			ret = s.isPeerUsefulForSyncNet(sub, nodeSyncnets)
-// 			return !ret
-// 		}
-
-// 		return true
-// 	})
-// 	return ret
-// }
-
-// func (s *Sentinel) isPeerUsefulForAttNet(sub *GossipSubscription, nodeAttnets bitfield.Bitvector64) bool {
-// 	splitTopic := strings.Split(sub.sub.Topic(), "/")
-// 	if len(splitTopic) < 4 {
-// 		return false
-// 	}
-// 	subnetIdStr, found := strings.CutPrefix(splitTopic[3], "beacon_attestation_")
-// 	if !found {
-// 		return false
-// 	}
-// 	subnetId, err := strconv.Atoi(subnetIdStr)
-// 	if err != nil {
-// 		log.Warn("Could not parse subnet id", "subnet", subnetIdStr, "err", err)
-// 		return false
-// 	}
-// 	// check if subnetIdth bit is set in nodeAttnets
-// 	return nodeAttnets.BitAt(uint64(subnetId))
-
-// }
-
-// func (s *Sentinel) isPeerUsefulForSyncNet(sub *GossipSubscription, nodeSyncnets bitfield.Bitvector4) bool {
-// 	splitTopic := strings.Split(sub.sub.Topic(), "/")
-// 	if len(splitTopic) < 4 {
-// 		return false
-// 	}
-// 	syncnetIdStr, found := strings.CutPrefix(splitTopic[3], "sync_committee_")
-// 	if !found {
-// 		return false
-// 	}
-// 	syncnetId, err := strconv.Atoi(syncnetIdStr)
-// 	if err != nil {
-// 		log.Warn("Could not parse syncnet id", "syncnet", syncnetIdStr, "err", err)
-// 		return false
-// 	}
-// 	// check if syncnetIdth bit is set in nodeSyncnets
-// 	if nodeSyncnets.BitAt(uint64(syncnetId)) {
-// 		return true
-// 	}
-// 	return false
-// }
 
 func (s *Sentinel) GetPeersCount() (active int, connected int, disconnected int) {
 	peers := s.p2p.Host().Network().Peers()
