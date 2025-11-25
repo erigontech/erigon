d2019dd6cb581cdc766aa8c5d3431c42a2d3480d
kewei <kewei.train@gmail.com>
2025-11-25 19:24:03 +0800

    update

--- cl/sentinel/sentinel.go
+++ cl/sentinel/sentinel.go
@@ -228,7 +228,6 @@ func (s *Sentinel) GetPeersInfos() *sentinelproto.PeersInfoResponse {
 
 	out := &sentinelproto.PeersInfoResponse{Peers: make([]*sentinelproto.Peer, 0, len(peers))}
 
-	log.Debug("[caplin sentinel] getting peers infos", "peers", len(peers))
 	for _, p := range peers {
 		entry := &sentinelproto.Peer{}
 		peerInfo := s.p2p.Host().Network().Peerstore().PeerInfo(p)
@@ -265,7 +264,6 @@ func (s *Sentinel) GetPeersInfos() *sentinelproto.PeersInfoResponse {
 		}
 		out.Peers = append(out.Peers, entry)
 	}
-	log.Debug("[caplin sentinel] peers infos", "peers", len(out.Peers))
 	return out
 }
 
