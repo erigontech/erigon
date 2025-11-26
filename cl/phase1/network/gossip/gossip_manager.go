aad00c451cfb18d13e934fec7a6273f3ae2e25f4
kewei <kewei.train@gmail.com>
2025-11-26 15:25:25 +0800

    update

--- cl/phase1/network/gossip/gossip_manager.go
+++ cl/phase1/network/gossip/gossip_manager.go
@@ -32,6 +32,7 @@ import (
 	"github.com/erigontech/erigon/cl/gossip"
 	"github.com/erigontech/erigon/cl/monitor"
 	"github.com/erigontech/erigon/cl/p2p"
+	serviceintf "github.com/erigontech/erigon/cl/phase1/network/services/service_interface"
 	"github.com/erigontech/erigon/cl/utils"
 	"github.com/erigontech/erigon/cl/utils/eth_clock"
 	"github.com/erigontech/erigon/common"
@@ -55,9 +56,6 @@ type GossipManager struct {
 	activeIndicies uint64
 	subscriptions  *TopicSubscriptions
 	subscribeAll   bool
-	//maxInboundTrafficPerPeer     datasize.ByteSize
-	//maxOutboundTrafficPerPeer    datasize.ByteSize
-	//adaptableTrafficRequirements bool
 }
 
 func NewGossipManager(
@@ -84,168 +82,12 @@ func NewGossipManager(
 	}
 
 	go gm.observeBandwidth(context.Background(), maxInboundTrafficPerPeer, maxOutboundTrafficPerPeer, adaptableTrafficRequirements)
-	gm.goCheckResubscribe()
+	gm.goCheckForkAndResubscribe()
 	gm.stats.goPrintStats()
 	return gm
 }
 
-/*
-func (g *GossipManager) onRecv(ctx context.Context, data *sentinelproto.GossipData, l log.Ctx) (err error) {
-	defer func() {
-		r := recover()
-		if r != nil {
-			err = fmt.Errorf("%v", r)
-		}
-	}()
-	// Make a copy of the gossip data so that we the received data is not modified.
-	// 1) When we publish and corrupt the data, the peers bans us.
-	// 2) We decode the block wrong
-	data = &sentinelproto.GossipData{
-		Name:     data.Name,
-		Peer:     data.Peer,
-		SubnetId: data.SubnetId,
-		Data:     common.Copy(data.Data),
-	}
-	monitor.ObserveGossipTopicSeen(data.Name, len(data.Data))
-
-	if err := g.routeAndProcess(ctx, data); errors.Is(err, services.ErrIgnore) || errors.Is(err, synced_data.ErrNotSynced) {
-		return nil
-	} else if err != nil { //nolint:govet
-		g.sentinel.BanPeer(ctx, data.Peer)
-		return err
-	}
-	return nil
-}
-
-func (g *GossipManager) routeAndProcess(ctx context.Context, data *sentinelproto.GossipData) error {
-	currentEpoch := g.ethClock.GetCurrentEpoch()
-	version := g.beaconConfig.GetCurrentStateVersion(currentEpoch)
-	for _, s := range g.registeredServices {
-		if s.service.IsMyGossipMessage(data.Name) {
-			// check if the message satisfies the conditions
-			if !s.SatisfiesConditions(data, version) {
-				g.stats.addReject(data.Name)
-				return services.ErrIgnore
-			}
-			// decode the message
-			msg, err := s.service.DecodeGossipMessage(data, version)
-			if err != nil {
-				log.Debug("Failed to decode gossip message", "name", data.Name, "error", err)
-				g.stats.addReject(data.Name)
-				g.sentinel.BanPeer(ctx, data.Peer)
-				return err
-			}
-			// process the message
-			g.stats.addAccept(data.Name)
-			return s.service.ProcessMessage(ctx, data.SubnetId, msg)
-		}
-	}
-	return fmt.Errorf("unknown message topic: %s", data.Name)
-}
-
-func (g *GossipManager) isReadyToProcessOperations() bool {
-	return g.forkChoice.HighestSeen()+8 >= g.ethClock.GetCurrentSlot()
-}
-
-func (g *GossipManager) Start(ctx context.Context) {
-	attestationCh := make(chan *sentinelproto.GossipData, 1<<20) // large quantity of attestation messages from gossip
-	operationsCh := make(chan *sentinelproto.GossipData, 1<<16)
-	blobsCh := make(chan *sentinelproto.GossipData, 1<<16)
-	blocksCh := make(chan *sentinelproto.GossipData, 1<<10)
-	syncCommitteesCh := make(chan *sentinelproto.GossipData, 1<<16)
-	dataColumnSidecarCh := make(chan *sentinelproto.GossipData, 1<<16)
-	defer close(operationsCh)
-	defer close(blobsCh)
-	defer close(blocksCh)
-	defer close(syncCommitteesCh)
-	defer close(attestationCh)
-	defer close(dataColumnSidecarCh)
-
-	// Start couple of goroutines that listen for new gossip messages and sends them to the operations processor.
-	goWorker := func(ch <-chan *sentinelproto.GossipData, workerCount int) {
-		worker := func() {
-			for {
-				select {
-				case <-ctx.Done():
-					return
-				case data := <-ch:
-					l := log.Ctx{}
-					if err := g.onRecv(ctx, data, l); err != nil && !errors.Is(err, services.ErrIgnore) && !errors.Is(err, synced_data.ErrNotSynced) {
-						log.Debug("[Beacon Gossip] Recoverable Error", "err", err)
-					}
-				}
-			}
-		}
-		for i := 0; i < workerCount; i++ {
-			go worker()
-		}
-	}
-	goWorker(attestationCh, int(g.networkConfig.AttestationSubnetCount))
-	goWorker(syncCommitteesCh, 4)
-	goWorker(operationsCh, 1)
-	goWorker(blocksCh, 1)
-	goWorker(blobsCh, 6)
-	goWorker(dataColumnSidecarCh, 6)
-
-	sendOrDrop := func(ch chan<- *sentinelproto.GossipData, data *sentinelproto.GossipData) {
-		// Skip processing the received data if the node is not ready to process operations.
-		if !g.isReadyToProcessOperations() &&
-			data.Name != gossip.TopicNameBeaconBlock &&
-			!gossip.IsTopicBlobSidecar(data.Name) &&
-			!gossip.IsTopicDataColumnSidecar(data.Name) {
-			return
-		}
-		select {
-		case ch <- data:
-		default:
-			log.Trace("[Beacon Gossip] Dropping message due to full channel", "topic", data.Name)
-		}
-	}
-
-Reconnect:
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		default:
-		}
-
-		subscription, err := g.sentinel.SubscribeGossip(ctx, &sentinelproto.SubscriptionData{}, grpc.WaitForReady(true))
-		if err != nil {
-			return
-		}
-
-		for {
-			data, err := subscription.Recv()
-			if err != nil {
-				if grpcutil.IsRetryLater(err) || grpcutil.IsEndOfStream(err) {
-					time.Sleep(3 * time.Second)
-					continue Reconnect
-				}
-				log.Warn("[Beacon Gossip] Fatal error receiving gossip", "err", err)
-				continue Reconnect
-			}
-
-			switch {
-			case data.Name == gossip.TopicNameBeaconBlock:
-				sendOrDrop(blocksCh, data)
-			case gossip.IsTopicBlobSidecar(data.Name):
-				sendOrDrop(blobsCh, data)
-			case gossip.IsTopicDataColumnSidecar(data.Name):
-				sendOrDrop(dataColumnSidecarCh, data)
-			case gossip.IsTopicSyncCommittee(data.Name) || data.Name == gossip.TopicNameSyncCommitteeContributionAndProof:
-				sendOrDrop(syncCommitteesCh, data)
-			case gossip.IsTopicBeaconAttestation(data.Name):
-				sendOrDrop(attestationCh, data)
-			default:
-				sendOrDrop(operationsCh, data)
-			}
-		}
-	}
-}
-*/
-
-func (g *GossipManager) newValidator(service GossipService) pubsub.ValidatorEx {
+func (g *GossipManager) newPubsubValidator(service serviceintf.Service[any], conditions ...ConditionFunc) pubsub.ValidatorEx {
 	return func(ctx context.Context, pid peer.ID, msg *pubsub.Message) pubsub.ValidationResult {
 		curVersion := g.beaconConfig.GetCurrentStateVersion(g.ethClock.GetCurrentEpoch())
 		// parse the topic and subnet
@@ -259,10 +101,11 @@ func (g *GossipManager) newValidator(service GossipService) pubsub.ValidatorEx {
 		}
 
 		// check if the message satisfies the extra conditions
-		passes := service.SatisfiesConditions(pid, msg, curVersion)
-		if !passes {
-			g.stats.addIgnore(name)
-			return pubsub.ValidationIgnore
+		for _, condition := range conditions {
+			if !condition(pid, msg, curVersion) {
+				g.stats.addIgnore(name)
+				return pubsub.ValidationIgnore
+			}
 		}
 
 		// decode the message
@@ -279,7 +122,7 @@ func (g *GossipManager) newValidator(service GossipService) pubsub.ValidatorEx {
 			return pubsub.ValidationReject
 		}
 		version := g.beaconConfig.GetCurrentStateVersion(g.ethClock.GetCurrentEpoch())
-		msgObj, err := service.Service.DecodeGossipMessage(pid, msgData, version)
+		msgObj, err := service.DecodeGossipMessage(pid, msgData, version)
 		if err != nil {
 			log.Debug("[GossipManager] reject decode message", "topic", name, "err", err)
 			g.stats.addReject(name)
@@ -298,7 +141,7 @@ func (g *GossipManager) newValidator(service GossipService) pubsub.ValidatorEx {
 			subnetIdVal := uint64(subnet)
 			subnetId = &subnetIdVal
 		}
-		err = service.Service.ProcessMessage(ctx, subnetId, msgObj)
+		err = service.ProcessMessage(ctx, subnetId, msgObj)
 		if errors.Is(err, synced_data.ErrNotSynced) || (err != nil && strings.Contains(err.Error(), "ignore")) {
 			// services.ErrIgnore is a big package. To avoid circular dependency, we use a simple string check.
 			log.Trace("[GossipManager] ignore message", "topic", name, "err", err)
@@ -317,14 +160,14 @@ func (g *GossipManager) newValidator(service GossipService) pubsub.ValidatorEx {
 	}
 }
 
-func (g *GossipManager) registerGossipService(service GossipService) error {
-	validator := g.newValidator(service)
+func (g *GossipManager) registerGossipService(service serviceintf.Service[any], conditions ...ConditionFunc) error {
+	validator := g.newPubsubValidator(service)
 	forkDigest, err := g.ethClock.CurrentForkDigest()
 	if err != nil {
 		return err
 	}
 	// register all topics and subscribe
-	for _, name := range service.Service.Names() {
+	for _, name := range service.Names() {
 		topic := fmt.Sprintf("/eth2/%x/%s/%s", forkDigest, name, gossip.SSZSnappyCodec)
 		if err := g.p2p.Pubsub().RegisterTopicValidator(topic, validator); err != nil {
 			return err
@@ -393,7 +236,7 @@ func (g *GossipManager) Publish(ctx context.Context, name string, data []byte) e
 	return topicHandle.topic.Publish(ctx, compressedData, pubsub.WithReadiness(pubsub.MinTopicSize(1)))
 }
 
-func (g *GossipManager) goCheckResubscribe() {
+func (g *GossipManager) goCheckForkAndResubscribe() {
 	// check upcoming fork digest every slot
 	ticker := time.NewTicker(time.Duration(g.beaconConfig.SecondsPerSlot))
 	defer ticker.Stop()
@@ -420,6 +263,7 @@ func (g *GossipManager) goCheckResubscribe() {
 					allTopics := g.subscriptions.AllTopics()
 					for _, topic := range allTopics {
 						if strings.Contains(topic, oldForkDigest) {
+							g.subscriptions.Unsubscribe(topic)
 							if err := g.subscriptions.Remove(topic); err != nil {
 								log.Warn("[GossipManager] failed to remove old topic", "topic", topic, "err", err)
 							}
