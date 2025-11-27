fea3322e3da6471eea631f8360a56be2d328a799
lupin012 <58134934+lupin012@users.noreply.github.com.>
2025-11-27 11:22:37 +0100

    fix lint

--- rpc/jsonrpc/eth_call.go
+++ rpc/jsonrpc/eth_call.go
@@ -333,7 +333,7 @@ func (api *APIImpl) EstimateGas(ctx context.Context, argsOrNil *ethapi2.CallArgs
 		if float64(hi-lo)/float64(hi) < estimateGasErrorRatio {
 			break
 		}
-		mid := (lo+hi)/2
+		mid := (hi + lo) / 2
 		if mid > lo*2 {
 			// Most txs don't need much higher gas limit than their gas used, and most txs don't
 			// require near the full block limit of gas, so the selection of where to bisect the
