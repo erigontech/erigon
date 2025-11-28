7348add588b4f9453b224f415f03e9a9c83795c0
sudeepdino008 <sudeepdino008@gmail.com>
2025-11-28 07:41:25 +0530

    HistoryRange: prefer files data over DB - because DB can be pruned (#18088)
    i execute `HistoryRange(14, 31)`:
    this should give for key=0xe (14), value = ""; this is because key=14 is
    populated at txNum=14,28,42,.... and will have values=nil,1,2,3... (this
    is how `filledHistory` does it)
    
    then collate files till [0-16) i.e. 1 step...
    
    with `collateAndMergeHistory(prune=false)`:
    - this works fine. Both db and files have entry (key=14,
    txnum=14,value=nil), so kv=(14,nil)
    
    with `collateAndMergeHistory(prune=true)`:
    - db has entry (key=14,txnum=28,value=1) and files has
    entry(key=14,txnum=14,value=nil).
    - history range combines with preference to db and compares only
    keys...which means kv=(14,1) is chosen, rather than (14,nil).
    
    
    ---
    solution: prefer files data over db when creating the `stream.UnionKV` -
    it'll protect us from pruned history db values. Also provides "don't
    read from db if there in files" property.

--- db/state/history_test.go
+++ db/state/history_test.go
@@ -376,6 +376,57 @@ func TestHistoryAfterPrune(t *testing.T) {
 	})
 }
 
+func TestHistoryRangeWithPrune(t *testing.T) {
+	logger := log.New()
+	logEvery := time.NewTicker(30 * time.Second)
+	defer logEvery.Stop()
+	ctx := context.Background()
+
+	db, h, _ := filledHistory(t, true, logger)
+	collateAndMergeHistory(t, db, h, 32, true)
+
+	roTx, err := db.BeginRo(ctx)
+	require.NoError(t, err)
+	defer roTx.Rollback()
+
+	hc := h.BeginFilesRo()
+	defer hc.Close()
+
+	var keys, vals []string
+	it, err := hc.HistoryRange(14, 31, order.Asc, -1, roTx)
+	require.NoError(t, err)
+
+	for it.HasNext() {
+		k, v, err := it.Next()
+		require.NoError(t, err)
+		keys = append(keys, fmt.Sprintf("%x", k))
+		vals = append(vals, fmt.Sprintf("%x", v))
+	}
+
+	db2, h2, _ := filledHistory(t, true, logger)
+	collateAndMergeHistory(t, db2, h2, 32, false)
+
+	roTx2, err := db2.BeginRo(ctx)
+	require.NoError(t, err)
+	defer roTx2.Rollback()
+	hc2 := h2.BeginFilesRo()
+	defer hc2.Close()
+
+	var keys2, vals2 []string
+	it2, err := hc2.HistoryRange(14, 31, order.Asc, -1, roTx2)
+	require.NoError(t, err)
+
+	for it2.HasNext() {
+		k, v, err := it2.Next()
+		require.NoError(t, err)
+		keys2 = append(keys2, fmt.Sprintf("%x", k))
+		vals2 = append(vals2, fmt.Sprintf("%x", v))
+	}
+
+	require.Equal(t, keys, keys2)
+	require.Equal(t, vals, vals2)
+}
+
 func TestHistoryCanPrune(t *testing.T) {
 	t.Parallel()
 
