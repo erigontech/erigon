c42b366cb2003169309e7940174f3845af488a5e
Alex Sharov <AskAlexSharov@gmail.com>
2025-12-01 06:39:35 +0700

    Revert "Remove duplicates from AccHistory and StorageHistory" (#18109)
    Reverts erigontech/erigon#17962
    
    <@525860938360815626> : Mike prepared a DB with commitments enabled for
    historical eth_getProof() verifications.
    Upon executing the historical tests, we observed that the state root
    calculation failed for few tests with postState and so verification of
    receiptsRoot(the calculated receipt root doesn't match
    block.receiptRoot).
    Previously, even when running the Hive tests (with commitment enabled),
    the state root calculation was correct.
    By bisecting the commits, we identified that the failure was introduced
    after the merge of PR #17962.
    PR #18108 enables historical tests with Erigon commitments and inserts
    two calls to TouchKey() in DomainPut(). This addition resolves the
    failed state root calculation and the verification of the receipts root.
    Since I am not an expert in this specific code component, could you
    please verify if this is the correct fix?

--- db/state/execctx/domain_shared.go
+++ db/state/execctx/domain_shared.go
@@ -357,6 +357,7 @@ func (sd *SharedDomains) DomainPut(domain kv.Domain, roTx kv.TemporalTx, k, v []
 		return fmt.Errorf("DomainPut: %s, trying to put nil value. not allowed", domain)
 	}
 	ks := string(k)
+	sd.sdCtx.TouchKey(domain, ks, v)
 
 	if prevVal == nil {
 		var err error
@@ -366,19 +367,17 @@ func (sd *SharedDomains) DomainPut(domain kv.Domain, roTx kv.TemporalTx, k, v []
 		}
 	}
 	switch domain {
-	case kv.CodeDomain, kv.AccountsDomain, kv.StorageDomain:
+	case kv.CodeDomain:
 		if bytes.Equal(prevVal, v) {
 			return nil
 		}
-	case kv.RCacheDomain, kv.CommitmentDomain:
+	case kv.StorageDomain, kv.AccountsDomain, kv.CommitmentDomain, kv.RCacheDomain:
 		//noop
 	default:
 		if bytes.Equal(prevVal, v) {
 			return nil
 		}
 	}
-	sd.sdCtx.TouchKey(domain, ks, v)
-
 	return sd.mem.DomainPut(domain, ks, v, txNum, prevVal, prevStep)
 }
 
