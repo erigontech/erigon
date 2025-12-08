38b1cf3a231f9f040626233f80633bbd78b90fa1
lupin012 <58134934+lupin012@users.noreply.github.com.>
2025-12-08 22:06:40 +0100

    update test with tag

--- .github/workflows/scripts/run_rpc_tests_ethereum.sh
+++ .github/workflows/scripts/run_rpc_tests_ethereum.sh
@@ -41,4 +41,4 @@ DISABLED_TEST_LIST=(
 DISABLED_TESTS=$(IFS=,; echo "${DISABLED_TEST_LIST[*]}")
 
 # Call the main test runner script with the required and optional parameters
-"$(dirname "$0")/run_rpc_tests.sh" mainnet lupin012/add_test_with_prestate_callTracer "$DISABLED_TESTS" "$WORKSPACE" "$RESULT_DIR"
+"$(dirname "$0")/run_rpc_tests.sh" mainnet v1.109.0 "$DISABLED_TESTS" "$WORKSPACE" "$RESULT_DIR"
