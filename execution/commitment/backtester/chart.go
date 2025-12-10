cd697eb6122d0b18878836384b4bcfa74e5b008c
taratorio <94537774+taratorio@users.noreply.github.com>
2025-12-11 02:10:36 +1100

    add top10 and gini

--- execution/commitment/backtester/chart.go
+++ execution/commitment/backtester/chart.go
@@ -18,13 +18,18 @@ package backtester
 
 import (
 	"bufio"
+	"cmp"
+	"encoding/hex"
 	"os"
 	"path"
+	"slices"
+	"strconv"
 
 	"github.com/go-echarts/go-echarts/v2/charts"
 	"github.com/go-echarts/go-echarts/v2/components"
 	"github.com/go-echarts/go-echarts/v2/opts"
 
+	"github.com/erigontech/erigon/common/length"
 	"github.com/erigontech/erigon/common/log/v3"
 	"github.com/erigontech/erigon/execution/commitment"
 )
@@ -62,20 +67,95 @@ func renderChartsPage(mv []MetricValues, outputDir string) (string, error) {
 func generateChartsPage(mv []MetricValues) *components.Page {
 	page := components.NewPage()
 	page.SetPageTitle("commitment backtest charts")
-	sharedGlobalOpts := []charts.GlobalOpts{
-		charts.WithInitializationOpts(opts.Initialization{Width: "100%"}),
-		charts.WithLegendOpts(opts.Legend{Bottom: "5%", Left: "center"}),
-		charts.WithGridOpts(opts.Grid{Bottom: "15%"}),
-	}
+	page.SetLayout(components.PageFlexLayout)
+	top10Slowest := generateTop10SlowestCharts(mv)
+	// place 3 in 1 row
+	top10Slowest.times.SetGlobalOptions(widthOpts("30vw"))
+	top10Slowest.updates.SetGlobalOptions(widthOpts("30vw"))
+	top10Slowest.suGinis.SetGlobalOptions(widthOpts("30vw"))
 	page.AddCharts(
-		generateProcessingTimesChart(mv, sharedGlobalOpts...),
-		generateTrieUpdatesChart(mv, sharedGlobalOpts...),
-		generateDbRwChart(mv, sharedGlobalOpts...),
+		top10Slowest.times,
+		top10Slowest.updates,
+		top10Slowest.suGinis,
+	)
+	// place 1 per row
+	processingTimes := generateProcessingTimesChart(mv)
+	processingTimes.SetGlobalOptions(widthOpts("100vw"))
+	trieUpdates := generateTrieUpdatesChart(mv)
+	trieUpdates.SetGlobalOptions(widthOpts("100vw"))
+	dbRw := generateDbRwChart(mv)
+	dbRw.SetGlobalOptions(widthOpts("100vw"))
+	page.AddCharts(
+		processingTimes,
+		trieUpdates,
+		dbRw,
 	)
 	return page
 }
 
-func generateProcessingTimesChart(mv []MetricValues, globalOpts ...charts.GlobalOpts) *charts.Line {
+type top10SlowestCharts struct {
+	times   *charts.Line
+	updates *charts.Bar
+	suGinis *charts.Line
+}
+
+func generateTop10SlowestCharts(mv []MetricValues) top10SlowestCharts {
+	mv = slices.SortedStableFunc(slices.Values(mv), func(v1 MetricValues, v2 MetricValues) int {
+		return -cmp.Compare(v1.SpentProcessing.Milliseconds(), v2.SpentProcessing.Milliseconds())
+	})
+	if len(mv) > 10 {
+		mv = mv[:10]
+	}
+	nums := make([]int, len(mv))
+	times := make([]opts.LineData, len(mv))
+	accs := make([]opts.BarData, len(mv))
+	storage := make([]opts.BarData, len(mv))
+	maxSuPerAcc := make([]opts.BarData, len(mv))
+	suGinis := make([]opts.LineData, len(mv))
+	for i := range mv {
+		name := strconv.FormatUint(mv[i].BatchId, 10)
+		nums[i] = i + 1
+		times[i] = opts.LineData{Name: name, Value: mv[i].SpentProcessing.Milliseconds()}
+		accs[i] = opts.BarData{Name: name, Value: mv[i].AddressKeys}
+		storage[i] = opts.BarData{Name: name, Value: mv[i].StorageKeys}
+		var accMaxSu uint64
+		accSus := make([]uint64, 0, len(mv[i].Accounts))
+		for k, v := range mv[i].Accounts {
+			if len(k) == hex.EncodedLen(length.Addr) && v.LoadBranch == 0 {
+				accMaxSu = max(accMaxSu, v.StorageUpates)
+				accSus = append(accSus, v.StorageUpates)
+			}
+		}
+		maxSuPerAcc[i] = opts.BarData{Name: name, Value: accMaxSu}
+		suGinis[i] = opts.LineData{Name: name, Value: giniCoefficient(accSus)}
+	}
+	timesChart := charts.NewLine()
+	timesChart.SetGlobalOptions(
+		subTitleOpts("top 10 slowest", "processing times"),
+		noLegendOpts(),
+	)
+	timesChart.SetXAxis(nums).
+		AddSeries("times", times)
+	updatesChart := charts.NewBar()
+	updatesChart.SetGlobalOptions(
+		subTitleOpts("top 10 slowest", "trie updates"),
+		legendOpts(),
+	)
+	updatesChart.SetXAxis(nums).
+		AddSeries("accs", accs).
+		AddSeries("storage", storage).
+		AddSeries("maxSuPerAcc", maxSuPerAcc)
+	suGinisChart := charts.NewLine()
+	suGinisChart.SetGlobalOptions(
+		subTitleOpts("top 10 slowest", "storage updates gini coefficient"),
+		noLegendOpts(),
+	)
+	suGinisChart.SetXAxis(nums).
+		AddSeries("gini", suGinis)
+	return top10SlowestCharts{times: timesChart, updates: updatesChart, suGinis: suGinisChart}
+}
+
+func generateProcessingTimesChart(mv []MetricValues) *charts.Line {
 	batchIds := make([]uint64, len(mv))
 	totalProcessingTime := make([]opts.LineData, len(mv))
 	unfoldingProcessingTime := make([]opts.LineData, len(mv))
@@ -87,8 +167,11 @@ func generateProcessingTimesChart(mv []MetricValues, globalOpts ...charts.Global
 		foldingProcessingTime[i] = opts.LineData{Value: mv[i].SpentFolding.Milliseconds()}
 	}
 	chart := charts.NewLine()
-	chart.SetGlobalOptions(charts.WithTitleOpts(opts.Title{Title: "processing times", Left: "center", Top: "5%"}))
-	chart.SetGlobalOptions(globalOpts...)
+	chart.SetGlobalOptions(
+		titleOpts("processing times"),
+		legendOpts(),
+		gridOpts(),
+	)
 	stackOpts := []charts.SeriesOpts{
 		charts.WithLineChartOpts(opts.LineChart{Stack: "total"}),
 		charts.WithAreaStyleOpts(opts.AreaStyle{Opacity: opts.Float(0.2)}),
@@ -100,7 +183,7 @@ func generateProcessingTimesChart(mv []MetricValues, globalOpts ...charts.Global
 	return chart
 }
 
-func generateTrieUpdatesChart(mv []MetricValues, globalOpts ...charts.GlobalOpts) *charts.Line {
+func generateTrieUpdatesChart(mv []MetricValues) *charts.Line {
 	batchIds := make([]uint64, len(mv))
 	accountUpdates := make([]opts.LineData, len(mv))
 	storageUpdates := make([]opts.LineData, len(mv))
@@ -110,8 +193,11 @@ func generateTrieUpdatesChart(mv []MetricValues, globalOpts ...charts.GlobalOpts
 		storageUpdates[i] = opts.LineData{Value: mv[i].StorageKeys}
 	}
 	chart := charts.NewLine()
-	chart.SetGlobalOptions(charts.WithTitleOpts(opts.Title{Title: "trie updates", Left: "center", Top: "5%"}))
-	chart.SetGlobalOptions(globalOpts...)
+	chart.SetGlobalOptions(
+		titleOpts("trie updates"),
+		legendOpts(),
+		gridOpts(),
+	)
 	chart.SetXAxis(batchIds).
 		AddSeries("account", accountUpdates).
 		AddSeries("storage", storageUpdates).
@@ -122,7 +208,7 @@ func generateTrieUpdatesChart(mv []MetricValues, globalOpts ...charts.GlobalOpts
 	return chart
 }
 
-func generateDbRwChart(mv []MetricValues, globalOpts ...charts.GlobalOpts) *charts.Line {
+func generateDbRwChart(mv []MetricValues) *charts.Line {
 	batchIds := make([]uint64, len(mv))
 	accountReads := make([]opts.LineData, len(mv))
 	storageReads := make([]opts.LineData, len(mv))
@@ -136,8 +222,11 @@ func generateDbRwChart(mv []MetricValues, globalOpts ...charts.GlobalOpts) *char
 		branchWrites[i] = opts.LineData{Value: mv[i].UpdateBranch}
 	}
 	chart := charts.NewLine()
-	chart.SetGlobalOptions(charts.WithTitleOpts(opts.Title{Title: "db reads/writes", Left: "center", Top: "5%"}))
-	chart.SetGlobalOptions(globalOpts...)
+	chart.SetGlobalOptions(
+		titleOpts("db reads/writes"),
+		legendOpts(),
+		gridOpts(),
+	)
 	chart.SetXAxis(batchIds).
 		AddSeries("account_r", accountReads).
 		AddSeries("storage_r", storageReads).
@@ -148,4 +237,47 @@ func generateDbRwChart(mv []MetricValues, globalOpts ...charts.GlobalOpts) *char
 			charts.WithAreaStyleOpts(opts.AreaStyle{Opacity: opts.Float(0.2)}),
 		)
 	return chart
+}
+
+func titleOpts(title string) charts.GlobalOpts {
+	return charts.WithTitleOpts(opts.Title{Title: title, Left: "center", Top: "5%"})
+}
+
+func subTitleOpts(title string, subtitle string) charts.GlobalOpts {
+	return charts.WithTitleOpts(opts.Title{Title: title, Subtitle: subtitle, Left: "center", Top: "2%"})
+}
+
+func widthOpts(width string) charts.GlobalOpts {
+	return charts.WithInitializationOpts(opts.Initialization{Width: width})
+}
+
+func legendOpts() charts.GlobalOpts {
+	return charts.WithLegendOpts(opts.Legend{Bottom: "5%", Left: "center"})
+}
+
+func noLegendOpts() charts.GlobalOpts {
+	return charts.WithLegendOpts(opts.Legend{Show: opts.Bool(false)})
+}
+
+func gridOpts() charts.GlobalOpts {
+	return charts.WithGridOpts(opts.Grid{Bottom: "15%"})
+}
+
+func giniCoefficient(values []uint64) float64 {
+	n := len(values)
+	if n == 0 {
+		return 0
+	}
+	sorted := make([]uint64, n)
+	copy(sorted, values)
+	slices.Sort(sorted)
+	var sum, weightedSum float64
+	for i, v := range sorted {
+		sum += float64(v)
+		weightedSum += (float64(i) + 1) * float64(v)
+	}
+	if sum == 0 {
+		return 0
+	}
+	return (2*weightedSum)/(float64(n)*sum) - (float64(n)+1)/float64(n)
 }
