f7a783b1406cdc6684a0a1496521e41b3a974474
RealMaxing <realmaxxinn@gmail.com>
2025-12-14 05:27:53 +0200

    refactor: use strings.Builder to improve string concatenation perform… (#18294)
    Replaces inefficient string concatenation operations with
    strings.Builder
    for better performance and reduced memory allocations.
    
    refer to #18189
    
    Tried my best to find all the same issue.... let me know if there are
    more.

--- rpc/jsonrpc/trace_types.go
+++ rpc/jsonrpc/trace_types.go
@@ -18,6 +18,7 @@ package jsonrpc
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/erigontech/erigon/common"
 	"github.com/erigontech/erigon/common/hexutil"
@@ -129,16 +130,16 @@ type TraceResult struct {
 
 // Allows for easy printing of a geth trace for debugging
 func (p GethTrace) String() string {
-	var ret string
-	ret += fmt.Sprintf("Type: %s\n", p.Type)
-	ret += fmt.Sprintf("From: %s\n", p.From)
-	ret += fmt.Sprintf("To: %s\n", p.To)
-	ret += fmt.Sprintf("Value: %s\n", p.Value)
-	ret += fmt.Sprintf("Gas: %s\n", p.Gas)
-	ret += fmt.Sprintf("GasUsed: %s\n", p.GasUsed)
-	ret += fmt.Sprintf("Input: %s\n", p.Input)
-	ret += fmt.Sprintf("Output: %s\n", p.Output)
-	return ret
+	var ret strings.Builder
+	ret.WriteString(fmt.Sprintf("Type: %s\n", p.Type))
+	ret.WriteString(fmt.Sprintf("From: %s\n", p.From))
+	ret.WriteString(fmt.Sprintf("To: %s\n", p.To))
+	ret.WriteString(fmt.Sprintf("Value: %s\n", p.Value))
+	ret.WriteString(fmt.Sprintf("Gas: %s\n", p.Gas))
+	ret.WriteString(fmt.Sprintf("GasUsed: %s\n", p.GasUsed))
+	ret.WriteString(fmt.Sprintf("Input: %s\n", p.Input))
+	ret.WriteString(fmt.Sprintf("Output: %s\n", p.Output))
+	return ret.String()
 }
 
 // Allows for easy printing of a parity trace for debugging
