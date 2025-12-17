7b3d29967cc6fdeda3fb67dcdb058c50e5acd303
VolodymyrBg <aqdrgg19@gmail.com>
2025-12-17 14:38:09 +0200

    rpc/gasprice: remove unused Oracle state fields

--- rpc/gasprice/gasprice.go
+++ rpc/gasprice/gasprice.go
@@ -56,8 +56,6 @@ type Cache interface {
 // blocks. Suitable for both light and full clients.
 type Oracle struct {
 	backend     OracleBackend
-	lastHead    common.Hash
-	lastPrice   *big.Int
 	maxPrice    *big.Int
 	ignorePrice *big.Int
 	cache       Cache
@@ -101,7 +99,6 @@ func NewOracle(backend OracleBackend, params gaspricecfg.Config, cache Cache, lo
 
 	return &Oracle{
 		backend:          backend,
-		lastPrice:        params.Default,
 		maxPrice:         maxPrice,
 		ignorePrice:      ignorePrice,
 		checkBlocks:      blocks,
