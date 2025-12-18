4fce7edf759ee9baccd04bae5eb07593fce7de30
Alexey Sharov <AskAlexSharov@gmail.com>
2025-12-18 11:12:41 +0700

    save

--- db/state/inverted_index_test.go
+++ db/state/inverted_index_test.go
@@ -748,7 +748,7 @@ func TestInvIndex_OpenFolder(t *testing.T) {
 }
 
 func TestInvIndexPruningPerf(t *testing.T) {
-	//t.Skip("for manual benchmarks ")
+	t.Skip("for manual benchmarks ")
 	testDbAndInvertedIndex2 := func(tb testing.TB, aggStep uint64, logger log.Logger) (kv.RwDB, *InvertedIndex) {
 		tb.Helper()
 		dirs := datadir.New("/erigon-data/remove_me_test")
