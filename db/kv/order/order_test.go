f0adafa2e5667f8548a5baa000715ca9f87587c9
sashass1315 <sashass1315@gmail.com>
2025-12-21 15:25:04 +0200

    fix test

--- db/kv/order/order_test.go
+++ db/kv/order/order_test.go
@@ -1,20 +1,18 @@
 package order
 
 import (
+	"os"
 	"testing"
 
 	"github.com/erigontech/erigon/common/dbg"
 )
 
-func withAssertsEnabled(t *testing.T, fn func()) {
-	t.Helper()
+func TestMain(m *testing.M) {
 	prev := dbg.AssertEnabled
 	dbg.AssertEnabled = true
-	defer func() {
-		dbg.AssertEnabled = prev
-	}()
-
-	fn()
+	code := m.Run()
+	dbg.AssertEnabled = prev
+	os.Exit(code)
 }
 
 func expectPanic(t *testing.T, fn func()) {
@@ -29,85 +27,73 @@ func expectPanic(t *testing.T, fn func()) {
 }
 
 func TestAssertListAsc_LastPairChecked_NoPanicOnSorted(t *testing.T) {
-	withAssertsEnabled(t, func() {
-		asc := Asc
-		keys := [][]byte{
-			{1},
-			{2},
-			{3},
-		}
+	asc := Asc
+	keys := [][]byte{
+		{1},
+		{2},
+		{3},
+	}
 
+	asc.AssertList(keys)
+}
+
+func TestAssertListAsc_LastPairChecked_PanicsOnUnsortedLastPair(t *testing.T) {
+	asc := Asc
+	keys := [][]byte{
+		{1},
+		{3},
+		{2},
+	}
+
+	expectPanic(t, func() {
 		asc.AssertList(keys)
 	})
 }
 
-func TestAssertListAsc_LastPairChecked_PanicsOnUnsortedLastPair(t *testing.T) {
-	withAssertsEnabled(t, func() {
-		asc := Asc
-		keys := [][]byte{
-			{1},
-			{3},
-			{2},
-		}
-
-		expectPanic(t, func() {
-			asc.AssertList(keys)
-		})
-	})
-}
-
 func TestAssertListAsc_TwoElements_PanicsWhenOutOfOrder(t *testing.T) {
-	withAssertsEnabled(t, func() {
-		asc := Asc
-		keys := [][]byte{
-			{2},
-			{1},
-		}
+	asc := Asc
+	keys := [][]byte{
+		{2},
+		{1},
+	}
 
-		expectPanic(t, func() {
-			asc.AssertList(keys)
-		})
+	expectPanic(t, func() {
+		asc.AssertList(keys)
 	})
 }
 
 func TestAssertListDesc_LastPairChecked_NoPanicOnSorted(t *testing.T) {
-	withAssertsEnabled(t, func() {
-		desc := Desc
-		keys := [][]byte{
-			{3},
-			{2},
-			{1},
-		}
+	desc := Desc
+	keys := [][]byte{
+		{3},
+		{2},
+		{1},
+	}
 
+	desc.AssertList(keys)
+}
+
+func TestAssertListDesc_LastPairChecked_PanicsOnUnsortedLastPair(t *testing.T) {
+	desc := Desc
+	keys := [][]byte{
+		{3},
+		{1},
+		{2},
+	}
+
+	expectPanic(t, func() {
 		desc.AssertList(keys)
 	})
 }
 
-func TestAssertListDesc_LastPairChecked_PanicsOnUnsortedLastPair(t *testing.T) {
-	withAssertsEnabled(t, func() {
-		desc := Desc
-		keys := [][]byte{
-			{3},
-			{1},
-			{2},
-		}
-
-		expectPanic(t, func() {
-			desc.AssertList(keys)
-		})
-	})
-}
-
 func TestAssertListDesc_TwoElements_PanicsWhenOutOfOrder(t *testing.T) {
-	withAssertsEnabled(t, func() {
-		desc := Desc
-		keys := [][]byte{
-			{1},
-			{2},
-		}
+	desc := Desc
+	keys := [][]byte{
+		{1},
+		{2},
+	}
 
-		expectPanic(t, func() {
-			desc.AssertList(keys)
-		})
+	expectPanic(t, func() {
+		desc.AssertList(keys)
 	})
 }
