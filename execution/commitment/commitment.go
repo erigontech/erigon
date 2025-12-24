97bf471ae7b02f172dbc03a643fe705a41f8e94e
Giulio <monkeair@MacBook-Air-di-Giulio.local>
2025-12-24 01:53:40 +0100

    sa

--- execution/commitment/commitment.go
+++ execution/commitment/commitment.go
@@ -1277,9 +1277,9 @@ func (t *Updates) HashSort(ctx context.Context, warmuper *Warmuper, fn func(hk, 
 					}
 				}
 				// Drain warmuper before the next processing.
-				if warmuper != nil {
-					warmuper.DrainPending()
-				}
+				// if warmuper != nil {
+				// 	warmuper.DrainPending()
+				// }
 				pairs = nil // Reset batch
 			}
 			return nil
