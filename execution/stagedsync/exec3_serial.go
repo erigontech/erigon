1295b5843eb705a94c8d44e5120ae7920a87b128
sudeepdino008 <sudeepdino008@gmail.com>
2025-12-26 15:28:28 +0530

    save

--- execution/stagedsync/exec3_serial.go
+++ execution/stagedsync/exec3_serial.go
@@ -69,7 +69,7 @@ func (se *serialExecutor) exec(ctx context.Context, execStage *StageState, u Unw
 
 	limit := min(blockNum+blockLimit, maxBlockNum)
 	for ; blockNum <= limit; blockNum++ {
-		log.Info("blockNum", "bn", blockNum)
+		//log.Info("blockNum", "bn", blockNum)
 		shouldGenerateChangesets := shouldGenerateChangeSets(se.cfg, blockNum, maxBlockNum, initialCycle)
 		changeSet := &changeset.StateChangeSet{}
 		if shouldGenerateChangesets && blockNum > 0 {
