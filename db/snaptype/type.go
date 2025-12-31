aaed6c11992851b4bd6e979c98bc7869368ab9f9
Giulio <monkeair@MacBook-Air-di-Giulio.local>
2025-12-31 15:18:21 +0100

    save

--- db/snaptype/type.go
+++ db/snaptype/type.go
@@ -167,6 +167,11 @@ func (i Index) HasFile(info FileInfo, logger log.Logger) bool {
 
 	// defer segment.Close()
 
+	// Let's actually
+	if _, err := os.Stat(info.Path); err != nil {
+		return false
+	}
+
 	fNameMask := IdxFileMask(info.From, info.To, i.Name)
 	fPath, fileVer, ok, err := version.FindFilesWithVersionsByPattern(filepath.Join(dir, fNameMask))
 	if err != nil {
