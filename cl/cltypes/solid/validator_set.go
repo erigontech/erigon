12d2aed998e507af8326686dde1abf80a072399e
Giulio <monkeair@MacBook-Air-di-Giulio.local>
2025-12-31 19:54:00 +0100

    save

--- cl/cltypes/solid/validator_set.go
+++ cl/cltypes/solid/validator_set.go
@@ -169,7 +169,7 @@ func (v *ValidatorSet) CopyTo(t *ValidatorSet) {
 		t.MerkleTree = nil
 	}
 	// skip copying (unsupported for phase0)
-	t.phase0Data = make([]Phase0Data, v.l)
+	// t.phase0Data = make([]Phase0Data, v.l)
 	copy(t.buffer, v.buffer)
 	copy(t.attesterBits, v.attesterBits)
 	t.buffer = t.buffer[:v.l*validatorSize]
