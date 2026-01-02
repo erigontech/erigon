2e72454d07d8d7648c763b065c7fd8c355259aab
Giulio <monkeair@MacBook-Air-di-Giulio.local>
2026-01-02 04:06:41 +0100

    save

--- db/etl/buffers.go
+++ db/etl/buffers.go
@@ -47,7 +47,7 @@ const (
 var BufferOptimalSize = dbg.EnvDataSize("ETL_OPTIMAL", 256*datasize.MB) /*  var because we want to sometimes change it from tests or command-line flags */
 
 // 3_domains * 2 + 3_history * 1 + 4_indices * 2 = 17 etl collectors, 17*(256Mb/8) = 512Mb - for all collectros
-var etlSmallBufRAM = dbg.EnvDataSize("ETL_SMALL", BufferOptimalSize/4)
+var etlSmallBufRAM = dbg.EnvDataSize("ETL_SMALL", 8)
 var SmallSortableBuffers = NewAllocator(&sync.Pool{
 	New: func() any {
 		return NewSortableBuffer(etlSmallBufRAM).Prealloc(1_024, int(etlSmallBufRAM/32))
