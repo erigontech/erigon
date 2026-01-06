359b8f2ff855ebfe0c33ee007e1cd6f2dc849d5e
yperbasis <andrey.ashikhmin@gmail.com>
2026-01-06 12:22:00 +0100

    Revert "try with sdc.Reset()"
    This reverts commit 01e5745b9de7ad22c9304833b557170061f66b01.

--- execution/commitment/commitmentdb/commitment_context.go
+++ execution/commitment/commitmentdb/commitment_context.go
@@ -303,7 +303,6 @@ func (sdc *SharedDomainsCommitmentContext) ComputeCommitment(ctx context.Context
 	}
 
 	trieContext := sdc.trieContext(tx)
-	sdc.Reset() // REMOVE ME
 
 	var warmupConfig commitment.WarmupConfig
 	if sdc.warmupDB != nil {
