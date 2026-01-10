2a65bee699618c2d7c3ae8644a1296881bfd8ec2
Alexey Sharov <AskAlexSharov@gmail.com>
2026-01-10 20:18:16 +0700

    save

--- execution/commitment/commitmentdb/commitment_context.go
+++ execution/commitment/commitmentdb/commitment_context.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"context"
 	"encoding/binary"
-	"encoding/hex"
 	"errors"
 	"fmt"
 	"sync/atomic"
@@ -519,7 +518,6 @@ func (sdc *SharedDomainsCommitmentContext) encodeAndStoreCommitmentState(trieCon
 		return nil
 	}
 
-	log.Debug("[commitment] store state", "block", blockNum, "txNum", txNum, "rootHash", hex.EncodeToString(rootHash))
 	return trieContext.PutBranch(KeyCommitmentState, encodedState, prevState, prevStep)
 }
 
