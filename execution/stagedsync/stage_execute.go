f238cf5f1330a1134a5aa6f43c859df43b522234
Alexey Sharov <AskAlexSharov@gmail.com>
2026-01-11 21:04:01 +0700

    save

--- execution/stagedsync/stage_execute.go
+++ execution/stagedsync/stage_execute.go
@@ -516,18 +516,18 @@ func PruneExecutionStage(ctx context.Context, s *PruneState, tx kv.RwTx, cfg Exe
 		//pruneTimeout = 12 * time.Hour
 
 		// allow greedy prune on non-chain-tip
-		greedyPruneCommitmentHistoryStartTime := time.Now()
-		if err = tx.(kv.TemporalRwTx).GreedyPruneHistory(ctx, kv.CommitmentDomain); err != nil {
-			return err
-		}
-		if duration := time.Since(greedyPruneCommitmentHistoryStartTime); duration > quickPruneTimeout {
-			logger.Debug(
-				fmt.Sprintf("[%s] greedy prune commitment history timing", s.LogPrefix()),
-				"duration", duration,
-				"initialCycle", s.CurrentSyncCycle.IsInitialCycle,
-				"externalTx", useExternalTx,
-			)
-		}
+		//greedyPruneCommitmentHistoryStartTime := time.Now()
+		//if err = tx.(kv.TemporalRwTx).GreedyPruneHistory(ctx, kv.CommitmentDomain); err != nil {
+		//	return err
+		//}
+		//if duration := time.Since(greedyPruneCommitmentHistoryStartTime); duration > quickPruneTimeout {
+		//	logger.Debug(
+		//		fmt.Sprintf("[%s] greedy prune commitment history timing", s.LogPrefix()),
+		//		"duration", duration,
+		//		"initialCycle", s.CurrentSyncCycle.IsInitialCycle,
+		//		"externalTx", useExternalTx,
+		//	)
+		//}
 	}
 
 	pruneSmallBatchesStartTime := time.Now()
