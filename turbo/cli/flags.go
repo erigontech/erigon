0fb86abf5deb9565b44b22e7ff39153f3a221c5f
awskii <artem.tsskiy@gmail.com>
2026-01-12 13:04:38 +1000

    use default val for l2rpc.receipt

--- turbo/cli/flags.go
+++ turbo/cli/flags.go
@@ -251,6 +251,9 @@ func ApplyFlagsForEthConfig(ctx *cli.Context, cfg *ethconfig.Config, logger log.
 	log.Info("[Arbitrum] Using L2 RPC server to fetch blocks", "address", cfg.L2RPCAddr)
 
 	cfg.L2RPCReceiptAddr = ctx.String(L2RPCReceiptAddrFlag.Name)
+	if cfg.L2RPCReceiptAddr == "" {
+		cfg.L2RPCReceiptAddr = cfg.L2RPCAddr
+	}
 	log.Info("[Arbitrum] Using L2 RPC server to fetch receipts", "address", cfg.L2RPCReceiptAddr)
 
 	blockDistance := ctx.Uint64(PruneBlocksDistanceFlag.Name)
@@ -372,6 +375,9 @@ func ApplyFlagsForEthConfigCobra(f *pflag.FlagSet, cfg *ethconfig.Config) {
 		cfg.L2RPCReceiptAddr = flg.Value.String()
 	} else {
 		cfg.L2RPCReceiptAddr = *f.String(L2RPCReceiptAddrFlag.Name, L2RPCReceiptAddrFlag.DefaultText, "")
+	}
+	if cfg.L2RPCReceiptAddr == "" {
+		cfg.L2RPCReceiptAddr = cfg.L2RPCAddr
 	}
 	if cfg.L2RPCReceiptAddr != "" {
 		log.Info("[Arbitrum] Using L2 RPC server to fetch receipts", "address", cfg.L2RPCReceiptAddr)
