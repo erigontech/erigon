cab52df9288788ff4d5afad3a7f8140852111488
Alexey Sharov <AskAlexSharov@gmail.com>
2026-01-15 10:43:03 +0700

    save

--- db/state/temporal_mem_batch.go
+++ db/state/temporal_mem_batch.go
@@ -259,7 +259,7 @@ func (sd *TemporalMemBatch) SizeEstimate() uint64 {
 	estimateRamUsage *= 2 // cover golang-data-structures overhead (and keep accounting cheap)
 	estimateRamUsage *= 2 // cover multiple versions of domains in-mem data - see `domains` field type
 	estimateRamUsage *= 2 // because Commitment storing copy of updates
-	estimateRamUsage *= 2 // because Commitment will produce `trie` updates - when batch is full
+	//estimateRamUsage *= 2 // because Commitment will produce `trie` updates - when batch is full
 	return estimateRamUsage
 }
 
