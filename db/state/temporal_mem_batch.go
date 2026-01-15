628009270588b7aa20f55f8a4ad0b85e79277375
Alexey Sharov <AskAlexSharov@gmail.com>
2026-01-15 09:54:26 +0700

    save

--- db/state/temporal_mem_batch.go
+++ db/state/temporal_mem_batch.go
@@ -255,11 +255,12 @@ func (sd *TemporalMemBatch) SizeEstimate() uint64 {
 	sd.latestStateLock.RLock()
 	defer sd.latestStateLock.RUnlock()
 
-	// multiply 2: to cover data-structures overhead (and keep accounting cheap)
-	// multiply 2 more: to cover `multiple versions of domains in-mem data` - see `domains` field type
-	// multiply 2 more: because Commitment storing copy of updates
-	// multiply 2 more: because Commitment will produce `trie` updates - when batch is full
-	return uint64(sd.metrics.CachePutSize) * 2 * 2 * 2 * 2
+	estimateSize := uint64(sd.metrics.CachePutSize)
+	estimateSize *= 2 // to cover data-structures overhead (and keep accounting cheap)
+	estimateSize *= 2 // to cover `multiple versions of domains in-mem data` - see `domains` field type
+	estimateSize *= 2 // to cover because Commitment storing copy of updates
+	estimateSize *= 2 // because Commitment will produce `trie` updates - when batch is full
+	return estimateSize
 }
 
 func (sd *TemporalMemBatch) ClearRam() {
