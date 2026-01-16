0e46943aa2b067d18a890f0bad646bb5e5e30659
JkLondon <me@ilyamikheev.com>
2026-01-16 15:13:30 +0100

    commit

--- db/snaptype/type.go
+++ db/snaptype/type.go
@@ -294,12 +294,7 @@ func (s SnapType) FileInfo(dir string, from uint64, to uint64) FileInfo {
 }
 
 func (s SnapType) FileInfoByMask(dir string, from uint64, to uint64) FileInfo {
-	mask, err := version.ReplaceVersionWithMask(s.FileName(s.versions.Current, from, to))
-	if err != nil {
-		log.Debug("[snaptype] replace with mask error", "err", err, "fName", s.FileName(s.versions.Current, from, to))
-		return FileInfo{}
-	}
-	fName, _, ok, err := version.FindFilesWithVersionsByPattern(filepath.Join(dir, mask))
+	fName, _, ok, err := version.FindFilesWithVersionsByPattern(filepath.Join(dir, s.FileMask(from, to)))
 	if err != nil {
 		log.Debug("[snaptype] file mask error", "err", err, "fName", s.FileName(s.versions.Current, from, to))
 		return FileInfo{}
