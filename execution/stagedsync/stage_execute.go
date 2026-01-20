9a3814659e5d4f48fc82dd363c136b75c7e504c2
Alexey Sharov <AskAlexSharov@gmail.com>
2026-01-20 10:45:49 +0700

    save,

--- execution/stagedsync/stage_execute.go
+++ execution/stagedsync/stage_execute.go
@@ -468,9 +468,9 @@ func PruneExecutionStage(ctx context.Context, s *PruneState, tx kv.RwTx, cfg Exe
 	// because on slow disks - prune is slower. but for now - let's tune for nvme first, and add `tx.SpaceDirty()` check later https://github.com/erigontech/erigon/issues/11635
 	quickPruneTimeout := 1 * 1024 * time.Millisecond
 
-	if timeout > 0 && timeout > quickPruneTimeout {
-		quickPruneTimeout = timeout
-	}
+	//if timeout > 0 && timeout > quickPruneTimeout {
+	//	quickPruneTimeout = timeout
+	//}
 
 	if s.ForwardProgress > cfg.syncCfg.MaxReorgDepth && !cfg.syncCfg.AlwaysGenerateChangesets {
 		// (chunkLen is 8Kb) * (1_000 chunks) = 8mb
