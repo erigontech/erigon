74267c498a208fcd46f4e8287ff1e1a8bd6e40ad
Alexey Sharov <AskAlexSharov@gmail.com>
2026-01-22 13:27:03 +0700

    save

--- execution/stagedsync/exec3_serial.go
+++ execution/stagedsync/exec3_serial.go
@@ -221,11 +221,11 @@ func (se *serialExecutor) exec(ctx context.Context, execStage *StageState, u Unw
 
 			pruneTimeout := 250 * time.Millisecond
 			if initialCycle {
-				pruneTimeout = 10 * time.Hour
-
-				if err = rwTx.GreedyPruneHistory(ctx, kv.CommitmentDomain); err != nil {
-					return nil, rwTx, err
-				}
+				//pruneTimeout = 10 * time.Hour
+				//
+				//if err = rwTx.GreedyPruneHistory(ctx, kv.CommitmentDomain); err != nil {
+				//	return nil, rwTx, err
+				//}
 			}
 
 			if _, err := rwTx.PruneSmallBatches(ctx, pruneTimeout); err != nil {
