55c956beaef2552ea10fb3cbb97b5f811fa4a6de
Alexey Sharov <AskAlexSharov@gmail.com>
2026-01-22 13:25:50 +0700

    save

--- db/state/aggregator.go
+++ db/state/aggregator.go
@@ -111,7 +111,7 @@ func newAggregator(ctx context.Context, dirs datadir.Dirs, reorgBlockDepth uint6
 		leakDetector:           dbg.NewLeakDetector("agg", dbg.SlowTx()),
 		ps:                     background.NewProgressSet(),
 		logger:                 logger,
-		collateAndBuildWorkers: 2,
+		collateAndBuildWorkers: 3,
 		mergeWorkers:           1,
 
 		produce: true,
