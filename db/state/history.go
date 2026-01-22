ffcc276a04068894b307193ecdb99104daabaab1
Alexey Sharov <AskAlexSharov@gmail.com>
2026-01-22 11:54:04 +0700

    save

--- db/state/history.go
+++ db/state/history.go
@@ -238,6 +238,9 @@ func (h *History) buildVI(ctx context.Context, historyIdxPath string, hist, efHi
 	var histKey []byte
 	var valOffset uint64
 
+	defer hist.MadvSequential().DisableReadAhead()
+	defer efHist.MadvSequential().DisableReadAhead()
+
 	iiReader := h.InvertedIndex.dataReader(efHist)
 
 	var keyBuf, valBuf []byte
