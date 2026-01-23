99c5ced0e18e59b509627ff794c038ec42f4d14c
Alexey Sharov <AskAlexSharov@gmail.com>
2026-01-23 11:36:03 +0700

    save

--- execution/stagedsync/exec3.go
+++ execution/stagedsync/exec3.go
@@ -33,6 +33,7 @@ import (
 	"github.com/erigontech/erigon/common"
 	"github.com/erigontech/erigon/common/cmp"
 	"github.com/erigontech/erigon/common/dbg"
+	"github.com/erigontech/erigon/common/estimate"
 	"github.com/erigontech/erigon/common/log/v3"
 	"github.com/erigontech/erigon/db/kv"
 	"github.com/erigontech/erigon/db/kv/rawdbv3"
@@ -151,6 +152,11 @@ func ExecV3(ctx context.Context,
 	}
 
 	agg := cfg.db.(dbstate.HasAgg).Agg().(*dbstate.Aggregator)
+	if !inMemExec && !isMining {
+		agg.SetCompressWorkers(estimate.CompressSnapshot.Workers())
+	} else {
+		agg.SetCompressWorkers(1)
+	}
 
 	var err error
 	if !inMemExec {
