093855ad989ad9806ac7acd37b1a21c3226d79f1
Mark Holt <mark.holt@erigon.tech>
2026-01-25 18:29:23 +0000

    remove warmup plrintln

--- execution/commitment/hex_patricia_hashed.go
+++ execution/commitment/hex_patricia_hashed.go
@@ -2479,7 +2479,7 @@ func (hph *HexPatriciaHashed) Process(ctx context.Context, updates *Updates, log
 		warmuper = NewWarmuper(ctx, warmup)
 		warmuper.Start()
 		defer warmuper.Close()
-		fmt.Println("Warmup enabled:", warmup.EnableWarmupCache)
+		//fmt.Println("Warmup enabled:", warmup.EnableWarmupCache)
 		// Set cache on trie if warmup cache is enabled
 		if warmup.EnableWarmupCache {
 			hph.cache = warmuper.Cache()
