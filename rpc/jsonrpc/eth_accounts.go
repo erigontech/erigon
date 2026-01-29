635e288163ff9e5140ba6ae832f36abbf7602ab4
lupin012 <58134934+lupin012@users.noreply.github.com.>
2026-01-29 19:19:16 +0100

    remove GetBlockNumber()

--- rpc/jsonrpc/eth_accounts.go
+++ rpc/jsonrpc/eth_accounts.go
@@ -160,17 +160,7 @@ func (api *APIImpl) GetStorageAt(ctx context.Context, address common.Address, in
 	}
 	defer tx.Rollback()
 
-	blockNumber, _, latest, err := rpchelper.GetBlockNumber(ctx, blockNrOrHash, tx, api._blockReader, api.filters)
-	if err != nil {
-		return "", err
-	}
-
-	err = api.BaseAPI.checkPruneHistory(ctx, tx, blockNumber)
-	if err != nil {
-		return "", err
-	}
-
-	reader, err := rpchelper.CreateStateReaderFromBlockNumber(ctx, tx, blockNumber, latest, 0, api.stateCache, api._txNumReader)
+        reader, err := rpchelper.CreateStateReader(ctx, tx, api._blockReader, blockNrOrHash, 0, api.filters, api.stateCache, api._txNumReader)
 	if err != nil {
 		return hexutil.Encode(common.LeftPadBytes(empty, 32)), err
 	}
