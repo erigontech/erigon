bd273c9e37eb440590fa3bd8aae5edae6f562725
taratorio <94537774+taratorio@users.noreply.github.com>
2026-02-02 16:05:11 +1100

    reproduce

---
+++ execution/tests/invalid_receipt_hash_high_mgas_test.go
@@ -0,0 +1,26 @@
+package executiontests
+
+import (
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/erigontech/erigon/common/log/v3"
+	"github.com/erigontech/erigon/common/testlog"
+)
+
+func TestInvalidReceiptHashHighMgas(t *testing.T) {
+	ctx := t.Context()
+	logger := testlog.Logger(t, log.LvlDebug)
+	testDir := path.Join(cornersDir, "invalid-receipt-hash-high-mgas")
+	preAllocsDir := path.Join(testDir, "pre_alloc")
+	payloadsDir := path.Join(testDir, "payloads")
+	engineXRunner, err := NewEngineXTestRunner(t, logger, preAllocsDir)
+	require.NoError(t, err)
+	tm := testMatcher{}
+	tm.walk(t, payloadsDir, func(t *testing.T, name string, test EngineXTestDefinition) {
+		err := engineXRunner.Run(ctx, test)
+		require.NoError(t, err)
+	})
+}
