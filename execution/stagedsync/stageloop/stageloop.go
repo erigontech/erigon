29f4a78f40454c2e4706fcae271b298767d1738f
Alex Sharov <AskAlexSharov@gmail.com>
2026-02-05 18:48:13 +0700

    ProcessFrozenBlocks: to support `isDomainAheadOfBlocks` (which we have in `fcu`) (#18981)
    Have "state files are ahead of block files" issue in main:
    ```
    [INFO] [02-05|05:40:47.504] [1/6 OtterSync] Total difficulty index: 15.61M/24.38M
    [INFO] [02-05|05:41:07.504] [1/6 OtterSync] Total difficulty index: 23.40M/24.38M
    [INFO] [02-05|05:41:22.050] [snapshots:download] Stat                blocks=24.38M indices=24.38M alloc=5.2GB sys=9.8GB
    [INFO] [02-05|05:41:22.054] [snapshots:history] Stat                 blocks=1 step2blockNum="8192=24.09M,8320=24.26M,8384=24.33M,8416=24.37M,8424=0" txs=3.29G first_history_idx_in_db=0 alloc=5.2GB sys=9.8GB
    [INFO] [02-05|05:41:22.054] [1/6 OtterSync] DONE                     in=1m17.875008871s block=24381999
    [EROR] [02-05|05:41:22.054] Could not start execution service        err="[4/6 Execution] behind commitment: TxNums index is at block 24381999 and behind commitment 24382199"
    [INFO] [02-05|05:41:41.216] Downloading Execution History            progress=2033/24388522 ETA=297h41m19s blk/sec=22.8
    [INFO] [02-05|05:42:11.216] Downloading Execution History            progress=2730/24388522 ETA=295h22m15s blk/sec=22.9
    ```
    
    Because:
    1: `ProcessFrozenBlocks()` doesn't support `isDomainAheadOfBlocks()`
    (which we have in fcu)
    2: `SharedDomains` now required to be passed from outside of `Stage
    Loop`: it creating chicken-egg problem. To run `StageSnapshots` (which
    downloading files and filling `TxNum index`) need to pass
    `SharedDomains` which expecting `TxNum` index to be filled (or failing
    in constructor). `SharedDomains` has smart constructor - because it
    helped us to catch many problems when prepared 3.0 release - but
    of-course it's anti-pattern.
    
    
    ----
    Still testing. And thinking what to do with point 2

--- execution/stagedsync/stageloop/stageloop.go
+++ execution/stagedsync/stageloop/stageloop.go
@@ -132,19 +132,27 @@ func ProcessFrozenBlocks(ctx context.Context, db kv.TemporalRwDB, blockReader se
 		return err
 	}
 	defer tx.Rollback()
-	doms, err := execctx.NewSharedDomains(ctx, tx, logger)
-	if err != nil {
-		return err
-	}
-	defer doms.Close()
+
 	// run stages first time - it will download blocks
-	err = sync.RunSnapshots(doms, tx)
+	err = sync.RunSnapshots(nil, tx)
 	if err != nil {
 		return err
 	}
 	if onlySnapDownload {
 		return nil
 	}
+
+	// after StageSnapshots (files downloading): if domains are ahead of block files, then nothing to execute.
+	if execctx.IsDomainAheadOfBlocks(ctx, tx, logger) {
+		return tx.Commit()
+	}
+
+	doms, err := execctx.NewSharedDomains(ctx, tx, logger)
+	if err != nil {
+		return err
+	}
+	defer doms.Close()
+
 	var finishStageBeforeSync uint64
 	if hook != nil {
 		finishStageBeforeSync, err = stages.GetStageProgress(tx, stages.Finish)
@@ -159,11 +167,11 @@ func ProcessFrozenBlocks(ctx context.Context, db kv.TemporalRwDB, blockReader se
 	for more := true; more; {
 		more, err = sync.Run(doms, tx, initialCycle, firstCycle)
 		if err != nil {
-			return err
+			return fmt.Errorf("ProcessFrozenBlocks: %w", err)
 		}
 
 		if err := sync.RunPrune(ctx, tx, initialCycle, 0); err != nil {
-			return err
+			return fmt.Errorf("ProcessFrozenBlocks: %w", err)
 		}
 
 		var finStageProgress uint64
@@ -185,7 +193,7 @@ func ProcessFrozenBlocks(ctx context.Context, db kv.TemporalRwDB, blockReader se
 		}
 
 		if err := doms.Flush(ctx, tx); err != nil {
-			return err
+			return fmt.Errorf("ProcessFrozenBlocks: %w", err)
 		}
 		doms.ClearRam(true)
 		if err := tx.Commit(); err != nil {
