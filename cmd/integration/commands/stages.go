22b420bc41bf922c2dcbe94fb74e521f5368607d
Alexey Sharov <AskAlexSharov@gmail.com>
2026-02-06 17:25:27 +0700

    save

--- cmd/integration/commands/stages.go
+++ cmd/integration/commands/stages.go
@@ -898,14 +898,14 @@ func stageExec(db kv.TemporalRwDB, ctx context.Context, logger log.Logger) error
 					return err
 				}
 			}
-			select {
-			case <-logEvery.C:
-				took := time.Since(t)
-				log.Info("[stage_exec] progress", "block_num", s.BlockNumber, "blk/sec", float64(bn-initialExecProgress)/took.Seconds(), "batchSize", batchSize)
-			default:
-			}
-
 			if doms.SizeEstimate() < uint64(batchSize) {
+				select {
+				case <-logEvery.C:
+					took := time.Since(t)
+					log.Info("[stage_exec] progress", "block_num", s.BlockNumber, "blk/sec", float64(bn-initialExecProgress)/took.Seconds(), "batchSize", doms.SizeEstimate())
+				default:
+				}
+
 				continue
 			}
 			if err := doms.Flush(ctx, tx); err != nil {
