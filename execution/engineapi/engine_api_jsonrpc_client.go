erigontech/erigon

commit bedb428578f966793f8b7ade381189da84779120

execution/tests: add benchmark engine x tests setup (#18921)
needed for reproducing https://github.com/erigontech/erigon/issues/18920
closes https://github.com/erigontech/erigon/issues/16562


- adds support for running op code benchmark tests for gas-repricing
locally
- uses blockchain engine x format
https://eest.ethereum.org/main/running_tests/test_formats/blockchain_test_engine/
- tests come from
https://github.com/ethereum/execution-spec-tests/releases/tag/benchmark%40v0.0.6
- for now, these are skipped from CI and are only for manual
runs/experiments as they take a long time to complete (we can disscuss
again in the future whether we want to run these only periodically e.g.
once a day/week and how to achieve that)

execution/engineapi/engine_api_jsonrpc_client.go
@@ -33,24 +33,37 @@ import (
 	"github.com/erigontech/erigon/rpc"
 )
 
-type JsonRpcClientOption func(*JsonRpcClient)
+type JsonRpcClientOption func(*jsonRpcClientOptions)
 
 func WithJsonRpcClientMaxRetries(maxRetries uint64) JsonRpcClientOption {
-	return func(client *JsonRpcClient) {
-		client.maxRetries = maxRetries
+	return func(opts *jsonRpcClientOptions) {
+		opts.maxRetries = maxRetries
 	}
 }
 
 func WithJsonRpcClientRetryBackOff(retryBackOff time.Duration) JsonRpcClientOption {
-	return func(client *JsonRpcClient) {
-		client.retryBackOff = retryBackOff
+	return func(opts *jsonRpcClientOptions) {
+		opts.retryBackOff = retryBackOff
+	}
+}
+
+func WithJsonRpcClientTimeout(timeout time.Duration) JsonRpcClientOption {
+	return func(opts *jsonRpcClientOptions) {
+		opts.timeout = timeout
 	}
 }
 
 func WithRetryableErrCheckers(retryableErrCheckers ...RetryableErrChecker) JsonRpcClientOption {
-	return func(client *JsonRpcClient) {
-		client.retryableErrCheckers = retryableErrCheckers
+	return func(opts *jsonRpcClientOptions) {
+		opts.retryableErrCheckers = retryableErrCheckers
 	}
+}
+
+type jsonRpcClientOptions struct {
+	maxRetries           uint64
+	retryBackOff         time.Duration
+	timeout              time.Duration
+	retryableErrCheckers []RetryableErrChecker
 }
 
 type JsonRpcClient struct {
@@ -75,23 +88,25 @@ func ErrContainsRetryableErrChecker(sub string) RetryableErrChecker {
 }
 
 func DialJsonRpcClient(url string, jwtSecret []byte, logger log.Logger, opts ...JsonRpcClientOption) (*JsonRpcClient, error) {
+	options := jsonRpcClientOptions{
+		maxRetries:   10,
+		retryBackOff: 100 * time.Millisecond,
+		timeout:      30 * time.Second,
+	}
+	for _, opt := range opts {
+		opt(&options)
+	}
 	jwtRoundTripper := jwt.NewHttpRoundTripper(http.DefaultTransport, jwtSecret)
-	httpClient := &http.Client{Timeout: 30 * time.Second, Transport: jwtRoundTripper}
+	httpClient := &http.Client{Timeout: options.timeout, Transport: jwtRoundTripper}
 	client, err := rpc.DialHTTPWithClient(url, httpClient, logger)
 	if err != nil {
 		return nil, err
 	}
-
 	res := &JsonRpcClient{
 		rpcClient:    client,
-		maxRetries:   10,
-		retryBackOff: 100 * time.Millisecond,
+		maxRetries:   options.maxRetries,
+		retryBackOff: options.retryBackOff,
 	}
-
-	for _, opt := range opts {
-		opt(res)
-	}
-
 	return res, nil
 }
 

execution/tests/benchmark_engine_x_test.go
@@ -1,0 +1,75 @@
+// Copyright 2026 The Erigon Authors
+// This file is part of Erigon.
+//
+// Erigon is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Erigon is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Erigon. If not, see <http://www.gnu.org/licenses/>.
+
+package executiontests
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/erigontech/erigon/common/dbg"
+	"github.com/erigontech/erigon/common/log/v3"
+	"github.com/erigontech/erigon/common/testlog"
+)
+
+func TestBenchmarkEngineXInstruction(t *testing.T) {
+	// if wanting to run only a particular test, pass a whitelist, e.g.
+	//benchmarkCategory(t, "instruction", ".*log\\.json", nil)
+	//benchmarkCategory(t, "instruction", ".*codecopy\\.json", nil)
+	benchmarkCategory(t, "instruction", "", nil)
+}
+
+func TestBenchmarkEngineXPrecompile(t *testing.T) {
+	benchmarkCategory(t, "precompile", "", nil)
+}
+
+func TestBenchmarkEngineXScenario(t *testing.T) {
+	benchmarkCategory(t, "scenario", "", nil)
+}
+
+func benchmarkCategory(t *testing.T, category string, whitelist string, skipload []string) {
+	if !dbg.EnvBool("BENCH_ENGINE_X_MANUAL_ALLOW", false) {
+		t.Skip("benchmark engine x tests are for manual use; enable via BENCH_ENGINE_X_MANUAL_ALLOW=true")
+	}
+	if testing.Short() {
+		t.Skip()
+	}
+	t.Parallel()
+	logger := testlog.Logger(t, log.LvlDebug)
+	engineXDir := filepath.Join(eestDir, "benchmark", "blockchain_tests_engine_x")
+	testsDir := filepath.Join(engineXDir, "benchmark", "compute", category)
+	preAllocDir := filepath.Join(engineXDir, "pre_alloc")
+	engineXRunner, err := NewEngineXTestRunner(t, logger, preAllocDir)
+	require.NoError(t, err)
+	tm := testMatcher{
+		// we re-use the same engine for tests,
+		// and we want to do sequential new payloads
+		// without getting SYNCING responses
+		noparallel: true,
+	}
+	if whitelist != "" {
+		tm.whitelist(whitelist)
+	}
+	for _, s := range skipload {
+		tm.skipLoad(s)
+	}
+	tm.walk(t, testsDir, func(t *testing.T, name string, test EngineXTestDefinition) {
+		err := engineXRunner.Run(t.Context(), test)
+		require.NoError(t, err)
+	})
+}

execution/tests/engine_api_tester.go
@@ -209,19 +209,25 @@ func InitialiseEngineApiTester(t *testing.T, args EngineApiTesterInitArgs) Engin
 	rpcApiClient := requests.NewRequestGenerator(rpcDaemonHttpUrl, logger)
 	contractBackend := contracts.NewJsonRpcBackend(rpcDaemonHttpUrl, logger)
 	//goland:noinspection HttpUrlsUsage
-	engineApiUrl := fmt.Sprintf("http://%s:%d", httpConfig.AuthRpcHTTPListenAddress, httpConfig.AuthRpcPort)
-	engineApiClient, err := engineapi.DialJsonRpcClient(
-		engineApiUrl,
-		jwtSecret,
-		logger,
+	engineApiClientOpts := []engineapi.JsonRpcClientOption{
 		// requests should not take more than 5 secs in a test env, yet we can spam frequently
-		engineapi.WithJsonRpcClientRetryBackOff(50*time.Millisecond),
+		engineapi.WithJsonRpcClientRetryBackOff(50 * time.Millisecond),
 		engineapi.WithJsonRpcClientMaxRetries(100),
 		engineapi.WithRetryableErrCheckers(
 			engineapi.ErrContainsRetryableErrChecker("connection refused"),
 			// below happened on win CI
 			engineapi.ErrContainsRetryableErrChecker("No connection could be made because the target machine actively refused it"),
 		),
+	}
+	if args.EngineApiClientTimeout != nil {
+		engineApiClientOpts = append(engineApiClientOpts, engineapi.WithJsonRpcClientTimeout(*args.EngineApiClientTimeout))
+	}
+	engineApiUrl := fmt.Sprintf("http://%s:%d", httpConfig.AuthRpcHTTPListenAddress, httpConfig.AuthRpcPort)
+	engineApiClient, err := engineapi.DialJsonRpcClient(
+		engineApiUrl,
+		jwtSecret,
+		logger,
+		engineApiClientOpts...,
 	)
 	require.NoError(t, err)
 	var mockCl *MockCl
@@ -230,7 +236,10 @@ func InitialiseEngineApiTester(t *testing.T, args EngineApiTesterInitArgs) Engin
 	} else {
 		mockCl = NewMockCl(ctx, logger, engineApiClient, ethBackend.StateDiffClient(), genesisBlock)
 	}
-	_, err = mockCl.BuildCanonicalBlock(ctx) // build 1 empty block before proceeding to properly initialise everything
+	if !args.NoEmptyBlock1 {
+		// build 1 empty block before proceeding to properly initialise everything
+		_, err = mockCl.BuildCanonicalBlock(ctx)
+	}
 	require.NoError(t, err)
 	return EngineApiTester{
 		GenesisBlock:         genesisBlock,
@@ -248,12 +257,14 @@ func InitialiseEngineApiTester(t *testing.T, args EngineApiTesterInitArgs) Engin
 }
 
 type EngineApiTesterInitArgs struct {
-	Logger           log.Logger
-	DataDir          string
-	Genesis          *types.Genesis
-	CoinbaseKey      *ecdsa.PrivateKey
-	EthConfigTweaker func(*ethconfig.Config)
-	MockClState      *MockClState
+	Logger                 log.Logger
+	DataDir                string
+	Genesis                *types.Genesis
+	CoinbaseKey            *ecdsa.PrivateKey
+	EthConfigTweaker       func(*ethconfig.Config)
+	MockClState            *MockClState
+	NoEmptyBlock1          bool
+	EngineApiClientTimeout *time.Duration
 }
 
 type EngineApiTester struct {

execution/tests/engine_x_test_runner.go
@@ -1,0 +1,268 @@
+// Copyright 2026 The Erigon Authors
+// This file is part of Erigon.
+//
+// Erigon is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Erigon is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Erigon. If not, see <http://www.gnu.org/licenses/>.
+
+package executiontests
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/jinzhu/copier"
+
+	"github.com/erigontech/erigon/common"
+	"github.com/erigontech/erigon/common/hexutil"
+	"github.com/erigontech/erigon/common/log/v3"
+	"github.com/erigontech/erigon/execution/chain"
+	enginetypes "github.com/erigontech/erigon/execution/engineapi/engine_types"
+	"github.com/erigontech/erigon/execution/tests/testforks"
+	"github.com/erigontech/erigon/execution/types"
+)
+
+func NewEngineXTestRunner(t *testing.T, logger log.Logger, preAllocsDir string) (*EngineXTestRunner, error) {
+	preAllocs := make(map[PreAllocHash]*PreAlloc)
+	err := filepath.WalkDir(preAllocsDir, func(path string, info os.DirEntry, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+		b, err := os.ReadFile(path)
+		if err != nil {
+			return err
+		}
+		var preAlloc PreAlloc
+		err = json.Unmarshal(b, &preAlloc)
+		if err != nil {
+			return err
+		}
+		preAllocs[PreAllocHash(strings.TrimSuffix(info.Name(), filepath.Ext(info.Name())))] = &preAlloc
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	runner := &EngineXTestRunner{
+		t:         t,
+		logger:    logger,
+		preAllocs: preAllocs,
+		testers:   make(map[Fork]map[PreAllocHash]EngineApiTester),
+	}
+	return runner, nil
+}
+
+type EngineXTestRunner struct {
+	t         *testing.T
+	logger    log.Logger
+	preAllocs map[PreAllocHash]*PreAlloc
+	mu        sync.Mutex
+	testers   map[Fork]map[PreAllocHash]EngineApiTester
+	wg        sync.WaitGroup
+}
+
+func (extr *EngineXTestRunner) Run(ctx context.Context, test EngineXTestDefinition) error {
+	tester, err := extr.getOrCreateTester(test.Fork, test.PreAllocHash)
+	if err != nil {
+		return err
+	}
+	if len(test.NewPayloads) > 0 {
+		// make sure each test begins at genesis
+		err = processFcu(ctx, tester, tester.GenesisBlock.Hash(), test.NewPayloads[0].FcuVersion)
+		if err != nil {
+			return err
+		}
+	}
+	for _, newPayload := range test.NewPayloads {
+		err = processNewPayload(ctx, tester, newPayload)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (extr *EngineXTestRunner) getOrCreateTester(fork Fork, preAllocHash PreAllocHash) (EngineApiTester, error) {
+	extr.mu.Lock()
+	defer extr.mu.Unlock()
+	testersPerAlloc, ok := extr.testers[fork]
+	if ok {
+		tester, ok := testersPerAlloc[preAllocHash]
+		if ok {
+			return tester, nil
+		}
+	} else {
+		testersPerAlloc = make(map[PreAllocHash]EngineApiTester)
+		extr.testers[fork] = testersPerAlloc
+	}
+	// create an engine api tester for [fork, preAllocHash] tuple
+	forkConfig, ok := testforks.Forks[fork.String()]
+	if !ok {
+		return EngineApiTester{}, testforks.UnsupportedForkError{Name: fork.String()}
+	}
+	alloc, ok := extr.preAllocs[preAllocHash]
+	if !ok {
+		return EngineApiTester{}, fmt.Errorf("pre_alloc %s not found", preAllocHash)
+	}
+	var forkConfigCopy chain.Config
+	err := copier.Copy(&forkConfigCopy, forkConfig)
+	if err != nil {
+		return EngineApiTester{}, err
+	}
+	forkConfig = &forkConfigCopy
+	genesis := alloc.Genesis
+	genesis.Alloc = alloc.Alloc
+	genesis.Config = forkConfig
+	engineApiClientTimeout := 10 * time.Minute
+	tester := InitialiseEngineApiTester(extr.t, EngineApiTesterInitArgs{
+		Logger:                 extr.logger,
+		DataDir:                extr.t.TempDir(),
+		Genesis:                &genesis,
+		NoEmptyBlock1:          true,
+		EngineApiClientTimeout: &engineApiClientTimeout,
+	})
+	testersPerAlloc[preAllocHash] = tester
+	return testersPerAlloc[preAllocHash], nil
+}
+
+func processNewPayload(ctx context.Context, tester EngineApiTester, payload EngineXTestNewPayload) error {
+	var enginePayload enginetypes.ExecutionPayload
+	var blobHashes []common.Hash
+	var parentBeaconRoot common.Hash
+	var executionRequests []hexutil.Bytes
+	err := json.Unmarshal(payload.Params[0], &enginePayload)
+	if err != nil {
+		return err
+	}
+	if len(payload.Params) > 1 {
+		err := json.Unmarshal(payload.Params[1], &blobHashes)
+		if err != nil {
+			return err
+		}
+	}
+	if len(payload.Params) > 2 {
+		err := json.Unmarshal(payload.Params[2], &parentBeaconRoot)
+		if err != nil {
+			return err
+		}
+	}
+	if len(payload.Params) > 3 {
+		err := json.Unmarshal(payload.Params[3], &executionRequests)
+		if err != nil {
+			return err
+		}
+	}
+	enginePayloadStatus, err := retryEngine(
+		ctx,
+		[]enginetypes.EngineStatus{enginetypes.SyncingStatus},
+		nil,
+		func() (*enginetypes.PayloadStatus, enginetypes.EngineStatus, error) {
+			var r *enginetypes.PayloadStatus
+			var err error
+			switch payload.NewPayloadVersion {
+			case "1":
+				r, err = tester.EngineApiClient.NewPayloadV1(ctx, &enginePayload)
+			case "2":
+				r, err = tester.EngineApiClient.NewPayloadV2(ctx, &enginePayload)
+			case "3":
+				r, err = tester.EngineApiClient.NewPayloadV3(ctx, &enginePayload, blobHashes, &parentBeaconRoot)
+			case "4":
+				r, err = tester.EngineApiClient.NewPayloadV4(ctx, &enginePayload, blobHashes, &parentBeaconRoot, executionRequests)
+			case "5":
+				r, err = tester.EngineApiClient.NewPayloadV5(ctx, &enginePayload, blobHashes, &parentBeaconRoot, executionRequests)
+			default:
+				return nil, "", fmt.Errorf("unsupported new payload version: %s", payload.NewPayloadVersion)
+			}
+			if err != nil {
+				return nil, "", err
+			}
+			return r, r.Status, nil
+		},
+	)
+	if err != nil {
+		return err
+	}
+	if enginePayloadStatus.Status != enginetypes.ValidStatus {
+		return fmt.Errorf("payload status is not valid: %s", enginePayloadStatus.Status)
+	}
+	return processFcu(ctx, tester, enginePayload.BlockHash, payload.FcuVersion)
+}
+
+func processFcu(ctx context.Context, tester EngineApiTester, head common.Hash, version string) error {
+	fcu := enginetypes.ForkChoiceState{
+		HeadHash:           head,
+		SafeBlockHash:      common.Hash{},
+		FinalizedBlockHash: common.Hash{},
+	}
+	r, err := retryEngine(
+		ctx,
+		[]enginetypes.EngineStatus{enginetypes.SyncingStatus},
+		nil,
+		func() (*enginetypes.ForkChoiceUpdatedResponse, enginetypes.EngineStatus, error) {
+			var r *enginetypes.ForkChoiceUpdatedResponse
+			var err error
+			switch version {
+			case "1":
+				r, err = tester.EngineApiClient.ForkchoiceUpdatedV1(ctx, &fcu, nil)
+			case "2":
+				r, err = tester.EngineApiClient.ForkchoiceUpdatedV2(ctx, &fcu, nil)
+			case "3":
+				r, err = tester.EngineApiClient.ForkchoiceUpdatedV3(ctx, &fcu, nil)
+			default:
+				return nil, "", fmt.Errorf("unsupported fcu version: %s", version)
+			}
+			if err != nil {
+				return nil, "", err
+			}
+			return r, r.PayloadStatus.Status, nil
+		},
+	)
+	if err != nil {
+		return err
+	}
+	if r.PayloadStatus.Status != enginetypes.ValidStatus {
+		return fmt.Errorf("payload status of fcu is not valid: %s", r.PayloadStatus.Status)
+	}
+	return nil
+}
+
+type EngineXTestDefinition struct {
+	Fork         Fork                    `json:"network"`
+	PreAllocHash PreAllocHash            `json:"prehash"`
+	NewPayloads  []EngineXTestNewPayload `json:"engineNewPayloads"`
+}
+
+type EngineXTestNewPayload struct {
+	Params            []json.RawMessage `json:"params"`
+	NewPayloadVersion string            `json:"newPayloadVersion"`
+	FcuVersion        string            `json:"forkchoiceUpdatedVersion"`
+}
+
+type PreAllocHash string
+
+type PreAlloc struct {
+	Genesis types.Genesis      `json:"genesis"`
+	Alloc   types.GenesisAlloc `json:"pre"`
+}
+
+type Fork string
+
+func (f Fork) String() string {
+	return string(f)
+}

execution/tests/init_test.go
@@ -80,6 +80,7 @@ type testMatcher struct {
 	skiploadpat  []*regexp.Regexp
 	slowpat      []*regexp.Regexp
 	whitelistpat *regexp.Regexp
+	noparallel   bool
 }
 
 type testConfig struct {
@@ -204,7 +205,9 @@ func (tm *testMatcher) walk(t *testing.T, dir string, runTest any) {
 }
 
 func (tm *testMatcher) runTestFile(t *testing.T, path, name string, runTest any) {
-	t.Parallel()
+	if !tm.noparallel {
+		t.Parallel()
+	}
 	if r, _ := tm.findSkip(name); r != "" {
 		t.Skip(r)
 	}
@@ -229,11 +232,7 @@ func (tm *testMatcher) runTestFile(t *testing.T, path, name string, runTest any)
 		for _, key := range keys {
 			i++
 			name := name + "/" + key
-			subTestName := key
-			if len(subTestName) > 32 {
-				subTestName = fmt.Sprintf("%s_%s_%d", key[:20], key[len(key)-20:], i)
-			}
-			t.Run(subTestName, func(t *testing.T) {
+			t.Run(key, func(t *testing.T) {
 				if r, _ := tm.findSkip(name); r != "" {
 					t.Skip(r)
 				}

execution/types/gen_genesis.go
@@ -26,7 +26,7 @@ func (g Genesis) MarshalJSON() ([]byte, error) {
 		Difficulty            *math.HexOrDecimal256                       `json:"difficulty" gencodec:"required"`
 		Mixhash               common.Hash                                 `json:"mixHash"`
 		Coinbase              common.Address                              `json:"coinbase"`
-		Alloc                 map[common.UnprefixedAddress]GenesisAccount `json:"alloc"      gencodec:"required"`
+		Alloc                 map[common.UnprefixedAddress]GenesisAccount `json:"alloc"`
 		AuRaSeal              *AuRaSeal                                   `json:"seal"`
 		Number                math.HexOrDecimal64                         `json:"number"`
 		GasUsed               math.HexOrDecimal64                         `json:"gasUsed"`
@@ -77,7 +77,7 @@ func (g *Genesis) UnmarshalJSON(input []byte) error {
 		Difficulty            *math.HexOrDecimal256                       `json:"difficulty" gencodec:"required"`
 		Mixhash               *common.Hash                                `json:"mixHash"`
 		Coinbase              *common.Address                             `json:"coinbase"`
-		Alloc                 map[common.UnprefixedAddress]GenesisAccount `json:"alloc"      gencodec:"required"`
+		Alloc                 map[common.UnprefixedAddress]GenesisAccount `json:"alloc"`
 		AuRaSeal              *AuRaSeal                                   `json:"seal"`
 		Number                *math.HexOrDecimal64                        `json:"number"`
 		GasUsed               *math.HexOrDecimal64                        `json:"gasUsed"`
@@ -119,12 +119,11 @@ func (g *Genesis) UnmarshalJSON(input []byte) error {
 	if dec.Coinbase != nil {
 		g.Coinbase = *dec.Coinbase
 	}
-	if dec.Alloc == nil {
-		return errors.New("missing required field 'alloc' for Genesis")
-	}
-	g.Alloc = make(GenesisAlloc, len(dec.Alloc))
-	for k, v := range dec.Alloc {
-		g.Alloc[common.Address(k)] = v
+	if dec.Alloc != nil {
+		g.Alloc = make(GenesisAlloc, len(dec.Alloc))
+		for k, v := range dec.Alloc {
+			g.Alloc[common.Address(k)] = v
+		}
 	}
 	if dec.AuRaSeal != nil {
 		g.AuRaSeal = dec.AuRaSeal

execution/types/genesis.go
@@ -50,7 +50,7 @@ type Genesis struct {
 	Difficulty *big.Int       `json:"difficulty" gencodec:"required"`
 	Mixhash    common.Hash    `json:"mixHash"`
 	Coinbase   common.Address `json:"coinbase"`
-	Alloc      GenesisAlloc   `json:"alloc"      gencodec:"required"`
+	Alloc      GenesisAlloc   `json:"alloc"`
 
 	AuRaSeal *AuRaSeal `json:"seal"`
 

node/ethconfig/gen_config.go
@@ -4,6 +4,7 @@ package ethconfig
 
 import (
 	"math/big"
+	"time"
 
 	"github.com/c2h5oh/datasize"
 	"github.com/erigontech/erigon/cl/clparams"
@@ -47,6 +48,7 @@ func (c Config) MarshalTOML() (interface{}, error) {
 		RPCGasCap                           uint64  `toml:",omitempty"`
 		RPCTxFeeCap                         float64 `toml:",omitempty"`
 		StateStream                         bool
+		ExperimentalBAL                     bool
 		HeimdallURL                         string
 		WithoutHeimdall                     bool
 		Ethstats                            string
@@ -69,6 +71,10 @@ func (c Config) MarshalTOML() (interface{}, error) {
 		PolygonPosSingleSlotFinality        bool
 		PolygonPosSingleSlotFinalityBlockAt uint64
 		AllowAA                             bool
+		FcuTimeout                          time.Duration
+		FcuBackgroundPrune                  bool
+		FcuBackgroundCommit                 bool
+		MCPAddress                          string
 	}
 	var enc Config
 	enc.Genesis = c.Genesis
@@ -94,6 +100,7 @@ func (c Config) MarshalTOML() (interface{}, error) {
 	enc.RPCGasCap = c.RPCGasCap
 	enc.RPCTxFeeCap = c.RPCTxFeeCap
 	enc.StateStream = c.StateStream
+	enc.ExperimentalBAL = c.ExperimentalBAL
 	enc.HeimdallURL = c.HeimdallURL
 	enc.WithoutHeimdall = c.WithoutHeimdall
 	enc.Ethstats = c.Ethstats
@@ -116,6 +123,10 @@ func (c Config) MarshalTOML() (interface{}, error) {
 	enc.PolygonPosSingleSlotFinality = c.PolygonPosSingleSlotFinality
 	enc.PolygonPosSingleSlotFinalityBlockAt = c.PolygonPosSingleSlotFinalityBlockAt
 	enc.AllowAA = c.AllowAA
+	enc.FcuTimeout = c.FcuTimeout
+	enc.FcuBackgroundPrune = c.FcuBackgroundPrune
+	enc.FcuBackgroundCommit = c.FcuBackgroundCommit
+	enc.MCPAddress = c.MCPAddress
 	return &enc, nil
 }
 
@@ -168,6 +179,10 @@ func (c *Config) UnmarshalTOML(unmarshal func(interface{}) error) error {
 		PolygonPosSingleSlotFinality        *bool
 		PolygonPosSingleSlotFinalityBlockAt *uint64
 		AllowAA                             *bool
+		FcuTimeout                          *time.Duration
+		FcuBackgroundPrune                  *bool
+		FcuBackgroundCommit                 *bool
+		MCPAddress                          *string
 	}
 	var dec Config
 	if err := unmarshal(&dec); err != nil {
@@ -310,6 +325,18 @@ func (c *Config) UnmarshalTOML(unmarshal func(interface{}) error) error {
 	}
 	if dec.AllowAA != nil {
 		c.AllowAA = *dec.AllowAA
+	}
+	if dec.FcuTimeout != nil {
+		c.FcuTimeout = *dec.FcuTimeout
+	}
+	if dec.FcuBackgroundPrune != nil {
+		c.FcuBackgroundPrune = *dec.FcuBackgroundPrune
+	}
+	if dec.FcuBackgroundCommit != nil {
+		c.FcuBackgroundCommit = *dec.FcuBackgroundCommit
+	}
+	if dec.MCPAddress != nil {
+		c.MCPAddress = *dec.MCPAddress
 	}
 	return nil
 }

commit 214a00bb28ad30773bef325e478718f144cd2ba4

execution/engineapi: fix flaky tests due to engine api tester regression (#19005)
fixes flakes like:
https://github.com/erigontech/erigon/actions/runs/21738296086/job/62708027554

```
engine_api_tester.go:243: 
        	Error Trace:	/home/runner/work/erigon/erigon/execution/tests/engine_api_tester.go:243
        	            				/home/runner/work/erigon/erigon/execution/tests/engine_api_tester.go:63
        	            				/home/runner/work/erigon/erigon/execution/tests/engine_api_reorg_test.go:40
        	Error:      	Received unexpected error:
        	            	build new payload failed: build new payload: fcu error: Post "http://127.0.0.1:6025/": dial tcp 127.0.0.1:6025: connect: connection refused
        	Test:       	TestEngineApiInvalidPayloadThenValidCanonicalFcuWithPayloadShouldSucceed
```

there is a retry for this race condition, but I accidentally broke it in
https://github.com/erigontech/erigon/pull/18921

execution/engineapi/engine_api_jsonrpc_client.go
@@ -103,9 +103,10 @@ func DialJsonRpcClient(url string, jwtSecret []byte, logger log.Logger, opts ...
 		return nil, err
 	}
 	res := &JsonRpcClient{
-		rpcClient:    client,
-		maxRetries:   options.maxRetries,
-		retryBackOff: options.retryBackOff,
+		rpcClient:            client,
+		maxRetries:           options.maxRetries,
+		retryBackOff:         options.retryBackOff,
+		retryableErrCheckers: options.retryableErrCheckers,
 	}
 	return res, nil
 }