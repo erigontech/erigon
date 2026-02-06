82bdbfb9c73d15f84d5cbf2aa5144ec81c404309
Alexey Sharov <AskAlexSharov@gmail.com>
2026-02-06 16:57:40 +0700

    save

--- cmd/integration/commands/stages.go
+++ cmd/integration/commands/stages.go
@@ -900,21 +900,31 @@ func stageExec(db kv.TemporalRwDB, ctx context.Context, logger log.Logger) error
 			if err := tx.Commit(); err != nil {
 				return err
 			}
-			if tx, err = db.BeginTemporalRw(ctx); err != nil {
-				return err
-			}
+			go func() {
+				tx, err := db.BeginTemporalRw(ctx)
+				if err != nil {
+					panic(err)
+				}
+				defer tx.Rollback()
 
-			pruneStage, err := sync.PruneStageState(stages.Execution, s.BlockNumber, tx, s.CurrentSyncCycle.IsInitialCycle)
-			if err != nil {
-				return err
-			}
-			if err := stagedsync.PruneExecutionStage(ctx, pruneStage, tx, cfg, 0, logger); err != nil {
-				return err
-			}
+				pruneStage, err := sync.PruneStageState(stages.Execution, s.BlockNumber, tx, s.CurrentSyncCycle.IsInitialCycle)
+				if err != nil {
+					panic(err)
+				}
+				if err := stagedsync.PruneExecutionStage(ctx, pruneStage, tx, cfg, 0, logger); err != nil {
+					panic(err)
+				}
 
-			if err := tx.Commit(); err != nil {
-				return err
-			}
+				if err := tx.Commit(); err != nil {
+					panic(err)
+				}
+				select {
+				case <-logEvery.C:
+					took := time.Since(t)
+					log.Info("[stage_exec] progress", "block_num", s.BlockNumber, "blk/sec", float64(bn-initialExecProgress)/took.Seconds())
+				default:
+				}
+			}()
 			if tx, err = db.BeginTemporalRw(ctx); err != nil {
 				return err
 			}
