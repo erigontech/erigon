906fa6a73cf15158955671f270e4e809e4aaeb16
Alexey Sharov <AskAlexSharov@gmail.com>
2026-02-06 18:55:16 +0700

    save

--- execution/state/rw_v3.go
+++ execution/state/rw_v3.go
@@ -267,7 +267,7 @@ func (rs *StateV3) SizeEstimateBeforeCommitment() uint64 {
 		return 0
 	}
 	sz := rs.domains.SizeEstimate()
-	sz *= 2 // to cover data-structures overhead (and keep accounting cheap)
+	sz *= 2 // to cover data-structures overhead (and keep accounting cheap). this overhead exists before and after Commitment calc
 	sz *= 4 // for Commitment calculation when batch is full
 	return sz
 }
