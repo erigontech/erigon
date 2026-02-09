ab894f4c9f4b641d2ea6e9c9720d6e6d8e19e83d
Matt Joiner <anacrolix@gmail.com>
2026-02-09 11:04:39 +1100

    Fix a few Windows build errors

--- node/debug/signal-unix.go
+++ node/debug/signal-unix.go
@@ -14,45 +14,19 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with Erigon. If not, see <http://www.gnu.org/licenses/>.
 
+//go:build unix
+
 package debug
 
 import (
 	"os"
-	"os/signal"
-	"runtime/pprof"
 
 	g "github.com/anacrolix/generics"
-	"github.com/erigontech/erigon/common/dbg"
+	"golang.org/x/sys/unix"
+
 	"github.com/erigontech/erigon/common/log/v3"
 )
 
-func listenSignalsInner(handle func(), logger log.Logger, shutdown []os.Signal, prof g.Option[os.Signal]) {
-	sigc := make(chan os.Signal, 1)
-	signal.Notify(sigc, shutdown...)
-	dbg.GetSigC(&sigc)
-	defer signal.Stop(sigc)
-
-	usr1 := make(chan os.Signal, 1)
-	if prof.Ok {
-		signal.Notify(usr1, prof.Value)
-	}
-	for {
-		select {
-		case <-sigc:
-			logger.Info("Got interrupt, shutting down...")
-			if handle != nil {
-				go handle()
-			}
-			for i := 10; i > 0; i-- {
-				<-sigc
-				if i > 1 {
-					logger.Warn("Already shutting down, interrupt more to panic.", "times", i-1)
-				}
-			}
-			Exit() // ensure trace and CPU profile data is flushed.
-			LoudPanic("boom")
-		case <-usr1:
-			pprof.Lookup("goroutine").WriteTo(os.Stdout, 1)
-		}
-	}
+func ListenSignals(handle func(), logger log.Logger) {
+	listenSignalsInner(handle, logger, []os.Signal{unix.SIGINT, unix.SIGTERM}, g.Some[os.Signal](unix.SIGUSR1))
 }
