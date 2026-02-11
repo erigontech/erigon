1fdd0c357dd5feca85f2cf3a634db09b41fa4796
Alexey Sharov <AskAlexSharov@gmail.com>
2026-02-11 20:37:22 +0700

    save

--- db/recsplit/simpleseq/simple_sequence.go
+++ db/recsplit/simpleseq/simple_sequence.go
@@ -3,7 +3,6 @@ package simpleseq
 import (
 	"encoding/binary"
 
-	"github.com/erigontech/erigon/common/log/v3"
 	"github.com/erigontech/erigon/db/kv/stream"
 )
 
@@ -75,17 +74,17 @@ var distLen [17]int
 func (s *SimpleSequence) search(seek uint64) (idx int, v uint64, ok bool) {
 	raw := s.raw
 	if len(raw) == 0 || seek > s.Max() {
-		distFound[16]++
+		//distFound[16]++
 		return 0, 0, false
 	}
-	if distFound[1]%100 == 0 && distFound[0]%100 == 0 {
-		log.Warn("[dbg] SimpleSequence.search", "distFound", distFound, "distLen", distLen)
-	}
-	distLen[s.Count()]++
+	//if distFound[1]%100 == 0 && distFound[0]%100 == 0 {
+	//	log.Warn("[dbg] SimpleSequence.search", "distFound", distFound, "distLen", distLen)
+	//}
+	//distLen[s.Count()]++
 	for i := 0; i < len(raw); i += 4 {
 		v = s.baseNum + uint64(binary.BigEndian.Uint32(raw[i:]))
 		if v >= seek {
-			distFound[i/4]++
+			//distFound[i/4]++
 			return i / 4, v, true
 		}
 	}
