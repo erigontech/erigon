b382a5eeecf9dbdb218f95fa2131bff803424372
Alexey Sharov <AskAlexSharov@gmail.com>
2026-02-15 12:48:55 +0700

    save

--- db/seg/sais/sais_test.go
+++ db/seg/sais/sais_test.go
@@ -1,6 +1,8 @@
 package sais
 
 import (
+	"fmt"
+	"math/rand"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,4 +16,23 @@ func TestSais(t *testing.T) {
 		t.Fatal(err)
 	}
 	assert.Equal(t, []int32{6, 3, 0, 7, 4, 1, 8, 5, 2}, sa)
+}
+
+func BenchmarkSais(b *testing.B) {
+	for _, size := range []int{1 << 10, 1 << 16, 1 << 20} {
+		b.Run(fmt.Sprintf("%d", size), func(b *testing.B) {
+			rng := rand.New(rand.NewSource(0))
+			data := make([]byte, size)
+			rng.Read(data)
+			sa := make([]int32, size)
+			b.SetBytes(int64(size))
+			b.ReportAllocs()
+			b.ResetTimer()
+			for b.Loop() {
+				if err := Sais(data, sa); err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
+	}
 }
