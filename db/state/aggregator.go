a25fd59731037a4095d04397d470513c44fd7875
Alexey Sharov <AskAlexSharov@gmail.com>
2026-02-15 18:43:51 +0700

    save

--- db/state/aggregator.go
+++ db/state/aggregator.go
@@ -1446,7 +1446,7 @@ func (at *AggregatorRoTx) mergeFiles(ctx context.Context, files *SelectedStaticF
 		}
 	}()
 
-	at.a.logger.Info("[snapshots] merge state " + r.String())
+	at.a.logger.Info("[snapshots] merge state "+r.String(), "merge_workers", at.a.mergeWorkers)
 	commitmentUseReferencedBranches := at.a.Cfg(kv.CommitmentDomain).ReplaceKeysInValues
 
 	accStorageMerged := new(sync.WaitGroup)
