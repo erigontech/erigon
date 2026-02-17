b49ae45594b855f594f2ca268064420d39577ee7
Bartosz Zawistowski <bartosz.zawistowski@erigon.tech>
2026-02-17 15:34:17 +0100

    Nullptr fixes

--- execution/stagedsync/bodydownload/body_algos.go
+++ execution/stagedsync/bodydownload/body_algos.go
@@ -49,13 +49,17 @@ func (bd *BodyDownload) UpdateFromDb(db kv.Tx) (err error) {
 	// Resetting for requesting a new range of blocks
 	bd.requestedLow = bodyProgress + 1
 	bd.requestedMap = make(map[BodyHashes]uint64)
-	bd.delivered.Clear()
+	if bd.deliveriesH == nil {
+		bd.delivered.Clear()
+	}
 	bd.deliveredCount = 0
 	bd.wastedCount = 0
 	clear(bd.deliveriesH)
 	clear(bd.requests)
 	clear(bd.peerMap)
-	bd.ClearBodyCache()
+	if bd.bodyCache != nil {
+		bd.ClearBodyCache()
+	}
 	return nil
 }
 
