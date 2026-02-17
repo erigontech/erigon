ab40d4767f122f0e4a2e3410ebeea63eb5338a30
Alexey Sharov <AskAlexSharov@gmail.com>
2026-02-17 12:29:07 +0700

    save

--- db/seg/parallel_compress.go
+++ db/seg/parallel_compress.go
@@ -181,10 +181,6 @@ func coverWordByPatterns(trace bool, input []byte, mf2 *patricia.MatchFinder2, o
 
 func coverWordsByPatternsWorker(ctx context.Context, trace bool, inputCh chan *CompressionWord, outCh chan *CompressionWord, completion *sync.WaitGroup, trie *patricia.PatriciaTree, inputSize, outputSize *atomic.Uint64, posMap map[uint64]uint64) {
 	defer completion.Done()
-	if err := WorkersLimiter.Acquire(ctx, 1); err != nil {
-		return
-	}
-	defer WorkersLimiter.Release(1)
 	var output = make([]byte, 0, 256)
 	var uncovered = make([]int, 256)
 	var patterns = make([]int, 0, 256)
@@ -779,10 +775,6 @@ func copyN(r io.Reader, w io.Writer, uncoveredCount int, buf []byte) error {
 func extractPatternsInSuperstrings(ctx context.Context, superstringCh chan []byte, dictCollector *etl.Collector, cfg Cfg, completion *sync.WaitGroup, logger log.Logger) {
 	minPatternScore, minPatternLen, maxPatternLen := cfg.MinPatternScore, cfg.MinPatternLen, cfg.MaxPatternLen
 	defer completion.Done()
-	if err := WorkersLimiter.Acquire(ctx, 1); err != nil {
-		return
-	}
-	defer WorkersLimiter.Release(1)
 	dictVal := make([]byte, 8)
 	dictKey := make([]byte, maxPatternLen)
 	var lcp, sa, inv []int32
