6d58212260d67e1b1345cc615f934ae70e20a8a3
Alexey Sharov <AskAlexSharov@gmail.com>
2026-02-17 10:41:35 +0700

    save

--- db/state/execctx/domain_shared.go
+++ db/state/execctx/domain_shared.go
@@ -367,11 +367,11 @@ func (sd *SharedDomains) DomainPut(domain kv.Domain, roTx kv.TemporalTx, k, v []
 		}
 	}
 	switch domain {
-	case kv.CodeDomain:
+	case kv.CodeDomain, kv.AccountsDomain, kv.StorageDomain, kv.CommitmentDomain:
 		if bytes.Equal(prevVal, v) {
 			return nil
 		}
-	case kv.StorageDomain, kv.AccountsDomain, kv.CommitmentDomain, kv.RCacheDomain:
+	case kv.RCacheDomain:
 		//noop
 	default:
 		if bytes.Equal(prevVal, v) {
