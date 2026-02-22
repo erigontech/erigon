c22410c1541a47b23d00f7b2bb78dba9aef43a48
Alexey Sharov <AskAlexSharov@gmail.com>
2026-02-22 18:00:42 +0700

    save

--- common/freeport/free_port.go
+++ common/freeport/free_port.go
@@ -17,72 +17,16 @@
 package freeport
 
 import (
-	"context"
-	"fmt"
-	"math/rand"
 	"net"
-	"strconv"
-	"sync"
-	"time"
-)
-
-const (
-	maxPort = 65535
-	minPort = 1024
-)
-
-var (
-	portMu  sync.Mutex
-	portNum int64
 )
 
 // NextFreePort uses a global circular port counter to find the next free port.
 // Note, it can be used by many goroutines at the same time.
 func NextFreePort() (int, error) {
-	portMu.Lock()
-	defer portMu.Unlock()
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
-	defer cancel()
-
-	attempts := -1
-	for {
-		attempts++
-
-		select {
-		case <-ctx.Done():
-			return 0, fmt.Errorf("could not find next free port: attempts %d: %w", attempts, ctx.Err())
-		default: // continue
-		}
-
-		portNum = nextPortNum(portNum)
-		listener, err := net.Listen("tcp", "127.0.0.1:"+strconv.FormatInt(portNum, 10))
-		if err != nil {
-			continue
-		}
-
-		err = listener.Close()
-		if err != nil {
-			return 0, err
-		}
-
-		return int(portNum), nil
+	l, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		return 0, err
 	}
-}
-
-func nextPortNum(port int64) int64 {
-	if port == 0 { // init case
-		// generate a random starting point to avoid clashes
-		// if more than 1 "go test ./erigon " processes are run on the same machine at the same time
-		// for simplicity, assume there are 60,000 ports and each go test process needs 2500 ports
-		// 60,000/2500=24 buckets - randomly pick 1 of these buckets
-		// note: this randomness is not needed for a single run of a "go test ./erigon" process
-		rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
-		n := rnd.Intn(24)
-		return minPort + int64(n)*2500
-	} else if port == maxPort {
-		return minPort
-	} else {
-		return port + 1
-	}
+	port := l.Addr().(*net.TCPAddr).Port
+	return port, nil
 }
