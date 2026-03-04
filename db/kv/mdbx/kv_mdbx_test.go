c9b682f48c957c1d30a406d84e2d5635ff278c07
Alexey Sharov <AskAlexSharov@gmail.com>
2026-03-04 16:51:55 +0700

    save

--- db/kv/mdbx/kv_mdbx_test.go
+++ db/kv/mdbx/kv_mdbx_test.go
@@ -1031,13 +1031,12 @@ func BenchmarkDB_Put(b *testing.B) {
 	table := "Table"
 	db := _db.(*MdbxKV)
 
-	// Ensure data is correct.
-	keys := make([][]byte, b.N)
-	for i := 1; i <= b.N; i++ {
+	const keyCount = 10000
+	keys := make([][]byte, keyCount)
+	for i := 1; i <= keyCount; i++ {
 		keys[i-1] = u64tob(uint64(i))
 	}
 
-	b.ResetTimer()
 	if err := db.Update(context.Background(), func(tx kv.RwTx) error {
 		var idx int
 		for b.Loop() {
@@ -1082,8 +1081,9 @@ func BenchmarkDB_Delete(b *testing.B) {
 	table := "Table"
 	db := _db.(*MdbxKV)
 
-	keys := make([][]byte, b.N)
-	for i := 1; i <= b.N; i++ {
+	const keyCount = 10000
+	keys := make([][]byte, keyCount)
+	for i := 1; i <= keyCount; i++ {
 		keys[i-1] = u64tob(uint64(i))
 	}
 
@@ -1099,8 +1099,6 @@ func BenchmarkDB_Delete(b *testing.B) {
 		b.Fatal(err)
 	}
 
-	// Ensure data is correct.
-	b.ResetTimer()
 	if err := db.Update(context.Background(), func(tx kv.RwTx) error {
 		var idx int
 		for b.Loop() {
