5deb6ae8900384fd37a95f75cc68e54d3c60e278
Alexey Sharov <AskAlexSharov@gmail.com>
2026-03-11 14:56:12 +0700

    save

--- cmd/utils/app/snapshots_cmd.go
+++ cmd/utils/app/snapshots_cmd.go
@@ -1304,7 +1304,7 @@ func doIntegrity(cliCtx *cli.Context) error {
 					}
 					scCopy := sc
 					scCopy.SampleRatio /= 100 // it's very slow check
-					if err := integrity.CheckCommitmentHistAtBlkRange(ctx, scCopy, db, blockReader, 1, to+1, logger); err != nil {
+					if err := integrity.CheckCommitmentHistAtBlkRange(ctx, scCopy, db, blockReader, 1, to+1, failFast, logger); err != nil {
 						return err
 					}
 				case integrity.StateVerify:
@@ -1413,7 +1413,7 @@ func doCheckStateRootByHistory(cliCtx *cli.Context, logger log.Logger) error {
 		return err
 	}
 	logger.Info("[check-commitment-hist-at-blk-range] sampling config", "seed", sc.Seed, "sampleRatio", sc.SampleRatio)
-	return integrity.CheckCommitmentHistAtBlkRange(ctx, sc, db, blockReader, from, to, logger)
+	return integrity.CheckCommitmentHistAtBlkRange(ctx, sc, db, blockReader, from, to, true /*failFast*/, logger)
 }
 
 func doVerifyState(cliCtx *cli.Context, logger log.Logger) error {
