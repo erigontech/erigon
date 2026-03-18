e97c5b714d920c81e520378c1ea1bcc70207f75a
kewei <kewei.train@gmail.com>
2026-03-19 04:09:44 +0800

    config for eip7708

--- execution/vm/evmtypes/rules.go
+++ execution/vm/evmtypes/rules.go
@@ -48,6 +48,7 @@ func (bc *BlockContext) Rules(c *chain.Config) *chain.Rules {
 		IsPrague:           c.IsPrague(bc.Time) || c.IsBhilai(bc.BlockNumber),
 		IsOsaka:            c.IsOsaka(bc.Time),
 		IsAmsterdam:        c.IsAmsterdam(bc.Time),
+		IsEIP7708:          c.IsAmsterdam(bc.Time) && !c.DisableEIP7708,
 		IsAura:             c.Aura != nil,
 	}
 }
