c64bd8929bb25d4a28c3512a31cddc18e1f4c66f
taratorio <94537774+taratorio@users.noreply.github.com>
2026-03-19 14:24:28 +1100

    update skips after bal@v5.5.1

--- execution/tests/eest_devnet/block_test.go
+++ execution/tests/eest_devnet/block_test.go
@@ -50,12 +50,20 @@ func TestExecutionSpecBlockchainDevnet(t *testing.T) {
 	bt.Whitelist(`.*for_amsterdam/.*`)
 	// static — tested in state test format by TestState
 	bt.SkipLoad(`^for_amsterdam/static/state_tests/`)
-	bt.SkipLoad(`^for_amsterdam/amsterdam/eip7954_increase_max_contract_size/test_max_code_size_deposit_gas.json`)    // block=1, gas used by execution: 38601120, in header: 16777216
-	bt.SkipLoad(`^for_amsterdam/amsterdam/eip7954_increase_max_contract_size/test_max_initcode_size_via_create.json`) // block=1, gas used by execution: 16777216, in header: 16645728
-	bt.SkipLoad(`^for_amsterdam/constantinople/eip1052_extcodehash/test_extcodehash_subcall_create2_oog.json`)        // block=1, receiptHash mismatch: 39c2dfd7f2067a536d977a40ae2f2b3f3614bbef0a4eb26f49b8114d29e9805f != 8e4253c0afb1566ce113e97f42df12955f2712695f33de2b2c6ab30f654f8897, headerNum=1, 4da65b36435c944e6f68e86aa3d5ad3b796d54b0f73f5e096b31e05ae4c8d32d
-	bt.SkipLoad(`^for_amsterdam/frontier/create/test_create_deposit_oog.json`)                                        // block=1, receiptHash mismatch: b02a14b5b6c881a265f2b3cde9e15388accd60f6ade5ac416448ae70be388b04 != 4b6910d817802196c2707443f8d462fb49eda48f61b5ae0b986ec93f512068d9, headerNum=1, 8f10f38664c884462846eb91339f5c5b5fe818b7f6b6f766ce19e45d109cb0cf
-	bt.SkipLoad(`^for_amsterdam/frontier/opcodes/test_genesis_hash_available.json`)                                   // block=257, gas used by execution: 62432, in header: 37568
-	bt.SkipLoad(`^for_amsterdam/frontier/scenarios/test_scenarios.json`)                                              // block=2, gas used by execution: 54514, in header: 349432
+	bt.SkipLoad(`^for_amsterdam/amsterdam/eip7928_block_level_access_lists/test_bal_sstore_and_oog.json`)                                     // block=1, gas used by execution: 37568, in header: 63573
+	bt.SkipLoad(`^for_amsterdam/amsterdam/eip7954_increase_max_contract_size/test_max_code_size_deposit_gas.json`)                            // block=1, gas used by execution: 38601120, in header: 16777216
+	bt.SkipLoad(`^for_amsterdam/amsterdam/eip7954_increase_max_contract_size/test_max_initcode_size_via_create.json`)                         // block=1, gas used by execution: 16777216, in header: 16645728
+	bt.SkipLoad(`^for_amsterdam/amsterdam/eip8037_state_creation_gas_cost_increase/test_call_oog_reservoir_inflation_detection.json`)         // block=1, gas used by execution: 45072, in header: 214128
+	bt.SkipLoad(`^for_amsterdam/amsterdam/eip8037_state_creation_gas_cost_increase/test_code_deposit_oog_reservoir_inflation_detection.json`) // block=1, receiptHash mismatch: d4195612a65aefcb515baca7d915d1ae0aa4a509919d9c0cf2650f6e0c66febc != d3b8ff8b6f44d3c85d89dbc66c553287bb46883b4844e68d0fa52b2f7162fe5a, headerNum=1, 2bafdd0dce825a5d00881f354532c919830dbf22d7d92dc99067083f2829cde7
+	bt.SkipLoad(`^for_amsterdam/amsterdam/eip8037_state_creation_gas_cost_increase/test_create_oog_reservoir_inflation_detection.json`)       // block=1, gas used by execution: 173948, in header: 211516
+	bt.SkipLoad(`^for_amsterdam/amsterdam/eip8037_state_creation_gas_cost_increase/test_selfdestruct_oog_reservoir_inflation_detection.json`) // block=1, gas used by execution: 41054, in header: 210110
+	bt.SkipLoad(`^for_amsterdam/amsterdam/eip8037_state_creation_gas_cost_increase/test_sstore_oog_no_reservoir_inflation.json`)              // block=1, gas used by execution: 37852, in header: 75420
+	bt.SkipLoad(`^for_amsterdam/amsterdam/eip8037_state_creation_gas_cost_increase/test_sstore_oog_reservoir_inflation_detection.json`)       // block=1, gas used by execution: 71507, in header: 109075
+	bt.SkipLoad(`^for_amsterdam/constantinople/eip1052_extcodehash/test_extcodehash_subcall_create2_oog.json`)                                // block=1, receiptHash mismatch: 39c2dfd7f2067a536d977a40ae2f2b3f3614bbef0a4eb26f49b8114d29e9805f != 8e4253c0afb1566ce113e97f42df12955f2712695f33de2b2c6ab30f654f8897, headerNum=1, 4da65b36435c944e6f68e86aa3d5ad3b796d54b0f73f5e096b31e05ae4c8d32d
+	bt.SkipLoad(`^for_amsterdam/frontier/create/test_create_deposit_oog.json`)                                                                // block=1, receiptHash mismatch: b02a14b5b6c881a265f2b3cde9e15388accd60f6ade5ac416448ae70be388b04 != 4b6910d817802196c2707443f8d462fb49eda48f61b5ae0b986ec93f512068d9, headerNum=1, 8f10f38664c884462846eb91339f5c5b5fe818b7f6b6f766ce19e45d109cb0cf
+	bt.SkipLoad(`^for_amsterdam/frontier/opcodes/test_genesis_hash_available.json`)                                                           // block=257, gas used by execution: 62432, in header: 37568
+	bt.SkipLoad(`^for_amsterdam/frontier/opcodes/test_value_transfer_gas_calculation.json`)                                                   // block=1, gas used by execution: 37443, in header: 168931
+	bt.SkipLoad(`^for_amsterdam/frontier/scenarios/test_scenarios.json`)                                                                      // block=2, gas used by execution: 54514, in header: 349432
 
 	bt.Walk(t, dir, func(t *testing.T, name string, test *testutil.BlockTest) {
 		// import pre accounts & construct test genesis block & state root
