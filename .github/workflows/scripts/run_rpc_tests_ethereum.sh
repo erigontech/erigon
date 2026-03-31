d3d64f9e24dc1aa9bd56bb9bd87f59b96c3b62d2
lupin012 <58134934+lupin012@users.noreply.github.com.>
2026-03-31 22:24:29 +0200

    uncomment 3 test

--- .github/workflows/scripts/run_rpc_tests_ethereum.sh
+++ .github/workflows/scripts/run_rpc_tests_ethereum.sh
@@ -29,13 +29,6 @@ DISABLED_TEST_LIST=(
   eth_getWork/test_01.json
   eth_mining/test_01.json
   eth_protocolVersion/test_1.json
-<<<<<<< HEAD
-  # eth_simulateV1 stateRoot issues under investigation (see PR #20062)
-  eth_simulateV1/test_23.json
-  eth_simulateV1/test_24.json
-  eth_simulateV1/test_26.json
-=======
->>>>>>> ca07330cac (update stateRoot according Geth and re-enable commented tests (#20062))
   eth_submitHashrate/test_1.json
   eth_submitWork/test_1.json
   net_peerCount/test_1.json
