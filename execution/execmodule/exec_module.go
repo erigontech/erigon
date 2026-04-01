9005363a95ffd1e163dfdef81fc545e252d7bb61
sudeepdino008 <sudeepdino008@gmail.com>
2026-04-01 14:51:09 +0200

    execmodule, stageloop: apply ERIGON_IN_MEM_HISTORY flag to ExecV3 path
    - StageLoopIteration (ExecV3 live exec path)
    - ValidateChain (fork validation path)
    - updateForkChoice (FCU path)
    
    Previously only ProcessFrozenBlocks respected the env flag.

--- execution/execmodule/exec_module.go
+++ execution/execmodule/exec_module.go
@@ -57,6 +57,8 @@ import (
 
 var ErrMissingChainSegment = errors.New("missing chain segment")
 
+var inMemHistoryReads = dbg.EnvBool("ERIGON_IN_MEM_HISTORY", true)
+
 func makeErrMissingChainSegment(blockHash common.Hash) error {
 	return errors.Join(ErrMissingChainSegment, errors.New("block hash: "+blockHash.String()))
 }
@@ -400,6 +402,7 @@ func (e *ExecModule) ValidateChain(ctx context.Context, req *executionproto.Vali
 	if err != nil {
 		return nil, err
 	}
+	doms.SetInMemHistoryReads(inMemHistoryReads)
 
 	// Set state cache in SharedDomains for use during state reading
 	if e.stateCache != nil && dbg.UseStateCache {
