d270a2d0cc01ce2adffad41d0ed2f5c776b14e2d
Alexey Sharov <AskAlexSharov@gmail.com>
2026-04-05 09:46:05 +0700

    save

--- db/state/aggregator.go
+++ db/state/aggregator.go
@@ -1173,12 +1173,12 @@ func (at *AggregatorRoTx) PruneSmallBatches(ctx context.Context, timeout time.Du
 				return true, nil
 			}
 			at.a.logger.Warn("[snapshots] PruneSmallBatches failed", "err", err, "is deadline?", errors.Is(err, context.DeadlineExceeded))
-			return false, err
+			return false, errDebug
 		}
 		if stat == nil || stat.PrunedNothing() {
 			at.a.logger.Debug("[snapshots] PruneSmallBatches nilled or nothing")
 			if !fullStat.PrunedNothing() {
-				at.a.logger.Info("[snapshots] PruneSmallBatches finished", "took", time.Since(started).String(), "stat", fullStat.String())
+				at.a.logger.Debug("[snapshots] PruneSmallBatches finished", "took", time.Since(started).String(), "stat", fullStat.String())
 			}
 			return false, nil
 		}
