2428133dc2321b7e5aa93c4c62cd14aab31e7544
Alexey Sharov <AskAlexSharov@gmail.com>
2026-04-07 11:34:29 +0700

    save

--- execution/state/rw_v3.go
+++ execution/state/rw_v3.go
@@ -281,7 +281,6 @@ func (rs *StateV3) SizeEstimateBeforeCommitment() uint64 {
 		return 0
 	}
 	sz := rs.domains.Size()
-	sz *= 2 // to cover data-structures overhead: map, btree, etc... and GC overhead (clean happening periodically)
 	sz *= 2 // for Commitment calculation when batch is full
 	return sz
 }
@@ -291,9 +290,7 @@ func (rs *StateV3) SizeEstimateAfterCommitment() uint64 {
 	if rs.domains == nil {
 		return 0
 	}
-	sz := rs.domains.Size()
-	sz *= 2 // to cover data-structures overhead: map, btree, etc... and GC overhead (clean happening periodically)
-	return sz
+	return rs.domains.Size()
 }
 
 type storageItem struct {
