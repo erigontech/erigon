975cf4dbaeee5fbe20d1b508fc11cb9941dd0419
Alexey Sharov <AskAlexSharov@gmail.com>
2026-04-11 11:03:01 +0700

    save

--- db/state/state_util.go
+++ db/state/state_util.go
@@ -27,11 +27,11 @@ import (
 // SaveExecV3PruneProgress saves latest pruned key in given table to the database.
 // nil key also allowed and means that latest pruning run has been finished.
 func SaveExecV3PruneProgress(db kv.Putter, prunedTblName string, prunedKey []byte) error {
-	empty := make([]byte, 1)
+	buf := make([]byte, 1, 1+len(prunedKey))
 	if prunedKey != nil {
-		empty[0] = 1
+		buf[0] = 1
 	}
-	return db.Put(kv.TblPruningProgress, []byte(prunedTblName), append(empty, prunedKey...))
+	return db.Put(kv.TblPruningProgress, []byte(prunedTblName), append(buf, prunedKey...))
 }
 
 // GetExecV3PruneProgress retrieves saved progress of given table pruning from the database.
