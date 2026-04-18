6cde7d80eea65835bf46db5c100cda1ead1f9341
Alex Sharov <AskAlexSharov@gmail.com>
2026-04-18 12:33:53 +0700

    `dbg.SaveHeapProfileNearOOMPeriodically()` call in all sub-commands (#20639)

--- node/eth/backend.go
+++ node/eth/backend.go
@@ -272,8 +272,6 @@ const blockBufferSize = 128
 // New creates a new Ethereum object (including the
 // initialisation of the common Ethereum object)
 func New(ctx context.Context, stack *node.Node, config *ethconfig.Config, logger log.Logger, tracer *tracers.Tracer) (*Ethereum, error) {
-	go dbg.SaveHeapProfileNearOOMPeriodically(ctx, dbg.SaveHeapWithLogger(&logger))
-
 	dirs := stack.Config().Dirs
 
 	tmpdir := dirs.Tmp
