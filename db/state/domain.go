fbe513562815c0414a465e2d8cafaad98309af93
Alexey Sharov <AskAlexSharov@gmail.com>
2026-04-22 10:25:30 +0700

    save

--- db/state/domain.go
+++ db/state/domain.go
@@ -2022,6 +2022,14 @@ func versionTooLowPanic(filename string, version version.Versions) {
 	))
 }
 
+func versionTooHighPanic(filename string, version version.Versions) {
+	panic(fmt.Sprintf(
+		"FileVersion is too high (binary is older than snapshot files), please upgrade erigon. file=%s, current=%s",
+		filename,
+		version.Current,
+	))
+}
+
 // [startTxNum, endTxNum)
 func (dt *DomainRoTx) TraceKey(ctx context.Context, key []byte, startTxNum, endTxNum uint64, roTx kv.Tx) (stream.U64V, error) {
 	// need to do this first as TraceKey doesn't work if internal seg readers are seeked into different locations
