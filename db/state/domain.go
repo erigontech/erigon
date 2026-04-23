4dd7651540c45b2b9010f9f321213428446439fb
Alexey Sharov <AskAlexSharov@gmail.com>
2026-04-23 11:22:29 +0700

    save

--- db/state/domain.go
+++ db/state/domain.go
@@ -783,7 +783,7 @@ func (d *Domain) collate(ctx context.Context, step kv.Step, txFrom, txTo uint64,
 				return coll, err
 			}
 			if binary.BigEndian.Uint64(v[:8]) != stepVal {
-				k, v, err = valsCursor.Next()
+				k, v, err = valsCursor.NextNoDup()
 				continue
 			}
 			if _, err = comp.Write(k); err != nil {
