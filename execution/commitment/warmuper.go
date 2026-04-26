137f89bedf63b9f5dbc653ffbc312f36756ca854
Alexey Sharov <AskAlexSharov@gmail.com>
2026-04-26 18:22:37 +0700

    save

--- execution/commitment/warmuper.go
+++ execution/commitment/warmuper.go
@@ -280,6 +280,7 @@ func (w *Warmuper) WarmKey(hashedKey []byte, startDepth int) {
 	select {
 	case w.work <- warmupWorkItem{hashedKey: hashedKey, startDepth: startDepth}:
 	case <-w.ctx.Done():
+	default: // non-blocking
 	}
 }
 
