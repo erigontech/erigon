748fbc6b6c78f6c6d64e093b30ce99f80d9563d1
Alexey Sharov <AskAlexSharov@gmail.com>
2026-04-26 16:44:28 +0700

    save

--- execution/commitment/commitmentdb/commitment_context.go
+++ execution/commitment/commitmentdb/commitment_context.go
@@ -353,7 +353,7 @@ func (sdc *SharedDomainsCommitmentContext) ComputeCommitment(ctx context.Context
 		warmupConfig = commitment.WarmupConfig{
 			Enabled:    sdc.trieWarmup,
 			CtxFactory: sdc.trieContextFactory(ctx, sdc.paraTrieDB, txNum),
-			NumWorkers: 2_000,
+			NumWorkers: 512,
 			MaxDepth:   commitment.WarmupMaxDepth,
 			LogPrefix:  logPrefix,
 		}
