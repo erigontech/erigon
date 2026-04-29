cecb36f75d33f81ef43eaa643d8858c92600504b
Alexey Sharov <AskAlexSharov@gmail.com>
2026-04-29 13:37:24 +0700

    stagedsync: call WarmupDB unconditionally

--- execution/stagedsync/exec3.go
+++ execution/stagedsync/exec3.go
@@ -129,9 +129,7 @@ func ExecV3(ctx context.Context,
 	}
 
 	agg := cfg.db.(dbstate.HasAgg).Agg().(*dbstate.Aggregator)
-	if initialCycle {
-		agg.WarmupDB()
-	}
+	agg.WarmupDB()
 	if isApplyingBlocks {
 		if initialCycle {
 			agg.PresetNonChainTipConcurrency()
