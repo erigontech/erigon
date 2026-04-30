erigontech/erigon

commit 2bc289d40f7d14ddbb88002a29e1803ece00057f

Support snapshot reset with symlinks (#18273)
Adds a ton of tests.

Fixes https://github.com/erigontech/erigon/issues/18135,
https://github.com/erigontech/erigon/issues/18191,
https://github.com/erigontech/erigon/issues/16693.

Adds a --preverified flag to snapshots reset to choose embedded, remote
or local file to use for reset. I used it for testing faster, but it is
helpful.

I tried to implement some extra security to avoid recursively deleting
if someone symlinked to the wrong place, but it got out of hand on
complexity. I ended up discovering fs.FS, and os.Root are mostly useless
for this. I settled on 2 possibilities: Checking symlinks to directories
always end with the same name as the source, and having a sequence of
os.Roots where deletion is allowed. The first was a bit too complex, and
the second would require a bunch of extra documentation and user
education which I don't think is worth it. Most good users will be
setting correct permissions, and using hard/bind mounts to do things
reliably.

cmd/utils/app/reset-datadir.go
@@ -3,25 +3,23 @@ package app
 import (
 	"errors"
 	"fmt"
-	"io/fs"
 	"os"
-	"path/filepath"
-	"strings"
 
 	g "github.com/anacrolix/generics"
-	"github.com/anacrolix/torrent/metainfo"
-	"github.com/urfave/cli/v2"
-
-	"github.com/erigontech/erigon/cmd/utils"
+	"github.com/anacrolix/missinggo/v2/panicif"
 	"github.com/erigontech/erigon/common/dir"
-	"github.com/erigontech/erigon/common/log/v3"
-	"github.com/erigontech/erigon/db/datadir"
+	"github.com/erigontech/erigon/db/datadir/reset"
 	"github.com/erigontech/erigon/db/kv"
 	"github.com/erigontech/erigon/db/kv/dbcfg"
 	"github.com/erigontech/erigon/db/kv/mdbx"
 	"github.com/erigontech/erigon/db/rawdb"
-	"github.com/erigontech/erigon/db/snapcfg"
 	"github.com/erigontech/erigon/execution/chain"
+	"github.com/urfave/cli/v2"
+
+	"github.com/erigontech/erigon/cmd/utils"
+	"github.com/erigontech/erigon/common/log/v3"
+	"github.com/erigontech/erigon/db/datadir"
+	"github.com/erigontech/erigon/db/snapcfg"
 )
 
 var (
@@ -38,6 +36,12 @@ var (
 		Value:    false,
 		Aliases:  []string{"n"},
 		Category: "Reset",
+	}
+	preverifiedFlag = cli.StringFlag{
+		Name:     "preverified",
+		Category: "Reset",
+		Usage:    "preverified to use (remote, local, embedded)",
+		Value:    "remote",
 	}
 )
 
@@ -91,15 +95,28 @@ func resetCliAction(cliCtx *cli.Context) (err error) {
 		return fmt.Errorf("failed to lock data dir %v: %w", dirs.DataDir, err)
 	}
 	defer unlock()
-	err = snapcfg.LoadRemotePreverified(cliCtx.Context)
-	if err != nil {
-		// TODO: Check if we should continue? What if we ask for a git revision and
-		// can't get it? What about a branch? Can we reset to the embedded snapshot hashes?
-		return fmt.Errorf("loading remote preverified snapshots: %w", err)
+
+	switch value := preverifiedFlag.Get(cliCtx); value {
+	case "local":
+		panicif.Err(os.Setenv(snapcfg.RemotePreverifiedEnvKey, dirs.PreverifiedPath()))
+		fallthrough
+	case "remote":
+		err = snapcfg.LoadRemotePreverified(cliCtx.Context)
+		if err != nil {
+			// TODO: Check if we should continue? What if we ask for a git revision and
+			// can't get it? What about a branch? Can we reset to the embedded snapshot hashes?
+			return fmt.Errorf("loading remote preverified snapshots: %w", err)
+		}
+	case "embedded":
+		// Should already be loaded.
+	default:
+		err = fmt.Errorf("invalid preverified flag value %q", value)
+		return
 	}
+
 	cfg, known := snapcfg.KnownCfg(chainName)
 	if !known {
-		// Wtf does this even mean?
+		// Wtf does this even imply?
 		return fmt.Errorf("config for chain %v is not known", chainName)
 	}
 	// Should we check cfg.Local? We could be resetting to the preverified.toml...?
@@ -108,61 +125,41 @@ func resetCliAction(cliCtx *cli.Context) (err error) {
 		"len", len(cfg.Preverified.Items),
 		"chain", chainName,
 	)
-	removeFunc := func(path string) error {
-		logger.Debug("Removing snapshot dir file", "path", path)
-		return dir.RemoveFile(filepath.Join(dirs.Snap, path))
-	}
+
 	if dryRun {
-		removeFunc = dryRunRemove
+		log.Warn("Resetting datadir in dry run mode. Files that would be removed will be printed to stdout.")
 	}
-	reset := reset{
-		removeUnknown: removeLocal,
-		logger:        logger,
+
+	// Here we intended to have a list of os.Root to restrict deletions. Instead, for now you should
+	// do a dry run, and make sure to use good permissioning.
+	//datadirOsRoot, err := os.OpenRoot(dirs.DataDir)
+	//if err != nil {
+	//	return fmt.Errorf("opening datadir: %w", err)
+	//}
+
+	r := reset.Reset{
+		Dirs:                 &dirs,
+		RemoveUnknown:        removeLocal,
+		Logger:               logger,
+		PreverifiedSnapshots: cfg.Preverified.Items,
+		RemoveFunc: func(osName reset.OsFilePath) error {
+			if dryRun {
+				println(osName)
+				return nil
+			}
+			logger.Debug("Removing datadir file", "name", osName)
+			//return datadirOsRoot.Remove(string(osName))
+			return dir.RemoveFile(string(osName))
+		},
 	}
-	logger.Info("Resetting snapshots directory", "path", dirs.Snap)
-	err = reset.walkSnapshots(dirs.Snap, cfg.Preverified.Items, removeFunc)
+	err = r.Run()
 	if err != nil {
-		err = fmt.Errorf("walking snapshots: %w", err)
 		return
 	}
-	logger.Info("Files NOT removed from snapshots directory",
-		"torrents", reset.stats.retained.torrentFiles,
-		"data", reset.stats.retained.dataFiles)
-	logger.Info("Files removed from snapshots directory",
-		"torrents", reset.stats.removed.torrentFiles,
-		"data", reset.stats.removed.dataFiles)
-	// Remove chaindata last, so that the config is available if there's an error.
-	if removeLocal {
-		for _, extraDir := range []string{
-			dbcfg.HeimdallDB,
-			dbcfg.PolygonBridgeDB,
-		} {
-			extraFullPath := filepath.Join(dirs.DataDir, extraDir)
-			err = dir.RemoveAll(extraFullPath)
-			if err != nil {
-				return fmt.Errorf("removing extra dir %q: %w", extraDir, err)
-			}
-		}
-		logger.Info("Removing chaindata dir", "path", dirs.Chaindata)
-		if !dryRun {
-			err = dir.RemoveAll(dirs.Chaindata)
-		}
-		if err != nil {
-			err = fmt.Errorf("removing chaindata dir: %w", err)
-			return
-		}
+	if !dryRun {
+		logger.Info("Reset complete. Start Erigon as usual, missing files will be downloaded.")
 	}
-	err = removeFunc(datadir.PreverifiedFileName)
-	if err == nil {
-		logger.Info("Removed snapshots lock file", "path", datadir.PreverifiedFileName)
-	} else {
-		if !errors.Is(err, fs.ErrNotExist) {
-			err = fmt.Errorf("removing snapshot lock file: %w", err)
-			return
-		}
-	}
-	logger.Info("Reset complete. Start Erigon as usual, missing files will be downloaded.")
-	return nil
+	return
 }
 
 func getChainNameFromChainData(cliCtx *cli.Context, logger log.Logger, chainDataDir string) (_ g.Option[string], err error) {
@@ -202,117 +199,4 @@ func getChainNameFromChainData(cliCtx *cli.Context, logger log.Logger, chainData
 		return
 	}
 	return g.Some(chainCfg.ChainName), nil
-}
-
-func dryRunRemove(path string) error {
-	return nil
-}
-
-type resetStats struct {
-	torrentFiles int
-	dataFiles    int
-	unknownFiles int
-}
-
-type reset struct {
-	logger        log.Logger
-	removeUnknown bool
-	stats         struct {
-		removed  resetStats
-		retained resetStats
-	}
-}
-
-type resetItemInfo struct {
-	path          string
-	realFilePath  func() string
-	hash          g.Option[string]
-	isTorrent     bool
-	inPreverified bool
-}
-
-// Walks the given snapshots directory, removing files that are not in the preverified set.
-func (me *reset) walkSnapshots(
-	// Could almost pass fs.FS here except metainfo.LoadFromFile expects a string filepath.
-	snapDir string,
-	preverified snapcfg.PreverifiedItems,
-	// path is the relative path to the walk root. Called for each file that should be removed.
-	// Error is passed back to the walk function.
-	remove func(path string) error,
-) error {
-	return fs.WalkDir(
-		os.DirFS(snapDir),
-		".",
-		func(path string, d fs.DirEntry, err error) error {
-			if err != nil {
-				// Our job is to remove anything that shouldn't be here... so if we can't read a dir
-				// we are in trouble.
-				return fmt.Errorf("error walking path %v: %w", path, err)
-			}
-			if d.IsDir() {
-				return nil
-			}
-			if path == datadir.PreverifiedFileName {
-				return nil
-			}
-			// Shouldn't be necessary with fs package.
-			slashPath := filepath.ToSlash(path)
-			itemName, _ := strings.CutSuffix(slashPath, ".part")
-			itemName, isTorrent := strings.CutSuffix(itemName, ".torrent")
-			item, ok := preverified.Get(itemName)
-			doRemove := me.decideRemove(resetItemInfo{
-				path:          path,
-				realFilePath:  func() string { return filepath.Join(snapDir, path) },
-				hash:          func() g.Option[string] { return g.OptionFromTuple(item.Hash, ok) }(),
-				isTorrent:     isTorrent,
-				inPreverified: ok,
-			})
-			stats := &me.stats.retained
-			if doRemove {
-				stats = &me.stats.removed
-				err = remove(path)
-				if err != nil {
-					return fmt.Errorf("removing file %v: %w", path, err)
-				}
-			}
-			if isTorrent {
-				stats.torrentFiles++
-			} else {
-				stats.dataFiles++
-			}
-			return nil
-		},
-	)
-}
-
-// Decides whether to remove a file, and logs the reasoning.
-func (me *reset) decideRemove(file resetItemInfo) bool {
-	logger := me.logger
-	path := file.path
-	if !file.inPreverified {
-		logger.Debug("file NOT in preverified list", "path", path)
-		return me.removeUnknown
-	}
-	if file.isTorrent {
-		mi, err := metainfo.LoadFromFile(file.realFilePath())
-		if err != nil {
-			logger.Error("error loading metainfo file", "path", path, "err", err)
-			return true
-		}
-		expectedHash := file.hash.Unwrap()
-		if mi.HashInfoBytes().String() == expectedHash {
-			logger.Debug("torrent file matches preverified hash", "path", path)
-			return false
-		} else {
-			logger.Debug("torrent file infohash does NOT match preverified",
-				"path", path,
-				"expected", expectedHash,
-				"actual", mi.HashInfoBytes())
-			return true
-		}
-	} else {
-		// No checks required. Downloader will clobber it into shape after reset on next run.
-		logger.Debug("data file is in preverified", "path", path)
-		return false
-	}
 }

cmd/utils/app/snapshots_cmd.go
@@ -281,6 +281,7 @@ var snapshotCommand = cli.Command{
 				&utils.DataDirFlag,
 				&dryRunFlag,
 				&removeLocalFlag,
+				&preverifiedFlag,
 			},
 		},
 		{

common/dir/rw_dir.go
@@ -63,11 +63,12 @@ func trackRemovedFiles() {
 	}
 }
 
+// user rwx, group rwx, other rx
+// x is required to navigate through directories. umask 0o022 is the default and will mask final
+// permissions to 0o755 for newly created files (and directories).
+const DirPerm = 0o775
+
 func MustExist(path ...string) {
-	// user rwx, group rwx, other rx
-	// x is required to navigate through directories. umask 0o022 is the default and will mask final
-	// permissions to 0o755 for newly created files (and directories).
-	const perm = 0o775
 	for _, p := range path {
 		exist, err := Exist(p)
 		if err != nil {
@@ -76,7 +77,7 @@ func MustExist(path ...string) {
 		if exist {
 			continue
 		}
-		if err := os.MkdirAll(p, perm); err != nil {
+		if err := os.MkdirAll(p, DirPerm); err != nil {
 			panic(err)
 		}
 	}

db/datadir/dirs.go
@@ -88,6 +88,11 @@ func New(datadir string) Dirs {
 	return dirs
 }
 
+// The subdirectory in the datadir for snapshots. This isn't encoded anywhere else because it's not
+// an MDBX name, but also a bunch of other datadir subdirs aren't encoded in Dirs, and Dirs does
+// absolute path stuff I don't want.
+const SnapDir = "snapshots"
+
 // Open new Dirs instance without forcing all the directories to exist.
 func Open(datadir string) Dirs {
 	relativeDataDir := datadir
@@ -105,13 +110,13 @@ func Open(datadir string) Dirs {
 		DataDir:          datadir,
 		Chaindata:        filepath.Join(datadir, "chaindata"),
 		Tmp:              filepath.Join(datadir, "temp"),
-		Snap:             filepath.Join(datadir, "snapshots"),
-		SnapIdx:          filepath.Join(datadir, "snapshots", "idx"),
-		SnapHistory:      filepath.Join(datadir, "snapshots", "history"),
-		SnapDomain:       filepath.Join(datadir, "snapshots", "domain"),
-		SnapAccessors:    filepath.Join(datadir, "snapshots", "accessor"),
-		SnapCaplin:       filepath.Join(datadir, "snapshots", "caplin"),
-		SnapForkable:     filepath.Join(datadir, "snapshots", "forkable"),
+		Snap:             filepath.Join(datadir, SnapDir),
+		SnapIdx:          filepath.Join(datadir, SnapDir, "idx"),
+		SnapHistory:      filepath.Join(datadir, SnapDir, "history"),
+		SnapDomain:       filepath.Join(datadir, SnapDir, "domain"),
+		SnapAccessors:    filepath.Join(datadir, SnapDir, "accessor"),
+		SnapCaplin:       filepath.Join(datadir, SnapDir, "caplin"),
+		SnapForkable:     filepath.Join(datadir, SnapDir, "forkable"),
 		Downloader:       filepath.Join(datadir, "downloader"),
 		TxPool:           filepath.Join(datadir, "txpool"),
 		Nodes:            filepath.Join(datadir, "nodes"),

db/datadir/reset/chaindata
@@ -1,0 +1,1 @@
+symlink

db/datadir/reset/newtypes.go
@@ -1,0 +1,58 @@
+package reset
+
+// Type wrappers to ensure valid runtime values.
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/anacrolix/missinggo/v2/panicif"
+)
+
+type (
+	// A confirmed OS file-path.
+	OsFilePath string
+	// A valid slash name.
+	slashName string
+)
+
+func (me OsFilePath) mustLocalRelSlash(base OsFilePath) slashName {
+	rel, err := filepath.Rel(base.ToString(), me.ToString())
+	panicif.Err(err)
+	if !filepath.IsLocal(rel) {
+		panic(fmt.Sprintf("not local: %q, obtained from filepath.Rel(%q, %q)", rel, base, me))
+	}
+	return slashName(filepath.ToSlash(rel))
+}
+
+func (me OsFilePath) MustRel(base OsFilePath) OsFilePath {
+	rel, err := filepath.Rel(string(base), string(me))
+	panicif.Err(err)
+	return OsFilePath(rel)
+}
+
+func (me OsFilePath) Join(other OsFilePath) OsFilePath {
+	return OsFilePath(filepath.Join(string(me), string(other)))
+}
+
+// Joins two paths, preferring the latter if it's absolute. Common behaviour in other languages.
+func (me OsFilePath) JoinClobbering(other OsFilePath) OsFilePath {
+	if filepath.IsAbs(string(other)) {
+		return other
+	}
+	return OsFilePath(filepath.Join(string(me), string(other)))
+}
+
+func (me OsFilePath) ToString() string {
+	return string(me)
+}
+
+func (me slashName) MustLocalize() OsFilePath {
+	fp, err := filepath.Localize(string(me))
+	panicif.Err(err)
+	return OsFilePath(fp)
+}
+
+func (me slashName) FromSlash() OsFilePath {
+	return OsFilePath(filepath.FromSlash(string(me)))
+}

db/datadir/reset/remove-all.go
@@ -1,0 +1,99 @@
+package reset
+
+import (
+	"errors"
+	"fmt"
+	"io/fs"
+	"os"
+
+	"github.com/erigontech/erigon/common/log/v3"
+)
+
+// Removes the contents of directories, and symlinks to *non-directories*. Non-directory symlink
+// targets will be cleaned up as appropriate if the target is found. We also remove anything else.
+type removeAll struct {
+	logger     log.Logger
+	root       OsFilePath
+	removeFunc removeAllRemoveFunc
+	warnNoRoot bool
+}
+
+// Calls the provided removeFunc which does filtering, stat collection and permission checks
+// determined by the caller.
+func (me *removeAll) remove(name OsFilePath, info os.FileInfo) error {
+	return me.removeFunc(name, info)
+}
+
+// Removes the contents of a directory. Does not remove the directory.
+func (me *removeAll) dir(name OsFilePath) error {
+	entries, err := os.ReadDir(string(name))
+	if err != nil {
+		if errors.Is(err, fs.ErrNotExist) {
+			err = nil
+		}
+		return err
+	}
+	for _, de := range entries {
+		info, err := de.Info()
+		if err != nil {
+			if errors.Is(err, fs.ErrNotExist) {
+				return nil
+			}
+			return err
+		}
+		fullName := name.Join(OsFilePath(de.Name()))
+		err = me.inner(fullName, info)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Remove name if appropriate.
+func (me *removeAll) inner(name OsFilePath, fi fs.FileInfo) error {
+	switch modeType := fi.Mode().Type(); modeType {
+	case fs.ModeDir:
+		err := me.dir(name)
+		if err != nil {
+			return err
+		}
+		// We don't super care if directories fail to get removed.
+		if err := me.remove(name, fi); err != nil {
+			// Should handle the case where it's a mount point.
+			me.logger.Debug("Error removing directory", "name", name, "err", err)
+		}
+		return nil
+	case fs.ModeSymlink:
+		targetInfo, err := os.Stat(string(name))
+		if err != nil {
+			// Dangling symlinks are bad because we can't decide if we should remove them because we
+			// want to preserve links to directories.
+			return fmt.Errorf("statting symlink target: %w", err)
+		}
+		if targetInfo.IsDir() {
+			// Remove the contents only
+			return me.dir(name)
+		} else {
+			// Remove the link itself
+			return me.remove(name, fi)
+		}
+	default:
+		return me.remove(name, fi)
+	}
+}
+
+func (me *removeAll) do() error {
+	println(me.root)
+	info, err := os.Lstat(string(me.root))
+	if err != nil {
+		if errors.Is(err, fs.ErrNotExist) {
+			if me.warnNoRoot {
+				me.logger.Warn("Error removing top-level", "root", me.root, "err", err)
+			}
+			return nil
+		}
+		return err
+	}
+	return me.inner(me.root, info)
+}

db/datadir/reset/reset.go
@@ -1,0 +1,211 @@
+package reset
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io/fs"
+	"os"
+	"strings"
+
+	"github.com/erigontech/erigon/common/log/v3"
+	"github.com/erigontech/erigon/db/preverified"
+
+	g "github.com/anacrolix/generics"
+	"github.com/anacrolix/torrent/metainfo"
+	"github.com/erigontech/erigon/db/datadir"
+	"github.com/erigontech/erigon/db/kv/dbcfg"
+)
+
+type Stats struct {
+	TorrentFiles int
+	DataFiles    int
+	UnknownFiles int
+}
+
+// Configuration struct to perform datadir resets.
+type Reset struct {
+	Logger               log.Logger
+	RemoveFunc           func(name OsFilePath) error
+	PreverifiedSnapshots preverified.SortedItems
+	RemoveUnknown        bool
+	RemoveLocal          bool
+	Dirs                 *datadir.Dirs
+
+	stats struct {
+		removed  Stats
+		retained Stats
+	}
+}
+
+func (reset *Reset) Run() (err error) {
+	logger := reset.Logger
+	logger.Info("Resetting snapshots directory", "path", reset.pathForLog(datadir.SnapDir))
+	err = reset.doSnapshots()
+	if err != nil {
+		err = fmt.Errorf("resetting snapshots: %w", err)
+		return
+	}
+	logger.Info("Files NOT removed from snapshots directory",
+		"torrents", reset.stats.retained.TorrentFiles,
+		"data", reset.stats.retained.DataFiles)
+	logger.Info("Files removed from snapshots directory",
+		"torrents", reset.stats.removed.TorrentFiles,
+		"data", reset.stats.removed.DataFiles)
+	// Remove chaindata last, so that the config is available if there's an error.
+	if reset.RemoveLocal {
+		for _, extraDir := range []slashName{
+			dbcfg.HeimdallDB,
+			dbcfg.PolygonBridgeDB,
+		} {
+			// Probably shouldn't log these unless they existed, it would confuse the user for
+			// unrelated chains.
+			ra := reset.makeRemoveAll(reset.dataDirOsPath().Join(extraDir.MustLocalize()))
+			ra.warnNoRoot = false
+			err = ra.do()
+			if err != nil {
+				return fmt.Errorf("removing extra dir %q: %w", extraDir, err)
+			}
+		}
+		logger.Info("Removing chaindata dir", "path", reset.pathForLog(dbcfg.ChainDB))
+		ra := reset.makeRemoveAll(slashName(dbcfg.ChainDB).MustLocalize())
+		err = ra.do()
+		if err != nil {
+			err = fmt.Errorf("removing chaindata dir: %w", err)
+			return
+		}
+	}
+	err = reset.remove(OsFilePath(reset.Dirs.PreverifiedPath()))
+	if err == nil {
+		logger.Info("Removed snapshots lock file", "path", datadir.PreverifiedFileName)
+	} else {
+		if !errors.Is(err, fs.ErrNotExist) {
+			err = fmt.Errorf("removing snapshot lock file: %w", err)
+			return
+		}
+	}
+	return nil
+}
+
+func (reset *Reset) dataDirOsPath() OsFilePath {
+	return OsFilePath(reset.Dirs.DataDir)
+}
+
+func (reset *Reset) remove(name OsFilePath) error {
+	return reset.RemoveFunc(reset.dataDirOsPath().JoinClobbering(name))
+}
+
+func (reset *Reset) makeRemoveAll(root OsFilePath) removeAll {
+	return removeAll{
+		logger: reset.Logger,
+		removeFunc: func(name OsFilePath, info fs.FileInfo) error {
+			return reset.RemoveFunc(name)
+		},
+		root:       reset.dataDirOsPath().JoinClobbering(root),
+		warnNoRoot: true,
+	}
+}
+
+type removeAllRemoveFunc func(name OsFilePath, info os.FileInfo) error
+
+func (me *removeAll) wrapRemove(wrapper func(inner removeAllRemoveFunc, name OsFilePath, info os.FileInfo) error) {
+	inner := me.removeFunc
+	me.removeFunc = func(name OsFilePath, info os.FileInfo) error {
+		return wrapper(inner, name, info)
+	}
+}
+
+// Probably want to render full/real name rather than rooted inside fs.
+func (reset *Reset) pathForLog(path string) string {
+	return path
+}
+
+type resetItemInfo struct {
+	filePath OsFilePath
+	// For logging, decision is already made based on this.
+	snapName      string
+	hash          g.Option[string]
+	isTorrent     bool
+	inPreverified bool
+}
+
+func (me *Reset) doSnapshots() (err error) {
+	snapDir := me.dataDirOsPath().Join(datadir.SnapDir)
+	ra := me.makeRemoveAll(snapDir)
+	ra.wrapRemove(func(inner removeAllRemoveFunc, filePath OsFilePath, info fs.FileInfo) error {
+		itemName := string(filePath.mustLocalRelSlash(snapDir))
+		itemName, _ = strings.CutSuffix(itemName, ".part")
+		itemName, isTorrent := strings.CutSuffix(itemName, ".torrent")
+		item, ok := me.PreverifiedSnapshots.Get(itemName)
+		doRemove := me.decideRemove(resetItemInfo{
+			filePath:      filePath,
+			snapName:      itemName,
+			hash:          func() g.Option[string] { return g.OptionFromTuple(item.Hash, ok) }(),
+			isTorrent:     isTorrent,
+			inPreverified: ok,
+		})
+		stats := &me.stats.retained
+		if doRemove {
+			stats = &me.stats.removed
+			err = inner(filePath, info)
+			if err != nil {
+				return fmt.Errorf("removing file %v: %w", filePath, err)
+			}
+		}
+		if isTorrent {
+			stats.TorrentFiles++
+		} else {
+			stats.DataFiles++
+		}
+		return nil
+	})
+	// Skip to the contents of the snapshots dir, we don't expect this directory to be empty after
+	// traversal.
+	return ra.dir(snapDir)
+}
+
+// Decides whether to remove a file, and logs the reasoning.
+func (me *Reset) decideRemove(file resetItemInfo) bool {
+	logger := me.Logger
+	name := file.snapName
+	if !file.inPreverified {
+		if !me.RemoveUnknown {
+			logger.Debug("skipping unknown file", "name", name)
+		}
+		return me.RemoveUnknown
+	}
+	// TODO: missing or incorrect torrent delete data file?
+	if file.isTorrent {
+		mi, err := me.loadMetainfoFromFile(file.filePath)
+		if err != nil {
+			logger.Error("error loading metainfo file", "name", name, "err", err)
+			return true
+		}
+		expectedHash := file.hash.Unwrap()
+		if mi.HashInfoBytes().String() == expectedHash {
+			logger.Debug("torrent file matches preverified hash", "name", name)
+			return false
+		} else {
+			logger.Debug("removing metainfo file with incorrect infohash",
+				"name", name,
+				"expected", expectedHash,
+				"actual", mi.HashInfoBytes())
+			return true
+		}
+	} else {
+		// No checks required. Downloader will clobber it into shape after reset on next run.
+		logger.Debug("skipping expected snapshot", "name", name)
+		return false
+	}
+}
+
+func (me *Reset) loadMetainfoFromFile(path OsFilePath) (mi *metainfo.MetaInfo, err error) {
+	f, err := os.Open(string(path))
+	if err != nil {
+		return
+	}
+	defer f.Close()
+	var buf bufio.Reader
+	buf.Reset(f)
+	return metainfo.Load(&buf)
+}

commit 6a0153e5387458567735a0f5d79cdb333f9e70c6

cp: fix seg reset not removing chaindata (#20908) (#20919)
- \`seg reset\` regressed in #18273: \`removeLocal\` from the CLI is
wired into \`reset.Reset.RemoveUnknown\` but not \`RemoveLocal\`, which
gates chaindata + Heimdall/PolygonBridge DB removal in
\`db/datadir/reset/reset.go\`. Result: \`seg reset\` no longer touches
chaindata at all.
- Library-level \`TestResetChaindata\` exists but sets \`RemoveLocal:
true\` directly, so the CLI plumbing was uncovered.

closes https://github.com/erigontech/erigon/issues/20260

cmd/utils/app/reset-datadir.go
@@ -121,6 +121,7 @@ func resetCliAction(cliCtx *cli.Context) (err error) {
 	r := reset.Reset{
 		Dirs:                 &dirs,
 		RemoveUnknown:        removeLocal,
+		RemoveLocal:          removeLocal,
 		Logger:               logger,
 		PreverifiedSnapshots: cfg.Preverified.Items,
 		RemoveFunc: func(osName reset.OsFilePath) error {