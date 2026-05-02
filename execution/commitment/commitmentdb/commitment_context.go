ce7b4d20dfbdb89fd8d73ca6dfa0afdc6d3a973a
Alexey Sharov <AskAlexSharov@gmail.com>
2026-05-02 15:21:15 +0700

    save

--- execution/commitment/commitmentdb/commitment_context.go
+++ execution/commitment/commitmentdb/commitment_context.go
@@ -288,10 +288,8 @@ func (sdc *SharedDomainsCommitmentContext) ComputeCommitment(ctx context.Context
 	updateCount := sdc.updates.Size()
 	start := time.Now()
 	defer func() {
-		took := time.Since(start)
-		var keysPerSec uint64
-		if took > 50*time.Millisecond {
-			keysPerSec = uint64(float64(updateCount) / took.Seconds())
+		if took := time.Since(start); took > 50*time.Millisecond {
+			keysPerSec := uint64(float64(updateCount) / took.Seconds())
 			log.Debug("[commitment] processed", "block", blockNum, "txNum", txNum, "keys", common.PrettyCounter(updateCount), "keys/s", common.PrettyCounter(keysPerSec), "mode", sdc.updates.Mode(), "spent", took, "rootHash", hex.EncodeToString(rootHash))
 		}
 	}()
