35026d1c77f5ad3e0e34ac923df483eda93cf754
Alexey Sharov <AskAlexSharov@gmail.com>
2026-05-29 12:44:35 +0700

    save

--- db/state/domain.go
+++ db/state/domain.go
@@ -402,7 +402,7 @@ func (dt *DomainRoTx) newWriter(tmpdir string, discard bool) *DomainBufferedWrit
 	}
 	if !discard {
 		w.values = etl.NewCollectorWithAllocator(dt.d.Name.String()+"domain.flush", tmpdir, etl.SmallSortableBuffers, dt.d.logger).
-			LogLvl(log.LvlTrace).SortAndFlushInBackground(true)
+			LogLvl(log.LvlTrace).SortAndFlushInBackground(false)
 	}
 	return w
 }
