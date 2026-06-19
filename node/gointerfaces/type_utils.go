7d70ca28085f06099a9de0055fe4795032d2bf91
Alexey Sharov <AskAlexSharov@gmail.com>
2026-06-19 10:22:39 +0700

    p2p, gointerfaces: reduce Sprintf and H256/H512/H2048 allocs
    - Replace fmt.Sprintf in hot paths with structured log args and strconv
    - Cache metrics.Gauge pointers in protoRW instead of re-acquiring lock+map on every message
    - Guard trackPeerStatistics call sites with diag enabled check to avoid unconditional string allocs
    - Pack H512/H256/H2048 nested proto messages into single arena allocations (7→1, 3→1 allocs)
    - Add BenchmarkBlockRpcConversion to measure round-trip allocs

--- node/gointerfaces/type_utils.go
+++ node/gointerfaces/type_utils.go
@@ -33,17 +33,32 @@ func ConvertH2048ToBloom(h2048 *typesproto.H2048) [256]byte {
 	return bloom
 }
 
+// h2048Arena holds an H2048 and all nested messages in one allocation.
+type h2048Arena struct {
+	h  typesproto.H2048
+	hi typesproto.H1024
+	lo typesproto.H1024
+	s0 h512Arena
+	s1 h512Arena
+	s2 h512Arena
+	s3 h512Arena
+}
+
 func ConvertBytesToH2048(data []byte) *typesproto.H2048 {
-	return &typesproto.H2048{
-		Hi: &typesproto.H1024{
-			Hi: ConvertBytesToH512(data),
-			Lo: ConvertBytesToH512(data[64:]),
-		},
-		Lo: &typesproto.H1024{
-			Hi: ConvertBytesToH512(data[128:]),
-			Lo: ConvertBytesToH512(data[192:]),
-		},
+	if len(data) < 256 {
+		var d [256]byte
+		copy(d[:], data)
+		data = d[:]
 	}
+	a := &h2048Arena{}
+	fillH512Arena(&a.s0, data[0:])
+	fillH512Arena(&a.s1, data[64:])
+	fillH512Arena(&a.s2, data[128:])
+	fillH512Arena(&a.s3, data[192:])
+	a.hi = typesproto.H1024{Hi: &a.s0.h, Lo: &a.s1.h}
+	a.lo = typesproto.H1024{Hi: &a.s2.h, Lo: &a.s3.h}
+	a.h = typesproto.H2048{Hi: &a.hi, Lo: &a.lo}
+	return &a.h
 }
 
 func ConvertH256ToHash(h256 *typesproto.H256) [32]byte {
@@ -68,11 +83,19 @@ func ConvertH512ToHash(h512 *typesproto.H512) [64]byte {
 	return b
 }
 
+// h256Arena holds an H256 and all nested messages in one allocation.
+type h256Arena struct {
+	h  typesproto.H256
+	hi typesproto.H128
+	lo typesproto.H128
+}
+
 func ConvertHashToH256(hash [32]byte) *typesproto.H256 {
-	return &typesproto.H256{
-		Lo: &typesproto.H128{Lo: binary.BigEndian.Uint64(hash[24:]), Hi: binary.BigEndian.Uint64(hash[16:])},
-		Hi: &typesproto.H128{Lo: binary.BigEndian.Uint64(hash[8:]), Hi: binary.BigEndian.Uint64(hash[0:])},
-	}
+	a := &h256Arena{}
+	a.hi = h128(hash[0:])
+	a.lo = h128(hash[16:])
+	a.h = typesproto.H256{Hi: &a.hi, Lo: &a.lo}
+	return &a.h
 }
 
 func ConvertHashToH512(hash [64]byte) *typesproto.H512 {
@@ -109,15 +132,42 @@ func ConvertUint256IntToH256(i *uint256.Int) *typesproto.H256 {
 		return nil
 	}
 	// Note: uint256.Int is an array of 4 uint64 in little-endian order, i.e. most significant word is [3]
-	return &typesproto.H256{
-		Lo: &typesproto.H128{Lo: i[0], Hi: i[1]},
-		Hi: &typesproto.H128{Lo: i[2], Hi: i[3]},
-	}
+	a := &h256Arena{}
+	a.lo = typesproto.H128{Lo: i[0], Hi: i[1]}
+	a.hi = typesproto.H128{Lo: i[2], Hi: i[3]}
+	a.h = typesproto.H256{Lo: &a.lo, Hi: &a.hi}
+	return &a.h
 }
 
 func ConvertH512ToBytes(h512 *typesproto.H512) []byte {
 	b := ConvertH512ToHash(h512)
 	return b[:]
+}
+
+// h512Arena holds an H512 and all nested messages in one allocation.
+type h512Arena struct {
+	h    typesproto.H512
+	hi   typesproto.H256
+	lo   typesproto.H256
+	hihi typesproto.H128
+	hilo typesproto.H128
+	lohi typesproto.H128
+	lolo typesproto.H128
+}
+
+// h128 reads 16 bytes from b and returns an H128.
+func h128(b []byte) typesproto.H128 {
+	return typesproto.H128{Hi: binary.BigEndian.Uint64(b), Lo: binary.BigEndian.Uint64(b[8:])}
+}
+
+func fillH512Arena(a *h512Arena, b []byte) {
+	a.hihi = h128(b[0:])
+	a.hilo = h128(b[16:])
+	a.lohi = h128(b[32:])
+	a.lolo = h128(b[48:])
+	a.hi = typesproto.H256{Hi: &a.hihi, Lo: &a.hilo}
+	a.lo = typesproto.H256{Hi: &a.lohi, Lo: &a.lolo}
+	a.h = typesproto.H512{Hi: &a.hi, Lo: &a.lo}
 }
 
 func ConvertBytesToH512(b []byte) *typesproto.H512 {
@@ -126,14 +176,7 @@ func ConvertBytesToH512(b []byte) *typesproto.H512 {
 		copy(b1[:], b)
 		b = b1[:]
 	}
-	return &typesproto.H512{
-		Lo: &typesproto.H256{
-			Lo: &typesproto.H128{Lo: binary.BigEndian.Uint64(b[56:]), Hi: binary.BigEndian.Uint64(b[48:])},
-			Hi: &typesproto.H128{Lo: binary.BigEndian.Uint64(b[40:]), Hi: binary.BigEndian.Uint64(b[32:])},
-		},
-		Hi: &typesproto.H256{
-			Lo: &typesproto.H128{Lo: binary.BigEndian.Uint64(b[24:]), Hi: binary.BigEndian.Uint64(b[16:])},
-			Hi: &typesproto.H128{Lo: binary.BigEndian.Uint64(b[8:]), Hi: binary.BigEndian.Uint64(b[0:])},
-		},
-	}
+	a := &h512Arena{}
+	fillH512Arena(a, b)
+	return &a.h
 }
