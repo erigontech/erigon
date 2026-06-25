ef0a7031cf724cca322de77f86b1295b1a18c89c
Andrew Ashikhmin <34320705+yperbasis@users.noreply.github.com>
2026-06-25 12:20:02 +0200

    execution/engineapi: address Copilot review on files test
    Add the standard LGPL header and assert eat.Close() succeeds (matching
    the other engineapi tests) instead of discarding its error.

--- execution/engineapi/engine_api_state_churn_files_test.go
+++ execution/engineapi/engine_api_state_churn_files_test.go
@@ -1,3 +1,19 @@
+// Copyright 2026 The Erigon Authors
+// This file is part of Erigon.
+//
+// Erigon is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Erigon is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Erigon. If not, see <http://www.gnu.org/licenses/>.
+
 package engineapi_test
 
 import (
@@ -69,7 +85,7 @@ func TestEngineApiUnwindAcrossSnapshotFileBoundary(t *testing.T) {
 		},
 	})
 	require.NoError(t, err)
-	t.Cleanup(func() { _ = eat.Close() })
+	t.Cleanup(func() { require.NoError(t, eat.Close()) })
 
 	eat.Run(t, func(ctx context.Context, t *testing.T, eat engineapitester.EngineApiTester) {
 		const pokes = 300
