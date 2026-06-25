bfed72f37fc2e77094c5aa25b955fdf00e8e4191
yperbasis <andrey.ashikhmin@gmail.com>
2026-06-25 16:25:29 +0200

    [r3.5] db/version: app version 3.5.0

--- db/version/app.go
+++ db/version/app.go
@@ -33,7 +33,7 @@ const (
 	Major                    = 3             // Major version component of the current release
 	Minor                    = 5             // Minor version component of the current release
 	Micro                    = 0             // Patch version component of the current release
-	Modifier                 = "dev"         // Modifier component of the current release
+	Modifier                 = ""            // Modifier component of the current release
 	DefaultSnapshotGitBranch = "release/3.4" // Branch of erigontech/erigon-snapshot to use in OtterSync.
 	VersionKeyCreated        = "ErigonVersionCreated"
 	VersionKeyFinished       = "ErigonVersionFinished"
