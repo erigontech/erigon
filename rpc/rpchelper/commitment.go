1514c0f073dc481fe1887ec7a25b98ae96c1a0e8
Andrew Ashikhmin <34320705+yperbasis@users.noreply.github.com>
2026-07-03 12:42:07 +0200

    rpc/jsonrpc, rpc/rpchelper: WithoutBranchCache for the remaining RPC-side SharedDomains
    RPC request SDs run outside the exec-module semaphore; keep them all off
    the shared BranchCache uniformly, as getProof/getWitness already are.

--- rpc/rpchelper/commitment.go
+++ rpc/rpchelper/commitment.go
@@ -94,7 +94,7 @@ func (r *CommitmentReplay) ComputeCustomCommitmentFromStateHistory(
 	}
 	defer ttx.Rollback()
 
-	tsd, err := execctx.NewSharedDomains(ctx, ttx, r.logger, execctx.WithoutDeferredBranchUpdates(), execctx.WithSequentialCommitment())
+	tsd, err := execctx.NewSharedDomains(ctx, ttx, r.logger, execctx.WithoutDeferredBranchUpdates(), execctx.WithoutBranchCache(), execctx.WithSequentialCommitment())
 	if err != nil {
 		return nil, err
 	}
