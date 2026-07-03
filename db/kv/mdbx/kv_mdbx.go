da8b68a8629a8c1ff1861ac48eb861749e1cb701
Alexey Sharov <AskAlexSharov@gmail.com>
2026-07-03 10:13:52 +0700

    save

--- db/kv/mdbx/kv_mdbx.go
+++ db/kv/mdbx/kv_mdbx.go
@@ -356,7 +356,7 @@ func (opts MdbxOpts) Open(ctx context.Context) (_ kv.RwDB, err error) {
 		//if err = env.SetSyncBytes(128 * 1024 * 1024); err != nil {
 		//	return nil, err
 		//}
-		if err = env.SetSyncPeriod(time.Second); err != nil {
+		if err = env.SetSyncPeriod(time.Minute); err != nil {
 			return nil, err
 		}
 		if err = env.SetSyncBytes(8 * 1024 * 1024 * 1024); err != nil {
