bc8065a56397f27982e069d94138f3c7d81b7b86
Alexey Sharov <AskAlexSharov@gmail.com>
2026-07-10 08:57:42 +0700

    db/snapshotsync/freezeblocks: don't open a read-tx just for FirstTxnNumNotInSnapshots in DumpBlocks

--- db/snapshotsync/freezeblocks/block_snapshots.go
+++ db/snapshotsync/freezeblocks/block_snapshots.go
@@ -570,13 +570,7 @@ func (br *BlockRetire) DisableReadAhead() {
 }
 
 func DumpBlocks(ctx context.Context, blockFrom, blockTo uint64, chainConfig *chain.Config, tmpDir, snapDir string, chainDB kv.RoDB, workers int, lvl log.Lvl, logger log.Logger, blockReader services.FullBlockReader, snCfg *snapcfg.Cfg, inProgress *snapshotsync.BaseRoSnapshots) error {
-	var firstTxNum uint64
-	if err := chainDB.View(ctx, func(tx kv.Tx) error {
-		firstTxNum = blockReader.FirstTxnNumNotInSnapshots(tx)
-		return nil
-	}); err != nil {
-		return err
-	}
+	firstTxNum := blockReader.FirstTxnNumNotInSnapshots(nil)
 
 	for i := blockFrom; i < blockTo; i = chooseSegmentEnd(i, blockTo, snaptype2.Enums.Headers, snCfg) {
 		lastTxNum, err := dumpBlocksRange(ctx, i, chooseSegmentEnd(i, blockTo, snaptype2.Enums.Headers, snCfg), tmpDir, snapDir, firstTxNum, chainDB, chainConfig, workers, lvl, logger, inProgress)
