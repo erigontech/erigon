24f36a83fe02063d428d68d5ae3521f3d1040e8e
Sagar Rana <sagarrana.pvt@gmail.com>
2026-07-24 22:36:18 +0530

    response path endpoint

--- rpc/sszql/ssz_ql_parser.go
+++ rpc/sszql/ssz_ql_parser.go
@@ -1,6 +1,12 @@
 package sszql
 
 func parseQuery(request SSZQLRequest, version int, block_id string) SSZQLResponse {
-	var res SSZQLResponse
-	return res
+
+	var response SSZQLResponse
+
+	for _, query := range request.Queries {
+		response.Paths = append(response.Paths, query.Path)
+	}
+
+	return response
 }
