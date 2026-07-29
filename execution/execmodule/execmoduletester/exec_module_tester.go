3a50d09f3a025329d20cbeb7ff577615c53492a7
Alexey Sharov <askalexsharov@gmail.com>
2026-07-29 12:10:02 +0700

    execution/execmodule, node/eth: rename mining-cancel channel to sealCancel
    It only aborts an in-flight engine.Seal; the old names overstated
    its scope.

--- execution/execmodule/execmoduletester/exec_module_tester.go
+++ execution/execmodule/execmoduletester/exec_module_tester.go
@@ -673,7 +673,7 @@ func New(tb testing.TB, opts ...Option) *ExecModuleTester {
 	snapDownloader := mockDownloader(ctrl, mock.Dirs.Snap)
 
 	// Never closed: finishBlock sends to it concurrently to abort an in-flight seal.
-	miningCancel := make(chan struct{})
+	sealCancel := make(chan struct{})
 
 	readAheader := exec.NewBlockReadAheader()
 	blkBuilder := builder.NewBuilder(
@@ -704,7 +704,7 @@ func New(tb testing.TB, opts ...Option) *ExecModuleTester {
 		&vm.Config{},
 		dirs.Tmp,
 		mock.TxPool,
-		miningCancel,
+		sealCancel,
 		latestBlockBuiltStore,
 		nil, /*sdProvider*/
 		logger,
