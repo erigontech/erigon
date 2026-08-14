785aa176cda3a5868c38338110ceff505df55e48
taratorio <94537774+taratorio@users.noreply.github.com>
2026-08-14 10:29:17 +0000

    execution: move GetValSize to kv.TemporalTx interface

--- db/state/execctx/domain_shared.go
+++ db/state/execctx/domain_shared.go
@@ -1619,14 +1619,7 @@ func (sd *SharedDomains) getLatestValSize(domain kv.Domain, tx kv.TemporalTx, k 
 		return 0, false, false, nil
 	}
 
-	type valSizeGetter interface {
-		GetLatestValSize(domain kv.Domain, k []byte, tx kv.Tx) (int, bool, error)
-	}
-	getter, ok := tx.AggTx().(valSizeGetter)
-	if !ok {
-		return 0, false, false, nil
-	}
-	size, found, err = getter.GetLatestValSize(domain, k, tx)
+	size, found, err = tx.GetLatestValSize(domain, k)
 	return size, found, true, err
 }
 
