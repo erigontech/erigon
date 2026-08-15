67d62a07be61189faacfe47316d8dd066a2b70fb
Alexey Sharov <askalexsharov@gmail.com>
2026-08-15 10:02:01 +0700

    db/state/execctx: move the rpc-cache tests to a sibling execctx_test pkg
    Also name the aura tests package auratests.

--- db/state/execctx_test/statecache_rpc_integration_test.go
+++ db/state/execctx_test/statecache_rpc_integration_test.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with Erigon. If not, see <http://www.gnu.org/licenses/>.
 
-package execctxtests
+package execctx_test
 
 import (
 	"testing"
