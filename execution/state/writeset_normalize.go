855c38f36209df749863e9bdfb0490888d1a7896
Alexey Sharov <askalexsharov@gmail.com>
2026-08-23 11:56:23 +0700

    save

--- execution/state/writeset_normalize.go
+++ execution/state/writeset_normalize.go
@@ -118,7 +118,7 @@ func (writes *WriteSet) Normalize(vm *VersionMap, blockNum uint64, txIndex int, 
 	normalizeStarted := time.Now()
 	defer func() {
 		took := time.Since(normalizeStarted)
-		if took > 100*time.Millisecond {
+		if took > 1*time.Millisecond {
 			mxNormalizeTook.Observe(took.Seconds())
 		}
 		if took >= slowNormalizeThreshold {
