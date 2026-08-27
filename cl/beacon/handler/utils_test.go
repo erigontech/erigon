5acc334b625345a3da16ab540348ad52a2470da6
kewei <kewei.train@gmail.com>
2026-08-27 20:41:27 +0800

    cl/beacon: update block service test fixture

--- cl/beacon/handler/utils_test.go
+++ cl/beacon/handler/utils_test.go
@@ -132,6 +132,7 @@ func setupTestingHandler(t *testing.T, v clparams.StateVersion, logger log.Logge
 	blockService.EXPECT().CommitGossipReservation(gomock.Any()).AnyTimes()
 	blockService.EXPECT().ReleaseGossipReservation(gomock.Any()).AnyTimes()
 	blockService.EXPECT().ScheduleBlockForLaterProcessing(gomock.Any()).AnyTimes()
+	blockService.EXPECT().SchedulePublishedBlockForLaterProcessing(gomock.Any(), gomock.Any()).AnyTimes()
 
 	// ctx context.Context, subnetID *uint64, msg *cltypes.SyncCommitteeMessage) error
 	syncCommitteeMessagesService.EXPECT().ProcessMessage(gomock.Any(), gomock.Any(), gomock.Any()).DoAndReturn(func(ctx context.Context, subnetID *uint64, msg *services.SyncCommitteeMessageForGossip) error {
