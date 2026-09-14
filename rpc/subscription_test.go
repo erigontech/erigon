5061cd3392debc57e67fdb8f581a0964ad69a0a5
Alexey Sharov <askalexsharov@gmail.com>
2026-09-14 16:43:23 +0700

    rpc, execution/types: encode log subscription notifications with MarshalFastJSON

--- rpc/subscription_test.go
+++ rpc/subscription_test.go
@@ -235,3 +235,19 @@ func readAndValidateMessage(in *json.Decoder) (*subConfirmation, *subscriptionRe
 		return nil, nil, fmt.Errorf("unrecognized message: %v", msg)
 	}
 }
+
+type fastJSONPayload struct{}
+
+func (fastJSONPayload) MarshalFastJSON() ([]byte, error) { return []byte(`"fast"`), nil }
+
+func TestNotifyUsesFastJSON(t *testing.T) {
+	t.Parallel()
+
+	n := &RemoteNotifier{sub: &Subscription{ID: "0x1"}}
+	if err := n.Notify("0x1", fastJSONPayload{}); err != nil {
+		t.Fatal(err)
+	}
+	if len(n.buffer) != 1 || string(n.buffer[0]) != `"fast"` {
+		t.Fatalf("want the MarshalFastJSON encoding, got %q", n.buffer)
+	}
+}
