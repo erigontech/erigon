85cf6b58a24cc553ec12b3a5ac82983cd36eb76c
Alexey Sharov <askalexsharov@gmail.com>
2026-09-16 10:22:57 +0700

    Revert "Merge remote-tracking branch 'origin/alex/rpc_http_inline_call_37' into alex/rpc_all_37_dbg"
    This reverts commit 55c08df4b0ee80e1c3b3bc647fc706796438138b, reversing
    changes made to 0950dc1780a9b356e64a8e2e8e780add46a5b8fe.

--- rpc/handler.go
+++ rpc/handler.go
@@ -442,16 +442,17 @@ func (h *handler) cancelServerSubscriptions(err error) {
 
 // startCallProc runs fn in a new goroutine tracked by h.callWG, or on the caller's goroutine when inlineCalls is set.
 func (h *handler) startCallProc(fn func(*callProc)) {
-	run := func() {
-		ctx, cancel := context.WithCancel(h.rootCtx)
-		defer cancel()
-		fn(&callProc{ctx: ctx})
-	}
 	if h.inlineCalls {
-		run()
+		h.runCallProc(fn)
 		return
 	}
-	h.callWG.Go(run)
+	h.callWG.Go(func() { h.runCallProc(fn) })
+}
+
+func (h *handler) runCallProc(fn func(*callProc)) {
+	ctx, cancel := context.WithCancel(h.rootCtx)
+	defer cancel()
+	fn(&callProc{ctx: ctx})
 }
 
 // handleImmediate executes non-call messages. It returns false if the message is a
