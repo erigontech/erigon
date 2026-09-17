30c26cce8b34eca73e60e78101b8da60b2fa6ece
Alexey Sharov <askalexsharov@gmail.com>
2026-09-17 15:14:31 +0700

    rpc/jsonstream: keep WriteHex output in place instead of copying it through WriteRawBytes
    WriteRawBytes appended the value, which already sat in the buffer tail, onto itself.

--- rpc/jsonstream/stack_stream.go
+++ rpc/jsonstream/stack_stream.go
@@ -93,8 +93,13 @@ func (s *StackStream) WriteHex(b []byte) {
 	buf := s.stream.Buffer()
 	start := len(buf)
 	buf = hexutil.AppendQuoted(slices.Grow(buf, hexutil.QuotedLen(len(b))), b)
-	s.stream.SetBuffer(buf[:start])
-	s.WriteRawBytes(buf[start:])
+	if s.out != nil && len(buf)-start >= FlushThreshold {
+		s.stream.SetBuffer(buf[:start])
+		s.writeThrough(buf[start:])
+	} else {
+		s.stream.SetBuffer(buf)
+	}
+	s.popCommaOrField()
 }
 
 // writeThrough drains what is buffered and hands content to the writer. The
