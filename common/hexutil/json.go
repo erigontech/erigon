3f982257e6c9849494b535498caf646841ce2078
Alexey Sharov <askalexsharov@gmail.com>
2026-09-17 15:40:43 +0700

    common/hexutil: format Uint64 as hex with a nibble loop
    strconv.AppendUint base 16 goes through the generic formatBits.

--- common/hexutil/json.go
+++ common/hexutil/json.go
@@ -24,6 +24,7 @@ import (
 	"encoding/json"
 	"errors"
 	"math/big"
+	"math/bits"
 	"reflect"
 	"strconv"
 
@@ -217,7 +218,14 @@ func (b Uint64) MarshalText() ([]byte, error) {
 
 // AppendText implements encoding.TextAppender (alloc-free MarshalText).
 func (b Uint64) AppendText(dst []byte) ([]byte, error) {
-	return strconv.AppendUint(append(dst, `0x`...), uint64(b), 16), nil
+	dst = append(dst, HexPrefix...)
+	if b == 0 {
+		return append(dst, '0'), nil
+	}
+	for shift := (bits.Len64(uint64(b))+3)/4*4 - 4; shift >= 0; shift -= 4 {
+		dst = append(dst, "0123456789abcdef"[b>>uint(shift)&0xf])
+	}
+	return dst, nil
 }
 
 // UnmarshalJSON implements json.Unmarshaler.
