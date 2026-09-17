17962b06f7c84e43207f4a9024a220a70b1cdbfd
Alexey Sharov <askalexsharov@gmail.com>
2026-09-17 09:46:08 +0700

    common/hexutil: serve the Bytes stream benchmark through rpc.Server like eth_getCode

--- common/hexutil/json_bench_test.go
+++ common/hexutil/json_bench_test.go
@@ -19,15 +19,20 @@
 package hexutil_test
 
 import (
+	"bytes"
+	"context"
 	"encoding/json/v2"
-	"io"
+	"net/http"
+	"net/http/httptest"
 	"slices"
 	"testing"
 
 	"github.com/holiman/uint256"
 
+	"github.com/erigontech/erigon/common"
 	"github.com/erigontech/erigon/common/hexutil"
-	"github.com/erigontech/erigon/rpc/jsonstream"
+	"github.com/erigontech/erigon/common/log/v3"
+	"github.com/erigontech/erigon/rpc"
 )
 
 func BenchmarkUnmarshalBig(b *testing.B) {
@@ -74,6 +79,21 @@ type discardJSONWriter struct{ buf []byte }
 func (w *discardJSONWriter) AvailableBuffer(n int) []byte { return slices.Grow(w.buf[:0], n) }
 func (w *discardJSONWriter) WriteRawBytes(v []byte)       { w.buf = v[:0] }
 
+type codeAPI struct{ code hexutil.Bytes }
+
+func (api *codeAPI) GetCode(context.Context, common.Address, *rpc.BlockNumberOrHash) (hexutil.Bytes, error) {
+	return api.code, nil
+}
+
+type discardResponseWriter struct {
+	header http.Header
+	n      int
+}
+
+func (w *discardResponseWriter) Header() http.Header         { return w.header }
+func (w *discardResponseWriter) WriteHeader(int)             {}
+func (w *discardResponseWriter) Write(b []byte) (int, error) { w.n += len(b); return len(b), nil }
+
 // BenchmarkBytesMarshalJSON compares a 64KB eth_getCode result encoded by json/v2 vs MarshalFastJSONTo.
 func BenchmarkBytesMarshalJSON(b *testing.B) {
 	code := make(hexutil.Bytes, 64*1024)
@@ -112,15 +132,25 @@ func BenchmarkBytesMarshalJSON(b *testing.B) {
 	})
 
 	b.Run("fast_v2_stream", func(b *testing.B) {
-		stream := jsonstream.Get(io.Discard)
-		defer jsonstream.Put(stream)
-
+		srv := rpc.NewServer(1, false, false, false, log.New(), 0)
+		if err := srv.RegisterName("eth", &codeAPI{code: code}); err != nil {
+			b.Fatal(err)
+		}
+		body := []byte(`{"jsonrpc":"2.0","id":1,"method":"eth_getCode","params":["0xdac17f958d2ee523a2206206994597c13d831ec7","latest"]}`)
+		w := &discardResponseWriter{header: http.Header{}}
+		serve := func() {
+			r := httptest.NewRequest(http.MethodPost, "/", bytes.NewReader(body))
+			r.Header.Set("content-type", "application/json")
+			srv.ServeHTTP(w, r)
+		}
+		serve()
+		if want := len(`{"jsonrpc":"2.0","id":1,"result":}`+"\n") + int(size); w.n != want {
+			b.Fatalf("response is %d bytes, want %d", w.n, want)
+		}
 		b.SetBytes(size)
 		b.ReportAllocs()
 		for b.Loop() {
-			if err := code.MarshalFastJSONTo(stream); err != nil {
-				b.Fatal(err)
-			}
+			serve()
 		}
 	})
 
