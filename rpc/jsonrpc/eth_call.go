2ff634f2af967966f93593ab8378c2a64bb6fc12
Alexey Sharov <askalexsharov@gmail.com>
2026-09-17 07:49:15 +0700

    rpc/jsonrpc: build the eth_getProof storage key without a hex round trip

--- rpc/jsonrpc/eth_call.go
+++ rpc/jsonrpc/eth_call.go
@@ -520,7 +520,7 @@ func (api *APIImpl) getProof(ctx context.Context, roTx kv.TemporalTx, address co
 
 	sdCtx.TouchKey(kv.AccountsDomain, string(address[:]), nil)
 	for _, storageKey := range storageKeys {
-		sdCtx.TouchKey(kv.StorageDomain, string(common.FromHex(address.Hex()[2:]+storageKey.Hash.String()[2:])), nil)
+		sdCtx.TouchKey(kv.StorageDomain, string(address[:])+string(storageKey.Hash[:]), nil)
 	}
 
 	// generate the trie for proofs, this works by loading the merkle paths to the touched keys
