54cbeea0983c378bea9b6606d8cf2be6d65e1cd0
Alexey Sharov <askalexsharov@gmail.com>
2026-09-20 13:41:39 +0700

    rpc/jsonstream: one generic hex-array writer per core type

--- rpc/ethapi/block_fastjson.go
+++ rpc/ethapi/block_fastjson.go
@@ -201,7 +201,7 @@ func writeHashes(w jsonw.JSONWriter, name string, hashes []common.Hash) {
 		return
 	}
 	jsonw.Field(w, name)
-	s.WriteHexes(hashes)
+	jsonstream.WriteHexes(s, hashes)
 }
 
 // writeTxElem never fails: RPCTransaction.MarshalFastJSONTo reports no error.
