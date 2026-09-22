6bd488d6ec4ce4127f5b4a345cfadbe131887d9e
Alexey Sharov <askalexsharov@gmail.com>
2026-09-22 15:16:09 +0700

    db/rawdb, execution/types: a header read by its hash keeps that hash
    Hash() no longer hashes the RLP again, e.g. for every eth_getTransactionByHash.
    
    (cherry picked from commit eaf02e6199581ceb783709b77896f9514a4ad919)

--- db/rawdb/accessors_chain.go
+++ db/rawdb/accessors_chain.go
@@ -362,7 +362,7 @@ func ReadHeader(db kv.Getter, hash common.Hash, number uint64) *types.Header {
 		log.Error("Invalid block header RLP", "hash", hash, "number", number, "err", err)
 		return nil
 	}
-	return header
+	return types.NewHeaderFromStorage(hash, header)
 }
 
 func ReadCurrentBlockNumber(db kv.Getter) *uint64 {
