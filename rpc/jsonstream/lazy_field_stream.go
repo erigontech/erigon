8c08c7843c0765f4b76d1d7a9f8f02ea0ef51407
Alexey Sharov <askalexsharov@gmail.com>
2026-09-22 11:18:51 +0700

    rpc/jsonstream: markSeparatorPending is part of Stream; a buffered empty notification is tested

--- rpc/jsonstream/lazy_field_stream.go
+++ rpc/jsonstream/lazy_field_stream.go
@@ -72,7 +72,7 @@ func (s *LazyFieldStream) ensure() {
 		s.written = true
 		s.mark, s.markDepth = len(s.inner.Buffer()), s.inner.Depth()
 		if s.prependSeparator {
-			markSeparator(s.inner)
+			s.inner.markSeparatorPending()
 		}
 		s.owner = s.inner.WriteObjectField(s.field)
 		s.openDepth = uint(s.inner.Depth() - 1)
@@ -167,11 +167,4 @@ func (s *LazyFieldStream) Reset(out io.Writer) {
 	s.written = false
 }
 
-func (s *LazyFieldStream) markSeparatorPending() { markSeparator(s.inner) }
-
-// separatorMarker is the streams that write separators themselves. It stays off Stream so
-// an implementation outside this package still satisfies it.
-type separatorMarker interface{ markSeparatorPending() }
-
-// markSeparator asserts a sibling precedes the next value.
-func markSeparator(s Stream) { s.(separatorMarker).markSeparatorPending() }
+func (s *LazyFieldStream) markSeparatorPending() { s.inner.markSeparatorPending() }
