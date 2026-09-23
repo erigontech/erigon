8ba0c17dd0ec419109b890d6677c00a6a22512ef
Alexey Sharov <askalexsharov@gmail.com>
2026-09-23 08:06:10 +0700

    execution/types: keep one Withdrawal fast-JSON encoder after the merge

--- execution/types/withdrawal.go
+++ execution/types/withdrawal.go
@@ -40,20 +40,6 @@ type Withdrawal struct {
 	Amount    hexutil.Uint64 `json:"amount"`         // value of withdrawal in GWei
 }
 
-func (obj *Withdrawal) MarshalFastJSONTo(s *jsonstream.StackStream) error {
-	if obj == nil {
-		s.WriteNil()
-		return nil
-	}
-	s.WriteObjectStart()
-	jsonstream.Text(s, "index", &obj.Index)
-	jsonstream.Text(s, "validatorIndex", &obj.Validator)
-	s.Field("address").WriteHex(obj.Address[:])
-	jsonstream.Text(s, "amount", &obj.Amount)
-	s.WriteObjectEnd()
-	return nil
-}
-
 func (obj *Withdrawal) EncodingSize() int {
 	encodingSize := 21 /* Address */
 	encodingSize += rlp.U64Len(uint64(obj.Index))
